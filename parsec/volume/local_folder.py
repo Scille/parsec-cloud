73bc881b1470353faab504b19f10ea9a3daf034f
Antoine Cardon <ant.cardon@gmail.com>
2017-02-09 11:02:58 +0100

    Work in progess. Still some tests tests to write.

--- parsec/volume/local_folder.py
+++ parsec/volume/local_folder.py
@@ -1,4 +1,6 @@
 import os
+from uuid import uuid4
+from json import dumps, loads
 from google.protobuf.message import DecodeError
 
 from ..abstract import BaseService
@@ -12,34 +14,114 @@ class CmdError(Exception):
         self.status_code = status_code
 
 
+class LocalFolderServiceException(CmdError):
+    pass
+
+
 def _clean_path(path):
     return '/' + '/'.join([e for e in path.split('/') if e])
 
 
 class LocalFolderVolumeService(BaseService):
 
-    def __init__(self, mock_path: str):
-        assert mock_path.startswith('/'), '`mock_path` must be absolute'
-        self.mock_path = _clean_path(mock_path)
+    """ This service might be used with a local folder you wan tto encrypt.
+    It could also be used by mapping a dropbox or a google drive folder to an other part
+    of the FS in order to let the third party softwar handle the synchronisation between
+    the local storage and the cloud.
+    """
 
-    def cmd_READ_FILE(self, cmd):
-        raise NotImplementedError()
+    def __init__(self, path: str):
+        self._initialized = False
+        path = _clean_path(path)
+        self._path = os.path.abspath(os.path.expanduser("~" + path)) + '/'
 
-    def cmd_WRITE_FILE(self, cmd):
-        raise NotImplementedError()
+    def _initialize_driver(self):
+        """ Looks up Parsec system files one the cloud and load their ID into
+        the Driver class instance. This Function also loads the mapping between
+        virtual IDs used by the VFS and the Physical IDs used on the cloud side.
+        Sets the initialized attribute to the according boolean value.
+        WARNING: You MUST not use the driver if not successfully initialized.
+            Returns:
+                None
+            Raises:
+                LocalFolderServiceException if a system file is not found
+        """
+        if self._initialized:
+            return
+        # Load the mapping file
+        try:
+            with open(self._path + 'MAP', 'br') as mapf:
+                self._mapping = loads(mapf.read().decode())
+        except (EnvironmentError, ValueError):
+            self._mapping = {}
+        self._initialized = True
 
-    def cmd_DELETE_FILE(self, path):
-        raise NotImplementedError()
+    def _sync(self):
+        try:
+            with open(self._path + 'MAP', 'bw') as mapf:
+                self._mapping = mapf.write(dumps(self._mapping).encode())
+        except (EnvironmentError, ValueError):
+            raise LocalFolderServiceException('Cannot save mapping file')
+
+    def _create_driver_files(self):
+        pass
+
+    def initialize_driver(self, force=False):
+        try:
+            self._initialize_driver()
+        except LocalFolderServiceException:
+            if force:
+                self._create_driver_files()
+        finally:
+            self._initialize_driver()
+
+    def _read_file(self, msg):
+        file_id = self._mapping.get(msg.vid)
+        if file_id is None:
+            return Response(status_code=Response.FILE_NOT_FOUND)
+        try:
+            with open(self._path + file_id, 'rb') as f:
+                file_content = f.read()
+        except IOError:
+            return Response(status_code=Response.FILE_NOT_FOUND)
+        return Response(status_code=Response.OK, content=file_content)
+
+    def _write_file(self, msg):
+        vid = msg.vid
+        if vid is None:
+            raise LocalFolderServiceException('A VID is mandatory')
+        file_id = self._mapping.get(vid)
+        if file_id is None:
+            pid = uuid4().hex
+            self._mapping[msg.vid] = pid
+        try:
+            with open(self._path + pid, 'wb')as f:
+                f.write(msg.content)
+        except IOError:
+            raise LocalFolderServiceException('Cannot write file')
+        self._sync()
+        return Response(status_code=Response.OK)
+
+    def _delete_file(self, msg):
+        file_id = self._mapping.get(msg.vid)
+        if file_id is not None:
+            os.remove(self._path + file_id)
+            del self._mapping[msg.vid]
+            return Response(status_code=Response.OK)
+        self._sync()
+        return Response(status_code=Response.FILE_NOT_FOUND)
+
+    _CMD_MAP = {
+        Request.READ_FILE: _read_file,
+        Request.WRITE_FILE: _write_file,
+        Request.DELETE_FILE: _delete_file,
+    }
 
     def dispatch_msg(self, msg):
         try:
-            if msg.type == Request.READ_FILE:
-                return self.cmd_READ_FILE(msg)
-            elif msg.type == Request.WRITE_FILE:
-                return self.cmd_WRITE_FILE(msg)
-            elif msg.type == Request.DELETE_FILE:
-                return self.cmd_DELETE_FILE(msg)
-            else:
+            try:
+                return self._CMD_MAP[msg.type](self, msg)
+            except KeyError:
                 raise CmdError('Unknown msg `%s`' % msg.type)
         except CmdError as exc:
             return Response(status_code=exc.status_code, error_msg=exc.error_msg)
