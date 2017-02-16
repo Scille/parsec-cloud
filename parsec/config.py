620e88eefe8bea3f3bbb1566e3f879c39a3a85a1
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2017-02-16 17:37:42 +0100

    Add -m and -V options to parsec cmd

--- parsec/config.py
+++ parsec/config.py
@@ -1,6 +1,7 @@
 import os
 import inspect
 import yaml
+import traceback
 from uuid import uuid4
 from importlib import import_module
 
@@ -11,6 +12,20 @@ TOP_LEVEL_FIELDS = {'servers', 'services', 'clients'}
 class ConfigError(Exception):
     def __init__(self, errors):
         self.errors = errors
+
+    def dump(self):
+
+        def _recursive_dump(error_dict, prefix=''):
+            errors = []
+            for key, error in error_dict.items():
+                error_path = '%s/%s' % (prefix, key)
+                if isinstance(error, dict):
+                    errors += _recursive_dump(error, prefix=error_path)
+                else:
+                    errors.append('[%s] %s' % (error_path, error))
+            return errors
+
+        return '\n'.join(_recursive_dump(self.errors))
 
 
 class Topology:
@@ -98,145 +113,153 @@ class LazyObj(BaseLazy):
         return deps
 
 
-def _retrieve_by_import(path):
-    path = path if path.startswith('parsec.') else 'parsec.' + path
-    module_path, name = path.rsplit('.', 1)
-    try:
-        module = import_module(module_path)
-        return getattr(module, name)
-    except (ImportError, AttributeError):
-        raise ConfigError("Unknown ressource `%s`." % path)
+class TopologyLoader:
 
+    def __init__(self, verbose=False):
+        self.verbose = verbose
 
-def _check_fields(data, required=None, optional=None):
-    errors = []
-    data_fields = set(data.keys())
-    required_fields = set(required or ())
-    allowed_fields = required_fields | set(optional or ())
-    unknown_fields = data_fields - allowed_fields
-    if unknown_fields:
-        errors.append('Unknown fields: %s.' % unknown_fields)
-    missing_fields = required_fields - data_fields
-    if missing_fields:
-        errors.append('Missing fields: %s.' % missing_fields)
-    type_checks = {}
-    if isinstance(required, dict):
-        type_checks.update(required)
-    if isinstance(optional, dict):
-        type_checks.update(optional)
-    for k, t in type_checks.items():
-        if k in data and not isinstance(data[k], t):
-            errors.append('Field `%s` should be of type `%s`' % (k, t))
-    if errors:
-        raise ConfigError(errors)
-
-
-def _build_obj_from_config(data):
-    """
-    Config example::
-
-        {
-            "id": "vfs-server-01",
-            "class": "vfs.VFSServer",
-            "params": {"mount_point": "/tmp/foo"}
-        }
-
-    """
-    _check_fields(data, required={'class': str}, optional={'id': str, 'params': dict})
-    cls = _retrieve_by_import(data['class'])
-    raw_params = data.get('params', {})
-    params = _cook_params_from_annotations(cls.__init__, raw_params)
-    return LazyObj(data.get('id', uuid4().hex), cls, **params)
-
-
-def _cook_params_from_annotations(builder, params):
-    argspec = inspect.getfullargspec(builder)
-    anns = argspec.annotations
-    unknown_fields = set(params.keys()) - set(anns.keys())
-    if unknown_fields:
-        raise ConfigError('Unknown fields: %s.' % unknown_fields)
-    with_default_fields = set()
-    if argspec.kwonlydefaults:
-        with_default_fields |= set(argspec.kwonlydefaults)
-    if argspec.args and argspec.defaults:
-        with_default_fields |= set(k for k, _ in zip(reversed(argspec.args), argspec.defaults))
-    missing_fields = set(anns.keys()) - set(params.keys()) - {'return'} - with_default_fields
-    if missing_fields:
-        raise ConfigError('Missing fields: %s.' % missing_fields)
-    cooked = {}
-    errors = []
-    for k, v in params.items():
-        t = anns[k]
-        if t in (int, float, str, list, dict):
-            if isinstance(v, str) and v.startswith('$'):
-                # Handle env var here
-                # If env var is not defined, use default value if available
-                env_var = v[1:]
-                if env_var in os.environ or k not in with_default_fields:
-                    try:
-                        cooked[k] = t(os.environ.get(v[1:], ''))
-                    except TypeError:
-                        errors.append('Field `%s` should be of type `%s`' % (k, t))
-            elif isinstance(v, t):
-                # Default json/yaml types
-                cooked[k] = v
-            else:
-                errors.append('Field `%s` should be of type `%s`' % (k, t))
-        else:
-            try:
-                # Otherwise consider the value is a python path to a parsec resource
-                if isinstance(v, dict):
-                    # Build object from the given dict
-                    cooked[k] = _build_obj_from_config(v)
-                elif isinstance(v, str):
-                    # Retrieve object
-                    cooked[k] = LazyRef(v, field=k, expected_type=t)
-                else:
-                    errors.append('Field `%s` should resolve to type `%s`' % (k, t))
-            except ConfigError as exc:
-                errors.append(exc.errors)
-    if errors:
-        raise ConfigError(errors)
-    return cooked
-
-
-def _parse_list_objs(config_list):
-    objs = []
-    errors = {}
-    for i, obj_config in enumerate(config_list):
+    def _retrieve_by_import(self, path):
+        path = path if path.startswith('parsec.') else 'parsec.' + path
+        module_path, name = path.rsplit('.', 1)
         try:
-            objs.append(_build_obj_from_config(obj_config))
+            module = import_module(module_path)
+            return getattr(module, name)
+        except (ImportError, AttributeError):
+            msg = "Unknown ressource `%s`." % path
+            if self.verbose:
+                msg += '\nError:\n' + traceback.format_exc()
+            raise ConfigError(msg)
+
+    def _check_fields(self, data, required=None, optional=None):
+        errors = []
+        data_fields = set(data.keys())
+        required_fields = set(required or ())
+        allowed_fields = required_fields | set(optional or ())
+        unknown_fields = data_fields - allowed_fields
+        if unknown_fields:
+            errors.append('Unknown fields: %s.' % unknown_fields)
+        missing_fields = required_fields - data_fields
+        if missing_fields:
+            errors.append('Missing fields: %s.' % missing_fields)
+        type_checks = {}
+        if isinstance(required, dict):
+            type_checks.update(required)
+        if isinstance(optional, dict):
+            type_checks.update(optional)
+        for k, t in type_checks.items():
+            if k in data and not isinstance(data[k], t):
+                errors.append('Field `%s` should be of type `%s`' % (k, t))
+        if errors:
+            raise ConfigError(errors)
+
+    def _build_obj_from_config(self, data):
+        """
+        Config example::
+
+            {
+                "id": "vfs-server-01",
+                "class": "vfs.VFSServer",
+                "params": {"mount_point": "/tmp/foo"}
+            }
+
+        """
+        self._check_fields(data, required={'class': str}, optional={'id': str, 'params': dict})
+        cls = self._retrieve_by_import(data['class'])
+        raw_params = data.get('params', {})
+        params = self._cook_params_from_annotations(cls.__init__, raw_params)
+        return LazyObj(data.get('id', uuid4().hex), cls, **params)
+
+    def _cook_params_from_annotations(self, builder, params):
+        argspec = inspect.getfullargspec(builder)
+        anns = argspec.annotations
+        unknown_fields = set(params.keys()) - set(anns.keys())
+        if unknown_fields:
+            raise ConfigError('Unknown fields: %s.' % unknown_fields)
+        with_default_fields = set()
+        if argspec.kwonlydefaults:
+            with_default_fields |= set(argspec.kwonlydefaults)
+        if argspec.args and argspec.defaults:
+            with_default_fields |= set(k for k, _ in zip(reversed(argspec.args), argspec.defaults))
+        missing_fields = set(anns.keys()) - set(params.keys()) - {'return'} - with_default_fields
+        if missing_fields:
+            raise ConfigError('Missing fields: %s.' % missing_fields)
+        cooked = {}
+        errors = []
+        for k, v in params.items():
+            t = anns[k]
+            if t in (int, float, str, list, dict):
+                if isinstance(v, str) and v.startswith('$'):
+                    # Handle env var here
+                    # If env var is not defined, use default value if available
+                    env_var = v[1:]
+                    if env_var in os.environ or k not in with_default_fields:
+                        try:
+                            cooked[k] = t(os.environ.get(v[1:], ''))
+                        except TypeError:
+                            errors.append('Field `%s` should be of type `%s`' % (k, t))
+                elif isinstance(v, t):
+                    # Default json/yaml types
+                    cooked[k] = v
+                else:
+                    errors.append('Field `%s` should be of type `%s`' % (k, t))
+            else:
+                try:
+                    # Otherwise consider the value is a python path to a parsec resource
+                    if isinstance(v, dict):
+                        # Build object from the given dict
+                        cooked[k] = self._build_obj_from_config(v)
+                    elif isinstance(v, str):
+                        # Retrieve object
+                        cooked[k] = LazyRef(v, field=k, expected_type=t)
+                    else:
+                        errors.append('Field `%s` should resolve to type `%s`' % (k, t))
+                except ConfigError as exc:
+                    errors.append(exc.errors)
+        if errors:
+            raise ConfigError(errors)
+        return cooked
+
+    def _parse_list_objs(self, config_list):
+        objs = []
+        errors = {}
+        for i, obj_config in enumerate(config_list):
+            try:
+                objs.append(self._build_obj_from_config(obj_config))
+            except ConfigError as exc:
+                errors[i] = exc.errors
+        if errors:
+            raise ConfigError(errors)
+        return objs
+
+    def load(self, raw_yaml):
+        self._check_fields(raw_yaml, optional={k: list for k in TOP_LEVEL_FIELDS})
+        topology = Topology()
+        errors = {}
+        try:
+            for service in self._parse_list_objs(raw_yaml.get('services')):
+                topology.add_service(service.id, service)
         except ConfigError as exc:
-            errors[i] = exc.errors
-    if errors:
-        raise ConfigError(errors)
-    return objs
+            errors['services'] = exc.errors
+        try:
+            for client in self._parse_list_objs(raw_yaml.get('clients')):
+                topology.add_client(client.id, client)
+        except ConfigError as exc:
+            errors['clients'] = exc.errors
+        try:
+            for server in self._parse_list_objs(raw_yaml.get('servers')):
+                topology.add_server(server.id, server)
+        except ConfigError as exc:
+            errors['servers'] = exc.errors
+        if errors:
+            raise ConfigError(errors)
+        return topology
 
 
-def load_config(fp):
-    return loads_config(fp.read())
+def load_config(fp, verbose=False):
+    return loads_config(fp.read(), verbose=verbose)
 
 
-def loads_config(raw_data):
+def loads_config(raw_data, verbose=False):
     raw_yaml = yaml.safe_load(raw_data)
-    _check_fields(raw_yaml, optional={k: list for k in TOP_LEVEL_FIELDS})
-    topology = Topology()
-    errors = {}
-    try:
-        for service in _parse_list_objs(raw_yaml.get('services')):
-            topology.add_service(service.id, service)
-    except ConfigError as exc:
-        errors['services'] = exc.errors
-    try:
-        for client in _parse_list_objs(raw_yaml.get('clients')):
-            topology.add_client(client.id, client)
-    except ConfigError as exc:
-        errors['clients'] = exc.errors
-    try:
-        for server in _parse_list_objs(raw_yaml.get('servers')):
-            topology.add_server(server.id, server)
-    except ConfigError as exc:
-        errors['servers'] = exc.errors
-    if errors:
-        raise ConfigError(errors)
-    return topology
+    loader = TopologyLoader(verbose=verbose)
+    return loader.load(raw_yaml)
--- parsec/startup.py
+++ parsec/startup.py
@@ -1,27 +1,32 @@
-import signal
 import logging
 import argparse
 from multiprocessing import Process
 
-from parsec.config import load_config
+from parsec.config import load_config, ConfigError
 
 
-def boot_servers(servers_factory):
+def boot_servers(servers_factory, main_factory=None):
     processes = []
     for factory in servers_factory:
+        if factory is main_factory:
+            continue
         p = Process(target=factory().start)
         p.start()
         processes.append(p)
 
-    def graceful_exit():
+    try:
+        if main_factory:
+            main_factory().start()
+        else:
+            for p in processes:
+                p.join()
+    except KeyboardInterrupt:
+        print('Trying to gracefully exiting processes... ')
         for p in processes:
             p.terminate()
-
-    signal.signal(signal.SIGINT, graceful_exit)
-    signal.signal(signal.SIGTERM, graceful_exit)
-
-    for p in processes:
-        p.join()
+        for p in processes:
+            p.join()
+        print('Bye ;-)')
 
 
 def execute_from_command_line():
@@ -32,12 +37,26 @@ def execute_from_command_line():
                         help='Log level (default: WARNING)',
                         choices=('DEBUG', 'INFO', 'WARNING', 'ERROR'))
     parser.add_argument('--servers', '-s', nargs='*', help='Only start given servers')
+    parser.add_argument('--verbose', '-V', action='store_true', help="Improve output verbosity")
+    parser.add_argument('--main', '-m',
+                        help='Server to start as main process (useful for debugging)')
     args = parser.parse_args()
     logging.basicConfig(level=getattr(logging, args.loglevel))
-    topology = load_config(args.config)
+    try:
+        topology = load_config(args.config, verbose=args.verbose)
+    except ConfigError as exc:
+        raise SystemExit("Configuration error:\n" + exc.dump())
 
     if args.servers:
         servers_factory = [f for k, f in topology.servers_factory.items() if k in args.servers]
     else:
-        servers_factory = topology.servers_factory.values()
-    boot_servers(servers_factory)
+        servers_factory = list(topology.servers_factory.values())
+    if args.main:
+        try:
+            main_factory = topology.servers_factory[args.main]
+        except KeyError:
+            raise SystemError('Unknown server `%s`' % args.main)
+        servers_factory.remove(main_factory)
+    else:
+        main_factory = None
+    boot_servers(servers_factory, main_factory=main_factory)
