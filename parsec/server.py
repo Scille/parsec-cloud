ea67e0ed36810fd626a9998a65a4239df72f25ac
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2017-02-25 12:08:08 +0100

    GSD: Add Request/Response mechanism to socket connection

--- parsec/server.py
+++ parsec/server.py
@@ -1,22 +1,122 @@
 import os
+import sys
 import asyncio
+from uuid import uuid4
+from base64 import b64decode, b64encode
+from logbook import Logger, StreamHandler
 
 
-async def _connection(reader, writer):
-    print('Connected with client')
-    while True:
-        data = await reader.readline()
-        if not data:
-            print('End of connection')
-            return
-        print("Received:", data.decode())
-        writer.write('teuebe.\n'.encode())
+StreamHandler(sys.stdout).push_application()
+log = Logger('Parsec')
+
+
+class Response:
+    @classmethod
+    def from_raw(cls, data: bytes):
+        assert isinstance(data, bytes)
+        args = data.split(b' ', maxsplit=1)
+        if len(args) == 2:
+            return cls(status=args[0].decode(), raw_body=args[1])
+        else:
+            return cls(status=args[0].decode())
+
+    def __init__(self, status: str='ok', body: bytes=None, raw_body: bytes=None):
+        assert raw_body is None or isinstance(raw_body, bytes)
+        assert isinstance(status, str)
+        if isinstance(body, str):
+            body = body.encode()
+        self.status = status
+        self._body = body
+        self._raw_body = raw_body or b''
+
+    @property
+    def body(self):
+        if self._body is None:
+            self._body = b64decode(self._raw_body)
+        return self._body
+
+    @property
+    def is_ok(self):
+        return self.status == 'ok'
+
+    def pack(self) -> bytes:
+        return self.status.encode() + b' ' + b64encode(self.body)
+
+    def __repr__(self):
+        return '<Response(status=%s, body=%s)>' % (self.status, self.body)
+
+
+class Request:
+    @classmethod
+    def from_raw(cls, data: bytes):
+        assert isinstance(data, bytes)
+        args = data.split(b' ', maxsplit=1)
+        if len(args) == 2:
+            return cls(args[0].decode(), raw_body=args[1])
+        else:
+            return cls(args[0].decode())
+
+    def __init__(self, cmdid: str, body: bytes=None, raw_body: bytes=None):
+        assert raw_body is None or isinstance(raw_body, bytes)
+        assert isinstance(cmdid, str)
+        if isinstance(body, str):
+            body = body.encode()
+        self.cmdid = cmdid
+        self._body = body
+        self._raw_body = raw_body or b''
+
+    @property
+    def body(self):
+        if self._body is None:
+            self._body = b64decode(self._raw_body)
+        return self._body
+
+    def pack(self) -> bytes:
+        return self.cmdid.encode() + b' ' + b64encode(self.body)
+
+    def __repr__(self):
+        return '<Request(cmdid=%s, body=%s)>' % (self.cmdid, self.body)
+
+
+class ParsecServer:
+    def __init__(self):
+        self._cmds = {}
+
+    def register_cmd(self, cmd, cb):
+        if cmd in self._cmds:
+            raise RuntimeError('Command `%s` already registered.' % cmd)
+        self._cmds[cmd] = cb
+
+    @staticmethod
+    def _parse_raw_msg(msg):
+        return msg.split(maxsplit=1)
+
+    async def on_connection(self, reader, writer):
+        conn_log = Logger('Connection ' + uuid4().hex)
+        conn_log.debug('Connection started')
+        while True:
+            raw_req = await reader.readline()
+            if not raw_req:
+                conn_log.debug('Connection stopped')
+                return
+            req = Request.from_raw(raw_req)
+            conn_log.debug('Received: %r' % req)
+            cmd = self._cmds.get(req.cmdid)
+            if not cmd:
+                resp = Response('badcmd', 'Unknown command `%s`' % req.cmdid)
+            else:
+                resp = await cmd(req)
+            conn_log.debug('Replied: %r' % resp)
+            writer.write(resp.pack())
+            writer.write(b'\n')
 
 
 def start_server(socket_path):
     loop = asyncio.get_event_loop()
+    server = ParsecServer()
     try:
-        connect_coro = asyncio.start_unix_server(_connection, path=socket_path, loop=loop)
+        connect_coro = asyncio.start_unix_server(
+            server.on_connection, path=socket_path, loop=loop)
         loop.create_task(connect_coro)
         loop.run_forever()
     finally:
--- parsec/shell.py
+++ parsec/shell.py
@@ -1,19 +1,28 @@
 import asyncio
 import readline  # noqa: side effect powaaa !
 
+from parsec.server import Request, Response
+
 
 async def repl(socket_path):
     reader, writer = await asyncio.open_unix_connection(path=socket_path)
     quit = False
     while not quit:
-        cmd = input(">>> ")
-        if cmd in ('quit', 'q'):
+        data = input(">>> ")
+        if data in ('quit', 'q'):
             writer.close()
             return
-        writer.write(cmd.encode())
+        elif data in ('help', 'h'):
+            print('No help for the braves !')
+            continue
+        args = data.split(' ', maxsplit=1)
+        cmdid = args[0]
+        req = Request(cmdid, body=(args[1].encode() if len(args) == 2 else b''))
+        writer.write(req.pack())
         writer.write(b'\n')
-        resp = await reader.readline()
-        print('Received: %s' % resp)
+        raw_resp = await reader.readline()
+        resp = Response.from_raw(raw_resp[:-1])
+        print('Received: %r' % resp)
 
 
 def start_shell(socket_path):
