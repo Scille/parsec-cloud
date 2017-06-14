8e2491334598b2c82496eee2052761128bbe0a24
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2017-06-14 20:45:49 +0200

    Create BaseIdentityService

--- parsec/core2/identity_service.py
+++ parsec/core2/identity_service.py
@@ -11,17 +11,14 @@ class cmd_IDENTITY_LOAD_Schema(BaseCmdSchema):
     key = fields.String(required=True)
 
 
-class IdentityService(BaseService):
-
+class BaseIdentityService(BaseService):
     name = 'IdentityService'
 
     on_identity_loaded = event('identity_loaded')
     on_identity_unloaded = event('identity_unloaded')
 
-    def __init__(self):
-        super().__init__()
-        self.id = None
-        self.private_key = None
+    id = NotImplemented  # Overwritten by implementations
+    private_key = NotImplemented  # Overwritten by implementations
 
     @cmd('identity_load')
     async def _cmd_IDENTITY_LOAD(self, session, msg):
@@ -33,12 +30,28 @@ class IdentityService(BaseService):
     async def _cmd_IDENTITY_INFO(self, session, msg):
         if not self.id:
             raise IdentityError('Identity not loaded')
-        return {'status': 'ok', 'id': id}
+        return {'status': 'ok', 'id': self.id}
 
     @cmd('identity_unload')
     async def _cmd_IDENTITY_UNLOAD(self, session, msg):
         await self.unload()
         return {'status': 'ok'}
+
+    async def load(self, id, raw_key):
+        raise NotImplementedError()
+
+    async def unload(self):
+        raise NotImplementedError()
+
+
+class IdentityService(BaseIdentityService):
+
+
+    def __init__(self):
+        super().__init__()
+        # TODO raise exception if accessing id/private_key before doing a `load`
+        self.id = None
+        self.private_key = None
 
     async def load(self, id, raw_key):
         # TODO encrypt private key
