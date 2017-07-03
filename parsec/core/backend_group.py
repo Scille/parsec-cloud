b149fb967e081451cebb25d419c31aa81239622c
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2017-07-03 19:18:45 +0200

    Add backend_group in core

---
+++ parsec/core/backend_group.py
@@ -0,0 +1,82 @@
+import attr
+from effect2 import Effect, do, TypeDispatcher
+
+from parsec.exceptions import exception_from_status
+from parsec.core.backend import BackendCmd
+
+
+@attr.s
+class EBackendGroupCreate:
+    name = attr.ib()
+
+
+@attr.s
+class EBackendGroupRead:
+    name = attr.ib()
+
+
+@attr.s
+class EBackendGroupAddIdentities:
+    name = attr.ib()
+    identities = attr.ib()
+    admin = attr.ib(default=False)
+
+
+@attr.s
+class EBackendGroupRemoveIdentities:
+    name = attr.ib()
+    identities = attr.ib()
+    admin = attr.ib(default=False)
+
+
+@attr.s
+class Group:
+    name = attr.ib()
+    admins = attr.ib()
+    users = attr.ib()
+
+
+@do
+def perform_group_create(intent):
+    msg = {'name': intent.name}
+    ret = yield Effect(BackendCmd('group_create', msg))
+    status = ret['status']
+    if status != 'ok':
+        raise exception_from_status(status)(ret['label'])
+
+
+@do
+def perform_group_read(intent):
+    msg = {'name': intent.name}
+    ret = yield Effect(BackendCmd('group_read', msg))
+    status = ret['status']
+    if status != 'ok':
+        raise exception_from_status(status)(ret['label'])
+    return Group(intent.name, ret['admins'], ret['users'])
+
+
+@do
+def perform_group_add_identities(intent):
+    msg = {'name': intent.name, 'identities': intent.identities, 'admin': intent.admin}
+    ret = yield Effect(BackendCmd('group_add_identities', msg))
+    status = ret['status']
+    if status != 'ok':
+        raise exception_from_status(status)(ret['label'])
+
+
+@do
+def perform_group_remove_identities(intent):
+    msg = {'name': intent.name, 'identities': intent.identities, 'admin': intent.admin}
+    ret = yield Effect(BackendCmd('group_remove_identities', msg))
+    status = ret['status']
+    if status != 'ok':
+        raise exception_from_status(status)(ret['label'])
+
+
+def backend_message_dispatcher_factory():
+    return TypeDispatcher({
+        EBackendGroupRead: perform_group_read,
+        EBackendGroupCreate: perform_group_create,
+        EBackendGroupAddIdentities: perform_group_add_identities,
+        EBackendGroupRemoveIdentities: perform_group_remove_identities
+    })
