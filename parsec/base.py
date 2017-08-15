f6225485d58a73a5359b931270118af0a7f0fdd4
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2017-08-15 19:52:11 +0200

    Create EventComponent

--- parsec/base.py
+++ parsec/base.py
@@ -1,5 +1,5 @@
 import attr
-from blinker import signal
+from collections import defaultdict
 
 from effect2 import (
     ComposedDispatcher, TypeDispatcher, base_dispatcher, base_asyncio_dispatcher, Effect, do
@@ -12,18 +12,49 @@ class EEvent:
     sender = attr.ib()
 
 
-@do
-def perform_event(intent):
-    effects = signal(intent.event).send(intent.sender)
-    for callback, effect in effects:
-        if effect:
-            assert isinstance(effect, Effect), \
-                "Event callback can only return Effect, %s did not." % callback
-            yield effect
+@attr.s
+class ERegisterEvent:
+    intent_factory = attr.ib()
+    event = attr.ib()
+    sender = attr.ib(default=None)
+
+
+@attr.s
+class EUnregisterEvent:
+    intent_factory = attr.ib()
+    event = attr.ib()
+    sender = attr.ib(default=None)
+
+
+@attr.s
+class EventComponent:
+    listeners = attr.ib(default=attr.Factory(lambda: defaultdict(set)))
+
+    @do
+    def perform_trigger_event(self, intent):
+        key = (intent.event, intent.sender)
+        for intent_factory in self.listeners[key]:
+            yield Effect(intent_factory(intent.event, intent.sender))
+
+    @do
+    def perform_register_event(self, intent):
+        key = (intent.event, intent.sender)
+        self.listeners[key].add(intent.intent_factory)
+
+    @do
+    def perform_unregister_event(self, intent):
+        key = (intent.event, intent.sender)
+        self.listeners[key].remove(intent.intent_factory)
+
+    def get_dispatcher(self):
+        return TypeDispatcher({
+            EEvent: self.perform_trigger_event,
+            ERegisterEvent: self.perform_register_event,
+            EUnregisterEvent: self.perform_register_event
+        })
 
 
 base_dispatcher = ComposedDispatcher([
     base_dispatcher,
     base_asyncio_dispatcher,
-    TypeDispatcher({EEvent: perform_event})
 ])
