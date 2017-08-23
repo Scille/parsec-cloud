dd2fa43756bfc691a41b30c338e191a06b8bbca0
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2017-08-23 11:43:27 +0200

    Add logs in backend start_api

--- parsec/backend/start_api.py
+++ parsec/backend/start_api.py
@@ -1,11 +1,15 @@
 import attr
 from effect2 import Effect, asyncio_perform
 from aiohttp import web
+from logbook import Logger
 
 from parsec.backend.privkey import EPrivKeyGet, EPrivKeyAdd
 from parsec.backend.pubkey import EPubKeyAdd
 from parsec.exceptions import PrivKeyHashCollision, PrivKeyNotFound
 from parsec.exceptions import ParsecError
+
+
+logger = Logger('start_api')
 
 
 def register_start_api(app, dispatcher, route='/start'):
@@ -20,21 +24,25 @@ def register_start_api(app, dispatcher, route='/start'):
 
     async def api_cipherkey_post(request):
         cipherkey = await request.read()
-        eff = Effect(EPrivKeyAdd(request.match_info['hash'], cipherkey))
+        hash = request.match_info['hash']
+        eff = Effect(EPrivKeyAdd(hash, cipherkey))
         try:
             await asyncio_perform(dispatcher, eff)
         except PrivKeyHashCollision:
             raise web.HTTPConflict(text='This hash already exists...')
+        logger.info('New cipherkey `%s` registered' % hash)
         return web.Response()
 
     async def api_pubkey_post(request):
         pubkey = await request.read()
         # TODO: should provide a token to avoid impersonation
-        eff = Effect(EPubKeyAdd(request.match_info['identity'], pubkey))
+        identity = request.match_info['identity']
+        eff = Effect(EPubKeyAdd(identity, pubkey))
         try:
             await asyncio_perform(dispatcher, eff)
         except ParsecError as exc:
             return web.HTTPConflict(text=exc.label)
+        logger.info('New identity `%s` registered' % identity)
         return web.Response()
 
     app.router.add_get(route + '/cipherkey/{hash}', api_cipherkey_get)
