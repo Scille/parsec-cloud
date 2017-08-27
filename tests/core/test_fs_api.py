136df610ca897ab15f3c8c3d34a892c95239255b
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2017-08-27 22:31:40 +0200

    Fix core/test_fs_api

--- tests/core/test_fs_api.py
+++ tests/core/test_fs_api.py
@@ -3,38 +3,28 @@ from effect2.testing import const, noop, perform_sequence
 
 from parsec.core.core_api import execute_cmd
 from parsec.core.fs import (
-    ESynchronize, EGroupCreate, EDustbinShow, EManifestHistory, EManifestRestore,
-    EFileCreate, EFileRead, EFileWrite, EFileTruncate, EFileHistory, EFileRestore, EFolderCreate,
-    EStat, EMove, EDelete, EUndelete
+    EFSFileCreate, EFSFileRead, EFSFileWrite, EFSFileTruncate, EFSFolderCreate,
+    EFSStat, EFSMove, EFSDelete
 )
 from parsec.tools import to_jsonb64
 
 
-def test_api_synchronize():
-    eff = execute_cmd('synchronize', {})
-    sequence = [
-        (ESynchronize(),
-            noop),
-    ]
-    resp = perform_sequence(sequence, eff)
-    assert resp == {'status': 'ok'}
-
-
+@pytest.mark.xfail(reason='not implemented yet')
 def test_api_group_create():
     eff = execute_cmd('group_create', {'group': 'share'})
     sequence = [
-        (EGroupCreate('share'),
-            noop),
+        (EFSGroupCreate('share'), noop),
     ]
     resp = perform_sequence(sequence, eff)
     assert resp == {'status': 'ok'}
 
 
+@pytest.mark.xfail(reason='not implemented yet')
 def test_api_dustbin_show():
     # Specific file in dustbin
     eff = execute_cmd('dustbin_show', {'path': '/foo'})
     sequence = [
-        (EDustbinShow('/foo'),
+        (EFSDustbinShow('/foo'),
             const([{'path': '/foo'}])),
     ]
     resp = perform_sequence(sequence, eff)
@@ -42,13 +32,14 @@ def test_api_dustbin_show():
     # All files in dustbin
     eff = execute_cmd('dustbin_show', {})
     sequence = [
-        (EDustbinShow(),
+        (EFSDustbinShow(),
             const([{'path': '/foo'}, {'path': '/bar'}])),
     ]
     resp = perform_sequence(sequence, eff)
     assert resp == {'status': 'ok', 'dustbin': [{'path': '/foo'}, {'path': '/bar'}]}
 
 
+@pytest.mark.xfail(reason='not implemented yet')
 def test_api_manifest_history():
     summary_history = {
         'summary_history': {
@@ -59,7 +50,7 @@ def test_api_manifest_history():
     }
     eff = execute_cmd('history', {})
     sequence = [
-        (EManifestHistory(1, None, False),
+        (EFSManifestHistory(1, None, False),
             const(summary_history)),
     ]
     resp = perform_sequence(sequence, eff)
@@ -67,10 +58,11 @@ def test_api_manifest_history():
     assert resp == summary_history
 
 
+@pytest.mark.xfail(reason='not implemented yet')
 def test_api_manifest_restore():
     eff = execute_cmd('restore', {})
     sequence = [
-        (EManifestRestore(None),
+        (EFSManifestRestore(None),
             noop),
     ]
     resp = perform_sequence(sequence, eff)
@@ -80,7 +72,7 @@ def test_api_manifest_restore():
 def test_api_file_create():
     eff = execute_cmd('file_create', {'path': '/foo'})
     sequence = [
-        (EFileCreate('/foo'),
+        (EFSFileCreate('/foo'),
             noop),
     ]
     resp = perform_sequence(sequence, eff)
@@ -90,7 +82,7 @@ def test_api_file_create():
 def test_api_file_read():
     eff = execute_cmd('file_read', {'path': '/foo'})
     sequence = [
-        (EFileRead('/foo', 0, None),
+        (EFSFileRead('/foo', 0, None),
             const('foo')),
     ]
     resp = perform_sequence(sequence, eff)
@@ -100,7 +92,7 @@ def test_api_file_read():
 def test_api_file_write():
     eff = execute_cmd('file_write', {'path': '/foo', 'content': to_jsonb64(b'foo'), 'offset': 0})
     sequence = [
-        (EFileWrite('/foo', b'foo', 0),
+        (EFSFileWrite('/foo', b'foo', 0),
             noop),
     ]
     resp = perform_sequence(sequence, eff)
@@ -110,19 +102,19 @@ def test_api_file_write():
 def test_api_file_truncate():
     eff = execute_cmd('file_truncate', {'path': '/foo', 'length': 5})
     sequence = [
-        (EFileTruncate('/foo', 5),
+        (EFSFileTruncate('/foo', 5),
             noop),
     ]
     resp = perform_sequence(sequence, eff)
     assert resp == {'status': 'ok'}
 
 
-@pytest.mark.xfail
+@pytest.mark.xfail(reason='not implemented yet')
 def test_api_file_history():
     raise NotImplementedError()
 
 
-@pytest.mark.xfail
+@pytest.mark.xfail(reason='not implemented yet')
 def test_api_file_restore():
     raise NotImplementedError()
 
@@ -130,7 +122,7 @@ def test_api_file_restore():
 def test_api_folder_create():
     eff = execute_cmd('folder_create', {'path': '/dir'})
     sequence = [
-        (EFolderCreate('/dir'),
+        (EFSFolderCreate('/dir'),
             noop),
     ]
     resp = perform_sequence(sequence, eff)
@@ -141,7 +133,7 @@ def test_api_stat():
     stat = {'type': 'file', 'id': '123'}
     eff = execute_cmd('stat', {'path': '/foo'})
     sequence = [
-        (EStat('/foo'),
+        (EFSStat('/foo'),
             const(stat)),
     ]
     resp = perform_sequence(sequence, eff)
@@ -152,7 +144,7 @@ def test_api_stat():
 def test_api_move():
     eff = execute_cmd('move', {'src': '/foo', 'dst': '/bar'})
     sequence = [
-        (EMove('/foo', '/bar'),
+        (EFSMove('/foo', '/bar'),
             noop),
     ]
     resp = perform_sequence(sequence, eff)
@@ -162,17 +154,18 @@ def test_api_move():
 def test_api_delete():
     eff = execute_cmd('delete', {'path': '/foo'})
     sequence = [
-        (EDelete('/foo'),
+        (EFSDelete('/foo'),
             noop),
     ]
     resp = perform_sequence(sequence, eff)
     assert resp == {'status': 'ok'}
 
 
+@pytest.mark.xfail(reason='not implemented yet')
 def test_api_undelete():
     eff = execute_cmd('undelete', {'vlob': '123'})
     sequence = [
-        (EUndelete('123'),
+        (EFSUndelete('123'),
             noop),
     ]
     resp = perform_sequence(sequence, eff)
