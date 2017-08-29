8f81164b44a6582ba8218c297d6dac3dd2f23392
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2017-08-29 18:27:12 +0200

    Fix crash in backend_start_api when connection to backend is impossible

--- parsec/core/backend_start_api.py
+++ parsec/core/backend_start_api.py
@@ -1,8 +1,11 @@
 import attr
 import aiohttp
+from functools import wraps
 
 from parsec.exceptions import (
-    PrivKeyHashCollision, PrivKeyError, PrivKeyNotFound, BackendIdentityRegisterError)
+    BackendConnectionError, PrivKeyHashCollision, PrivKeyError, PrivKeyNotFound,
+    BackendIdentityRegisterError
+)
 from parsec.crypto import hash_id_password
 
 
@@ -34,10 +37,21 @@ class EBackendIdentityRegister:
     pubkey = attr.ib()
 
 
+def catch_connection_error(fn):
+    @wraps(fn)
+    async def wrapper(*args, **kwargs):
+        try:
+            return await fn(*args, **kwargs)
+        except aiohttp.client_exceptions.ClientConnectorError as exc:
+            raise BackendConnectionError(str(exc))
+    return wrapper
+
+
 @attr.s
 class StartAPIComponent:
     url = attr.ib()
 
+    @catch_connection_error
     async def perform_identity_register(self, intent):
         route = '%s/pubkey/%s' % (self.url, intent.id)
         async with aiohttp.ClientSession() as session:
@@ -46,7 +60,7 @@ class StartAPIComponent:
                     error_msg = await resp.text()
                     raise BackendIdentityRegisterError(error_msg)
 
-
+    @catch_connection_error
     async def perform_cipherkey_add(self, intent):
         hash = hash_id_password(intent.id, intent.password)
         route = '%s/cipherkey/%s' % (self.url, hash)
@@ -59,6 +73,7 @@ class StartAPIComponent:
                     else:
                         raise PrivKeyError(error_msg)
 
+    @catch_connection_error
     async def perform_cipherkey_get(self, intent):
         hash = hash_id_password(intent.id, intent.password)
         route = '%s/cipherkey/%s' % (self.url, hash)
