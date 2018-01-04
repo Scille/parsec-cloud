4ec6ce8a8ff49d444952e6d380ad8c8254b30a79
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2018-01-04 12:22:05 +0100

    Rework travis, drop Python 3.5 (need async yield, but workaround possible in the future)

--- .travis/run.sh
+++
@@ -1,9 +0,0 @@
-if [ -n "${OPENSSL}" ]; then
-    OPENSSL_DIR="ossl-1/${OPENSSL}"
-
-    export PATH="$HOME/$OPENSSL_DIR/bin:$PATH"
-    export CFLAGS="-I$HOME/$OPENSSL_DIR/include"
-    export LDFLAGS="-L$HOME/$OPENSSL_DIR/lib -Wl,-rpath=$HOME/$OPENSSL_DIR/lib"
-fi
-
-tox -e $(echo py$TRAVIS_PYTHON_VERSION | tr -d .) -- -v $TOX_FLAGS
