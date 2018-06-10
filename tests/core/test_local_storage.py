948bce76ec0258ebb8ca4726f764ec2098252f59
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2018-06-10 12:34:29 +0200

    Add device and user in core local_storage

--- tests/core/test_local_storage.py
+++ tests/core/test_local_storage.py
@@ -47,3 +47,29 @@ async def test_move_manifest_manifest(local_storage):
     assert old is None
     new = local_storage.fetch_manifest("<id#2>")
     assert new == b"<manifest>"
+
+
+@pytest.mark.trio
+async def test_fetch_device_verifykey_not_available(local_storage):
+    verifykey = local_storage.fetch_device_verifykey("john", "pc1")
+    assert verifykey is None
+
+
+@pytest.mark.trio
+async def test_flush_and_fetch_device_verifykey(local_storage):
+    local_storage.flush_device_verifykey("john", "pc1", b"<device's verifykey>")
+    verifykey = local_storage.fetch_device_verifykey("john", "pc1")
+    assert verifykey == b"<device's verifykey>"
+
+
+@pytest.mark.trio
+async def test_fetch_user_pubkey_not_available(local_storage):
+    verifykey = local_storage.fetch_user_pubkey("john")
+    assert verifykey is None
+
+
+@pytest.mark.trio
+async def test_flush_and_fetch_user_pubkey(local_storage):
+    local_storage.flush_user_pubkey("john", b"<device's verifykey>")
+    verifykey = local_storage.fetch_user_pubkey("john")
+    assert verifykey == b"<device's verifykey>"
