6341b2039f50d03b6b66b0ae12517f92401a687c
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2018-07-15 15:53:17 +0200

    Fix and simplify test_cooked_socket_communication

--- tests/test_networking.py
+++ tests/test_networking.py
@@ -19,34 +19,21 @@ json_nested_strategy = st.recursive(
 json_dict_strategy = st.dictionaries(st.text(printable), json_nested_strategy)
 
 
-# TODO: Improve this (see https://github.com/python-trio/pytest-trio/issues/42)
+@pytest.mark.slow
+@pytest.mark.trio
+@given(payload=json_dict_strategy)
+async def test_cooked_socket_communication(payload):
+    rserver, rclient = trio.testing.memory_stream_pair()
+    cclient = CookedSocket(rclient)
+    cserver = CookedSocket(rserver)
 
+    await cclient.send(payload)
+    server_payload = await cserver.recv()
+    assert server_payload == payload
 
-class TestCookedSocketHypothesis:
-    @pytest.mark.slow
-    @given(json_dict_strategy)
-    async def test_cooked_socket_communication(self, payload):
-        rserver, rclient = trio.testing.memory_stream_pair()
-        cclient = CookedSocket(rclient)
-        cserver = CookedSocket(rserver)
-
-        await cclient.send(payload)
-        server_payload = await cserver.recv()
-        assert server_payload == payload
-
-        await cserver.send(server_payload)
-        roundtrip_payload = await cclient.recv()
-        assert roundtrip_payload == payload
-
-    def execute_example(self, f):
-        # Nothing but a dirty hack...
-        from unittest.mock import Mock
-
-        node = Mock()
-        node.function = f
-        from pytest_trio.plugin import _trio_test_runner_factory
-
-        return _trio_test_runner_factory(node)()
+    await cserver.send(server_payload)
+    roundtrip_payload = await cclient.recv()
+    assert roundtrip_payload == payload
 
 
 class TestCookedSocket:
