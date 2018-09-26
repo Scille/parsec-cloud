9ac2e302b0491af7a0565a406840163ccca4069c
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2018-09-26 20:32:50 +0200

    Rework test_backend_cmds_sender to use default factory

--- tests/core/test_backend_cmds_sender.py
+++ tests/core/test_backend_cmds_sender.py
@@ -7,26 +7,19 @@ from tests.open_tcp_stream_mock_wrapper import offline
 
 
 @pytest.fixture
-async def backend_cmds(running_backend, backend_addr, alice):
-    async with trio.open_nursery() as nursery:
-        backend_cmds = BackendCmdsSender(alice, running_backend.addr)
-        await backend_cmds.init(nursery)
-        try:
-            yield backend_cmds
-
-        finally:
-            await backend_cmds.teardown()
-            nursery.cancel_scope.cancel()
+async def backend_cmds_sender(backend_cmds_sender_factory, alice):
+    async with backend_cmds_sender_factory(alice) as backend_cmds_sender:
+        yield backend_cmds_sender
 
 
 @pytest.mark.trio
-async def test_base(backend_cmds):
-    rep = await backend_cmds.send({"cmd": "ping", "ping": "hello"})
+async def test_base(backend_cmds_sender):
+    rep = await backend_cmds_sender.send({"cmd": "ping", "ping": "hello"})
     assert rep == {"status": "ok", "pong": "hello"}
 
 
 @pytest.mark.trio
-async def test_concurrency_sends(backend_cmds):
+async def test_concurrency_sends(backend_cmds_sender):
     CONCURRENCY = 10
 
     work_done_counter = 0
@@ -42,7 +35,7 @@ async def test_concurrency_sends(backend_cmds):
 
     async with trio.open_nursery() as nursery:
         for x in range(CONCURRENCY):
-            nursery.start_soon(sender, backend_cmds, str(x))
+            nursery.start_soon(sender, backend_cmds_sender, str(x))
 
     # TODO: ideally we would have to spy on the socket to make sure
     # the trames were strictly on a request/response order pattern
@@ -53,40 +46,40 @@ async def test_concurrency_sends(backend_cmds):
 @pytest.mark.trio
 async def test_backend_offline(unused_tcp_addr, alice):
     async with trio.open_nursery() as nursery:
-        backend_cmds = BackendCmdsSender(alice, unused_tcp_addr)
-        await backend_cmds.init(nursery)
+        backend_cmds_sender = BackendCmdsSender(alice, unused_tcp_addr)
+        await backend_cmds_sender.init(nursery)
 
         with pytest.raises(BackendNotAvailable):
-            await backend_cmds.send({"cmd": "ping", "ping": "hello"})
+            await backend_cmds_sender.send({"cmd": "ping", "ping": "hello"})
         with pytest.raises(BackendNotAvailable):
-            await backend_cmds.send({"cmd": "ping", "ping": "hello"})
+            await backend_cmds_sender.send({"cmd": "ping", "ping": "hello"})
 
-        await backend_cmds.teardown()
+        await backend_cmds_sender.teardown()
         nursery.cancel_scope.cancel()
 
 
 @pytest.mark.trio
-async def test_backend_switch_offline(backend_cmds, tcp_stream_spy):
+async def test_backend_switch_offline(backend_cmds_sender, tcp_stream_spy):
     # First, have a good request to make sure a socket has been opened
 
-    rep = await backend_cmds.send({"cmd": "ping", "ping": "hello"})
+    rep = await backend_cmds_sender.send({"cmd": "ping", "ping": "hello"})
     assert rep == {"status": "ok", "pong": "hello"}
 
     # Current socket is down, but opening another socket
     # should solve the trouble
 
-    tcp_stream_spy.socks[backend_cmds.backend_addr][-1].send_stream.close()
+    tcp_stream_spy.socks[backend_cmds_sender.backend_addr][-1].send_stream.close()
 
-    rep = await backend_cmds.send({"cmd": "ping", "ping": "hello"})
+    rep = await backend_cmds_sender.send({"cmd": "ping", "ping": "hello"})
     assert rep == {"status": "ok", "pong": "hello"}
 
     # Now sockets will never be able to reach the backend no matter what
 
-    with offline(backend_cmds.backend_addr):
+    with offline(backend_cmds_sender.backend_addr):
         with pytest.raises(BackendNotAvailable):
-            await backend_cmds.send({"cmd": "ping", "ping": "hello"})
+            await backend_cmds_sender.send({"cmd": "ping", "ping": "hello"})
 
     # Finally make sure we can still connect to the backend
 
-    rep = await backend_cmds.send({"cmd": "ping", "ping": "hello"})
+    rep = await backend_cmds_sender.send({"cmd": "ping", "ping": "hello"})
     assert rep == {"status": "ok", "pong": "hello"}
