84f408bf20f17a3ad773047c7b089da151f0b33f
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2018-10-26 17:18:24 +0200

    Improve cli error message in case of missing/bad import

--- parsec/cli.py
+++ parsec/cli.py
@@ -1,40 +1,47 @@
 import click
+import traceback
 
 from parsec.ui import shell
 
+
+def _generate_bad_cmd(exc, hint=None):
+    error_msg = "".join(
+        [
+            "Not available: Importing this module has failed with error:\n\n",
+            *traceback.format_exception(exc, exc, exc.__traceback__),
+            f"\n\n{hint}\n" if hint else "",
+        ]
+    )
+
+    @click.command(
+        context_settings=dict(ignore_unknown_options=True),
+        help=f"Not available{' (' + hint + ')' if hint else ''}",
+    )
+    @click.argument("args", nargs=-1, type=click.UNPROCESSED)
+    def bad_cmd(args):
+        raise SystemExit(error_msg)
+
+    return bad_cmd
+
+
 try:
     from parsec.core.cli import core_cmd
-except ImportError:
-
-    @click.command()
-    def core_cmd():
-        raise SystemExit("Not available.")
+except ImportError as exc:
+    core_cmd = _generate_bad_cmd(exc)
 
 
 try:
     from parsec.backend.cli import backend_cmd, init_cmd, revoke_cmd
-except ImportError:
-
-    @click.command()
-    def backend_cmd():
-        raise SystemExit("Not available.")
-
-    @click.command()
-    def init_cmd():
-        raise SystemExit("Not available.")
-
-    @click.command()
-    def revoke_cmd():
-        raise SystemExit("Not available.")
+except ImportError as exc:
+    backend_cmd = _generate_bad_cmd(exc)
+    init_cmd = _generate_bad_cmd(exc)
+    revoke_cmd = _generate_bad_cmd(exc)
 
 
 try:
     from parsec.ui.fuse import cli as fuse_cmd
-except ImportError:
-
-    @click.command()
-    def fuse_cmd():
-        raise RuntimeError("Not available, is fusepy installed ?")
+except ImportError as exc:
+    fuse_cmd = _generate_bad_cmd(exc, hint="is fusepy installed ?")
 
 
 except NameError:
