f1d8b08a8c87d3092e64b5390cb21022fb275637
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2018-11-20 13:25:49 +0100

    Force Qt config to IniFormat (for Windows compatibility)

--- parsec/core/gui/settings.py
+++ parsec/core/gui/settings.py
@@ -23,7 +23,7 @@ def get_value(key, default=None):
     decoder = ENTRIES[key]["decode"]
     value = QSettings(
         os.path.join(core_call().get_config().base_settings_path, "parsec_gui.cfg"),
-        QSettings.NativeFormat,
+        QSettings.IniFormat,
     ).value(key, default)
     if value is not None:
         value = decoder(value)
@@ -34,7 +34,7 @@ def set_value(key, value):
     encode = ENTRIES[key]["encode"]
     settings = QSettings(
         os.path.join(core_call().get_config().base_settings_path, "parsec_gui.cfg"),
-        QSettings.NativeFormat,
+        QSettings.IniFormat,
     )
     settings.setValue(key, encode(value))
     settings.sync()
