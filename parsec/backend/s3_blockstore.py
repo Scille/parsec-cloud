2fce71f2b82e783c0f29c2b929330c98819d9a3a
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2018-12-31 15:39:00 +0100

    Fix crash when using s3

--- parsec/backend/s3_blockstore.py
+++ parsec/backend/s3_blockstore.py
@@ -9,14 +9,14 @@ from functools import partial
 
 from parsec.types import DeviceID
 from parsec.backend.blockstore import (
-    BaseblockStoreComponent,
+    BaseblockstoreComponent,
     BlockstoreAlreadyExistsError,
     BlockstoreNotFoundError,
     BlockstoreTimeoutError,
 )
 
 
-class S3BlockstoreComponent(BaseblockStoreComponent):
+class S3BlockstoreComponent(BaseblockstoreComponent):
     def __init__(self, s3_region, s3_bucket, s3_key, s3_secret):
         self._s3 = None
         self._s3_bucket = None
