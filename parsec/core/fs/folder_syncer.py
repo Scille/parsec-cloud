a572b0d7244ba3526502aacb4659f4d0e396ca33
Vincent Michel <vxgmichel@gmail.com>
2019-03-01 21:41:24 +0100

    Properly update the local storage after folder synchronization

--- parsec/core/fs/folder_syncer.py
+++ parsec/core/fs/folder_syncer.py
@@ -203,4 +203,9 @@ class FolderSyncerMixin(BaseSyncer):
                 original_id=original_id,
                 diverged_id=diverged_id,
             )
-        self.local_folder_fs.set_manifest(access, final_manifest)
+        # New manifest is not up to date with the remote
+        if final_manifest.need_sync:
+            self.local_folder_fs.set_local_manifest(access, final_manifest)
+        # New manifest is up to date with the remote: safely clean up the local manifest
+        else:
+            self.local_folder_fs.set_remote_manifest(access, final_manifest, force=True)
