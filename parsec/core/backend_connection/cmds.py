a5e94203fc4b31761ebb39994db39c9ff00f8418
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2019-03-06 13:52:16 +0100

    Add better exceptions for bad command responses from the backend

--- parsec/core/backend_connection/cmds.py
+++ parsec/core/backend_connection/cmds.py
@@ -41,10 +41,10 @@ from parsec.api.protocole import (
 )
 from parsec.core.types import RemoteDevice, RemoteUser, RemoteDevicesMapping
 from parsec.core.backend_connection.exceptions import (
+    raise_on_bad_response,
     BackendNotAvailable,
     BackendCmdsInvalidRequest,
     BackendCmdsInvalidResponse,
-    BackendCmdsBadResponse,
 )
 
 
@@ -61,7 +61,7 @@ async def _send_cmd(transport, serializer, **req):
         raw_req = serializer.req_dumps(req)
 
     except ProtocoleError as exc:
-        raise BackendCmdsInvalidRequest() from exc
+        raise BackendCmdsInvalidRequest(exc) from exc
 
     try:
         await transport.send(raw_req)
@@ -81,6 +81,8 @@ async def _send_cmd(transport, serializer, **req):
     if rep["status"] == "invalid_msg_format":
         raise BackendCmdsInvalidRequest(rep)
 
+    raise_on_bad_response(rep)
+
     return rep
 
 
@@ -92,8 +94,6 @@ async def _send_cmd(transport, serializer, **req):
 
 async def ping(transport: Transport, ping: str) -> str:
     rep = await _send_cmd(transport, ping_serializer, cmd="ping", ping=ping)
-    if rep["status"] != "ok":
-        raise BackendCmdsBadResponse(rep)
     return rep["pong"]
 
 
@@ -103,7 +103,7 @@ async def events_subscribe(
     vlob_group_updated: Iterable[UUID] = (),
     pinged: Iterable[str] = (),
 ) -> None:
-    rep = await _send_cmd(
+    await _send_cmd(
         transport,
         events_subscribe_serializer,
         cmd="events_subscribe",
@@ -111,14 +111,10 @@ async def events_subscribe(
         vlob_group_updated=vlob_group_updated,
         pinged=pinged,
     )
-    if rep["status"] != "ok":
-        raise BackendCmdsBadResponse(rep)
 
 
 async def events_listen(transport: Transport, wait: bool = True) -> dict:
     rep = await _send_cmd(transport, events_listen_serializer, cmd="events_listen", wait=wait)
-    if rep["status"] != "ok":
-        raise BackendCmdsBadResponse(rep)
     rep.pop("status")
     return rep
 
@@ -127,17 +123,13 @@ async def events_listen(transport: Transport, wait: bool = True) -> dict:
 
 
 async def message_send(transport: Transport, recipient: UserID, body: bytes) -> None:
-    rep = await _send_cmd(
+    await _send_cmd(
         transport, message_send_serializer, cmd="message_send", recipient=recipient, body=body
     )
-    if rep["status"] != "ok":
-        raise BackendCmdsBadResponse(rep)
 
 
 async def message_get(transport: Transport, offset: int) -> List[Tuple[int, DeviceID, bytes]]:
     rep = await _send_cmd(transport, message_get_serializer, cmd="message_get", offset=offset)
-    if rep["status"] != "ok":
-        raise BackendCmdsBadResponse(rep)
     return [(item["count"], item["sender"], item["body"]) for item in rep["messages"]]
 
 
@@ -148,40 +140,30 @@ async def vlob_group_check(transport: Transport, to_check: list) -> list:
     rep = await _send_cmd(
         transport, vlob_group_check_serializer, cmd="vlob_group_check", to_check=to_check
     )
-    if rep["status"] != "ok":
-        raise BackendCmdsBadResponse(rep)
     return rep["changed"]
 
 
 async def vlob_create(transport: Transport, group: UUID, id: UUID, blob: bytes) -> None:
-    rep = await _send_cmd(
+    await _send_cmd(
         transport, vlob_create_serializer, cmd="vlob_create", group=group, id=id, blob=blob
     )
-    if rep["status"] != "ok":
-        raise BackendCmdsBadResponse(rep)
 
 
 async def vlob_read(transport: Transport, id: UUID, version: int = None) -> Tuple[int, bytes]:
     rep = await _send_cmd(transport, vlob_read_serializer, cmd="vlob_read", id=id, version=version)
-    if rep["status"] != "ok":
-        raise BackendCmdsBadResponse(rep)
     return rep["version"], rep["blob"]
 
 
 async def vlob_update(transport: Transport, id: UUID, version: int, blob: bytes) -> None:
-    rep = await _send_cmd(
+    await _send_cmd(
         transport, vlob_update_serializer, cmd="vlob_update", id=id, version=version, blob=blob
     )
-    if rep["status"] != "ok":
-        raise BackendCmdsBadResponse(rep)
 
 
 async def vlob_group_get_rights(transport: Transport, id: UUID) -> Dict[UserID, Dict]:
     rep = await _send_cmd(
         transport, vlob_group_get_rights_serializer, cmd="vlob_group_get_rights", id=id
     )
-    if rep["status"] != "ok":
-        raise BackendCmdsBadResponse(rep)
     return rep["users"]
 
 
@@ -193,7 +175,7 @@ async def vlob_group_update_rights(
     read_right: bool,
     write_right: bool,
 ) -> None:
-    rep = await _send_cmd(
+    await _send_cmd(
         transport,
         vlob_group_update_rights_serializer,
         cmd="vlob_group_update_rights",
@@ -203,8 +185,6 @@ async def vlob_group_update_rights(
         read_right=read_right,
         write_right=write_right,
     )
-    if rep["status"] != "ok":
-        raise BackendCmdsBadResponse(rep)
 
 
 async def vlob_group_poll(
@@ -217,8 +197,6 @@ async def vlob_group_poll(
         id=id,
         last_checkpoint=last_checkpoint,
     )
-    if rep["status"] != "ok":
-        raise BackendCmdsBadResponse(rep)
     return (rep["current_checkpoint"], rep["changes"])
 
 
@@ -226,7 +204,7 @@ async def vlob_group_poll(
 
 
 async def block_create(transport: Transport, id: UUID, vlob_group: UUID, block: bytes) -> None:
-    rep = await _send_cmd(
+    await _send_cmd(
         transport,
         block_create_serializer,
         cmd="block_create",
@@ -234,14 +212,10 @@ async def block_create(transport: Transport, id: UUID, vlob_group: UUID, block: 
         vlob_group=vlob_group,
         block=block,
     )
-    if rep["status"] != "ok":
-        raise BackendCmdsBadResponse(rep)
 
 
 async def block_read(transport: Transport, id: UUID) -> bytes:
     rep = await _send_cmd(transport, block_read_serializer, cmd="block_read", id=id)
-    if rep["status"] != "ok":
-        raise BackendCmdsBadResponse(rep)
     return rep["block"]
 
 
@@ -252,8 +226,6 @@ async def user_get(
     transport: Transport, user_id: UserID
 ) -> Tuple[RemoteUser, Dict[DeviceID, RemoteDevice]]:
     rep = await _send_cmd(transport, user_get_serializer, cmd="user_get", user_id=user_id)
-    if rep["status"] != "ok":
-        raise BackendCmdsBadResponse(rep)
 
     devices = []
     for rep_device in rep["devices"].values():
@@ -306,30 +278,24 @@ async def user_find(
         per_page=per_page,
         omit_revocated=omit_revocated,
     )
-    if rep["status"] != "ok":
-        raise BackendCmdsBadResponse(rep)
     return rep["results"]
 
 
 async def user_invite(transport: Transport, user_id: UserID) -> bytes:
     rep = await _send_cmd(transport, user_invite_serializer, cmd="user_invite", user_id=user_id)
-    if rep["status"] != "ok":
-        raise BackendCmdsBadResponse(rep)
     return rep["encrypted_claim"]
 
 
 async def user_cancel_invitation(transport: Transport, user_id: UserID) -> None:
-    rep = await _send_cmd(
+    await _send_cmd(
         transport, user_cancel_invitation_serializer, cmd="user_cancel_invitation", user_id=user_id
     )
-    if rep["status"] != "ok":
-        raise BackendCmdsBadResponse(rep)
 
 
 async def user_create(
     transport: Transport, certified_user: bytes, certified_device: bytes, is_admin: bool
 ) -> None:
-    rep = await _send_cmd(
+    await _send_cmd(
         transport,
         user_create_serializer,
         cmd="user_create",
@@ -337,8 +303,6 @@ async def user_create(
         certified_device=certified_device,
         is_admin=is_admin,
     )
-    if rep["status"] != "ok":
-        raise BackendCmdsBadResponse(rep)
 
 
 async def device_invite(transport: Transport, invited_device_name: DeviceName) -> bytes:
@@ -348,34 +312,28 @@ async def device_invite(transport: Transport, invited_device_name: DeviceName) -
         cmd="device_invite",
         invited_device_name=invited_device_name,
     )
-    if rep["status"] != "ok":
-        raise BackendCmdsBadResponse(rep)
     return rep["encrypted_claim"]
 
 
 async def device_cancel_invitation(transport: Transport, invited_device_name: DeviceName) -> None:
-    rep = await _send_cmd(
+    await _send_cmd(
         transport,
         device_cancel_invitation_serializer,
         cmd="device_cancel_invitation",
         invited_device_name=invited_device_name,
     )
-    if rep["status"] != "ok":
-        raise BackendCmdsBadResponse(rep)
 
 
 async def device_create(
     transport: Transport, certified_device: bytes, encrypted_answer: bytes
 ) -> None:
-    rep = await _send_cmd(
+    await _send_cmd(
         transport,
         device_create_serializer,
         cmd="device_create",
         certified_device=certified_device,
         encrypted_answer=encrypted_answer,
     )
-    if rep["status"] != "ok":
-        raise BackendCmdsBadResponse(rep)
 
 
 async def device_revoke(
@@ -387,8 +345,6 @@ async def device_revoke(
         cmd="device_revoke",
         certified_revocation=certified_revocation,
     )
-    if rep["status"] != "ok":
-        raise BackendCmdsBadResponse(rep)
     return rep["user_revocated_on"]
 
 
@@ -405,8 +361,6 @@ async def organization_create(transport: Transport, organization_id: Organizatio
         cmd="organization_create",
         organization_id=organization_id,
     )
-    if rep["status"] != "ok":
-        raise BackendCmdsBadResponse(rep)
     return rep["bootstrap_token"]
 
 
@@ -418,7 +372,7 @@ async def organization_bootstrap(
     certified_user: bytes,
     certified_device: bytes,
 ) -> None:
-    rep = await _send_cmd(
+    await _send_cmd(
         transport,
         organization_bootstrap_serializer,
         cmd="organization_bootstrap",
@@ -428,8 +382,6 @@ async def organization_bootstrap(
         certified_user=certified_user,
         certified_device=certified_device,
     )
-    if rep["status"] != "ok":
-        raise BackendCmdsBadResponse(rep)
 
 
 async def user_get_invitation_creator(transport: Transport, invited_user_id: UserID) -> RemoteUser:
@@ -439,8 +391,6 @@ async def user_get_invitation_creator(transport: Transport, invited_user_id: Use
         cmd="user_get_invitation_creator",
         invited_user_id=invited_user_id,
     )
-    if rep["status"] != "ok":
-        raise BackendCmdsBadResponse(rep)
     return RemoteUser(
         user_id=rep["user_id"],
         created_on=rep["created_on"],
@@ -450,15 +400,13 @@ async def user_get_invitation_creator(transport: Transport, invited_user_id: Use
 
 
 async def user_claim(transport: Transport, invited_user_id: UserID, encrypted_claim: bytes) -> None:
-    rep = await _send_cmd(
+    await _send_cmd(
         transport,
         user_claim_serializer,
         cmd="user_claim",
         invited_user_id=invited_user_id,
         encrypted_claim=encrypted_claim,
     )
-    if rep["status"] != "ok":
-        raise BackendCmdsBadResponse(rep)
 
 
 async def device_get_invitation_creator(
@@ -470,8 +418,6 @@ async def device_get_invitation_creator(
         cmd="device_get_invitation_creator",
         invited_device_id=invited_device_id,
     )
-    if rep["status"] != "ok":
-        raise BackendCmdsBadResponse(rep)
     return RemoteUser(
         user_id=rep["user_id"],
         created_on=rep["created_on"],
@@ -490,6 +436,4 @@ async def device_claim(
         invited_device_id=invited_device_id,
         encrypted_claim=encrypted_claim,
     )
-    if rep["status"] != "ok":
-        raise BackendCmdsBadResponse(rep)
     return rep["encrypted_answer"]
