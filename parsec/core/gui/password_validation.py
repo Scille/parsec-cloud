28642cfc77662f43616b519920bdb944724b9938
Maxime GRANDCOLAS <maxime.grandcolas@gmail.com>
2019-03-13 17:46:44 +0100

    [GUI] Display when a password is too short

--- parsec/core/gui/password_validation.py
+++ parsec/core/gui/password_validation.py
@@ -6,6 +6,7 @@ from zxcvbn import zxcvbn
 
 
 PASSWORD_STRENGTH_TEXTS = [
+    QCoreApplication.translate("PasswordStrength", "Too short"),
     QCoreApplication.translate("PasswordStrength", "Very weak"),
     QCoreApplication.translate("PasswordStrength", "Weak"),
     QCoreApplication.translate("PasswordStrength", "Average"),
@@ -14,13 +15,16 @@ PASSWORD_STRENGTH_TEXTS = [
 ]
 
 PASSWORD_CSS = {
-    0: "color: white; background-color: rgb(194, 51, 51);",
+    0: "color: white; background-color: rgb(64, 64, 64);",
     1: "color: white; background-color: rgb(194, 51, 51);",
-    2: "color: white; background-color: rgb(219, 125, 58);",
-    3: "color: white; background-color: rgb(55, 130, 65);",
+    2: "color: white; background-color: rgb(194, 51, 51);",
+    3: "color: white; background-color: rgb(219, 125, 58);",
+    4: "color: white; background-color: rgb(55, 130, 65);",
 }
 
 
 def get_password_strength(password):
+    if len(password) < 8:
+        return 0
     result = zxcvbn(password)
-    return result["score"]
+    return result["score"] + 1
