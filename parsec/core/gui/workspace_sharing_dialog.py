624aba832b584470c23ee1e808b9536c5ee5831f
Maxime GRANDCOLAS <maxime.grandcolas@gmail.com>
2019-03-20 10:20:55 +0100

    [GUI] Hide/disable functionnalities in sharing dialog depending on the user's permissions

--- parsec/core/gui/workspace_sharing_dialog.py
+++ parsec/core/gui/workspace_sharing_dialog.py
@@ -7,7 +7,7 @@ from PyQt5.QtWidgets import QDialog, QCompleter, QWidget
 
 from parsec.core.fs.sharing import SharingRecipientError
 
-from parsec.core.gui.custom_widgets import show_error, show_warning, ask_question
+from parsec.core.gui.custom_widgets import show_error, show_warning, ask_question, show_info
 
 from parsec.core.gui.ui.workspace_sharing_dialog import Ui_WorkspaceSharingDialog
 from parsec.core.gui.ui.sharing_widget import Ui_SharingWidget
@@ -16,10 +16,23 @@ from parsec.core.gui.ui.sharing_widget import Ui_SharingWidget
 class SharingWidget(QWidget, Ui_SharingWidget):
     remove_clicked = pyqtSignal(QWidget)
 
-    def __init__(self, name, is_current_user, is_creator, admin, read, write, *args, **kwargs):
+    def __init__(
+        self,
+        name,
+        is_current_user,
+        is_creator,
+        admin,
+        read,
+        write,
+        can_change_permissions,
+        *args,
+        **kwargs,
+    ):
         super().__init__(*args, **kwargs)
         self.setupUi(self)
-        if is_creator:
+        self.is_creator = is_creator
+        self.name = name
+        if self.is_creator:
             self.label_name.setText(f"<b>{name}</b>")
         else:
             self.label_name.setText(name)
@@ -27,7 +40,7 @@ class SharingWidget(QWidget, Ui_SharingWidget):
         self.checkbox_write.setChecked(write)
         self.checkbox_admin.setChecked(admin)
         self.is_current_user = is_current_user
-        if is_current_user:
+        if self.is_current_user or self.is_creator or not can_change_permissions:
             self.checkbox_read.setDisabled(True)
             self.checkbox_write.setDisabled(True)
             self.checkbox_admin.setDisabled(True)
@@ -46,10 +59,6 @@ class SharingWidget(QWidget, Ui_SharingWidget):
     @property
     def write_rights(self):
         return self.checkbox_write.isChecked()
-
-    @property
-    def name(self):
-        return self.label_name.text()
 
     def should_update(self):
         return (
@@ -133,6 +142,7 @@ class WorkspaceSharingDialog(QDialog, Ui_WorkspaceSharingDialog):
                 admin=self.checkbox_admin.isChecked(),
                 read=self.checkbox_read.isChecked(),
                 write=self.checkbox_write.isChecked(),
+                can_change_permissions=self.checkbox_admin.isChecked(),
             )
             self.checkbox_admin.setChecked(True)
             self.checkbox_read.setChecked(True)
@@ -155,7 +165,9 @@ class WorkspaceSharingDialog(QDialog, Ui_WorkspaceSharingDialog):
                 ).format(self.name, user),
             )
 
-    def add_participant(self, participant, is_current_user, is_creator, admin, read, write):
+    def add_participant(
+        self, participant, is_current_user, is_creator, admin, read, write, can_change_permissions
+    ):
         w = SharingWidget(
             name=participant,
             is_current_user=is_current_user,
@@ -163,18 +175,20 @@ class WorkspaceSharingDialog(QDialog, Ui_WorkspaceSharingDialog):
             admin=admin,
             read=read,
             write=write,
+            can_change_permissions=can_change_permissions,
             parent=self,
         )
         self.scroll_content.layout().insertWidget(0, w)
 
     def apply_changes(self):
+        errors = []
+        updated = False
         for i in range(self.scroll_content.layout().count()):
             item = self.scroll_content.layout().itemAt(i)
             w = item.widget()
             if not w or not isinstance(w, SharingWidget):
                 continue
-            errors = []
-            if not w.is_current_user and w.should_update():
+            if not w.is_current_user and not w.is_creator and w.should_update():
                 try:
                     self.portal.run(
                         self.core.fs.share,
@@ -184,6 +198,7 @@ class WorkspaceSharingDialog(QDialog, Ui_WorkspaceSharingDialog):
                         w.read_rights,
                         w.write_rights,
                     )
+                    updated = True
                 except SharingRecipientError:
                     errors.append(w.name)
                     import traceback
@@ -194,23 +209,28 @@ class WorkspaceSharingDialog(QDialog, Ui_WorkspaceSharingDialog):
                     import traceback
 
                     traceback.print_exc()
-            if errors:
-                show_error(
-                    self,
-                    QCoreApplication.translate(
-                        "WorkspaceSharing",
-                        "Permissions could not be updated for the following users: {}".format(
-                            "\n".join(errors)
-                        ),
+        if errors:
+            show_error(
+                self,
+                QCoreApplication.translate(
+                    "WorkspaceSharing",
+                    "Permissions could not be updated for the following users: {}".format(
+                        "\n".join(errors)
                     ),
-                )
-            self.reset()
+                ),
+            )
+        elif updated:
+            show_info(
+                self,
+                QCoreApplication.translate("WorkspaceSharing", "Permissions have been updated."),
+            )
+        self.reset()
 
     def has_changes(self):
         for i in range(self.scroll_content.layout().count() - 1):
             item = self.scroll_content.layout().itemAt(i)
             w = item.widget()
-            if not w.is_current_user and w.should_update():
+            if not w.is_current_user and not w.is_creator and w.should_update():
                 return True
         return False
 
@@ -241,6 +261,10 @@ class WorkspaceSharingDialog(QDialog, Ui_WorkspaceSharingDialog):
         path = os.path.join("/", self.name)
         stat = self.portal.run(self.core.fs.stat, path)
         sharing_info = self.portal.run(self.core.fs.get_permissions, path)
+        current_user_permission = sharing_info[self.core.device.user_id]
+        can_update = current_user_permission["admin_right"]
+        self.button_apply.setVisible(can_update)
+        self.widget_add.setVisible(can_update)
         for user, permissions in sharing_info.items():
             self.add_participant(
                 user,
@@ -249,4 +273,5 @@ class WorkspaceSharingDialog(QDialog, Ui_WorkspaceSharingDialog):
                 admin=permissions["admin_right"],
                 read=permissions["read_right"],
                 write=permissions["write_right"],
+                can_change_permissions=current_user_permission["admin_right"],
             )
