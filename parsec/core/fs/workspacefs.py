253a2f09d8c04680abfa82457b9170d3b123355c
Vincent Michel <vxgmichel@gmail.com>
2019-04-03 01:23:19 +0200

    Make user and workspace fs use file transactions

--- parsec/core/fs/workspacefs.py
+++ parsec/core/fs/workspacefs.py
@@ -6,7 +6,6 @@ from uuid import UUID
 
 from parsec.core.types import FsPath
 from parsec.core.fs.local_folder_fs import FSManifestLocalMiss, FSMultiManifestLocalMiss
-from parsec.core.fs.local_file_fs import FSBlocksLocalMiss
 
 
 class WorkspaceFS:
@@ -18,7 +17,7 @@ class WorkspaceFS:
         backend_cmds,
         event_bus,
         _local_folder_fs,
-        _local_file_fs,
+        _file_transactions,
         _remote_loader,
         _syncer,
     ):
@@ -28,7 +27,7 @@ class WorkspaceFS:
         self.backend_cmds = backend_cmds
         self.event_bus = event_bus
         self._local_folder_fs = _local_folder_fs
-        self._local_file_fs = _local_file_fs
+        self._file_transactions = _file_transactions
         self._remote_loader = _remote_loader
         self._syncer = _syncer
 
@@ -49,10 +48,6 @@ class WorkspaceFS:
             except FSMultiManifestLocalMiss as exc:
                 for access in exc.accesses:
                     await self._remote_loader.load_manifest(access)
-
-            except FSBlocksLocalMiss as exc:
-                for access in exc.accesses:
-                    await self._remote_loader.load_block(access)
 
     async def stat(self, path: FsPath) -> dict:
         path = self._cook_path(path)
@@ -86,25 +81,25 @@ class WorkspaceFS:
     async def file_fd_open(self, path: FsPath, mode="rw") -> int:
         path = self._cook_path(path)
         access = await self._load_and_retry(self._local_folder_fs.get_access, path, mode)
-        return self._local_file_fs.open(access)
+        return self._file_transactions.open(access)
 
     async def file_fd_close(self, fd: int) -> None:
-        self._local_file_fs.close(fd)
+        await self._file_transactions.close(fd)
 
     async def file_fd_seek(self, fd: int, offset: int) -> None:
-        self._local_file_fs.seek(fd, offset)
+        await self._file_transactions.seek(fd, offset)
 
     async def file_fd_truncate(self, fd: int, length: int) -> None:
-        self._local_file_fs.truncate(fd, length)
+        await self._file_transactions.truncate(fd, length)
 
     async def file_fd_write(self, fd: int, content: bytes, offset: int = None) -> int:
-        return self._local_file_fs.write(fd, content, offset)
+        return await self._file_transactions.write(fd, content, offset)
 
     async def file_fd_flush(self, fd: int) -> None:
-        self._local_file_fs.flush(fd)
+        await self._file_transactions.flush(fd)
 
     async def file_fd_read(self, fd: int, size: int = -1, offset: int = None) -> bytes:
-        return await self._load_and_retry(self._local_file_fs.read, fd, size, offset)
+        return await self._file_transactions.read(fd, size, offset)
 
     async def file_create(self, path: FsPath) -> UUID:
         path = self._cook_path(path)
