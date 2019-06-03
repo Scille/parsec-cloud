1e2655ef72c501a752f809c0d582969ea9e5e78d
c4ffein <c4ffein@gmail.com>
2019-06-03 18:50:44 +0200

    tests/core/test_invite_claim.py : separated test_invite_claim_user into test_invite_claim_non_admin_user and test_invite_claim_admin_user to check that the is_admin parameter of invite_and_create_user and of the returned LocalDevice by claim_user is correctly set

--- tests/core/test_invite_claim.py
+++ tests/core/test_invite_claim.py
@@ -15,7 +15,7 @@ from parsec.core.backend_connection import backend_cmds_pool_factory
 
 
 @pytest.mark.trio
-async def test_invite_claim_user(running_backend, backend, alice):
+async def test_invite_claim_non_admin_user(running_backend, backend, alice):
     new_device_id = DeviceID("zack@pc1")
     new_device = None
     token = generate_invitation_token()
@@ -33,6 +33,37 @@ async def test_invite_claim_user(running_backend, backend, alice):
             with trio.fail_after(1):
                 await spy.wait("event.connected", kwargs={"event_name": "user.claimed"})
         nursery.start_soon(_from_mallory)
+
+    assert new_device.is_admin is False
+
+    # Now connect as the new user
+    async with backend_cmds_pool_factory(
+        new_device.organization_addr, new_device.device_id, new_device.signing_key
+    ) as cmds:
+        await cmds.ping("foo")
+
+
+@pytest.mark.trio
+async def test_invite_claim_admin_user(running_backend, backend, alice):
+    new_device_id = DeviceID("zack@pc1")
+    new_device = None
+    token = generate_invitation_token()
+
+    async def _from_alice():
+        await invite_and_create_user(alice, new_device_id.user_id, token=token, is_admin=True)
+
+    async def _from_mallory():
+        nonlocal new_device
+        new_device = await claim_user(alice.organization_addr, new_device_id, token=token)
+
+    async with trio.open_nursery() as nursery:
+        with running_backend.backend.event_bus.listen() as spy:
+            nursery.start_soon(_from_alice)
+            with trio.fail_after(1):
+                await spy.wait("event.connected", kwargs={"event_name": "user.claimed"})
+        nursery.start_soon(_from_mallory)
+
+    assert new_device.is_admin
 
     # Now connect as the new user
     async with backend_cmds_pool_factory(
