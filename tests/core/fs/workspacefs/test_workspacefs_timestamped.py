0ad69f84e7e845dcc1d67ae4525ec61e07fe0f21
c4ffein <c4ffein>
2019-06-14 12:04:11 +0200

    fixed test_workspacefs_timestamped.py : added workspace.sync at the right time

--- tests/core/fs/workspacefs/test_workspacefs_timestamped.py
+++ tests/core/fs/workspacefs/test_workspacefs_timestamped.py
@@ -24,8 +24,10 @@ async def alice_workspace(alice_user_fs, running_backend):
         wid = await alice_user_fs.workspace_create("w")
         workspace = alice_user_fs.get_workspace(wid)
         await workspace.mkdir("/foo")
+        await workspace.sync("/")
     with freeze_time(day2):
         await workspace.touch("/foo/bar")
+        await workspace.sync("/")
     with freeze_time(day3):
         await workspace.touch("/foo/baz")
         await workspace.sync("/")
@@ -34,8 +36,10 @@ async def alice_workspace(alice_user_fs, running_backend):
         await workspace.mkdir("/files")
         await workspace.touch("/files/content")
         await workspace.write_bytes("/files/content", b"abcde")
+        await workspace.sync("/")
     with freeze_time(day5):
         await workspace.write_bytes("/files/content", b"fghij")
+        await workspace.sync("/")
 
     return workspace
 
