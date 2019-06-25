9e27b3c042a84f55037eccc1e63f63aea05613c1
c4ffein <c4ffein>
2019-06-25 19:52:32 +0200

    implemented read_bytes method bypassing local_storage for entries and manifests but not for blocks

--- parsec/core/fs/workspacefs/workspacefs_timestamped.py
+++ parsec/core/fs/workspacefs/workspacefs_timestamped.py
@@ -5,11 +5,18 @@ import errno
 from typing import Iterator
 import pendulum
 
-from parsec.core.types import FsPath
+from parsec.core.types import FsPath, LocalFileManifest, FileCursor
+
+# Needed if we want to completly bypass local_storage for blocks :  BlockAccess
 
 from parsec.core.fs.workspacefs.entry_transactions import from_errno
+from parsec.core.fs.workspacefs.file_transactions import normalize_offset, merge_buffers
 from parsec.core.fs.workspacefs.workspacefs import AnyPath
 import parsec.core.fs.workspacefs as workspacefs
+
+from parsec.core.fs.utils import is_file_manifest
+
+from parsec.core.local_storage import LocalStorageMissingError
 
 
 class WorkspaceFSTimestamped(workspacefs.WorkspaceFS):
@@ -84,10 +91,72 @@ class WorkspaceFSTimestamped(workspacefs.WorkspaceFS):
         for child in info["children"]:
             yield path / child
 
+    # Naive read_bytes not implementing caching, and private helpers for bypassing local_storage
+
+    # # Needed only if we want to bypass local_storage for blocks
+    # async def _load_block_no_storage(self, access: BlockAccess) -> bytes:
+    #     """
+    #     Raises:
+    #         BackendConnectionError
+    #         CryptoError
+    #     """
+    #     ciphered_block = await self.backend_cmds.block_read(access.id)
+    #     block = decrypt_raw_with_secret_key(access.key, ciphered_block)
+    #     assert sha256(block).hexdigest() == access.digest, access
+    #     return block
+
+    def _attempt_read(self, manifest: LocalFileManifest, start: int, end: int):
+        missing = []
+        data = bytearray(end - start)
+        merged = merge_buffers(manifest, start, end)
+        for cs in merged.spaces:
+            for bs in cs.buffers:
+                block_access = bs.buffer.access
+                try:
+                    buff = self.local_storage.get_block(block_access.id)
+                except LocalStorageMissingError:
+                    missing.append(block_access)
+                    continue
+
+                data[bs.start - cs.start : bs.end - cs.start] = buff[
+                    bs.buffer_slice_start : bs.buffer_slice_end
+                ]
+
+        return data, missing
+
     async def read_bytes(self, path: AnyPath, size: int = -1, offset: int = 0) -> bytes:
         path = FsPath(path)
-        _, fd = await self.entry_transactions.file_open(path, "r", self.timestamp)
-        try:
-            return await self.file_transactions.fd_read(fd, size, offset)
-        finally:
-            await self.file_transactions.fd_close(fd)
+
+        # Loop over attemps
+        missing = []
+
+        entry_id = await self.path_id(path)
+        remote_manifest = await self.remote_loader.load_manifest(entry_id, timestamp=self.timestamp)
+        manifest = remote_manifest.to_local(self.local_storage.device_id)
+        if not is_file_manifest(manifest):
+            raise from_errno(errno.EISDIR, str(path))
+        cursor = FileCursor(entry_id)
+
+        while True:
+            # for block_access in missing:
+            #    blocsk[block_access.id] = await _load_block_no_storage(block_access)
+
+            # No-op
+            offset = normalize_offset(offset, cursor, manifest)
+            if offset is not None and offset > manifest.size:
+                return b""
+
+            # Prepare
+            start = offset
+            size = manifest.size if size < 0 else size
+            end = min(offset + size, manifest.size)
+            data, missing = self._attempt_read(manifest, start, end)
+
+            # Retry
+            if missing:
+                continue
+
+            # Atomic change
+            cursor.offset = end
+
+            return data
