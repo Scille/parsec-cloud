51a96bd8ebdc95269ea989d04ba5ac60114ea844
c4ffein <c4ffein@gmail.com>
2019-06-27 11:41:23 +0200

    fix test_sync_monitor by touilleman (#535)

--- tests/core/test_sync_monitor.py
+++ tests/core/test_sync_monitor.py
@@ -6,13 +6,14 @@ import trio
 
 @pytest.mark.trio
 async def test_autosync_on_modification(mock_clock, running_backend, alice_core, alice2_user_fs):
-    mock_clock.autojump_threshold = 0.1
-
+    mock_clock.rate = 0  # Avoid potential concurrency with monitors
     await alice_core.event_bus.spy.wait_for_backend_connection_ready()
     wid = await alice_core.user_fs.workspace_create("w")
     workspace = alice_core.user_fs.get_workspace(wid)
     await alice_core.user_fs.sync()
 
+    mock_clock.rate = 1
+    mock_clock.autojump_threshold = 0.1
     with alice_core.event_bus.listen() as spy:
         await workspace.mkdir("/foo")
         foo_id = await workspace.path_id("/foo")
