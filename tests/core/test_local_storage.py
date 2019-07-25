f44c5da207e9b6fef6fbd4a0498cc1fc62fc81b2
Vincent Michel <vxgmichel@gmail.com>
2019-07-25 12:15:52 +0200

    Add tests for cache miss in local storage

--- tests/core/test_local_storage.py
+++ tests/core/test_local_storage.py
@@ -6,10 +6,27 @@ from parsec.types import DeviceID
 from parsec.crypto import SecretKey
 from parsec.core.local_storage import LocalStorage
 from parsec.core.persistent_storage import LocalStorageMissingError
-from parsec.core.types import LocalUserManifest, EntryID
+from parsec.core.types import (
+    LocalUserManifest,
+    LocalWorkspaceManifest,
+    LocalFolderManifest,
+    LocalFileManifest,
+    EntryID,
+)
 
 
 DEVICE = DeviceID("a@b")
+
+
+def create_entry(type=LocalUserManifest):
+    entry_id = EntryID()
+    if type is LocalUserManifest:
+        manifest = LocalUserManifest(
+            author=DEVICE, base_version=0, is_placeholder=True, need_sync=True
+        )
+    else:
+        manifest = type.make_placeholder(author=DEVICE, parent_id=EntryID())
+    return entry_id, manifest
 
 
 @pytest.fixture
@@ -20,31 +37,36 @@ def local_storage(tmpdir):
 
 
 def test_get_manifest(local_storage):
-    entry_id = EntryID()
+    entry_id, manifest = create_entry()
     with pytest.raises(LocalStorageMissingError):
         local_storage.get_manifest(entry_id)
-    local_storage.local_manifest_cache[entry_id] = "manifest"
-    assert local_storage.get_manifest(entry_id) == "manifest"
+    local_storage.local_manifest_cache[entry_id] = manifest
+    assert local_storage.get_manifest(entry_id) == manifest
 
 
 def test_set_manifest(local_storage):
-    entry_id = EntryID()
-    manifest = LocalUserManifest(
-        author=DEVICE, base_version=1, is_placeholder=False, need_sync=False
-    )
+    entry_id, manifest = create_entry()
     local_storage.set_manifest(entry_id, manifest)
     assert local_storage.local_manifest_cache[entry_id] == manifest
 
 
 def test_clear_manifest(local_storage):
-    entry_id = EntryID()
-    manifest = LocalUserManifest(
-        author=DEVICE, base_version=1, is_placeholder=False, need_sync=False
-    )
+    entry_id, manifest = create_entry()
     local_storage.set_manifest(entry_id, manifest)
-    assert local_storage.get_manifest(entry_id)
+    assert local_storage.get_manifest(entry_id) == manifest
 
     local_storage.clear_manifest(entry_id)
     assert entry_id not in local_storage.local_manifest_cache
     with pytest.raises(LocalStorageMissingError):
         local_storage.get_manifest(entry_id)
+
+
+@pytest.mark.parametrize(
+    "type", [LocalUserManifest, LocalWorkspaceManifest, LocalFolderManifest, LocalFileManifest]
+)
+def test_get_manifest_with_cache_miss(local_storage, type):
+    entry_id, manifest = create_entry(type)
+    local_storage.set_manifest(entry_id, manifest)
+    assert local_storage.get_manifest(entry_id) == manifest
+    local_storage.clear_memory_cache()
+    assert local_storage.get_manifest(entry_id) == manifest
