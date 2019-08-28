4b9ff1278a6c6e3558cfacd9d99a0a5056f3fc6a
Vincent Michel <vxgmichel@gmail.com>
2019-08-28 13:55:23 +0200

    Refactor state machines in fs tests (PR #643)

--- tests/core/fs/test_fs_offline_restart_and_tree.py
+++ tests/core/fs/test_fs_offline_restart_and_tree.py
@@ -3,16 +3,8 @@
 import os
 import pytest
 from hypothesis import strategies as st
-from hypothesis_trio.stateful import (
-    initialize,
-    rule,
-    run_state_machine_as_test,
-    TrioAsyncioRuleBasedStateMachine,
-    Bundle,
-)
+from hypothesis_trio.stateful import initialize, rule, Bundle
 from string import ascii_lowercase
-
-from tests.common import call_with_control
 
 
 def get_path(path):
@@ -25,36 +17,16 @@ st_entry_name = st.text(alphabet=ascii_lowercase, min_size=1, max_size=3)
 
 @pytest.mark.slow
 @pytest.mark.skipif(os.name == "nt", reason="Windows path style not compatible with oracle")
-def test_fs_offline_restart_and_tree(
-    hypothesis_settings, reset_testbed, oracle_fs_factory, user_fs_factory, alice
-):
-    class FSOfflineRestartAndTree(TrioAsyncioRuleBasedStateMachine):
+def test_fs_offline_restart_and_tree(user_fs_state_machine, oracle_fs_factory, alice):
+    class FSOfflineRestartAndTree(user_fs_state_machine):
         Files = Bundle("file")
         Folders = Bundle("folder")
 
-        async def restart_user_fs(self):
-            try:
-                await self.fs_controller.stop()
-            except AttributeError:
-                pass
-
-            async def _user_fs_controlled_cb(started_cb):
-                async with user_fs_factory(device=self.device) as user_fs:
-                    await started_cb(user_fs=user_fs)
-
-            self.user_fs_controller = await self.get_root_nursery().start(
-                call_with_control, _user_fs_controlled_cb
-            )
-
-        @property
-        def user_fs(self):
-            return self.user_fs_controller.user_fs
-
         @initialize(target=Folders)
         async def init(self):
-            await reset_testbed()
+            await self.reset_all()
             self.device = alice
-            await self.restart_user_fs()
+            await self.restart_user_fs(self.device)
             wid = await self.user_fs.workspace_create("w")
             self.workspace = self.user_fs.get_workspace(wid)
 
@@ -64,7 +36,7 @@ def test_fs_offline_restart_and_tree(
 
         @rule()
         async def restart(self):
-            await self.restart_user_fs()
+            await self.restart_user_fs(self.device)
 
         @rule(target=Files, parent=Folders, name=st_entry_name)
         async def create_file(self, parent, name):
@@ -130,4 +102,4 @@ def test_fs_offline_restart_and_tree(
                     await self.workspace.rename(get_path(src), get_path(dst))
             return dst
 
-    run_state_machine_as_test(FSOfflineRestartAndTree, settings=hypothesis_settings)
+    FSOfflineRestartAndTree.run_as_test()
