7e3db7a8e8a28d92145baf32e61b6d77106ed12e
Maxime GRANDCOLAS <maxime.grandcolas@gmail.com>
2019-08-30 13:47:46 +0200

    [GUI] Correctly displays if a user is admin

--- parsec/core/gui/users_widget.py
+++ parsec/core/gui/users_widget.py
@@ -25,6 +25,7 @@ class UserButton(QWidget, Ui_UserButton):
     ):
         super().__init__(*args, **kwargs)
         self.setupUi(self)
+        self.is_admin = is_admin
         if is_admin:
             self.label.setPixmap(QPixmap(":/icons/images/icons/owner2.png"))
         else:
@@ -66,6 +67,9 @@ class UserButton(QWidget, Ui_UserButton):
     def show_user_info(self):
         text = "{}\n\n".format(self.user_name)
         text += _("USER_CREATED_ON_{}").format(format_datetime(self.certified_on, full=True))
+        if self.is_admin:
+            text += "\n\n"
+            text += _("USER_IS_ADMIN")
         if self.label.is_revoked:
             text += "\n\n"
             text += _("USER_IS_REVOKED")
@@ -212,7 +216,7 @@ class UsersWidget(QWidget, Ui_UsersWidget):
             self.add_user(
                 str(user_info.user_id),
                 is_current_user=current_user == user,
-                is_admin=False,
+                is_admin=user_info.is_admin,
                 certified_on=user_info.certified_on,
                 is_revoked=all([device.revoked_on for device in user_devices]),
             )
