a3f999993a31eebec928ffbe31d3397a69206f98
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2019-09-10 20:32:28 +0200

    Fix concurrency error with test_device_claim_ok and test_user_claim_ok

--- tests/backend/user/test_user_claim.py
+++ tests/backend/user/test_user_claim.py
@@ -1,10 +1,12 @@
 # Parsec Cloud (https://parsec.cloud) Copyright (c) AGPLv3 2019 Scille SAS
 
 import pytest
+import trio
 from pendulum import Pendulum
 from async_generator import asynccontextmanager
 
 from parsec.api.protocol import (
+    UserID,
     DeviceID,
     user_get_invitation_creator_serializer,
     user_claim_serializer,
@@ -43,24 +45,44 @@ async def user_claim(sock, **kwargs):
 
 
 @pytest.mark.trio
-async def test_user_claim_ok(backend, anonymous_backend_sock, coolorg, alice, mallory_invitation):
-    with freeze_time(mallory_invitation.created_on), backend.event_bus.listen() as spy:
+async def test_user_claim_ok(
+    monkeypatch, backend, anonymous_backend_sock, coolorg, alice, mallory_invitation
+):
+    user_invitation_retreived = trio.Event()
+
+    vanilla_claim_user_invitation = backend.user.claim_user_invitation
+
+    async def _mocked_claim_user_invitation(*args, **kwargs):
+        ret = await vanilla_claim_user_invitation(*args, **kwargs)
+        user_invitation_retreived.set()
+        return ret
+
+    monkeypatch.setattr(backend.user, "claim_user_invitation", _mocked_claim_user_invitation)
+
+    with freeze_time(mallory_invitation.created_on):
         async with user_claim(
             anonymous_backend_sock,
             invited_user_id=mallory_invitation.user_id,
             encrypted_claim=b"<foo>",
         ) as prep:
 
-            await spy.wait_with_timeout("event.connected", {"event_name": "user.created"})
+            # `backend.user.create_user` will destroy the user invitation,
+            # so make sure we retreived it before
+            await user_invitation_retreived.wait()
 
-            # Ignored event
-            backend.event_bus.send(
-                "user.created",
-                organization_id=coolorg.organization_id,
-                user_id="dummy",
-                user_certificate=b"<dummy user certif>",
-                first_device_id="dummy@dummy",
-                first_device_certificate=b"<dummy device certif>",
+            # No the user we are waiting for
+            await backend.user.create_user(
+                alice.organization_id,
+                User(
+                    user_id=UserID("dummy"),
+                    user_certificate=b"<user certif>",
+                    user_certifier=alice.device_id,
+                ),
+                Device(
+                    device_id=DeviceID("dummy@pc1"),
+                    device_certificate=b"<device certif>",
+                    device_certifier=alice.device_id,
+                ),
             )
 
             await backend.user.create_user(
