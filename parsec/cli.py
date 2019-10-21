255368f9b62b559730d38b105638028fefa3746c
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2019-10-21 19:37:13 +0200

    Fix support of PARSEC_CMD_ARGS env var for cli when parsec is pip installed

--- parsec/cli.py
+++ parsec/cli.py
@@ -29,9 +29,23 @@ def cli():
 cli.add_command(core_cmd, "core")
 cli.add_command(backend_cmd, "backend")
 
+# Add support for PARSEC_CMD_ARGS env var
+
+vanilla_cli_main = cli.main
+
+
+def patched_cli_main(args=None, **kwargs):
+    if args is None:
+        args = sys.argv[1:]
+
+    raw_extra_args = os.environ.get("PARSEC_CMD_ARGS", "")
+    args += [os.path.expandvars(x) for x in raw_extra_args.split()]
+
+    return vanilla_cli_main(args=args, **kwargs)
+
+
+cli.main = patched_cli_main
+
 
 if __name__ == "__main__":
-    raw_extra_args = os.environ.get("PARSEC_CMD_ARGS", "")
-    extra_args = [os.path.expandvars(x) for x in raw_extra_args.split()]
-    argv = sys.argv[1:] + extra_args
-    cli(argv)
+    cli()
