6a914672a4c8f68f43a7eeb7d4bff7d24fb9c050
c4ffein <c4ffein@gmail.com>
2019-11-07 20:28:06 +0100

    setup.py : enforce OS check for PKCS11 installation as it tried to install on Windows with 'pip install -e .[all]'

--- setup.py
+++ setup.py
@@ -285,7 +285,10 @@ test_requirements = [
 PYQT_DEP = "PyQt5==5.13.1"
 BABEL_DEP = ("Babel==2.6.0",)
 extra_requirements = {
-    "pkcs11": ["python-pkcs11==0.5.0", "pycrypto==2.6.1"],
+    "pkcs11": [
+        'python-pkcs11==0.5.0;platform_system="Linux"',
+        'pycrypto==2.6.1;platform_system="Linux"',
+    ],
     "core": [
         PYQT_DEP,
         BABEL_DEP,
