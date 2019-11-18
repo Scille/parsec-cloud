b559c68ba5d58eb50d9edd7a9dbc5f79c6fac3a0
Vincent Michel <vxgmichel@gmail.com>
2019-11-18 17:25:57 +0100

    Work around trio issue #1308

--- tests/core/mountpoint/test_base.py
+++ tests/core/mountpoint/test_base.py
@@ -507,15 +507,16 @@ async def test_mountpoint_access_unicode(base_mountpoint, alice_user_fs, event_b
 
         await mountpoint_manager.mount_workspace(wid)
 
-        def _do():
-            root_path = mountpoint_manager.get_path_in_mountpoint(wid, FsPath(f"/"))
-            items = [x.name for x in Path(root_path).iterdir()]
-            assert items == [weird_name]
+        root_path = mountpoint_manager.get_path_in_mountpoint(wid, FsPath(f"/"))
 
-            item_path = mountpoint_manager.get_path_in_mountpoint(wid, FsPath(f"/{weird_name}"))
-            assert Path(item_path).exists()
+        # Work around trio issue #1308 (https://github.com/python-trio/trio/issues/1308)
+        items = await trio.run_sync_in_worker_thread(
+            lambda: [path.name for path in Path(root_path).iterdir()]
+        )
+        assert items == [weird_name]
 
-        await trio.run_sync_in_worker_thread(_do)
+        item_path = mountpoint_manager.get_path_in_mountpoint(wid, FsPath(f"/{weird_name}"))
+        assert await trio.Path(item_path).exists()
 
 
 @pytest.mark.mountpoint
