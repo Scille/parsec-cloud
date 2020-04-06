2013b4bd6fc796feaf3f3c1e94b80db2395113e6
Antoine FERRON <antoine.ferron@bitlogik.fr>
2020-04-06 13:00:54 +0200

    Fix dep conflicts #1097
    and remove twine installation from testing pipelines

--- setup.py
+++ setup.py
@@ -304,6 +304,8 @@ test_requirements = [
     # - see pip issues #7096/#6239/#4391/#988
     # Looking forward to the new pip dependency resolver!
     'pywin32==227;platform_system=="Windows"',
+    # Fix botocore and sphinx conflicting requirements on docutils
+    "docutils>=0.12,<0.16",
     # Documentation generation requirements
     "sphinx==2.4.3",
     "sphinx-intl==2.0.0",
