b54a9906f8af97ab4534be78b12053c62d1d0201
valentin <valentin.monte@yahoo.fr>
2020-04-09 12:35:42 +0200

    Remove the makemigration command and use the importlib_resources instead some old methods

--- parsec/backend/cli/migration.py
+++ parsec/backend/cli/migration.py
@@ -1,62 +1,33 @@
 # Parsec Cloud (https://parsec.cloud) Copyright (c) AGPLv3 2019 Scille SAS
 
 import click
-import glob
 import re
 
-from pathlib import Path
+import importlib_resources
 
 from parsec.utils import trio_run
 from parsec.cli_utils import spinner, cli_exception_handler
-from parsec.backend.postgresql import migrate_db
+from parsec.backend.postgresql import migrate_db, migrations
 
 
 MIGRATIONS_FOLDER = "parsec/backend/postgresql/migrations"
-MIGRATION_FILE_PATTERN = r"(?P<idx>\d{4})_\w*.sql"
+MIGRATION_FILE_PATTERN = r"(?P<idx>\d{4})_\w*.sql$"
 
 
 def _sorted_file_migrations():
     full_file_pattern = r"\S*(?P<name>%s)" % MIGRATION_FILE_PATTERN
     files = []
-    for f in glob.glob(f"{MIGRATIONS_FOLDER}/*.sql"):
-        match = re.search(full_file_pattern, f)
+    for f in importlib_resources.contents(migrations):
+        match = re.search(MIGRATION_FILE_PATTERN, f)
         if match:
-            files.append(match.group("name"))
+            files.append(f)
     return sorted(files, key=lambda f: re.sub(full_file_pattern, r"\g<1>", f))
-
-
-def _migration_idx(file_name):
-    match = re.search(MIGRATION_FILE_PATTERN, file_name)
-    return match.group("idx")
 
 
 def _validate_postgres_db_url(ctx, param, value):
     if not (value.startswith("postgresql://") or value.startswith("postgres://")):
         raise click.BadParameter("Must start with `postgresql://` or `postgres://`")
     return value
-
-
-@click.command(short_help="init a new migration file")
-@click.option(
-    "--name", required=True, help="Migration file name. Use to described the migration content"
-)
-def make_migration(name):
-    """
-    Initialize a new migration file.
-    """
-    migrations = _sorted_file_migrations()
-    idx = None
-    try:
-        last_migration = migrations[-1]
-    except IndexError:
-        idx = "0001"
-    else:
-        idx = _migration_idx(last_migration)
-        new_idx = "%04d" % (int(idx) + 1)
-        slugify_name = "_".join(name.split())
-        new_file_name = f"{MIGRATIONS_FOLDER}/{new_idx}_{slugify_name}.sql"
-        Path(f"{new_file_name}").touch()
-        click.echo(f"The migration file is created: {new_file_name}")
 
 
 @click.command(short_help="Updates database schema")
