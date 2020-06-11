90b019d42bd72c392af0dd4b691d1b6c7bcd23da
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2020-06-11 16:48:11 +0200

    Simplify SQL queries for realm

--- parsec/backend/postgresql/realm_queries/update_roles.py
+++ parsec/backend/postgresql/realm_queries/update_roles.py
@@ -1,8 +1,6 @@
 # Parsec Cloud (https://parsec.cloud) Copyright (c) AGPLv3 2019 Scille SAS
 
 from typing import Optional
-from pypika import Parameter
-from pypika.enums import Order
 
 from parsec.api.data import UserProfile
 from parsec.api.protocol import OrganizationID, RealmRole
@@ -17,51 +15,50 @@ from parsec.backend.realm import (
 from parsec.backend.postgresql.handler import send_signal
 from parsec.backend.postgresql.utils import query
 from parsec.backend.postgresql.message import send_message
-from parsec.backend.postgresql.tables import (
-    STR_TO_REALM_ROLE,
-    q_realm_internal_id,
-    q_realm,
-    q_realm_user_role,
+from parsec.backend.postgresql.tables import STR_TO_REALM_ROLE
+from parsec.backend.postgresql.queries import (
+    Q,
     q_user,
     q_user_internal_id,
     q_device_internal_id,
+    q_realm,
+    q_realm_internal_id,
 )
 
 
-_q_get_user_profile = (
-    q_user(organization_id=Parameter("$1"), user_id=Parameter("$2")).select("profile")
-).get_sql()
+_q_get_user_profile = Q(
+    q_user(organization_id="$organization_id", user_id="$user_id", select="profile")
+)
 
 
-_q_get_realm_status = (
-    q_realm(organization_id=Parameter("$1"), realm_id=Parameter("$2")).select(
-        "encryption_revision",
-        "maintenance_started_by",
-        "maintenance_started_on",
-        "maintenance_type",
+_q_get_realm_status = Q(
+    q_realm(
+        organization_id="$organization_id",
+        realm_id="$realm_id",
+        select="encryption_revision, maintenance_started_by, maintenance_started_on, maintenance_type",
     )
-).get_sql()
+)
 
 
-_q_get_roles = """
+_q_get_roles = Q(
+    f"""
 SELECT
-({}),
-({})
-FROM UNNEST($3::VARCHAR[]) AS needle_user_id
-""".format(
-    q_user_internal_id(organization_id=Parameter("$1"), user_id=Parameter("needle_user_id")),
-    q_realm_user_role(
-        organization_id=Parameter("$1"),
-        user_id=Parameter("needle_user_id"),
-        realm_id=Parameter("$2"),
+    { q_user_internal_id(organization_id="$organization_id", user_id="needle_user_id") },
+    (
+        SELECT role
+        FROM realm_user_role
+        WHERE
+            user_ = { q_user_internal_id(organization_id="$organization_id", user_id="needle_user_id") }
+            AND realm = { q_realm_internal_id(organization_id="$organization_id", realm_id="$realm_id") }
+        ORDER BY certified_on DESC LIMIT 1
     )
-    .select("role")
-    .orderby("certified_on", order=Order.desc)
-    .limit(1),
+FROM UNNEST($users_ids::VARCHAR[]) AS needle_user_id
+"""
 )
 
 
-_q_insert_realm_user_role = """
+_q_insert_realm_user_role = Q(
+    f"""
 INSERT INTO realm_user_role(
     realm,
     user_,
@@ -70,19 +67,13 @@ INSERT INTO realm_user_role(
     certified_by,
     certified_on
 ) SELECT
-    ({}),
-    ({}),
-    ({}),
-    ({}),
-    ({}),
-    ({})
-""".format(
-    q_realm_internal_id(organization_id=Parameter("$1"), realm_id=Parameter("$2")),
-    q_user_internal_id(organization_id=Parameter("$1"), user_id=Parameter("$3")),
-    Parameter("$4"),
-    Parameter("$5"),
-    q_device_internal_id(organization_id=Parameter("$1"), device_id=Parameter("$6")),
-    Parameter("$7"),
+    { q_realm_internal_id(organization_id="$organization_id", realm_id="$realm_id") },
+    { q_user_internal_id(organization_id="$organization_id", user_id="$user_id") },
+    $role,
+    $certificate,
+    { q_device_internal_id(organization_id="$organization_id", device_id="$granted_by") },
+    $granted_on
+"""
 )
 
 
@@ -97,7 +88,9 @@ async def query_update_roles(
         raise RealmAccessError("Cannot modify our own role")
 
     # Make sure user profile is compatible
-    rep = await conn.fetchrow(_q_get_user_profile, organization_id, new_role.user_id)
+    rep = await conn.fetchrow(
+        *_q_get_user_profile(organization_id=organization_id, user_id=new_role.user_id)
+    )
     if not rep:
         raise RealmNotFoundError(f"User `{new_role.user_id}` doesn't exist")
     if rep["profile"] == UserProfile.OUTSIDER.value and new_role.role in (
@@ -107,7 +100,9 @@ async def query_update_roles(
         raise RealmIncompatibleProfileError("User with OUTSIDER profile cannot be MANAGER or OWNER")
 
     # Retrieve realm and make sure it is not under maintenance
-    rep = await conn.fetchrow(_q_get_realm_status, organization_id, new_role.realm_id)
+    rep = await conn.fetchrow(
+        *_q_get_realm_status(organization_id=organization_id, realm_id=new_role.realm_id)
+    )
     if not rep:
         raise RealmNotFoundError(f"Realm `{new_role.realm_id}` doesn't exist")
     if rep["maintenance_type"]:
@@ -115,10 +110,11 @@ async def query_update_roles(
 
     # Check access rights and user existance
     ((author_id, author_role), (user_id, existing_user_role)) = await conn.fetch(
-        _q_get_roles,
-        organization_id,
-        new_role.realm_id,
-        (new_role.granted_by.user_id, new_role.user_id),
+        *_q_get_roles(
+            organization_id=organization_id,
+            realm_id=new_role.realm_id,
+            users_ids=(new_role.granted_by.user_id, new_role.user_id),
+        )
     )
     assert author_id
     assert user_id
@@ -139,14 +135,15 @@ async def query_update_roles(
         raise RealmRoleAlreadyGranted()
 
     await conn.execute(
-        _q_insert_realm_user_role,
-        organization_id,
-        new_role.realm_id,
-        new_role.user_id,
-        new_role.role.value if new_role.role else None,
-        new_role.certificate,
-        new_role.granted_by,
-        new_role.granted_on,
+        *_q_insert_realm_user_role(
+            organization_id=organization_id,
+            realm_id=new_role.realm_id,
+            user_id=new_role.user_id,
+            role=new_role.role.value if new_role.role else None,
+            certificate=new_role.certificate,
+            granted_by=new_role.granted_by,
+            granted_on=new_role.granted_on,
+        )
     )
 
     await send_signal(
