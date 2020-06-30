7db80e80ef7026b49d98505661ad4e50c4692299
c4ffein <c4ffein@gmail.com>
2020-06-30 06:03:57 +0200

    Made a docker-compose

---
+++ packaging/docker/wait-for-postgres.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# wait-for-postgres.sh
+
+# Make Parsec wait for Postgres as it is not resilient enough
+# As recommended as a quickfix even by Docker documentation
+# Will be rendered useless when docker-compose is updated
+
+set -e
+
+host="$1"
+shift
+cmd="$@"
+
+apt-get update -y; apt-get install postgresql-client -y  # I'm ashamed.
+
+until psql "$host" -c '\q'; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "Postgres is up - executing command"
+exec $cmd
