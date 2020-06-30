22b2da5195577c6e69b5484b098c8a218ed7e289
Sithis <ma.sithis@gmail.com>
2020-06-30 14:38:15 +0200

    Adding workspacefile Object and modifying the open_file() method in workspacefs

--- parsec/core/fs/workspacefs/workspacefs.py
+++ parsec/core/fs/workspacefs/workspacefs.py
@@ -41,6 +41,7 @@ from parsec.core.fs.exceptions import (
     FSNotADirectoryError,
 )
 
+from parsec.core.fs.workspacefs.workspacefile import WorkspaceFile
 
 AnyPath = Union[FsPath, str]
 
@@ -346,6 +347,13 @@ class WorkspaceFS:
             return await self.transactions.fd_read(fd, size, offset)
         finally:
             await self.transactions.fd_close(fd)
+
+    async def open_file(self, path: AnyPath, mode="r"):
+        path = FsPath(path)
+        _, fd = await self.transactions.file_open(path, mode)
+        f = WorkspaceFile(fd, self.transactions, mode=mode, path=path)
+        await f.ainit()
+        return f
 
     async def write_bytes(
         self, path: AnyPath, data: bytes, offset: int = 0, truncate: bool = True
