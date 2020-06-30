e116cfa320ad2bee716e18188a558ec1eebf19b0
Sithis <ma.sithis@gmail.com>
2020-06-30 14:41:00 +0200

    Using workspacefile object in files_widget.py

--- parsec/core/gui/files_widget.py
+++ parsec/core/gui/files_widget.py
@@ -99,14 +99,15 @@ async def _do_import(workspace_fs, files, total_size, progress_signal):
             progress_signal.emit(src.name, current_size)
 
             async with await trio.open_file(src, "rb") as f:
-                read_size = 0
-                while True:
-                    chunk = await f.read(DEFAULT_BLOCK_SIZE)
-                    if not chunk:
-                        break
-                    await workspace_fs.write_bytes(dst, chunk, read_size)
-                    read_size += len(chunk)
-                    progress_signal.emit(src.name, current_size + read_size)
+                async with await workspace_fs.open_file(dst, "w") as dest_file:
+                    read_size = 0
+                    while True:
+                        chunk = await f.read(DEFAULT_BLOCK_SIZE)
+                        if not chunk:
+                            break
+                        await dest_file.write(chunk)
+                        read_size += len(chunk)
+                        progress_signal.emit(src.name, current_size + read_size)
             current_size += read_size + 1
             progress_signal.emit(src.name, current_size)
 
