bitlogik/guardata

[GUI] Sexier log in screen

parsec/core/gui/login_widget.py
@@ -1,12 +1,105 @@
 # Parsec Cloud (https://parsec.cloud) Copyright (c) AGPLv3 2019 Scille SAS
 
+from pathlib import Path
+
 from PyQt5.QtCore import pyqtSignal, Qt
 from PyQt5.QtWidgets import QWidget
 
 from parsec.core.local_device import list_available_devices
+from parsec.api.protocol import OrganizationID, DeviceID
+
 from parsec.core.gui.lang import translate as _
 from parsec.core.gui.parsec_application import ParsecApp
 from parsec.core.gui.ui.login_widget import Ui_LoginWidget
+from parsec.core.gui.ui.account_button import Ui_AccountButton
+from parsec.core.gui.ui.login_accounts_widget import Ui_LoginAccountsWidget
+from parsec.core.gui.ui.login_password_input_widget import Ui_LoginPasswordInputWidget
+from parsec.core.gui.ui.login_no_devices_widget import Ui_LoginNoDevicesWidget
+
+
+class AccountButton(QWidget, Ui_AccountButton):
+    clicked = pyqtSignal(OrganizationID, DeviceID, Path)
+
+    def __init__(self, organization_id, device_id, key_file):
+        super().__init__()
+        self.setupUi(self)
+        self.organization_id = organization_id
+        self.device_id = device_id
+        self.key_file = key_file
+        self.label_device.setText(self.device_id.device_name)
+        self.label_name.setText(self.device_id.user_id)
+        self.label_organization.setText(self.organization_id)
+        if str(device_id.user_id) == "bob":
+            self.label_device.setText("Desktop")
+            self.label_name.setText("Maxime GRANDCOLAS <maxime.grandcolas@gmail.com>")
+            self.label_organization.setText("Scille")
+
+    def mousePressEvent(self, event):
+        if event.button() & Qt.LeftButton:
+            self.clicked.emit(self.organization_id, self.device_id, self.key_file)
+
+
+class LoginAccountsWidget(QWidget, Ui_LoginAccountsWidget):
+    account_clicked = pyqtSignal(OrganizationID, DeviceID, Path)
+
+    def __init__(self, devices):
+        super().__init__()
+        self.setupUi(self)
+        for o, d, t, kf in devices:
+            if not ParsecApp.is_device_connected(o, d):
+                ab = AccountButton(o, d, kf)
+                ab.clicked.connect(self.account_clicked.emit)
+                self.accounts_widget.layout().addWidget(ab)
+
+
+class LoginPasswordInputWidget(QWidget, Ui_LoginPasswordInputWidget):
+    back_clicked = pyqtSignal()
+    log_in_clicked = pyqtSignal(Path, str)
+
+    def __init__(self, organization_id, device_id, key_file):
+        super().__init__()
+        self.setupUi(self)
+        self.key_file = key_file
+        self.button_back.clicked.connect(self.back_clicked.emit)
+        self.button_login.clicked.connect(self._on_log_in_clicked)
+        self.label_instructions.setText(
+            _("TEXT_LOGIN_ENTER_PASSWORD_INSTRUCTIONS_organization-device-user-email").format(
+                organization=organization_id,
+                user=device_id.user_id,
+                device=device_id.device_name,
+                email="",
+            )
+        )
+
+    def _on_log_in_clicked(self):
+        self.button_login.setDisabled(True)
+        self.button_login.setText(_("ACTION_LOGGING_IN"))
+        self.log_in_clicked.emit(self.key_file, self.line_edit_password.text())
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_Return and self.button_login.isEnabled():
+            self._on_log_in_clicked()
+        event.accept()
+
+    def reset(self):
+        self.button_login.setDisabled(False)
+        self.line_edit_password.setText("")
+        self.button_login.setText(_("ACTION_LOG_IN"))
+
+
+class LoginNoDevicesWidget(QWidget, Ui_LoginNoDevicesWidget):
+    join_organization_clicked = pyqtSignal()
+    create_organization_clicked = pyqtSignal()
+
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        if ParsecApp.connected_devices:
+            self.label_no_device.setText(_("TEXT_LOGIN_NO_AVAILABLE_DEVICE"))
+        else:
+            self.label_no_device.setText(_("TEXT_LOGIN_NO_DEVICE_ON_MACHINE"))
+        self.button_create_org.clicked.connect(self.create_organization_clicked.emit)
+        self.button_join_org.clicked.connect(self.join_organization_clicked.emit)
 
 
 class LoginWidget(QWidget, Ui_LoginWidget):
@@ -23,57 +116,53 @@ class LoginWidget(QWidget, Ui_LoginWidget):
         self.login_failed_sig = login_failed_sig
 
         login_failed_sig.connect(self.on_login_failed)
-        self.button_login.clicked.connect(self.try_login)
-        self.combo_username.currentTextChanged.connect(self.line_edit_password.clear)
-        self.button_create_org.clicked.connect(self.create_organization_clicked.emit)
-        self.button_join_org.clicked.connect(self.join_organization_clicked.emit)
         self.reload_devices()
-        self.button_login.setEnabled(self.combo_username.count() > 0)
-        self.line_edit_password.setFocus()
 
     def on_login_failed(self):
-        self.button_login.setEnabled(self.combo_username.count() > 0)
-        self.button_login.setText(_("ACTION_LOG_IN"))
-
-    def keyPressEvent(self, event):
-        if event.key() == Qt.Key_Return:
-            self.try_login()
-        event.accept()
+        item = self.widget.layout().itemAt(0)
+        if item:
+            lw = item.widget()
+            lw.reset()
 
     def reload_devices(self):
-        while self.combo_username.count():
-            self.combo_username.removeItem(0)
+        self._clear_widget()
         devices = list_available_devices(self.config.config_dir)
-        # Display devices in `<organization>:<device_id>` format
-        self.devices = {}
-        for o, d, t, kf in devices:
-            if not ParsecApp.is_device_connected(o, d):
-                self.combo_username.addItem(f"{o}:{d}")
-                self.devices[f"{o}:{d}"] = (o, d, t, kf)
-        last_device = self.config.gui_last_device
-        if last_device and last_device in self.devices:
-            self.combo_username.setCurrentText(last_device)
-        if len(self.devices):
-            self.widget_no_device.hide()
-            self.widget_login.show()
+        if len(devices):
+            accounts_widget = LoginAccountsWidget(devices)
+            accounts_widget.account_clicked.connect(self._on_account_clicked)
+            self.widget.layout().addWidget(accounts_widget)
+            accounts_widget.setFocus()
         else:
-            self.widget_no_device.show()
-            self.widget_login.hide()
-            if ParsecApp.connected_devices:
-                self.label_no_device.setText(_("TEXT_LOGIN_NO_AVAILABLE_DEVICE"))
-            else:
-                self.label_no_device.setText(_("TEXT_LOGIN_NO_DEVICE_ON_MACHINE"))
+            no_device_widget = LoginNoDevicesWidget()
+            no_device_widget.create_organization_clicked.connect(
+                self.create_organization_clicked.emit
+            )
+            no_device_widget.join_organization_clicked.connect(self.join_organization_clicked.emit)
+            self.widget.layout().addWidget(no_device_widget)
+            no_device_widget.setFocus()
 
-    def try_login(self):
-        if not self.combo_username.currentText():
-            return
-        *args, key_file = self.devices[self.combo_username.currentText()]
-        self.button_login.setDisabled(True)
-        self.button_login.setText(_("ACTION_LOGGING_IN"))
-        self.login_with_password_clicked.emit(key_file, self.line_edit_password.text())
+    def _clear_widget(self):
+        while self.widget.layout().count() != 0:
+            item = self.widget.layout().takeAt(0)
+            if item:
+                w = item.widget()
+                self.widget.layout().removeWidget(w)
+                w.hide()
+                w.setParent(None)
+
+    def _on_account_clicked(self, organization_id, device_id, key_file):
+        self._clear_widget()
+        lw = LoginPasswordInputWidget(organization_id, device_id, key_file)
+        lw.back_clicked.connect(self.reload_devices)
+        lw.log_in_clicked.connect(self.try_login)
+        self.widget.layout().addWidget(lw)
+        lw.line_edit_password.setFocus()
+
+    def _on_back_clicked(self):
+        self.reload_devices()
+
+    def try_login(self, key_file, password):
+        self.login_with_password_clicked.emit(key_file, password)
 
     def disconnect_all(self):
         pass
-
-    def emit_login_with_password(self, key_file, password):
-        self.login_with_password_clicked.emit(key_file, password)

parsec/core/gui/parsec-gui.pro
@@ -46,7 +46,11 @@ FORMS += forms/main_window.ui \
     forms/file_history_button.ui \
     forms/create_org_first_page_widget.ui \
     forms/create_org_widget.ui \
-    forms/create_org_second_page_widget.ui
+    forms/create_org_second_page_widget.ui \
+    forms/account_button.ui \
+    forms/login_accounts_widget.ui \
+    forms/login_password_input_widget.ui \
+    forms/login_no_devices_widget.ui
 
 RESOURCES += rc/resources.qrc \
     forms/rc/resources.qrc

Scille/parsec-cloud

parsec/core/gui/login_widget.py
@@ -1,16 +1,104 @@
 # Parsec Cloud (https://parsec.cloud) Copyright (c) AGPLv3 2019 Scille SAS
 
+from pathlib import Path
+
 from PyQt5.QtCore import pyqtSignal, Qt
 from PyQt5.QtWidgets import QWidget
 
-from parsec.core.local_device import list_available_devices
+from parsec.core.local_device import list_available_devices, AvailableDevice
+
 from parsec.core.gui.lang import translate as _
 from parsec.core.gui.parsec_application import ParsecApp
 from parsec.core.gui.ui.login_widget import Ui_LoginWidget
+from parsec.core.gui.ui.account_button import Ui_AccountButton
+from parsec.core.gui.ui.login_accounts_widget import Ui_LoginAccountsWidget
+from parsec.core.gui.ui.login_password_input_widget import Ui_LoginPasswordInputWidget
+from parsec.core.gui.ui.login_no_devices_widget import Ui_LoginNoDevicesWidget
+
+
+class AccountButton(QWidget, Ui_AccountButton):
+    clicked = pyqtSignal(AvailableDevice)
+
+    def __init__(self, device):
+        super().__init__()
+        self.setupUi(self)
+        self.device = device
+        self.label_device.setText(self.device.device_display)
+        self.label_name.setText(self.device.short_user_display)
+        self.label_organization.setText(self.device.organization_id)
+
+    def mousePressEvent(self, event):
+        if event.button() & Qt.LeftButton:
+            self.clicked.emit(self.device)
+
+
+class LoginAccountsWidget(QWidget, Ui_LoginAccountsWidget):
+    account_clicked = pyqtSignal(AvailableDevice)
+
+    def __init__(self, devices):
+        super().__init__()
+        self.setupUi(self)
+        for available_device in devices:
+            if not ParsecApp.is_device_connected(
+                available_device.organization_id, available_device.device_id
+            ):
+                ab = AccountButton(available_device)
+                ab.clicked.connect(self.account_clicked.emit)
+                self.accounts_widget.layout().insertWidget(0, ab)
+
+
+class LoginPasswordInputWidget(QWidget, Ui_LoginPasswordInputWidget):
+    back_clicked = pyqtSignal()
+    log_in_clicked = pyqtSignal(Path, str)
+
+    def __init__(self, device):
+        super().__init__()
+        self.setupUi(self)
+        self.device = device
+        self.button_back.clicked.connect(self.back_clicked.emit)
+        self.button_login.clicked.connect(self._on_log_in_clicked)
+        self.label_instructions.setText(
+            _("TEXT_LOGIN_ENTER_PASSWORD_INSTRUCTIONS_organization-device-user-email").format(
+                organization=self.device.organization_id,
+                user=self.device.short_user_display,
+                device=self.device.device_display,
+                email="",
+            )
+        )
+
+    def _on_log_in_clicked(self):
+        self.button_login.setDisabled(True)
+        self.button_login.setText(_("ACTION_LOGGING_IN"))
+        self.log_in_clicked.emit(self.device.key_file_path, self.line_edit_password.text())
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_Return and self.button_login.isEnabled():
+            self._on_log_in_clicked()
+        event.accept()
+
+    def reset(self):
+        self.button_login.setDisabled(False)
+        self.line_edit_password.setText("")
+        self.button_login.setText(_("ACTION_LOG_IN"))
+
+
+class LoginNoDevicesWidget(QWidget, Ui_LoginNoDevicesWidget):
+    join_organization_clicked = pyqtSignal()
+    create_organization_clicked = pyqtSignal()
+
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        if ParsecApp.connected_devices:
+            self.label_no_device.setText(_("TEXT_LOGIN_NO_AVAILABLE_DEVICE"))
+        else:
+            self.label_no_device.setText(_("TEXT_LOGIN_NO_DEVICE_ON_MACHINE"))
+        self.button_create_org.clicked.connect(self.create_organization_clicked.emit)
+        self.button_join_org.clicked.connect(self.join_organization_clicked.emit)
 
 
 class LoginWidget(QWidget, Ui_LoginWidget):
-    login_with_password_clicked = pyqtSignal(object, str)
+    login_with_password_clicked = pyqtSignal(Path, str)
     create_organization_clicked = pyqtSignal()
     join_organization_clicked = pyqtSignal()
 
@@ -23,58 +111,53 @@ class LoginWidget(QWidget, Ui_LoginWidget):
         self.login_failed_sig = login_failed_sig
 
         login_failed_sig.connect(self.on_login_failed)
-        self.button_login.clicked.connect(self.try_login)
-        self.combo_username.currentTextChanged.connect(self.line_edit_password.clear)
-        self.button_create_org.clicked.connect(self.create_organization_clicked.emit)
-        self.button_join_org.clicked.connect(self.join_organization_clicked.emit)
         self.reload_devices()
-        self.button_login.setEnabled(self.combo_username.count() > 0)
-        self.line_edit_password.setFocus()
 
     def on_login_failed(self):
-        self.button_login.setEnabled(self.combo_username.count() > 0)
-        self.button_login.setText(_("ACTION_LOG_IN"))
-
-    def keyPressEvent(self, event):
-        if event.key() == Qt.Key_Return and self.button_login.isEnabled():
-            self.try_login()
-        event.accept()
+        item = self.widget.layout().itemAt(0)
+        if item:
+            lw = item.widget()
+            lw.reset()
 
     def reload_devices(self):
-        while self.combo_username.count():
-            self.combo_username.removeItem(0)
-        # Display devices in `<organization>:<user>@<device>` format
-        self.devices = {}
-        for available_device in list_available_devices(self.config.config_dir):
-            if not ParsecApp.is_device_connected(
-                available_device.organization_id, available_device.device_id
-            ):
-                name = f"{available_device.organization_id}: {available_device.user_display} @ {available_device.device_display}"
-                self.combo_username.addItem(name)
-                self.devices[name] = available_device
-        last_device = self.config.gui_last_device
-        if last_device and last_device in self.devices:
-            self.combo_username.setCurrentText(last_device)
-        if len(self.devices):
-            self.widget_no_device.hide()
-            self.widget_login.show()
+        self._clear_widget()
+        devices = list_available_devices(self.config.config_dir)
+        if not len(devices):
+            no_device_widget = LoginNoDevicesWidget()
+            no_device_widget.create_organization_clicked.connect(
+                self.create_organization_clicked.emit
+            )
+            no_device_widget.join_organization_clicked.connect(self.join_organization_clicked.emit)
+            self.widget.layout().addWidget(no_device_widget)
+            no_device_widget.setFocus()
         else:
-            self.widget_no_device.show()
-            self.widget_login.hide()
-            if ParsecApp.connected_devices:
-                self.label_no_device.setText(_("TEXT_LOGIN_NO_AVAILABLE_DEVICE"))
-            else:
-                self.label_no_device.setText(_("TEXT_LOGIN_NO_DEVICE_ON_MACHINE"))
+            accounts_widget = LoginAccountsWidget(devices)
+            accounts_widget.account_clicked.connect(self._on_account_clicked)
+            self.widget.layout().addWidget(accounts_widget)
+            accounts_widget.setFocus()
 
-    def try_login(self):
-        if not self.combo_username.currentText():
-            return
-        available_device = self.devices[self.combo_username.currentText()]
-        self.button_login.setDisabled(True)
-        self.button_login.setText(_("ACTION_LOGGING_IN"))
-        self.login_with_password_clicked.emit(
-            available_device.key_file_path, self.line_edit_password.text()
-        )
+    def _clear_widget(self):
+        while self.widget.layout().count() != 0:
+            item = self.widget.layout().takeAt(0)
+            if item:
+                w = item.widget()
+                self.widget.layout().removeWidget(w)
+                w.hide()
+                w.setParent(None)
+
+    def _on_account_clicked(self, device):
+        self._clear_widget()
+        lw = LoginPasswordInputWidget(device)
+        lw.back_clicked.connect(self.reload_devices)
+        lw.log_in_clicked.connect(self.try_login)
+        self.widget.layout().addWidget(lw)
+        lw.line_edit_password.setFocus()
+
+    def _on_back_clicked(self):
+        self.reload_devices()
+
+    def try_login(self, key_file, password):
+        self.login_with_password_clicked.emit(key_file, password)
 
     def disconnect_all(self):
         pass

parsec/core/gui/parsec-gui.pro
@@ -60,7 +60,11 @@ FORMS += forms/main_window.ui \
     forms/greet_device_widget.ui \
     forms/claim_device_instructions_widget.ui \
     forms/claim_device_code_exchange_widget.ui \
-    forms/claim_device_provide_info_widget.ui
+    forms/claim_device_provide_info_widget.ui \
+    forms/account_button.ui \
+    forms/login_accounts_widget.ui \
+    forms/login_password_input_widget.ui \
+    forms/login_no_devices_widget.ui
 
 RESOURCES += rc/resources.qrc \
     forms/rc/resources.qrc