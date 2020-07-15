1d2d9c59e5d5ae189ed8cbc549ac5df2e142f51f
AntoineDupre <dupre.antoine.nicolas@gmail.com>
2020-07-15 22:08:44 +0200

    Fix flaky test

--- tests/core/gui/test_revoked_user.py
+++ tests/core/gui/test_revoked_user.py
@@ -2,27 +2,9 @@
 
 import pendulum
 import pytest
-from PyQt5 import QtCore
 from PyQt5.QtWidgets import QLabel
 
 from parsec.api.data import RevokedUserCertificateContent
-from parsec.core.local_device import save_device_with_password
-
-
-@pytest.fixture
-async def logged_gui(aqtbot, gui_factory, running_backend, autoclose_dialog, core_config, bob):
-    save_device_with_password(core_config.config_dir, bob, "P@ssw0rd")
-    gui = await gui_factory()
-    lw = gui.test_get_login_widget()
-    tabw = gui.test_get_tab()
-
-    # assert lw.combo_username.currentText() == f"{bob.organization_id}:{bob.device_id}"
-    await aqtbot.key_clicks(lw.line_edit_password, "P@ssw0rd")
-
-    async with aqtbot.wait_signals([lw.login_with_password_clicked, tabw.logged_in]):
-        await aqtbot.mouse_click(lw.button_login, QtCore.Qt.LeftButton)
-
-    return gui
 
 
 @pytest.mark.gui
@@ -35,22 +17,20 @@ async def test_revoked_notification(
         author=alice_user_fs.device.device_id, timestamp=now, user_id=bob.user_id
     ).dump_and_sign(alice_user_fs.device.signing_key)
     central_widget = logged_gui.test_get_central_widget()
+
     assert central_widget is not None
-    async with aqtbot.wait_signal(central_widget.new_notification):
+    async with aqtbot.wait_signal(central_widget.new_notification, timeout=3000):
         await alice_user_fs.backend_cmds.user_revoke(revoked_device_certificate)
-        await aqtbot.mouse_click(central_widget.menu.button_users, QtCore.Qt.LeftButton)
-        await aqtbot.mouse_click(central_widget.menu.button_devices, QtCore.Qt.LeftButton)
 
     # Assert dialog
     assert len(autoclose_dialog.dialogs) == 1
     dialog = autoclose_dialog.dialogs[0]
     assert dialog[0] == "Error"
     assert dialog[1] == "This device is revoked"
-
-    # Assert device and user widgets
-    devices_w = logged_gui.test_get_devices_widget()
-    users_w = logged_gui.test_get_users_widget()
-    assert devices_w.layout_devices.count() == 1
-    assert isinstance(devices_w.layout_devices.itemAt(0).widget(), QLabel)
+    # Users widget
+    users_w = await logged_gui.test_switch_to_users_widget(error=True)
     assert users_w.layout_users.count() == 1
     assert isinstance(users_w.layout_users.itemAt(0).widget(), QLabel)
+    # Devices widget
+    devices_w = await logged_gui.test_switch_to_devices_widget(error=True)
+    assert isinstance(devices_w.layout_devices.itemAt(0).widget(), QLabel)
