2c75d8b32cfc40ba4c756ef7f9b6ea28eb73497a
c4ffein <c4ffein@gmail.com>
2020-07-15 15:53:05 +0200

    fixed tests

--- tests/backend/test_postgres_db.py
+++ tests/backend/test_postgres_db.py
@@ -21,24 +21,25 @@ async def test_postgresql_connection_ok(
 @pytest.mark.trio
 @pytest.mark.postgresql
 async def test_postgresql_connection_not_ok(
-    postgresql_url, alice, bob, backend_factory, backend_sock_factory
+    postgresql_url, alice, bob, backend_factory, backend_sock_factory, caplog
 ):
     postgresql_url = re.sub(
         ":(\\d+)/", lambda m: f":{str(int(m.group()[1:-1])+1)}/", postgresql_url
     )
-    with pytest.warns(None) as record:
-        with pytest.raises(OSError):
-            async with backend_factory(
-                config={"blockstore_config": PostgreSQLBlockStoreConfig(), "db_url": postgresql_url}
-            ):
-                pass
-    assert len(record) == 0
+    with pytest.raises(OSError):
+        async with backend_factory(
+            config={"blockstore_config": PostgreSQLBlockStoreConfig(), "db_url": postgresql_url}
+        ):
+            pass
+    assert len(caplog.records) == 1
+    assert caplog.records[0].levelname == "ERROR"
+    assert "initial db connection failed" in caplog.records[0].message
 
 
 @pytest.mark.trio
 @pytest.mark.postgresql
 async def test_postgresql_connection_not_ok_retrying(
-    postgresql_url, alice, bob, backend_factory, backend_sock_factory, recwarn
+    postgresql_url, alice, bob, backend_factory, backend_sock_factory, recwarn, caplog
 ):
     postgresql_url = re.sub(
         ":(\\d+)/", lambda m: f":{str(int(m.group()[1:-1])+1)}/", postgresql_url
@@ -53,4 +54,9 @@ async def test_postgresql_connection_not_ok_retrying(
             }
         ):
             pass
-    assert len(recwarn) == 4
+    assert len(caplog.records) == 4
+    for record in caplog.records[:3]:
+        assert record.levelname == "WARNING"
+        assert "initial db connection failed" in record.message
+    assert caplog.records[3].levelname == "ERROR"
+    assert "initial db connection failed" in caplog.records[3].message
