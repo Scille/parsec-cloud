b3a9e307326fb73571b62dbbca03ff402c0cf5b3
c4ffein <c4ffein@gmail.com>
2020-07-16 03:09:22 +0200

    try fix

--- tests/backend/test_postgres_db.py
+++ tests/backend/test_postgres_db.py
@@ -21,7 +21,8 @@ async def test_postgresql_connection_not_ok(
         async with backend_factory(config={"db_url": postgresql_url}):
             pass
     assert "[Errno 111] Connect call failed" in str(exc.value)
-    assert len(caplog.records) == 1
+    # assert len(caplog.records) == 1
+    assert [caplog.records[0].msg, caplog.records[1].msg] == "test"  # TODO DELETE
     assert caplog.records[0].levelname == "ERROR"
     assert "initial db connection failed" in caplog.records[0].message
 
@@ -31,17 +32,20 @@ async def test_postgresql_connection_not_ok(
 async def test_postgresql_connection_not_ok_retrying(
     postgresql_url, backend_factory, caplog, unused_tcp_port, autojump_clock
 ):
+    tries_number = 4
+    tries_sleep = 3
     postgresql_url = f"postgresql://localhost:{unused_tcp_port}/dummy"
-    with pytest.raises(OSError):
-        with trio.fail_after(3 * 3 + 1):
+    with pytest.raises(OSError) as exc:
+        with trio.fail_after((tries_number - 1) * tries_sleep + 1):
             async with backend_factory(
                 config={
                     "db_url": postgresql_url,
-                    "db_first_tries_number": 4,
-                    "db_first_tries_sleep": 3,
+                    "db_first_tries_number": tries_number,
+                    "db_first_tries_sleep": tries_sleep,
                 }
             ):
                 pass
+    assert "[Errno 111] Connect call failed" in str(exc.value)
     assert len(caplog.records) == 4
     for record in caplog.records[:3]:
         assert record.levelname == "WARNING"
