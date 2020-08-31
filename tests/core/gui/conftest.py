8c890dc96cbf1dcc32da63d6ec600cbe9ab30026
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2020-08-31 22:27:55 +0200

    Rework core/gui/conftest.py to provide TestingMainWindow.gui.test_switch_to_logged_in

--- tests/core/gui/conftest.py
+++ tests/core/gui/conftest.py
@@ -11,7 +11,6 @@ from concurrent import futures
 from importlib import import_module
 from PyQt5 import QtCore
 
-from parsec.event_bus import EventBus
 from parsec import __version__ as parsec_version
 from parsec.core.local_device import save_device_with_password
 from parsec.core.gui.main_window import MainWindow
@@ -268,7 +267,13 @@ def widget_catcher_factory(aqtbot, monkeypatch):
 
 @pytest.fixture
 def gui_factory(
-    aqtbot, qtbot, qt_thread_gateway, testing_main_window_cls, core_config, monkeypatch
+    aqtbot,
+    qtbot,
+    qt_thread_gateway,
+    testing_main_window_cls,
+    core_config,
+    monkeypatch,
+    event_bus_factory,
 ):
     windows = []
 
@@ -283,7 +288,7 @@ def gui_factory(
             gui_language="en",
             gui_show_confined=False,
         )
-        event_bus = event_bus or EventBus()
+        event_bus = event_bus or event_bus_factory()
         # Language config rely on global var, must reset it for each test !
         switch_language(core_config)
 
@@ -334,36 +339,8 @@ async def logged_gui(aqtbot, gui_factory, core_config, alice, bob, fixtures_cust
         device = alice
     else:
         device = bob
-
-    save_device_with_password(core_config.config_dir, device, "P@ssw0rd")
-
     gui = await gui_factory()
-    lw = gui.test_get_login_widget()
-    tabw = gui.test_get_tab()
-
-    accounts_w = lw.widget.layout().itemAt(0).widget()
-    assert accounts_w
-
-    async with aqtbot.wait_signal(accounts_w.account_clicked):
-        await aqtbot.mouse_click(
-            accounts_w.accounts_widget.layout().itemAt(0).widget(), QtCore.Qt.LeftButton
-        )
-
-    def _password_widget_shown():
-        assert isinstance(lw.widget.layout().itemAt(0).widget(), LoginPasswordInputWidget)
-
-    await aqtbot.wait_until(_password_widget_shown)
-
-    password_w = lw.widget.layout().itemAt(0).widget()
-
-    await aqtbot.key_clicks(password_w.line_edit_password, "P@ssw0rd")
-
-    async with aqtbot.wait_signals([lw.login_with_password_clicked, tabw.logged_in]):
-        await aqtbot.mouse_click(password_w.button_login, QtCore.Qt.LeftButton)
-
-    central_widget = gui.test_get_central_widget()
-    assert central_widget is not None
-
+    gui.test_switch_to_logged_in(device)
     return gui
 
 
@@ -517,5 +494,38 @@ def testing_main_window_cls(aqtbot, qt_thread_gateway):
             await aqtbot.wait_until(_entry_available)
 
             return f_w
+
+        async def test_switch_to_logged_in(self, device):
+            save_device_with_password(self.config.config_dir, device, "P@ssw0rd")
+
+            lw = self.test_get_login_widget()
+            # Reload to take into account the new saved device
+            await aqtbot.run(lw.reload_devices)
+            tabw = self.test_get_tab()
+
+            accounts_w = lw.widget.layout().itemAt(0).widget()
+            assert accounts_w
+
+            async with aqtbot.wait_signal(accounts_w.account_clicked):
+                await aqtbot.mouse_click(
+                    accounts_w.accounts_widget.layout().itemAt(0).widget(), QtCore.Qt.LeftButton
+                )
+
+            def _password_widget_shown():
+                assert isinstance(lw.widget.layout().itemAt(0).widget(), LoginPasswordInputWidget)
+
+            await aqtbot.wait_until(_password_widget_shown)
+
+            password_w = lw.widget.layout().itemAt(0).widget()
+
+            await aqtbot.key_clicks(password_w.line_edit_password, "P@ssw0rd")
+
+            async with aqtbot.wait_signals([lw.login_with_password_clicked, tabw.logged_in]):
+                await aqtbot.mouse_click(password_w.button_login, QtCore.Qt.LeftButton)
+
+            central_widget = self.test_get_central_widget()
+            assert central_widget is not None
+
+            return central_widget
 
     return TestingMainWindow
