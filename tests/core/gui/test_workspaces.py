710ca19c946bface10616637604c862b218c3210
AntoineDupre <dupre.antoine.nicolas@gmail.com>
2020-09-04 13:40:03 +0200

    Increase mountpoint gui test timeout

--- tests/core/gui/test_workspaces.py
+++ tests/core/gui/test_workspaces.py
@@ -235,7 +235,7 @@ async def test_mountpoint_open_in_explorer_button(aqtbot, running_backend, logge
         assert wk_button.button_open.isEnabled()
         assert wk_button.switch_button.isChecked()
 
-    await aqtbot.wait_until(_mounted)
+    await aqtbot.wait_until(_mounted, timeout=2000)
     _on_switch_clicked_mock.assert_called_once()
     _on_switch_clicked_mock.reset_mock()
 
@@ -257,5 +257,5 @@ async def test_mountpoint_open_in_explorer_button(aqtbot, running_backend, logge
         assert not wk_button.switch_button.isChecked()
         assert not wk_button.button_open.isEnabled()
 
-    await aqtbot.wait_until(_unmounted)
+    await aqtbot.wait_until(_unmounted, timeout=2000)
     _on_switch_clicked_mock.assert_called_once()
