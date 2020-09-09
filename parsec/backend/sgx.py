a4fe531e7cd10c2fb1c65b7f199d31c592d6f835
Sithi5 <ma.sithis@gmail.com>
2020-09-09 16:55:07 +0200

    test

--- parsec/backend/sgx.py
+++ parsec/backend/sgx.py
@@ -4,11 +4,18 @@ from parsec.api.protocol import APIV1_HandshakeType, sgx_hello_world_serializer
 from parsec.backend.utils import catch_protocol_errors, api
 
 from sys import stderr
+from ctypes import cdll
+from pathlib import Path
+import os.path
+
+dll_name = "sgxlib.dll"
+dll_location = Path(os.path.dirname(__file__) + "/" + dll_name)
+lib_sgx = cdll.LoadLibrary(str(dll_location))
 
 
 class BaseSgxComponent:
     def __init__(self):
-        pass
+        self.lib_sgx = lib_sgx
 
     @api("sgx_hello_world", handshake_types=[APIV1_HandshakeType.ADMINISTRATION])
     @catch_protocol_errors
@@ -18,4 +25,4 @@ class BaseSgxComponent:
         return sgx_hello_world_serializer.rep_dump({"status": "ok"})
 
     async def hello_world(self):
-        print("Hello World !", file=stderr)
+        print(self.lib_sgx.initialize_enclave(), file=stderr)
