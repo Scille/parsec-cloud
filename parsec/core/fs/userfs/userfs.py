72289a4a360f288a0e0549003ced5f6a41f720b6
Vincent Michel <vxgmichel@gmail.com>
2020-09-11 17:11:53 +0200

    Add trio_typing to the requirements and bump trio to 0.16.0

--- parsec/core/fs/userfs/userfs.py
+++ parsec/core/fs/userfs/userfs.py
@@ -2,6 +2,7 @@
 
 import trio
 from pathlib import Path
+from trio_typing import TaskStatus
 from pendulum import Pendulum, now as pendulum_now
 from typing import (
     Tuple,
@@ -264,7 +265,7 @@ class UserFS:
         path = self.path / str(workspace_id)
 
         async def workspace_storage_task(
-            task_status: trio.TaskStatus = trio.TASK_STATUS_IGNORED
+            task_status: TaskStatus = trio.TASK_STATUS_IGNORED
         ) -> None:
             async with WorkspaceStorage.run(self.device, path, workspace_id) as workspace_storage:
                 task_status.started(workspace_storage)
