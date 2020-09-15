e8dd520eff5a71a78d59b1421695f7f3df70c6a2
Maxime GRANDCOLAS <maxime.grandcolas@gmail.com>
2020-09-15 11:58:16 +0200

    [GUI] Refresh devices list when logging out

--- tests/core/gui/test_login.py
+++ tests/core/gui/test_login.py
@@ -1,14 +1,16 @@
 # Parsec Cloud (https://parsec.cloud) Copyright (c) AGPLv3 2019 Scille SAS
 
 import pytest
-from PyQt5 import QtCore
+from PyQt5 import QtCore, QtWidgets
 
 from parsec.core.local_device import save_device_with_password, list_available_devices
 from parsec.core.gui.parsec_application import ParsecApp
+from parsec.core.gui.central_widget import CentralWidget
 from parsec.core.gui.login_widget import (
     LoginPasswordInputWidget,
     LoginAccountsWidget,
     LoginNoDevicesWidget,
+    LoginWidget,
 )
 
 
@@ -141,3 +143,76 @@ async def test_login_no_available_devices(
 
     no_device_w = lw.widget.layout().itemAt(0).widget()
     assert isinstance(no_device_w, LoginNoDevicesWidget)
+
+
+@pytest.mark.gui
+@pytest.mark.trio
+async def test_login_logout_account_list_refresh(
+    aqtbot, gui_factory, autoclose_dialog, core_config, alice, bob
+):
+    # Create two devices before starting the gui
+    password = "P@ssw0rd"
+    save_device_with_password(core_config.config_dir, alice, password)
+    save_device_with_password(core_config.config_dir, bob, password)
+
+    gui = await gui_factory()
+    lw = gui.test_get_login_widget()
+    tabw = gui.test_get_tab()
+
+    acc_w = lw.widget.layout().itemAt(0).widget()
+    assert acc_w
+
+    # 3 because we have a spacer
+    assert acc_w.accounts_widget.layout().count() == 3
+
+    async with aqtbot.wait_signal(acc_w.account_clicked):
+        await aqtbot.mouse_click(
+            acc_w.accounts_widget.layout().itemAt(0).widget(), QtCore.Qt.LeftButton
+        )
+
+    def _password_widget_shown():
+        assert isinstance(lw.widget.layout().itemAt(0).widget(), LoginPasswordInputWidget)
+
+    await aqtbot.wait_until(_password_widget_shown)
+
+    password_w = lw.widget.layout().itemAt(0).widget()
+
+    await aqtbot.key_clicks(password_w.line_edit_password, "P@ssw0rd")
+
+    async with aqtbot.wait_signals([lw.login_with_password_clicked, tabw.logged_in]):
+        await aqtbot.mouse_click(password_w.button_login, QtCore.Qt.LeftButton)
+
+    central_widget = gui.test_get_central_widget()
+    assert central_widget is not None
+    corner_widget = gui.tab_center.cornerWidget(QtCore.Qt.TopLeftCorner)
+    assert isinstance(corner_widget, QtWidgets.QPushButton)
+    assert corner_widget.isVisible()
+
+    # Now add a new tab
+    await aqtbot.mouse_click(corner_widget, QtCore.Qt.LeftButton)
+
+    def _switch_to_login_tab():
+        assert gui.tab_center.count() == 2
+        gui.tab_center.setCurrentIndex(1)
+        assert isinstance(gui.test_get_login_widget(), LoginWidget)
+
+    await aqtbot.wait_until(_switch_to_login_tab)
+
+    acc_w = gui.test_get_login_widget().widget.layout().itemAt(0).widget()
+    # 2 because we have a spacer
+    assert acc_w.accounts_widget.layout().count() == 2
+
+    def _switch_to_main_tab():
+        gui.tab_center.setCurrentIndex(0)
+        assert isinstance(gui.test_get_central_widget(), CentralWidget)
+
+    await aqtbot.wait_until(_switch_to_main_tab)
+    await gui.test_logout()
+
+    assert gui.tab_center.count() == 1
+
+    def _wait_devices_refreshed():
+        acc_w = gui.test_get_login_widget().widget.layout().itemAt(0).widget()
+        assert acc_w.accounts_widget.layout().count() == 3
+
+    await aqtbot.wait_until(_wait_devices_refreshed)
