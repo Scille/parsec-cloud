404ca3e6973c76c92d178fb7a56fb2e5d47d3e71
Sithi5 <ma.sithis@gmail.com>
2020-09-21 14:37:47 +0200

    pagination in sql

--- parsec/backend/postgresql/user_queries/find.py
+++ parsec/backend/postgresql/user_queries/find.py
@@ -2,7 +2,7 @@
 
 from pendulum import now as pendulum_now
 from functools import lru_cache
-from typing import Tuple, List
+from typing import Tuple, List, Optional
 
 from parsec.api.protocol import UserID, OrganizationID, HumanHandle
 from parsec.backend.user import HumanFindResultItem
@@ -10,7 +10,7 @@ from parsec.backend.postgresql.utils import Q, q_organization_internal_id, query
 
 
 @lru_cache()
-def _q_factory(query, omit_revoked):
+def _q_factory(query, omit_revoked, offset, limit):
     conditions = []
     if query:
         conditions.append("AND user_id ~* $query")
@@ -18,44 +18,62 @@ def _q_factory(query, omit_revoked):
         conditions.append("AND (revoked_on IS NULL OR revoked_on > $now)")
     return Q(
         f"""
-SELECT user_id
+SELECT
+    user_id,
+    count(*) OVER() AS full_count
 FROM user_
 WHERE
     organization = { q_organization_internal_id("$organization_id") }
     { " ".join(conditions) }
 ORDER BY user_id
+LIMIT { limit }
+OFFSET {offset}
     """
     )
 
 
 @lru_cache()
-def _q_human_factory(query, omit_revoked, omit_non_human):
+def _q_human_factory(query, omit_revoked, omit_non_human, offset, limit):
     conditions = []
-    if query:
-        conditions.append("AND CONCAT(human.label,human.email,user_.user_id) ~* $query")
     if omit_revoked:
         conditions.append("AND (user_.revoked_on IS NULL OR user_.revoked_on > $now)")
-    if omit_non_human:
+    # We only query on human
+    if omit_non_human or query:
         conditions.append("AND user_.human IS NOT NULL")
+    if query:
+        conditions.append("AND CONCAT(human.label,human.email) ~* $query")
+        conditions.append("ORDER BY human.label")
     return Q(
-        f"""
-SELECT
+        f"""SELECT
     user_.user_id,
     human.email,
     human.label,
-    user_.revoked_on IS NOT NULL AND user_.revoked_on <= $now
-FROM user_ LEFT JOIN human ON user_.human=human._id
-WHERE
+    user_.revoked_on IS NOT NULL AND user_.revoked_on <= $now,
+    count(*) OVER() AS full_count
+    FROM user_ LEFT JOIN human ON user_.human=human._id
+    WHERE
     user_.organization = { q_organization_internal_id("$organization_id") }
     { " ".join(conditions) }
+
+    LIMIT { limit }
+    OFFSET {offset}
     """
     )
 
 
 @query()
 async def query_find(
-    conn, organization_id: OrganizationID, query: str, page: int, per_page: int, omit_revoked: bool
+    conn,
+    organization_id: OrganizationID,
+    page: int = 1,
+    per_page: int = 100,
+    omit_revoked: bool = False,
+    query: Optional[str] = None,
 ) -> Tuple[List[UserID], int]:
+    if page >= 1:
+        offset = (page - 1) * per_page
+    else:
+        return ([], 0)
     if query:
         try:
             UserID(query)
@@ -63,79 +81,72 @@ async def query_find(
             # Contains invalid caracters, no need to go further
             return ([], 0)
 
+    q = _q_factory(
+        query=True if query else False,
+        omit_revoked=True if omit_revoked else False,
+        offset=offset,
+        limit=per_page,
+    )
+    if query:
         if omit_revoked:
-            q = _q_factory(query=True, omit_revoked=True)
-            args = q(organization_id=organization_id, query=query, now=pendulum_now())
-
+            args = q(organization_id=organization_id, now=pendulum_now(), query=query)
         else:
-            q = _q_factory(query=True, omit_revoked=False)
             args = q(organization_id=organization_id, query=query)
-
     else:
-
         if omit_revoked:
-            q = _q_factory(query=False, omit_revoked=True)
             args = q(organization_id=organization_id, now=pendulum_now())
-
         else:
-            q = _q_factory(query=False, omit_revoked=False)
             args = q(organization_id=organization_id)
 
-    all_results = await conn.fetch(*args)
-    # TODO: should user LIMIT and OFFSET in the SQL query instead
-    results = [UserID(x[0]) for x in all_results[(page - 1) * per_page : page * per_page]]
-    return results, len(all_results)
+    raw_results = await conn.fetch(*args)
+    if raw_results:
+        total = raw_results[0]["full_count"]
+    else:
+        total = 0
+    results = [UserID(x[0]) for x in raw_results]
+
+    return results, total
 
 
 @query()
 async def query_find_humans(
     conn,
     organization_id: OrganizationID,
-    query: str,
-    page: int,
-    per_page: int,
-    omit_revoked: bool,
-    omit_non_human: bool,
+    omit_revoked: bool = False,
+    omit_non_human: bool = False,
+    page: int = 1,
+    per_page: int = 100,
+    query: Optional[str] = None,
 ) -> Tuple[List[HumanFindResultItem], int]:
-    if query:
-
-        if omit_revoked:
-            q = _q_human_factory(query=True, omit_revoked=True, omit_non_human=omit_non_human)
-            args = q(organization_id=organization_id, now=pendulum_now(), query=query)
-
-        else:
-            q = _q_human_factory(query=True, omit_revoked=False, omit_non_human=omit_non_human)
-            args = q(organization_id=organization_id, now=pendulum_now(), query=query)
-
+    if page >= 1:
+        offset = (page - 1) * per_page
     else:
+        return ([], 0)
 
-        if omit_revoked:
-            q = _q_human_factory(query=False, omit_revoked=True, omit_non_human=omit_non_human)
-            args = q(organization_id=organization_id, now=pendulum_now())
-
-        else:
-            q = _q_human_factory(query=False, omit_revoked=False, omit_non_human=omit_non_human)
-            args = q(organization_id=organization_id, now=pendulum_now())
+    q = _q_human_factory(
+        query=True if query else False,
+        omit_revoked=True if omit_revoked else False,
+        omit_non_human=True if omit_non_human else False,
+        offset=offset,
+        limit=per_page,
+    )
+    if query:
+        args = q(organization_id=organization_id, now=pendulum_now(), query=query)
+    else:
+        args = q(organization_id=organization_id, now=pendulum_now())
 
     raw_results = await conn.fetch(*args)
-
-    humans = [
+    if raw_results:
+        total = raw_results[0]["full_count"]
+    else:
+        total = 0
+    results = [
         HumanFindResultItem(
             user_id=UserID(user_id),
-            human_handle=HumanHandle(email=email, label=label),
+            human_handle=HumanHandle(email=email, label=label) if email is not None else None,
             revoked=revoked,
         )
-        for user_id, email, label, revoked in raw_results
-        if email is not None
+        for user_id, email, label, revoked, _ in raw_results
     ]
-    non_humans = [
-        HumanFindResultItem(user_id=UserID(user_id), human_handle=None, revoked=revoked)
-        for user_id, email, label, revoked in raw_results
-        if email is None
-    ]
-    results = [
-        *sorted(humans, key=lambda x: (x.human_handle.label.lower(), x.user_id.lower())),
-        *sorted(non_humans, key=lambda x: x.user_id.lower()),
-    ]
-    # TODO: should user LIMIT and OFFSET in the SQL query instead
-    return results[(page - 1) * per_page : page * per_page], len(results)
+
+    return results, total
