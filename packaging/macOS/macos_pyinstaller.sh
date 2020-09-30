1d2722c7fd1e7bf96e9f840b9545fa9792aa53ce
Alban Costaz <alban.costaz@gmail.com>
2020-09-30 11:28:33 +0200

    Modified macOS pyinstaller script

--- packaging/macOS/macos_pyinstaller.sh
+++ packaging/macOS/macos_pyinstaller.sh
@@ -1,4 +1,9 @@
 # Note : pyinstaller doesn't work with python 3.8 yet.
 # see https://github.com/pyinstaller/pyinstaller/issues/4311
 
-pip install pyinstaller && pyinstaller parsec.spec
+cd ../..
+python3 setup.py bdist_wheel
+python3 -m pip install .
+python3 -m pip install 'pyinstaller==4.0'
+cd packaging/macOS
+pyinstaller parsec.spec
