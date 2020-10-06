359bfd3fa9a1ee7af32f4411e9139bacb3103762
Sithi5 <ma.sithis@gmail.com>
2020-10-06 16:23:55 +0200

    API protocol for organization stats (v2)

--- parsec/api/protocol/cmds.py
+++ parsec/api/protocol/cmds.py
@@ -42,6 +42,8 @@ AUTHENTICATED_CMDS = {
     "realm_update_roles",
     "realm_start_reencryption_maintenance",
     "realm_finish_reencryption_maintenance",
+    # Organization
+    "organization_stats",
 }
 INVITED_CMDS = {
     "ping",  # TODO: remove ping and ping event (only have them in tests)
