7e29b12b3aa470dd98842d466f38710ddcca9b26
Alban Costaz <alban.costaz@gmail.com>
2020-10-27 10:59:48 +0100

    Rebased on master, adjusted pyinstaller hooks

--- packaging/macOS/my_hooks/hook-pendulum.py
+++ packaging/macOS/my_hooks/hook-pendulum.py
@@ -12,11 +12,11 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # ------------------------------------------------------------------
 
-# from PyInstaller.utils.hooks import collect_data_files, collect_submodules
+from PyInstaller.utils.hooks import collect_data_files, collect_submodules
 
 
 # Pendulum checks for locale modules via os.path.exists before import.
 # If the include_py_files option is turned off, this check fails, pendulum
 # will raise a ValueError.
-# datas = collect_data_files("pendulum.locales", include_py_files=True)
-# hiddenimports = collect_submodules("pendulum.locales")
+datas = collect_data_files("pendulum.locales", include_py_files=True)
+hiddenimports = collect_submodules("pendulum.locales")
