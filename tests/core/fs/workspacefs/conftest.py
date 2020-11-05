8fd72b8bd72d667eaf976fe074633fb2bce55725
Sithi5 <ma.sithis@gmail.com>
2020-11-05 16:53:30 +0100

    improve test_file_history

--- tests/core/fs/workspacefs/conftest.py
+++ tests/core/fs/workspacefs/conftest.py
@@ -8,10 +8,13 @@ import pytest
 async def alice_workspace(alice_user_fs, running_backend):
     wid = await alice_user_fs.workspace_create("w")
     workspace = alice_user_fs.get_workspace(wid)
+
     await workspace.mkdir("/foo")
     await workspace.touch("/foo/bar")
     await workspace.touch("/foo/baz")
     await workspace.sync()
+    await alice_user_fs.sync()
+
     return workspace
 
 
@@ -20,6 +23,9 @@ async def alice_workspace(alice_user_fs, running_backend):
 async def bob_workspace(bob_user_fs, running_backend):
     wid = await bob_user_fs.workspace_create("w")
     workspace = bob_user_fs.get_workspace(wid)
+
     await workspace.mkdir("/foo")
     await workspace.sync()
+    await bob_user_fs.sync()
+
     return workspace
--- tests/core/fs/workspacefs/test_file_history.py
+++ tests/core/fs/workspacefs/test_file_history.py
@@ -2,43 +2,44 @@
 
 import pytest
 
-from parsec.core.fs.workspacefs.versioning_helpers import VersionLister
+from parsec.core.fs.workspacefs.versioning_helpers import VersionLister, TimestampBoundedData
 from parsec.core.types import FsPath
+from unittest.mock import ANY
 
 
 @pytest.mark.trio
-async def test_file_history(alice_workspace):
+async def test_file_history(alice, alice_workspace):
     sync_by_id = alice_workspace.sync_by_id
     entry = alice_workspace.get_workspace_entry()
     wid = entry.id
 
-    # Empty workspace
-    await sync_by_id(wid)
-
-    # Creating files and writing first byte
+    # Creating files
     await alice_workspace.touch("/f")
 
     # Test version before first sync
-    f_versions = await VersionLister(alice_workspace).list(FsPath("/f"))
+    versions_list, download_limit_reached = await VersionLister(alice_workspace).list(FsPath("/f"))
+    assert download_limit_reached is True
+    assert not versions_list
 
-    # Assert if task list have been completed
-    assert f_versions[1] is True
-
-    # Should have no version yet because we didn't sync
-    assert not f_versions[0]
-
-    # Syncronysing the files
     await sync_by_id(wid)
 
     # Checking again, should have the first version now
-    f_versions = await VersionLister(alice_workspace).list(FsPath("/f"))
-
-    # Assert if task list have been completed
-    assert f_versions[1] is True
-    # Assert there is a version in the list
-    assert f_versions[0]
-    # Checking the first version of the list
-    assert getattr(f_versions[0][0], "version") == 1
+    versions_list, download_limit_reached = await VersionLister(alice_workspace).list(FsPath("/f"))
+    assert download_limit_reached is True
+    assert versions_list == [
+        TimestampBoundedData(
+            id=ANY,
+            version=1,
+            early=ANY,
+            late=ANY,
+            creator=alice.device_id,
+            updated=ANY,
+            is_folder=False,
+            size=0,
+            source=None,
+            destination=None,
+        )
+    ]
 
     # Updating the file a couple of time and sync again to test the version list
     for i in range(20):
@@ -46,20 +47,19 @@ async def test_file_history(alice_workspace):
         await f.write(str(i).encode())
         await f.close()
         await sync_by_id(wid)
-    f_versions = await VersionLister(alice_workspace).list(FsPath("/f"))
-    # Assert if task list have been completed
-    assert f_versions[1] is True
+    versions_list, download_limit_reached = await VersionLister(alice_workspace).list(FsPath("/f"))
+    assert download_limit_reached is True
 
     # _sanitize_list is removing the first 1 version because it is the empty manifest set
     version_nb = 2
     previous_late = None
-    for version in f_versions[0]:
+    for version in versions_list:
         assert version.version == version_nb
         version_nb += 1
-        # Checking DateTime is coherent
+        # Checking if DateTime is coherent
         if previous_late:
             assert previous_late == version.early
         previous_late = version.late
-    # File should have 21 versions (20 modification + 1 creation). version_nb - 1 because it is
+    # File should have 21 versions (20 modifications + 1 creation). version_nb - 1 because it is
     # incremented once too often at the last loop cycle.
     assert version_nb - 1 == 21
