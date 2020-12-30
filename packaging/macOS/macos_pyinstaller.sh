63334d63f2289367338b830186d57411acd2debd
Ciem <martin.corentin98@gmail.com>
2020-12-30 10:24:04 +0100

    Fix macos installation

--- packaging/macOS/macos_pyinstaller.sh
+++ packaging/macOS/macos_pyinstaller.sh
@@ -5,13 +5,13 @@
 
 BASEDIR=$(dirname $(greadlink -f "$0") )
 cd $BASEDIR/../..
-python3 -m pip install 'pyinstaller==4.0'
+python3 -m pip install 'pyinstaller==4.1'
 # Pyinstaller read the imports in the codebase recursively then copy the
 # corresponding needed packages from the current virtualenv.
-python3 -m pip install[core]
+python3 -m pip install .[core]
 cd $BASEDIR
 
-if [ $1 == '-f' -o $1 == '--force' ]
+if [[ $1 == '-f' || $1 == '--force' ]]
 then
     FORCE="--noconfirm";
 else
