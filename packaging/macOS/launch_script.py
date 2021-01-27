fac4a1c12575844ceb369493439567c46228783c
Alban Costaz <alban.costaz@gmail.com>
2021-01-27 17:16:35 +0100

    Added setlocale to MacOS launch script

--- packaging/macOS/launch_script.py
+++ packaging/macOS/launch_script.py
@@ -3,9 +3,12 @@
 import parsec.cli
 import sys
 import os
+import locale
 
 os.environ["SENTRY_URL"] = "https://863e60bbef39406896d2b7a5dbd491bb@sentry.io/1212848"
 os.environ["PREFERRED_ORG_CREATION_BACKEND_ADDR"] = "parsec://saas.parsec.cloud/"
+
+locale.setlocale(locale.LC_ALL, "")
 
 sys.argv = [sys.argv[0], "core", "gui", *sys.argv[1:]]
 parsec.cli.cli.main()
