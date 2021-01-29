12afdd6a4669e65d7f96c8329363741bb80e577a
Alban Costaz <alban.costaz@gmail.com>
2021-01-29 13:12:20 +0100

    .dmg background image

--- packaging/macOS/launch_script.py
+++ packaging/macOS/launch_script.py
@@ -8,6 +8,8 @@ import locale
 os.environ["SENTRY_URL"] = "https://863e60bbef39406896d2b7a5dbd491bb@sentry.io/1212848"
 os.environ["PREFERRED_ORG_CREATION_BACKEND_ADDR"] = "parsec://saas.parsec.cloud/"
 
+# The locale values appear to be None by default when launching the app from the
+# Finder. If not set, Click will report an encoding error and the app won't launch.
 locale.setlocale(locale.LC_ALL, "")
 
 sys.argv = [sys.argv[0], "core", "gui", *sys.argv[1:]]
