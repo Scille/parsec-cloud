28c9506bd3ff5dcddaf8e7b20d61557027addcf1
Alban Costaz <alban.costaz@gmail.com>
2021-02-11 13:00:07 +0100

    Added Qt envvar to launch_script for 11.X compatibility

--- packaging/macOS/launch_script.py
+++ packaging/macOS/launch_script.py
@@ -8,6 +8,10 @@ import locale
 os.environ["SENTRY_URL"] = "https://863e60bbef39406896d2b7a5dbd491bb@sentry.io/1212848"
 os.environ["PREFERRED_ORG_CREATION_BACKEND_ADDR"] = "parsec://saas.parsec.cloud/"
 
+# Setting QT_MAC_WANTS_LAYER makes Qt work properly on MacOS 11.X
+# without having to use another version.
+os.environ["QT_MAC_WANTS_LAYER"] = "1"
+
 # The locale values appear to be None by default when launching the app from the
 # Finder. If not set, Click will report an encoding error and the app won't launch.
 locale.setlocale(locale.LC_ALL, "")
