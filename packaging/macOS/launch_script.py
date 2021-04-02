43c69787bfee86ab7db9e9951793d10d9e5d18a9
Alban Costaz <alban.costaz@gmail.com>
2021-04-02 12:19:42 +0200

    Bump pyqt5-sip to 12.8.1, added MacOS launch_script comment

--- packaging/macOS/launch_script.py
+++ packaging/macOS/launch_script.py
@@ -6,8 +6,19 @@ import os
 from parsec.cli import cli
 from click import core
 
-# This prevents click from raising encoding errors at startup
 core._verify_python3_env = lambda: None
+# This prevents click from raising encoding errors at startup.
+# It would be better to wrap the original _verify_python3_env so that we could
+# log an error when it raises an exception instead of silence it. This would
+# allow us to have more insight of when those errors occurs (OS version,
+# locales used etc.)
+# However the crash occurs before we initialize sentry logging, so it would be a
+# mess to catch the exception, save it on the side, then retrieve and log it
+# after sentry initialization...
+# On top of that, we don't really care about unicode in click here (we are
+# expecting to receive no arguments from argv or a single url which is should
+# not contain unicode, similarly we output ascii logs to stdout but nobody read
+# them anyway when starting the application this way)
 
 os.environ["SENTRY_URL"] = "https://863e60bbef39406896d2b7a5dbd491bb@sentry.io/1212848"
 os.environ["PREFERRED_ORG_CREATION_BACKEND_ADDR"] = "parsec://saas.parsec.cloud/"
