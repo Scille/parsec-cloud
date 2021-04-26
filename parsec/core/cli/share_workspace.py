03163322b45dda816914cff01ae9df22e583fae0
AntoineDupre <dupre.antoine.nicolas@gmail.com>
2021-04-26 15:27:34 +0200

    Fix None value in click.choice

--- parsec/core/cli/share_workspace.py
+++ parsec/core/cli/share_workspace.py
@@ -11,14 +11,18 @@ from parsec.core.types import WorkspaceRole
 
 
 class WorkspaceRoleChoice(click.Choice):
-    STR_TO_ROLE = {"NONE": None, **{role.value: role for role in WorkspaceRole}}
+    STR_TO_ROLE = {"NONE": "None", **{role.value: role for role in WorkspaceRole}}
 
     def __init__(self, **kwargs):
-        super().__init__(self.STR_TO_ROLE, **kwargs)
+        super().__init__(self.STR_TO_ROLE.keys(), **kwargs)
 
     def convert(self, value, param, ctx):
         ret = super().convert(value, param, ctx)
         return self.STR_TO_ROLE[ret.upper()]
+
+    @staticmethod
+    def fix_none_choice(choice):
+        return None if choice == "None" else choice
 
 
 async def _share_workspace(config, device, name, user_id, user_role):
@@ -30,10 +34,12 @@ async def _share_workspace(config, device, name, user_id, user_role):
 @core_config_and_device_options
 @click.argument("workspace_name")
 @click.argument("user_id", type=UserID, required=True)
-@click.option("--role", type=WorkspaceRoleChoice(case_sensitive=False))
+@click.option("--role", required=True, type=WorkspaceRoleChoice(case_sensitive=False))
 def share_workspace(config, device, workspace_name, user_id, role, **kwargs):
     """
     Create a new workspace for the given device.
     """
+    # Unfortunatly, Choice.conver is not invoked for values that are None (the missing value).
+    role = WorkspaceRoleChoice.fix_none_choice(role)
     with cli_exception_handler(config.debug):
         trio_run(_share_workspace, config, device, workspace_name, user_id, role)
