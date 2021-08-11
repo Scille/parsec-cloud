7c8734ed8ef9d28813e4964ddd1eb31af04392e2
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2021-08-11 15:51:23 +0200

    Improve doc in api.protocol.organization and correct APIV1_OrganizationCreateReqSchema.active_users_limit attributes

--- parsec/api/protocol/organization.py
+++ parsec/api/protocol/organization.py
@@ -7,8 +7,12 @@ from parsec.api.protocol.types import OrganizationIDField, DeviceIDField, UserPr
 class APIV1_OrganizationCreateReqSchema(BaseReqSchema):
     organization_id = OrganizationIDField(required=True)
     expiration_date = fields.DateTime(allow_none=True, required=False)
+    # /!\ Missing field and field set to `None` does not mean the same thing:
+    # - missing field: ask the backend to use it default value for this field
+    # - field set to `None`: `None` is a valid value to use for this field
     user_profile_outsider_allowed = fields.Boolean(required=False)
-    active_users_limit = fields.Integer(required=False)
+    # `None` stands for "no limit" here
+    active_users_limit = fields.Integer(allow_none=True, required=False)
 
 
 class APIV1_OrganizationCreateRepSchema(BaseRepSchema):
@@ -112,6 +116,7 @@ class APIV1_OrganizationStatusRepSchema(BaseRepSchema):
     is_bootstrapped = fields.Boolean(required=True)
     expiration_date = fields.DateTime(allow_none=True, required=False)
     user_profile_outsider_allowed = fields.Boolean(required=True)
+    # `None` stands for "no limit" here
     active_users_limit = fields.Integer(allow_none=True, required=True)
 
 
@@ -127,6 +132,7 @@ class OrganizationConfigReqSchema(BaseReqSchema):
 class OrganizationConfigRepSchema(BaseRepSchema):
     expiration_date = fields.DateTime(allow_none=True, required=False)
     user_profile_outsider_allowed = fields.Boolean(required=True)
+    # `None` stands for "no limit" here
     active_users_limit = fields.Integer(allow_none=True, required=True)
 
 
@@ -137,8 +143,12 @@ organization_config_serializer = CmdSerializer(
 
 class APIV1_OrganizationUpdateReqSchema(BaseReqSchema):
     organization_id = OrganizationIDField(required=True)
+    # /!\ Missing field and field set to `None` does not mean the same thing:
+    # - missing field: don't modify this field
+    # - field set to `None`: `None` is a valid value to use for this field
     expiration_date = fields.DateTime(allow_none=True, required=False)
     user_profile_outsider_allowed = fields.Boolean(required=False)
+    # `None` stands for "no limit" here
     active_users_limit = fields.Integer(allow_none=True, required=False)
 
 
