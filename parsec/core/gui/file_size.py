8c674840bf7a8eb901e87be425bf35f08c5d0acd
Vincent Michel <vxgmichel@gmail.com>
2021-08-16 15:34:41 +0200

    Improve file size formatting for values in the 1000-1024 range

--- parsec/core/gui/file_size.py
+++ parsec/core/gui/file_size.py
@@ -11,37 +11,51 @@ def size(bytes, system):
     - `0 <= bytes < 10`:          1 significant figures:     `X B`
     - `10 <= bytes < 100`:        2 significant figures:    `XY B`
     - `100 <= bytes < 1000`:      3 significant figures:   `XYZ B`
-    - `1000 <= bytes < 1024`:     4 significant figures:  `10XY B` TODO: Do better
+    - `1000 <= bytes < 1024`:     2 significant figures: `0.9X KB`
     - `1 <= kilobytes < 10`:      2 significant figures:  `X.Y KB`
     - `10 <= kilobytes < 100`:    3 significant figures: `XY.Z KB`
     - `100 <= kilobytes < 1000`:  3 significant figures:  `XYZ KB`
-    - `1000 <= kilobytes < 1024`: 4 significant figures: `10XY KB` TODO: Do better
+    - `1000 <= kilobytes < 1024`: 2 significant figures: `0.9X MB`
     - And so on for MB, GB and TB
     """
-    # Iterate over factors, expecting them to be in decreasing order
+    # Bytes should be zero or greater
+    assert bytes >= 0
+    # Iterate over factors, expecting them to be in increasing order
     for factor, suffix in system:
         # Stop when the right factor is reached
-        if bytes >= factor:
+        if bytes / factor < 999.5:
             break
     # Convert to the right unit
     amount = bytes / factor
-    # Format the amount and add the suffix
-    formatted_amount = f"{amount:.1f}" if amount < 99.95 and factor > 1 else f"{amount:.0f}"
+    # Truncate to two decimals in order to avoid misleading rounding to 1.00
+    amount = int(amount * 100) / 100
+    # Factor is one, the amount is in integer
+    if factor == 1:
+        formatted_amount = f"{bytes:d}"
+    # Amount is less than one, display either 0.97, 0.98 or 0.99
+    elif amount < 1.0:
+        formatted_amount = f"{amount:.2f}"
+    # Amount is displayed with a one or two digits on the left side, add an extra significant digit
+    elif amount < 99.95:
+        formatted_amount = f"{amount:.1f}"
+    # Amount is displayed with 3 digits on the left side, no need for an extra digit
+    else:
+        formatted_amount = f"{amount:.0f}"
     return f"{formatted_amount} {suffix}"
 
 
 def get_filesize(bytesize):
-    # Our system of unit in decreasing order
+    # Our system of unit in increasing order
     # We're using the 1K=1024 conversion in order to match the Windows file explorer.
     # TODO: adapt the conversion depending on the system:
     # - Windows: 1K=1024
     # - Linux: 1k=1000
     # - MacOS: 1k=1000
     SYSTEM = [
-        (1024 ** 4, _("TEXT_FILE_SIZE_TB")),
-        (1024 ** 3, _("TEXT_FILE_SIZE_GB")),
-        (1024 ** 2, _("TEXT_FILE_SIZE_MB")),
-        (1024 ** 1, _("TEXT_FILE_SIZE_KB")),
         (1024 ** 0, _("TEXT_FILE_SIZE_B")),
+        (1024 ** 1, _("TEXT_FILE_SIZE_KB")),
+        (1024 ** 2, _("TEXT_FILE_SIZE_MB")),
+        (1024 ** 3, _("TEXT_FILE_SIZE_GB")),
+        (1024 ** 4, _("TEXT_FILE_SIZE_TB")),
     ]
     return size(bytesize, system=SYSTEM)
