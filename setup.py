6b3cfa0e3982b4bb4bd4450a20ed71a20f8dc4dd
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2021-08-26 11:16:25 +0200

    Upgrade sentry-sdk dep

--- setup.py
+++ setup.py
@@ -287,7 +287,7 @@ requirements = [
     "async_generator>=1.9",
     'contextvars==2.1;python_version<"3.7"',
     'typing-extensions;python_version<"3.8"',
-    "sentry-sdk==0.14.3",
+    "sentry-sdk==1.3.1",
     "structlog==19.2.0",
     "importlib_resources==1.0.2",
     "colorama==0.4.0",  # structlog colored output
