8ef39c300ac4da581578b8a96fb6925129238858
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2021-08-27 11:04:36 +0200

    Fix SQL migration 8

--- parsec/backend/postgresql/migrations/0008_apvi1_removal.sql
+++ parsec/backend/postgresql/migrations/0008_apvi1_removal.sql
@@ -17,9 +17,9 @@ ALTER TABLE organization RENAME COLUMN expiration_date TO _expired_on;
 
 ALTER TABLE organization ADD _created_on TIMESTAMPTZ;
 UPDATE organization SET _created_on = (
-    CASE WHEN root_verify_key is NULL THEN
-        -- If bootsrapped, use creation date of the first user
-        (SELECT created_on FROM user_ WHERE user_.organization = organization._id AND user_.user_certifier IS NULL)
+    CASE WHEN root_verify_key IS NOT NULL THEN
+        -- If bootstrapped, use creation date of the first user
+        (SELECT user_.created_on FROM user_ WHERE user_.organization = organization._id AND user_.user_certifier IS NULL)
     ELSE
         -- Else fallback by using the expired date if any or the current date
         LEAST(organization._expired_on, NOW())
