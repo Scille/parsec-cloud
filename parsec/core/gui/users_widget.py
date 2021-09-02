1bbf0d7011afe0c1df7776e31639f20c5274d6a7
AntoineDupre <dupre.antoine.nicolas@gmail.com>
2021-09-02 15:43:34 +0200

    Filter invitation and revoked users

--- parsec/core/gui/users_widget.py
+++ parsec/core/gui/users_widget.py
@@ -173,14 +173,20 @@ async def _do_revoke_user(core, user_info):
         raise JobResultError("error") from exc
 
 
-async def _do_list_users_and_invitations(core, page, pattern=None):
+async def _do_list_users_and_invitations(
+    core, page, pattern=None, omit_revoked=False, omit_invitation=False
+):
     try:
         if pattern is None:
-            users, total = await core.find_humans(page=page, per_page=USERS_PER_PAGE)
-            invitations = await core.list_invitations()
+            users, total = await core.find_humans(
+                page=page, per_page=USERS_PER_PAGE, omit_revoked=omit_revoked
+            )
+            invitations = [] if omit_invitation else await core.list_invitations()
             return total, users, [inv for inv in invitations if inv["type"] == InvitationType.USER]
         else:
-            users, total = await core.find_humans(page=page, per_page=USERS_PER_PAGE, query=pattern)
+            users, total = await core.find_humans(
+                page=page, per_page=USERS_PER_PAGE, query=pattern, omit_revoked=omit_revoked
+            )
             return total, users, []
     except BackendNotAvailable as exc:
         raise JobResultError("offline") from exc
@@ -247,6 +253,8 @@ class UsersWidget(QWidget, Ui_UsersWidget):
         self.invite_user_error.connect(self._on_invite_user_error)
         self.cancel_invitation_success.connect(self._on_cancel_invitation_success)
         self.cancel_invitation_error.connect(self._on_cancel_invitation_error)
+        self.checkbox_filter_revoked.clicked.connect(self.reset)
+        self.checkbox_filter_invitation.clicked.connect(self.reset)
 
     def show(self):
         self._page = 1
@@ -531,4 +539,6 @@ class UsersWidget(QWidget, Ui_UsersWidget):
             _do_list_users_and_invitations,
             core=self.core,
             page=self._page,
+            omit_revoked=self.checkbox_filter_revoked.isChecked(),
+            omit_invitation=self.checkbox_filter_invitation.isChecked(),
         )
