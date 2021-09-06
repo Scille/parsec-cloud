a74e0161f0b34e5f65fa1410f6102149ee1e5311
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2021-09-06 18:48:38 +0200

    Correct run_testenv.fish

--- tests/scripts/run_testenv.fish
+++ tests/scripts/run_testenv.fish
@@ -1,9 +1,4 @@
 #!/usr/bin/env fish
-: '
-Create a temporary environment and initialize a test setup for parsec.
-
-Run `tests/scripts/run_testenv.fish --help` for more information.
-'
 
 # Make sure this script is sourced
 set SOURCE_COMMANDS source .
@@ -16,9 +11,11 @@ end
 # set SCRIPT_DIR (dirname (realpath -s $argv[1]))
 set SCRIPT_DIR (dirname (realpath (status -f)))
 
+# In Python we trust (aka shell's tempfile&mktemp doesn't work on all platforms)
+set SOURCE_FILE (python -c "import tempfile; print(tempfile.mkstemp()[1])")
+
 # Run python script and source
-set SOURCE_FILE (tempfile)
-$SCRIPT_DIR/run_testenv.py --source-file $SOURCE_FILE $argv || exit $status
+eval $SCRIPT_DIR/run_testenv.py --source-file $SOURCE_FILE $argv; or exit $status
 source $SOURCE_FILE
 
 # Clean up
