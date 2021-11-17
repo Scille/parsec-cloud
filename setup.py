4c2fc97fc6849fa5663df8f9eec0f956cd7bfa17
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2021-11-17 12:05:26 +0100

    Add pyproject.toml to replace setup_requires in setup.py

--- setup.py
+++ setup.py
@@ -319,15 +319,13 @@ test_requirements = [
 ]
 
 
-PYQT_DEPS = ["PyQt5==5.15.2", "pyqt5-sip==12.8.1", "qtrio==0.5.0"]
-GUI_DEPS = [*PYQT_DEPS, "qrcode==6.1"]
-BABEL_DEP = "Babel==2.6.0"
-WHEEL_DEP = "wheel==0.34.2"
-DOCUTILS_DEP = "docutils==0.15"
 extra_requirements = {
     "core": [
-        *GUI_DEPS,
-        BABEL_DEP,
+        "PyQt5==5.15.2",
+        "pyqt5-sip==12.8.1",
+        "qtrio==0.5.0",
+        "qrcode==6.1",
+        "Babel==2.6.0",
         'fusepy==3.0.1;platform_system=="Linux" or platform_system=="Darwin"',
         'winfspy==0.8.2;platform_system=="Windows"',
         "zxcvbn==4.4.27",
@@ -361,7 +359,6 @@ setup(
     python_requires="~=3.7",
     packages=find_packages(include=["parsec", "parsec.*"]),
     package_dir={"parsec": "parsec"},
-    setup_requires=[WHEEL_DEP, *GUI_DEPS, BABEL_DEP, DOCUTILS_DEP],  # To generate resources bundle
     install_requires=requirements,
     extras_require=extra_requirements,
     cmdclass={
