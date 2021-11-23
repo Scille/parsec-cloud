99ae181b5253f256b32cda6f53419a4f997cf2e8
Vincent Michel <vxgmichel@gmail.com>
2021-11-23 18:36:21 +0100

    Implement support for comparison and bytearrays in HashDigest rust implementation

--- oxidation/libparsec_python/src/crypto.rs
+++ oxidation/libparsec_python/src/crypto.rs
@@ -1,5 +1,7 @@
 // Parsec Cloud (https://parsec.cloud) Copyright (c) BSLv1.1 (eventually AGPLv3) 2016-2021 Scille SAS
 
+use pyo3::basic::CompareOp;
+use pyo3::types::{PyBool, PyByteArray};
 use pyo3::{exceptions::PyValueError, prelude::*, types::PyBytes};
 
 #[pyclass(name = "_Rs_HashDigest")]
@@ -16,8 +18,16 @@ impl HashDigest {
     }
 
     #[staticmethod]
-    fn from_data(data: &[u8]) -> PyResult<HashDigest> {
-        Ok(Self(parsec_api_crypto::HashDigest::from_data(data)))
+    fn from_data(py: Python, data: PyObject) -> PyResult<HashDigest> {
+        let copy;
+        let bytes = match data.extract::<&PyByteArray>(py) {
+            Ok(x) => {
+                copy = x.to_vec();
+                &copy
+            }
+            Err(_) => data.extract::<&PyBytes>(py)?.as_bytes(),
+        };
+        Ok(Self(parsec_api_crypto::HashDigest::from_data(bytes)))
     }
 
     #[getter]
@@ -31,5 +41,13 @@ impl HashDigest {
 
     fn __repr__(&self) -> PyResult<String> {
         Ok(format!("HashDigest({})", self.0.hexdigest()))
+    }
+
+    fn __richcmp__(&self, py: Python, value: &HashDigest, op: CompareOp) -> PyResult<PyObject> {
+        match op {
+            CompareOp::Eq => Ok(PyBool::new(py, self.0 == value.0).into_py(py)),
+            CompareOp::Ne => Ok(PyBool::new(py, self.0 != value.0).into_py(py)),
+            _ => Ok(py.NotImplemented()),
+        }
     }
 }
