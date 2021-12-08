943c8c1d29f110001b40f4e8821445fd11110983
Antoine Delattre <antoine.delattre@epita.fr>
2021-12-08 17:13:06 +0100

    Changed default value to the helper and changed click option --query to argument

--- parsec/core/cli/human_find.py
+++ parsec/core/cli/human_find.py
@@ -8,7 +8,15 @@ from parsec.core.cli.utils import cli_command_base_options, core_config_and_devi
 from parsec.cli_utils import cli_exception_handler
 
 
-async def _human_find(config, device, query, page, per_page, omit_revoked, omit_non_human):
+async def _human_find(
+    config,
+    device,
+    query,
+    page: int = 1,
+    per_page: int = 100,
+    omit_revoked: bool = False,
+    omit_non_human: bool = False,
+):
     async with logged_core_factory(config, device) as core:
         user_info_tab, nb = await core.find_humans(
             query, page, per_page, omit_revoked, omit_non_human
@@ -20,18 +28,9 @@ async def _human_find(config, device, query, page, per_page, omit_revoked, omit_
 
 
 @click.command(short_help="search user id from email address")
-@click.option("--query", required=True, type=str, help="Search through names and email addresses")
+@click.argument("query", type=str)
 @core_config_and_device_options
 @cli_command_base_options
-def human_find(
-    config,
-    device,
-    query: str = None,
-    page: int = 1,
-    per_page: int = 100,
-    omit_revoked: bool = False,
-    omit_non_human: bool = False,
-    **kwargs,
-) -> dict:
+def human_find(config, device, query, **kwargs) -> dict:
     with cli_exception_handler(config.debug):
-        trio_run(_human_find, config, device, query, page, per_page, omit_revoked, omit_non_human)
+        trio_run(_human_find, config, device, query)
