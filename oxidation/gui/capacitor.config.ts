399c87aa1d80f96ed6b195cb7c391739344286c7
NicoTuxx <nicolasstempert@gmail.com>
2022-03-10 13:08:37 +0100

    fix: commit ignored .ts

---
+++ oxidation/gui/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'gui',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
