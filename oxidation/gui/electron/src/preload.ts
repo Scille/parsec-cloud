242b8a691309402bec3bf7a7da0ee25ec92ae089
NicoTuxx <nicolasstempert@gmail.com>
2022-03-14 15:30:47 +0100

    fix: remove old angular ionic version

--- oxidation/gui/electron/src/preload.ts
+++
@@ -1,7 +0,0 @@
-require('./rt/electron-rt');
-const { contextBridge } = require('electron');
-const libparsec = require('../../libparsec');
-
-contextBridge.exposeInMainWorld('libparsec', {
-  submitJob: libparsec.submitJob
-});
