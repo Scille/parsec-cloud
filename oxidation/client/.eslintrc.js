3d547de6fc8a9c116f7cac6c2c5a65100e39606a
NicoTuxx <nicolasstempert@gmail.com>
2022-03-21 15:13:26 +0100

    fix: cypress ESLint

--- oxidation/client/.eslintrc.js
+++ oxidation/client/.eslintrc.js
@@ -13,6 +13,9 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2020
   },
+  plugins: [
+      'cypress'
+  ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
