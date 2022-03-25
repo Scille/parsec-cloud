3bbd68fbd334cd03479c06fd95435d80ad40e144
AntoineDupre <dupre.antoine.nicolas@gmail.com>
2022-03-25 15:36:00 +0100

    Add pki certificate request and reply structure

--- parsec/api/data/pki.py
+++ parsec/api/data/pki.py
@@ -1,9 +1,8 @@
-from typing import Optional
+# Parsec Cloud (https://parsec.cloud) Copyright (c) AGPLv3 2016-2021 Scille SAS
 
 import attr
 
 from parsec.api.data.base import BaseAPIData
-from parsec.crypto import VerifyKey, PublicKey
 from parsec.api.protocol import (
     DeviceID,
     DeviceIDField,
@@ -12,23 +11,26 @@ from parsec.api.protocol import (
     UserProfile,
     UserProfileField,
 )
-from parsec.serde import fields, BaseSchema
+from parsec.crypto import PublicKey, VerifyKey
+from parsec.serde import BaseSchema, fields
 
 
 @attr.s(slots=True, frozen=True, auto_attribs=True, kw_only=True, eq=False)
 class PkiRequest(BaseAPIData):
     class SCHEMA_CLS(BaseSchema):
         der_x509_certificate = fields.Bytes(require=True)
-        signed_verify_key = fields.VerifyKey(required=True)
-        signed_public_key = fields.PublicKey(required=True)
+        verify_key = fields.VerifyKey(required=True)
+        public_key = fields.PublicKey(required=True)
+        signature = fields.Bytes(required=True)
         requested_human_handle = HumanHandleField(required=True)
         requested_device_name = fields.String(required=True)
 
     der_x509_certificate: bytes
-    signed_verify_key: VerifyKey
-    signed_public_key: PublicKey
-    requested_human_handle: Optional[HumanHandle]
-    requested_device_name: Optional[str]
+    verify_key: VerifyKey
+    public_key: PublicKey
+    signature: bytes
+    requested_human_handle: HumanHandle
+    requested_device_name: str
 
 
 @attr.s(slots=True, frozen=True, auto_attribs=True, kw_only=True, eq=False)
@@ -47,6 +49,3 @@ class PkiReply(BaseAPIData):
     device_label: str
     human_handle: HumanHandle
     profile: UserProfile
-
-
-PkiRequestField = fields.bytes_based_field_factory(PkiRequest)
