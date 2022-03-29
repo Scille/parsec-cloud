93f7fd64e52d47b6368d4b54ba316334efc0c1de
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2022-03-29 19:16:19 +0200

    Update renamed pki cmds in parsec/core/backend_connection/authenticated.py

--- parsec/core/backend_connection/authenticated.py
+++ parsec/core/backend_connection/authenticated.py
@@ -93,8 +93,9 @@ class BackendAuthenticatedCmds:
     )
     organization_stats = expose_cmds_with_retrier(cmds.organization_stats)
     organization_config = expose_cmds_with_retrier(cmds.organization_config)
-    pki_enrollment_get_requests = expose_cmds_with_retrier(cmds.pki_enrollment_get_requests)
-    pki_enrollment_reply = expose_cmds_with_retrier(cmds.pki_enrollment_reply)
+    pki_enrollment_list = (expose_cmds_with_retrier(cmds.pki_enrollment_list),)
+    pki_enrollment_reject = (expose_cmds_with_retrier(cmds.pki_enrollment_reject),)
+    pki_enrollment_accept = (expose_cmds_with_retrier(cmds.pki_enrollment_accept),)
 
 
 for cmd in AUTHENTICATED_CMDS:
