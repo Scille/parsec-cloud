ce9b789420f93372e7e7fbe7b7cc58566703c1e1
Vincent Michel <vxgmichel@gmail.com>
2022-04-21 22:04:42 +0200

    Add compatibility parameters to test_bootstrap_organization

--- tests/core/test_bootstrap_organization.py
+++ tests/core/test_bootstrap_organization.py
@@ -1,7 +1,9 @@
 # Parsec Cloud (https://parsec.cloud) Copyright (c) AGPLv3 2016-2021 Scille SAS
 
 import pytest
+from unittest.mock import AsyncMock
 
+from parsec.backend.http import HTTPResponse
 from parsec.api.data import UserProfile, EntryName
 from parsec.api.protocol import OrganizationID, DeviceLabel, HumanHandle
 from parsec.core.types import BackendOrganizationBootstrapAddr
@@ -11,7 +13,16 @@ from parsec.core.fs.storage.user_storage import user_storage_non_speculative_ini
 
 @pytest.mark.trio
 @pytest.mark.parametrize("with_labels", [False, True])
-async def test_good(running_backend, backend, user_fs_factory, with_labels, data_base_dir):
+@pytest.mark.parametrize("backend_version", ["2.5", "2.6", "latest"])
+async def test_good(
+    running_backend,
+    backend,
+    user_fs_factory,
+    with_labels,
+    data_base_dir,
+    backend_version,
+    monkeypatch,
+):
     org_id = OrganizationID("NewOrg")
     org_token = "123456"
     await backend.organization.create(org_id, org_token)
@@ -27,9 +38,25 @@ async def test_good(running_backend, backend, user_fs_factory, with_labels, data
         human_handle = None
         device_label = None
 
+    if backend_version == "2.5":
+        _handle_request = AsyncMock(return_value=HTTPResponse.build_msgpack(404, {}))
+        monkeypatch.setattr("parsec.backend.http.HTTPComponent.handle_request", _handle_request)
+
+    if backend_version == "2.6":
+        _handle_request = AsyncMock(
+            return_value=HTTPResponse.build_msgpack(200, {"status": "unknown_command"})
+        )
+        monkeypatch.setattr("parsec.backend.http.HTTPComponent.handle_request", _handle_request)
+
+    if backend_version == "latest":
+        _handle_request = None
+
     new_device = await bootstrap_organization(
         organization_addr, human_handle=human_handle, device_label=device_label
     )
+
+    if _handle_request is not None:
+        _handle_request.assert_awaited_once()
 
     assert new_device is not None
     assert new_device.organization_id == org_id
