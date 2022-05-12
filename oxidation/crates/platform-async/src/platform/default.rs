4851ec2a36fd12a130dbf79cc8812a6198fa0302
firelight.flagboy <firelight.flagboy@gmail.com>
2022-05-12 11:34:34 +0200

    [wip] basic spawn body function

--- oxidation/crates/platform-async/src/platform/default.rs
+++ oxidation/crates/platform-async/src/platform/default.rs
@@ -1,1 +1,54 @@
-pub fn spawn<F>(
+use std::{
+    future::Future,
+    sync::{Arc, Mutex},
+};
+
+pub fn spawn<F>(future: F) -> Task<F::Output>
+where
+    F: Future + Send + 'static,
+    F::Output: Send + 'static,
+{
+    let shared_state = SharedState::default();
+    let shared_state = Arc::new(Mutex::new(shared_state));
+
+    let task = Task::new(shared_state.clone());
+    let runnable = Runnable::new(future, shared_state);
+
+    let _handle = tokio::spawn(runnable);
+
+    task
+}
+
+struct SharedState<T> {
+    value: Option<T>,
+}
+
+impl<T> Default for SharedState<T> {
+    fn default() -> Self {
+        Self { value: None }
+    }
+}
+
+pub struct Task<T> {
+    shared_state: Arc<Mutex<SharedState<T>>>,
+}
+
+impl<T> Task<T> {
+    fn new(shared_state: Arc<Mutex<SharedState<T>>>) -> Self {
+        Self { shared_state }
+    }
+}
+
+pub struct Runnable<F, T> {
+    future: F,
+    shared_state: Arc<Mutex<SharedState<T>>>,
+}
+
+impl<F, T> Runnable<F, T> {
+    fn new(future: F, shared_state: Arc<Mutex<SharedState<T>>>) -> Self {
+        Self {
+            future,
+            shared_state,
+        }
+    }
+}
