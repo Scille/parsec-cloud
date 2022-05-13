c83cd7d09d5e8f287cf2001a893d993ab7f5402f
firelight.flagboy <firelight.flagboy@gmail.com>
2022-05-13 15:51:12 +0200

    rename file `default` to `native`

--- oxidation/libparsec/crates/platform-async/src/platform/mod.rs
+++ oxidation/libparsec/crates/platform-async/src/platform/mod.rs
@@ -1,8 +1,8 @@
 // Parsec Cloud (https://parsec.cloud) Copyright (c) BSLv1.1 (eventually AGPLv3) 2016-2021 Scille SAS
 
 #[cfg(not(target_arch = "wasm32"))]
-mod default;
+mod native;
 #[cfg(not(target_arch = "wasm32"))]
-use default as platform;
+use native as platform;
 
 pub use platform::{spawn, Task};
