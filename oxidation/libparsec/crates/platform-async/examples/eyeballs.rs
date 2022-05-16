e94b4b1dae0f39937030c66a4513e38400a28fa2
firelight.flagboy <firelight.flagboy@gmail.com>
2022-05-16 11:11:52 +0200

    use internal notify

--- oxidation/libparsec/crates/platform-async/examples/eyeballs.rs
+++ oxidation/libparsec/crates/platform-async/examples/eyeballs.rs
@@ -1,12 +1,12 @@
 // Parsec Cloud (https://parsec.cloud) Copyright (c) BSLv1.1 (eventually AGPLv3) 2016-2021 Scille SAS
 #![cfg(not(target_arch = "wasm32"))]
 
-use libparsec_platform_async::JoinSet;
+use libparsec_platform_async::{JoinSet, Notify};
 use std::{
     sync::{Arc, Mutex},
     time::{Duration, Instant},
 };
-use tokio::sync::{mpsc, Notify};
+use tokio::sync::mpsc;
 
 const TIMEOUT: u64 = 250;
 
