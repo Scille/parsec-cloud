c6647aa488de8c5ee866b8c9b8facf276d4bdfef
firelight.flagboy <firelight.flagboy@gmail.com>
2022-05-16 15:48:21 +0200

    force create to not be compile on target_arch wasm32

--- oxidation/libparsec_python/src/lib.rs
+++ oxidation/libparsec_python/src/lib.rs
@@ -1,5 +1,7 @@
 // Parsec Cloud (https://parsec.cloud) Copyright (c) BSLv1.1 (eventually AGPLv3) 2016-2021 Scille SAS
 
+#![cfg(not(target_arch = "wasm32"))]
+
 use pyo3::prelude::{pymodule, wrap_pyfunction, PyModule, PyResult, Python};
 
 mod addrs;
