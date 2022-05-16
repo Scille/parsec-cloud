83677d2eb063c38b19315a21935050726000ce9b
firelight.flagboy <firelight.flagboy@gmail.com>
2022-05-16 10:22:54 +0200

    add Timer impl

--- oxidation/libparsec/crates/platform-async/src/lib.rs
+++ oxidation/libparsec/crates/platform-async/src/lib.rs
@@ -1,8 +1,9 @@
 // Parsec Cloud (https://parsec.cloud) Copyright (c) BSLv1.1 (eventually AGPLv3) 2016-2021 Scille SAS
 
 #[cfg(not(target_arch = "wasm32"))]
-mod native;
+pub mod native;
 #[cfg(not(target_arch = "wasm32"))]
-use native as platform;
+pub use native as platform;
 
+pub use platform::Timer;
 pub use platform::{spawn, JoinSet, Task};
--- oxidation/libparsec/crates/platform-async/src/native/mod.rs
+++ oxidation/libparsec/crates/platform-async/src/native/mod.rs
@@ -1,5 +1,7 @@
 pub mod join_set;
 pub mod task;
+pub mod timer;
 
 pub use join_set::JoinSet;
 pub use task::{spawn, Task};
+pub use timer::Timer;
