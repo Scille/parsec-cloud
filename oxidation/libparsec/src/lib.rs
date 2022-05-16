b06f8a3fa565cb06e8224237ff0451721db968c7
firelight.flagboy <firelight.flagboy@gmail.com>
2022-05-16 15:06:53 +0200

    configure libparsec using target_arch instead of using features native / web

--- oxidation/libparsec/src/lib.rs
+++ oxidation/libparsec/src/lib.rs
@@ -1,15 +1,9 @@
 // Parsec Cloud (https://parsec.cloud) Copyright (c) BSLv1.1 (eventually AGPLv3) 2016-2021 Scille SAS
 
-#[cfg(not(any(feature = "native", feature = "web")))]
-compile_error!("Feature \"native\" or \"web\" must be enabled for this crate.");
-
-#[cfg(all(feature = "native", feature = "web"))]
-compile_error!("Features \"native\" and \"web\" cannot be enabled at the same time.");
-
-#[cfg(feature = "native")]
+#[cfg(not(target_arch = "wasm32"))]
 pub use libparsec_platform_native::{create_context, RuntimeContext};
 
-#[cfg(feature = "web")]
+#[cfg(target_arch = "wasm32")]
 pub use libparsec_platform_web::{create_context, RuntimeContext};
 
 pub use parsec_api_crypto::*;
