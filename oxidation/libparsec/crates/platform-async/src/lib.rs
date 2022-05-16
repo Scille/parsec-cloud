0918e971b352fd21954441fc5c7149ac1dcd38b1
firelight.flagboy <firelight.flagboy@gmail.com>
2022-05-16 09:17:46 +0200

    flatten platform module

--- oxidation/libparsec/crates/platform-async/src/lib.rs
+++ oxidation/libparsec/crates/platform-async/src/lib.rs
@@ -1,3 +1,8 @@
 // Parsec Cloud (https://parsec.cloud) Copyright (c) BSLv1.1 (eventually AGPLv3) 2016-2021 Scille SAS
 
-pub mod platform;
+#[cfg(not(target_arch = "wasm32"))]
+mod native;
+#[cfg(not(target_arch = "wasm32"))]
+use native as platform;
+
+pub use platform::{spawn, JoinSet, Task};
