1a18f057836c73860778da014bba7d0c99c5d13e
firelight.flagboy <firelight.flagboy@gmail.com>
2022-05-18 08:20:52 +0200

    add test to notify + remove notify_waiters

--- oxidation/libparsec/crates/platform-async/src/native/sync/notify.rs
+++ oxidation/libparsec/crates/platform-async/src/native/sync/notify.rs
@@ -11,27 +11,6 @@ use std::{
 /// `Notify` provides a basic mechanism to notify a single task of an event.
 /// `Notify` itself does not carry any data.
 /// Instead, it is to be used to signal another task to perform an operation.
-///
-/// ```
-/// use libparsec_platform_async::{Notify, spawn};
-/// use std::sync::Arc;
-///
-/// # #[tokio::main]
-/// # async fn main() {
-/// let notify = Arc::new(Notify::default());
-/// let notify2 = notify.clone();
-///
-/// let handle = spawn(async move {
-///     notify2.notified().await;
-///     println!("receive notification");
-/// });
-///
-/// println!("sending notification");
-/// notify.notify_one();
-///
-/// handle.await;
-/// # }
-/// ```
 pub struct Notify(tokio::sync::Notify);
 
 impl Notify {
@@ -41,13 +20,29 @@ impl Notify {
     }
 
     /// Notifies a waiting task.
+    ///
+    /// ```
+    /// use libparsec_platform_async::{Notify, spawn};
+    /// use std::sync::{Arc, atomic::{Ordering, AtomicBool}};
+    ///
+    /// # #[tokio::main]
+    /// # async fn main() {
+    /// let notify = Arc::new(Notify::default());
+    /// let notify2 = notify.clone();
+    ///
+    /// let handle = spawn(async move {
+    ///     notify2.notified().await;
+    ///     42
+    /// });
+    ///
+    /// println!("sending notification");
+    /// notify.notify_one();
+    ///
+    /// assert_eq!(handle.await, 42);
+    /// # }
+    /// ```
     pub fn notify_one(&self) {
         self.0.notify_one()
-    }
-
-    /// Notifies all waiting tasks.
-    pub fn notify_waiters(&self) {
-        self.0.notify_waiters()
     }
 }
 
@@ -66,4 +61,32 @@ impl<'a> Future for Notified<'a> {
     fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
         self.0.as_mut().poll(cx)
     }
+}
+
+#[tokio::test]
+async fn notify_before() {
+    let notify = Notify::default();
+
+    notify.notify_one();
+    notify.notified().await;
+}
+
+#[tokio::test]
+async fn notify_after() {
+    use crate::spawn;
+    use std::sync::Arc;
+    use tokio::task::yield_now;
+
+    let notify = Arc::new(Notify::default());
+    let notify2 = notify.clone();
+
+    let task = spawn(async move {
+        notify2.notified().await;
+        42
+    });
+
+    yield_now().await;
+    notify.notify_one();
+    let result = task.await;
+    assert_eq!(result, 42);
 }
