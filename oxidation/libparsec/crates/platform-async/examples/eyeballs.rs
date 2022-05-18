700030acd0ea111ff8d4b0baf91ee91b2bd55209
firelight.flagboy <firelight.flagboy@gmail.com>
2022-05-18 10:15:46 +0200

    remove `TaskTrait`

--- oxidation/libparsec/crates/platform-async/examples/eyeballs.rs
+++ oxidation/libparsec/crates/platform-async/examples/eyeballs.rs
@@ -1,10 +1,7 @@
 // Parsec Cloud (https://parsec.cloud) Copyright (c) BSLv1.1 (eventually AGPLv3) 2016-2021 Scille SAS
 #![cfg(not(target_arch = "wasm32"))]
 
-use libparsec_platform_async::{
-    platform::{spawn, JoinSet, Task},
-    task::TaskTrait,
-};
+use libparsec_platform_async::platform::JoinSet;
 use std::{
     sync::{Arc, Mutex},
     time::{Duration, Instant},
