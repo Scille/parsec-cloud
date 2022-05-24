862402e03cb142b731411a6a289caeeeb53c9a6d
Pascal <pascal.chen@devling.xyz>
2022-05-24 12:10:05 +0200

    Use real_clock_timeout instead of trio.fails_after

--- tests/core/test_invite.py
+++ tests/core/test_invite.py
@@ -31,7 +31,7 @@ from parsec.core.invite import (
 from parsec.backend.backend_events import BackendEvent
 from parsec.core.backend_connection.exceptions import BackendInvitationAlreadyUsed
 from parsec.core.fs.storage.user_storage import user_storage_non_speculative_init
-from tests.common import customize_fixtures
+from tests.common import customize_fixtures, real_clock_timeout
 
 from tests.common import real_clock_timeout
 
