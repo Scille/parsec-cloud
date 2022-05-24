f31e53d99411d15d74037cf3f54baaa5377ef610
Pascal <pascal.chen@devling.xyz>
2022-05-24 11:36:09 +0200

    Add comment to explain why dead code

--- oxidation/crates/parsec_core_fs/src/lib.rs
+++ oxidation/crates/parsec_core_fs/src/lib.rs
@@ -1,7 +1,6 @@
 // Parsec Cloud (https://parsec.cloud) Copyright (c) BSLv1.1 (eventually AGPLv3) 2016-2021 Scille SAS
 
 mod error;
-#[allow(dead_code)]
 mod extensions;
 mod schema;
 mod storage;
--- oxidation/crates/parsec_core_fs/src/workspacefs/mod.rs
+++ oxidation/crates/parsec_core_fs/src/workspacefs/mod.rs
@@ -1,4 +1,5 @@
 // Parsec Cloud (https://parsec.cloud) Copyright (c) BSLv1.1 (eventually AGPLv3) 2016-2021 Scille SAS
 
+// Not used for the moment
 #[allow(dead_code)]
 mod file_transactions;
