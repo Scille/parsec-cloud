fc357c5276f533f05e65a9aeb2210acccefdf29e
firelight flagboy <firelight.flagboy@gmail.com>
2022-06-07 16:18:55 +0200

    unwanted comment on pretty assert_eq

--- oxidation/crates/parsec_api_crypto/tests/sign.rs
+++ oxidation/crates/parsec_api_crypto/tests/sign.rs
@@ -1,7 +1,7 @@
 // Parsec Cloud (https://parsec.cloud) Copyright (c) BSLv1.1 (eventually AGPLv3) 2016-2021 Scille SAS
 
 use hex_literal::hex;
-// use pretty_assertions::assert_eq;
+use pretty_assertions::assert_eq;
 use serde_test::{assert_tokens, Token};
 use std::convert::TryFrom;
 
