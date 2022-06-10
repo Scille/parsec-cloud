eb1659dbb8f13ff81c79430f7ccf759e38233ee6
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2022-06-10 21:52:27 +0200

    Remove unused check_anonymous_api_allowed and tests/backend/test_anonymous.py

--- parsec/backend/utils.py
+++ parsec/backend/utils.py
@@ -64,14 +64,6 @@ def collect_apis(*components) -> Dict[ClientType, Dict[str, Callable]]:
     return apis
 
 
-def check_anonymous_api_allowed(fn):
-    if not getattr(fn, "_anonymous_api_allowed", False):
-        raise RuntimeError(
-            f"Trying to add {fn!r} to the anonymous api "
-            "(need to use @anonymous_api decorator for this)."
-        )
-
-
 def catch_protocol_errors(fn):
     @wraps(fn)
     async def wrapper(*args, **kwargs):
