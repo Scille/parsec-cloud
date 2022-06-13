93ba54bf0898dd129df9462c3bd92ffb4af52291
firelight flagboy <firelight.flagboy@gmail.com>
2022-06-13 10:28:44 +0200

    create handle for client & server

--- oxidation/libparsec/crates/client-connection/tests/auth_cmds.rs
+++ oxidation/libparsec/crates/client-connection/tests/auth_cmds.rs
@@ -4,6 +4,8 @@ use std::net::{IpAddr, Ipv4Addr, SocketAddr, SocketAddrV4};
 
 use libparsec_client_connection::AuthenticatedCmds;
 use parsec_api_crypto::SigningKey;
+use parsec_api_protocol::authenticated_cmds;
+use tokio::sync::oneshot::{channel, Receiver, Sender};
 use utils::server::MakeSignatureVerifier;
 
 use hyper::{server::Server, service::Service};
@@ -17,14 +19,21 @@ async fn verify_signed_request() {
 
     let kp = SigningKey::generate();
     let url = format!("http://{}:{}", IP, PORT);
-    let client = generate_client(kp.clone(), USER_ID.as_bytes(), &url);
+    let auth_cmds = generate_client(kp.clone(), USER_ID.as_bytes(), &url);
 
     let mut signature_verifier = MakeSignatureVerifier::default();
     signature_verifier.register_public_key(USER_ID.to_string(), kp.verify_key());
-    let server = Server::bind(&socket_addr).serve(signature_verifier);
+    let (ready_send, ready_recv) = channel();
+    let (stop_send, stop_recv) = channel();
 
-    let ping_handle =
-        tokio::task::spawn_local(client.ping("hello from the client side!".to_string()));
+    let server = server(socket_addr, signature_verifier, ready_send, stop_recv);
+    let server_handle = tokio::task::spawn_local(server);
+
+    ready_recv
+        .await
+        .expect("failed to recv ready signal from server");
+
+    let client_handle = tokio::task::spawn_local(client(auth_cmds, stop_send));
 }
 
 fn generate_client(signing_key: SigningKey, user_id: &[u8], root_url: &str) -> AuthenticatedCmds {
@@ -33,4 +42,34 @@ fn generate_client(signing_key: SigningKey, user_id: &[u8], root_url: &str) -> A
         .expect("cannot build client");
     AuthenticatedCmds::new(client, root_url, user_id, signing_key)
         .expect("failed to build auth cmds client")
+}
+
+async fn server(
+    socket_addr: SocketAddr,
+    signature_verifier: MakeSignatureVerifier,
+    notify_ready: Sender<()>,
+    notify_stop: Receiver<()>,
+) -> anyhow::Result<()> {
+    let server = Server::bind(&socket_addr).serve(signature_verifier);
+    let graceful = server.with_graceful_shutdown(async {
+        notify_stop.await.expect("notify stop receiver failed");
+    });
+
+    notify_ready.send(()).expect("failed to send server ready");
+
+    Ok(graceful.await?)
+}
+
+async fn client(
+    client: AuthenticatedCmds,
+    notify_stop: Sender<()>,
+) -> anyhow::Result<authenticated_cmds::ping::Rep> {
+    let rep = client
+        .ping("hello from the client side!".to_string())
+        .await
+        .map_err(anyhow::Error::from);
+    notify_stop
+        .send(())
+        .expect("cannot send stop signal to the server");
+    rep
 }
