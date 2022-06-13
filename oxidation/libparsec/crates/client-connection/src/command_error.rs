4c43049e19aa0ea6515749d6f42f8ff38265afb3
firelight flagboy <firelight.flagboy@gmail.com>
2022-06-13 17:47:09 +0200

    add header

--- oxidation/libparsec/crates/client-connection/src/command_error.rs
+++ oxidation/libparsec/crates/client-connection/src/command_error.rs
@@ -1,3 +1,5 @@
+// Parsec Cloud (https://parsec.cloud) Copyright (c) BSLv1.1 (eventually AGPLv3) 2016-2021 Scille SAS
+
 use std::{error::Error, fmt::Display};
 
 pub type Result<T> = core::result::Result<T, CommandError>;
