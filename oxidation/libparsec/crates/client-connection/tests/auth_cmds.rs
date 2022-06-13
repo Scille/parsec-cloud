48471d61a6bb228ea493d5cd1062d1b6259371a8
firelight flagboy <firelight.flagboy@gmail.com>
2022-06-13 11:25:10 +0200

    basic result assert

--- oxidation/libparsec/crates/client-connection/tests/auth_cmds.rs
+++ oxidation/libparsec/crates/client-connection/tests/auth_cmds.rs
@@ -1,6 +1,6 @@
 mod utils;
 
-use std::net::{IpAddr, Ipv4Addr, SocketAddr, SocketAddrV4};
+use std::net::{Ipv4Addr, SocketAddr, SocketAddrV4};
 
 use libparsec_client_connection::AuthenticatedCmds;
 use parsec_api_crypto::SigningKey;
@@ -8,10 +8,12 @@ use parsec_api_protocol::authenticated_cmds;
 use tokio::sync::oneshot::{channel, Receiver, Sender};
 use utils::server::MakeSignatureVerifier;
 
-use hyper::{server::Server, service::Service};
+use hyper::server::Server;
 
 #[tokio::test]
 async fn verify_signed_request() {
+    simple_logger::init_with_level(log::Level::Debug).expect("cannot initialize simple logger");
+
     const USER_ID: &str = "foobar";
     const IP: Ipv4Addr = Ipv4Addr::new(127, 0, 0, 1);
     const PORT: u16 = 14689;
@@ -27,13 +29,27 @@ async fn verify_signed_request() {
     let (stop_send, stop_recv) = channel();
 
     let server = server(socket_addr, signature_verifier, ready_send, stop_recv);
-    let server_handle = tokio::task::spawn_local(server);
+    let server_handle = tokio::task::spawn(server);
 
     ready_recv
         .await
         .expect("failed to recv ready signal from server");
+    log::debug!("[test] server is ready, we can continue");
 
-    let client_handle = tokio::task::spawn_local(client(auth_cmds, stop_send));
+    let client_handle = tokio::task::spawn(client(auth_cmds, stop_send));
+
+    let client_response = client_handle.await.expect("client handle failed");
+    let server_response = server_handle.await.expect("server handle failed");
+
+    log::debug!("[test] server response: {server_response:?}");
+    assert!(
+        server_response.is_ok(),
+        "server failed with error {}",
+        server_response.unwrap_err()
+    );
+
+    log::debug!("[test] client response: {client_response:?}");
+    assert!(client_response.is_ok());
 }
 
 fn generate_client(signing_key: SigningKey, user_id: &[u8], root_url: &str) -> AuthenticatedCmds {
@@ -53,11 +69,13 @@ async fn server(
     let server = Server::bind(&socket_addr).serve(signature_verifier);
     let graceful = server.with_graceful_shutdown(async {
         notify_stop.await.expect("notify stop receiver failed");
+        log::debug!("[server] recv signal to stop");
     });
 
     notify_ready.send(()).expect("failed to send server ready");
+    log::debug!("[server] signal we're ready");
 
-    Ok(graceful.await?)
+    Ok(dbg!(graceful.await)?)
 }
 
 async fn client(
@@ -68,6 +86,8 @@ async fn client(
         .ping("hello from the client side!".to_string())
         .await
         .map_err(anyhow::Error::from);
+    log::info!("[client] recv response: {rep:?}");
+    log::debug!("[client] notify server to stop");
     notify_stop
         .send(())
         .expect("cannot send stop signal to the server");
