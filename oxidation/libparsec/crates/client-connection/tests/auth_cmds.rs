f93aebe6021bb1c3794937c18448139a639a851e
firelight flagboy <firelight.flagboy@gmail.com>
2022-06-13 13:51:16 +0200

    wip add test with invalid signature

--- oxidation/libparsec/crates/client-connection/tests/auth_cmds.rs
+++ oxidation/libparsec/crates/client-connection/tests/auth_cmds.rs
@@ -57,6 +57,42 @@ async fn valid_request() {
     );
 }
 
+#[tokio::test]
+async fn invalid_request() {
+    simple_logger::init_with_level(log::Level::Debug).expect("cannot initialize simple logger");
+
+    const PING_MESSAGE: &str = "hello from the client side!";
+    const USER_ID: &str = "foobar";
+    const IP: Ipv4Addr = Ipv4Addr::new(127, 0, 0, 1);
+    const PORT: u16 = 14689;
+    let socket_addr: SocketAddr = SocketAddr::V4(SocketAddrV4::new(IP, PORT));
+
+    let client_kp = SigningKey::generate();
+    let other_kp = SigningKey::generate();
+    let url = format!("http://{}:{}", IP, PORT);
+    let auth_cmds = generate_client(client_kp, USER_ID.as_bytes(), &url);
+
+    let mut signature_verifier = MakeSignatureVerifier::default();
+    signature_verifier.register_public_key(USER_ID.to_string(), other_kp.verify_key());
+
+    let (server_handle, client_handle) =
+        send_ping(socket_addr, signature_verifier, auth_cmds, PING_MESSAGE).await;
+
+    let client_response = client_handle.await.expect("client handle failed");
+    let server_response = server_handle.await.expect("server handle failed");
+
+    log::debug!("[test] server response: {server_response:?}");
+    assert!(
+        server_response.is_ok(),
+        "server failed with error {}",
+        server_response.unwrap_err()
+    );
+
+    log::debug!("[test] client response: {client_response:?}");
+    assert!(client_response.is_err());
+    let client_response = dbg!(client_response.unwrap_err());
+}
+
 fn generate_client(signing_key: SigningKey, user_id: &[u8], root_url: &str) -> AuthenticatedCmds {
     let client = reqwest::ClientBuilder::new()
         .build()
