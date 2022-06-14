9e93e1997701b5ce8652c50cb062c3bb5c982ed2
AntoineDupre <dupre.antoine.nicolas@gmail.com>
2022-06-14 11:33:01 +0200

    run tpek service as anonymous command

--- parsec/backend/tpek.py
+++ parsec/backend/tpek.py
@@ -4,7 +4,7 @@ from uuid import UUID
 from parsec.api.data.base import DataError
 from parsec.api.data.tpek import TpekDerServiceEncryptionKey
 
-from parsec.api.protocol.types import OrganizationID, UserProfile
+from parsec.api.protocol.types import OrganizationID
 from parsec.backend.client_context import AuthenticatedClientContext
 from parsec.backend.utils import ClientType, api, catch_protocol_errors
 from parsec.event_bus import EventBus
@@ -25,16 +25,11 @@ class BaseTpekComponent:
     def __init__(self, event_bus: EventBus):
         self._event_bus = event_bus
 
-    @api("tpek_register_service", client_types=[ClientType.AUTHENTICATED])
+    @api("tpek_register_service", client_types=[ClientType.ANONYMOUS])
     @catch_protocol_errors
     async def api_tpek_register_service(
         self, client_ctx: AuthenticatedClientContext, msg: dict
     ) -> dict:
-        if client_ctx.profile != UserProfile.ADMIN:
-            return {
-                "status": "not_allowed",
-                "reason": f"User `{client_ctx.device_id.user_id}` is not admin",
-            }
 
         msg = tpek_register_service_serializer.req_load(msg)
         tpek_encryption_key_payload = msg["tpek_der_payload"]
