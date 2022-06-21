3b44fc728f3404a92fb6de429d878fcc03605153
firelight flagboy <firelight.flagboy@gmail.com>
2022-06-21 14:43:49 +0200

    rename error
    - rename `NoResponse`
    - rename `InvalidResponseContent`

--- oxidation/libparsec/crates/client_connection/src/command_error.rs
+++ oxidation/libparsec/crates/client_connection/src/command_error.rs
@@ -8,7 +8,7 @@ pub type Result<T> = core::result::Result<T, CommandError>;
 #[derive(Debug)]
 pub enum CommandError {
     /// We failed to retrieve the reply.
-    Response(reqwest::Error),
+    NoResponse(reqwest::Error),
     /// We receive a response but with an unexpected status code.
     InvalidResponseStatus(reqwest::StatusCode, reqwest::Response),
     /// We failed to deserialize the reply.
@@ -18,8 +18,8 @@ pub enum CommandError {
 impl Error for CommandError {
     fn source(&self) -> Option<&(dyn Error + 'static)> {
         match self {
-            CommandError::Deserialization(e) => Some(e),
-            CommandError::Response(e) => Some(e),
+            CommandError::InvalidResponseContent(e) => Some(e),
+            CommandError::NoResponse(e) => Some(e),
             _ => None,
         }
     }
@@ -28,13 +28,13 @@ impl Error for CommandError {
 impl Display for CommandError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            CommandError::Response(reason) => {
+            CommandError::NoResponse(reason) => {
                 write!(f, "failed to retrieving the response: {reason}")
             }
             CommandError::InvalidResponseStatus(code, _response) => {
                 write!(f, "unexpected response status {code}")
             }
-            CommandError::Deserialization(reason) => {
+            CommandError::InvalidResponseContent(reason) => {
                 write!(f, "failed to deserialize the response: {reason}")
             }
         }
@@ -43,12 +43,12 @@ impl Display for CommandError {
 
 impl From<libparsec_protocol::DecodeError> for CommandError {
     fn from(e: libparsec_protocol::DecodeError) -> Self {
-        Self::Deserialization(e)
+        Self::InvalidResponseContent(e)
     }
 }
 
 impl From<reqwest::Error> for CommandError {
     fn from(e: reqwest::Error) -> Self {
-        Self::Response(e)
+        Self::NoResponse(e)
     }
 }
