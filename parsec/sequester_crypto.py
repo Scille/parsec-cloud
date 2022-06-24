51cf6848d3cbdc6205398557790b568071dfdfd1
AntoineDupre <dupre.antoine.nicolas@gmail.com>
2022-06-24 19:00:43 +0200

    Helper to handle errors

--- parsec/sequester_crypto.py
+++ parsec/sequester_crypto.py
@@ -1,5 +1,6 @@
 # Parsec Cloud (https://parsec.cloud) Copyright (c) AGPLv3 2016-2021 Scille SAS
 
+from functools import wraps
 import oscrypto
 import oscrypto.asymmetric
 
@@ -16,34 +17,59 @@ class SequesterCryptoError(Exception):
     pass
 
 
+class SequesterCryptoInvalidKeyError(SequesterCryptoError):
+    pass
+
+
+class SequesterCryptoWrongTypeError(SequesterCryptoError):
+    pass
+
+
+class SequesterCryptoOsNotCompatibleError(SequesterCryptoError):
+    pass
+
+
 class SequesterCryptoSignatureError(SequesterCryptoError):
     pass
 
 
+def handle_loading_error(func):
+    @wraps
+    def wrapper(*args, **kwargs):
+        try:
+            func(*args, **kwargs)
+        except ValueError as exc:
+            raise SequesterCryptoInvalidKeyError() from exc
+        except TypeError as exc:
+            raise SequesterCryptoWrongTypeError() from exc
+        except oscrypto.errors.AsymmetricKeyError as exc:
+            raise SequesterCryptoOsNotCompatibleError() from exc
+        except OSError as exc:
+            raise SequesterCryptoError from exc
+
+    return wrapper
+
+
+@handle_loading_error
 def load_sequester_public_key(bytes_key: bytes) -> SequesterPublicKey:
     """
     Raises:
         SequesterCryptoError
     """
-    try:
-        return oscrypto.asymmetric.load_public_key(bytes_key)
-    except ValueError:
-        raise SequesterCryptoError()
+    return oscrypto.asymmetric.load_public_key(bytes_key)
 
 
 def dump_sequester_public_key(public_key: SequesterPublicKey) -> bytes:
     return oscrypto.asymmetric.dump_public_key(public_key)  # Dump in pem format
 
 
+@handle_loading_error
 def load_sequester_private_key(bytes_key: bytes) -> SequesterPrivateKey:
     """
     Raises:
         SequesterCryptoError
     """
-    try:
-        return oscrypto.asymmetric.load_private_key(bytes_key)
-    except ValueError:
-        raise SequesterCryptoError()
+    return oscrypto.asymmetric.load_private_key(bytes_key)
 
 
 def verify_sequester(public_key: SequesterPublicKey, data: bytes, signature: bytes) -> None:
