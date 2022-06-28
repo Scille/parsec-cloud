cfe927ed8dd49421207ae729356fd3b0ecbbb0cf
firelight flagboy <firelight.flagboy@gmail.com>
2022-06-28 08:43:48 +0200

    keep only faulty tests

--- oxidation/crates/parsec_api_types/tests/test_addr/test_from_http_redirect.rs
+++ oxidation/crates/parsec_api_types/tests/test_addr/test_from_http_redirect.rs
@@ -3,12 +3,11 @@ use rstest::rstest;
 use parsec_api_types::BackendAddr;
 
 #[rstest]
-#[case("https://foo.bar", false)]
-#[case("https://foo.bar/redirection", false)]
-#[case("https://foo.bar/redirect", true)]
-#[case("https://foo.bar/not_valid", false)]
-fn test_faulty_addr_redirection(#[case] raw_url: &str, #[case] is_valid: bool) {
+#[case("https://foo.bar")]
+#[case("https://foo.bar/redirection")]
+#[case("https://foo.bar/not_valid")]
+fn test_faulty_addr_redirection(#[case] raw_url: &str) {
     let res = dbg!(BackendAddr::from_http_redirection(raw_url));
 
-    assert_eq!(res.is_ok(), is_valid)
+    assert!(res.is_err());
 }
