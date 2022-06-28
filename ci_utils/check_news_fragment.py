5f064b018b9b858856df9ee3d10fe05008faa549
firelight flagboy <firelight.flagboy@gmail.com>
2022-06-28 16:12:47 +0200

    migrate QA job to github action

---
+++ ci_utils/check_news_fragment.py
@@ -0,0 +1,39 @@
+# PRs must contain a newsfragment that reference an opened issue
+from pathlib import Path
+from subprocess import run
+from urllib.request import urlopen, Request, HTTPError
+from concurrent.futures import ThreadPoolExecutor
+import json
+# If file never existed in master, consider as a new newsfragment
+# Cannot just git diff against master branch here given newsfragments
+# removed in master will be considered as new items in our branch
+# --exit-code makes the command exit with 0 if there are changes
+basecmd = 'git log origin/master --exit-code --'.split()
+
+def check_newsfragment(fragment):
+    ret = run([*basecmd, fragment], capture_output=True)
+    if ret.returncode == 0:
+        print(f'Found new newsfragment {fragment.name}')
+        id, *_ = fragment.name.split('.')
+        req = Request(
+            method='GET',
+            url=f'https://api.github.com/repos/Scille/parsec-cloud/issues/{id}',
+            headers={'Accept': 'application/vnd.github.v3+json'},
+        )
+        try:
+            ret = urlopen(req)
+        except HTTPError as exc:
+            raise SystemExit('New newsfragment ID doesn\'t correspond to an issue !')
+        data = json.loads(ret.read())
+        if 'pull_request' in data:
+            raise SystemExit('New newsfragement ID correspond to a pull request instead of an issue !')
+        if data['state'] != 'open':
+            raise SystemExit('New newsfragement ID correspond to a closed issue')
+        return True
+    return False
+
+with ThreadPoolExecutor() as pool:
+    ret = pool.map(check_newsfragment, Path('newsfragments').glob('*.rst'))
+
+if True not in ret:
+    raise SystemExit('No new newsfragment found')
