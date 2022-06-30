d68ae74217735b9c28dad60b4945cc4c97f4b089
Pascal <pascal.chen@devling.xyz>
2022-06-30 17:41:03 +0200

    Platform binding base

--- oxidation/bindings/common/src/lib.rs
+++ oxidation/bindings/common/src/lib.rs
@@ -1,37 +1,36 @@
 // Parsec Cloud (https://parsec.cloud) Copyright (c) BSLv1.1 (eventually AGPLv3) 2016-2021 Scille SAS
 
+use base64::DecodeError;
+use libparsec::SecretKey;
 pub use libparsec::{create_context, RuntimeContext};
 
-use libparsec::SecretKey;
-
 pub fn decode_and_execute(cmd: &str, payload: &str) -> Result<String, String> {
-    fn _get_key_and_data(payload: &str) -> Result<(SecretKey, Vec<u8>), String> {
-        let mut splitted = payload.split(':');
-        let b64_key = splitted.next(); // key as base64
-        let b64_data = splitted.next(); // message as base64
-        match (b64_key, b64_data, splitted.next()) {
-            (Some(b64_key), Some(b64_data), None) => {
-                match (base64::decode(b64_key), base64::decode(b64_data)) {
-                    (Ok(raw_key), Ok(data)) => match SecretKey::try_from(&raw_key[..]) {
-                        Ok(key) => Ok((key, data)),
-                        err => Err(format!("bad_params_value:{:?}", err)),
-                    },
-                    err => Err(format!("bad_params_encoding:{:?}", err)),
-                }
-            }
-            err => Err(format!("bad_params_number:{:?}", err)),
-        }
-    }
+    let version = "v0.0.1".into();
+    let payload = payload
+        .split(':')
+        .map(base64::decode)
+        .collect::<Result<Vec<_>, DecodeError>>()
+        .map_err(|err| format!("bad_params_encoding: {err:?}"))?;
 
-    match cmd {
-        "encrypt" => _get_key_and_data(payload).map(|(key, data)| {
-            let encrypted = key.encrypt(&data);
-            base64::encode(encrypted)
-        }),
-        "decrypt" => _get_key_and_data(payload).and_then(|(key, data)| match key.decrypt(&data) {
-            Ok(cleartext) => Ok(base64::encode(cleartext)),
-            Err(err) => Err(format!("decryption_error:{:?}", err)),
-        }),
-        err => Err(format!("unknown_command:{}", err)),
+    match (cmd, &payload[..]) {
+        ("version", _) => Ok(version),
+        ("encrypt", [raw_key, data]) => match SecretKey::try_from(&raw_key[..]) {
+            Ok(key) => {
+                let encrypted = key.encrypt(data);
+                Ok(base64::encode(encrypted))
+            }
+            Err(err) => Err(format!("bad_params_value: {err:?}")),
+        },
+        ("decrypt", [raw_key, data]) => match SecretKey::try_from(&raw_key[..]) {
+            Ok(key) => match key.decrypt(data) {
+                Ok(cleartext) => Ok(base64::encode(cleartext)),
+                Err(err) => Err(format!("decryption_error: {err:?}")),
+            },
+            Err(err) => Err(format!("bad_params_value: {err:?}")),
+        },
+        (unknown_cmd, payload) => Err(format!(
+            "unknown_command or invalid_payload_length: {unknown_cmd} {}",
+            payload.len()
+        )),
     }
 }
--- oxidation/client/src/router/index.ts
+++ oxidation/client/src/router/index.ts
@@ -3,16 +3,22 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
 import HomePage from '../views/HomePage.vue';
+import TestPage from '../views/TestPage.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    redirect: '/home'
+    redirect: '/test'
   },
   {
     path: '/home',
     name: 'Home',
     component: HomePage
+  },
+  {
+    path: '/test',
+    name: 'Test',
+    component: TestPage
   }
 ];
 
