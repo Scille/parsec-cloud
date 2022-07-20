eeaa7a73ff140ceb4084feb8749a3a945bfa6a8c
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2022-07-20 22:33:48 +0200

    Fix parsec/api/version.py (APIv4 add by mistake)

--- parsec/api/version.py
+++ parsec/api/version.py
@@ -8,8 +8,14 @@ class ApiVersion(namedtuple("ApiVersion", "version revision")):
         return f"{self.version}.{self.revision}"
 
 
+# API major versions:
+# v1: Original API
+# - v1.1
+# v2 (Parsec 1.14+): Incompatible hanshake with system with SAS-based authentication
+# - v2.8 (Parsec 2.11+): Sequester API
+# v3 (Parsec 2.9+): Incompatible handshake challenge answer format
+# - v3.1 (Parsec 2.11+): Sequester API
 API_V1_VERSION = ApiVersion(version=1, revision=3)
 API_V2_VERSION = ApiVersion(version=2, revision=7)
 API_V3_VERSION = ApiVersion(version=3, revision=0)
-API_V4_VERSION = ApiVersion(version=4, revision=0)
-API_VERSION = API_V4_VERSION
+API_VERSION = API_V3_VERSION
