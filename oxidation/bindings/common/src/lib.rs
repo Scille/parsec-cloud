71f058ee5ce5b79fb865f2e031eb84f524ce9d72
Pascal <pascal.chen@devling.xyz>
2022-07-25 15:21:02 +0200

    Add comment

--- oxidation/bindings/common/src/lib.rs
+++ oxidation/bindings/common/src/lib.rs
@@ -8,6 +8,7 @@ pub use libparsec::{create_context, RuntimeContext};
 
 #[cfg(not(target_arch = "wasm32"))]
 use libparsec::core::list_available_devices;
+/// We can't access file system in web environment
 #[cfg(target_arch = "wasm32")]
 fn list_available_devices(_config_dir: &std::path::Path) -> Result<(), ()> {
     Err(())
