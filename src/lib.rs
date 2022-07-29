28000d84b97ffc696fdbe9f224b7bfc148bd5bb4
firelight flagboy <firelight.flagboy@gmail.com>
2022-07-29 11:21:37 +0200

    rename `maturin` generated lib to `_parsec`

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
 
 /// A Python module implemented in Rust.
 #[pymodule]
-#[pyo3(name = "parsec")]
+#[pyo3(name = "_parsec")]
 fn entrypoint(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
     Ok(())
