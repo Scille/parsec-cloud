6f909af5e78caf60a06be7fc79d6b09fcfd7ed74
Alban Costaz <alban.costaz@gmail.com>
2022-08-01 10:11:23 +0200

    Updated min macfuse version to 4.4.0

--- parsec/core/gui/instance_widget.py
+++ parsec/core/gui/instance_widget.py
@@ -36,7 +36,7 @@ from parsec.core.gui.central_widget import CentralWidget
 
 logger = get_logger()
 
-MIN_MACFUSE_VERSION = Version("4.2.3")
+MIN_MACFUSE_VERSION = Version("4.4.0")
 
 
 async def _do_run_core(config, device, qt_on_ready):
