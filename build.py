9b8adefef9680c7d895ef273a58c9fc5d56dc8a2
Vincent Michel <vxgmichel@gmail.com>
2022-08-03 13:29:32 +0200

    Experiment with pretending we're in a venv while building wheel in a docker container

--- build.py
+++ build.py
@@ -1,6 +1,7 @@
 # Parsec Cloud (https://parsec.cloud) Copyright (c) AGPL-3.0 2016-present Scille SAS
 
 import os
+import sys
 import logging
 import subprocess
 
@@ -20,25 +21,40 @@ def run(cmd, **kwargs):
     return ret
 
 
-def is_in_venv():
-    if os.environ.get("CONDA_PREFIX") or os.environ.get("VIRTUAL_ENV"):
+def in_cibuildwheel():
+    return bool(int(os.environ.get("CIBUILDWHEEL", "0")))
+
+
+def check_venv():
+    # Both conda and venv are configured, pick conda
+    if os.environ.get("CONDA_PREFIX") and os.environ.get("VIRTUAL_ENV"):
         del os.environ["VIRTUAL_ENV"]
-    return os.environ.get("CONDA_PREFIX") or os.environ.get("VIRTUAL_ENV")
+    # Linux build with cibuildwheel detected, set `VIRTUAL_ENV`
+    if sys.platform == "linux" and in_cibuildwheel():
+        proc = subprocess.run(
+            "which python", shell=True, check=True, capture_output=True, text=True
+        )
+        os.environ["VIRTUAL_ENV"] = os.path.dirname(os.path.dirname(proc.stdout.strip()))
+        log.debug(
+            f"Linux build with cibuildwheel detected, set `VIRATUAL_ENV` as: {os.environ['VIRTUAL_ENV']}"
+        )
+    if not os.environ.get("CONDA_PREFIX") and not os.environ.get("VIRTUAL_ENV"):
+        raise RuntimeError("A virtual env is required to run `maturin develop`")
 
 
-def build(setup_kargs):
-    log.debug(f"setup_kargs: {str(setup_kargs)}")
-
+def build():
     run("python --version")
     run("pip freeze")
     run("python misc/generate_pyqt.py")
-    if is_in_venv():
-        run("maturin develop")
-    else:
-        run("maturin build -r")
-        run("unzip -o target/wheels/parsec-*.whl parsec/_parsec.abi3.so")
+    check_venv()
+    release = "--release" if in_cibuildwheel() else ""
+    run(f"maturin develop {release}")
+    # An alternative to `maturin develop` is:
+    # run("maturin build -r")
+    # run("unzip -o target/wheels/parsec-*.whl parsec/_parsec.abi3.so")
+    # Note that it does not require a virtualenv, as opposed to `develop`
 
 
 if __name__ == "__main__":
-    log.debug("launching build script in manual")
-    build({})
+    log.debug("Launching poetry build.py script")
+    build()
