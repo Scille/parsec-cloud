91ac0f0f88f4e2ecc019dc0e83b38d29642d9cc2
firelight flagboy <firelight.flagboy@gmail.com>
2022-08-04 15:53:50 +0200

    config setup-rust for macos

--- ci/setup-rust.sh
+++ ci/setup-rust.sh
@@ -9,6 +9,6 @@ trap "rm -vrf $TMP_DIR" EXIT INT TERM
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs > $TMP_DIR/rust-install.sh
 
-bash $TMP_DIR/rust-install.sh -y --default-toolchain 1.62 --profile minimal
+bash $TMP_DIR/rust-install.sh -y --default-toolchain 1.62 --profile minimal ${@:2}
 
 echo "export PATH=$PATH:$HOME/.cargo/bin"
