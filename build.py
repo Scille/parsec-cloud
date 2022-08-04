922c0c84cfe6ba6538beb04e1d2d5c617317d543
firelight flagboy <firelight.flagboy@gmail.com>
2022-08-04 08:38:54 +0200

    Add `build.py` script to setup the env
    `build.py` will on installation & building wheel:
    
    - Generate PyQt binding
    - Compile the Rust lib

---
+++ build.py
@@ -0,0 +1,62 @@
+# Parsec Cloud (https://parsec.cloud) Copyright (c) AGPL-3.0 2016-present Scille SAS
+
+import os
+import sys
+import logging
+import subprocess
+
+log = logging.getLogger("build")
+
+YELLOW_FG = "\x1b[33m"
+DEFAULT_FG = "\x1b[39m"
+
+logging.basicConfig(
+    level=logging.DEBUG, format=f"{YELLOW_FG}[%(levelname)s] %(message)s{DEFAULT_FG}"
+)
+
+
+def run(cmd, **kwargs):
+    log.debug(f">>> {cmd}")
+    ret = subprocess.run(cmd, shell=True, check=True, **kwargs)
+    return ret
+
+
+def in_cibuildwheel():
+    return bool(int(os.environ.get("CIBUILDWHEEL", "0")))
+
+
+def check_venv():
+    # Both conda and venv are configured, pick conda
+    if os.environ.get("CONDA_PREFIX") and os.environ.get("VIRTUAL_ENV"):
+        del os.environ["VIRTUAL_ENV"]
+    # Linux build with cibuildwheel detected, set `VIRTUAL_ENV`
+    if sys.platform == "linux" and in_cibuildwheel():
+        proc = subprocess.run(
+            "which python", shell=True, check=True, capture_output=True, text=True
+        )
+        python_path = proc.stdout.strip()
+        log.debug(f"python_path={python_path}, sys.executable={sys.executable}")
+        os.environ["VIRTUAL_ENV"] = os.path.dirname(os.path.dirname(python_path))
+        log.debug(
+            f"Linux build with cibuildwheel detected, set `VIRTUAL_ENV` as: {os.environ['VIRTUAL_ENV']}"
+        )
+    if not os.environ.get("CONDA_PREFIX") and not os.environ.get("VIRTUAL_ENV"):
+        raise RuntimeError("A virtual env is required to run `maturin develop`")
+
+
+def build():
+    run("python --version")
+    run("pip freeze")
+    run("python misc/generate_pyqt.py")
+    check_venv()
+    release = "--release" if in_cibuildwheel() else ""
+    run(f"maturin develop {release}")
+    # An alternative to `maturin develop` is:
+    # run("maturin build -r")
+    # run("unzip -o target/wheels/parsec-*.whl parsec/_parsec.abi3.so")
+    # Note that it does not require a virtualenv, as opposed to `develop`
+
+
+if __name__ == "__main__":
+    log.debug("Launching poetry build.py script")
+    build()
