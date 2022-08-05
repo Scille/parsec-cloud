8b9f7fd7cd274b23195adbb65a9500b48f34ac60
firelight flagboy <firelight.flagboy@gmail.com>
2022-08-05 17:05:41 +0200

    Don't use a tempdir for the installation script of rust

--- misc/setup-rust.sh
+++ misc/setup-rust.sh
@@ -12,15 +12,10 @@
 # ```
 
 set -eux
+set -o pipefail
 
 RUST_TOOLCHAIN=${1:-1.62}
 
-TMP_DIR=$(mktemp -d -t rust-install.XXXXXXXXXX)
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | bash -s -- -y --default-toolchain 1.62 --profile minimal ${@:2}
 
-trap "rm -vrf $TMP_DIR" EXIT INT TERM
-
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs > $TMP_DIR/rust-install.sh
-
-bash $TMP_DIR/rust-install.sh -y --default-toolchain 1.62 --profile minimal ${@:2}
-
-echo "export PATH=$PATH:$HOME/.cargo/bin"
+echo "Successfully installed rust-${RUST_TOOLCHAIN}"
