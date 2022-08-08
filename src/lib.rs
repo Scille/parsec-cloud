5f620fb5fa5afd6cf3304277306f0e662d812edb
firelight flagboy <firelight.flagboy@gmail.com>
2022-08-08 13:48:09 +0200

    Remove example function

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,8 @@
 use pyo3::prelude::*;
 
-/// Formats the sum of two numbers as string.
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
-}
-
 /// A Python module implemented in Rust.
 #[pymodule]
 #[pyo3(name = "_parsec")]
-fn entrypoint(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+fn entrypoint(_py: Python, _m: &PyModule) -> PyResult<()> {
     Ok(())
 }
