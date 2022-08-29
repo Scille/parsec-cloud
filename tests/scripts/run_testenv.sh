9b66ad88b4e77c85feeca3fb2590404b7c4ed657
FirelightFlagboy <30697622+FirelightFlagboy@users.noreply.github.com>
2022-08-29 14:26:48 +0200

    Format shell script with `editorconfig` (#2959)

--- tests/scripts/run_testenv.sh
+++ tests/scripts/run_testenv.sh
@@ -11,26 +11,26 @@ For MacOS, please install coreutils from brew to get realpath:
 # Make sure this script is sourced
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]
 then
-  echo "This script must be sourced. Use --help for more information."
-  exit 1
+    echo "This script must be sourced. Use --help for more information."
+    exit 1
 fi
 
 # Prevent pre-commit from losing it work dir
 if [[ -z "$PRE_COMMIT_HOME" ]]
 then
-  if [[ -z "$XDG_CACHE_HOME" ]]
-  then
-    export PRE_COMMIT_HOME="$HOME/.cache/pre-commit"
-  else
-    export PRE_COMMIT_HOME="$XDG_CACHE_HOME/pre-commit"
-  fi
+    if [[ -z "$XDG_CACHE_HOME" ]]
+    then
+        export PRE_COMMIT_HOME="$HOME/.cache/pre-commit"
+    else
+        export PRE_COMMIT_HOME="$XDG_CACHE_HOME/pre-commit"
+    fi
 fi
 
 # Cross-shell script directory detection
 if [ -n "$BASH_SOURCE" ]; then
-  SCRIPT_DIR=$(dirname $(realpath -s $BASH_SOURCE))
+    SCRIPT_DIR=$(dirname $(realpath -s $BASH_SOURCE))
 elif [ -n "$ZSH_VERSION" ]; then
-  SCRIPT_DIR=$(dirname $(realpath -s $0))
+    SCRIPT_DIR=$(dirname $(realpath -s $0))
 fi
 
 # In Python we trust (aka shell's tempfile&mktemp doesn't work on all platforms)
