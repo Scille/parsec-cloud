1282b3c1f273a063ded993dbf4d449d1ea0f1538
Pascal <pascal.chen@devling.xyz>
2022-09-02 16:28:39 +0200

    Replace Message

--- parsec/api/protocol/message.py
+++ parsec/api/protocol/message.py
@@ -1,26 +1,10 @@
 # Parsec Cloud (https://parsec.cloud) Copyright (c) AGPL-3.0 2016-present Scille SAS
 
-from parsec.serde import BaseSchema, fields
-from parsec.api.protocol.base import BaseReqSchema, BaseRepSchema, CmdSerializer
-from parsec.api.protocol.types import DeviceIDField
+from parsec._parsec import MessageGetReq, MessageGetRep
+from parsec.api.protocol.base import ApiCommandSerializer
 
 
 __all__ = ("message_get_serializer",)
 
 
-class MessageGetReqSchema(BaseReqSchema):
-    offset = fields.Integer(required=True, validate=lambda n: n >= 0)
-
-
-class MessageSchema(BaseSchema):
-    count = fields.Integer(required=True)
-    sender = DeviceIDField(required=True)
-    timestamp = fields.DateTime(required=True)
-    body = fields.Bytes(required=True)
-
-
-class MessageGetRepSchema(BaseRepSchema):
-    messages = fields.List(fields.Nested(MessageSchema), required=True)
-
-
-message_get_serializer = CmdSerializer(MessageGetReqSchema, MessageGetRepSchema)
+message_get_serializer = ApiCommandSerializer(MessageGetReq, MessageGetRep)
