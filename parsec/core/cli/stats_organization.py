c78eefab56946bb83545a65f135990832ecb8d4d
Volham <corentin.machu@protonmail.com>
2022-09-20 17:32:35 +0200

    Added new stats_server core command

--- parsec/core/cli/stats_organization.py
+++ parsec/core/cli/stats_organization.py
@@ -1,15 +1,34 @@
 # Parsec Cloud (https://parsec.cloud) Copyright (c) AGPL-3.0 2016-present Scille SAS
 from __future__ import annotations
 
+import re
 import click
+from click.exceptions import BadArgumentUsage
 
-from parsec.utils import trio_run
+from parsec.utils import DateTime, trio_run
 from parsec.api.protocol import OrganizationID
 from parsec.api.rest import organization_stats_rep_serializer
 from parsec.cli_utils import cli_exception_handler
 from parsec.core.types import BackendAddr
 from parsec.core.backend_connection.transport import http_request
 from parsec.core.cli.utils import cli_command_base_options
+
+
+def _date_from_str(input: str) -> DateTime:
+    regex = re.compile(r"(\d{4})[-/](\d{1,2})[-/](\d{1,2})")
+    matched = regex.match(input)
+
+    if not matched:
+        raise BadArgumentUsage("Provided date is not a valid timestamp")
+
+    return DateTime(
+        int(matched.group(1)),
+        int(matched.group(2)),
+        int(matched.group(3)),
+        hour=0,
+        minute=0,
+        second=0,
+    )
 
 
 async def _stats_organization(
@@ -42,3 +61,23 @@ def stats_organization(
 ) -> None:
     with cli_exception_handler(debug):
         trio_run(_stats_organization, organization_id, addr, administration_token)
+
+
+@click.command(short_help="Get a per-organization report of monthly usage")
+@click.option("--addr", "-B", required=True, type=BackendAddr.from_url, envvar="PARSEC_ADDR")
+@click.option("--admin-token", "-T", required=True, envvar="PARSEC_ADMINISTRATION_TOKEN")
+@click.option("--date-from", required=True, type=_date_from_str)
+@click.option("--date-to", required=True, type=_date_from_str)
+@click.option("--output", required=True)
+@click.option("--format", default="csv")
+@cli_command_base_options
+def stats_server(
+    addr: BackendAddr,
+    admin_token: str,
+    date_from: DateTime,
+    date_to: DateTime,
+    output: str,
+    format: str,
+    **kwargs,
+) -> None:
+    pass
