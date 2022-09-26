3277e8a021de9b0ee84fea982b410ffedc2b6968
firelight flagboy <firelight.flagboy@gmail.com>
2022-09-26 16:49:37 +0200

    Re-export types from the `parser` module

--- oxidation/libparsec/crates/serialization_format/src/protocol/intermediate/mod.rs
+++ oxidation/libparsec/crates/serialization_format/src/protocol/intermediate/mod.rs
@@ -1,5 +1,10 @@
 pub mod cmd;
 pub mod collection;
+use crate::parser;
 
 pub use cmd::Cmd;
 pub use collection::ProtocolCollection;
+
+pub use parser::{
+    CustomEnum, CustomStruct, CustomType, Field, MajorMinorVersion, Request, Response, Variant,
+};
