848c24901ca4a2919515975fbf7eadd676652ee4
firelight flagboy <firelight.flagboy@gmail.com>
2022-09-27 16:32:49 +0200

    Wip quote `Field`

--- oxidation/libparsec/crates/serialization_format/src/protocol/parser/field.rs
+++ oxidation/libparsec/crates/serialization_format/src/protocol/parser/field.rs
@@ -1,17 +1,10 @@
+use std::collections::HashMap;
+
+use crate::protocol::utils::{quote_serde_as, validate_raw_type};
+
 use super::MajorMinorVersion;
 
 use serde::Deserialize;
-
-#[derive(Clone, Copy)]
-pub enum Vis {
-    Public,
-    Private,
-}
-
-pub enum SerdeAttr {
-    Rename,
-    Tag,
-}
 
 #[cfg_attr(test, derive(PartialEq, Eq))]
 #[derive(Debug, Deserialize, Clone)]
@@ -37,6 +30,64 @@ impl Default for Field {
             ty: "String".to_string(),
             introduced_in: None,
             default: None,
+        }
+    }
+}
+
+impl Field {
+    pub fn quote(
+        &self,
+        visibility: syn::Visibility,
+        types: &HashMap<String, String>,
+    ) -> syn::ExprType {
+        let (rename, name) = self.quote_name();
+        let (ty, serde_skip) = self.quote_type(types);
+
+        let mut attrs: Vec<syn::Attribute> = Vec::new();
+
+        if let Some(rename) = rename {
+            attrs.push(syn::parse_quote!(#[serde(rename = #rename)]))
+        }
+        attrs.push(quote_serde_as(&ty));
+        if let Some(serde_skip) = serde_skip {
+            attrs.push(serde_skip)
+        }
+        if let Some(default) = &self.default {
+            attrs.push(syn::parse_quote!(#[serde(default = #default)]));
+        }
+        if let Some(introduced_in) = self.introduced_in {
+            let introduced_text = format!("Introduced in API-{introduced_in}");
+            attrs.push(syn::parse_quote!(#[doc = #introduced_text]));
+        }
+
+        syn::parse_quote! {
+            #(#attrs)*
+            #visibility #name: #ty
+        }
+    }
+
+    fn quote_name(&self) -> (Option<&'static str>, syn::Ident) {
+        let rename = match self.name.as_str() {
+            "type" => Some("ty"),
+            _ => None,
+        };
+        let quote = syn::parse_str(rename.unwrap_or(&self.name)).expect("A valid name");
+
+        (rename, quote)
+    }
+
+    fn quote_type(&self, types: &HashMap<String, String>) -> (syn::Type, Option<syn::Attribute>) {
+        let ty = validate_raw_type(&self.ty, types).expect("A valid type");
+
+        if self.introduced_in.is_some() {
+            (
+                syn::parse_quote!(libparsec_types::Maybe<#ty>),
+                Some(
+                    syn::parse_quote!(#[serde(default, skip_serializing_if = "libparsec_types::Maybe::is_absent")]),
+                ),
+            )
+        } else {
+            (ty, None)
         }
     }
 }
