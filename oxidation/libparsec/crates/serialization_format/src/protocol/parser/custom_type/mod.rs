af7d19aaefa0312b84e97db94717e237b6ed0f0f
firelight flagboy <firelight.flagboy@gmail.com>
2022-09-28 15:58:46 +0200

    Test `CustomType` quotation

---
+++ oxidation/libparsec/crates/serialization_format/src/protocol/parser/custom_type/mod.rs
@@ -0,0 +1,134 @@
+pub mod custom_enum;
+pub mod custom_struct;
+pub mod variant;
+
+use std::collections::HashMap;
+
+use serde::Deserialize;
+
+pub use custom_enum::CustomEnum;
+pub use custom_struct::CustomStruct;
+pub use variant::Variant;
+
+#[cfg_attr(test, derive(PartialEq, Eq))]
+#[derive(Debug, Deserialize, Clone)]
+#[serde(untagged)]
+pub enum CustomType {
+    Struct(CustomStruct),
+    Enum(CustomEnum),
+}
+
+impl CustomType {
+    pub fn label(&self) -> &str {
+        match self {
+            CustomType::Struct(custom_struct) => &custom_struct.label,
+            CustomType::Enum(custom_enum) => &custom_enum.label,
+        }
+    }
+}
+
+impl CustomType {
+    pub fn quote(&self, types: &HashMap<String, String>) -> syn::Item {
+        match self {
+            CustomType::Struct(custom_struct) => syn::Item::Struct(custom_struct.quote(types)),
+            CustomType::Enum(custom_enum) => syn::Item::Enum(custom_enum.quote(types)),
+        }
+    }
+}
+
+fn shared_attribute() -> Vec<syn::Attribute> {
+    vec![
+        syn::parse_quote!(#[::serde_with::serde_as]),
+        syn::parse_quote!(#[derive(Debug, Clone, ::serde::Deserialize, ::serde::Serialize, PartialEq, Eq)]),
+    ]
+}
+
+#[cfg(test)]
+mod test {
+    use super::{variant::Variant, CustomEnum, CustomStruct, CustomType};
+    use crate::protocol::parser::field::Field;
+    use rstest::rstest;
+
+    #[rstest]
+    #[case::deserialize_enum(
+        r#"{
+        "label": "APIEvent",
+        "discriminant_field": "event",
+        "variants": [
+            {
+                "name": "Pinged",
+                "discriminant_value": "pinged",
+                "fields": [
+                    {
+                        "name": "ping",
+                        "type": "String"
+                    }
+                ]
+            }
+        ]
+    }"#
+    , CustomType::Enum(CustomEnum {
+        label: "APIEvent".to_string(),
+        discriminant_field: Some("event".to_string()),
+        variants: vec![
+            Variant {
+                name: "Pinged".to_string(),
+                discriminant_value: "pinged".to_string(),
+                fields: vec![
+                    Field {
+                        name: "ping".to_string(),
+                        ty: "String".to_string(),
+                        introduced_in: None,
+                        default: None,
+                    }
+                ]
+            }
+        ]
+    }))]
+    #[case::deserialize_struct(
+        r#"{
+        "label": "HumanFindResultItem",
+        "fields": [
+            {
+                "name": "user_id",
+                "type": "UserID"
+            },
+            {
+                "name": "human_handle",
+                "type": "Option<HumanHandle>"
+            },
+            {
+                "name": "revoked",
+                "type": "Boolean"
+            }
+        ]
+    }"#,
+    CustomType::Struct(CustomStruct {
+        label: "HumanFindResultItem".to_string(),
+        fields: vec![
+            Field {
+                name: "user_id".to_string(),
+                ty: "UserID".to_string(),
+                introduced_in: None,
+                default: None,
+            },
+            Field {
+                name: "human_handle".to_string(),
+                ty: "Option<HumanHandle>".to_string(),
+                introduced_in: None,
+                default: None,
+            },
+            Field {
+                name: "revoked".to_string(),
+                ty: "Boolean".to_string(),
+                introduced_in: None,
+                default: None,
+            }
+        ]
+    }))]
+    fn test_deserialization(#[case] input: &str, #[case] expected: CustomType) {
+        let custom_type =
+            serde_json::from_str::<CustomType>(input).expect("Got error on valid data");
+        assert_eq!(custom_type, expected);
+    }
+}
---
+++ oxidation/libparsec/crates/serialization_format/src/protocol/parser/custom_type/variant.rs
@@ -0,0 +1,137 @@
+use std::collections::HashMap;
+
+use serde::Deserialize;
+
+use crate::protocol::parser::field::{quote_fields, Field};
+
+#[cfg_attr(test, derive(PartialEq, Eq))]
+#[derive(Debug, Deserialize, Clone)]
+pub struct Variant {
+    pub name: String,
+    pub discriminant_value: String,
+    pub fields: Vec<Field>,
+}
+
+#[cfg(test)]
+impl Default for Variant {
+    fn default() -> Self {
+        Self {
+            discriminant_value: "type".to_string(),
+            fields: vec![],
+            name: "FooVariant".to_string(),
+        }
+    }
+}
+
+impl Variant {
+    pub fn quote(&self, types: &HashMap<String, String>) -> syn::Variant {
+        let rename = match self.name.as_str() {
+            "type" => Some("ty"),
+            _ => None,
+        };
+        // TODO: Format the name in pascal case
+        let name = syn::parse_str::<syn::Ident>(rename.unwrap_or(&self.name))
+            .expect("A valid variant name");
+
+        let mut attrs: Vec<syn::Attribute> = Vec::new();
+        if rename.is_some() {
+            let name = &&self.name;
+            attrs.push(syn::parse_quote!(#[serde(rename = #name)]))
+        }
+
+        let fields = quote_fields(&self.fields, types);
+
+        if fields.is_empty() {
+            syn::parse_quote! {
+                #(#attrs)*
+                #name
+            }
+        } else {
+            syn::parse_quote! {
+                #(#attrs)*
+                #name {
+                    #(#fields),*
+                }
+            }
+        }
+    }
+}
+
+pub fn quote_variants(variants: &[Variant], types: &HashMap<String, String>) -> Vec<syn::Variant> {
+    variants
+        .iter()
+        .map(|variant| variant.quote(types))
+        .collect()
+}
+
+#[cfg(test)]
+#[rstest::rstest]
+#[case::empty(
+    Variant {
+        ..Default::default()
+    },
+    quote::quote! {
+        FooVariant
+    }
+)]
+#[case::with_field(
+    Variant {
+        fields: vec![
+            Field::default()
+        ],
+        ..Default::default()
+    },
+    quote::quote! {
+        FooVariant {
+            #[serde_as(as = "_")]
+            pub foo_type: String
+        }
+    }
+)]
+#[case::with_multiple_fields(
+    Variant {
+        fields: vec![
+            Field::default(),
+            Field::default(),
+        ],
+        ..Default::default()
+    },
+    quote::quote! {
+        FooVariant {
+            #[serde_as(as = "_")]
+            pub foo_type: String,
+            #[serde_as(as = "_")]
+            pub foo_type: String
+        }
+    }
+)]
+#[case::rename(
+    Variant {
+        name: "type".to_string(),
+        ..Default::default()
+    },
+    quote::quote! {
+        #[serde(rename = "type")]
+        ty
+    }
+)]
+#[case::lowercase_name(
+    Variant {
+        name: "foo_bar".to_string(),
+        ..Default::default()
+    },
+    quote::quote! {
+        foo_bar
+    }
+)]
+fn test_quote(#[case] variant: Variant, #[case] expected: proc_macro2::TokenStream) {
+    use quote::ToTokens;
+
+    pretty_assertions::assert_eq!(
+        variant
+            .quote(&HashMap::new())
+            .into_token_stream()
+            .to_string(),
+        expected.to_string()
+    )
+}
