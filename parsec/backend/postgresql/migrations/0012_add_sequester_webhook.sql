232c59d726379ec478ef56b733309e7aec7f5e08
AntoineDupre <dupre.antoine.nicolas@gmail.com>
2022-09-30 09:28:38 +0200

    Set default service type STORAGE to previous register services

--- parsec/backend/postgresql/migrations/0012_add_sequester_webhook.sql
+++ parsec/backend/postgresql/migrations/0012_add_sequester_webhook.sql
@@ -6,4 +6,6 @@
 CREATE TYPE sequester_service_type AS ENUM ('STORAGE', 'WEBHOOK');
 
 ALTER TABLE sequester_service ADD webhook_url TEXT; -- NULL if service_type != WEBHOOK;
-ALTER TABLE sequester_service ADD service_type sequester_service_type NOT NULL;
+ALTER TABLE sequester_service ADD service_type sequester_service_type;
+UPDATE sequester_service SET service_type = 'STORAGE' WHERE service_type is NULL;
+ALTER TABLE sequester_service ALTER COLUMN service_type SET NOT NULL
