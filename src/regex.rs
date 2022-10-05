7c837993c41003fd865f170b4b8861ac1eb7883b
Pascal <pascal.chen@devling.xyz>
2022-10-05 16:18:55 +0200

    Enhance Regex

--- src/regex.rs
+++ src/regex.rs
@@ -1,53 +1,31 @@
-use std::fmt::Display;
-
 // Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 (eventually AGPL-3.0) 2016-present Scille SAS
-use pyo3::{exceptions::PyValueError, pyclass, pymethods, types::PyType, PyResult};
 
-use crate::binding_utils::gen_proto;
+use pyo3::{exceptions::PyValueError, pyclass, pymethods, types::PyType, PyResult};
 
 #[pyclass]
 pub(crate) struct Regex(pub libparsec::types::regex::Regex);
+
+crate::binding_utils::gen_proto!(Regex, __richcmp__, eq);
+crate::binding_utils::gen_proto!(Regex, __str__);
 
 #[pymethods]
 impl Regex {
     #[classmethod]
     fn from_pattern(_cls: &PyType, pattern: &str) -> PyResult<Self> {
-        if let Ok(reg) = libparsec::types::regex::Regex::from_pattern(pattern) {
-            Ok(Regex(reg))
-        } else {
-            Err(PyValueError::new_err(format!(
-                "Invalid pattern `{}`",
-                pattern
-            )))
-        }
+        libparsec::types::regex::Regex::from_pattern(pattern)
+            .map(Regex)
+            .map_err(|_| PyValueError::new_err(format!("Invalid pattern `{}`", pattern)))
     }
 
     #[classmethod]
     fn from_regex_str(_cls: &PyType, regex_str: &str) -> PyResult<Self> {
-        if let Ok(reg) = libparsec::types::regex::Regex::from_regex_str(regex_str) {
-            Ok(Regex(reg))
-        } else {
-            Err(PyValueError::new_err(format!(
-                "Invalid regex `{}`",
-                regex_str
-            )))
-        }
-    }
-
-    fn __str__(&self) -> String {
-        self.to_string()
+        libparsec::types::regex::Regex::from_regex_str(regex_str)
+            .map(Regex)
+            .map_err(|_| PyValueError::new_err(format!("Invalid regex `{}`", regex_str)))
     }
 
     #[getter]
-    fn pattern(&self) -> String {
-        self.to_string()
+    fn pattern(&self) -> PyResult<&str> {
+        Ok(self.0.as_ref())
     }
 }
-
-impl Display for Regex {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.write_str(&self.0.to_string())
-    }
-}
-
-gen_proto!(Regex, __richcmp__, eq);
