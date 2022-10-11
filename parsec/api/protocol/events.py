02a8ad6ff38aac5f53645e2ad09c605b8f7d80de
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2022-10-11 13:36:38 +0200

    Remove `parsec.api.protocol.APIEvent` (replaced by oxidation `EventsSubscribeRep`)

--- parsec/api/protocol/events.py
+++ parsec/api/protocol/events.py
@@ -1,23 +1,10 @@
 # Parsec Cloud (https://parsec.cloud) Copyright (c) AGPL-3.0 2016-present Scille SAS
 
-from enum import Enum
-
 from parsec._parsec import EventsListenRep, EventsListenReq, EventsSubscribeRep, EventsSubscribeReq
 from parsec.api.protocol.base import ApiCommandSerializer
 
 
-__all__ = ("APIEvent", "events_listen_serializer", "events_subscribe_serializer")
-
-
-class APIEvent(Enum):
-    PINGED = "pinged"
-    MESSAGE_RECEIVED = "message.received"
-    INVITE_STATUS_CHANGED = "invite.status_changed"
-    REALM_MAINTENANCE_FINISHED = "realm.maintenance_finished"
-    REALM_MAINTENANCE_STARTED = "realm.maintenance_started"
-    REALM_VLOBS_UPDATED = "realm.vlobs_updated"
-    REALM_ROLES_UPDATED = "realm.roles_updated"
-    PKI_ENROLLMENTS_UPDATED = "pki_enrollment.updated"
+__all__ = ("events_listen_serializer", "events_subscribe_serializer")
 
 
 events_listen_serializer = ApiCommandSerializer(EventsListenReq, EventsListenRep)
