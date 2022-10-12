3a36e1856551780328210e5b687747141418a40f
firelight flagboy <firelight.flagboy@gmail.com>
2022-10-12 09:59:36 +0200

    Add build script for crates that rely on json schema

---
+++ oxidation/libparsec/crates/client_types/build.rs
@@ -0,0 +1,10 @@
+// Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 (eventually AGPL-3.0) 2016-present Scille SAS
+
+use glob::glob;
+
+fn main() {
+    for entry in glob("schema/*.json").expect("Failed to parse glob pattern") {
+        let path = entry.expect("Glob iteration error");
+        println!("cargo:rerun-if-changed={}", path.display())
+    }
+}
