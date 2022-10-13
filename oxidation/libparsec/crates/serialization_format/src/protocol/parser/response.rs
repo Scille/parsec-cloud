4c186d811be864f9de81f31625426e8fb292758c
FirelightFlagboy <30697622+FirelightFlagboy@users.noreply.github.com>
2022-10-13 16:50:41 +0200

    Rust protocol scheme rework (#3227)
    * Update json schema for the new protocol format
    
    * Update protocol scheme format
    
    * Update `serialization_format` for the new format
    
    * Add new line for better error reporting
    
    Adding the extra newline when reading data from a file greatly improve the error message,
    Because it would actually contain the line from where the error come from instead of an imaginary columns

--- oxidation/libparsec/crates/serialization_format/src/protocol/parser/response.rs
+++ oxidation/libparsec/crates/serialization_format/src/protocol/parser/response.rs
@@ -4,126 +4,125 @@ use std::collections::HashMap;
 
 use serde::Deserialize;
 
-use crate::protocol::parser::field::{quote_fields, Field};
+use crate::protocol::parser::field::{quote_fields, Fields};
 use crate::protocol::utils::to_pascal_case;
 
-#[cfg_attr(test, derive(PartialEq, Eq))]
+/// A collection of [Response].
+/// Each keys correspond to a response type/status.
+pub type Responses = HashMap<String, Response>;
+
+#[cfg_attr(test, derive(PartialEq, Eq, Default))]
 #[derive(Debug, Deserialize, Clone)]
 pub struct Response {
-    pub status: String,
     #[serde(default)]
     pub unit: Option<String>,
     #[serde(default)]
-    pub other_fields: Vec<Field>,
-}
-
-#[cfg(test)]
-impl Default for Response {
-    fn default() -> Self {
-        Self {
-            status: "foo_response".to_string(),
-            unit: None,
-            other_fields: vec![],
-        }
-    }
+    pub fields: Fields,
 }
 
 impl Response {
-    pub fn quote(&self, types: &HashMap<String, String>) -> syn::Variant {
+    pub fn quote(&self, name: &str, types: &HashMap<String, String>) -> syn::Variant {
         if let Some(unit) = &self.unit {
-            self.quote_unit(unit)
-        } else if self.other_fields.is_empty() {
-            self.quote_empty()
+            self.quote_unit(name, unit)
+        } else if self.fields.is_empty() {
+            self.quote_empty(name)
         } else {
-            self.quote_fields(types)
+            self.quote_fields(name, types)
         }
     }
 
-    fn quote_unit(&self, unit: &str) -> syn::Variant {
-        let name = self.quote_name();
-        let rename = &self.status;
+    fn quote_unit(&self, name: &str, unit: &str) -> syn::Variant {
+        let name_ident = self.quote_name(name);
+        let rename = name;
         let unit = syn::parse_str::<syn::Type>(unit).expect("A valid unit");
 
         syn::parse_quote! {
             #[serde(rename = #rename)]
-            #name(#unit)
+            #name_ident(#unit)
         }
     }
 
-    fn quote_empty(&self) -> syn::Variant {
-        let name = self.quote_name();
-        let rename = &self.status;
+    fn quote_empty(&self, name: &str) -> syn::Variant {
+        let name_ident = self.quote_name(name);
+        let rename = name;
 
         syn::parse_quote! {
             #[serde(rename = #rename)]
-            #name
+            #name_ident
         }
     }
 
-    fn quote_fields(&self, types: &HashMap<String, String>) -> syn::Variant {
-        let name = self.quote_name();
-        let rename = &self.status;
-        let fields = quote_fields(&self.other_fields, Some(syn::Visibility::Inherited), types);
+    fn quote_fields(&self, name: &str, types: &HashMap<String, String>) -> syn::Variant {
+        let name_ident = self.quote_name(name);
+        let rename = name;
+        let fields = quote_fields(&self.fields, Some(syn::Visibility::Inherited), types);
 
         syn::parse_quote! {
             #[serde(rename = #rename)]
-            #name {
+            #name_ident {
                 #(#fields),*
             }
         }
     }
 
-    fn quote_name(&self) -> syn::Ident {
-        syn::parse_str(&to_pascal_case(&self.status)).expect("A valid status")
+    fn quote_name(&self, name: &str) -> syn::Ident {
+        syn::parse_str(&to_pascal_case(name)).expect("A valid status")
     }
 }
 
 #[cfg(test)]
-#[rstest::rstest]
-#[case::empty(
-    Response::default(),
-    quote::quote! {
-        #[serde(rename = "foo_response")]
-        FooResponse
-    }
-)]
-#[case::with_unit(
-    Response {
-        unit: Some("String".to_string()),
-        ..Default::default()
-    },
-    quote::quote! {
-        #[serde(rename = "foo_response")]
-        FooResponse(String)
-    }
-)]
-#[case::with_fields(
-    Response {
-        other_fields: vec![
-            Field::default(),
-            Field::default()
-        ],
-        ..Default::default()
-    },
-    quote::quote! {
-        #[serde(rename = "foo_response")]
-        FooResponse {
-            #[serde_as(as = "_")]
-            foo_type: String,
-            #[serde_as(as = "_")]
-            foo_type: String
-        }
-    }
-)]
-fn test_quote(#[case] request: Response, #[case] expected: proc_macro2::TokenStream) {
+mod test {
     use pretty_assertions::assert_eq;
-    use quote::ToTokens;
+    use quote::{quote, ToTokens};
+    use rstest::rstest;
 
-    assert_eq!(
-        request
-            .quote(&HashMap::new())
-            .into_token_stream()
-            .to_string(),
-        expected.to_string()
-    );
+    use super::{Fields, HashMap, Response};
+
+    use crate::protocol::parser::Field;
+
+    #[rstest]
+    #[case::empty(
+        Response::default(),
+        quote! {
+            #[serde(rename = "foo_response")]
+            FooResponse
+        }
+    )]
+    #[case::with_unit(
+        Response {
+            unit: Some("String".to_string()),
+            ..Default::default()
+        },
+        quote! {
+            #[serde(rename = "foo_response")]
+            FooResponse(String)
+        }
+    )]
+    #[case::with_fields(
+        Response {
+            fields: Fields::from([
+                ("foo".to_string(), Field::default()),
+                ("bar".to_string(), Field::default())
+            ]),
+            ..Default::default()
+        },
+        quote! {
+            #[serde(rename = "foo_response")]
+            FooResponse {
+                #[serde_as(as = "_")]
+                bar: String,
+                #[serde_as(as = "_")]
+                foo: String
+            }
+        }
+    )]
+    fn test_quote(#[case] request: Response, #[case] expected: proc_macro2::TokenStream) {
+        assert_eq!(
+            request
+                .quote("foo_response", &HashMap::new())
+                .into_token_stream()
+                .to_string(),
+            expected.to_string()
+        );
+    }
 }
