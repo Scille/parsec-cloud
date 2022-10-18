f32d180a9f4527a91fa1867346caa769edbfbab6
Corentin Machu <corentin.machu@protonmail.com>
2022-10-18 13:51:58 +0200

    mypy: Added stubs for `pypac`

---
+++ parsec/stubs/pypac/__init__.py
@@ -0,0 +1,17 @@
+# Parsec Cloud (https://parsec.cloud) Copyright (c) AGPL-3.0 2016-present Scille SAS
+
+from typing import Optional
+from pypac.api import PACFile
+
+import requests
+
+
+def get_pac(
+    url: Optional[str] = None,
+    js: Optional[str] = None,
+    from_dns: bool = True,
+    timeout: int = 2,
+    allowed_content_types: Optional[str] = None,
+    sessions: Optional[requests.Session] = None,
+) -> Optional[PACFile]:
+    ...
