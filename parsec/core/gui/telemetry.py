11ec3fc0f73ad634d5afc086895cc3e92588fa29
firelight flagboy <firelight.flagboy@gmail.com>
2022-10-26 10:13:13 +0200

    Add mypy typing for `core.gui.{systray,telemetry}`

--- parsec/core/gui/telemetry.py
+++ parsec/core/gui/telemetry.py
@@ -5,7 +5,7 @@ from parsec.logging import configure_sentry_logging, disable_sentry_logging
 from parsec.core.config import CoreConfig
 
 
-def init(config: CoreConfig):
+def init(config: CoreConfig) -> None:
     if config.telemetry_enabled and config.sentry_dsn:
         configure_sentry_logging(dsn=config.sentry_dsn, environment=config.sentry_environment)
     else:
