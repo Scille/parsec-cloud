1cacf9eb8be82da141880d52aead886149cd3d1b
firelight flagboy <firelight.flagboy@gmail.com>
2022-10-26 12:03:02 +0200

    Type `smartcard_authentication_widget`

--- parsec/core/gui/smartcard_authentication_widget.py
+++ parsec/core/gui/smartcard_authentication_widget.py
@@ -1,5 +1,6 @@
 # Parsec Cloud (https://parsec.cloud) Copyright (c) AGPL-3.0 2016-present Scille SAS
 from __future__ import annotations
+from typing import Literal
 
 from PyQt5.QtCore import pyqtSignal
 from PyQt5.QtWidgets import QWidget
@@ -10,9 +11,9 @@ from parsec.core.gui.ui.smartcard_authentication_widget import Ui_SmartcardAuthe
 class SmartCardAuthenticationWidget(QWidget, Ui_SmartcardAuthenticationWidget):
     authentication_state_changed = pyqtSignal(bool)
 
-    def __init__(self, parent=None):
+    def __init__(self, parent: QWidget | None = None) -> None:
         super().__init__(parent)
         self.setupUi(self)
 
-    def is_auth_valid(self):
+    def is_auth_valid(self) -> Literal[True]:
         return True
