cd07155862e074719529d0bb0f31deaf2c31a363
Corentin Machu <corentin.machu@protonmail.com>
2022-10-26 13:38:49 +0200

    mypy: Added mypy typing for `babel_qt_extractor`

--- parsec/core/gui/babel_qt_extractor.py
+++ parsec/core/gui/babel_qt_extractor.py
@@ -1,5 +1,6 @@
 # Parsec Cloud (https://parsec.cloud) Copyright (c) AGPL-3.0 2016-present Scille SAS
 from __future__ import annotations
+from typing import Iterable, Literal, Tuple, Optional
 
 import xml.etree.ElementTree as ET
 
@@ -7,17 +8,20 @@ import xml.etree.ElementTree as ET
 IGNORE_LIST = ["Dialog", "Form"]
 
 
-def get_location(elem):
+def get_location(elem: ET.Element) -> Tuple[str, int]:
     loc = elem.find("location")
+    assert loc is not None
     return loc.attrib["filename"], int(loc.attrib["line"])
 
 
-def get_translation_string(elem):
+def get_translation_string(elem: ET.Element) -> Optional[str]:
     tr = elem.find("source")
-    return tr.text
+    return tr.text if tr is not None else None
 
 
-def extract_qt(fileobj, keywords, comment_tags, options):
+def extract_qt(
+    fileobj: str, keywords: object, comment_tags: object, options: object
+) -> Iterable[Tuple[int, Literal[None], Optional[str], list[object]]]:
     tree = ET.parse(fileobj)
     root = tree.getroot()
     for message in root.iter("message"):
