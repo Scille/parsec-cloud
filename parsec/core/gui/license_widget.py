3979ef782772dd6eef847588c7b96906d71f3ac8
Corentin Machu <corentin.machu@protonmail.com>
2022-10-27 10:52:14 +0200

    mypy: Added mypy typing for `license_widget`

--- parsec/core/gui/license_widget.py
+++ parsec/core/gui/license_widget.py
@@ -1,5 +1,6 @@
 # Parsec Cloud (https://parsec.cloud) Copyright (c) AGPL-3.0 2016-present Scille SAS
 from __future__ import annotations
+from typing import Optional
 
 from PyQt5.QtWidgets import QWidget
 
@@ -8,7 +9,7 @@ from parsec.core.gui.ui.license_widget import Ui_LicenseWidget
 
 
 class LicenseWidget(QWidget, Ui_LicenseWidget):
-    def __init__(self, parent=None):
+    def __init__(self, parent: Optional[QWidget] = None) -> None:
         super().__init__(parent)
         self.setupUi(self)
         self.text_license.setHtml(_("TEXT_PARSEC_LICENSE"))
