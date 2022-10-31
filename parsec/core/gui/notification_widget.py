5f6f3d745cc6f5f58ec86e890ed3485c983d6cd2
TimeEngineer <chenpascal241096@yahoo.fr>
2022-10-31 12:02:05 +0100

    Add mypy for gui.notification

--- parsec/core/gui/notification_widget.py
+++ parsec/core/gui/notification_widget.py
@@ -1,11 +1,12 @@
 # Parsec Cloud (https://parsec.cloud) Copyright (c) AGPL-3.0 2016-present Scille SAS
 from __future__ import annotations
+from typing import Any
 
 from parsec._parsec import DateTime
 
 from PyQt5.QtCore import pyqtSignal
 from PyQt5.QtWidgets import QWidget, QStyle, QStyleOption
-from PyQt5.QtGui import QPixmap, QPainter
+from PyQt5.QtGui import QPixmap, QPainter, QPaintEvent
 
 from parsec.core.gui.ui.notification_widget import Ui_NotificationWidget
 from parsec.core.gui.lang import format_datetime
@@ -14,7 +15,7 @@ from parsec.core.gui.lang import format_datetime
 class NotificationWidget(QWidget, Ui_NotificationWidget):
     close_clicked = pyqtSignal(QWidget)
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
         super().__init__(*args, **kwargs)
         self.setupUi(self)
         now = DateTime.now()
@@ -22,20 +23,20 @@ class NotificationWidget(QWidget, Ui_NotificationWidget):
         self.button_close.clicked.connect(self.emit_close_clicked)
 
     @property
-    def message(self):
+    def message(self) -> str:
         return self.label_message.text()
 
     @message.setter
-    def message(self, val):
+    def message(self, val: str) -> None:
         self.label_message.setText(val)
 
-    def emit_close_clicked(self):
+    def emit_close_clicked(self) -> None:
         self.close_clicked.emit(self)
 
-    def set_icon(self, icon_path):
+    def set_icon(self, icon_path: str) -> None:
         self.label_icon.setPixmap(QPixmap(icon_path))
 
-    def paintEvent(self, _):
+    def paintEvent(self, _: QPaintEvent) -> None:
         opt = QStyleOption()
         opt.initFrom(self)
         p = QPainter(self)
@@ -43,7 +44,7 @@ class NotificationWidget(QWidget, Ui_NotificationWidget):
 
 
 class ErrorNotificationWidget(NotificationWidget):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
         super().__init__(*args, **kwargs)
         self.label_message.setStyleSheet("color: rgb(218, 53, 69);")
         self.label_date.setStyleSheet("color: rgb(218, 53, 69);")
@@ -51,7 +52,7 @@ class ErrorNotificationWidget(NotificationWidget):
 
 
 class WarningNotificationWidget(NotificationWidget):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
         super().__init__(*args, **kwargs)
         self.label_message.setStyleSheet("color: rgb(254, 195, 7);")
         self.label_date.setStyleSheet("color: rgb(254, 195, 7);")
@@ -59,7 +60,7 @@ class WarningNotificationWidget(NotificationWidget):
 
 
 class InfoNotificationWidget(NotificationWidget):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
         super().__init__(*args, **kwargs)
         self.label_message.setStyleSheet("color: rgb(20, 160, 183);")
         self.label_date.setStyleSheet("color: rgb(20, 160, 183);")
