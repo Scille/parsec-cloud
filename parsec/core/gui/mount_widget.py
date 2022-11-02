0f62a1e6aab6ff32382debd874fc01e2129a6903
Corentin Machu <corentin.machu@protonmail.com>
2022-11-02 10:22:14 +0100

    mypy: Add mypy typing to `mount_widget`

--- parsec/core/gui/mount_widget.py
+++ parsec/core/gui/mount_widget.py
@@ -1,17 +1,20 @@
 # Parsec Cloud (https://parsec.cloud) Copyright (c) AGPL-3.0 2016-present Scille SAS
 from __future__ import annotations
 
-from typing import Optional
+from typing import Any, Optional
 
 from PyQt5.QtCore import pyqtSignal
 from PyQt5.QtWidgets import QWidget
 
 from parsec._parsec import DateTime
-from parsec.core.fs import FsPath
+from parsec.core.fs import FsPath, WorkspaceFS
 
 from parsec.core.gui.files_widget import FilesWidget, Clipboard
+from parsec.core.gui.trio_jobs import QtToTrioJobScheduler
 from parsec.core.gui.workspaces_widget import WorkspacesWidget
 from parsec.core.gui.ui.mount_widget import Ui_MountWidget
+from parsec.core.logged_core import LoggedCore
+from parsec.event_bus import EventBus
 
 
 class MountWidget(QWidget, Ui_MountWidget):
@@ -20,14 +23,16 @@ class MountWidget(QWidget, Ui_MountWidget):
     # we can't set the type.
     folder_changed = pyqtSignal(object, str)
 
-    def __init__(self, core, jobs_ctx, event_bus, **kwargs):
+    def __init__(
+        self, core: LoggedCore, jobs_ctx: QtToTrioJobScheduler, event_bus: EventBus, **kwargs: Any
+    ):
         super().__init__(**kwargs)
         self.setupUi(self)
 
         self.core = core
         self.jobs_ctx = jobs_ctx
         self.event_bus = event_bus
-        self.global_clipboard = None
+        self.global_clipboard: Optional[Clipboard] = None
         self.workspaces_widget = WorkspacesWidget(
             self.core, self.jobs_ctx, self.event_bus, parent=self
         )
@@ -40,22 +45,24 @@ class MountWidget(QWidget, Ui_MountWidget):
         self.files_widget.back_clicked.connect(self.show_workspaces_widget)
         self.show_workspaces_widget()
 
-    def load_workspace(self, workspace_fs, default_path, select=False):
+    def load_workspace(
+        self, workspace_fs: WorkspaceFS, default_path: FsPath, select: bool = False
+    ) -> None:
         self.show_files_widget(workspace_fs, default_path, select)
 
-    def load_path(self, path):
+    def load_path(self, path: FsPath) -> None:
         if not self.files_widget.isVisible():
             return
         self.files_widget.load(FsPath(str(path)))
 
     def show_files_widget(
         self,
-        workspace_fs,
-        default_path,
-        selected=False,
-        mount_it=False,
+        workspace_fs: WorkspaceFS,
+        default_path: FsPath,
+        selected: bool = False,
+        mount_it: bool = False,
         timestamp: Optional[DateTime] = None,
-    ):
+    ) -> None:
         self.workspaces_widget.hide()
 
         if mount_it and not self.workspaces_widget.is_workspace_mounted(
@@ -73,11 +80,11 @@ class MountWidget(QWidget, Ui_MountWidget):
         )
         self.files_widget.show()
 
-    def show_workspaces_widget(self):
+    def show_workspaces_widget(self) -> None:
         self.folder_changed.emit(None, None)
         self.files_widget.hide()
         self.workspaces_widget.show()
         self.workspaces_widget.reset()
 
-    def clipboard_updated(self, clipboard: Optional[Clipboard] = None):
+    def clipboard_updated(self, clipboard: Optional[Clipboard] = None) -> None:
         self.global_clipboard = clipboard
