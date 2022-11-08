65363ce697c4781d49d8c3cb5ea4a8f5df453c53
Corentin Machu <corentin.machu@protonmail.com>
2022-11-08 11:00:03 +0100

    mypy: Various windows fixes

--- parsec/core/mountpoint/manager.py
+++ parsec/core/mountpoint/manager.py
@@ -1,13 +1,13 @@
 # Parsec Cloud (https://parsec.cloud) Copyright (c) AGPL-3.0 2016-present Scille SAS
 from __future__ import annotations
-from pathlib import Path
+from pathlib import Path, PurePath
 
 import trio
 import logging
 import sys
 
 from structlog import get_logger
-from typing import Any, AsyncGenerator, Sequence, Optional, Callable, Dict, Tuple, cast
+from typing import Any, AsyncGenerator, Sequence, Optional, Callable, Union, cast
 from importlib import __import__ as import_function
 from subprocess import CalledProcessError
 from contextlib import _AsyncGeneratorContextManager, asynccontextmanager
@@ -35,7 +35,7 @@ from parsec.core.win_registry import cleanup_parsec_drive_icons
 
 RunnerType = Callable[
     [UserFS, WorkspaceFS, Path, dict[Any, Any], EventBus],
-    _AsyncGeneratorContextManager[Path],
+    _AsyncGeneratorContextManager[Union[Path, PurePath]],
 ]
 
 # Importing winfspy can take some time (about 0.4 seconds)
@@ -170,7 +170,7 @@ class MountpointManager:
         workspace_id = workspace_fs.workspace_id
         key = workspace_id, timestamp
 
-        async def curried_runner(task_status: TaskStatus[Path]) -> None:
+        async def curried_runner(task_status: TaskStatus[PurePath]) -> None:
             event_kwargs = {}
 
             try:
