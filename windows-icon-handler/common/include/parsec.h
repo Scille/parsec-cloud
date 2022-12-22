c98c41fa410bf9e75c8281fd051f19f27e8018d7
Corentin Machu <corentin.machu@protonmail.com>
2022-12-22 02:46:04 -0800

    Added parsec TRACE macro on debug builds

--- windows-icon-handler/common/include/parsec.h
+++ windows-icon-handler/common/include/parsec.h
@@ -21,6 +21,58 @@ namespace parsec
         NotSet
     };
 
+#ifdef _DEBUG
+    inline std::ostream& operator<<(std::ofstream& os, SyncState state)
+    {
+        switch (state)
+        {
+        case SyncState::Synced:
+            return os << "Synced";
+        case SyncState::Refresh:
+            return os << "Refresh";
+        case SyncState::NotSet:
+            return os << "NotSet";
+        }
+
+        // Unreachable
+        return os;
+    }
+
+    template<typename T>
+    concept Streamable = requires(std::ofstream& os, T a) {
+        { os << a } -> std::convertible_to<std::ostream&>;
+    };
+
+    void log_impl(std::ofstream& ofs)
+    {
+        ofs << '\n';
+    }
+
+    template<typename T, typename... Args> requires Streamable<T>
+    void log_impl(std::ofstream& ofs, T value, Args... args)
+    {
+       ofs << value << ' ';
+       log_impl(ofs, args...);
+    }
+
+    template<typename T, typename... Args>
+    void log(T x, Args... args)
+    {
+        // Replace username to debug:
+        std::ofstream ofs("C:\\Users\\MyUser\\parsec.log");
+
+        if (!ofs.good())
+            return;
+
+        log_impl(ofs, x, args...);
+        ofs.flush();
+    }
+
+#define TRACE(...) ::parsec::log(__FILE__, __LINE__, __VA_ARGS__)
+#else
+#define TRACE(...) /* nothing */
+#endif
+
     inline SyncState is_member_file(const std::wstring_view& pwsz_path)
     {
         constexpr std::array<wchar_t, 26> DRIVES = {
@@ -34,7 +86,10 @@ namespace parsec
         //// one of those, we can return FALSE immediately
         //// A is 0, Z is 25, our drive should be between 7 and 25 included
         if (drive_number == -1 || drive_number < 7 || PathIsRootW(pwsz_path.data()))
+        {
+            TRACE("Drive number is", drive_number);
             return SyncState::NotSet;
+        }
 
         // Parsec write a registry key to set the icon for its workspaces.
         // We check if the key is present for the current drive.
@@ -49,7 +104,10 @@ namespace parsec
         HKEY key = nullptr;
 
         if (RegOpenKeyEx(HKEY_CURRENT_USER, reg_key.get(), 0, KEY_READ, &key) != ERROR_SUCCESS)
+        {
+            TRACE("Call to `RegOpenKeyEx` failed, return `NotSet`");
             return SyncState::NotSet;
+        }
 
         auto entry_infos = std::ifstream(std::format(L"{}.__parsec_entry_info__", pwsz_path));
 
@@ -63,10 +121,23 @@ namespace parsec
         try
         {
             auto json_object = json::parse(buffer.str());
+
+            TRACE(
+                "Json parsing OK `is_sync` value",
+                json_object["need_sync"].get<bool>(),
+                "return value is",
+                json_object["need_sync"].get<bool>() ? SyncState::Refresh : SyncState::Synced
+            );
+
             return json_object["need_sync"].get<bool>() ? SyncState::Refresh : SyncState::Synced;
         }
+#ifdef _DEBUG
+        catch (json::exception& e)
+#else
         catch (json::exception&)
+#endif
         {
+            TRACE("Json parsing failed. Reason:", e.what());
             return SyncState::NotSet;
         }
     }
