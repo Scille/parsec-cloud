ea5cb2305ad09f97e608330be4d25b7a87556015
Pascal <pascal.chen@devling.xyz>
2022-12-30 16:27:27 +0100

    Add OrganizationConfig & OrganizationStats and move UsersPerProfileDetailItem

--- src/protocol/organization.rs
+++ src/protocol/organization.rs
@@ -9,46 +9,12 @@ use pyo3::{
 use libparsec::protocol::authenticated_cmds::v2::{organization_config, organization_stats};
 
 use crate::{
-    enumerate::UserProfile,
+    data::UsersPerProfileDetailItem,
     protocol::{
         error::{ProtocolError, ProtocolErrorFields, ProtocolResult},
         gen_rep, Reason,
     },
 };
-
-#[pyclass]
-#[derive(Clone)]
-pub(crate) struct UsersPerProfileDetailItem(pub organization_stats::UsersPerProfileDetailItem);
-
-crate::binding_utils::gen_proto!(UsersPerProfileDetailItem, __repr__);
-crate::binding_utils::gen_proto!(UsersPerProfileDetailItem, __richcmp__, eq);
-
-#[pymethods]
-impl UsersPerProfileDetailItem {
-    #[new]
-    fn new(profile: UserProfile, active: u64, revoked: u64) -> PyResult<Self> {
-        Ok(Self(organization_stats::UsersPerProfileDetailItem {
-            profile: profile.0,
-            active,
-            revoked,
-        }))
-    }
-
-    #[getter]
-    fn profile(&self) -> PyResult<&'static PyObject> {
-        Ok(UserProfile::from_profile(self.0.profile))
-    }
-
-    #[getter]
-    fn active(&self) -> PyResult<u64> {
-        Ok(self.0.active)
-    }
-
-    #[getter]
-    fn revoked(&self) -> PyResult<u64> {
-        Ok(self.0.revoked)
-    }
-}
 
 #[pyclass]
 #[derive(Clone)]
