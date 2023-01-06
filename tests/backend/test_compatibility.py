beb18e3b0164cdada7ea8ab4ec158332b5f16d22
AntoineDupre <dupre.antoine.nicolas@gmail.com>
2023-01-06 10:08:19 +0100

    Drop APIV2 on client side

--- tests/backend/test_compatibility.py
+++ tests/backend/test_compatibility.py
@@ -1,17 +1,9 @@
 # Parsec Cloud (https://parsec.cloud) Copyright (c) AGPL-3.0 2016-present Scille SAS
 from __future__ import annotations
 
-from parsec._parsec import ApiVersion, DateTime, RealmCreateRepBadTimestamp
-from parsec.api.protocol.base import packb, serializer_factory
-from parsec.api.protocol.handshake import (
-    ApiVersionField,
-    AuthenticatedClientHandshake,
-    HandshakeType,
-    ServerHandshake,
-    handshake_answer_serializer,
-    handshake_challenge_serializer,
-    handshake_result_serializer,
-)
+from parsec._parsec import DateTime, RealmCreateRepBadTimestamp
+from parsec.api.protocol.base import serializer_factory
+from parsec.api.protocol.handshake import ApiVersionField, handshake_challenge_serializer
 from parsec.serde import BaseSchema, fields
 from parsec.utils import BALLPARK_CLIENT_EARLY_OFFSET, BALLPARK_CLIENT_LATE_OFFSET
 
@@ -71,58 +63,3 @@ def test_handshake_challenge_schema_compatibility():
     # Backend API < 2.4 with newer clients
     data = older_handshake_challenge_serializer.dumps(old_data)
     assert handshake_challenge_serializer.loads(data) == {**compat_data, "client_timestamp": None}
-
-
-def test_handshake_challenge_schema_for_client_server_api_compatibility(
-    mallory, alice, monkeypatch
-):
-    ash = ServerHandshake()
-
-    bch = AuthenticatedClientHandshake(
-        mallory.organization_id, mallory.device_id, mallory.signing_key, mallory.root_verify_key
-    )
-
-    challenge = b"1234567890"
-
-    # Backend API >= 2.5 and Client API < 2.5
-    client_version = ApiVersion(2, 4)
-    backend_version = ApiVersion(2, 5)
-
-    answer = {
-        "handshake": "answer",
-        "type": HandshakeType.AUTHENTICATED.value,
-        "client_api_version": client_version,
-        "organization_id": alice.organization_id.str,
-        "device_id": alice.device_id.str,
-        "rvk": alice.root_verify_key.encode(),
-        "answer": alice.signing_key.sign(challenge),
-    }
-
-    ash.build_challenge_req()
-    ash.challenge = challenge
-
-    ash.process_answer_req(packb(answer))
-    result_req = ash.build_result_req(alice.verify_key)
-
-    result = handshake_result_serializer.loads(result_req)
-    assert result["result"] == "ok"
-
-    # Backend API < 2.5 and Client API >= 2.5
-    client_version = ApiVersion(2, 5)
-    backend_version = ApiVersion(2, 4)
-
-    req = {
-        "handshake": "challenge",
-        "challenge": challenge,
-        "supported_api_versions": [backend_version],
-        "backend_timestamp": DateTime.now(),
-        "ballpark_client_early_offset": BALLPARK_CLIENT_EARLY_OFFSET,
-        "ballpark_client_late_offset": BALLPARK_CLIENT_LATE_OFFSET,
-    }
-
-    monkeypatch.setattr(bch, "SUPPORTED_API_VERSIONS", [client_version])
-
-    answer_req = bch.process_challenge_req(packb(req))
-
-    answer = handshake_answer_serializer.loads(answer_req)
-    assert mallory.verify_key.verify(answer["answer"]) == challenge
