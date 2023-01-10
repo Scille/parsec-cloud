294e4feb89307aec90466b5ed1ddb3e210de11d0
Maxime GRANDCOLAS <maxime.grandcolas@gmail.com>
2023-01-10 13:43:18 +0100

    Adds an OrganizationCard component (#3882)
    * Adds an OrganizationCard component
    
    * Removed last login from OrganizationCard
    
    * Update OrganizationCard.vue
    
    Co-authored-by: NicoTuxx <nicolasstempert@gmail.com>

---
+++ oxidation/client/src/components/OrganizationCard.vue
@@ -0,0 +1,70 @@
+<!-- Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 (eventually AGPL-3.0) 2016-present Scille SAS -->
+
+<template>
+  <div class="organization-card">
+    <ion-grid>
+      <ion-row class="ion-align-items-center">
+        <ion-col size="auto">
+          <ion-avatar>
+            <span>{{ device.organization_id?.substring(0, 2) }}</span>
+          </ion-avatar>
+        </ion-col>
+        <ion-col size="auto" class="organization-info">
+          <p class="organization-label">
+            {{ device.organization_id }}
+          </p>
+          <p>
+            {{ device.human_handle }}
+          </p>
+        </ion-col>
+      </ion-row>
+    </ion-grid>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+import { IonGrid, IonCol, IonRow, IonAvatar } from '@ionic/vue';
+import { AvailableDevice } from '../plugins/libparsec/definitions';
+
+defineProps<{
+  device: AvailableDevice
+}>();
+
+</script>
+
+<style lang="scss" scoped>
+.organization-card {
+  background: #F9F9FB;
+  margin: 1em 1.5em;
+  ion-avatar {
+    background: white;
+    color: #0058cc;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 2em;
+    text-transform: uppercase;
+  }
+
+  .organization-info {
+    text-align: left;
+
+    p {
+      margin: 0px;
+      margin-bottom: 2px;
+    }
+
+    .organization-label {
+      color: #004299;
+      font-size: 1.5em;
+    }
+  }
+}
+
+.organization-card-hover:hover {
+  background: #E5F1FF;
+  cursor: pointer;
+}
+
+</style>
