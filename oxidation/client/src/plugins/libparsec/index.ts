869dd8531f55a52936b395f06b4ec4ecae93867a
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2023-01-10 17:22:20 +0100

    Fix web vs native packaging for libparsec in client

--- oxidation/client/src/plugins/libparsec/index.ts
+++ oxidation/client/src/plugins/libparsec/index.ts
@@ -3,21 +3,27 @@
 import { registerPlugin } from '@capacitor/core';
 
 import type { LibParsecPlugin } from './definitions';
-export type { LibParsecPlugin, Result } from './definitions';
+import { LoadWebLibParsecPlugin } from './trampoline';
 
 export const libparsec = registerPlugin<LibParsecPlugin>(
   'LibParsec',
   {
-    web: async () => {
-      // Don't put the module name as a literal in the import function,
-      // otherwise the compilation will eagerly try to access this module
-      // which doesn't exists when building from scratch for non-web.
-      const moduleName = '../../../pkg';
-      const libparsecWasm: any = await import(moduleName);
-      await libparsecWasm.default();  // Call wasm module's init
-      return libparsecWasm;
-    },
-    electron: () => (window as any).libparsec_plugin
+    electron: () => (window as any).libparsec_plugin,
+
+    // In Web we have the whole libparsec compiled in Wasm and provided as a module
+    // that we can import just like regular js.
+    // This is cool but has an unexpected side effect: Webpack is going to package
+    // this module event if we are not building the application for web.
+    // Worst: if we are building from a clean repo, the Wasm module doesn't exist
+    // at all and the packaging fails :'(
+    //
+    // To avoid this we have to make sure the code that contain the import of the
+    // Wasm module is only present when building the application for web.
+    // We do this by extracting the `LoadWebLibParsecPlugin` into a `trampoline.ts`, this
+    // file having two implementation (the real one when building for the web and a dummy
+    // one otherwise) that are selected when `vue-cli-service` is called (see `vue.config.js`).
+    web: LoadWebLibParsecPlugin
+
     // In Android, capacitor already knows about the plugin given it has been registered from Java
   }
 );
