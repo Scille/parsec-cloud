73ad70b357ad8e1686f8a7b4865898344a9c28f5
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2023-01-16 08:35:11 +0100

    Remove useless `platform_native/web` crates, mock `android bindings` (not implemented for the moment) (#3908)

--- oxidation/libparsec/src/lib.rs
+++ oxidation/libparsec/src/lib.rs
@@ -1,12 +1,6 @@
 // Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 (eventually AGPL-3.0) 2016-present Scille SAS
 
 #[cfg(not(target_arch = "wasm32"))]
-pub use libparsec_platform_native::{create_context, RuntimeContext};
-
-#[cfg(target_arch = "wasm32")]
-pub use libparsec_platform_web::{create_context, RuntimeContext};
-
-#[cfg(not(target_arch = "wasm32"))]
 pub use libparsec_client_types as client_types;
 #[cfg(not(target_arch = "wasm32"))]
 pub use libparsec_core as core;
