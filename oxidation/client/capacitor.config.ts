cfb0ffa8f94584f88c59e36ceae82d3ce7d392ed
NicoTuxx <nicolasstempert@gmail.com>
2023-01-24 15:41:58 +0100

    improve electron release config
    appname, app hover text and hide menubar

--- oxidation/client/capacitor.config.ts
+++ oxidation/client/capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.scille.parsec',
-  appName: 'client',
+  appName: 'Parsec',
   webDir: 'dist',
   bundledWebRuntime: false,
   plugins: {
