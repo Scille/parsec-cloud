eb1042c9ef9c6440b6cb3fae239fb48f99e646e7
Vincent Michel <vxgmichel@gmail.com>
2023-01-30 13:03:17 +0100

    [GUI] Improve UX for offline availability dialog

--- parsec/core/gui/remanence_management_widget.py
+++ parsec/core/gui/remanence_management_widget.py
@@ -13,6 +13,7 @@ from parsec.core.gui.custom_dialogs import GreyedDialog, show_error
 from parsec.core.gui.file_size import get_filesize
 from parsec.core.gui.lang import translate as T
 from parsec.core.gui.snackbar_widget import SnackbarManager
+from parsec.core.gui.switch_button import SwitchButton
 from parsec.core.gui.trio_jobs import QtToTrioJob, QtToTrioJobScheduler
 from parsec.core.gui.ui.remanence_management_widget import Ui_RemanenceManagementWidget
 from parsec.core.logged_core import LoggedCore
@@ -27,30 +28,19 @@ class RemanenceManagementWidget(QWidget, Ui_RemanenceManagementWidget):
         self.jobs_ctx = jobs_ctx
         self.workspace_fs = workspace_fs
         self.dialog: GreyedDialog[RemanenceManagementWidget]
+        self._download_started: bool = False
+        self.switch_button = SwitchButton()
+        self.switch_layout.insertWidget(1, self.switch_button)
 
         # Connect signals
+        self.switch_button.clicked.connect(self._on_switch_clicked)
         self.button_close_dialog.clicked.connect(self._on_close_clicked)
-        self.enable_block_remanence_button.clicked.connect(self._on_enable_block_remanence_clicked)
-        self.disable_block_remanence_button.clicked.connect(
-            self._on_disable_block_remanence_clicked
-        )
-
-        # Hide widgets
-        self.label_blocks.hide()
-        self.progress_bar.hide()
-        self.enable_block_remanence_button.hide()
-        self.disable_block_remanence_button.hide()
-        self.label_warning.hide()
 
         # Set waiting state
-        self._show_waiting_widgets()
+        self._update_text()
+        self._update_switch_button(disabled=True)
         self.wait_prepared_job: QtToTrioJob[None] = self.jobs_ctx.submit_job(
             None, None, self._wait_prepared
-        )
-
-        # Update style for close button
-        self.setStyleSheet(
-            "#button_close_dialog {background-color: darkgrey;} #button_close_dialog:hover {background-color: grey;}"
         )
 
     @contextmanager
@@ -63,28 +53,13 @@ class RemanenceManagementWidget(QWidget, Ui_RemanenceManagementWidget):
             self.dialog.accept()
             return
 
-    def _show_waiting_widgets(self) -> None:
-        self.waiting_widget.show()
-        self.waiting_label.show()
-        self.waiting_label.setText(T("TEXT_REMANENCE_WAITING"))
-
-    def _hide_waiting_widget(self) -> None:
-        self.waiting_widget.hide()
-        self.waiting_label.hide()
-
-    def _update_buttons(self) -> None:
-        self.enable_block_remanence_button.setEnabled(True)
-        self.disable_block_remanence_button.setEnabled(True)
-        if self.workspace_fs.is_block_remanent():
-            self.enable_block_remanence_button.hide()
-            self.disable_block_remanence_button.show()
-        else:
-            self.enable_block_remanence_button.show()
-            self.disable_block_remanence_button.hide()
+    def _update_switch_button(self, disabled: bool = False) -> None:
+        self.switch_button.setDisabled(disabled)
+        self.switch_button.setChecked(self.workspace_fs.is_block_remanent())
 
     def _update_text(self) -> None:
         # Do not update while we're performing an operation
-        if self.waiting_widget.isVisible():
+        if not self.switch_button.isEnabled():
             return
         info = self.workspace_fs.get_remanence_manager_info()
         # Label info label
@@ -95,15 +70,8 @@ class RemanenceManagementWidget(QWidget, Ui_RemanenceManagementWidget):
             self.label_info.setText(
                 T("TEXT_BLOCK_REMANENCE_DISABLED_cache_size").format(cache_size=cache_size)
             )
-        # Lable size label
-        self.label_size.setText(
-            T("TEXT_WORKSPACE_SIZE_local_size_total_size").format(
-                local_size=get_filesize(info.local_and_remote_size),
-                total_size=get_filesize(info.total_size),
-            )
-        )
         # Warning label
-        if not info.is_block_remanent and info.remote_only_size > 0:
+        if not info.is_block_remanent and info.is_prepared and info.remote_only_size > 0:
             self.label_warning.setText(
                 T("TEXT_BLOCK_REMANENCE_WARNING_remote_size").format(
                     remote_size=get_filesize(info.remote_only_size)
@@ -112,86 +80,101 @@ class RemanenceManagementWidget(QWidget, Ui_RemanenceManagementWidget):
             self.label_warning.show()
         else:
             self.label_warning.hide()
-
-    def _update_progress_bar(self) -> None:
+        # Close button
+        if info.is_block_remanent and info.is_prepared and info.remote_only_size > 0:
+            self.button_close_dialog.setText(T("ACTION_CONTINUE_IN_BACKGROUND"))
+        else:
+            self.button_close_dialog.setText(T("ACTION_CLOSE"))
         # Progress bar
-        info = self.workspace_fs.get_remanence_manager_info()
-        self.progress_bar.setMinimum(0)
-        self.progress_bar.setMaximum(info.total_size)
-        self.progress_bar.setValue(info.local_and_remote_size)
+        self.progress_bar.show()
+        self.progress_bar.setEnabled(info.is_block_remanent)
+        if not info.is_prepared:
+            self.progress_bar.setMinimum(0)
+            self.progress_bar.setMaximum(0)
+            self.progress_bar.setValue(0)
+        elif info.total_size == 0:
+            self.progress_bar.setFormat(T("TEXT_WORKSPACE_IS_EMPTY"))
+            self.progress_bar.setMinimum(0)
+            self.progress_bar.setMaximum(1)
+            self.progress_bar.setValue(1)
+        else:
+            title = (
+                T("TEXT_REMANENCE_PROGRESS_BAR_TITLE_DOWNLOADING")
+                if info.is_block_remanent and info.remote_only_size
+                else T("TEXT_REMANENCE_PROGRESS_BAR_TITLE_IDLE")
+            )
+            format_value = "{} - {} / {} (%p%)".format(
+                title, get_filesize(info.local_and_remote_size), get_filesize(info.total_size)
+            )
+            self.progress_bar.setFormat(format_value)
+            self.progress_bar.setMinimum(0)
+            self.progress_bar.setMaximum(info.total_size)
+            self.progress_bar.setValue(info.local_and_remote_size)
+        # Congratulate when a download is finished
+        if (
+            self._download_started
+            and info.is_prepared
+            and info.total_size > 0
+            and info.remote_only_size == 0
+        ):
+            self._download_started = False
+            SnackbarManager.congratulate(T("TEXT_BLOCK_DOWNLOADING_FINISHED"))
 
     async def _wait_prepared(self) -> None:
         with self._handle_error(T("TEXT_GET_REMANENCE_STATUS_FAILED")):
             await self.workspace_fs.remanence_manager.wait_prepared()
-            # Done waiting
-            self._hide_waiting_widget()
+            # The dialog has been closed
+            if not self.dialog.isVisible():
+                return
             # Update content
             self._update_text()
-            self._update_buttons()
+            self._update_switch_button()
             # Update text periodically
             while True:
                 # The dialog has been closed
                 if not self.dialog.isVisible():
                     return
-                # Update 1 time per seconds
+                # Refresh content
                 self._update_text()
-                await trio.sleep(1)
+                # Update 2 times per seconds
+                await trio.sleep(0.5)
 
-    async def _on_enable_block_remanence_clicked(self, *_: object) -> None:
+    async def _on_switch_clicked(self, state: bool) -> None:
+        if state:
+            await self._enable_block_remanence()
+        else:
+            await self._disable_block_remanence()
+
+    async def _enable_block_remanence(self) -> None:
         # Close the dialog when an error occurs
         with self._handle_error(T("TEXT_ENABLE_BLOCK_REMANENCE_FAILED")):
-            # Disable the corresponding button
-            self.enable_block_remanence_button.setDisabled(True)
+            # Update widgets
+            self.switch_button.setDisabled(True)
             self.label_warning.hide()
-            # Show waiting state
-            self._show_waiting_widgets()
-            # Actually enable block remanence
+            # Enable block remanence
             await self.workspace_fs.enable_block_remanence()
-            # Hide waiting state
-            self._hide_waiting_widget()
             # Update content
-            self._update_buttons()
+            self._update_switch_button()
             self._update_text()
-            # No data to download
-            if not self.workspace_fs.get_remanence_manager_info().remote_only_size:
+            info = self.workspace_fs.get_remanence_manager_info()
+            if not info.is_prepared or info.remote_only_size == 0:
+                # No data to download
                 SnackbarManager.congratulate(T("TEXT_ENABLE_BLOCK_REMANENCE_SUCCESS"))
-                return
-            # Some data needs to be downloaded
-            SnackbarManager.inform(T("TEXT_BLOCK_DOWNLOADING_STARTED"))
-            # Show progress bar
-            self.progress_bar.show()
-            # Loop while there's more data to download
-            while self.workspace_fs.get_remanence_manager_info().remote_only_size:
-                # Update content
-                self._update_progress_bar()
-                # Workspace is no longer remanent
-                if not self.workspace_fs.is_block_remanent():
-                    self.progress_bar.hide()
-                    return
-                # The dialog has been closed
-                if not self.dialog.isVisible():
-                    return
-                # Update 20 times per seconds
-                await trio.sleep(0.05)
-            # Hide the progress bar
-            self.progress_bar.hide()
-            self._update_text()
-            # Send notification
-            SnackbarManager.congratulate(T("TEXT_BLOCK_DOWNLOADING_FINISHED"))
+            else:
+                # Some data needs to be downloaded
+                SnackbarManager.inform(T("TEXT_BLOCK_DOWNLOADING_STARTED"))
+                self._download_started = True
 
-    async def _on_disable_block_remanence_clicked(self, *_: object) -> None:
+    async def _disable_block_remanence(self, *_: object) -> None:
         # Close the dialog when a error occurs
         with self._handle_error(T("TEXT_DISABLE_BLOCK_REMANENCE_FAILED")):
-            # Disable the corresponding button
-            self.disable_block_remanence_button.setDisabled(True)
-            self.progress_bar.hide()
-            # Show waiting widgets while disabling block remanance
-            self._show_waiting_widgets()
+            # Update widgets
+            self.switch_button.setDisabled(True)
+            # Disable block remanance
             await self.workspace_fs.disable_block_remanence()
-            self._hide_waiting_widget()
             # Update content
+            self._update_switch_button()
             self._update_text()
-            self._update_buttons()
 
     def _on_close_clicked(self) -> None:
         self.dialog.accept()
@@ -212,6 +195,7 @@ class RemanenceManagementWidget(QWidget, Ui_RemanenceManagementWidget):
             parent=parent,
             width=1000,
             close_on_click=False,
+            hide_close=True,
         )
         widget.dialog = dialog
 
