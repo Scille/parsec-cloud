874835fb31527f3a8700f5c11f90f22859fed31b
TimeEngineer <chenpascal241096@yahoo.fr>
2023-02-13 19:55:49 +0100

    Remove reqwest dep for StatusCode, because we use custom code

--- src/backend_connection/error.rs
+++ src/backend_connection/error.rs
@@ -1,7 +1,6 @@
 // Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 (eventually AGPL-3.0) 2016-present Scille SAS
 
 use pyo3::{exceptions::PyException, PyErr};
-use reqwest::StatusCode;
 
 pyo3::create_exception!(_parsec, BackendConnectionError, PyException);
 pyo3::create_exception!(_parsec, BackendProtocolError, BackendConnectionError);
@@ -32,12 +31,12 @@ impl From<CommandExc> for PyErr {
                 BackendNotAvailable::new_err(err.0.to_string())
             }
             libparsec::client_connection::CommandError::InvalidResponseStatus(status, ..)
-                if status == StatusCode::INTERNAL_SERVER_ERROR =>
+                if status == 500 =>
             {
                 BackendNotAvailable::new_err(err.0.to_string())
             }
             libparsec::client_connection::CommandError::InvalidResponseStatus(status, ..)
-                if (status == StatusCode::UNAUTHORIZED || status == StatusCode::NOT_FOUND) =>
+                if (status == 401 || status == 404) =>
             {
                 BackendConnectionRefused::new_err("Invalid handshake information")
             }
