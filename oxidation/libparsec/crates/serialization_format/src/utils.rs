82aab92b9039f00c89fcd8d4414f9363305c2013
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2023-02-15 13:20:30 +0100

    Improve snake_to_pascal_case in serialization_format's utils

--- oxidation/libparsec/crates/serialization_format/src/utils.rs
+++ oxidation/libparsec/crates/serialization_format/src/utils.rs
@@ -5,20 +5,46 @@ use std::collections::HashMap;
 use syn::{GenericArgument, PathArguments, Type};
 
 pub(crate) fn snake_to_pascal_case(s: &str) -> String {
-    let mut out = s[..1].to_uppercase();
-    let mut chars = s.chars().skip(1);
-    while let Some(c) = chars.next() {
-        if c == '_' {
-            match chars.next().unwrap_or_else(|| unreachable!()) {
-                c @ 'a'..='z' => out.push((c as u8 - b'a' + b'A') as char),
-                c => out.push(c),
+    let mut out = String::with_capacity(s.len());
+    // Start with capitalization
+    let mut capitalize_next_char = true;
+    for c in s.chars() {
+        match (capitalize_next_char, c) {
+            (_, '_') => {
+                capitalize_next_char = true;
             }
-        } else {
-            out.push(c);
+            (true, c @ 'a'..='z') => {
+                out.push((c as u8 - b'a' + b'A') as char);
+                capitalize_next_char = false;
+            }
+            (_, c) => {
+                out.push(c);
+                capitalize_next_char = false;
+            }
         }
     }
-
+    // Weird corner case: `s` was only composed of underscores
+    if out.is_empty() {
+        out.push('_');
+    }
     out
+}
+
+#[cfg(test)]
+#[test]
+fn test_snake_to_pascal_case() {
+    assert_eq!(snake_to_pascal_case("a"), "A");
+    assert_eq!(snake_to_pascal_case("#a!"), "#a!");
+    assert_eq!(snake_to_pascal_case("a_!b"), "A!b");
+    assert_eq!(snake_to_pascal_case("aa"), "Aa");
+    assert_eq!(snake_to_pascal_case("a_b"), "AB");
+    assert_eq!(snake_to_pascal_case("aa_bb_cCc"), "AaBbCCc");
+    assert_eq!(snake_to_pascal_case("_a"), "A");
+    assert_eq!(snake_to_pascal_case("_1_2b_3C"), "12b3C");
+    assert_eq!(snake_to_pascal_case("_"), "_");
+    assert_eq!(snake_to_pascal_case("___"), "_");
+    assert_eq!(snake_to_pascal_case("__a__b__"), "AB");
+    assert_eq!(snake_to_pascal_case("a_b_"), "AB");
 }
 
 pub(crate) fn inspect_type(ty: &str, types: &HashMap<String, String>) -> String {
