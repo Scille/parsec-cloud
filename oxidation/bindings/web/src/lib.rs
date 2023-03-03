ff7a81a23c566fdd12405c9253353b07f095c2e3
Maxime GRANDCOLAS <maxime.grandcolas@gmail.com>
2023-03-03 10:53:05 +0100

    Added console logger initialization to js binding

--- oxidation/bindings/web/src/lib.rs
+++ oxidation/bindings/web/src/lib.rs
@@ -1,5 +1,14 @@
 // Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 (eventually AGPL-3.0) 2016-present Scille SAS
 
 #![cfg(target_arch = "wasm32")]
+
+#[allow(unused_imports)]
+use wasm_bindgen::prelude::*;
+
 mod meths;
 pub use meths::*;
+
+#[wasm_bindgen]
+pub fn init_logger() {
+    console_log::init_with_level(log::Level::Info).expect("cannot initialize console logger");
+}
