ccd17d26fde179dae8a0617a8eae0d56bb2f416a
NicoTuxx <nicolasstempert@gmail.com>
2023-04-17 17:36:46 +0200

    fix icon 'name' attribute problem

--- oxidation/client/src/views/SidebarMenu.vue
+++ oxidation/client/src/views/SidebarMenu.vue
@@ -3,7 +3,10 @@
     <ion-split-pane
       content-id="main"
     >
-      <ion-menu content-id="main" class="sidebar-menu">
+      <ion-menu
+        content-id="main"
+        class="sidebar-menu"
+      >
         <ion-header>
           <!-- active oragnization -->
           <ion-card class="menu-orga-card">
@@ -24,19 +27,24 @@
                 </div>
               </div>
               <!-- new icon to provide -->
-              <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <svg
+                width="32"
+                height="32"
+                viewBox="0 0 32 32"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
                 <path d="M10.3495 20.4231L15.6049 26.795C15.6896 26.8977 15.7947 26.9801 15.9129 27.0366C16.0311 27.0931 16.1597 27.1223 16.2899 27.1223C16.42 27.1223 16.5486 27.0931 16.6669 27.0366C16.7851 26.9801 16.8902 26.8977 16.9749 26.795L22.2303 20.4231C22.7319 19.8149 22.316 18.8755 21.5453 18.8755L11.033 18.8755C10.2622 18.8755 9.84641 19.8149 10.3495 20.4231Z" fill="#F9F9FB"/>
                 <path d="M22.2326 13.4558L16.9772 7.08389C16.8925 6.98124 16.7874 6.89884 16.6691 6.84234C16.5509 6.78585 16.4223 6.7566 16.2921 6.7566C16.162 6.7566 16.0334 6.78585 15.9151 6.84234C15.7969 6.89884 15.6918 6.98124 15.6071 7.08389L10.3517 13.4558C9.85015 14.064 10.266 15.0034 11.0367 15.0034L21.549 15.0034C22.3198 15.0034 22.7356 14.064 22.2326 13.4558Z" fill="#F9F9FB"/>
               </svg>
-
             </ion-card-header>
 
             <div class="orga-manage-btn">
               <ion-icon
-                  name="logo-ionic"
-                  :icon="cog"
-                  slot="start"
-                  size="small" />
+                :icon="cog"
+                slot="start"
+                size="small"
+              />
               <ion-text
                 class="subtitles-sm"
                 button
@@ -44,10 +52,6 @@
               >
                 Gérer mon organisation
               </ion-text>
-              <ion-icon
-                :icon="business"
-                slot="start"
-              />
             </div>
           </ion-card>
           <!-- end of active organzation -->
@@ -68,7 +72,8 @@
               lines="none"
               button
               @click="navigateToPage('workspaces')"
-              v-for="workspace in workspacesExampleData" :key="workspace.id"
+              v-for="workspace in workspacesExampleData"
+              :key="workspace.id"
             >
               <ion-icon
                 :icon="business"
@@ -79,12 +84,10 @@
           </ion-list>
           <!-- list of workspaces -->
         </ion-content>
-
-
       </ion-menu>
 
-    <!-- doit-on séparer les deux views ? -->
-    <!-- <div class="ion-page" id="main">
+      <!-- doit-on séparer les deux views ? -->
+      <!-- <div class="ion-page" id="main">
       <ion-header>
         <ion-toolbar>
           <ion-title>Main View</ion-title>
@@ -136,7 +139,7 @@ import {
   IonAvatar,
   IonItem,
   IonRouterOutlet,
-  menuController,
+  menuController
 } from '@ionic/vue';
 import {
   business,
@@ -145,9 +148,8 @@ import {
 import { onMounted } from 'vue';
 import { useI18n } from 'vue-i18n';
 
-import { useRouter, useRoute } from 'vue-router';
+import { useRouter, useRoute } from 'vue-router';
 import { AvailableDevice } from '../plugins/libparsec/definitions';
-
 
 // const props =defineProps<{
 //   device: AvailableDevice
@@ -157,30 +159,30 @@ let device: any;
 
 // fake data
 const workspacesExampleData = [
-{
-'id': 1234,
-'name': 'Product Design',
-'userRole': 'Owner',
-'userCount': 3
-},
-{
-'id': 2345,
-'name': 'Marketing',
-'userRole': 'Contributor',
-'userCount': 1
-},
-{
-'id': 3456,
-'name': 'Engineering',
-'userRole': 'Contributor',
-'userCount': 4
-},
-{
-'id': 4567,
-'name': 'Research',
-'userRole': 'Reader',
-'userCount': 3
-}
+  {
+    id: 1234,
+    name: 'Product Design',
+    userRole: 'Owner',
+    userCount: 3
+  },
+  {
+    id: 2345,
+    name: 'Marketing',
+    userRole: 'Contributor',
+    userCount: 1
+  },
+  {
+    id: 3456,
+    name: 'Engineering',
+    userRole: 'Contributor',
+    userCount: 4
+  },
+  {
+    id: 4567,
+    name: 'Research',
+    userRole: 'Reader',
+    userCount: 3
+  }
 ];
 const router = useRouter();
 const currentRoute = useRoute();
@@ -196,9 +198,6 @@ onMounted(() => {
   device = currentRoute.params.device;
   console.log(device);
 });
-
-
-
 
 </script>
 
@@ -244,6 +243,7 @@ onMounted(() => {
   .orga-manage-btn{
     padding:0.625em 1em;
     display: flex;
+    align-items: center;
     gap: 1em;
     color: var(--parsec-color-light-secondary-light);
     border-top: 1px solid var(--parsec-color-light-primary-30-opacity15);
