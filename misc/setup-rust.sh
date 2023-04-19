41f309655ed9d3248e83cb49725f4ed22b8f7508
firelight flagboy <firelight.flagboy@gmail.com>
2023-04-19 16:29:27 +0200

    Remove rust toolchain version in `setup-rust.sh` since we use `rust-toolchain.toml`

--- misc/setup-rust.sh
+++ misc/setup-rust.sh
@@ -7,18 +7,16 @@
 # Basic usage:
 #
 # ```bash
-# $ bash setup-rust.sh # Will install rust-1.68.0 (The default)
-# $ bash setup-rust.sh 1.68.0 # Will install rust-1.68.0
+# $ bash setup-rust.sh
 # ```
+
 
 set -eux
 set -o pipefail
 
-RUST_TOOLCHAIN=${1:-1.68.0}
-
-if command -v rustc && [[ "$(rustc --version)" == *"${RUST_TOOLCHAIN}"* ]]; then
-    echo "rust-${RUST_TOOLCHAIN} already installed"
+if command -v rustup; then
+    echo "rust already installed"
 else
-    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | bash -s -- -y --default-toolchain ${RUST_TOOLCHAIN} --profile minimal ${@:2}
-    echo "Successfully installed rust-${RUST_TOOLCHAIN}"
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | bash -s -- -y --default-toolchain none --profile minimal
+    echo "Successfully installed rust"
 fi
