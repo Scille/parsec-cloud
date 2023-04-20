5148c075dcaac27c686d4988426064215a6a2369
firelight flagboy <firelight.flagboy@gmail.com>
2023-04-20 10:22:24 +0200

    [web] Update project configuration
    - Added `declare module '*.vue'` in `vite-env.d.ts`, the correct the error `cannot find module ./App.vue`.
    - Correct wrong formatted `/// <refere...` caused by the lint `space-comment`.
    - In `tsconfig.json`
      - Explicitly set `useDefineForClassFields` to `true` (that the current default value by using `ESNext`).
      - Add `vite/client` to `types` list

--- oxidation/client/src/vite-env.d.ts
+++ oxidation/client/src/vite-env.d.ts
@@ -1,3 +1,11 @@
 // Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 (eventually AGPL-3.0) 2016-present Scille SAS
 
-// / <reference types="vite/client" />
+// eslint-disable-next-line spaced-comment
+/// <reference types="vite/client" />
+
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue';
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
+}
