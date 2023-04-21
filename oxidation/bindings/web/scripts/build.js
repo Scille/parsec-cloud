86cc3d296adcc22654be63b2b5119921890e243c
firelight flagboy <firelight.flagboy@gmail.com>
2023-04-21 09:01:08 +0200

    Re-enable web unit test
    Remove `wasm-pack` from `devDependencies` since it don't work.
    Instead install it from the wasm-pack repo.
    
    Other Changes
    -------------
    
    - Correct unused warning when compiling `platform_http_proxy` on `wasm32`.
    - In `bindings/web/scripts/build.js`:
      - Provide `process.env` to sub command.
      - Now the script will directly invoque `wasm-pack`.
      - Ensure rust target `wasm32-unknown-unknown` is installed.
      - Ensure a link exist between `ci-rust` & `debug`.
    - Cache rust artifact when build the web bindings.
    
    Closes #4447

--- oxidation/bindings/web/scripts/build.js
+++ oxidation/bindings/web/scripts/build.js
@@ -3,78 +3,158 @@
 const { argv, exit, platform } = require('node:process');
 const { spawnSync } = require('node:child_process');
 const path = require("path");
+const fs = require('node:fs');
 
 const DEFAULT_PROFILE = "release";
-const WORKDIR = path.join(__dirname, "..");
+const ROOT_DIR = path.resolve(__dirname, "../../../..");
+const WORKDIR = path.resolve(__dirname, '..');
+const TARGET = "wasm32-unknown-unknown";
 
 switch (argv.length) {
-    // argv[0] is node, argv[1] is build.js
-    case 2:
-        profile = DEFAULT_PROFILE;
-        break;
-    case 3:
-        profile = argv[2];
-        break;
+  // argv[0] is node, argv[1] is build.js
+  case 2:
+    profile = DEFAULT_PROFILE;
+    break;
+  case 3:
+    profile = argv[2];
+    break;
 
-    default:
-        console.log("Usage: build.js [profile]");
-        exit(1);
+  default:
+    console.log("Usage: build.js [profile]");
+    exit(1);
 }
+
+console.log(`Current working dir ${WORKDIR}`);
+
+function on_windows() {
+  return platform == "win32";
+}
+
+function exec_cmd(args, options) {
+  console.log(">>> ", args.join(" "));
+
+  ret = spawnSync(args[0], args.slice(1), options);
+
+  if (ret.signal != null) {
+    console.log(`The command ${args.join(" ")} receive the signal ${ret.signal}`);
+    exit(1);
+  }
+
+  return ret;
+}
+
 
 // Fetch Cargo compile flags
-
-const cmd1_cmd = "python";
-const cmd1_args = [
-    path.join(__dirname, "../../../../make.py"),
+function get_cargo_flag(profile) {
+  const cargo_flag_args = [
+    "python",
+    path.resolve(ROOT_DIR, "make.py"),
     `web-${profile}-libparsec-cargo-flags`,
     "--quiet",
-]
-console.log(">>> ", cmd1_cmd, cmd1_args.join(" "));
-const ret1 = spawnSync(
-    cmd1_cmd,
-    cmd1_args,
-    {
-        // ignore stdin, stdout in pipe, stderr to actual stderr
-        stdio: ['ignore', 'pipe', 'inherit'],
-        cwd: WORKDIR,
-    }
-);
-if (ret1.status != 0) {
-    console.log("stdout:", ret1.stdout.toString());
-    exit(ret1.status);
+  ];
+
+  const ret = exec_cmd(cargo_flag_args, {
+    // ignore stdin, stdout in pipe, stderr to actual stderr
+    stdio: ['ignore', 'pipe', 'inherit'],
+    cwd: WORKDIR,
+    env: process.env
+  });
+
+  if (ret.status != 0) {
+    console.log("stdout:", ret.stdout.toString());
+    exit(ret.status);
+  }
+  return ret.stdout.toString(encoding = "ascii").trim().split(" ");
 }
-const cargo_flags = ret1.stdout.toString(encoding="ascii").trim().split(" ");
+
+function ensure_rust_target_installed() {
+
+  const rustup_target_args = [
+    on_windows() ? "rustup.exe" : "rustup",
+    "target",
+    "add",
+    TARGET
+  ];
+
+  const ret = exec_cmd(rustup_target_args, { stdio: 'inherit', cwd: WORKDIR, env: process.env });
+
+  if (ret.status != 0) {
+    console.log("Failed too add rust wasm target :", ret.stdout());
+    exit(ret.status);
+  }
+}
+
+function hack_wasm_pack_folder(source, dest) {
+  source = path.resolve(ROOT_DIR, "target", TARGET, source);
+  dest = path.resolve(ROOT_DIR, "target", TARGET, dest);
+
+  function create_link() {
+    console.log(`Create a link between ${source} => ${dest}`);
+    fs.symlinkSync(source, dest, 'dir');
+  }
+
+  function remove_file(file) {
+    console.log(`Removing ${dest}`);
+    fs.rmSync(dest, { force: true, recursive: true });
+  }
+
+  try {
+    fs.accessSync(dest, fs.constants.F_OK);
+  } catch {
+    console.log("Link is missing, will create it...");
+    create_link();
+  } finally {
+    let stat = fs.lstatSync(dest);
+    if (!stat.isSymbolicLink()) {
+      console.log("Dest isn't a symlink, removing it...");
+      remove_file(dest);
+    }
+    else {
+      let link_source = fs.readlinkSync(dest);
+      if (link_source != source) {
+        console.log("Dest is a symlink but with the wrong source, removing it...");
+        remove_file(dest);
+      } else {
+        console.log(`Symlink ${dest} already point to ${link_source}`);
+        return;
+      }
+      create_link();
+    }
+  }
+}
+
+function build_wasm(cargo_flags) {
+  // On Windows only .exe/.bat can be directly execute
+  const wasm_build_args = [
+    on_windows() ? "wasm-pack.exe" : "wasm-pack",
+    "build",
+    "--target=web",
+    ...cargo_flags
+  ];
+
+  const ret = exec_cmd(
+    wasm_build_args,
+    {
+      stdio: 'inherit',
+      cwd: WORKDIR,
+      env: process.env
+    },
+  );
+  return ret;
+}
+
+
+const cargo_flags = get_cargo_flag(profile);
+
+ensure_rust_target_installed();
+
+switch (profile) {
+  case "ci":
+    hack_wasm_pack_folder("ci-rust", "debug");
+}
 
 // Actually do the compilation
-
-// On Windows only .exe/.bat can be directly executed, `npx.cmd` is the bat version of `npx`
-const cmd2_cmd = platform == "win32" ? "npx.cmd": "npx";
-const cmd2_args = [
-    "wasm-pack",
-    "build",
-    "--target",
-    "web",
-    // Small hack here: we always pass the `--dev` flag to wasm-pack given it
-    // considers there is no need to pass extra args to cargo (i.e. cargo build
-    // for dev by default). This way we can pass our own `--profile=foo` extra args
-    // without cargo complaining it is clashing with `--release` (provided by wasm-pack, and
-    // equivalent to `--profile=realese`).
-    // This should be safe given if anything change, cargo won't allow multiple profiles
-    // to be passed (hence this script will simply fail).
-    // cf. https://github.com/rustwasm/wasm-pack/blob/b4e619c8a13a8441b804895348afbfd4fb1a68a3/src/build/mod.rs#L91-L106
-    // and https://github.com/rustwasm/wasm-pack/blob/b4e619c8a13a8441b804895348afbfd4fb1a68a3/src/command/build.rs#L220
-    "--dev",
-    "--",
-].concat(cargo_flags);
-console.log(">>> ", cmd2_cmd, cmd2_args.join(" "));
-const ret2 = spawnSync(
-    cmd2_cmd,
-    cmd2_args,
-    {
-        stdio: 'inherit',
-        cwd: WORKDIR,
-    },
-);
-if (ret2.status != 0) {
-    exit(ret2.status);
+const build_res = build_wasm(cargo_flags);
+if (build_res.status != 0) {
+  exit(build_res.status);
 }
