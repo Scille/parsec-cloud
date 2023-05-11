e643bde5e4d9a5292cdf28f32110726d15cfcb6a
fabienSvstr <fabien.sevestre@my-digital-school.org>
2023-05-11 08:54:53 +0200

    Modal settings • Style close arrow btn • Add component settings option for toggle settings • Add a watcher (not working yet) • Add border-radius

--- oxidation/client/src/components/SettingsOption.vue
+++ oxidation/client/src/components/SettingsOption.vue
@@ -1,7 +1,7 @@
 <!-- Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 (eventually AGPL-3.0) 2016-present Scille SAS -->
 
 <template>
-  <ion-item class="toggle-settings">
+  <div class="toggle-settings">
     <div class="toggle-settings__content">
       <ion-text class="body title">
         {{ title }}
@@ -12,29 +12,36 @@
     </div>
     <ion-toggle
       class="toggle-settings__toggle"
-      value=""
+      ref="toggle"
     />
-  </ion-item>
+  </div>
 </template>
 
 <script setup lang="ts">
 import {
-  IonItem,
-  IonToggle
+  IonToggle,
+  IonText
 } from '@ionic/vue';
+import { defineProps, ref } from 'vue';
+
+const toggle = ref();
+// console.log(toggle);
 
 defineProps<{
   title: string
   description?: string
 }>();
+
 </script>
 
 <style scoped lang="scss">
 .toggle-settings {
   display: flex;
   justify-content: space-between;
-  padding-bottom: 0.75rem;
+  align-items: center;
+  padding: 0.75rem 1rem 0.75rem 0;
   border-bottom: 1px solid var(--parsec-color-light-secondary-disabled);
+  gap: 1.5rem;
 
   &__content {
     display: flex;
@@ -53,18 +60,18 @@ defineProps<{
 
   &__toggle {
     width: auto;
-    --handle-width: 30px;
-    --handle-height: 30px;
+    --handle-width: 22px;
+    --handle-height: 22px;
     --handle-max-height: auto;
     --handle-spacing: 3px;
 
     // uncheck
     &::part(track) {
-      height: 36px;
-      width: 60px;
+      height: 28px;
+      width: 50px;
       border-radius: 20px;
       background: var(--parsec-color-light-secondary-inversed-contrast);
-      border: 1.5px solid var(--parsec-color-light-primary-600);
+      box-shadow:inset 0px 0px 0px 1px var(--parsec-color-light-primary-600);
       /* Required for iOS handle to overflow the height of the track */
       overflow: visible;
       opacity: .4;
