662ea2613fe89c6d50aabeea9e6288a43a0d19cb
firelight flagboy <firelight.flagboy@gmail.com>
2023-05-16 14:58:21 +0200

    Add documentation for building testbed server
    Add instructions to:
    
    - Setup docker with the github container registry.
    - Build the testbed docker image.
    - Test the image.
    - Publish the image.
    
    Other Changes
    -------------
    
    - Remove `latest` docker tag from the script
    
      We don't want to use the latest tag for our image because it will be too simple to break something that rely on it.
    
    - Correct typo `nothing`
    
    Closes #4609
    
    Co-authored-by: Marcos Medrano <786907+mmmarcos@users.noreply.github.com>
    Co-authored-by: Alban Costaz <41511029+Ironicbay@users.noreply.github.com>

--- packaging/testbed-server/build.sh
+++ packaging/testbed-server/build.sh
@@ -32,12 +32,11 @@ echo "On top of that the image will use the following prefix \`$PREFIX\`"
 
 DOCKER_BUILDKIT=1 docker build \
     -f $SCRIPTDIR/testbed-server.dockerfile \
-    -t $PREFIX/parsec-testbed-server:latest \
     -t $PREFIX/parsec-testbed-server:$UNIQ_TAG \
     $ROOTDIR $@
 
 echo "You can now test/use the docker image with:"
-echo "docker --port 6777:6777 --rm --name=parsec-testbed-server $PREFIX/parsec-testbed-server:$UNIQ_TAG"
+echo "docker run --publish 6777:6777 --rm --name=parsec-testbed-server $PREFIX/parsec-testbed-server:$UNIQ_TAG"
 
 echo "Once You have tested that the image is working you can push it with"
-echo "for tag in latest $UNIQ_TAG; do docker push $PREFIX/parsec-testbed-server:\$tag; done"
+echo "for tag in $UNIQ_TAG; do docker push $PREFIX/parsec-testbed-server:\$tag; done"
