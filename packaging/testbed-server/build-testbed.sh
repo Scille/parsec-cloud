e9e226fc6ab7bc86e991d5790d3a871e9604cef3
firelight flagboy <firelight.flagboy@gmail.com>
2023-05-22 16:31:31 +0200

    Add workflow to publish `testbed` docker image
    - Rework tag used for the docker testbed image the format is `vMAJOR.MINOR.PATCH-dev.YYYY-MM-DD-sha.<commit-sha>`.
    - Allow to overwrite computed `SRCDIR` & `ROOTDIR`.

--- packaging/testbed-server/build-testbed.sh
+++ packaging/testbed-server/build-testbed.sh
@@ -5,9 +5,6 @@ set -xe
 # Remove bindings crates that aren't needed for testbed-server
 sed -i -e '\;oxidation/bindings;d' Cargo.toml
 
-# Remove the line that generate pyqt code in the build script since the backend don't need it.
-sed -i -e '\;misc/generate_pyqt.py;d' build.py
-
 # Install Rust (actual toolchain is going to be installed by maturin according to `rust-toolchain.toml`)
 curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain none
 
@@ -17,7 +14,7 @@ export PATH="/root/.cargo/bin:$PATH"
 python -m venv venv
 
 # Compile in CI mode to reduce size while still retain `test-utils` feature
-POETRY_LIBPARSEC_BUILD_PROFILE=ci ./venv/bin/python -m pip install .[backend]
+POETRY_PYQT_BUILD_STRATEGY=no_build POETRY_LIBPARSEC_BUILD_PROFILE=ci ./venv/bin/python -m pip install .[backend]
 
 # PSutil is among the dev requirements, retrieve it version and install it manually
 VERSION=$(grep "psutil-" ./poetry.lock | head -n 1 | sed -E 's/.*psutil-([0-9.]+).*/\1/') \
