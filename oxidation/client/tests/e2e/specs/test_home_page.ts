4c44307abd364b5eb8958e76ac1d0ee8960e411f
Maxime GRANDCOLAS <maxime.grandcolas@scille.fr>
2023-05-31 16:40:53 +0200

    [MS] Homepage makeover (YAS QUEEN)     Credits to fabienSvtr and NicoTuxx

--- oxidation/client/tests/e2e/specs/test_home_page.ts
+++ oxidation/client/tests/e2e/specs/test_home_page.ts
@@ -11,28 +11,28 @@ describe('Check organization list', () => {
   });
 
   it('Visit the app root url', () => {
-    cy.contains('List of your organizations');
-    cy.get('.organization-card-container').should('have.length', 3);
+    cy.contains('Your organizations');
+    cy.get('.organization-list-row__col').should('have.length', 3);
   });
 
   it('Go to login page and back to organizations', () => {
-    cy.get('.organization-card-container').should('have.length', 3);
-    cy.get('#login-button-container').should('not.exist');
+    cy.get('.organization-list-row__col').should('have.length', 3);
+    cy.get('.login-button-container').should('not.exist');
     cy.contains('Boby McBobFace').click();
     cy.get('.organization-card').contains('Org');
-    cy.get('#login-button-container').should('exist');
+    cy.get('.login-button-container').should('exist');
     cy.contains('Return to organizations').click();
-    cy.contains('List of your organizations');
-    cy.get('.organization-card-container').should('have.length', 3);
+    cy.contains('Your organizations');
+    cy.get('.organization-list-row__col').should('have.length', 3);
   });
 
   it('Go to login page and enter password', () => {
     cy.contains('Boby McBobFace').click();
-    cy.get('#login-button-container > ion-button').should('have.class', 'button-disabled');
+    cy.get('.login-button-container > ion-button').should('have.class', 'button-disabled');
     cy.get('#password-input').find('input').invoke('attr', 'type').should('eq', 'password');
     cy.get('#password-input').find('input').type('P@ssw0rd');
-    cy.get('#login-button-container > ion-button').should('not.have.class', 'button-disabled');
-    cy.get('#login-button-container > ion-button').click();
+    cy.get('.login-button-container > ion-button').should('not.have.class', 'button-disabled');
+    cy.get('.login-button-container > ion-button').click();
     cy.get('@configPath').then(($elem) => {
       const orgId = ($elem as unknown as string).split('/').slice(-1)[0];
       cy.get('@consoleLog').should('have.been.calledWith', `Log in to ${orgId} with password "P@ssw0rd"`);
@@ -41,37 +41,37 @@ describe('Check organization list', () => {
   });
 
   it('Go to login page and sort and filter orgs', () => {
-    cy.get('.organization-card-container').should('have.length', 3);
+    cy.get('.organization-list-row__col').as('orgList').should('have.length', 3);
     // Sorted by org name asc by default
-    cy.get('.organization-card-container').first().contains('Alicey McAliceFace');
-    cy.get('.organization-card-container').last().contains('Boby McBobFace');
+    cy.get('@orgList').first().contains('Alicey McAliceFace');
+    cy.get('@orgList').last().contains('Boby McBobFace');
     cy.get('#search-input').find('input').type('alice');
-    cy.get('.organization-card-container').should('have.length', 2);
+    cy.get('@orgList').should('have.length', 2);
     // Only 2 devices shown
-    cy.get('.organization-card-container').first().contains('Alicey McAliceFace');
-    cy.get('.organization-card-container').last().contains('Alicey McAliceFace');
+    cy.get('@orgList').first().contains('Alicey McAliceFace');
+    cy.get('@orgList').last().contains('Alicey McAliceFace');
     cy.get('#search-input').find('input').clear();
-    cy.get('.organization-card-container').should('have.length', 3);
+    cy.get('@orgList').should('have.length', 3);
     // Change sort order
     cy.get('#filter-select').contains('Organization').click();
     cy.get('.option').should('have.length', 4);
     cy.get('.option').eq(2).contains('User Name').click();
     cy.get('#filter-select').contains('User Name').click();
     cy.get('.option').should('have.length', 4);
-    cy.get('.option').first().contains('Ascending order').click();
+    cy.get('.option').first().contains('Ascending').click();
     // Now sorted by user name desc
-    cy.get('.organization-card-container').first().contains('Boby McBobFace');
-    cy.get('.organization-card-container').last().contains('Alicey McAliceFace');
+    cy.get('@orgList').first().contains('Boby McBobFace');
+    cy.get('@orgList').last().contains('Alicey McAliceFace');
   });
 
   it('Open create organization dialog', () => {
-    cy.contains('Create an organization').click();
-    cy.contains('Use the main PARSEC server');
+    cy.get('#create-organization-button').click();
+    cy.get('.popover-viewport').contains('I want to create an organization');
   });
 
   it('Open join organization dialog', () => {
-    cy.contains('Join an organization').click();
-    cy.contains('Please enter the organization\'s URL');
+    cy.get('#create-organization-button').click();
+    cy.get('.popover-viewport').contains('I received a Parsec Cloud invitation');
   });
 
   it('Log into organization with command', () => {
