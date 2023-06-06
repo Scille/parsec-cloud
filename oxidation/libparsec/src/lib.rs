e72f164de7a8b6766704732de713760746d44270
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2023-06-06 14:45:40 +0200

    Remove libparsec_core&libparsec_core_fs (will be replaced by libparsec_client)

--- oxidation/libparsec/src/lib.rs
+++ oxidation/libparsec/src/lib.rs
@@ -1,7 +1,5 @@
 // Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 (eventually AGPL-3.0) 2016-present Scille SAS
 
-#[cfg(not(target_arch = "wasm32"))]
-pub use libparsec_core_fs as core_fs;
 #[cfg(all(not(target_arch = "wasm32"), feature = "test-utils"))]
 pub mod local_db {
     pub use libparsec_platform_storage::sqlite::{
@@ -17,7 +15,6 @@ pub use libparsec_testbed as testbed;
 pub use libparsec_types as types;
 
 pub use libparsec_client_connection as client_connection;
-pub use libparsec_core as core;
 pub use libparsec_crypto as crypto;
 pub use libparsec_platform_device_loader as platform_device_loader;
 
