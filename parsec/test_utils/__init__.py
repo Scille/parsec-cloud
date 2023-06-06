9efafc4dc4e33eb9ab140492999fbaf660202f80
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2023-06-06 18:19:21 +0200

    Remove useless parsec/test_utils/workspacefs.py

--- parsec/test_utils/__init__.py
+++ parsec/test_utils/__init__.py
@@ -2,17 +2,5 @@
 from __future__ import annotations
 
 from parsec.test_utils.organization import initialize_test_organization
-from parsec.test_utils.workspacefs import (
-    create_inconsistent_workspace,
-    make_workspace_dir_complex_versions,
-    make_workspace_dir_inconsistent,
-    make_workspace_dir_simple_versions,
-)
 
-__all__ = (
-    "make_workspace_dir_inconsistent",
-    "make_workspace_dir_simple_versions",
-    "make_workspace_dir_complex_versions",
-    "create_inconsistent_workspace",
-    "initialize_test_organization",
-)
+__all__ = ("initialize_test_organization",)
