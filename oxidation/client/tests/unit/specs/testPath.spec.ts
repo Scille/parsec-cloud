c767cca71aab9e125adb0bd5ca23f86e8dd1e791
Maxime GRANDCOLAS <maxime.grandcolas@scille.fr>
2023-06-07 13:54:07 +0200

    [MS] Added unit test for path

---
+++ oxidation/client/tests/unit/specs/testPath.spec.ts
@@ -0,0 +1,17 @@
+// Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 (eventually AGPL-3.0) 2016-present Scille SAS
+
+import { parse } from '@/common/path';
+
+describe('Path', () => {
+  it('test path parsing', async () => {
+    expect(parse('/')).to.deep.equal(['/']);
+    expect(parse('/a/b')).to.deep.equal(['/', 'a', 'b']);
+    expect(parse('/a/')).to.deep.equal(['/', 'a']);
+    expect(parse('/a//b')).to.deep.equal(['/', 'a', 'b']);
+    expect(parse('/////a')).to.deep.equal(['/', 'a']);
+    expect(parse('//')).to.deep.equal(['/']);
+    expect(parse('///////')).to.deep.equal(['/']);
+    expect(() => parse('')).to.throw('Path should be at least "/" and start with "/"');
+    expect(() => parse('a/b')).to.throw('Path should be at least "/" and start with "/"');
+  });
+});
