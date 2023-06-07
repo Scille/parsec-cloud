9878566b9d2711c58b7d02b19b8373f378d370cb
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2023-06-07 10:17:10 +0200

    Remove run_testenv.* (must be rewritten given Python client is no longer present)

--- parsec/test_utils/__init__.py
+++
@@ -1,6 +0,0 @@
-# Parsec Cloud (https://parsec.cloud) Copyright (c) AGPL-3.0 2016-present Scille SAS
-from __future__ import annotations
-
-from parsec.test_utils.organization import initialize_test_organization
-
-__all__ = ("initialize_test_organization",)
