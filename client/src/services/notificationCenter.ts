b815157405bc421ce84fccd7fde7155acbf9f129
Maxime GRANDCOLAS <maxime.grandcolas@scille.fr>
2023-06-14 11:31:38 +0200

    [MS] Added notification center

---
+++ client/src/services/notificationCenter.ts
@@ -0,0 +1,119 @@
+// Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 (eventually AGPL-3.0) 2016-present Scille SAS
+
+import { DateTime } from 'luxon';
+import { v4 as uuid4 } from 'uuid';
+
+export enum NotificationLevel {
+  Info = 'INFO',
+  Success = 'SUCCESS',
+  Warning = 'WARNING',
+  Error = 'ERROR'
+}
+
+export interface Notification {
+  id: string,
+  title: string,
+  message: string,
+  level: NotificationLevel,
+  read: boolean,
+  time: DateTime,
+  data: object
+}
+
+export class NotificationCenter {
+  notifications: Notification[];
+
+  constructor() {
+    this.notifications = [];
+  }
+
+  async showModal(
+    title: string,
+    message: string,
+    level: NotificationLevel,
+    addToList: boolean,
+    trace = false,
+    data = {}
+  ): Promise<void> {
+    const notif: Notification = {
+      id: uuid4(),
+      title: title,
+      message: message,
+      level: level,
+      read: false,
+      time: DateTime.now(),
+      data: data
+    };
+    if (trace) {
+      this.trace(notif);
+    }
+    if (addToList) {
+      this.notifications.push(notif);
+    }
+  }
+
+  async showSnackbar(
+    title: string,
+    message: string,
+    level: NotificationLevel,
+    addToList: boolean,
+    trace: false,
+    data = {}
+  ): Promise<void> {
+    const notif: Notification = {
+      id: uuid4(),
+      title: title,
+      message: message,
+      level: level,
+      read: false,
+      time: DateTime.now(),
+      data: data
+    };
+    if (trace) {
+      this.trace(notif);
+    }
+    if (addToList) {
+      this.notifications.push(notif);
+    }
+  }
+
+  async addToList(title: string, message: string, level: NotificationLevel, trace = false, data = {}): Promise<void> {
+    const notif: Notification = {
+      id: uuid4(),
+      title: title,
+      message: message,
+      level: level,
+      read: false,
+      time: DateTime.now(),
+      data: data
+    };
+    if (trace) {
+      this.trace(notif);
+    }
+    this.notifications.push(notif);
+  }
+
+  markAsRead(id: string, read = true): void {
+    const notif = this.notifications.find((n) => n.id === id);
+
+    if (notif) {
+      notif.read = read;
+    }
+  }
+
+  getNotifications(): Notification[] {
+    return this.notifications;
+  }
+
+  hasUnreadNotifications(): boolean {
+    return this.notifications.some((n) => n.read === false);
+  }
+
+  clear(): void {
+    this.notifications = [];
+  }
+
+  private trace(notif: Notification): void {
+    console.trace(`[${notif.level}] ${notif.title} : ${notif.message}`);
+  }
+}
---
+++ client/tests/unit/specs/testNotificationCenter.spec.ts
@@ -0,0 +1,114 @@
+// Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 (eventually AGPL-3.0) 2016-present Scille SAS
+
+import {
+  Notification as MsNotification,
+  NotificationLevel,
+  NotificationCenter
+} from '@/services/notificationCenter';
+import { vi } from 'vitest';
+import { DateTime } from 'luxon';
+
+describe('Notification Center', () => {
+  let NOTIFS: MsNotification[];
+
+  beforeEach(() => {
+    vi.useFakeTimers();
+    vi.setSystemTime(new Date(2000, 1, 1));
+    vi.mock('uuid', () => {
+      const v4 = vi.fn();
+      v4.mockReturnValue('1234');
+      return { v4 };
+    });
+
+    NOTIFS = [
+      {
+        id: '1234',
+        title: 'A',
+        message: 'B',
+        level: NotificationLevel.Warning,
+        read: false,
+        time: DateTime.now(),
+        data: {}
+      },
+      {
+        id: '1234',
+        title: 'C',
+        message: 'D',
+        level: NotificationLevel.Info,
+        read: false,
+        time: DateTime.now(),
+        data: {
+          'key': 'value'
+        }
+      },
+      {
+        id: '1234',
+        title: 'E',
+        message: 'F',
+        level: NotificationLevel.Error,
+        read: false,
+        time: DateTime.now(),
+        data: {}
+      }
+    ];
+  });
+
+  afterEach(() => {
+    vi.useRealTimers();
+  });
+
+  it('Check initial state', async () => {
+    const center = new NotificationCenter();
+
+    expect(center.notifications).to.deep.equal([]);
+    expect(center.getNotifications()).to.deep.equal([]);
+    expect(center.hasUnreadNotifications()).to.be.false;
+  });
+
+  it('Adds notification to list', async () => {
+    const center = new NotificationCenter();
+
+    expect(center.notifications).to.deep.equal([]);
+    center.showModal('A', 'B', NotificationLevel.Warning, true, false);
+    expect(center.getNotifications()).to.deep.equal(NOTIFS.slice(0, 1));
+    expect(center.hasUnreadNotifications()).to.be.true;
+
+    center.showSnackbar('C', 'D', NotificationLevel.Info, true, false, {'key': 'value'});
+    expect(center.getNotifications()).to.deep.equal(NOTIFS.slice(0, 2));
+
+    center.addToList('E', 'F', NotificationLevel.Error, false);
+    expect(center.getNotifications()).to.deep.equal(NOTIFS);
+
+    center.clear();
+    expect(center.getNotifications()).to.deep.equal([]);
+    expect(center.hasUnreadNotifications()).to.be.false;
+  });
+
+  it('Do not add notification to list', async () => {
+    const center = new NotificationCenter();
+
+    expect(center.notifications).to.deep.equal([]);
+    center.showModal('A', 'B', NotificationLevel.Warning, false, false);
+    expect(center.getNotifications()).to.deep.equal([]);
+    expect(center.hasUnreadNotifications()).to.be.false;
+
+    center.showSnackbar('C', 'D', NotificationLevel.Info, false, false, {'key': 'value'});
+    expect(center.getNotifications()).to.deep.equal([]);
+
+    center.addToList('E', 'F', NotificationLevel.Error, false);
+    expect(center.getNotifications()).to.deep.equal(NOTIFS.slice(2, 3));
+    expect(center.hasUnreadNotifications()).to.be.true;
+  });
+
+  it('Mark as read', async () => {
+    const center = new NotificationCenter();
+
+    expect(center.hasUnreadNotifications()).to.be.false;
+    center.showModal('A', 'B', NotificationLevel.Warning, true, false);
+    expect(center.hasUnreadNotifications()).to.be.true;
+    center.markAsRead('1234', true);
+    expect(center.hasUnreadNotifications()).to.be.false;
+    center.markAsRead('1234', false);
+    expect(center.hasUnreadNotifications()).to.be.true;
+  });
+});
