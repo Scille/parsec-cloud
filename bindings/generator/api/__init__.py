4a2c1ef024faa43b88bdd2fefb8f74f57d4b3357
firelight flagboy <firelight.flagboy@gmail.com>
2023-08-01 16:10:11 +0200

    Re-license `bindings` folder to busl

--- bindings/generator/api/__init__.py
+++ bindings/generator/api/__init__.py
@@ -1,4 +1,4 @@
-# Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 (eventually AGPL-3.0) 2016-present Scille SAS
+# Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 2016-present Scille SAS
 
 from .cancel import *  # noqa: F403
 from .client import *  # noqa: F403
