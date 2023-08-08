611143c94474c7b9097ff78bc3b96db95eefbbae
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2023-08-08 15:15:37 +0200

    Corrections in libparsec high level api for invite claimer

--- bindings/generator/api/events.py
+++ bindings/generator/api/events.py
@@ -1,8 +1,14 @@
 # Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 (eventually AGPL-3.0) 2016-present Scille SAS
 
+from typing import Callable
+
 from .common import Variant
 
 
 class ClientEvent(Variant):
     class Ping:
         ping: str
+
+
+class OnClientEventCallback(Callable[[ClientEvent], None]):
+    pass
--- libparsec/src/invite.rs
+++ libparsec/src/invite.rs
@@ -1,16 +1,29 @@
 // Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 (eventually AGPL-3.0) 2016-present Scille SAS
 
-use std::sync::Arc;
+use std::{path::PathBuf, sync::Arc};
 
-use libparsec_types::anyhow::anyhow;
 pub use libparsec_types::prelude::*;
 
-use crate::{ClientConfig, ClientEvent, OnEventCallbackPlugged};
+use crate::{
+    handle::{register_handle, take_and_close_handle, Handle, HandleItem},
+    ClientConfig, ClientEvent, OnEventCallbackPlugged,
+};
 
 #[derive(Debug, Clone)]
 pub enum DeviceSaveStrategy {
     Password { password: Password },
     Smartcard,
+}
+
+impl DeviceSaveStrategy {
+    pub fn into_access(self, key_file: PathBuf) -> DeviceAccessStrategy {
+        match self {
+            DeviceSaveStrategy::Password { password } => {
+                DeviceAccessStrategy::Password { key_file, password }
+            }
+            DeviceSaveStrategy::Smartcard => DeviceAccessStrategy::Smartcard { key_file },
+        }
+    }
 }
 
 /*
@@ -105,12 +118,7 @@ pub async fn bootstrap_organization(
             &config.config_dir,
             &finalize_ctx.new_local_device,
         );
-        match save_strategy {
-            DeviceSaveStrategy::Password { password } => {
-                DeviceAccessStrategy::Password { key_file, password }
-            }
-            DeviceSaveStrategy::Smartcard => DeviceAccessStrategy::Smartcard { key_file },
-        }
+        save_strategy.into_access(key_file)
     };
 
     let available_device = finalize_ctx
@@ -140,7 +148,7 @@ pub async fn claimer_retrieve_info(
     #[cfg(target_arch = "wasm32")] _on_event_callback: Arc<dyn Fn(ClientEvent)>,
 
     addr: BackendInvitationAddr,
-) -> Result<UserOrDeviceClaimInitialCtxHandle, ClaimerRetrieveInfoError> {
+) -> Result<UserOrDeviceClaimInitialInfo, ClaimerRetrieveInfoError> {
     let config: Arc<libparsec_client::ClientConfig> = config.into();
     // TODO
     // let events_plugged = Arc::new(OnEventCallbackPlugged::new(on_event_callback));
@@ -152,9 +160,8 @@ pub async fn claimer_retrieve_info(
             let claimer_email = ctx.claimer_email.clone();
             let greeter_user_id = ctx.greeter_user_id().to_owned();
             let greeter_human_handle = ctx.greeter_human_handle().to_owned();
-            let handle =
-                crate::handle::register_handle(crate::handle::HandleItem::UserClaimInitial(ctx));
-            Ok(UserOrDeviceClaimInitialCtxHandle::User {
+            let handle = register_handle(HandleItem::UserClaimInitial(ctx));
+            Ok(UserOrDeviceClaimInitialInfo::User {
                 handle,
                 claimer_email,
                 greeter_user_id,
@@ -164,9 +171,8 @@ pub async fn claimer_retrieve_info(
         libparsec_client::UserOrDeviceClaimInitialCtx::Device(ctx) => {
             let greeter_user_id = ctx.greeter_user_id().to_owned();
             let greeter_human_handle = ctx.greeter_human_handle().to_owned();
-            let handle =
-                crate::handle::register_handle(crate::handle::HandleItem::DeviceClaimInitial(ctx));
-            Ok(UserOrDeviceClaimInitialCtxHandle::Device {
+            let handle = register_handle(HandleItem::DeviceClaimInitial(ctx));
+            Ok(UserOrDeviceClaimInitialInfo::Device {
                 handle,
                 greeter_user_id,
                 greeter_human_handle,
@@ -175,37 +181,36 @@ pub async fn claimer_retrieve_info(
     }
 }
 
-pub enum UserOrDeviceClaimInitialCtxHandle {
+pub enum UserOrDeviceClaimInitialInfo {
     User {
-        handle: crate::handle::Handle,
+        handle: Handle,
         claimer_email: String,
         greeter_user_id: UserID,
         greeter_human_handle: Option<HumanHandle>,
     },
     Device {
-        handle: crate::handle::Handle,
+        handle: Handle,
         greeter_user_id: UserID,
         greeter_human_handle: Option<HumanHandle>,
     },
 }
 
 pub async fn claimer_user_initial_ctx_do_wait_peer(
-    handle: crate::handle::Handle,
-) -> Result<UserClaimInProgress1CtxHandle, ClaimInProgressError> {
-    let ctx = crate::handle::take_and_close_handle(handle, |x| match x {
-        crate::handle::HandleItem::UserClaimInitial(ctx) => Some(ctx),
+    handle: Handle,
+) -> Result<UserClaimInProgress1Info, ClaimInProgressError> {
+    let ctx = take_and_close_handle(handle, |x| match x {
+        HandleItem::UserClaimInitial(ctx) => Some(ctx),
         _ => None,
     })
-    .ok_or_else(|| anyhow!("Invalid handle"))?;
+    .ok_or_else(|| anyhow::anyhow!("Invalid handle"))?;
 
     let ctx = ctx.do_wait_peer().await?;
     let greeter_sas_choices = ctx.generate_greeter_sas_choices(4);
     let greeter_sas = ctx.greeter_sas().to_owned();
 
-    let new_handle =
-        crate::handle::register_handle(crate::handle::HandleItem::UserClaimInProgress1(ctx));
+    let new_handle = register_handle(HandleItem::UserClaimInProgress1(ctx));
 
-    Ok(UserClaimInProgress1CtxHandle {
+    Ok(UserClaimInProgress1Info {
         handle: new_handle,
         greeter_sas,
         greeter_sas_choices,
@@ -213,195 +218,183 @@ pub async fn claimer_user_initial_ctx_do_wait_peer(
 }
 
 pub async fn claimer_device_initial_ctx_do_wait_peer(
-    handle: crate::handle::Handle,
-) -> Result<DeviceClaimInProgress1CtxHandle, ClaimInProgressError> {
-    let ctx = crate::handle::take_and_close_handle(handle, |x| match x {
-        crate::handle::HandleItem::DeviceClaimInitial(ctx) => Some(ctx),
+    handle: Handle,
+) -> Result<DeviceClaimInProgress1Info, ClaimInProgressError> {
+    let ctx = take_and_close_handle(handle, |x| match x {
+        HandleItem::DeviceClaimInitial(ctx) => Some(ctx),
         _ => None,
     })
-    .ok_or_else(|| anyhow!("Invalid handle"))?;
+    .ok_or_else(|| anyhow::anyhow!("Invalid handle"))?;
 
     let ctx = ctx.do_wait_peer().await?;
     let greeter_sas_choices = ctx.generate_greeter_sas_choices(4);
     let greeter_sas = ctx.greeter_sas().to_owned();
 
-    let new_handle =
-        crate::handle::register_handle(crate::handle::HandleItem::DeviceClaimInProgress1(ctx));
+    let new_handle = register_handle(HandleItem::DeviceClaimInProgress1(ctx));
 
-    Ok(DeviceClaimInProgress1CtxHandle {
+    Ok(DeviceClaimInProgress1Info {
         handle: new_handle,
         greeter_sas,
         greeter_sas_choices,
     })
 }
 
-pub struct UserClaimInProgress1CtxHandle {
-    pub handle: crate::handle::Handle,
+pub struct UserClaimInProgress1Info {
+    pub handle: Handle,
     pub greeter_sas: SASCode,
     pub greeter_sas_choices: Vec<SASCode>,
 }
-pub struct DeviceClaimInProgress1CtxHandle {
-    pub handle: crate::handle::Handle,
+pub struct DeviceClaimInProgress1Info {
+    pub handle: Handle,
     pub greeter_sas: SASCode,
     pub greeter_sas_choices: Vec<SASCode>,
 }
 
 pub async fn claimer_user_in_progress_2_do_signify_trust(
-    handle: crate::handle::Handle,
-) -> Result<UserClaimInProgress2CtxHandle, ClaimInProgressError> {
-    let ctx = crate::handle::take_and_close_handle(handle, |x| match x {
-        crate::handle::HandleItem::UserClaimInProgress1(ctx) => Some(ctx),
+    handle: Handle,
+) -> Result<UserClaimInProgress2Info, ClaimInProgressError> {
+    let ctx = take_and_close_handle(handle, |x| match x {
+        HandleItem::UserClaimInProgress1(ctx) => Some(ctx),
         _ => None,
     })
-    .ok_or_else(|| anyhow!("Invalid handle"))?;
+    .ok_or_else(|| anyhow::anyhow!("Invalid handle"))?;
 
     let ctx = ctx.do_signify_trust().await?;
     let claimer_sas = ctx.claimer_sas().to_owned();
 
-    let new_handle =
-        crate::handle::register_handle(crate::handle::HandleItem::UserClaimInProgress2(ctx));
+    let new_handle = register_handle(HandleItem::UserClaimInProgress2(ctx));
 
-    Ok(UserClaimInProgress2CtxHandle {
+    Ok(UserClaimInProgress2Info {
         handle: new_handle,
         claimer_sas,
     })
 }
 
 pub async fn claimer_device_in_progress_2_do_signify_trust(
-    handle: crate::handle::Handle,
-) -> Result<DeviceClaimInProgress2CtxHandle, ClaimInProgressError> {
-    let ctx = crate::handle::take_and_close_handle(handle, |x| match x {
-        crate::handle::HandleItem::DeviceClaimInProgress1(ctx) => Some(ctx),
+    handle: Handle,
+) -> Result<DeviceClaimInProgress2Info, ClaimInProgressError> {
+    let ctx = take_and_close_handle(handle, |x| match x {
+        HandleItem::DeviceClaimInProgress1(ctx) => Some(ctx),
         _ => None,
     })
-    .ok_or_else(|| anyhow!("Invalid handle"))?;
+    .ok_or_else(|| anyhow::anyhow!("Invalid handle"))?;
 
     let ctx = ctx.do_signify_trust().await?;
     let claimer_sas = ctx.claimer_sas().to_owned();
 
-    let new_handle =
-        crate::handle::register_handle(crate::handle::HandleItem::DeviceClaimInProgress2(ctx));
+    let new_handle = register_handle(HandleItem::DeviceClaimInProgress2(ctx));
 
-    Ok(DeviceClaimInProgress2CtxHandle {
+    Ok(DeviceClaimInProgress2Info {
         handle: new_handle,
         claimer_sas,
     })
 }
 
-pub struct UserClaimInProgress2CtxHandle {
-    pub handle: crate::handle::Handle,
+pub struct UserClaimInProgress2Info {
+    pub handle: Handle,
     pub claimer_sas: SASCode,
 }
-pub struct DeviceClaimInProgress2CtxHandle {
-    pub handle: crate::handle::Handle,
+pub struct DeviceClaimInProgress2Info {
+    pub handle: Handle,
     pub claimer_sas: SASCode,
 }
 
 pub async fn claimer_user_in_progress_2_do_wait_peer_trust(
-    handle: crate::handle::Handle,
-) -> Result<UserClaimInProgress3CtxHandle, ClaimInProgressError> {
-    let ctx = crate::handle::take_and_close_handle(handle, |x| match x {
-        crate::handle::HandleItem::UserClaimInProgress2(ctx) => Some(ctx),
+    handle: Handle,
+) -> Result<UserClaimInProgress3Info, ClaimInProgressError> {
+    let ctx = take_and_close_handle(handle, |x| match x {
+        HandleItem::UserClaimInProgress2(ctx) => Some(ctx),
         _ => None,
     })
-    .ok_or_else(|| anyhow!("Invalid handle"))?;
+    .ok_or_else(|| anyhow::anyhow!("Invalid handle"))?;
 
     let ctx = ctx.do_wait_peer_trust().await?;
 
-    let new_handle =
-        crate::handle::register_handle(crate::handle::HandleItem::UserClaimInProgress3(ctx));
+    let new_handle = register_handle(HandleItem::UserClaimInProgress3(ctx));
 
-    Ok(UserClaimInProgress3CtxHandle { handle: new_handle })
+    Ok(UserClaimInProgress3Info { handle: new_handle })
 }
 
 pub async fn claimer_device_in_progress_2_do_wait_peer_trust(
-    handle: crate::handle::Handle,
-) -> Result<DeviceClaimInProgress3CtxHandle, ClaimInProgressError> {
-    let ctx = crate::handle::take_and_close_handle(handle, |x| match x {
-        crate::handle::HandleItem::DeviceClaimInProgress2(ctx) => Some(ctx),
+    handle: Handle,
+) -> Result<DeviceClaimInProgress3Info, ClaimInProgressError> {
+    let ctx = take_and_close_handle(handle, |x| match x {
+        HandleItem::DeviceClaimInProgress2(ctx) => Some(ctx),
         _ => None,
     })
-    .ok_or_else(|| anyhow!("Invalid handle"))?;
+    .ok_or_else(|| anyhow::anyhow!("Invalid handle"))?;
 
     let ctx = ctx.do_wait_peer_trust().await?;
 
-    let new_handle =
-        crate::handle::register_handle(crate::handle::HandleItem::DeviceClaimInProgress3(ctx));
+    let new_handle = register_handle(HandleItem::DeviceClaimInProgress3(ctx));
 
-    Ok(DeviceClaimInProgress3CtxHandle { handle: new_handle })
+    Ok(DeviceClaimInProgress3Info { handle: new_handle })
 }
 
-pub struct UserClaimInProgress3CtxHandle {
-    pub handle: crate::handle::Handle,
+pub struct UserClaimInProgress3Info {
+    pub handle: Handle,
 }
-pub struct DeviceClaimInProgress3CtxHandle {
-    pub handle: crate::handle::Handle,
+pub struct DeviceClaimInProgress3Info {
+    pub handle: Handle,
 }
 
 pub async fn claimer_user_in_progress_3_do_claim(
-    handle: crate::handle::Handle,
+    handle: Handle,
     requested_device_label: Option<DeviceLabel>,
     requested_human_handle: Option<HumanHandle>,
-) -> Result<UserClaimFinalizeCtxHandle, ClaimInProgressError> {
-    let ctx = crate::handle::take_and_close_handle(handle, |x| match x {
-        crate::handle::HandleItem::UserClaimInProgress3(ctx) => Some(ctx),
+) -> Result<UserClaimFinalizeInfo, ClaimInProgressError> {
+    let ctx = take_and_close_handle(handle, |x| match x {
+        HandleItem::UserClaimInProgress3(ctx) => Some(ctx),
         _ => None,
     })
-    .ok_or_else(|| anyhow!("Invalid handle"))?;
+    .ok_or_else(|| anyhow::anyhow!("Invalid handle"))?;
 
     let ctx = ctx
         .do_claim_user(requested_device_label, requested_human_handle)
         .await?;
 
-    let new_handle =
-        crate::handle::register_handle(crate::handle::HandleItem::UserClaimFinalize(ctx));
+    let new_handle = register_handle(HandleItem::UserClaimFinalize(ctx));
 
-    Ok(UserClaimFinalizeCtxHandle { handle: new_handle })
+    Ok(UserClaimFinalizeInfo { handle: new_handle })
 }
 
 pub async fn claimer_device_in_progress_3_do_claim(
-    handle: crate::handle::Handle,
+    handle: Handle,
     requested_device_label: Option<DeviceLabel>,
-) -> Result<DeviceClaimFinalizeCtxHandle, ClaimInProgressError> {
-    let ctx = crate::handle::take_and_close_handle(handle, |x| match x {
-        crate::handle::HandleItem::DeviceClaimInProgress3(ctx) => Some(ctx),
+) -> Result<DeviceClaimFinalizeInfo, ClaimInProgressError> {
+    let ctx = take_and_close_handle(handle, |x| match x {
+        HandleItem::DeviceClaimInProgress3(ctx) => Some(ctx),
         _ => None,
     })
-    .ok_or_else(|| anyhow!("Invalid handle"))?;
+    .ok_or_else(|| anyhow::anyhow!("Invalid handle"))?;
 
     let ctx = ctx.do_claim_device(requested_device_label).await?;
 
-    let new_handle =
-        crate::handle::register_handle(crate::handle::HandleItem::DeviceClaimFinalize(ctx));
+    let new_handle = register_handle(HandleItem::DeviceClaimFinalize(ctx));
 
-    Ok(DeviceClaimFinalizeCtxHandle { handle: new_handle })
+    Ok(DeviceClaimFinalizeInfo { handle: new_handle })
 }
 
-pub struct UserClaimFinalizeCtxHandle {
-    pub handle: crate::handle::Handle,
+pub struct UserClaimFinalizeInfo {
+    pub handle: Handle,
 }
-pub struct DeviceClaimFinalizeCtxHandle {
-    pub handle: crate::handle::Handle,
+pub struct DeviceClaimFinalizeInfo {
+    pub handle: Handle,
 }
 
 pub async fn claimer_user_finalize_save_local_device(
-    handle: crate::handle::Handle,
+    handle: Handle,
     save_strategy: DeviceSaveStrategy,
 ) -> Result<AvailableDevice, ClaimInProgressError> {
-    let ctx = crate::handle::take_and_close_handle(handle, |x| match x {
-        crate::handle::HandleItem::UserClaimFinalize(ctx) => Some(ctx),
+    let ctx = take_and_close_handle(handle, |x| match x {
+        HandleItem::UserClaimFinalize(ctx) => Some(ctx),
         _ => None,
     })
-    .ok_or_else(|| anyhow!("Invalid handle"))?;
+    .ok_or_else(|| anyhow::anyhow!("Invalid handle"))?;
 
     let access = {
         let key_file = ctx.get_default_key_file();
-        match save_strategy {
-            DeviceSaveStrategy::Password { password } => {
-                DeviceAccessStrategy::Password { key_file, password }
-            }
-            DeviceSaveStrategy::Smartcard => DeviceAccessStrategy::Smartcard { key_file },
-        }
+        save_strategy.into_access(key_file)
     };
 
     let available_device = ctx.save_local_device(&access).await?;
@@ -410,23 +403,18 @@ pub async fn claimer_user_finalize_save_local_device(
 }
 
 pub async fn claimer_device_finalize_save_local_device(
-    handle: crate::handle::Handle,
+    handle: Handle,
     save_strategy: DeviceSaveStrategy,
 ) -> Result<AvailableDevice, ClaimInProgressError> {
-    let ctx = crate::handle::take_and_close_handle(handle, |x| match x {
-        crate::handle::HandleItem::DeviceClaimFinalize(ctx) => Some(ctx),
+    let ctx = take_and_close_handle(handle, |x| match x {
+        HandleItem::DeviceClaimFinalize(ctx) => Some(ctx),
         _ => None,
     })
-    .ok_or_else(|| anyhow!("Invalid handle"))?;
+    .ok_or_else(|| anyhow::anyhow!("Invalid handle"))?;
 
     let access = {
         let key_file = ctx.get_default_key_file();
-        match save_strategy {
-            DeviceSaveStrategy::Password { password } => {
-                DeviceAccessStrategy::Password { key_file, password }
-            }
-            DeviceSaveStrategy::Smartcard => DeviceAccessStrategy::Smartcard { key_file },
-        }
+        save_strategy.into_access(key_file)
     };
 
     let available_device = ctx.save_local_device(&access).await?;
