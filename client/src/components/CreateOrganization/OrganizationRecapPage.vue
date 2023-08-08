c94dc9f97affc257c7dbc31947552723a49b9819
fabienSvstr <fabien.sevestre@my-digital-school.org>
2023-08-08 15:35:47 +0200

    Added OrganizationRecapPage

---
+++ client/src/components/CreateOrganization/OrganizationRecapPage.vue
@@ -0,0 +1,33 @@
+<!-- Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 (eventually AGPL-3.0) 2016-present Scille SAS -->
+
+<template>
+  <ion-list class="choose-server-page">
+    <ion-item
+      class="recap-item"
+    >
+      <organization-recap-item
+        :title="t('CreateOrganization.overview.organization')"
+        :text="organization"
+      />
+    </ion-item>
+  </ion-list>
+</template>
+
+<script setup lang="ts">
+import {
+  IonList,
+  IonItem,
+} from '@ionic/vue';
+import OrganizationRecapItem from '@/components/CreateOrganization/OrganizationRecapItem.vue';
+import { useI18n } from 'vue-i18n';
+
+const { t } = useI18n();
+
+defineProps<{
+  organization: string,
+}>();
+</script>
+
+<style scoped lang="scss">
+
+</style>
