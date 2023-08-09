41d05c825576c6fa217cda0737428c0ee1cf8f67
NicoTuxx <nicolasstempert@gmail.com>
2023-08-09 20:02:48 +0200

    Implements MsModal; Adapts JoinByLinkModal to new usage

---
+++ client/src/components/core/ms-modal/MsModalConfig.ts
@@ -0,0 +1,19 @@
+// Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 (eventually AGPL-3.0) 2016-present Scille SAS
+
+export interface MsModalConfig {
+  title: string,
+  subtitle?: string,
+  closeButtonEnabled: boolean,
+  cancelButton?: {
+    disabled: boolean,
+    label: string,
+    // eslint-disable-next-line @typescript-eslint/ban-types
+    onClick?: Function,
+  },
+  confirmButton?: {
+    disabled: boolean,
+    label: string,
+    // eslint-disable-next-line @typescript-eslint/ban-types
+    onClick?: Function,
+  },
+}
--- client/src/views/home/JoinByLinkModal.vue
+++ client/src/views/home/JoinByLinkModal.vue
@@ -1,91 +1,37 @@
 <!-- Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 (eventually AGPL-3.0) 2016-present Scille SAS -->
 
 <template>
-  <ion-page>
-    <ion-buttons
-      slot="end"
-      class="closeBtn-container"
-    >
-      <ion-button
-        slot="icon-only"
-        @click="closeModal()"
-        class="closeBtn"
-      >
-        <ion-icon
-          :icon="close"
-          size="large"
-          class="closeBtn__icon"
-        />
-      </ion-button>
-    </ion-buttons>
-    <div class="modal">
-      <ion-header class="modal-header">
-        <ion-title class="modal-header__title title-h2">
-          {{ $t('JoinByLinkModal.pageTitle') }}
-        </ion-title>
-        <ion-text
-          class="modal-header__text body"
-        >
-          {{ $t('JoinByLinkModal.pleaseEnterUrl') }}
-        </ion-text>
-      </ion-header>
-      <div class="modal-content inner-content">
-        <ms-input
-          :label="$t('JoinOrganization.linkFormLabel')"
-          :placeholder="$t('JoinOrganization.linkFormPlaceholder')"
-          name="joinOrganization"
-          v-model="joinLink"
-          id="link-orga-input"
-          type="url"
-        />
-      </div>
-      <ion-footer class="modal-footer">
-        <ion-toolbar>
-          <ion-buttons
-            slot="primary"
-            class="modal-footer-buttons"
-          >
-            <ion-button
-              fill="solid"
-              size="default"
-              id="next-button"
-              type="submit"
-              @click="confirm()"
-              :disabled="claimLinkValidator(joinLink) !== Validity.Valid"
-            >
-              {{ $t('JoinByLinkModal.join') }}
-            </ion-button>
-          </ion-buttons>
-        </ion-toolbar>
-      </ion-footer>
-    </div>
-  </ion-page>
+  <ms-modal
+    :title="$t('JoinByLinkModal.pageTitle')"
+    :subtitle="$t('JoinByLinkModal.pleaseEnterUrl')"
+    :close-button-enabled="true"
+    :confirm-button="{
+      label: $t('JoinByLinkModal.join'),
+      disabled: claimLinkValidator(joinLink) !== Validity.Valid,
+      onClick: confirm
+    }"
+  >
+    <ms-input
+      :label="$t('JoinOrganization.linkFormLabel')"
+      :placeholder="$t('JoinOrganization.linkFormPlaceholder')"
+      name="joinOrganization"
+      v-model="joinLink"
+      id="link-orga-input"
+      type="url"
+    />
+  </ms-modal>
 </template>
 
 <script setup lang="ts">
-import {
-  IonText,
-  IonPage,
-  IonHeader,
-  IonTitle,
-  IonToolbar,
-  IonButtons,
-  IonButton,
-  modalController,
-  IonFooter,
-  IonIcon,
-} from '@ionic/vue';
 import { ref } from 'vue';
-import { close } from 'ionicons/icons';
-import { claimLinkValidator, Validity } from '@/common/validators';
+import MsModal from '@/components/core/ms-modal/MsModal.vue';
+import { Validity, claimLinkValidator } from '@/common/validators';
+import { modalController } from '@ionic/vue';
 import { ModalResultCode } from '@/common/constants';
 import MsInput from '@/components/core/ms-input/MsInput.vue';
 
 const joinLink = ref('');
 
-function closeModal(): Promise<boolean> {
-  return modalController.dismiss(null, ModalResultCode.Cancel);
-}
 /* by the way pressing Enter won't send the form, you unfortunately have to click the button
 see https://github.com/ionic-team/ionic-framework/issues/19368 */
 function confirm(): Promise<boolean> {
@@ -94,77 +40,4 @@ function confirm(): Promise<boolean> {
 </script>
 
 <style lang="scss" scoped>
-.modal {
-  padding: 3.5rem;
-  justify-content: start;
-}
-
-.closeBtn-container {
-    position: absolute;
-    top: 2rem;
-    right: 2rem;
-  }
-
-.closeBtn-container, .closeBtn {
-  margin: 0;
-  --padding-start: 0;
-  --padding-end: 0;
-}
-
-.closeBtn {
-  width: fit-content;
-  height: fit-content;
-  --border-radius: var(--parsec-radius-4);
-  --background-hover: var(--parsec-color-light-primary-50);
-  border-radius: var(--parsec-radius-4);
-
-  &__icon {
-    padding: 4px;
-    color: var(--parsec-color-light-primary-500);
-
-    &:hover {
-      --background-hover: var(--parsec-color-light-primary-50);
-    }
-  }
-
-  &:active {
-    border-radius: var(--parsec-radius-4);
-    background: var(--parsec-color-light-primary-100);
-  }
-}
-
-.modal-header {
-  margin-bottom: 2rem;
-
-  &__title {
-    padding: 0;
-    margin-bottom: 1.5rem;
-    color: var(--parsec-color-light-primary-600);
-    display: flex;
-    align-items: center;
-    gap: 1rem;
-  }
-
-  &__text {
-    color: var(--parsec-color-light-secondary-grey);
-  }
-}
-
-.modal-content {
-  --background: transparent;
-}
-
-.modal-footer {
-  margin-top: 2.5rem;
-
-  &::before {
-    background: transparent;
-  }
-
-  &-buttons {
-    display: flex;
-    justify-content: end;
-    gap: 1rem;
-  }
-}
 </style>
