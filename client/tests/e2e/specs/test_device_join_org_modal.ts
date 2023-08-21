703def2872a2953e2ee8964d2eff196739f054db
fabienSvstr <fabien.sevestre@my-digital-school.org>
2023-08-21 13:29:00 +0200

    Creating test for join device

---
+++ client/tests/e2e/specs/test_device_join_org_modal.ts
@@ -0,0 +1,115 @@
+// Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 2016-present Scille SAS
+
+describe('Claim new device', () => {
+  const INVITATION_LINK = 'parsec://parsec.cloud/Test?action=claim_device&token=47265123969c4d6584c2bc15960cf212';
+  const WAIT_TIME = 1000;
+
+  beforeEach(() => {
+    cy.visitApp();
+  });
+
+  afterEach(() => {
+    cy.dropTestbed();
+  });
+
+  it('Open org join modal', () => {
+    cy.get('.join-organization-modal').should('not.exist');
+    cy.get('#create-organization-button').click();
+    cy.get('.popover-viewport').find('ion-item').should('have.length', 2);
+    cy.get('.popover-viewport').find('ion-item').last().contains('Join');
+    cy.get('.popover-viewport').find('ion-item').last().click();
+
+    cy.get('.join-by-link-modal').should('exist');
+    cy.get('.join-by-link-modal').find('ion-title').contains('Join by link');
+    cy.get('.join-by-link-modal').find('ion-footer ion-button').as('joinButton').contains('Join');
+    cy.get('@joinButton').should('have.attr', 'disabled');
+
+    cy.wait(WAIT_TIME);
+    cy.get('.join-by-link-modal').find('ion-input').find('input').type(INVITATION_LINK);
+
+    cy.get('@joinButton').should('not.have.attr', 'disabled');
+    cy.get('@joinButton').click();
+
+    cy.get('.join-by-link-modal').should('not.exist');
+    cy.get('.join-organization-modal').should('exist');
+    cy.get('.modal-header__title').contains('Add a new device');
+
+    cy.wait(WAIT_TIME);
+    cy.get('.label-waiting').should('not.be.visible');
+    cy.get('#next-button').should('be.visible');
+    cy.get('#next-button').contains('I understand!');
+  });
+
+  it('Go through join process', () => {
+    function checkStepper(activeIndex: number): void {
+      cy.get('ion-modal').find('.ms-wizard-stepper-step').as('steps').should('have.length', 3);
+      for (let i = 0; i < 2; i++) {
+        if (i < activeIndex) {
+          cy.get('@steps').eq(i).find('.circle').find('.inner-circle-done').should('exist');
+        } else if (i === activeIndex) {
+          cy.get('@steps').eq(i).find('.circle').find('.inner-circle-active').should('exist');
+        } else {
+          cy.get('@steps').eq(i).find('.circle').find('div').should('have.length', 0);
+        }
+      }
+    }
+
+    cy.get('#create-organization-button').click();
+    cy.get('.popover-viewport').find('ion-item').last().click();
+    cy.wait(WAIT_TIME);
+    cy.get('.join-by-link-modal').find('ion-input').find('input').type(INVITATION_LINK);
+    cy.get('.join-by-link-modal').find('ion-footer ion-button').click();
+
+    cy.get('.join-organization-modal').should('exist');
+    cy.get('.modal-header__title').as('modalTitle').contains('Add a new device');
+
+    cy.wait(WAIT_TIME);
+    cy.get('#next-button').should('be.visible');
+    cy.get('#next-button').click();
+
+    // Page with four codes, host SAS code
+    cy.get('@modalTitle').contains('Get host code');
+    cy.get('.modal-header__text').as('modalSubtitle').contains('Click on the code you see on the main device.');
+
+    cy.get('ion-modal').find('.ms-wizard-stepper__step').as('steps').should('have.length', 3);
+    cy.get('@steps').eq(0).find('.step-title').contains('Host code');
+    cy.get('@steps').eq(1).find('.step-title').contains('Guest code');
+    cy.get('@steps').eq(2).find('.step-title').contains('Password');
+    checkStepper(0);
+
+    cy.get('ion-modal').find('.button-choice').as('choiceButtons').should('have.length', 4);
+    cy.get('@choiceButtons').eq(0).contains('ABCD');
+    cy.get('@choiceButtons').eq(1).contains('EFGH');
+    cy.get('@choiceButtons').eq(2).contains('IJKL');
+    cy.get('@choiceButtons').eq(3).contains('MNOP');
+    cy.get('ion-modal').find('.button-clear').contains('None of the codes');
+    cy.get('@choiceButtons').eq(1).click();
+
+    // Page with one code, guest SAS code
+    cy.get('.label-waiting').should('be.visible');
+    checkStepper(1);
+
+    // Waiting for the host to enter the code
+    cy.wait(WAIT_TIME);
+
+    // Page with two inputs, device new password
+    checkStepper(2);
+
+    cy.get('@modalTitle').contains('Create a password');
+    cy.get('@modalSubtitle').contains('Finally, create a password for your new device.');
+
+    cy.get('#get-password').find('ion-input').as('inputs').should('have.length', 2);
+    cy.get('#next-button').should('have.attr', 'disabled');
+    cy.get('#next-button').contains('Confirm password');
+    cy.get('@inputs').eq(0).find('input').type('Password23;-$aze');
+    cy.get('#next-button').should('have.attr', 'disabled');
+    cy.get('@inputs').eq(1).find('input').type('Password23;-$aze');
+    cy.get('#next-button').should('not.have.attr', 'disabled');
+    cy.get('#next-button').click();
+
+    // Last page
+    cy.get('@modalTitle').contains('The device has been added!');
+
+    cy.get('#next-button').contains('Log in');
+  });
+});
