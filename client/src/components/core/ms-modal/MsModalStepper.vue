933721159e1e22bd53989826bf3eb92ce8c2e08e
fabienSvstr <fabien.sevestre@my-digital-school.org>
2023-08-23 09:31:04 +0200

    Created component MsModalStepper

---
+++ client/src/components/core/ms-modal/MsModalStepper.vue
@@ -0,0 +1,244 @@
+<!-- Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 2016-present Scille SAS -->
+
+<template>
+  <ion-page>
+    <wizard-stepper
+      v-show="stepper.show"
+      :current-index="stepper.currentIndex"
+      :titles="stepper.titles"
+    />
+    <ion-buttons
+      slot="end"
+      class="closeBtn-container"
+      v-if="closeButton.show"
+    >
+      <ion-button
+        slot="icon-only"
+        @click="closeButton.onClick"
+        class="closeBtn"
+      >
+        <ion-icon
+          :icon="close"
+          size="large"
+          class="closeBtn__icon"
+        />
+      </ion-button>
+    </ion-buttons>
+    <div class="ms-modal">
+      <ion-header class="ms-modal-header">
+        <ion-title class="ms-modal-header__title title-h2">
+          {{ title }}
+        </ion-title>
+        <ion-text
+          class="ms-modal-header__text body"
+          v-if="subtitle"
+        >
+          {{ subtitle }}
+        </ion-text>
+      </ion-header>
+      <div
+        class="ms-modal-content inner-content"
+      >
+        <slot />
+      </div>
+      <ion-footer class="ms-modal-footer">
+        <ion-toolbar>
+          <ion-buttons
+            slot="primary"
+            class="ms-modal-footer-buttons"
+          >
+            <ion-button
+              v-if="cancelButton"
+              fill="clear"
+              size="default"
+              id="cancel-button"
+              @click="cancelButton.onClick ? cancelButton.onClick() : cancel()"
+              :disabled="cancelButton.disabled"
+            >
+              {{ cancelButton.label }}
+            </ion-button>
+            <ion-button
+              v-if="confirmButton"
+              fill="solid"
+              size="default"
+              id="next-button"
+              type="submit"
+              @click="confirmButton.onClick ? confirmButton.onClick() : confirm()"
+              :disabled="confirmButton.disabled"
+              v-show="confirmButton.show"
+            >
+              {{ confirmButton.label }}
+            </ion-button>
+            <div
+              v-show="spinner.show"
+              class="spinner-container"
+            >
+              <ion-label
+                class="label-waiting"
+              >
+                {{ spinner.label }}
+              </ion-label>
+              <ion-spinner
+                name="crescent"
+                color="primary"
+              />
+            </div>
+          </ion-buttons>
+        </ion-toolbar>
+      </ion-footer>
+    </div>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import {
+  IonText,
+  IonPage,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonButtons,
+  IonButton,
+  modalController,
+  IonFooter,
+  IonIcon,
+  IonSpinner,
+  IonLabel,
+} from '@ionic/vue';
+import { close } from 'ionicons/icons';
+import { ModalResultCode } from '@/common/constants';
+import WizardStepper from '@/components/core/ms-stepper/MsWizardStepper.vue';
+
+// We are forced to re-define interface here since we are not in Vue 3.3 see: https://github.com/vuejs/core/issues/4294
+export interface MsModalConfig {
+  title: string,
+  subtitle?: string,
+  closeButton: {
+    show: boolean,
+    // eslint-disable-next-line @typescript-eslint/ban-types
+    onClick?: Function,
+  },
+  cancelButton?: {
+    show: boolean,
+    disabled: boolean,
+    label: string,
+    // eslint-disable-next-line @typescript-eslint/ban-types
+    onClick?: Function,
+  },
+  confirmButton?: {
+    show: boolean,
+    disabled: boolean,
+    label: string,
+    // eslint-disable-next-line @typescript-eslint/ban-types
+    onClick?: Function,
+  },
+  spinner: {
+    show: boolean,
+    label: string,
+  },
+  stepper: {
+    show: boolean,
+    currentIndex: number,
+    titles: string[],
+  }
+}
+
+defineProps<MsModalConfig>();
+
+function cancel(): Promise<boolean> {
+  return modalController.dismiss(null, ModalResultCode.Cancel);
+}
+function confirm(): Promise<boolean> {
+  return modalController.dismiss(null, ModalResultCode.Confirm);
+}
+</script>
+
+<style lang="scss" scoped>
+.ms-modal {
+  padding: 3.5rem;
+  justify-content: start;
+}
+
+.closeBtn-container {
+    position: absolute;
+    top: 2rem;
+    right: 2rem;
+  }
+
+.closeBtn-container, .closeBtn {
+  margin: 0;
+  --padding-start: 0;
+  --padding-end: 0;
+}
+
+.closeBtn {
+  width: fit-content;
+  height: fit-content;
+  --border-radius: var(--parsec-radius-4);
+  --background-hover: var(--parsec-color-light-primary-50);
+  border-radius: var(--parsec-radius-4);
+
+  &__icon {
+    padding: 4px;
+    color: var(--parsec-color-light-primary-500);
+
+    &:hover {
+      --background-hover: var(--parsec-color-light-primary-50);
+    }
+  }
+
+  &:active {
+    border-radius: var(--parsec-radius-4);
+    background: var(--parsec-color-light-primary-100);
+  }
+}
+
+.ms-modal-header {
+  margin-bottom: 2rem;
+
+  &__title {
+    padding: 0;
+    margin-bottom: 1.5rem;
+    color: var(--parsec-color-light-primary-600);
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+  }
+
+  &__text {
+    color: var(--parsec-color-light-secondary-grey);
+  }
+}
+
+.ms-modal-content {
+  --background: transparent;
+}
+
+.ms-modal-footer {
+  margin-top: 2.5rem;
+
+  &::before {
+    background: transparent;
+  }
+
+  &-buttons {
+    display: flex;
+    justify-content: end;
+    gap: 1rem;
+  }
+
+  .label-waiting {
+  color: var(--parsec-color-light-secondary-grey);
+  font-style: italic;
+  padding-left: 2em;
+  padding-right: 2em;
+}
+
+  .spinner-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+  }
+}
+</style>
