604624d0c589096e5e77f69b2ac79022da6a1709
Maxime GRANDCOLAS <maxime.grandcolas@scille.fr>
2023-09-12 11:28:45 +0200

    [MS] Cleaned devices page

--- client/src/views/devices/DevicesPage.vue
+++ client/src/views/devices/DevicesPage.vue
@@ -4,24 +4,36 @@
   <ion-page>
     <ion-content :fullscreen="true">
       <div class="devices-container">
-        <h1>{{ $t("DevicesPage.title") }}</h1>
-        <ion-button
-          fill="outline"
-          @click="onAddDeviceClick()"
-        >
-          <ion-icon
-            :icon="add"
-          />
-          {{ $t("DevicesPage.addDevice") }}
-        </ion-button>
-        <ion-list>
-          <ion-item
-            v-for="device in devices"
-            :key="device.slug"
+        <div class="title">
+          <h1>{{ $t('DevicesPage.title') }}</h1>
+          <ion-button
+            fill="clear"
+            @click="onAddDeviceClick()"
           >
-            <span>{{ device.deviceLabel }}</span>
-          </ion-item>
-        </ion-list>
+            <ion-icon
+              :icon="add"
+            />
+            {{ $t('DevicesPage.addDevice') }}
+          </ion-button>
+        </div>
+        <div class="list">
+          <ion-text v-if="devices.length === 0">
+            {{ $t('DevicesPage.noDevices') }}
+          </ion-text>
+          <ion-list
+            v-if="devices.length > 0"
+          >
+            <ion-item
+              v-for="device in devices"
+              :key="device.slug"
+            >
+              <device-card
+                :label="device.label || ''"
+                :is-current="true"
+              />
+            </ion-item>
+          </ion-list>
+        </div>
       </div>
     </ion-content>
   </ion-page>
@@ -30,10 +42,18 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import { add } from 'ionicons/icons';
-import { IonButton, IonList, IonItem, IonIcon, IonPage, IonContent } from '@ionic/vue';
-import { getMockDevices } from '@/common/mocks';
+import {
+  IonButton,
+  IonList,
+  IonItem,
+  IonIcon,
+  IonPage,
+  IonContent,
+  IonText,
+} from '@ionic/vue';
+import DeviceCard from '@/components/devices/DeviceCard.vue';
 
-const devices = ref(getMockDevices());
+const devices = ref([{label: 'A Mock Device', slug: 1}]);
 
 function onAddDeviceClick(): void {
   console.log('Add device clicked');
@@ -44,5 +64,23 @@ function onAddDeviceClick(): void {
 .devices-container {
   margin: 2em;
   background-color: white;
+  width: 50%;
+}
+
+.title {
+  display: flex;
+  border-bottom: 1px solid var(--parsec-color-light-secondary-disabled);
+}
+
+.title h1 {
+  color: var(--parsec-color-light-primary-700);
+}
+
+.title ion-button {
+  margin-left: auto;
+}
+
+.list {
+  padding-top: 2em;
 }
 </style>
