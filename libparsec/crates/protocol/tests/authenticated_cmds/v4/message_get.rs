9b6b767827495aca359b4a3b1aca2c9cf7cb7778
firelight flagboy <firelight.flagboy@gmail.com>
2023-09-20 10:30:20 +0200

    Migrate `message_get` tests

--- libparsec/crates/protocol/tests/authenticated_cmds/v4/message_get.rs
+++ libparsec/crates/protocol/tests/authenticated_cmds/v4/message_get.rs
@@ -1,15 +1,79 @@
 // Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 2016-present Scille SAS
 
+#![allow(clippy::unwrap_used)]
+
+use libparsec_tests_lite::{hex, p_assert_eq};
+
 use super::authenticated_cmds;
 
 // Request
 
 pub fn req() {
-    // TODO #4545: Implement test
+    // Generated from Python implementation (Parsec v2.6.0+dev)
+    // Content:
+    //   cmd: "message_get"
+    //   offset: 8
+    let raw = hex!("82a3636d64ab6d6573736167655f676574a66f666673657408");
+
+    let req = authenticated_cmds::message_get::Req { offset: 8 };
+
+    let expected = authenticated_cmds::AnyCmdReq::MessageGet(req);
+
+    let data = authenticated_cmds::AnyCmdReq::load(&raw).unwrap();
+
+    p_assert_eq!(data, expected);
+
+    let authenticated_cmds::AnyCmdReq::MessageGet(req2) = data else {
+        unreachable!()
+    };
+
+    let raw2 = req2.dump().unwrap();
+
+    let data2 = authenticated_cmds::AnyCmdReq::load(&raw2).unwrap();
+
+    p_assert_eq!(data2, expected);
 }
 
 // Responses
 
 pub fn rep_ok() {
-    // TODO #4545: Implement test
+    // Generated from Python implementation (Parsec v2.16.0+dev)
+    // Content:
+    //   messages: [
+    //     {
+    //       index: 1
+    //       certificate_index: 0
+    //       timestamp: 2000-1-2T01:00:00Z
+    //       body: b"foobar"
+    //       count: 1
+    //       sender: "alice@dev1"
+    //     }
+    //   ]
+    //   status: "ok"
+    let raw = hex!(
+        "82a6737461747573a26f6ba86d657373616765739185a4626f6479c406666f6f626172b163"
+        "657274696669636174655f696e64657800a5696e64657801a673656e646572aa616c696365"
+        "4064657631a974696d657374616d70d70141cc375188000000"
+    );
+
+    let expected = authenticated_cmds::message_get::Rep::Ok {
+        messages: vec![authenticated_cmds::message_get::Message {
+            index: 1,
+            sender: "alice@dev1".parse().unwrap(),
+            timestamp: "2000-1-2T01:00:00Z".parse().unwrap(),
+            body: b"foobar".as_ref().into(),
+            certificate_index: 0,
+        }],
+    };
+
+    let data = authenticated_cmds::message_get::Rep::load(&raw).unwrap();
+
+    p_assert_eq!(data, expected);
+
+    // Also test serialization round trip
+    let raw2 = data.dump().unwrap();
+
+    let data2 = authenticated_cmds::message_get::Rep::load(&raw2).unwrap();
+
+    p_assert_eq!(data2, expected);
 }
