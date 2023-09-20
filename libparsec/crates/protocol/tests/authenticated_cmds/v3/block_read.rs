a80f9d5f885ef88f7f044b14d0ebf0d5c4897e73
firelight flagboy <firelight.flagboy@gmail.com>
2023-09-20 16:26:23 +0200

    Migrage `block_read` tests

--- libparsec/crates/protocol/tests/authenticated_cmds/v3/block_read.rs
+++ libparsec/crates/protocol/tests/authenticated_cmds/v3/block_read.rs
@@ -1,31 +1,103 @@
 // Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 2016-present Scille SAS
 
+// `allow-unwrap-in-test` don't behave as expected, see:
+// https://github.com/rust-lang/rust-clippy/issues/11119
+#![allow(clippy::unwrap_used)]
+
+use libparsec_tests_lite::{hex, p_assert_eq};
+use libparsec_types::BlockID;
+
 use super::authenticated_cmds;
 
 // Request
 
 pub fn req() {
-    // TODO #4545: Implement test
+    // Generated from Python implementation (Parsec v2.6.0+dev)
+    // Content:
+    //   block_id: ext(2, hex!("57c629b69d6c4abbaf651cafa46dbc93"))
+    //   cmd: "block_read"
+    let raw = hex!(
+        "82a8626c6f636b5f6964d80257c629b69d6c4abbaf651cafa46dbc93a3636d64aa626c6f63"
+        "6b5f72656164"
+    );
+
+    let req = authenticated_cmds::block_read::Req {
+        block_id: BlockID::from_hex("57c629b69d6c4abbaf651cafa46dbc93").unwrap(),
+    };
+
+    let expected = authenticated_cmds::AnyCmdReq::BlockRead(req.clone());
+
+    let data = authenticated_cmds::AnyCmdReq::load(&raw).unwrap();
+
+    p_assert_eq!(data, expected);
+
+    // Also test serialization round trip
+    let raw2 = req.dump().unwrap();
+
+    let data2 = authenticated_cmds::AnyCmdReq::load(&raw2).unwrap();
+
+    p_assert_eq!(data2, expected);
 }
 
 // Responses
 
 pub fn rep_ok() {
-    // TODO #4545: Implement test
+    // Generated from Python implementation (Parsec v2.6.0+dev)
+    // Content:
+    //   block: hex!("666f6f626172")
+    //   status: "ok"
+    let raw = hex!("82a5626c6f636bc406666f6f626172a6737461747573a26f6b");
+    let expected = authenticated_cmds::block_read::Rep::Ok {
+        block: bytes::Bytes::from_static(b"foobar"),
+    };
+    rep_helper(&raw, expected);
 }
 
 pub fn rep_not_found() {
-    // TODO #4545: Implement test
+    // Generated from Python implementation (Parsec v2.6.0+dev)
+    // Content:
+    //   status: "not_found"
+    let raw = hex!("81a6737461747573a96e6f745f666f756e64");
+    let expected = authenticated_cmds::block_read::Rep::NotFound;
+    rep_helper(&raw, expected);
 }
 
 pub fn rep_timeout() {
-    // TODO #4545: Implement test
+    // Generated from Python implementation (Parsec v2.6.0+dev)
+    // Content:
+    //   status: "timeout"
+    let raw = hex!("81a6737461747573a774696d656f7574");
+    let expected = authenticated_cmds::block_read::Rep::Timeout;
+    rep_helper(&raw, expected);
 }
 
 pub fn rep_not_allowed() {
-    // TODO #4545: Implement test
+    // Generated from Python implementation (Parsec v2.6.0+dev)
+    // Content:
+    //   status: "not_allowed"
+    let raw = hex!("81a6737461747573ab6e6f745f616c6c6f776564");
+    let expected = authenticated_cmds::block_read::Rep::NotAllowed;
+    rep_helper(&raw, expected);
 }
 
 pub fn rep_in_maintenance() {
-    // TODO #4545: Implement test
+    // Generated from Python implementation (Parsec v2.6.0+dev)
+    // Content:
+    //   status: "in_maintenance"
+    let raw = hex!("81a6737461747573ae696e5f6d61696e74656e616e6365");
+    let expected = authenticated_cmds::block_read::Rep::InMaintenance;
+    rep_helper(&raw, expected);
+}
+
+fn rep_helper(raw: &[u8], expected: authenticated_cmds::block_read::Rep) {
+    let data = authenticated_cmds::block_read::Rep::load(raw).unwrap();
+
+    p_assert_eq!(data, expected);
+
+    // Also test serialization round trip
+    let raw2 = data.dump().unwrap();
+
+    let data2 = authenticated_cmds::block_read::Rep::load(&raw2).unwrap();
+
+    p_assert_eq!(data2, expected);
 }
