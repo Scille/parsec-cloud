bc7d608a8bd24f2d92ff8d8d1e3bea07568d20eb
firelight flagboy <firelight.flagboy@gmail.com>
2023-09-22 16:49:04 +0200

    Migrate `auth_cmds::invite_3a_greeter_wait_peer_trust`

--- libparsec/crates/protocol/tests/authenticated_cmds/v3/invite_3a_greeter_wait_peer_trust.rs
+++ libparsec/crates/protocol/tests/authenticated_cmds/v3/invite_3a_greeter_wait_peer_trust.rs
@@ -1,27 +1,98 @@
 // Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 2016-present Scille SAS
 
+// `allow-unwrap-in-test` don't behave as expected, see:
+// https://github.com/rust-lang/rust-clippy/issues/11119
+#![allow(clippy::unwrap_used)]
+
+use libparsec_tests_lite::{hex, p_assert_eq};
+use libparsec_types::InvitationToken;
+
 use super::authenticated_cmds;
 
 // Request
 
 pub fn req() {
-    // TODO #4545: Implement test
+    // Generated from Python implementation (Parsec v2.6.0+dev)
+    // Content:
+    //   cmd: "invite_3a_greeter_wait_peer_trust"
+    //   token: ext(2, hex!("d864b93ded264aae9ae583fd3d40c45a"))
+    let raw = hex!(
+        "82a3636d64d921696e766974655f33615f677265657465725f776169745f706565725f7472"
+        "757374a5746f6b656ed802d864b93ded264aae9ae583fd3d40c45a"
+    );
+
+    let req = authenticated_cmds::invite_3a_greeter_wait_peer_trust::Req {
+        token: InvitationToken::from_hex("d864b93ded264aae9ae583fd3d40c45a").unwrap(),
+    };
+
+    let expected = authenticated_cmds::AnyCmdReq::Invite3aGreeterWaitPeerTrust(req);
+
+    let data = authenticated_cmds::AnyCmdReq::load(&raw).unwrap();
+
+    p_assert_eq!(data, expected);
+
+    // Also test serialization round trip
+    let authenticated_cmds::AnyCmdReq::Invite3aGreeterWaitPeerTrust(req2) = data else {
+        unreachable!()
+    };
+    let raw2 = req2.dump().unwrap();
+
+    let data2 = authenticated_cmds::AnyCmdReq::load(&raw2).unwrap();
+
+    p_assert_eq!(data2, expected);
 }
 
 // Responses
 
 pub fn rep_ok() {
-    // TODO #4545: Implement test
+    // Generated from Python implementation (Parsec v2.6.0+dev)
+    // Content:
+    //   status: "ok"
+    let raw = hex!("81a6737461747573a26f6b");
+    let expected = authenticated_cmds::invite_3a_greeter_wait_peer_trust::Rep::Ok;
+
+    rep_helper(&raw, expected);
 }
 
 pub fn rep_not_found() {
-    // TODO #4545: Implement test
+    // Generated from Python implementation (Parsec v2.6.0+dev)
+    // Content:
+    //   status: "not_found"
+    let raw = hex!("81a6737461747573a96e6f745f666f756e64");
+    let expected = authenticated_cmds::invite_3a_greeter_wait_peer_trust::Rep::NotFound;
+
+    rep_helper(&raw, expected);
 }
 
 pub fn rep_already_deleted() {
-    // TODO #4545: Implement test
+    // Generated from Python implementation (Parsec v2.6.0+dev)
+    // Content:
+    //   status: "already_deleted"
+    let raw = hex!("81a6737461747573af616c72656164795f64656c65746564");
+    let expected = authenticated_cmds::invite_3a_greeter_wait_peer_trust::Rep::AlreadyDeleted;
+
+    rep_helper(&raw, expected);
 }
 
 pub fn rep_invalid_state() {
-    // TODO #4545: Implement test
+    // Generated from Python implementation (Parsec v2.6.0+dev)
+    // Content:
+    //   status: "invalid_state"
+    let raw = hex!("81a6737461747573ad696e76616c69645f7374617465");
+    let expected = authenticated_cmds::invite_3a_greeter_wait_peer_trust::Rep::InvalidState;
+
+    rep_helper(&raw, expected);
+}
+
+fn rep_helper(raw: &[u8], expected: authenticated_cmds::invite_3a_greeter_wait_peer_trust::Rep) {
+    let data = authenticated_cmds::invite_3a_greeter_wait_peer_trust::Rep::load(raw).unwrap();
+
+    p_assert_eq!(data, expected);
+
+    // Also test serialization round trip
+    let raw2 = data.dump().unwrap();
+
+    let data2 = authenticated_cmds::invite_3a_greeter_wait_peer_trust::Rep::load(&raw2).unwrap();
+
+    p_assert_eq!(data2, expected);
 }
