7ed67be9316be017230ce798e68c09121ec2bc87
firelight flagboy <firelight.flagboy@gmail.com>
2023-09-27 10:47:55 +0200

    Add documentation to install the parsec backend on-premise
    This documentation provide a basic `parsec` infra to test the parsec
    solution.
    It rely on `docker-compose` to host the needed services for the infra
    like the database, the s3 and the server.
    
    Co-authored-by: Marcos Medrano <786907+mmmarcos@users.noreply.github.com>

---
+++ docs/adminguide/hosting/setup-tls.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+function generate_cert_conf() {
+    local name=$1
+    local san=$2
+
+    echo "Generating $name.crt.conf"
+
+    cat << EOF > $name.crt.conf
+[req]
+distinguished_name = req_dist_name
+req_extensions = req_ext
+prompt = no
+
+[req_dist_name]
+CN = $name
+
+[req_ext]
+subjectAltName = $san
+EOF
+}
+
+function generate_certificate_request() {
+    local name=$1
+    echo "Generate certificate request $name.csr"
+    openssl req -batch \
+        -new -sha512 -noenc -newkey rsa:4096 \
+        -config $name.crt.conf \
+        -keyout $name.key -out $name.csr
+}
+
+function sign_crt_with_ca() {
+    local ca_crt=$1
+    local ca_key=$2
+    local name=$3
+
+    echo "Sign certificate request $name.crt"
+
+    openssl x509 -req -in $name.csr \
+        -CA $ca_crt -CAkey $ca_key \
+        -extfile $name.crt.conf \
+        -extensions req_ext \
+        -CAcreateserial -out $name.crt \
+        -days 10 -sha512
+}
+
+if [ ! -f custom-ca.key ]; then
+    echo "Generate a mini Certificate Authority"
+    openssl req -batch \
+        -x509 -sha512 -nodes -days 10 -newkey rsa:4096 \
+        -subj "/CN=Mini Certificate Authority" \
+        -keyout custom-ca.key -out custom-ca.crt
+fi
+
+for service in parsec-{s3,backend}; do
+    if [ ! -f $service.crt.conf ]; then
+        generate_cert_conf $service DNS:$service,DNS:localhost,IP:127.0.0.1
+    fi
+
+    if [ ! -f $service.key ]; then
+        generate_certificate_request $service
+    fi
+
+    if [ ! -f $service.crt ]; then
+        sign_crt_with_ca custom-ca.{crt,key} $service
+    fi
+done
+
+if [ $(stat -c %g parsec-backend.key) -ne 1234 ]; then
+    echo "Changing group id of parsec-backend.key to 1234"
+    sudo chown $USER:1234 parsec-backend.key
+fi
+
+if [ $(stat -c %a parsec-backend.key) -ne 640 ]; then
+    echo "Changing permission of parsec-backend.key to 640"
+    chmod 640 parsec-backend.key
+fi
