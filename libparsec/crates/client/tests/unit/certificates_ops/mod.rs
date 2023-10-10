faccbd6a1eb43ec1665fe17af787ba75f471d590
TimeEngineer <chenpascal241096@yahoo.fr>
2023-10-10 17:54:47 +0200

    Add tests for CertificatesOps add_certificates_batch device

--- libparsec/crates/client/tests/unit/certificates_ops/mod.rs
+++ libparsec/crates/client/tests/unit/certificates_ops/mod.rs
@@ -1,6 +1,7 @@
 // Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 2016-present Scille SAS
 
 mod add_common;
+mod add_device_certificate;
 mod add_user_certificate;
 mod store;
 mod utils;
