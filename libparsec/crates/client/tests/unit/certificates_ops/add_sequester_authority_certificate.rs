8882d017dd27fdd5b466151eaca28637cceb77ba
TimeEngineer <chenpascal241096@yahoo.fr>
2023-10-13 17:13:47 +0200

    Add tests for CertificatesOps add_certificates_batch sequester_authority

---
+++ libparsec/crates/client/tests/unit/certificates_ops/add_sequester_authority_certificate.rs
@@ -0,0 +1,121 @@
+// Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 2016-present Scille SAS
+
+use libparsec_tests_fixtures::prelude::*;
+use libparsec_types::prelude::*;
+
+use crate::certificates_ops::{AddCertificateError, InvalidCertificateError, MaybeRedactedSwitch};
+
+use super::utils::certificates_ops_factory;
+
+#[parsec_test(testbed = "empty")]
+async fn ok(env: &TestbedEnv) {
+    let env = env.customize(|builder| {
+        builder
+            .bootstrap_organization("alice")
+            .and_set_sequestered_organization();
+    });
+    let alice = env.local_device("alice@dev1");
+    let ops = certificates_ops_factory(&env, &alice).await;
+
+    let store = ops.store.for_write().await;
+    let (_, authority_signed) = env.get_sequester_authority_certificate();
+
+    let switch = ops
+        .add_certificates_batch(&store, 0, [authority_signed].into_iter())
+        .await
+        .unwrap();
+
+    p_assert_matches!(switch, MaybeRedactedSwitch::NoSwitch);
+}
+
+#[parsec_test(testbed = "empty")]
+async fn content_already_exists(env: &TestbedEnv) {
+    let env = env.customize(|builder| {
+        builder
+            .bootstrap_organization("alice")
+            .and_set_sequestered_organization();
+    });
+    let alice = env.local_device("alice@dev1");
+    let ops = certificates_ops_factory(&env, &alice).await;
+
+    let store = ops.store.for_write().await;
+    let (_, authority_signed) = env.get_sequester_authority_certificate();
+
+    let err = ops
+        .add_certificates_batch(
+            &store,
+            0,
+            [authority_signed.clone(), authority_signed].into_iter(),
+        )
+        .await
+        .unwrap_err();
+
+    p_assert_matches!(
+        err,
+        AddCertificateError::InvalidCertificate(
+            InvalidCertificateError::ContentAlreadyExists { .. }
+        )
+    )
+}
+
+#[parsec_test(testbed = "empty")]
+async fn index_already_exists(env: &TestbedEnv) {
+    let env = env.customize(|builder| {
+        builder
+            .bootstrap_organization("alice")
+            .and_set_sequestered_organization();
+    });
+    let alice = env.local_device("alice@dev1");
+    let ops = certificates_ops_factory(&env, &alice).await;
+
+    let store = ops.store.for_write().await;
+    let (_, alice_signed) = env.get_user_certificate("alice");
+    let (_, authority_signed) = env.get_sequester_authority_certificate();
+
+    ops.add_certificates_batch(&store, 0, [alice_signed].into_iter())
+        .await
+        .unwrap();
+
+    let err = ops
+        .add_certificates_batch(&store, 0, [authority_signed].into_iter())
+        .await
+        .unwrap_err();
+
+    p_assert_matches!(
+        err,
+        AddCertificateError::InvalidCertificate(InvalidCertificateError::IndexAlreadyExists { .. })
+    );
+}
+
+#[parsec_test(testbed = "empty")]
+async fn invalid_timestamp(env: &TestbedEnv) {
+    let env = env.customize(|builder| {
+        builder
+            .bootstrap_organization("alice")
+            .and_set_sequestered_organization();
+    });
+    let alice = env.local_device("alice@dev1");
+    let ops = certificates_ops_factory(&env, &alice).await;
+
+    let store = ops.store.for_write().await;
+    let (_, alice_signed) = env.get_user_certificate("alice");
+    let (authority_certif, _) = env.get_sequester_authority_certificate();
+
+    let mut old_authority_certif = (*authority_certif).clone();
+    old_authority_certif.timestamp = DateTime::from_ymd_hms_us(1999, 1, 1, 0, 0, 0, 0).unwrap();
+    let old_authority_signed = Bytes::from(old_authority_certif.dump_and_sign(&alice.signing_key));
+
+    let err = ops
+        .add_certificates_batch(&store, 0, [alice_signed, old_authority_signed].into_iter())
+        .await
+        .unwrap_err();
+
+    p_assert_matches!(
+        err,
+        AddCertificateError::InvalidCertificate(InvalidCertificateError::InvalidTimestamp {
+            last_certificate_timestamp,
+            ..
+        })
+        if last_certificate_timestamp == DateTime::from_ymd_hms_us(2000, 1, 2, 0, 0, 0, 0).unwrap()
+    );
+}
--- libparsec/crates/client/tests/unit/certificates_ops/mod.rs
+++ libparsec/crates/client/tests/unit/certificates_ops/mod.rs
@@ -2,6 +2,7 @@
 
 mod add_common;
 mod add_device_certificate;
+mod add_sequester_authority_certificate;
 mod add_user_certificate;
 mod add_user_revoked_certificate;
 mod add_user_update_certificate;
