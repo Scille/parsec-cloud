b7045ef9223aa715c6055058a993c240ce0e8309
Firelight Flagboy <firelight.flagboy@gmail.com>
2023-10-27 11:54:06 +0200

    Handle `docs/pyproject.toml` python versions

--- misc/version_updater.py
+++ misc/version_updater.py
@@ -134,6 +134,10 @@ FILES_WITH_VERSION_INFO: Dict[Path, Dict[Tool, RawRegexes]] = {
     ROOT_DIR
     / "docs/conf.py": {Tool.Parsec: [ReplaceRegex(r'version = ".*"', 'version = "{version}"')]},
     ROOT_DIR
+    / "docs/pyproject.toml": {
+        Tool.Python: [ReplaceRegex(r'^python = "\^[0-9.]+"$', 'python = "^{version}"')]
+    },
+    ROOT_DIR
     / "licenses/BUSL-Scille.txt": {
         Tool.Parsec: [
             ReplaceRegex(r"^Licensed Work:  Parsec v.*$", "Licensed Work:  Parsec v{version}")
