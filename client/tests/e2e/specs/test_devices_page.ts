f9cae4e32151215b64a93e12c29bb8f14379e906
fabienSvstr <fabien.sevestre@my-digital-school.org>
2023-11-08 16:44:21 +0100

    Fixed locales, test (device_page) and date

--- client/tests/e2e/specs/test_devices_page.ts
+++ client/tests/e2e/specs/test_devices_page.ts
@@ -20,4 +20,14 @@ describe('Display client devices', () => {
     cy.get('@devices').first().find('ion-text').contains('My First Device');
     cy.get('@devices').first().find('ion-text').contains('Current');
   });
+
+  // check if restore-password section is displayed
+  it('Check if restore-password section is displayed', () => {
+    cy.get('.restore-password').find('h3').contains('Password recovery files');
+    cy.get('.restore-password').find('ion-label').should('have.class', 'danger');
+    cy.get('.restore-password').find('.restore-password-button').find('ion-button').contains('Create recovery files');
+    cy.get('.restore-password').find('.restore-password-button').find('ion-button').click();
+    cy.get('.restore-password').find('ion-label').should('have.class', 'done');
+    cy.get('.restore-password').find('.restore-password-button').find('ion-button').contains('Downloading files again');
+  });
 });
