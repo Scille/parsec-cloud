85aa5bc3e04a3b94cc0e3f5ba75fb17ee89bd2cc
fabienSvstr <fabien.sevestre@my-digital-school.org>
2023-11-28 11:42:45 +0100

    tooltip

--- client/src/components/core/ms-dropdown/MsDropdownPopover.vue
+++ client/src/components/core/ms-dropdown/MsDropdownPopover.vue
@@ -11,14 +11,35 @@
       v-for="option in options"
       :key="option.key"
       @click="onOptionClick(option)"
+      @mouseenter="openTooltip($event, option.label)"
+      @mouseleave="option.disabled"
     >
-      {{ option.label }}
+      <div class="option-text">
+        <span class="option-text__title body">{{ option.label }}</span>
+        <span class="option-text__subtitle body-sm">{{ option.sublabel }}</span>
+      </div>
       <ion-icon
         slot="end"
         :icon="checkmark"
-        class="checked"
+        class="icon checked"
         :class="{ selected: selectedOption?.key === option.key }"
         v-if="selectedOption?.key === option.key"
+      />
+      <button
+        v-tooltip
+        title="Hello World!"
+      >
+        coucou
+        <ion-icon
+          slot="end"
+          :icon="informationCircle"
+          class="icon disabled-icon"
+          v-if="option.disabled"
+        />
+      </button>
+      <ms-dropdown-tooltip
+        :text="option.label"
+        class="tooltip"
       />
     </ion-item>
   </ion-list>
@@ -27,8 +48,10 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import { IonList, IonItem, IonIcon, popoverController } from '@ionic/vue';
-import { checkmark } from 'ionicons/icons';
+import { checkmark, informationCircle } from 'ionicons/icons';
 import { MsDropdownOption, getMsOptionByKey } from '@/components/core/ms-types';
+import MsDropdownTooltip from '@/components/core/ms-dropdown/MsDropdownTooltip.vue';
+import { tooltip } from '@ionited/tooltip-vue';
 
 const props = defineProps<{
   defaultOption?: any;
@@ -48,17 +71,84 @@ function onOptionClick(option?: MsDropdownOption): void {
 </script>
 
 <style lang="scss" scoped>
+.container {
+  padding: .5rem;
+}
+
+// eslint-disable-next-line vue-scoped-css/no-unused-selector
 .option {
-  --background-hover: var(--parsec-color-light-primary-50);
-  --background-hover-opacity: 1;
   --color: var(--parsec-color-light-secondary-grey);
-  --color-hover: var(--parsec-color-light-primary-700);
+  padding: .75rem 0 .75rem .75rem;
+  --background: none;
+  border-radius: var(--parsec-radius-6);
+  --min-height: 0;
+  display: flex;
+  align-items: center;
+  position: relative;
+  z-index: 2;
+
+  &:hover {
+    background: var(--parsec-color-light-primary-50);
+    --background-hover: var(--parsec-color-light-primary-50);
+    --color-hover: var(--parsec-color-light-primary-700);
+  }
+
+  &::part(native) {
+    padding: 0;
+  }
+
+  &-text {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    &__subtitle {
+      margin-bottom:  .25rem;
+    }
+  }
+
+  .icon {
+    margin: 0;
+  }
 
   &.selected {
-    color: var(--parsec-color-light-primary-700);
+
+    .option-text {
+
+      &__title {
+        color: var(--parsec-color-light-primary-700);
+      }
+
+      &__subtitle {
+        color: var(--parsec-color-light-secondary-grey);
+      }
+    }
   }
+
   .checked.selected {
     color: var(--parsec-color-light-primary-700);
+  }
+
+  &.item-disabled {
+    opacity: 1;
+
+    .option-text {
+      opacity: .5;
+
+      &__title {
+        --color: var(--parsec-color-light-secondary-text);
+      }
+
+      &__subtitle {
+        --color: var(--parsec-color-light-secondary-grey);
+      }
+    }
+
+    .disabled-icon {
+      opacity: .8;
+      --color: var(--parsec-color-light-secondary-grey);
+      position: relative;
+    }
   }
 }
 </style>
---
+++ client/src/components/core/ms-dropdown/MsDropdownTooltip.vue
@@ -0,0 +1,43 @@
+<!-- Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 2016-present Scille SAS -->
+
+<template>
+  <div class="tooltip-container">
+    <ion-text class="tooltip-text body-sm">
+      {{ text }}
+    </ion-text>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { IonText } from '@ionic/vue';
+import { defineProps } from 'vue';
+
+defineProps<{
+  text: string;
+}>();
+</script>
+
+<style lang="scss" scoped>
+.tooltip-container {
+  background: var(--parsec-color-light-primary-900);
+  color: var(--parsec-color-light-primary-30);
+  max-width: 14.5rem;
+  padding: .75rem;
+  position: sticky;
+  border-radius: var(--parsec-radius-6);
+  box-shadow: var(--parsec-shadow-light);
+  text-align: center;
+}
+
+/* Tooltip arrow */
+.tooltip-container::after {
+  content: "";
+  position: absolute;
+  top: 100%;
+  left: 50%;
+  margin-left: -10px;
+  border-width: 10px;
+  border-style: solid;
+  border-color: var(--parsec-color-light-primary-900) transparent transparent transparent;
+}
+</style>
