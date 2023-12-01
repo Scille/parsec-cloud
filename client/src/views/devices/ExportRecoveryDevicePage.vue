0cb472aa711cc650e3a0595c6b0976536928d573
fabienSvstr <fabien.sevestre@my-digital-school.org>
2023-12-01 11:16:33 +0100

    Add style on export recovery device page

--- client/src/views/devices/ExportRecoveryDevicePage.vue
+++ client/src/views/devices/ExportRecoveryDevicePage.vue
@@ -3,91 +3,137 @@
 <template>
   <ion-page>
     <ion-content :fullscreen="true">
-      <div class="container">
-        <div class="recovery-container">
-          <div v-if="state === ExportDevicePageState.Start">
-            <ms-informative-text>
-              {{ $t('ExportRecoveryDevicePage.subtitles.newPassword') }}
-            </ms-informative-text>
+      <div class="recovery">
+        <!-- STEP 1 • Information -->
+        <div
+          class="recovery-container information"
+          v-if="state === ExportDevicePageState.Start"
+        >
+          <ms-informative-text>
+            {{ $t('ExportRecoveryDevicePage.subtitles.newPassword') }}
+          </ms-informative-text>
+
+          <div class="file">
             <ms-informative-text>
               {{ $t('ExportRecoveryDevicePage.subtitles.twoFilesToKeep') }}
             </ms-informative-text>
 
-            <div>
-              <div class="block">
-                {{ $t('ExportRecoveryDevicePage.titles.recoveryFile') }}
+            <div class="file-list">
+              <div class="file-item">
+                <div class="file-item__title subtitles-normal">
+                  <ion-icon :icon="document" />
+                  {{ $t('ExportRecoveryDevicePage.titles.recoveryFile') }}
+                </div>
               </div>
-              <div class="block">
-                {{ $t('ExportRecoveryDevicePage.titles.recoveryKey') }}
+              <div class="file-item">
+                <div class="file-item__title subtitles-normal">
+                  <ion-icon :icon="key" />
+                  {{ $t('ExportRecoveryDevicePage.titles.recoveryKey') }}
+                </div>
               </div>
             </div>
-            <ion-button
-              @click="exportDevice()"
-              id="exportDevice"
-            >
-              {{ $t('ExportRecoveryDevicePage.actions.understand') }}
-            </ion-button>
           </div>
-          <div v-else-if="state === ExportDevicePageState.Download">
-            <ms-informative-text>
-              {{ $t('ExportRecoveryDevicePage.subtitles.keepFilesSeparate') }}
-            </ms-informative-text>
 
-            <div>
-              <div class="block">
+          <ion-button
+            @click="exportDevice()"
+            id="exportDevice"
+          >
+            {{ $t('ExportRecoveryDevicePage.actions.understand') }}
+          </ion-button>
+        </div>
+
+        <!-- STEP 2 • Download files -->
+        <div
+          class="recovery-container download"
+          v-else-if="state === ExportDevicePageState.Download"
+        >
+          <ms-informative-text>
+            {{ $t('ExportRecoveryDevicePage.subtitles.keepFilesSeparate') }}
+          </ms-informative-text>
+
+          <div class="file-list">
+            <div class="file-item">
+              <div class="file-item__title subtitles-normal">
+                <ion-icon :icon="document" />
                 {{ $t('ExportRecoveryDevicePage.titles.recoveryFile') }}
-                <div v-show="!recoveryFileDownloaded">
-                  <ion-button
-                    @click="downloadRecoveryFile()"
-                    id="downloadButton"
-                  >
-                    {{ $t('ExportRecoveryDevicePage.actions.download') }}
-                  </ion-button>
-                </div>
-                <div v-show="recoveryFileDownloaded">
-                  <ion-icon
-                    :icon="checkmarkCircle"
-                    class="checked"
-                  />
-                  {{ $t('ExportRecoveryDevicePage.subtitles.fileDownloaded') }}
-                </div>
               </div>
-              <div class="block">
-                {{ $t('ExportRecoveryDevicePage.titles.recoveryKey') }}
-                <div v-show="!recoveryKeyDownloaded">
-                  <ion-button
-                    @click="downloadRecoveryKey()"
-                    id="downloadButton"
-                  >
-                    {{ $t('ExportRecoveryDevicePage.actions.download') }}
-                  </ion-button>
-                </div>
-                <div v-show="recoveryKeyDownloaded">
-                  <ion-icon
-                    :icon="checkmarkCircle"
-                    class="checked"
-                  />
-                  {{ $t('ExportRecoveryDevicePage.subtitles.fileDownloaded') }}
-                </div>
-              </div>
-              <a
-                ref="downloadLink"
-                v-show="false"
-              />
-              <div v-show="recoveryKeyDownloaded && recoveryFileDownloaded">
+              <ion-text class="file-item__description body">
+                {{ $t('ExportRecoveryDevicePage.subtitles.fileExplanation') }}
+              </ion-text>
+              <div
+                class="file-item__button"
+                v-show="!recoveryFileDownloaded"
+              >
                 <ion-button
-                  class="return-btn button-outline"
-                  @click="onBackToWorkspacesClick()"
-                  id="back-to-workspaces-button"
+                  @click="downloadRecoveryFile()"
+                  id="downloadButton"
+                  size="default"
                 >
-                  <ion-icon
-                    :icon="home"
-                    class="icon"
-                  />
-                  {{ $t('ExportRecoveryDevicePage.actions.backToWorkspaces') }}
+                  <ion-icon :icon="download" />
+                  {{ $t('ExportRecoveryDevicePage.actions.download') }}
                 </ion-button>
               </div>
+              <div
+                class="file-item__downloaded body"
+                v-show="recoveryFileDownloaded"
+              >
+                <ion-icon
+                  :icon="checkmarkCircle"
+                  class="checked"
+                />
+                {{ $t('ExportRecoveryDevicePage.subtitles.fileDownloaded') }}
+              </div>
             </div>
+            <div class="file-item">
+              <div class="file-item__title subtitles-normal">
+                <ion-icon :icon="key" />
+                {{ $t('ExportRecoveryDevicePage.titles.recoveryKey') }}
+              </div>
+              <ion-text class="file-item__description body">
+                {{ $t('ExportRecoveryDevicePage.subtitles.keyExplanation') }}
+              </ion-text>
+              <div
+                v-show="!recoveryKeyDownloaded"
+                class="file-item__button"
+              >
+                <ion-button
+                  @click="downloadRecoveryKey()"
+                  id="downloadButton"
+                  size="default"
+                >
+                  <ion-icon :icon="download" />
+                  {{ $t('ExportRecoveryDevicePage.actions.download') }}
+                </ion-button>
+              </div>
+              <div
+                class="file-item__downloaded body"
+                v-show="recoveryKeyDownloaded"
+              >
+                <ion-icon
+                  :icon="checkmarkCircle"
+                  class="checked"
+                />
+                {{ $t('ExportRecoveryDevicePage.subtitles.fileDownloaded') }}
+              </div>
+            </div>
+          </div>
+          <a
+            ref="downloadLink"
+            v-show="false"
+          />
+          <div v-show="recoveryKeyDownloaded && recoveryFileDownloaded">
+            <ion-button
+              class="return-btn button-outline"
+              fill="outline"
+              @click="onBackToDevicesClick()"
+              id="back-to-devices-button"
+            >
+              <ion-icon
+                :icon="home"
+                class="icon"
+              />
+              {{ $t('ExportRecoveryDevicePage.actions.backToDevices') }}
+            </ion-button>
           </div>
         </div>
       </div>
@@ -96,7 +142,7 @@
 </template>
 
 <script setup lang="ts">
-import { IonPage, IonContent, IonButton, IonIcon } from '@ionic/vue';
+import { IonPage, IonContent, IonButton, IonIcon, IonText } from '@ionic/vue';
 import MsInformativeText from '@/components/core/ms-text/MsInformativeText.vue';
 import { ref, inject, onMounted } from 'vue';
 import { getPasswordFromUser } from '@/components/core/ms-modal/MsPasswordInputModal.vue';
@@ -105,7 +151,7 @@ import { exportRecoveryDevice, RecoveryDeviceErrorTag } from '@/parsec';
 import { getClientInfo } from '@/parsec/login';
 import { NotificationManager, Notification, NotificationKey, NotificationLevel } from '@/services/notificationManager';
 import { routerNavigateTo } from '@/router';
-import { home, checkmarkCircle } from 'ionicons/icons';
+import { home, checkmarkCircle, document, key, download } from 'ionicons/icons';
 
 const { t } = useI18n();
 
@@ -185,41 +231,113 @@ async function fileDownload(data: string, fileName: string): Promise<void> {
 }
 
 // Placeholder page reset causing visual flickering
-function onBackToWorkspacesClick(): void {
+function onBackToDevicesClick(): void {
   recoveryFileDownloaded.value = false;
   recoveryKeyDownloaded.value = false;
   state.value = ExportDevicePageState.Start;
-  routerNavigateTo('workspaces');
+  routerNavigateTo('devices');
 }
 </script>
 
 <style scoped lang="scss">
-.container {
+.recovery {
   display: flex;
   max-width: 70rem;
-}
-
-.recovery-container {
   margin: 2.5em 2rem 0;
 }
 
-.block {
-  background-color: green;
-  color: khaki;
-  width: 15rem;
-  height: 8rem;
-  border: 3px solid magenta;
-  float: left;
-  margin: 1rem;
-}
+.recovery-container {
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
 
-.return-btn {
-  &::part(native) {
-    background: none;
+  .file {
+    display: flex;
+    flex-direction: column;
+    gap: 1.5rem;
+  }
+
+  .file-list {
+    display: flex;
+    gap: 1.5rem;
+
+    .file-item {
+      padding: 1.5rem;
+      border-radius: var(--parsec-radius-8);
+      background: var(--parsec-color-light-primary-30);
+      max-width: 24rem;
+
+      &__title {
+        gap: 1rem;
+        display: flex;
+        align-items: center;
+        color: var(--parsec-color-light-primary-700);
+
+        ion-icon {
+          font-size: 1.5rem;
+        }
+      }
+
+      &__description {
+        color: var(--parsec-color-light-secondary-grey);
+      }
+
+      &__button {
+        margin-top: 2rem;
+        display: flex;
+        align-items: center;
+        gap: 0.5rem;
+        color: var(--parsec-color-light-primary-700);
+
+        ion-button {
+          margin: 0;
+        }
+
+        #downloadButton {
+          display: flex;
+          align-items: center;
+
+          ion-icon {
+            font-size: 1.25rem;
+            margin-right: 0.625rem;
+          }
+        }
+      }
+
+      &__downloaded {
+        display: flex;
+        align-items: center;
+        margin-top: 2rem;
+        padding: 0.375rem 0;
+        gap: 0.5rem;
+        color: var(--parsec-color-light-secondary-grey);
+
+        .checked {
+          font-size: 1.25rem;
+          color: var(--parsec-color-light-success-500);
+        }
+      }
+    }
   }
 }
 
-.checked {
-  color: lightgreen;
+.information {
+  #exportDevice {
+    margin-top: 0.5rem;
+    width: fit-content;
+  }
+}
+
+.download {
+  .file-item__title {
+    margin-bottom: 1rem;
+  }
+}
+
+.return-btn {
+  .icon {
+    font-size: 1rem;
+    margin-right: 0.625rem;
+  }
 }
 </style>
