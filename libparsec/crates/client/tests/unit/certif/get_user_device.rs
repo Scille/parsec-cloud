5a4f9265a7e3b76efd3bcf878c0fe5c884b0f00a
TimeEngineer <chenpascal241096@yahoo.fr>
2024-01-30 12:17:16 +0100

    Add tests for CertifOps: get_user_device

---
+++ libparsec/crates/client/tests/unit/certif/get_user_device.rs
@@ -0,0 +1,37 @@
+// Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 2016-present Scille SAS
+
+use libparsec_tests_fixtures::prelude::*;
+
+use crate::certif::CertifGetUserDeviceError;
+
+use super::utils::certificates_ops_factory;
+
+#[parsec_test(testbed = "minimal")]
+async fn ok(env: &TestbedEnv) {
+    let env = env.customize(|builder| {
+        builder.certificates_storage_fetch_certificates("alice@dev1");
+    });
+    let alice = env.local_device("alice@dev1");
+    let ops = certificates_ops_factory(&env, &alice).await;
+
+    let res = ops
+        .get_user_device("alice@dev1".parse().unwrap())
+        .await
+        .unwrap();
+
+    p_assert_eq!(res.0.id, "alice".parse().unwrap());
+    p_assert_eq!(res.1.id, "alice@dev1".parse().unwrap());
+}
+
+#[parsec_test(testbed = "minimal")]
+async fn non_existing(env: &TestbedEnv) {
+    let alice = env.local_device("alice@dev1");
+    let ops = certificates_ops_factory(env, &alice).await;
+
+    let err = ops
+        .get_user_device("alice@dev1".parse().unwrap())
+        .await
+        .unwrap_err();
+
+    p_assert_matches!(err, CertifGetUserDeviceError::NonExisting);
+}
