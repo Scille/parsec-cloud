b7e51db5fa95c42c75b27bdf876969fcd264c15b
fabienSvstr <fabien.sevestre@my-digital-school.org>
2024-01-31 14:16:17 +0100

    Update notification style item

--- client/src/components/header/NotificationItem.vue
+++ client/src/components/header/NotificationItem.vue
@@ -12,30 +12,22 @@
       class="element ion-no-padding"
       @click="onClick"
     >
-      <div class="element-type">
-        <div class="element-type__icon">
-          <!-- This icon is only a default placeholder, replace/add notification specific icons -->
-          <ms-image
-            :image="LogoIconGradient"
-            class="file-icon"
-          />
-        </div>
+      <div class="element-icon">
+        <!-- This icon is only a default placeholder, replace/add notification specific icons -->
+        <ms-image
+          :image="LogoIconGradient"
+          class="file-icon"
+        />
       </div>
       <div class="element-details">
-        <ion-text class="element-details__title body">
-          {{ notification.title }}
-        </ion-text>
-        <ion-label class="element-details__message body-sm">
-          <span class="default-state">{{ notification.message }}</span>
-          <span class="hover-state">
-            {{ $t('notificationCenter.browse') }}
-          </span>
+        <ion-label class="element-details__message body">
+          <span>{{ notification.message }}</span>
         </ion-label>
+        <ion-text class="element-details__time body-sm">
+          <span class="default-state">{{ formatTimeSince(notification.time, '', 'short') }}</span>
+          <span class="hover-state">{{ $t('notificationCenter.browse') }}</span>
+        </ion-text>
       </div>
-
-      <ion-label class="element-details__time body-sm default-state">
-        <span class="default-state">{{ formatTimeSince(notification.time, '', 'short') }}</span>
-      </ion-label>
       <ion-icon
         class="arrow-icon hover-state"
         :icon="arrowForward"
@@ -76,12 +68,16 @@ async function onClick(): Promise<void> {
   transition: background 0.2s ease-in-out;
   position: relative;
 
+  &:not(:last-child) {
+    border-bottom: 1px solid var(--parsec-color-light-secondary-disabled);
+  }
+
   .hover-state {
     display: none;
   }
 
   &:hover {
-    background: var(--parsec-color-light-primary-50);
+    background: var(--parsec-color-light-secondary-disabled);
     .default-state {
       display: none;
     }
@@ -98,14 +94,33 @@ async function onClick(): Promise<void> {
 .element {
   --background: none;
   --inner-padding-end: 0;
-  // @SharkDesigner: this is commented because of the ion-list border bottom
-  // padding: 1rem;
+  --border-width: 0;
+  padding: 1rem 1rem 1rem 1.75rem;
 
-  .file-icon {
-    width: 2rem;
-    height: 2rem;
-    // @SharkDesigner: this is a temporary fix to manage the padding: 1rem that should be on .element
-    margin: 1rem 0 1rem 1rem;
+  // This will allow to change background and color of the icon
+  --background-icon-info: var(--parsec-color-light-primary-50);
+  --background-icon-success: var(--parsec-color-light-success-100);
+  --background-icon-danger: var(--parsec-color-light-danger-100);
+  --background-icon-warning: var(--parsec-color-light-warning-100);
+  --color-icon-info: var(--parsec-color-light-primary-700);
+  --color-icon-success: var(--parsec-color-light-success-700);
+  --color-icon-danger: var(--parsec-color-light-danger-700);
+  --color-icon-warning: var(--parsec-color-light-warning-700);
+
+  &-icon {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 2.5rem;
+    height: 2.5rem;
+    padding: 0.5rem;
+    background: var(--background-icon-info);
+    border-radius: var(--parsec-radius-12);
+
+    .file-icon {
+      width: 1.5rem;
+      height: 1.5rem;
+    }
   }
 
   &-details {
@@ -114,26 +129,34 @@ async function onClick(): Promise<void> {
     position: relative;
     margin-left: 0.875rem;
 
-    &__title {
-      color: var(--parsec-color-light-primary-800);
+    &__message {
+      color: var(--parsec-color-light-secondary-text);
     }
 
-    &__message,
     &__time {
       color: var(--parsec-color-light-secondary-grey);
-      text-align: right;
     }
   }
 
-  .element-details__time,
   .arrow-icon {
     margin-left: auto;
-    // @SharkDesigner: this is a temporary fix to manage the padding: 1rem that should be on .element
-    margin-right: 1rem;
   }
 }
 
 .unread {
   background: var(--parsec-color-light-secondary-medium);
+
+  &::before {
+    content: '';
+    position: absolute;
+    top: 50%;
+    left: 0.5rem;
+    transform: translateY(-50%);
+    width: 0.5rem;
+    height: 0.5rem;
+    display: flex;
+    background: var(--parsec-color-light-gradient);
+    border-radius: var(--parsec-radius-circle);
+  }
 }
 </style>
