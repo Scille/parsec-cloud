bf84db7d97c79b3dc14a06b96242491682fa3ee4
TimeEngineer <chenpascal241096@yahoo.fr>
2024-02-01 15:06:44 +0100

    Rename CertifOps poll test file

--- libparsec/crates/client/tests/unit/certif/mod.rs
+++ libparsec/crates/client/tests/unit/certif/mod.rs
@@ -7,7 +7,7 @@ mod add_sequester_authority_certificate;
 mod add_user_certificate;
 mod add_user_revoked_certificate;
 mod add_user_update_certificate;
-mod poll;
+mod poll_server_for_new_certificates;
 mod store;
 mod utils;
 mod validate_manifest;
