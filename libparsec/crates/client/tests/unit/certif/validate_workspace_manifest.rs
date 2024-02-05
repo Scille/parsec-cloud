a434a3e8a537ec131fa9457a999c1a2f6a1968e6
TimeEngineer <chenpascal241096@yahoo.fr>
2024-02-05 14:03:02 +0100

    Split Certifops validate_manifest tests

--- libparsec/crates/client/tests/unit/certif/validate_workspace_manifest.rs
+++ libparsec/crates/client/tests/unit/certif/validate_workspace_manifest.rs
@@ -10,28 +10,49 @@ use crate::certif::{CertifValidateManifestError, InvalidManifestError};
 
 #[parsec_test(testbed = "minimal")]
 async fn ok(env: &TestbedEnv) {
-    let (env, (expected_manifest, vlob, vlob_timestamp)) = env.customize_with_map(|builder| {
-        builder.new_user_realm("alice");
-        builder.certificates_storage_fetch_certificates("alice@dev1");
-        builder
-            .create_or_update_user_manifest_vlob("alice")
-            .map(|e| {
-                (
-                    e.manifest.clone(),
-                    e.encrypted(&env.template),
-                    e.manifest.timestamp,
-                )
-            })
-    });
+    let (env, (realm_id, expected_manifest, vlob, vlob_timestamp)) =
+        env.customize_with_map(|builder| {
+            let realm_id = builder
+                .new_realm("alice")
+                .then_do_initial_key_rotation()
+                .map(|event| event.realm);
+            builder.certificates_storage_fetch_certificates("alice@dev1");
+            builder
+                .create_or_update_workspace_manifest_vlob("alice@dev1", realm_id)
+                .map(|e| {
+                    (
+                        realm_id,
+                        e.manifest.clone(),
+                        e.encrypted(&env.template),
+                        e.manifest.timestamp,
+                    )
+                })
+        });
+    let (_, realm_key_index) = env.get_last_realm_key(realm_id);
 
     let alice = env.local_device("alice@dev1");
 
     let ops = certificates_ops_factory(&env, &alice).await;
 
+    let keys_bundle = env.get_last_realm_keys_bundle(realm_id);
+    let keys_bundle_access = env.get_last_realm_keys_bundle_access_for(realm_id, alice.user_id());
+    test_register_send_hook(
+        &env.discriminant_dir,
+        move |_: authenticated_cmds::latest::realm_get_keys_bundle::Req| {
+            authenticated_cmds::latest::realm_get_keys_bundle::Rep::Ok {
+                key_index: 1,
+                keys_bundle,
+                keys_bundle_access,
+            }
+        },
+    );
+
     let manifest = ops
-        .validate_user_manifest(
-            env.get_last_realm_certificate_timestamp(alice.user_realm_id),
+        .validate_workspace_manifest(
+            env.get_last_realm_certificate_timestamp(realm_id),
             env.get_last_common_certificate_timestamp(),
+            realm_id,
+            realm_key_index,
             &alice.device_id,
             1,
             vlob_timestamp,
@@ -52,7 +73,16 @@ async fn offline(env: &TestbedEnv) {
     let ops = certificates_ops_factory(env, &alice).await;
 
     let err = ops
-        .validate_user_manifest(now, now, &alice.device_id, 0, now, b"")
+        .validate_workspace_manifest(
+            now,
+            now,
+            VlobID::default(),
+            0,
+            &alice.device_id,
+            0,
+            now,
+            b"",
+        )
         .await
         .unwrap_err();
 
@@ -60,39 +90,42 @@ async fn offline(env: &TestbedEnv) {
 }
 
 #[parsec_test(testbed = "minimal")]
-async fn stopped(env: &TestbedEnv) {
-    let now = DateTime::from_ymd_hms_us(2020, 1, 1, 0, 0, 0, 0).unwrap();
-
-    let alice = env.local_device("alice@dev1");
-
-    let ops = certificates_ops_factory(env, &alice).await;
-    ops.stop().await.unwrap();
-
-    let err = ops
-        .validate_user_manifest(now, now, &alice.device_id, 0, now, b"")
-        .await
-        .unwrap_err();
-
-    p_assert_matches!(err, CertifValidateManifestError::Stopped);
-}
-
-#[parsec_test(testbed = "minimal")]
 async fn non_existent_author(env: &TestbedEnv) {
-    let env = env.customize(|builder| {
-        builder.new_user_realm("alice");
+    let (env, realm_id) = env.customize_with_map(|builder| {
+        let realm_id = builder
+            .new_realm("alice")
+            .then_do_initial_key_rotation()
+            .map(|event| event.realm);
         builder.certificates_storage_fetch_certificates("alice@dev1");
-    });
 
+        realm_id
+    });
     let now = DateTime::from_ymd_hms_us(2020, 1, 1, 0, 0, 0, 0).unwrap();
+    let (_, realm_key_index) = env.get_last_realm_key(realm_id);
 
     let alice = env.local_device("alice@dev1");
 
     let ops = certificates_ops_factory(&env, &alice).await;
 
+    let keys_bundle = env.get_last_realm_keys_bundle(realm_id);
+    let keys_bundle_access = env.get_last_realm_keys_bundle_access_for(realm_id, alice.user_id());
+    test_register_send_hook(
+        &env.discriminant_dir,
+        move |_: authenticated_cmds::latest::realm_get_keys_bundle::Req| {
+            authenticated_cmds::latest::realm_get_keys_bundle::Rep::Ok {
+                key_index: 1,
+                keys_bundle,
+                keys_bundle_access,
+            }
+        },
+    );
+
     let err = ops
-        .validate_user_manifest(
-            env.get_last_realm_certificate_timestamp(alice.user_realm_id),
+        .validate_workspace_manifest(
+            env.get_last_realm_certificate_timestamp(realm_id),
             env.get_last_common_certificate_timestamp(),
+            realm_id,
+            realm_key_index,
             &"alice@dev2".parse().unwrap(),
             0,
             now,
@@ -110,10 +143,15 @@ async fn non_existent_author(env: &TestbedEnv) {
 
 #[parsec_test(testbed = "minimal")]
 async fn corrupted(env: &TestbedEnv) {
-    let env = env.customize(|builder| {
-        builder.new_user_realm("alice");
+    let (env, realm_id) = env.customize_with_map(|builder| {
+        let realm_id = builder
+            .new_realm("alice")
+            .then_do_initial_key_rotation()
+            .map(|event| event.realm);
         builder.certificates_storage_fetch_certificates("alice@dev1");
+        realm_id
     });
+    let (_, realm_key_index) = env.get_last_realm_key(realm_id);
 
     let now = DateTime::from_ymd_hms_us(2020, 1, 1, 0, 0, 0, 0).unwrap();
 
@@ -121,59 +159,29 @@ async fn corrupted(env: &TestbedEnv) {
 
     let ops = certificates_ops_factory(&env, &alice).await;
 
+    let keys_bundle = env.get_last_realm_keys_bundle(realm_id);
+    let keys_bundle_access = env.get_last_realm_keys_bundle_access_for(realm_id, alice.user_id());
+    test_register_send_hook(
+        &env.discriminant_dir,
+        move |_: authenticated_cmds::latest::realm_get_keys_bundle::Req| {
+            authenticated_cmds::latest::realm_get_keys_bundle::Rep::Ok {
+                key_index: 1,
+                keys_bundle,
+                keys_bundle_access,
+            }
+        },
+    );
+
     let err = ops
-        .validate_user_manifest(
-            env.get_last_realm_certificate_timestamp(alice.user_realm_id),
+        .validate_workspace_manifest(
+            env.get_last_realm_certificate_timestamp(realm_id),
             env.get_last_common_certificate_timestamp(),
+            realm_id,
+            realm_key_index,
             &alice.device_id,
             0,
             now,
             b"",
-        )
-        .await
-        .unwrap_err();
-
-    p_assert_matches!(
-        err,
-        CertifValidateManifestError::InvalidManifest(boxed)
-        if matches!(*boxed, InvalidManifestError::Corrupted { .. })
-    );
-}
-
-#[parsec_test(testbed = "minimal")]
-async fn user_manifest_corrupted_by_bad_realm_id(env: &TestbedEnv) {
-    let (env, (other_realm_id, timestamp)) = env.customize_with_map(|builder| {
-        builder.new_user_realm("alice");
-        let (realm_id, timestamp) = builder
-            .new_realm("alice")
-            .map(|e| (e.realm_id, e.timestamp));
-        builder.certificates_storage_fetch_certificates("alice@dev1");
-        (realm_id, timestamp)
-    });
-
-    let alice = env.local_device("alice@dev1");
-
-    let ops = certificates_ops_factory(&env, &alice).await;
-
-    let alice_manifest = UserManifest {
-        author: alice.device_id.clone(),
-        timestamp,
-        // Bad realm !
-        id: other_realm_id,
-        version: 0,
-        created: timestamp,
-        updated: timestamp,
-        workspaces_legacy_initial_info: vec![],
-    };
-
-    let err = ops
-        .validate_user_manifest(
-            env.get_last_realm_certificate_timestamp(alice.user_realm_id),
-            env.get_last_common_certificate_timestamp(),
-            &alice.device_id,
-            0,
-            timestamp,
-            &alice_manifest.dump_sign_and_encrypt(&alice.signing_key, &ops.device.local_symkey),
         )
         .await
         .unwrap_err();
@@ -247,31 +255,53 @@ async fn author_no_access_to_realm(env: &TestbedEnv) {
 
 #[parsec_test(testbed = "minimal")]
 async fn revoked(env: &TestbedEnv) {
-    let env = env.customize(|builder| {
+    let (env, realm_id) = env.customize_with_map(|builder| {
         builder.new_user("bob");
-        builder.new_user_realm("bob");
+        let realm_id = builder
+            .new_realm("bob")
+            .then_do_initial_key_rotation()
+            .map(|event| event.realm);
         builder.revoke_user("bob");
         builder.certificates_storage_fetch_certificates("bob@dev1");
         builder.with_check_consistency_disabled(|builder| {
-            builder.create_or_update_user_manifest_vlob("bob");
-        })
+            builder.create_or_update_workspace_manifest_vlob("bob@dev1", realm_id);
+        });
+
+        realm_id
     });
+
+    let (_, realm_key_index) = env.get_last_realm_key(realm_id);
 
     let bob = env.local_device("bob@dev1");
 
     let ops = certificates_ops_factory(&env, &bob).await;
 
     let (vlob, vlob_timestamp) = match env.template.events.iter().last().unwrap() {
-        TestbedEvent::CreateOrUpdateUserManifestVlob(e) => {
+        TestbedEvent::CreateOrUpdateWorkspaceManifestVlob(e) => {
             (e.encrypted(&env.template), e.manifest.timestamp)
         }
         e => panic!("Unexpected event {:?}", e),
     };
 
+    let keys_bundle = env.get_last_realm_keys_bundle(realm_id);
+    let keys_bundle_access = env.get_last_realm_keys_bundle_access_for(realm_id, bob.user_id());
+    test_register_send_hook(
+        &env.discriminant_dir,
+        move |_: authenticated_cmds::latest::realm_get_keys_bundle::Req| {
+            authenticated_cmds::latest::realm_get_keys_bundle::Rep::Ok {
+                key_index: 1,
+                keys_bundle,
+                keys_bundle_access,
+            }
+        },
+    );
+
     let err = ops
-        .validate_user_manifest(
-            env.get_last_realm_certificate_timestamp(bob.user_realm_id),
+        .validate_workspace_manifest(
+            env.get_last_realm_certificate_timestamp(realm_id),
             env.get_last_common_certificate_timestamp(),
+            realm_id,
+            realm_key_index,
             &bob.device_id,
             1,
             vlob_timestamp,
@@ -294,10 +324,10 @@ async fn revoked(env: &TestbedEnv) {
                 }
                 if {
                     p_assert_eq!(*author, bob.device_id);
-                    p_assert_eq!(*realm, bob.user_realm_id);
+                    p_assert_eq!(*realm, realm_id);
                     p_assert_eq!(*timestamp, vlob_timestamp);
                     p_assert_eq!(*version, 1);
-                    p_assert_eq!(*vlob, bob.user_realm_id);
+                    p_assert_eq!(*vlob, realm_id);
                     true
                 }
         )
@@ -305,7 +335,7 @@ async fn revoked(env: &TestbedEnv) {
 }
 
 #[parsec_test(testbed = "minimal")]
-async fn workspace_manifest_cannot_write(env: &TestbedEnv) {
+async fn cannot_write(env: &TestbedEnv) {
     let (env, shared_realm_id) = env.customize_with_map(|builder| {
         builder.new_user("bob");
         let realm_id = builder
@@ -395,63 +425,27 @@ async fn workspace_manifest_cannot_write(env: &TestbedEnv) {
 }
 
 #[parsec_test(testbed = "minimal")]
-async fn user_manifest_cannot_write(env: &TestbedEnv) {
-    let (env, (shared_realm_id, vlob, vlob_timestamp)) = env.customize_with_map(|builder| {
-        builder.new_user("bob");
-        let realm_id = builder
-            .new_user_realm("alice")
-            // Key rotation is not supposed to be used for user realm, but
-            // current test is rather odd anyway (access rights should never
-            // change for user realm)
-            .then_do_initial_key_rotation()
-            .map(|e| e.realm);
-        builder.share_realm(realm_id, "bob", Some(RealmRole::Owner));
-        builder.share_realm(realm_id, "alice", Some(RealmRole::Reader));
-        builder.certificates_storage_fetch_certificates("alice@dev1");
-        let (vlob, vlob_timestamp) = builder.with_check_consistency_disabled(|builder| {
-            builder
-                .create_or_update_user_manifest_vlob("alice")
-                .map(|e| (e.encrypted(&env.template), e.manifest.timestamp))
-        });
-        (realm_id, vlob, vlob_timestamp)
-    });
+async fn stopped(env: &TestbedEnv) {
+    let now = DateTime::from_ymd_hms_us(2020, 1, 1, 0, 0, 0, 0).unwrap();
 
     let alice = env.local_device("alice@dev1");
 
-    let ops = certificates_ops_factory(&env, &alice).await;
+    let ops = certificates_ops_factory(env, &alice).await;
+    ops.stop().await.unwrap();
 
     let err = ops
-        .validate_user_manifest(
-            env.get_last_realm_certificate_timestamp(shared_realm_id),
-            env.get_last_common_certificate_timestamp(),
+        .validate_workspace_manifest(
+            now,
+            now,
+            VlobID::default(),
+            0,
             &alice.device_id,
-            1,
-            vlob_timestamp,
-            &vlob,
+            0,
+            now,
+            b"",
         )
         .await
         .unwrap_err();
 
-    p_assert_matches!(
-        err,
-        CertifValidateManifestError::InvalidManifest(boxed) if matches!(&*boxed,
-            InvalidManifestError::AuthorRealmRoleCannotWrite {
-                author,
-                author_role,
-                realm,
-                timestamp,
-                version,
-                vlob,
-            }
-        if {
-            p_assert_eq!(*author, alice.device_id);
-            p_assert_eq!(*author_role, RealmRole::Reader);
-            p_assert_eq!(*realm, shared_realm_id);
-            p_assert_eq!(*timestamp, vlob_timestamp);
-            p_assert_eq!(*version, 1);
-            p_assert_eq!(*vlob, shared_realm_id);
-            true
-        },
-        )
-    );
+    p_assert_matches!(err, CertifValidateManifestError::Stopped);
 }
