f377b176e602d47fcdade6fc2fefc5a60a51990c
TimeEngineer <chenpascal241096@yahoo.fr>
2024-02-09 15:20:01 +0100

    Add tests add_certificates_batch for SequesterRevokedServiceCertificate

--- libparsec/crates/client/tests/unit/certif/mod.rs
+++ libparsec/crates/client/tests/unit/certif/mod.rs
@@ -7,6 +7,7 @@ mod add_realm_key_rotation_certificate;
 mod add_realm_name_certificate;
 mod add_realm_role_certificate;
 mod add_sequester_authority_certificate;
+mod add_sequester_revoked_service_certificate;
 mod add_sequester_service_certificate;
 mod add_user_certificate;
 mod add_user_revoked_certificate;
