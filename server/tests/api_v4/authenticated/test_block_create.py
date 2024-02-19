3dd81a3ab4025f0623c95c2ba02f4dc36d19c932
Marcos Medrano <marcosmedrano0@gmail.com>
2024-02-19 19:34:09 -0300

    Add missing tests for block_create

--- server/tests/api_v4/authenticated/test_block_create.py
+++ server/tests/api_v4/authenticated/test_block_create.py
@@ -2,7 +2,11 @@
 
 from unittest.mock import ANY
 
-from parsec._parsec import BlockID, DateTime, authenticated_cmds
+import pytest
+
+from parsec._parsec import BlockID, DateTime, VlobID, authenticated_cmds
+from parsec.components.block import BlockReadBadOutcome
+from parsec.components.blockstore import BlockStoreReadBadOutcome
 from tests.common import Backend, CoolorgRpcClients
 
 
@@ -27,6 +31,28 @@ async def test_authenticated_block_create_ok(coolorg: CoolorgRpcClients, backend
     assert dump == expected_dump
 
 
+async def test_authenticated_block_create_realm_not_found(
+    coolorg: CoolorgRpcClients, backend: Backend
+) -> None:
+    block_id = BlockID.new()
+    block = b"<block content>"
+
+    # Try to create block with an invalid realm ID
+    rep = await coolorg.alice.block_create(block_id, VlobID.new(), block)
+    assert rep == authenticated_cmds.v4.block_create.RepRealmNotFound()
+
+    # Block should not exist
+    content = await backend.block.read_as_user(
+        organization_id=coolorg.organization_id,
+        author=coolorg.alice.device_id.user_id,
+        block_id=block_id,
+    )
+    assert content != BlockReadBadOutcome.BLOCK_NOT_FOUND.value
+
+    dump = await backend.block.test_dump_blocks(coolorg.organization_id)
+    assert not dump  # No changes!
+
+
 async def test_authenticated_block_create_already_exists(
     coolorg: CoolorgRpcClients, backend: Backend
 ) -> None:
@@ -48,7 +74,7 @@ async def test_authenticated_block_create_already_exists(
     assert rep == authenticated_cmds.v4.block_create.RepBlockAlreadyExists()
 
     dump = await backend.block.test_dump_blocks(coolorg.organization_id)
-    assert dump == expected_dump  # No changes !
+    assert dump == expected_dump  # No changes!
 
 
 async def test_authenticated_block_create_not_allowed(
@@ -63,4 +89,32 @@ async def test_authenticated_block_create_not_allowed(
     assert rep == authenticated_cmds.v4.block_create.RepAuthorNotAllowed()
 
     dump = await backend.block.test_dump_blocks(coolorg.organization_id)
-    assert dump == expected_dump  # No changes !
+    assert dump == expected_dump  # No changes!
+
+
+async def test_authenticated_block_create_store_unavailable(
+    coolorg: CoolorgRpcClients, backend: Backend, monkeypatch: pytest.MonkeyPatch
+) -> None:
+    block_id = BlockID.new()
+    block = b"<block content>"
+
+    async def mocked_blockstore_create(*args, **kwargs):
+        return BlockStoreReadBadOutcome.STORE_UNAVAILABLE
+
+    monkeypatch.setattr(
+        "parsec.components.memory.MemoryBlockStoreComponent.create", mocked_blockstore_create
+    )
+
+    rep = await coolorg.alice.block_create(block_id, coolorg.wksp1_id, block)
+    assert rep == authenticated_cmds.v4.block_create.RepStoreUnavailable()
+
+    # Block should not exist
+    content = await backend.block.read_as_user(
+        organization_id=coolorg.organization_id,
+        author=coolorg.alice.device_id.user_id,
+        block_id=block_id,
+    )
+    assert content != BlockReadBadOutcome.BLOCK_NOT_FOUND.value
+
+    dump = await backend.block.test_dump_blocks(coolorg.organization_id)
+    assert not dump  # No changes!
