Scille/parsec-cloud

commit 1cb7311c91675a8c864be4c1d0b13de2098bad5e

Fix dev specifier for full version
The semver specification does not allow the use of a hyphen to separate a prerelease part from another, we should use a dot instead.

misc/releaser.py
@@ -192,10 +192,12 @@ class Version:
     def __str__(self) -> str:
         base = f"{self.major}.{self.minor}.{self.patch}"
         prerelease = self.prerelease
+        sep = "-"
         if prerelease is not None:
-            base += "-" + prerelease
+            base += sep + prerelease
+            sep = "."
         if self.dev is not None:
-            base += f"-dev.{self.dev}"
+            base += f"{sep}dev.{self.dev}"
         if self.local is not None:
             base += "+" + self.local
         return base
@@ -334,12 +336,12 @@ assert Version.parse("1.2.3-b10+dev") < Version.parse("1.2.3-rc1+dev")
 assert Version.parse("1.2.3-b10+dev") < Version.parse("1.2.3+dev")
 _test_format_version(Version(1, 2, 3), "1.2.3")
 _test_format_version(Version(1, 2, 3, prerelease="a1"), "1.2.3-a.1")
-_test_format_version(Version(1, 2, 3, prerelease="b2", dev=4), "1.2.3-b.2-dev.4")
+_test_format_version(Version(1, 2, 3, prerelease="b2", dev=4), "1.2.3-b.2.dev.4")
 _test_format_version(Version(1, 2, 3, dev=0), "1.2.3-dev.0")
 _test_format_version(Version(1, 2, 3, dev=5), "1.2.3-dev.5")
 _test_format_version(Version(1, 2, 3, prerelease="a1", local="foo.bar"), "1.2.3-a.1+foo.bar")
 _test_format_version(
-    Version(1, 2, 3, prerelease="b2", dev=4, local="foo.bar"), "1.2.3-b.2-dev.4+foo.bar"
+    Version(1, 2, 3, prerelease="b2", dev=4, local="foo.bar"), "1.2.3-b.2.dev.4+foo.bar"
 )
 _test_format_version(Version(1, 2, 3, dev=4, local="foo.bar"), "1.2.3-dev.4+foo.bar")
 

commit d4e5386f6043f1e1e880f3cbb8651d9c5f879568

Fix parsing version with dev spec
The bug was introduced in #6556 where I change the dev separator from a hyphen to a dot (to be compatible with <semver.org> spec).
What was needed to fix the issue was to update the regex used to parse the version string.

misc/releaser.py
@@ -89,7 +89,7 @@ RELEASE_REGEX = re.compile(
     r"v?"
     r"(?P<release>(?P<major>[0-9]+)\.(?P<minor>[0-9]+)\.(?P<patch>[0-9]+))"
     rf"(?:-(?P<pre>{PRERELEASE_EXPR}))?"
-    r"(?:-dev\.(?P<dev>[0-9]+))?"
+    r"(?:.dev\.(?P<dev>[0-9]+))?"
     r"(?:\+(?P<local>[a-z0-9]+(?:[-_\.][a-z0-9]+)*))?"
     r"$"
 )
@@ -317,6 +317,10 @@ _test_parse_version_string(
 _test_parse_version_string("1.2.3-b42", Version(1, 2, 3, prerelease="b42"))
 _test_parse_version_string("1.2.3-rc1+dev", Version(1, 2, 3, prerelease="rc1", local="dev"))
 _test_parse_version_string("v2.15.0+dev.2950f88", Version(2, 15, 0, local="dev.2950f88"))
+_test_parse_version_string(
+    "2.16.0-a.0.dev.19786+a4f9627ee",
+    Version(2, 16, 0, prerelease="a.0", dev=19786, local="a4f9627ee"),
+)
 assert Version.parse("1.2.3") < Version.parse("1.2.4")
 assert Version.parse("1.2.3-a1") < Version.parse("1.2.3-a2")
 assert Version.parse("1.2.3-a10") < Version.parse("1.2.3-b1")