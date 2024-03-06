ecbe60cddd3151e84f92bf7338fd7dbfa15b94ba
Marcos Medrano <marcosmedrano0@gmail.com>
2024-03-06 12:07:26 +0100

    Add tests for user_revoke

--- server/tests/api_v4/authenticated/test_user_revoke.py
+++ server/tests/api_v4/authenticated/test_user_revoke.py
@@ -3,8 +3,17 @@
 import httpx
 import pytest
 
-from parsec._parsec import DateTime, RevokedUserCertificate, authenticated_cmds
+from parsec._parsec import (
+    DateTime,
+    DeviceID,
+    RevokedUserCertificate,
+    authenticated_cmds,
+)
 from parsec.events import EventUserRevokedOrFrozen
+from tests.api_v4.authenticated.test_user_create import (
+    generate_new_mike_device_certificates,
+    generate_new_mike_user_certificates,
+)
 from tests.common import Backend, CoolorgRpcClients, RpcTransportError
 
 
@@ -77,6 +86,43 @@ async def test_disconnect_sse(
     assert rep.status_code == 461
 
 
+async def test_authenticated_user_revoke_author_not_allowed(
+    coolorg: CoolorgRpcClients, backend: Backend
+) -> None:
+    now = DateTime.now()
+    certif = RevokedUserCertificate(
+        author=coolorg.bob.device_id,
+        timestamp=now,
+        user_id=coolorg.alice.device_id.user_id,
+    )
+
+    expected_dump = await backend.user.test_dump_current_users(coolorg.organization_id)
+    expected_dump[coolorg.alice.device_id.user_id].is_revoked = False
+
+    rep = await coolorg.bob.user_revoke(
+        revoked_user_certificate=certif.dump_and_sign(coolorg.bob.signing_key)
+    )
+    assert rep == authenticated_cmds.v4.user_revoke.RepAuthorNotAllowed()
+
+    dump = await backend.user.test_dump_current_users(coolorg.organization_id)
+    assert dump == expected_dump
+
+
+async def test_authenticated_user_revoke_user_not_found(
+    coolorg: CoolorgRpcClients, backend: Backend
+) -> None:
+    now = DateTime.now()
+    certif = RevokedUserCertificate(
+        author=coolorg.alice.device_id,
+        timestamp=now,
+        user_id=DeviceID("mike@new_dev").user_id,
+    )
+    rep = await coolorg.alice.user_revoke(
+        revoked_user_certificate=certif.dump_and_sign(coolorg.alice.signing_key)
+    )
+    assert rep == authenticated_cmds.v4.user_revoke.RepUserNotFound()
+
+
 async def test_authenticated_user_revoke_user_already_revoked(
     coolorg: CoolorgRpcClients, backend: Backend
 ) -> None:
@@ -108,4 +154,107 @@ async def test_authenticated_user_revoke_user_already_revoked(
     )
     assert rep == authenticated_cmds.v4.user_revoke.RepUserAlreadyRevoked(
         last_common_certificate_timestamp=t1
+    )
+
+
+@pytest.mark.parametrize(
+    "kind",
+    (
+        "revoked_user_certificate",
+        "revoked_user_certificate_not_author_user_id",
+        "revoked_user_certificate_author_device_mismatch",
+    ),
+)
+async def test_authenticated_user_revoke_invalid_certificate(
+    coolorg: CoolorgRpcClients,
+    backend: Backend,
+    kind: str,
+) -> None:
+    now = DateTime.now()
+
+    match kind:
+        case "revoked_user_certificate":
+            certif = b"<dummy>"
+        case "revoked_user_certificate_not_author_user_id":
+            certif = RevokedUserCertificate(
+                author=coolorg.bob.device_id,
+                timestamp=now,
+                user_id=coolorg.bob.device_id.user_id,
+            ).dump_and_sign(coolorg.alice.signing_key)
+        case "revoked_user_certificate_author_device_mismatch":
+            certif = RevokedUserCertificate(
+                author=DeviceID("alice@dev2"),
+                timestamp=now,
+                user_id=coolorg.bob.device_id.user_id,
+            ).dump_and_sign(coolorg.alice.signing_key)
+        case unknown:
+            assert False, unknown
+
+    rep = await coolorg.alice.user_revoke(revoked_user_certificate=certif)
+    assert rep == authenticated_cmds.v4.user_revoke.RepInvalidCertificate()
+
+
+async def test_authenticated_user_revoke_timestamp_out_of_ballpark(
+    coolorg: CoolorgRpcClients,
+    backend: Backend,
+) -> None:
+    t0 = DateTime.now().subtract(seconds=3600)
+    certif = RevokedUserCertificate(
+        author=coolorg.alice.device_id,
+        timestamp=t0,
+        user_id=coolorg.bob.device_id.user_id,
+    ).dump_and_sign(coolorg.alice.signing_key)
+
+    rep = await coolorg.alice.user_revoke(revoked_user_certificate=certif)
+    assert isinstance(rep, authenticated_cmds.v4.user_revoke.RepTimestampOutOfBallpark)
+    assert rep.ballpark_client_early_offset == 300.0
+    assert rep.ballpark_client_late_offset == 320.0
+    assert rep.client_timestamp == t0
+
+
+@pytest.mark.parametrize("kind", ("same_timestamp", "previous_timestamp"))
+async def test_authenticated_user_revoke_require_greater_timestamp(
+    coolorg: CoolorgRpcClients,
+    backend: Backend,
+    kind: str,
+) -> None:
+    now = DateTime.now()
+    match kind:
+        case "same_timestamp":
+            revoked_user_timestamp = now
+        case "previous_timestamp":
+            revoked_user_timestamp = now.subtract(seconds=1)
+        case unknown:
+            assert False, unknown
+
+    # 1) Create a certificate in the organization
+
+    user_certif, redacted_user_certif = generate_new_mike_user_certificates(coolorg.alice, now)
+    device_certif, redacted_device_certif = generate_new_mike_device_certificates(
+        coolorg.alice, now
+    )
+
+    await backend.user.create_user(
+        organization_id=coolorg.organization_id,
+        now=now,
+        author=coolorg.alice.device_id,
+        author_verify_key=coolorg.alice.signing_key.verify_key,
+        user_certificate=user_certif,
+        device_certificate=device_certif,
+        redacted_user_certificate=redacted_user_certif,
+        redacted_device_certificate=redacted_device_certif,
+    )
+
+    # 2) Create revoke user certificate where timestamp is clashing
+    #    with the previous certificate
+
+    certif = RevokedUserCertificate(
+        author=coolorg.alice.device_id,
+        timestamp=revoked_user_timestamp,
+        user_id=coolorg.bob.device_id.user_id,
+    ).dump_and_sign(coolorg.alice.signing_key)
+
+    rep = await coolorg.alice.user_revoke(revoked_user_certificate=certif)
+    assert rep == authenticated_cmds.v4.user_revoke.RepRequireGreaterTimestamp(
+        strictly_greater_than=now
     )
