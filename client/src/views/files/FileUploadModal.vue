77fb05be137b7f1827981d4178bebafcaea91d7a
Maxime GRANDCOLAS <maxime.grandcolas@scille.fr>
2024-03-08 13:15:00 +0100

    [MS] Handle already existing files

--- client/src/views/files/FileUploadModal.vue
+++ client/src/views/files/FileUploadModal.vue
@@ -14,10 +14,11 @@
 </template>
 
 <script setup lang="ts">
-import { MsModal } from '@/components/core';
+import { Answer, askQuestion, MsModal } from '@/components/core';
 import { FileDropZone, FileImport } from '@/components/files';
-import { Path, WorkspaceHandle, WorkspaceID } from '@/parsec';
+import { entryStat, Path, WorkspaceHandle, WorkspaceID } from '@/parsec';
 import { ImportManager, ImportManagerKey } from '@/services/importManager';
+import { translate } from '@/services/translation';
 import { inject } from 'vue';
 
 const importManager = inject(ImportManagerKey) as ImportManager;
@@ -28,33 +29,104 @@ const props = defineProps<{
   workspaceId: WorkspaceID;
 }>();
 
-async function uploadFileEntry(currentPath: string, fsEntry: FileSystemEntry): Promise<void> {
+interface FileImport {
+  file: File;
+  path: string;
+}
+
+async function importFiles(imports: FileImport[]): Promise<void> {
+  const existing: FileImport[] = [];
+
+  for (const imp of imports) {
+    const fullPath = await Path.join(imp.path, imp.file.name);
+    const result = await entryStat(props.workspaceHandle, fullPath);
+    if (result.ok && result.value.isFile()) {
+      existing.push(imp);
+    }
+  }
+
+  if (existing.length > 0) {
+    const answer = await askQuestion(
+      translate('FoldersPage.importModal.replaceTitle', {}, 1),
+      translate('FoldersPage.importModal.replaceQuestion', { file: existing[0].file.name, count: existing.length }),
+      {
+        yesText: translate('FoldersPage.importModal.replaceText', {}, 1),
+        noText: translate('FoldersPage.importModal.skipText', {}, 1),
+      },
+    );
+    if (answer === Answer.No) {
+      imports = imports.filter((imp) => {
+        return (
+          existing.find((ex) => {
+            return ex.file.name === imp.file.name && ex.path === imp.path;
+          }) === undefined
+        );
+      });
+    }
+  }
+  for (const imp of imports) {
+    await importManager.importFile(props.workspaceHandle, props.workspaceId, imp.file, imp.path);
+  }
+}
+
+async function convertEntryToFile(fsEntry: FileSystemEntry): Promise<File> {
+  return new Promise((resolve, reject) => {
+    (fsEntry as FileSystemFileEntry).file(
+      (file) => {
+        resolve(file);
+      },
+      () => {
+        reject();
+      },
+    );
+  });
+}
+
+async function getEntries(fsEntry: FileSystemDirectoryEntry): Promise<FileSystemEntry[]> {
+  return new Promise((resolve, reject) => {
+    const reader = fsEntry.createReader();
+    reader.readEntries(
+      (entries) => {
+        resolve(entries);
+      },
+      () => {
+        reject();
+      },
+    );
+  });
+}
+
+async function unwindEntry(currentPath: string, fsEntry: FileSystemEntry): Promise<FileImport[]> {
   const parsecPath = await Path.join(currentPath, fsEntry.name);
+  const imports: FileImport[] = [];
 
   if (fsEntry.isDirectory) {
-    const reader = (fsEntry as FileSystemDirectoryEntry).createReader();
-    reader.readEntries((entries) => {
-      entries.forEach(async (entry) => {
-        await uploadFileEntry(parsecPath, entry);
-      });
-    });
+    const entries = await getEntries(fsEntry as FileSystemDirectoryEntry);
+    for (const entry of entries) {
+      const result = await unwindEntry(parsecPath, entry);
+      imports.push(...result);
+    }
   } else {
-    (fsEntry as FileSystemFileEntry).file(async (file) => {
-      importManager.importFile(props.workspaceHandle, props.workspaceId, file, currentPath);
-    });
+    const result = await convertEntryToFile(fsEntry);
+    imports.push({ file: result, path: currentPath });
   }
+  return imports;
 }
 
 async function onFilesDrop(entries: FileSystemEntry[]): Promise<void> {
+  const imports: FileImport[] = [];
   for (const entry of entries) {
-    await uploadFileEntry(props.currentPath, entry);
+    const result = await unwindEntry(props.currentPath, entry);
+    imports.push(...result);
   }
+  await importFiles(imports);
 }
 
 async function onFilesImport(files: File[]): Promise<void> {
-  for (const file of files) {
-    importManager.importFile(props.workspaceHandle, props.workspaceId, file, props.currentPath);
-  }
+  const imports: FileImport[] = files.map((file): FileImport => {
+    return { file: file, path: props.currentPath };
+  });
+  await importFiles(imports);
 }
 </script>
 
