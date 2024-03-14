d8613f78e7aa0f9207de4871fe219a4a808147e4
Firelight Flagboy <firelight.flagboy@gmail.com>
2024-03-14 16:23:05 +0100

    [partial #6749]: Add test ok

--- server/tests/api_v4/authenticated/test_invite_cancel.py
+++ server/tests/api_v4/authenticated/test_invite_cancel.py
@@ -1,1 +1,10 @@
 # Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 2016-present Scille SAS
+
+from parsec._parsec import authenticated_cmds
+from tests.common import CoolorgRpcClients
+
+
+async def test_ok(coolorg: CoolorgRpcClients) -> None:
+    rep = await coolorg.alice.invite_cancel(coolorg.invited_alice_dev3.token)
+
+    assert rep == authenticated_cmds.v4.invite_cancel.RepOk()
