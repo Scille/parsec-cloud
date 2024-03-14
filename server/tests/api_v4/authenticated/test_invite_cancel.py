1440dad51dd415d976e97c921e691534ed496596
Firelight Flagboy <firelight.flagboy@gmail.com>
2024-03-14 16:25:14 +0100

    [fix #6749]: Add test invitation already deleted

--- server/tests/api_v4/authenticated/test_invite_cancel.py
+++ server/tests/api_v4/authenticated/test_invite_cancel.py
@@ -14,3 +14,11 @@ async def test_invitation_not_found(coolorg: CoolorgRpcClients) -> None:
     rep = await coolorg.alice.invite_cancel(InvitationToken.new())
 
     assert rep == authenticated_cmds.v4.invite_cancel.RepInvitationNotFound()
+
+
+async def test_invitation_already_deleted(coolorg: CoolorgRpcClients) -> None:
+    await test_ok(coolorg)
+
+    rep = await coolorg.alice.invite_cancel(coolorg.invited_alice_dev3.token)
+
+    assert rep == authenticated_cmds.v4.invite_cancel.RepInvitationAlreadyDeleted()
