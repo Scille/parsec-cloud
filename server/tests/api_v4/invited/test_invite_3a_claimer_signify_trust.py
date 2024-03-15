873d8b17892e9c9b50ac12c1eac0c7e2bdd1f63c
Firelight Flagboy <firelight.flagboy@gmail.com>
2024-03-15 09:53:23 +0100

    [fix #6764]: Add test enrollment wrong state

--- server/tests/api_v4/invited/test_invite_3a_claimer_signify_trust.py
+++ server/tests/api_v4/invited/test_invite_3a_claimer_signify_trust.py
@@ -41,3 +41,8 @@ async def test_ok(run_order: str, coolorg: CoolorgRpcClients) -> None:
         await second(tg.cancel_scope)
 
     assert rep == invited_cmds.v4.invite_3a_claimer_signify_trust.RepOk()
+
+
+async def test_enrollment_wrong_state(coolorg: CoolorgRpcClients) -> None:
+    rep = await coolorg.invited_alice_dev3.invite_3a_claimer_signify_trust()
+    assert rep == invited_cmds.v4.invite_3a_claimer_signify_trust.RepEnrollmentWrongState()
