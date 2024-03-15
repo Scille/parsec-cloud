fb16259a7e1d3159e13b538db83eca599761c95c
Firelight Flagboy <firelight.flagboy@gmail.com>
2024-03-15 15:52:33 +0100

    [partial #6803]: Add test organization already bootstrapped

--- server/tests/api_v4/anonymous/test_organization_bootstrap.py
+++ server/tests/api_v4/anonymous/test_organization_bootstrap.py
@@ -106,3 +106,51 @@ async def test_ok(
     )
 
     assert rep == anonymous_cmds.v4.organization_bootstrap.RepOk()
+
+
+@pytest.mark.usefixtures("ballpark_always_ok")
+async def test_organization_already_bootstrapped(
+    organization_id: OrganizationID,
+    backend_bootstrap_config: ConfigureBackend,
+    testbed: TestbedBackend,
+    anonymous_client: AnonymousRpcClient,
+) -> None:
+    config = await backend_bootstrap_config(organization_id)
+
+    # 1) To do the bootstrap we need certificates, so steal them from another organization
+    _, _, coolorg_content = await testbed.get_template("coolorg")
+    bootstrap_event = coolorg_content.events[0]
+    assert isinstance(bootstrap_event, tb.TestbedEventBootstrapOrganization)
+    user_certificate = bootstrap_event.first_user_raw_certificate
+    redacted_user_certificate = bootstrap_event.first_user_raw_redacted_certificate
+    device_certificate = bootstrap_event.first_user_first_device_raw_certificate
+    redacted_device_certificate = bootstrap_event.first_user_first_device_raw_redacted_certificate
+    root_verify_key = bootstrap_event.root_signing_key.verify_key
+    if config.sequestered:
+        pytest.xfail(reason="TODO: no sequestered template so far !")
+    else:
+        sequester_authority_certificate = None
+
+    rep = await anonymous_client.organization_bootstrap(
+        bootstrap_token=config.bootstrap_token,
+        root_verify_key=root_verify_key,
+        user_certificate=user_certificate,
+        device_certificate=device_certificate,
+        redacted_user_certificate=redacted_user_certificate,
+        redacted_device_certificate=redacted_device_certificate,
+        sequester_authority_certificate=sequester_authority_certificate,
+    )
+
+    assert rep == anonymous_cmds.v4.organization_bootstrap.RepOk()
+
+    rep = await anonymous_client.organization_bootstrap(
+        bootstrap_token=config.bootstrap_token,
+        root_verify_key=root_verify_key,
+        user_certificate=user_certificate,
+        device_certificate=device_certificate,
+        redacted_user_certificate=redacted_user_certificate,
+        redacted_device_certificate=redacted_device_certificate,
+        sequester_authority_certificate=sequester_authority_certificate,
+    )
+
+    assert rep == anonymous_cmds.v4.organization_bootstrap.RepOrganizationAlreadyBootstrapped()
