449a3616746ee5e71bd8427c9da6e6ddf425c054
Firelight Flagboy <firelight.flagboy@gmail.com>
2024-03-15 16:02:27 +0100

    [partial #6803]: Add test invalid bootstrap token

--- server/tests/api_v4/anonymous/test_organization_bootstrap.py
+++ server/tests/api_v4/anonymous/test_organization_bootstrap.py
@@ -1,6 +1,5 @@
 # Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 2016-present Scille SAS
 
-# TODO: test organization_already_bootstrapped is not returned if a invalid token is provided
 from collections.abc import Awaitable
 from dataclasses import dataclass
 from typing import Protocol
@@ -154,3 +153,39 @@ async def test_organization_already_bootstrapped(
     )
 
     assert rep == anonymous_cmds.v4.organization_bootstrap.RepOrganizationAlreadyBootstrapped()
+
+
+@pytest.mark.usefixtures("ballpark_always_ok")
+async def test_invalid_bootstrap_token(
+    organization_id: OrganizationID,
+    backend_bootstrap_config: ConfigureBackend,
+    testbed: TestbedBackend,
+    anonymous_client: AnonymousRpcClient,
+) -> None:
+    config = await backend_bootstrap_config(organization_id)
+
+    # 1) To do the bootstrap we need certificates, so steal them from another organization
+    _, _, coolorg_content = await testbed.get_template("coolorg")
+    bootstrap_event = coolorg_content.events[0]
+    assert isinstance(bootstrap_event, tb.TestbedEventBootstrapOrganization)
+    user_certificate = bootstrap_event.first_user_raw_certificate
+    redacted_user_certificate = bootstrap_event.first_user_raw_redacted_certificate
+    device_certificate = bootstrap_event.first_user_first_device_raw_certificate
+    redacted_device_certificate = bootstrap_event.first_user_first_device_raw_redacted_certificate
+    root_verify_key = bootstrap_event.root_signing_key.verify_key
+    if config.sequestered:
+        pytest.xfail(reason="TODO: no sequestered template so far !")
+    else:
+        sequester_authority_certificate = None
+
+    rep = await anonymous_client.organization_bootstrap(
+        bootstrap_token=BootstrapToken.new(),
+        root_verify_key=root_verify_key,
+        user_certificate=user_certificate,
+        device_certificate=device_certificate,
+        redacted_user_certificate=redacted_user_certificate,
+        redacted_device_certificate=redacted_device_certificate,
+        sequester_authority_certificate=sequester_authority_certificate,
+    )
+
+    assert rep == anonymous_cmds.v4.organization_bootstrap.RepInvalidBootstrapToken()
