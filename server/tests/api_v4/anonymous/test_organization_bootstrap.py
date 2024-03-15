8a2cd4162f92b8edb5206ca9f02b63303d8cbacc
Firelight Flagboy <firelight.flagboy@gmail.com>
2024-03-15 14:51:18 +0100

    Rework test ok for `organization_bootstrap` command

--- server/tests/api_v4/anonymous/test_organization_bootstrap.py
+++ server/tests/api_v4/anonymous/test_organization_bootstrap.py
@@ -1,6 +1,10 @@
 # Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 2016-present Scille SAS
 
 # TODO: test organization_already_bootstrapped is not returned if a invalid token is provided
+from collections.abc import Awaitable
+from dataclasses import dataclass
+from typing import Protocol
+
 import pytest
 
 from parsec._parsec import BootstrapToken, DateTime, OrganizationID, anonymous_cmds
@@ -8,37 +12,76 @@ from parsec._parsec import testbed as tb
 from tests.common import AnonymousRpcClient, AsyncClient, Backend, TestbedBackend
 
 
-@pytest.mark.parametrize("kind", ("standard", "sequestered", "spontaneous"))
-async def test_anonymous_organization_bootstrap_ok(
-    kind: str,
+@pytest.fixture
+def organization_id() -> OrganizationID:
+    return OrganizationID("NewOrg")
+
+
+@pytest.fixture
+def anonymous_client(client: AsyncClient, organization_id: OrganizationID) -> AnonymousRpcClient:
+    return AnonymousRpcClient(client, organization_id)
+
+
+@dataclass
+class BackendPreConfig:
+    spontaneous: bool
+    sequestered: bool
+
+
+@dataclass
+class BackendConfig:
+    spontaneous: bool
+    sequestered: bool
+    bootstrap_token: BootstrapToken | None
+
+
+class ConfigureBackend(Protocol):
+    def __call__(self, organization_id: OrganizationID) -> Awaitable[BackendConfig]:
+        ...
+
+
+@pytest.fixture(
+    params=[
+        pytest.param(BackendPreConfig(spontaneous=False, sequestered=False), id="standard"),
+        pytest.param(BackendPreConfig(spontaneous=False, sequestered=True), id="sequestered"),
+        pytest.param(BackendPreConfig(spontaneous=True, sequestered=False), id="spontaneous"),
+    ]
+)
+def backend_bootstrap_config(backend: Backend, request: pytest.FixtureRequest) -> ConfigureBackend:
+    config: BackendPreConfig = request.param
+
+    async def _backend_bootstrap_config(organization_id: OrganizationID) -> BackendConfig:
+        """
+        Configure the backend to either allow spontaneous bootstrapping or not.
+        When bootstrapping is not spontaneous, a bootstrap token is created and returned.
+        """
+        if config.spontaneous:
+            bootstrap_token = None
+            backend.config.organization_spontaneous_bootstrap = True
+        else:
+            bootstrap_token = await backend.organization.create(
+                now=DateTime.now(), id=organization_id
+            )
+            assert isinstance(bootstrap_token, BootstrapToken), bootstrap_token
+        return BackendConfig(
+            spontaneous=config.spontaneous,
+            sequestered=config.sequestered,
+            bootstrap_token=bootstrap_token,
+        )
+
+    return _backend_bootstrap_config
+
+
+@pytest.mark.usefixtures("ballpark_always_ok")
+async def test_ok(
+    organization_id: OrganizationID,
+    backend_bootstrap_config: ConfigureBackend,
     testbed: TestbedBackend,
-    backend: Backend,
-    client: AsyncClient,
-    ballpark_always_ok: None,
+    anonymous_client: AnonymousRpcClient,
 ) -> None:
-    match kind:
-        case "standard":
-            spontaneous = False
-            sequestered = False
-        case "sequestered":
-            spontaneous = False
-            sequestered = True
-        case "spontaneous":
-            spontaneous = True
-            sequestered = False
-        case unknown:
-            assert False, unknown
+    config = await backend_bootstrap_config(organization_id)
 
-    # 1) Create the organization to bootstrap
-    organization_id = OrganizationID("NewOrg")
-    if spontaneous:
-        bootstrap_token = None
-        backend.config.organization_spontaneous_bootstrap = True
-    else:
-        bootstrap_token = await backend.organization.create(now=DateTime.now(), id=organization_id)
-        assert isinstance(bootstrap_token, BootstrapToken), bootstrap_token
-
-    # 2) To do the bootstrap we need certificates, so steal them from another organization
+    # 1) To do the bootstrap we need certificates, so steal them from another organization
     _, _, coolorg_content = await testbed.get_template("coolorg")
     bootstrap_event = coolorg_content.events[0]
     assert isinstance(bootstrap_event, tb.TestbedEventBootstrapOrganization)
@@ -47,15 +90,13 @@ async def test_anonymous_organization_bootstrap_ok(
     device_certificate = bootstrap_event.first_user_first_device_raw_certificate
     redacted_device_certificate = bootstrap_event.first_user_first_device_raw_redacted_certificate
     root_verify_key = bootstrap_event.root_signing_key.verify_key
-    if sequestered:
+    if config.sequestered:
         pytest.xfail(reason="TODO: no sequestered template so far !")
     else:
         sequester_authority_certificate = None
 
-    rpc = AnonymousRpcClient(client, organization_id)
-
-    rep = await rpc.organization_bootstrap(
-        bootstrap_token=bootstrap_token,
+    rep = await anonymous_client.organization_bootstrap(
+        bootstrap_token=config.bootstrap_token,
         root_verify_key=root_verify_key,
         user_certificate=user_certificate,
         device_certificate=device_certificate,
