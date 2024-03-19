12b2c1e6abef307b3559e746ef2955ebb7f871f2
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2024-03-19 13:18:43 +0100

    Change testbed bindings methods to return a result type instead of panicking

--- bindings/generator/api/testbed.py
+++ bindings/generator/api/testbed.py
@@ -3,22 +3,34 @@
 from typing import Optional
 
 from .addr import ParsecAddr, ParsecOrganizationBootstrapAddr
-from .common import OrganizationID, Path, Ref
+from .common import OrganizationID, Path, Ref, ErrorVariant, Result
 
 
-async def test_new_testbed(template: Ref[str], test_server: Optional[Ref[ParsecAddr]]) -> Path:
+class TestbedError(ErrorVariant):
+    class Disabled:
+        pass
+
+    class Internal:
+        pass
+
+
+async def test_new_testbed(
+    template: Ref[str], test_server: Optional[Ref[ParsecAddr]]
+) -> Result[Path, TestbedError]:
     raise NotImplementedError
 
 
-def test_get_testbed_organization_id(discriminant_dir: Ref[Path]) -> Optional[OrganizationID]:
+def test_get_testbed_organization_id(
+    discriminant_dir: Ref[Path],
+) -> Result[Optional[OrganizationID], TestbedError]:
     raise NotImplementedError
 
 
 def test_get_testbed_bootstrap_organization_addr(
     discriminant_dir: Ref[Path],
-) -> Optional[ParsecOrganizationBootstrapAddr]:
+) -> Result[Optional[ParsecOrganizationBootstrapAddr], TestbedError]:
     raise NotImplementedError
 
 
-async def test_drop_testbed(path: Ref[Path]) -> None:
+async def test_drop_testbed(path: Ref[Path]) -> Result[None, TestbedError]:
     raise NotImplementedError
--- libparsec/src/testbed.rs
+++ libparsec/src/testbed.rs
@@ -4,54 +4,79 @@ use std::path::{Path, PathBuf};
 
 use libparsec_types::prelude::*;
 
-#[cfg(feature = "test-utils")]
-pub async fn test_new_testbed(template: &str, server_addr: Option<&ParsecAddr>) -> PathBuf {
-    let env = libparsec_testbed::test_new_testbed(template, server_addr).await;
-
-    env.discriminant_dir.clone()
-}
-
-#[cfg(not(feature = "test-utils"))]
-pub async fn test_new_testbed(_template: &str, _server_addr: Option<&ParsecAddr>) -> PathBuf {
-    panic!("Test features are disabled")
+#[derive(Debug, thiserror::Error)]
+pub enum TestbedError {
+    #[error("Test features are disabled")]
+    Disabled,
+    #[error(transparent)]
+    Internal(#[from] anyhow::Error),
 }
 
 #[cfg(feature = "test-utils")]
-pub fn test_get_testbed_organization_id(discriminant_dir: &Path) -> Option<OrganizationID> {
-    libparsec_testbed::test_get_testbed(discriminant_dir).map(|env| env.organization_id.clone())
+pub async fn test_new_testbed(
+    template: &str,
+    server_addr: Option<&ParsecAddr>,
+) -> Result<PathBuf, TestbedError> {
+    let env = libparsec_testbed::test_new_testbed(template, server_addr).await?;
+
+    Ok(env.discriminant_dir.clone())
 }
 
 #[cfg(not(feature = "test-utils"))]
-pub fn test_get_testbed_organization_id(_discriminant_dir: &Path) -> Option<OrganizationID> {
-    panic!("Test features are disabled")
+pub async fn test_new_testbed(
+    _template: &str,
+    _server_addr: Option<&ParsecAddr>,
+) -> Result<PathBuf, TestbedError> {
+    Err(TestbedError::Disabled)
+}
+
+#[cfg(feature = "test-utils")]
+pub fn test_get_testbed_organization_id(
+    discriminant_dir: &Path,
+) -> Result<Option<OrganizationID>, TestbedError> {
+    Ok(
+        libparsec_testbed::test_get_testbed(discriminant_dir)
+            .map(|env| env.organization_id.clone()),
+    )
+}
+
+#[cfg(not(feature = "test-utils"))]
+pub fn test_get_testbed_organization_id(
+    _discriminant_dir: &Path,
+) -> Result<Option<OrganizationID>, TestbedError> {
+    Err(TestbedError::Disabled)
 }
 
 #[cfg(feature = "test-utils")]
 pub fn test_get_testbed_bootstrap_organization_addr(
     discriminant_dir: &Path,
-) -> Option<ParsecOrganizationBootstrapAddr> {
-    libparsec_testbed::test_get_testbed(discriminant_dir).map(|env| {
-        ParsecOrganizationBootstrapAddr::new(
-            env.server_addr.clone(),
-            env.organization_id.clone(),
-            None,
-        )
-    })
+) -> Result<Option<ParsecOrganizationBootstrapAddr>, TestbedError> {
+    Ok(
+        libparsec_testbed::test_get_testbed(discriminant_dir).map(|env| {
+            ParsecOrganizationBootstrapAddr::new(
+                env.server_addr.clone(),
+                env.organization_id.clone(),
+                None,
+            )
+        }),
+    )
 }
 
 #[cfg(not(feature = "test-utils"))]
 pub fn test_get_testbed_bootstrap_organization_addr(
     _discriminant_dir: &Path,
-) -> Option<ParsecOrganizationBootstrapAddr> {
-    panic!("Test features are disabled")
+) -> Result<Option<ParsecOrganizationBootstrapAddr>, TestbedError> {
+    Err(TestbedError::Disabled)
 }
 
 #[cfg(feature = "test-utils")]
-pub async fn test_drop_testbed(discriminant_dir: &Path) {
-    libparsec_testbed::test_drop_testbed(discriminant_dir).await
+pub async fn test_drop_testbed(discriminant_dir: &Path) -> Result<(), TestbedError> {
+    libparsec_testbed::test_drop_testbed(discriminant_dir)
+        .await
+        .map_err(TestbedError::Internal)
 }
 
 #[cfg(not(feature = "test-utils"))]
-pub async fn test_drop_testbed(_discriminant_dir: &Path) {
-    panic!("Test features are disabled")
+pub async fn test_drop_testbed(_discriminant_dir: &Path) -> Result<(), TestbedError> {
+    Err(TestbedError::Disabled)
 }
