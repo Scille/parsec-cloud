dcd5db74a1c46a9cc089e24c846ac550c577c520
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2024-03-19 15:43:06 +0100

    disable winfsp

--- libparsec/crates/platform_mountpoint/src/lib.rs
+++ libparsec/crates/platform_mountpoint/src/lib.rs
@@ -2,16 +2,16 @@
 
 #[cfg(target_os = "linux")]
 mod unix;
-#[cfg(target_os = "windows")]
-mod windows;
+// #[cfg(target_os = "windows")]
+// mod windows;
 
 #[cfg(target_os = "linux")]
 pub(crate) use unix as platform;
-#[cfg(target_os = "windows")]
-pub(crate) use windows as platform;
+// #[cfg(target_os = "windows")]
+// pub(crate) use windows as platform;
 
 // TODO: UNIX implementation is linux-only for the moment
-#[cfg(target_os = "macos")]
+#[cfg(any(target_os = "macos", target_os = "windows"))]
 pub(crate) mod platform {
     use std::sync::Arc;
 
@@ -48,7 +48,7 @@ pub(crate) use platform::LOOKUP_HOOK;
 pub use platform::Mountpoint;
 
 // TODO: implement for macOS !
-#[cfg(not(target_os = "macos"))]
+#[cfg(not(any(target_os = "windows", target_os = "macos")))]
 #[cfg(test)]
 #[path = "../tests/unit/operations/mod.rs"]
 #[allow(clippy::unwrap_used)]
