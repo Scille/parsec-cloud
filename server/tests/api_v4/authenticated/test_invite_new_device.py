9edf34ca5f1e53292aadfd6a1e45fd74c964357e
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2024-03-20 14:22:39 +0100

    Fix idempotent® server invitation on server

--- server/tests/api_v4/authenticated/test_invite_new_device.py
+++ server/tests/api_v4/authenticated/test_invite_new_device.py
@@ -4,14 +4,14 @@ from unittest.mock import ANY
 
 import pytest
 
-from parsec._parsec import InvitationStatus, authenticated_cmds
+from parsec._parsec import DateTime, InvitationStatus, authenticated_cmds
 from parsec.components.invite import DeviceInvitation
 from parsec.events import EventInvitation
 from tests.common import Backend, MinimalorgRpcClients
 
 
 @pytest.mark.parametrize("send_email", (False, True))
-async def test_authenticated_invite_new_device_ok(
+async def test_authenticated_invite_new_device_ok_new(
     send_email: bool, minimalorg: MinimalorgRpcClients, backend: Backend
 ) -> None:
     if send_email:
@@ -50,6 +50,46 @@ async def test_authenticated_invite_new_device_ok(
             status=InvitationStatus.IDLE,
         )
     ]
+    assert (
+        await backend.invite.test_dump_all_invitations(minimalorg.organization_id)
+        == expected_invitations
+    )
+
+
+async def test_authenticated_invite_new_device_ok_already_exist(
+    minimalorg: MinimalorgRpcClients, backend: Backend
+) -> None:
+    t1 = DateTime.now()
+
+    outcome = await backend.invite.new_for_device(
+        now=t1,
+        organization_id=minimalorg.organization_id,
+        author=minimalorg.alice.device_id.user_id,
+        send_email=False,
+    )
+    assert isinstance(outcome, tuple)
+    (invitation_token, _) = outcome
+
+    expected_invitations = await backend.invite.test_dump_all_invitations(
+        minimalorg.organization_id
+    )
+
+    with backend.event_bus.spy() as spy:
+        rep = await minimalorg.alice.invite_new_device(
+            send_email=False,
+        )
+        assert isinstance(rep, authenticated_cmds.v4.invite_new_device.RepOk)
+        assert rep.token == invitation_token
+
+        await spy.wait_event_occurred(
+            EventInvitation(
+                organization_id=minimalorg.organization_id,
+                greeter=minimalorg.alice.device_id.user_id,
+                token=invitation_token,
+                status=InvitationStatus.IDLE,
+            )
+        )
+
     assert (
         await backend.invite.test_dump_all_invitations(minimalorg.organization_id)
         == expected_invitations
