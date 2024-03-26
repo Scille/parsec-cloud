b6bbfdfb77485a96ea827ffe21cbd71943fe7d1f
Firelight Flagboy <firelight.flagboy@gmail.com>
2024-03-26 15:34:31 +0100

    [fix #6952]: Add test for keepalive in `events_listen`

--- server/tests/api_v4/authenticated/test_events_listen.py
+++ server/tests/api_v4/authenticated/test_events_listen.py
@@ -323,6 +323,22 @@ async def test_receive_event_of_newly_shared_realm(
         )
 
 
-# TODO: test keepalive
 # TODO: test `Last-Event-ID`
 # TODO: test connection gets closed due to SseAPiEventsListenBadOutcome
+
+
+@pytest.mark.timeout(2)
+async def test_keep_alive(minimalorg: MinimalorgRpcClients, backend: Backend) -> None:
+    # Reduce keepalive to 0.1s to speed up the test
+    backend.config.sse_keepalive = 0.1
+    got_keepalive = False
+
+    async with minimalorg.alice.raw_sse_connection() as raw_sse_stream:
+        async for line in raw_sse_stream.aiter_lines():
+            line = line.rstrip("\n")
+
+            if line == ":keepalive":
+                got_keepalive = True
+                break
+
+    assert got_keepalive
