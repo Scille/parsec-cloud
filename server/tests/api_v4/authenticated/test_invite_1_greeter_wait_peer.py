116efea3a3fd5dcc291c798f643909b5a4404003
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2024-03-27 15:04:34 +0100

    Improve invite commands in server to handle retry-on-gateway-timeout

--- server/tests/api_v4/authenticated/test_invite_1_greeter_wait_peer.py
+++ server/tests/api_v4/authenticated/test_invite_1_greeter_wait_peer.py
@@ -7,6 +7,8 @@ from parsec._parsec import InvitationToken, PrivateKey, authenticated_cmds
 from parsec.components.invite import ConduitState
 from parsec.events import EventEnrollmentConduit
 from tests.common import Backend, CoolorgRpcClients
+from tests.common.client import RpcTransportError
+from tests.common.invite import patch_backend_for_timeout_during_conduit_listen
 
 Response = authenticated_cmds.v4.invite_1_greeter_wait_peer.Rep | None
 
@@ -17,7 +19,7 @@ async def test_ok(run_order: str, coolorg: CoolorgRpcClients, backend: Backend) 
     invitation_token = coolorg.invited_alice_dev3.token
     rep: Response = None
 
-    async def claimer_step_1(cancel_scope: anyio.CancelScope) -> None:
+    async def claimer_step_1() -> None:
         await backend.invite.conduit_exchange(
             organization_id=coolorg.organization_id,
             greeter=None,
@@ -26,13 +28,12 @@ async def test_ok(run_order: str, coolorg: CoolorgRpcClients, backend: Backend) 
             payload=claimer_private_key.public_key.encode(),
         )
 
-    async def greeter_step_1(cancel_scope: anyio.CancelScope) -> None:
+    async def greeter_step_1() -> None:
         nonlocal rep
         rep = await coolorg.alice.invite_1_greeter_wait_peer(
             token=invitation_token,
             greeter_public_key=PrivateKey.generate().public_key,
         )
-        cancel_scope.cancel()
 
     match run_order:
         case "greeter_first":
@@ -46,10 +47,10 @@ async def test_ok(run_order: str, coolorg: CoolorgRpcClients, backend: Backend) 
 
     with backend.event_bus.spy() as spy:
         async with anyio.create_task_group() as tg:
-            tg.start_soon(first, tg.cancel_scope)
+            tg.start_soon(first)
             await spy.wait(EventEnrollmentConduit)
 
-            await second(tg.cancel_scope)
+            await second()
 
     assert rep == authenticated_cmds.v4.invite_1_greeter_wait_peer.RepOk(
         claimer_public_key=claimer_private_key.public_key
@@ -74,3 +75,102 @@ async def test_invitation_deleted(coolorg: CoolorgRpcClients) -> None:
     )
 
     assert rep == authenticated_cmds.v4.invite_1_greeter_wait_peer.RepInvitationDeleted()
+
+
+async def test_gateway_timeout_recover_before_peer_arrives(
+    coolorg: CoolorgRpcClients, backend: Backend
+) -> None:
+    claimer_private_key = PrivateKey.generate()
+    greeter_public_key = PrivateKey.generate().public_key
+    invitation_token = coolorg.invited_alice_dev3.token
+    rep: Response = None
+
+    # 1) Greeter arrives first, waits too long and gets a gateway timeout !
+
+    patch_backend_for_timeout_during_conduit_listen(
+        backend, state=ConduitState.STATE_1_WAIT_PEERS, is_greeter=True
+    )
+
+    with pytest.raises(RpcTransportError) as exc:
+        await coolorg.alice.invite_1_greeter_wait_peer(
+            token=invitation_token,
+            greeter_public_key=greeter_public_key,
+        )
+    assert exc.value.rep.status_code == 504
+
+    # 2) Greeter reconnects before claimer arrives and hence can pretend its timeout never occured
+
+    rep = None
+
+    async def greeter_step_1() -> None:
+        nonlocal rep
+        rep = await coolorg.alice.invite_1_greeter_wait_peer(
+            token=invitation_token,
+            greeter_public_key=greeter_public_key,
+        )
+
+    with backend.event_bus.spy() as spy:
+        async with anyio.create_task_group() as tg:
+            tg.start_soon(greeter_step_1)
+            await spy.wait(EventEnrollmentConduit)
+
+            await backend.invite.conduit_exchange(
+                organization_id=coolorg.organization_id,
+                greeter=None,
+                token=invitation_token,
+                state=ConduitState.STATE_1_WAIT_PEERS,
+                payload=claimer_private_key.public_key.encode(),
+            )
+
+    assert rep == authenticated_cmds.v4.invite_1_greeter_wait_peer.RepOk(
+        claimer_public_key=claimer_private_key.public_key
+    )
+
+
+async def test_gateway_timeout_recover_after_peer_arrives(
+    coolorg: CoolorgRpcClients, backend: Backend
+) -> None:
+    claimer_private_key = PrivateKey.generate()
+    greeter_public_key = PrivateKey.generate().public_key
+    invitation_token = coolorg.invited_alice_dev3.token
+
+    # 1) Greeter arrives first, waits too long and gets a gateway timeout !
+
+    patch_backend_for_timeout_during_conduit_listen(
+        backend, state=ConduitState.STATE_1_WAIT_PEERS, is_greeter=True
+    )
+
+    with pytest.raises(RpcTransportError) as exc:
+        await coolorg.alice.invite_1_greeter_wait_peer(
+            token=invitation_token,
+            greeter_public_key=greeter_public_key,
+        )
+    assert exc.value.rep.status_code == 504
+
+    # 2) Then claimer arrives, talks, but have to wait for the greeter to come back
+
+    with backend.event_bus.spy() as spy:
+        async with anyio.create_task_group() as tg:
+
+            async def claimer_step_1() -> None:
+                await backend.invite.conduit_exchange(
+                    organization_id=coolorg.organization_id,
+                    greeter=None,
+                    token=invitation_token,
+                    state=ConduitState.STATE_1_WAIT_PEERS,
+                    payload=claimer_private_key.public_key.encode(),
+                )
+
+            tg.start_soon(claimer_step_1)
+            await spy.wait(EventEnrollmentConduit)
+
+            # 3) Greeter reconnects and pretends its timeout never occured
+
+            rep = await coolorg.alice.invite_1_greeter_wait_peer(
+                token=invitation_token,
+                greeter_public_key=greeter_public_key,
+            )
+
+            assert rep == authenticated_cmds.v4.invite_1_greeter_wait_peer.RepOk(
+                claimer_public_key=claimer_private_key.public_key
+            )
--- server/tests/api_v4/invited/test_invite_1_claimer_wait_peer.py
+++ server/tests/api_v4/invited/test_invite_1_claimer_wait_peer.py
@@ -7,6 +7,8 @@ from parsec._parsec import PrivateKey, invited_cmds
 from parsec.components.invite import ConduitState
 from parsec.events import EventEnrollmentConduit
 from tests.common import Backend, CoolorgRpcClients
+from tests.common.client import RpcTransportError
+from tests.common.invite import patch_backend_for_timeout_during_conduit_listen
 
 
 @pytest.mark.parametrize("first_to_run", ("greeter_first", "claimer_first"))
@@ -16,14 +18,13 @@ async def test_ok(first_to_run: str, coolorg: CoolorgRpcClients, backend: Backen
     invitation_token = coolorg.invited_alice_dev3.token
     rep: invited_cmds.v4.invite_1_claimer_wait_peer.Rep | None = None
 
-    async def claimer_step_1(cancel_scope: anyio.CancelScope):
+    async def claimer_step_1():
         nonlocal rep
         rep = await coolorg.invited_alice_dev3.invite_1_claimer_wait_peer(
             claimer_public_key=claimer_private_key.public_key,
         )
-        cancel_scope.cancel()
 
-    async def greeter_step_1(cancel_scope: anyio.CancelScope):
+    async def greeter_step_1():
         await backend.invite.conduit_exchange(
             organization_id=coolorg.organization_id,
             greeter=coolorg.alice.device_id.user_id,
@@ -44,11 +45,106 @@ async def test_ok(first_to_run: str, coolorg: CoolorgRpcClients, backend: Backen
 
     with backend.event_bus.spy() as spy:
         async with anyio.create_task_group() as tg:
-            tg.start_soon(first, tg.cancel_scope)
+            tg.start_soon(first)
             await spy.wait(EventEnrollmentConduit)
 
-            await second(tg.cancel_scope)
+            await second()
 
     assert rep == invited_cmds.v4.invite_1_claimer_wait_peer.RepOk(
         greeter_public_key=greeter_private_key.public_key
     )
+
+
+async def test_gateway_timeout_recover_before_peer_arrives(
+    coolorg: CoolorgRpcClients, backend: Backend
+) -> None:
+    greeter_private_key = PrivateKey.generate()
+    claimer_private_key = PrivateKey.generate()
+    invitation_token = coolorg.invited_alice_dev3.token
+    rep: invited_cmds.v4.invite_1_claimer_wait_peer.Rep | None = None
+
+    # 1) Claimer arrives first, waits too long and gets a gateway timeout !
+
+    patch_backend_for_timeout_during_conduit_listen(
+        backend, state=ConduitState.STATE_1_WAIT_PEERS, is_greeter=False
+    )
+
+    with pytest.raises(RpcTransportError) as exc:
+        await coolorg.invited_alice_dev3.invite_1_claimer_wait_peer(
+            claimer_public_key=claimer_private_key.public_key,
+        )
+    assert exc.value.rep.status_code == 504
+
+    # 2) Claimer reconnects before greeter arrives and hence can pretend its timeout never occured
+
+    rep = None
+
+    async def greeter_step_1() -> None:
+        nonlocal rep
+        rep = await coolorg.invited_alice_dev3.invite_1_claimer_wait_peer(
+            claimer_public_key=claimer_private_key.public_key,
+        )
+
+    with backend.event_bus.spy() as spy:
+        async with anyio.create_task_group() as tg:
+            tg.start_soon(greeter_step_1)
+            await spy.wait(EventEnrollmentConduit)
+
+            await backend.invite.conduit_exchange(
+                organization_id=coolorg.organization_id,
+                greeter=coolorg.alice.device_id.user_id,
+                token=invitation_token,
+                state=ConduitState.STATE_1_WAIT_PEERS,
+                payload=greeter_private_key.public_key.encode(),
+            )
+
+    assert rep == invited_cmds.v4.invite_1_claimer_wait_peer.RepOk(
+        greeter_public_key=greeter_private_key.public_key
+    )
+
+
+async def test_gateway_timeout_recover_after_peer_arrives(
+    coolorg: CoolorgRpcClients, backend: Backend
+) -> None:
+    greeter_private_key = PrivateKey.generate()
+    claimer_private_key = PrivateKey.generate()
+    invitation_token = coolorg.invited_alice_dev3.token
+    rep: invited_cmds.v4.invite_1_claimer_wait_peer.Rep | None = None
+
+    # 1) Claimer arrives first, waits too long and gets a gateway timeout !
+
+    patch_backend_for_timeout_during_conduit_listen(
+        backend, state=ConduitState.STATE_1_WAIT_PEERS, is_greeter=False
+    )
+
+    with pytest.raises(RpcTransportError) as exc:
+        await coolorg.invited_alice_dev3.invite_1_claimer_wait_peer(
+            claimer_public_key=claimer_private_key.public_key,
+        )
+    assert exc.value.rep.status_code == 504
+
+    # 2) Then greeter arrives, talks, but have to wait for the claimer to come back
+
+    with backend.event_bus.spy() as spy:
+        async with anyio.create_task_group() as tg:
+
+            async def greeter_step_1() -> None:
+                await backend.invite.conduit_exchange(
+                    organization_id=coolorg.organization_id,
+                    greeter=coolorg.alice.device_id.user_id,
+                    token=invitation_token,
+                    state=ConduitState.STATE_1_WAIT_PEERS,
+                    payload=greeter_private_key.public_key.encode(),
+                )
+
+            tg.start_soon(greeter_step_1)
+            await spy.wait(EventEnrollmentConduit)
+
+            # 3) Claimer reconnects and pretends its timeout never occured
+
+            rep = await coolorg.invited_alice_dev3.invite_1_claimer_wait_peer(
+                claimer_public_key=claimer_private_key.public_key,
+            )
+            assert rep == invited_cmds.v4.invite_1_claimer_wait_peer.RepOk(
+                greeter_public_key=greeter_private_key.public_key
+            )
