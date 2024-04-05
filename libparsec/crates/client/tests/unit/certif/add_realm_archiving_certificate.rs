048a51880f8c7de63da525c30b565d68aa733966
TimeEngineer <chenpascal241096@yahoo.fr>
2024-04-05 12:12:58 +0200

    Use fetch for add multiple certificates tests

--- libparsec/crates/client/tests/unit/certif/add_realm_archiving_certificate.rs
+++ libparsec/crates/client/tests/unit/certif/add_realm_archiving_certificate.rs
@@ -1,5 +1,7 @@
 // Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 2016-present Scille SAS
 
+use std::collections::HashMap;
+
 use libparsec_tests_fixtures::prelude::*;
 use libparsec_types::prelude::*;
 
@@ -65,24 +67,29 @@ async fn multiple(
     second_conf: RealmArchivingConfiguration,
     env: &TestbedEnv,
 ) {
-    let env = env.customize(|builder| {
+    let (env, realm_id) = env.customize_with_map(|builder| {
         let realm_id = builder
             .new_realm("alice")
             .then_do_initial_key_rotation()
             .map(|event| event.realm);
-
+        builder.certificates_storage_fetch_certificates("alice@dev1");
         builder.archive_realm(realm_id, first_conf);
         builder.archive_realm(realm_id, second_conf);
+        realm_id
     });
     let alice = env.local_device("alice@dev1");
     let ops = certificates_ops_factory(&env, &alice).await;
+    let realm_certificates = &env.get_realms_certificates_signed()[&realm_id];
 
     let switch = ops
         .add_certificates_batch(
-            &env.get_common_certificates_signed(),
             &[],
             &[],
-            &env.get_realms_certificates_signed(),
+            &[],
+            &HashMap::from([(
+                realm_id,
+                realm_certificates[realm_certificates.len() - 2..].to_vec(),
+            )]),
         )
         .await
         .unwrap();
