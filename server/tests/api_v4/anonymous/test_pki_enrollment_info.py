cd2c0c771efcf2fa1132bc011f7eee537b263372
Marcos Medrano <marcosmedrano0@gmail.com>
2024-04-23 16:16:18 +0200

    Add stubs for missing PKI command tests

--- server/tests/api_v4/anonymous/test_pki_enrollment_info.py
+++ server/tests/api_v4/anonymous/test_pki_enrollment_info.py
@@ -1,1 +1,13 @@
 # Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 2016-present Scille SAS
+
+import pytest
+
+
+@pytest.mark.xfail(reason="TODO: PKI is not fully implemented yet")
+async def test_anonymous_pki_enrollment_info_ok() -> None:
+    raise NotImplementedError
+
+
+@pytest.mark.xfail(reason="TODO: PKI is not fully implemented yet")
+async def test_anonymous_pki_enrollment_info_enrollment_not_found() -> None:
+    raise NotImplementedError
--- server/tests/api_v4/authenticated/test_pki_enrollment_reject.py
+++ server/tests/api_v4/authenticated/test_pki_enrollment_reject.py
@@ -1,1 +1,23 @@
 # Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 2016-present Scille SAS
+
+import pytest
+
+
+@pytest.mark.xfail(reason="TODO: PKI is not fully implemented yet")
+async def test_authenticated_pki_enrollment_reject_author_not_allowed() -> None:
+    raise NotImplementedError
+
+
+@pytest.mark.xfail(reason="TODO: PKI is not fully implemented yet")
+async def test_authenticated_pki_enrollment_reject_enrollment_no_longer_available() -> None:
+    raise NotImplementedError
+
+
+@pytest.mark.xfail(reason="TODO: PKI is not fully implemented yet")
+async def test_authenticated_pki_enrollment_reject_enrollment_not_found() -> None:
+    raise NotImplementedError
+
+
+@pytest.mark.xfail(reason="TODO: PKI is not fully implemented yet")
+async def test_authenticated_pki_enrollment_reject_ok() -> None:
+    raise NotImplementedError
