979b766dae3740ea3af584124902b16cb666aae3
Maxime GRANDCOLAS <maxime.grandcolas@scille.fr>
2024-04-25 08:24:20 +0200

    [MS] Updated playwright config

--- client/playwright.config.ts
+++ client/playwright.config.ts
@@ -20,11 +20,11 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   webServer: {
-    command: 'npm run dev',
+    command: 'npm run dev -- --port 8080',
     url: 'http://localhost:8080',
     reuseExistingServer: true,
   },
@@ -73,11 +73,4 @@ export default defineConfig({
     //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
     // },
   ],
-
-  /* Run your local dev server before starting the tests */
-  webServer: {
-    command: 'npm run dev -- --port 8080',
-    url: 'http://localhost:8080',
-    reuseExistingServer: !process.env.CI,
-  },
 });
