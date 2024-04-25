64540da5e41fc26698313fb8bc86b91d82fb3ab5
Marcos Medrano <marcosmedrano0@gmail.com>
2024-04-25 12:32:05 +0200

    Fix send invitation mail

--- server/tests/api_v4/authenticated/test_invite_new_user.py
+++ server/tests/api_v4/authenticated/test_invite_new_user.py
@@ -5,7 +5,7 @@ from unittest.mock import ANY
 import pytest
 
 from parsec._parsec import DateTime, InvitationStatus, authenticated_cmds
-from parsec.components.invite import UserInvitation
+from parsec.components.invite import SendEmailBadOutcome, UserInvitation
 from parsec.events import EventInvitation
 from tests.common import Backend, CoolorgRpcClients, MinimalorgRpcClients
 
@@ -14,13 +14,10 @@ from tests.common import Backend, CoolorgRpcClients, MinimalorgRpcClients
 async def test_authenticated_invite_new_user_ok_new(
     send_email: bool, minimalorg: MinimalorgRpcClients, backend: Backend
 ) -> None:
-    if send_email:
-        pytest.xfail(reason="TODO")
-
     with backend.event_bus.spy() as spy:
         rep = await minimalorg.alice.invite_new_user(
             claimer_email="new@example.invalid",
-            send_email=False,
+            send_email=send_email,
         )
         assert isinstance(rep, authenticated_cmds.v4.invite_new_user.RepOk)
         assert (
@@ -114,3 +111,66 @@ async def test_authenticated_invite_new_user_claimer_email_already_enrolled(
         send_email=False,
     )
     assert rep == authenticated_cmds.v4.invite_new_user.RepClaimerEmailAlreadyEnrolled()
+
+
+@pytest.mark.parametrize(
+    "bad_outcome",
+    (
+        SendEmailBadOutcome.BAD_SMTP_CONFIG,
+        SendEmailBadOutcome.RECIPIENT_REFUSED,
+        SendEmailBadOutcome.SERVER_UNAVAILABLE,
+    ),
+)
+async def test_authenticated_invite_new_user_send_email_bad_outcome(
+    minimalorg: MinimalorgRpcClients,
+    backend: Backend,
+    bad_outcome: SendEmailBadOutcome,
+    monkeypatch,
+) -> None:
+    async def _mocked_send_email(*args, **kwargs):
+        return bad_outcome
+
+    monkeypatch.setattr("parsec.components.invite.send_email", _mocked_send_email)
+
+    with backend.event_bus.spy() as spy:
+        rep = await minimalorg.alice.invite_new_user(
+            claimer_email="new@example.invalid",
+            send_email=True,
+        )
+        assert isinstance(rep, authenticated_cmds.v4.invite_new_user.RepOk)
+        invitation_token = rep.token
+
+        match bad_outcome:
+            case SendEmailBadOutcome.BAD_SMTP_CONFIG:
+                expected_email_sent_status = authenticated_cmds.v4.invite_new_user.InvitationEmailSentStatus.SERVER_UNAVAILABLE
+            case SendEmailBadOutcome.RECIPIENT_REFUSED:
+                expected_email_sent_status = authenticated_cmds.v4.invite_new_user.InvitationEmailSentStatus.RECIPIENT_REFUSED
+            case SendEmailBadOutcome.SERVER_UNAVAILABLE:
+                expected_email_sent_status = authenticated_cmds.v4.invite_new_user.InvitationEmailSentStatus.SERVER_UNAVAILABLE
+        assert rep.email_sent == expected_email_sent_status
+
+        await spy.wait_event_occurred(
+            EventInvitation(
+                organization_id=minimalorg.organization_id,
+                greeter=minimalorg.alice.device_id.user_id,
+                token=invitation_token,
+                status=InvitationStatus.IDLE,
+            )
+        )
+
+    expected_invitations = {
+        minimalorg.alice.device_id.user_id: [
+            UserInvitation(
+                token=invitation_token,
+                created_on=ANY,
+                claimer_email="new@example.invalid",
+                created_by=minimalorg.alice.device_id,
+                created_by_human_handle=minimalorg.alice.human_handle,
+                status=InvitationStatus.IDLE,
+            )
+        ]
+    }
+    assert (
+        await backend.invite.test_dump_all_invitations(minimalorg.organization_id)
+        == expected_invitations
+    )
