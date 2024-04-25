b8f557b0baf56c44bacbd56159b621213d19e356
Alban Costaz <alban.costaz@gmail.com>
2024-04-25 10:59:07 +0200

    [MS] Replaced ms-dropdown by megashark-lib

--- client/src/components/core/ms-dropdown/index.ts
+++
@@ -1,5 +0,0 @@
-// Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 2016-present Scille SAS
-import MsDropdown from '@/components/core/ms-dropdown/MsDropdown.vue';
-
-export * from '@/components/core/ms-dropdown/types';
-export { MsDropdown };
