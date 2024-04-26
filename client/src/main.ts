31b166c69f459bb91bc8683e43afb5030e04de3e
NicoTuxx <nicotuxxdev@gmail.com>
2024-04-26 17:51:38 +0200

    feat: reorganize ionic css imports

--- client/src/main.ts
+++ client/src/main.ts
@@ -13,27 +13,8 @@ import {
   navigateTo,
   switchOrganization,
 } from '@/router';
-
-import { IonicVue, modalController } from '@ionic/vue';
-
-/* Core CSS required for Ionic components to work properly */
-import '@ionic/vue/css/core.css';
-
-/* Basic CSS for apps built with Ionic */
-import '@ionic/vue/css/normalize.css';
-import '@ionic/vue/css/structure.css';
-import '@ionic/vue/css/typography.css';
-
-/* Optional CSS utils that can be commented out */
-import '@ionic/vue/css/display.css';
-import '@ionic/vue/css/flex-utils.css';
-import '@ionic/vue/css/float-elements.css';
-import '@ionic/vue/css/padding.css';
-import '@ionic/vue/css/text-alignment.css';
-import '@ionic/vue/css/text-transformation.css';
-
 import { Config, StorageManager, StorageManagerKey } from '@/services/storageManager';
-import { isPlatform } from '@ionic/vue';
+import { IonicVue, isPlatform, modalController } from '@ionic/vue';
 
 /* Theme variables */
 import '@/theme/global.scss';
