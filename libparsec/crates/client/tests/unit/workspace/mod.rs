d2c9690b19801e2756b47de8f1d69fe506b5b2b1
TimeEngineer <chenpascal241096@yahoo.fr>
2024-04-30 21:23:37 +0200

    Add File transactions stateful test

--- libparsec/crates/client/tests/unit/workspace/mod.rs
+++ libparsec/crates/client/tests/unit/workspace/mod.rs
@@ -1,5 +1,7 @@
 // Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 2016-present Scille SAS
 
+use proptest_state_machine::ReferenceStateMachine;
+
 mod base;
 mod create_file;
 mod create_folder;
@@ -9,6 +11,7 @@ mod fd_read;
 mod fd_write;
 mod file_operations;
 mod file_operations_stateful;
+mod file_transactions_stateful;
 mod folder_transactions;
 mod inbound_sync_file;
 mod inbound_sync_folder;
@@ -21,3 +24,109 @@ mod outbound_sync_root;
 mod remove_entry;
 mod stat_entry;
 mod utils;
+
+pub trait AsyncStateMachineTest {
+    /// The concrete state, that is the system under test (SUT).
+    type SystemUnderTest;
+
+    /// The abstract state machine that implements [`ReferenceStateMachine`]
+    /// drives the generation of the state machine's transitions.
+    type Reference: ReferenceStateMachine;
+
+    /// Initialize the state of SUT.
+    ///
+    /// If the reference state machine is generated from a non-constant
+    /// strategy, ensure to use it to initialize the SUT to a corresponding
+    /// state.
+    async fn init_test(
+        ref_state: &<Self::Reference as ReferenceStateMachine>::State,
+    ) -> Self::SystemUnderTest;
+
+    /// Apply a transition in the SUT state and check post-conditions.
+    /// The post-conditions are properties of your state machine that you want
+    /// to assert.
+    ///
+    /// Note that the `ref_state` is the state *after* this `transition` is
+    /// applied. You can use it to compare it with your SUT after you apply
+    /// the transition.
+    async fn apply(
+        state: Self::SystemUnderTest,
+        ref_state: &<Self::Reference as ReferenceStateMachine>::State,
+        transition: <Self::Reference as ReferenceStateMachine>::Transition,
+    ) -> Self::SystemUnderTest;
+
+    /// Check some invariant on the SUT state after every transition.
+    ///
+    /// Note that just like in [`StateMachineTest::apply`] you can use
+    /// the `ref_state` to compare it with your SUT.
+    async fn check_invariants(
+        state: &Self::SystemUnderTest,
+        ref_state: &<Self::Reference as ReferenceStateMachine>::State,
+    ) {
+        // This is to avoid `unused_variables` warning
+        let _ = (state, ref_state);
+    }
+
+    /// Override this function to add some teardown logic on the SUT state
+    /// at the end of each test case. The default implementation simply drops
+    /// the state.
+    async fn teardown(state: Self::SystemUnderTest) {
+        // This is to avoid `unused_variables` warning
+        let _ = state;
+    }
+}
+
+#[macro_export]
+macro_rules! impl_async_state_machine {
+    ($t:ty) => {
+        impl StateMachineTest for $t {
+            type SystemUnderTest = (
+                tokio::runtime::Runtime,
+                <Self as AsyncStateMachineTest>::SystemUnderTest,
+            );
+            type Reference = <Self as AsyncStateMachineTest>::Reference;
+
+            fn init_test(
+                ref_state: &<Self::Reference as ReferenceStateMachine>::State,
+            ) -> Self::SystemUnderTest {
+                let rt = tokio::runtime::Builder::new_current_thread()
+                    .enable_all()
+                    .build()
+                    .unwrap();
+                let state = rt.block_on(async {
+                    <Self as AsyncStateMachineTest>::init_test(ref_state).await
+                });
+                (rt, state)
+            }
+
+            fn apply(
+                state: Self::SystemUnderTest,
+                ref_state: &<Self::Reference as ReferenceStateMachine>::State,
+                transition: <Self::Reference as ReferenceStateMachine>::Transition,
+            ) -> Self::SystemUnderTest {
+                let (rt, state) = state;
+                let state = rt.block_on(async {
+                    <Self as AsyncStateMachineTest>::apply(state, ref_state, transition).await
+                });
+                (rt, state)
+            }
+
+            fn check_invariants(
+                state: &Self::SystemUnderTest,
+                ref_state: &<Self::Reference as ReferenceStateMachine>::State,
+            ) {
+                let (rt, state) = state;
+                rt.block_on(async {
+                    <Self as AsyncStateMachineTest>::check_invariants(state, ref_state).await
+                });
+            }
+
+            fn teardown(state: Self::SystemUnderTest) {
+                let (rt, state) = state;
+                rt.block_on(async {
+                    <Self as AsyncStateMachineTest>::teardown(state).await;
+                });
+            }
+        }
+    };
+}
