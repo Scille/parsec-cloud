c56718ae9f1d97b195c417d443d388b184875648
Firelight Flagboy <firelight.flagboy@gmail.com>
2024-05-27 13:54:32 +0200

    Override method `getLatestVersion` to allow the use of the nightly channel
    Inspired by https://github.com/electron-userland/electron-builder/blob/77f977435c99247d5db395895618b150f5006e8f/packages/electron-updater/src/providers/GitHubProvider.ts

--- client/electron/src/updater.ts
+++ client/electron/src/updater.ts
@@ -1,18 +1,50 @@
 // Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 2016-present Scille SAS
 
-import type { CustomPublishOptions, ProgressInfo, UpdateInfo } from 'builder-util-runtime';
+import type { CustomPublishOptions, ProgressInfo, UpdateInfo, XElement } from 'builder-util-runtime';
+import { newError, parseXml } from 'builder-util-runtime';
 import type { Logger } from 'electron-log';
 import type { BaseUpdater, AppUpdater as _AppUpdater } from 'electron-updater';
-import { GitHubProvider } from 'electron-updater/out/providers/GitHubProvider';
-import type { ProviderRuntimeOptions } from 'electron-updater/out/providers/Provider';
+import { CancellationToken } from 'electron-updater';
+import { GitHubProvider, computeReleaseNotes } from 'electron-updater/out/providers/GitHubProvider';
+import { parseUpdateInfo, type ProviderRuntimeOptions } from 'electron-updater/out/providers/Provider';
+import { getChannelFilename, newUrlFromBase } from 'electron-updater/out/util';
+import * as semver from 'semver';
 import type { CustomPublishOptions as CustomGitHubOptions } from '../assets/publishConfig';
 
-// @ts-expect-error TS2415: GithubProvider don't expose `getChannelFilePrefix` method from `Provider` which we need to override.
+// Greatly inspired by (it isn't exported by `electron-updater`)
+// https://github.com/electron-userland/electron-builder/blob/77f977435c99247d5db395895618b150f5006e8f/packages/electron-updater/src/providers/GitHubProvider.ts#L11-L13
+interface GithubUpdateInfo extends UpdateInfo {
+  tag: string;
+}
+
+interface GithubReleaseInfo {
+  readonly tag_name: string;
+}
+
+interface GithubReleaseData {
+  tag?: string;
+  feedElement?: XElement;
+}
+
+enum ErrorCodes {
+  InvalidReleaseFeed = 'ERR_UPDATER_INVALID_RELEASE_FEED',
+  LatestVersionNotFound = 'ERR_UPDATER_LATEST_VERSION_NOT_FOUND',
+  NoPublishedVersions = 'ERR_UPDATER_NO_PUBLISHED_VERSIONS',
+  ChannelFileNotFound = 'ERR_UPDATER_CHANNEL_FILE_NOT_FOUND',
+}
+
+/** Match the tag part of a github tag link */
+const HREF_REGEXP = /\/tag\/([^/]+)$/;
+
+// @ts-expect-error TS2415: GithubProvider don't expose
+//  - `getChannelFilePrefix` method from `Provider` which we need to override.
+//  - `getLatestVersion` method that we need to override to allow the use of the nightly channel.
 class CustomGithubProvider extends GitHubProvider {
   constructor(
     protected readonly options: CustomGitHubOptions,
     protected readonly updater: _AppUpdater,
-    protected readonly runtimeOptions: ProviderRuntimeOptions) {
+    protected readonly runtimeOptions: ProviderRuntimeOptions,
+  ) {
     super(
       {
         ...options,
@@ -42,6 +74,177 @@ class CustomGithubProvider extends GitHubProvider {
       default:
         return `-${this.runtimeOptions.platform}-${arch}`;
     }
+  }
+
+  async getLatestVersion(): Promise<GithubUpdateInfo> {
+    const cancellationToken = new CancellationToken();
+
+    // 1. Fetch the latest release.
+    const feed = await this.fetchReleaseFeed(cancellationToken);
+
+    const latestReleaseData = await this.findLatestRelease(feed, cancellationToken);
+
+    if (latestReleaseData.tag === null) {
+      throw newError('No published versions on GitHub', ErrorCodes.NoPublishedVersions);
+    }
+
+    // 2. Fetch the channel file.
+    let rawData: string;
+    let channelFile = '';
+    let channelFileUrl: any = '';
+    const fetchData = async (channelName: string) => {
+      channelFile = getChannelFilename(channelName);
+      channelFileUrl = newUrlFromBase(this.getBaseDownloadPath(latestReleaseData.tag, channelFile), this.baseUrl);
+      const requestOptions = this.createRequestOptions(channelFileUrl);
+      try {
+        return await this.executor.request(requestOptions, cancellationToken);
+      } catch (e) {
+        throw newError(
+          `Cannot find ${channelFile} in the latest release artifacts (${channelFileUrl}): ${e.stack || e.message}`,
+          ErrorCodes.ChannelFileNotFound,
+        );
+      }
+    };
+
+    try {
+      const channel = this.updater.allowPrerelease
+        ? this.getCustomChannelName(String(semver.prerelease(latestReleaseData.tag)?.[0]) || 'latest')
+        : this.getDefaultChannelName();
+      rawData = await fetchData(channel);
+    } catch (e) {
+      if (this.updater.allowPrerelease) {
+        // Fallback to the default channel if the custom channel doesn't exist.
+        rawData = await fetchData(this.getDefaultChannelName());
+      } else {
+        throw e;
+      }
+    }
+
+    // 3. Parse the channel file.
+    const result = parseUpdateInfo(rawData, channelFile, channelFileUrl);
+    if (result.releaseName === null) {
+      result.releaseName = latestReleaseData.feedElement.elementValueOrEmpty('title');
+    }
+
+    if (result.releaseNotes === null) {
+      result.releaseNotes = computeReleaseNotes(
+        this.updater.currentVersion,
+        this.updater.fullChangelog,
+        feed,
+        latestReleaseData.feedElement,
+      );
+    }
+
+    return {
+      tag: latestReleaseData.tag,
+      ...result,
+    };
+  }
+
+  async fetchReleaseFeed(cancellationToken: CancellationToken): Promise<XElement> {
+    const feedXml = await this.httpRequest(
+      newUrlFromBase(`${this.baseUrl}.atom`, this.baseUrl),
+      { accept: 'application/xml, application/atom+xml, text/xml, */*' },
+      cancellationToken,
+    );
+    return parseXml(feedXml);
+  }
+
+  private async findLatestRelease(feed: XElement, cancellationToken: CancellationToken): Promise<GithubReleaseData> {
+    let latestRelease: XElement | null = feed.element('entry', false, 'No published versions on GitHub');
+    let tag: string | null = null;
+
+    try {
+      if (this.updater.allowPrerelease) {
+        [tag, latestRelease] = this.findLatestPreRelease(tag, latestRelease, feed);
+      } else {
+        tag = await this.getLatestTagName(cancellationToken);
+        for (const element of feed.getElements('entry')) {
+          if (HREF_REGEXP.exec(element.element('link').attribute('href'))![1] === tag) {
+            latestRelease = element;
+            break;
+          }
+        }
+      }
+    } catch (e) {
+      throw newError(`Cannot parse releases feed: ${e.stack || e.message},\nXML:\n${feed}`, ErrorCodes.InvalidReleaseFeed);
+    }
+
+    return { tag, feedElement: latestRelease };
+  }
+
+  private findLatestPreRelease(tag: string, latestRelease: XElement, feed: XElement): [string, XElement] {
+    const currentChannel = this.updater?.channel || (semver.prerelease(this.updater.currentVersion)?.[0] as string) || null;
+
+    // If we don't have a channel, we use the first entry in the feed.
+    if (currentChannel === null) {
+      tag = HREF_REGEXP.exec(latestRelease.element('link').attribute('href'))![1];
+    } else {
+      for (const element of feed.getElements('entry')) {
+        const hrefElement = HREF_REGEXP.exec(element.element('link').attribute('href'));
+
+        // Skip entry without link.href
+        if (hrefElement === null) {
+          continue;
+        }
+
+        // Current entry GitHub tag.
+        const hrefTag = hrefElement[1];
+
+        const hrefChannel = semver.prerelease(hrefTag)?.[0] || null;
+
+        const shouldFetchVersion = !currentChannel || ['alpha', 'beta'].includes(currentChannel);
+        const isCustomChannel = hrefChannel !== null && !['alpha', 'beta'].includes(String(hrefChannel));
+
+        const channelMismatch = currentChannel === 'beta' && hrefChannel === 'alpha';
+
+        if (shouldFetchVersion && !isCustomChannel && !channelMismatch) {
+          latestRelease = element;
+          tag = hrefTag;
+          break;
+        }
+
+        const isNextPreRelease = hrefChannel && hrefChannel === currentChannel;
+        if (isNextPreRelease) {
+          latestRelease = element;
+          tag = hrefTag;
+          break;
+        }
+      }
+    }
+    return [tag, latestRelease];
+  }
+
+  private async getLatestTagName(cancellationToken: CancellationToken): Promise<string | null> {
+    const options = this.options;
+
+    const url =
+      options.host === null || options.host === 'github.com'
+        ? newUrlFromBase(`${this.basePath}/latest`, this.baseUrl)
+        : new URL(`${this.computeGithubBasePath(`/repos/${options.owner}/${options.repo}/releases`)}/latest`, this.baseApiUrl);
+
+    try {
+      const rawData = await this.httpRequest(url, { accept: 'application/json' }, cancellationToken);
+      if (rawData == null) {
+        return null;
+      }
+
+      const releaseInfo: GithubReleaseInfo = JSON.parse(rawData);
+      return releaseInfo.tag_name;
+    } catch (e) {
+      throw newError(
+        `Unable to find latest version on GitHub (${url}), please ensure a production release exists: ${e.stack || e.message}`,
+        ErrorCodes.LatestVersionNotFound,
+      );
+    }
+  }
+
+  protected get basePath(): string {
+    return `/${this.options.owner}/${this.options.repo}/releases`;
+  }
+
+  protected getBaseDownloadPath(tag: string, fileName: string): string {
+    return `${this.basePath}/download/${tag}/${fileName}`;
   }
 }
 
