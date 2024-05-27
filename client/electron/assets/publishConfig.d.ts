0b4f1886af97f8db624665abaaefe9aba5a06654
Firelight Flagboy <firelight.flagboy@gmail.com>
2024-05-27 13:54:32 +0200

    Override method `getLatestVersion` to allow the use of the nightly channel
    Inspired by https://github.com/electron-userland/electron-builder/blob/77f977435c99247d5db395895618b150f5006e8f/packages/electron-updater/src/providers/GitHubProvider.ts

--- client/electron/assets/publishConfig.d.ts
+++ client/electron/assets/publishConfig.d.ts
@@ -1,11 +1,33 @@
 // Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 2016-present Scille SAS
 
 export interface CustomPublishOptions {
-  provider: 'custom';
-  /** The name of the repository */
-  repo: string;
-  /** The owner of the repository */
-  owner: string;
+  readonly provider: 'custom';
   /** The machine arch the electron-builder is running on */
-  buildMachineArch: string;
+  readonly buildMachineArch: string;
+  /**
+   * The repository name.
+   */
+  readonly repo: string;
+  /**
+   * The owner.
+   */
+  readonly owner: string;
+  /**
+   * The host (including the port if need).
+   * @default github.com
+   */
+  readonly host?: string | null;
+  /**
+   * The channel.
+   * @default latest
+   */
+  readonly channel?: string | null;
+  /**
+   * The type of release. By default `draft` release will be created.
+   *
+   * Also you can set release type using environment variable.
+   * If `EP_DRAFT`is set to `true` — `draft`, if `EP_PRE_RELEASE`is set to `true` — `prerelease`.
+   * @default draft
+   */
+  releaseType?: 'draft' | 'prerelease' | 'release' | null;
 }
