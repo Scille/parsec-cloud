5c43df198448bead8205e6049a6df607a1118cdb
Firelight Flagboy <firelight.flagboy@gmail.com>
2024-05-28 16:29:11 +0200

    Add mountpoint for macos

--- libparsec/crates/platform_mountpoint/src/lib.rs
+++ libparsec/crates/platform_mountpoint/src/lib.rs
@@ -1,54 +1,22 @@
 // Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 2016-present Scille SAS
 
-#[cfg(target_os = "linux")]
+#[cfg(target_family = "unix")]
 mod unix;
 #[cfg(target_os = "windows")]
 mod windows;
 
-#[cfg(target_os = "linux")]
+#[cfg(target_family = "unix")]
 pub(crate) use unix as platform;
 #[cfg(target_os = "windows")]
 pub(crate) use windows as platform;
 
-// TODO: UNIX implementation is linux-only for the moment
-#[cfg(target_os = "macos")]
-pub(crate) mod platform {
-    use std::sync::Arc;
-
-    use libparsec_client::WorkspaceOps;
-    use libparsec_types::prelude::*;
-
-    #[derive(Debug)]
-    pub struct Mountpoint {}
-
-    impl Mountpoint {
-        pub fn path(&self) -> &std::path::Path {
-            todo!()
-        }
-
-        pub fn to_os_path(&self, _parsec_path: &FsPath) -> std::path::PathBuf {
-            todo!()
-        }
-
-        pub async fn mount(_ops: Arc<WorkspaceOps>) -> anyhow::Result<Self> {
-            todo!()
-        }
-
-        pub async fn unmount(self) -> anyhow::Result<()> {
-            todo!()
-        }
-    }
-}
-
-// TODO: implement for Windows & macOS !
-#[cfg(not(any(target_os = "windows", target_os = "macos")))]
+// TODO: implement for Windows !
+#[cfg(not(target_os = "windows"))]
 #[cfg(test)]
 pub(crate) use platform::LOOKUP_HOOK;
 
 pub use platform::Mountpoint;
 
-// TODO: implement for macOS !
-#[cfg(not(target_os = "macos"))]
 #[cfg(test)]
 #[path = "../tests/unit/operations/mod.rs"]
 #[allow(clippy::unwrap_used)]
