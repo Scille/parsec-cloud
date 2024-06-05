a40a4fecbf78cf6cd8028df02aa308abc88c3dbe
Maxime GRANDCOLAS <maxime.grandcolas@scille.fr>
2024-06-05 12:43:07 +0200

    [MS] Keep same order of argument of workspace menu click event

--- client/src/views/workspaces/WorkspacesPage.vue
+++ client/src/views/workspaces/WorkspacesPage.vue
@@ -498,7 +498,7 @@ async function onWorkspaceShareClick(workspace: WorkspaceInfo): Promise<void> {
   await refreshWorkspacesList();
 }
 
-async function onOpenWorkspaceContextMenu(event: Event, workspace: WorkspaceInfo, onFinished?: () => void): Promise<void> {
+async function onOpenWorkspaceContextMenu(workspace: WorkspaceInfo, event: Event, onFinished?: () => void): Promise<void> {
   await openWorkspaceContextMenu(event, workspace, favorites.value, eventDistributor, informationManager, storageManager);
   await refreshWorkspacesList();
   if (onFinished) {
