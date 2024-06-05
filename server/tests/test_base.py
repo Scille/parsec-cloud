41606d1e996867217a145655908033b517e4e6fb
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2024-06-05 11:44:35 +0200

    Introduce `use-zstd-in-serialization-format` feature in liparsec to disable zstd compilation on wasm
    This is a temporary feature to disable the use of zstd, as its compilation with WASM is buggy on some host (e.g.
    MacOS/Windows/Ubuntu 20.04).
    Of course this should not be used in production given this totally breaks compatibility with other clients !

--- server/tests/test_base.py
+++ server/tests/test_base.py
@@ -2,6 +2,10 @@
 
 import httpx
 
+from parsec._parsec import DateTime, FileManifest, VlobID
+
+from .common import MinimalorgRpcClients
+
 
 async def test_main_page(client: httpx.AsyncClient) -> None:
     response = await client.get("http://parsec.invalid/")
@@ -25,3 +29,33 @@ async def test_static_404(client: httpx.AsyncClient) -> None:
     response = await client.get("http://parsec.invalid/static/unknown")
     assert response.status_code == 404
     assert response.json() == {"detail": "Not Found"}
+
+
+# Currently only format version v0 is supported in production, however
+# we also have a vFF format to circumvent issues with Zstd WASM compilation
+# issues on macOS/Windows.
+# Hence this test that ensures v0 format is still the main format used when
+# doing serialization !
+def test_serialization_uses_v0_format(minimalorg: MinimalorgRpcClients):
+    now = DateTime.now()
+    data = FileManifest(
+        author=minimalorg.alice.device_id,
+        timestamp=now,
+        id=VlobID.new(),
+        parent=VlobID.new(),
+        version=1,
+        created=now,
+        updated=now,
+        blocksize=512,
+        size=0,
+        blocks=(),
+    )
+    signed = data.dump_and_sign(minimalorg.alice.signing_key)
+    # In this test we don't care about the signature, so remove it...
+    serialized = minimalorg.alice.signing_key.verify_key.verify(signed)
+    # ...what we care about is the serialization format version
+    assert serialized[0] == 0x00
+    # Format v0 uses Zstd compression, so we should find its magic header number
+    # Magic number is 4 Bytes, little-endian format. Value : 0xFD2FB528
+    # see https://github.com/facebook/zstd/blob/dev/doc/zstd_compression_format.md#zstandard-frames
+    assert serialized[1:5] == b"\x28\xb5\x2f\xfd"
