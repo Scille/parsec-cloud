1a693dfe41fc4630f217a453c3ad28fb911b28c6
Firelight Flagboy <firelight.flagboy@gmail.com>
2024-06-05 10:35:38 +0200

    Harden cache entry with runner version
    Add runner version to:
    
    - Rust cache entry
    - libparsec cache entry
    
    Increase timeout for check agnostic codebase
    
    Co-authored-by: Marcos Medrano <786907+mmmarcos@users.noreply.github.com>

---
+++ .github/actions/system-info/system-info.py
@@ -0,0 +1,33 @@
+# Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 2016-present Scille SAS
+
+import platform
+import sys
+
+print(f"{sys.platform=}", file=sys.stderr)
+
+if sys.platform == "linux":
+    platform_info = platform.freedesktop_os_release()
+    # VERSION_ID is not available in all platforms
+    release = platform_info.get("VERSION_ID", "unknown")
+    os = platform_info["ID"]
+elif sys.platform == "darwin":
+    platform_info = platform.mac_ver()
+    release = platform_info[0]
+    os = "macos"
+elif sys.platform == "win32":
+    platform_info = platform.win32_ver()
+    release = platform_info[0]
+    os = "windows"
+else:
+    raise ValueError(f"Unsupported platform {sys.platform}")
+
+print(f"{platform_info=}", file=sys.stderr)
+print(f"{os=} {release=}", file=sys.stderr)
+
+id = f"{os}-{release}"
+print(f"{id=}", file=sys.stderr)
+
+print(f"platform={sys.platform}")
+print(f"{id=}")
+print(f"{os=}")
+print(f"{release=}")
