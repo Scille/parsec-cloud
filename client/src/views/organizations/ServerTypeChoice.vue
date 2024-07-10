e20f68ca75e4d4f93ccdf370efcda6f072a0415d
fabienSvstr <fabien.sevestre@my-digital-school.org>
2024-07-10 14:14:57 +0200

    Enhanced choose server modal

--- client/src/views/organizations/ServerTypeChoice.vue
+++ client/src/views/organizations/ServerTypeChoice.vue
@@ -1,40 +1,78 @@
 <!-- Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 2016-present Scille SAS -->
 
 <template>
-  <ion-page>
+  <ion-page class="server-modal">
     <create-organization-modal-header
       @close-clicked="$emit('closeRequested')"
-      title="CHOOSE WISELY"
+      title="CreateOrganization.title.create"
+      subtitle="CreateOrganization.subtitle.server"
     />
-    <div>
+    <div class="server-choice">
+      <!-- Saas -->
       <div
-        class="server-type-container"
+        class="server-choice-item"
         @click="serverChoice = ServerType.Saas"
         :class="{ selected: serverChoice === ServerType.Saas }"
       >
-        SAAS
+        <!-- <ms-image
+          :image="ParsecMockup"
+          class="server-choice-item__image"
+        /> -->
+        <!-- should be replaced with megashark-lib -->
+        <div class="server-choice-item__image">
+          <img
+            src="@/assets/images/mockup.svg"
+            alt="trial"
+          />
+        </div>
+        <ion-text class="server-choice-item__label subtitles-normal">
+          {{ $msTranslate('CreateOrganization.server.saas') }}
+        </ion-text>
       </div>
+
+      <!-- Trial -->
       <div
-        class="server-type-container"
+        class="server-choice-item"
         @click="serverChoice = ServerType.Trial"
         :class="{ selected: serverChoice === ServerType.Trial }"
       >
-        TRIAL
+        <!-- <ms-image
+          :image="Trial"
+          class="server-choice-item__image"
+        /> -->
+        <!-- should be replaced with megashark-lib -->
+        <div class="server-choice-item__image">
+          <img
+            src="@/assets/images/trial.svg"
+            alt="trial"
+          />
+        </div>
+        <ion-text class="server-choice-item__label subtitles-normal">
+          {{ $msTranslate('CreateOrganization.server.trial.title') }}
+          <span class="body">{{ $msTranslate('CreateOrganization.server.trial.description') }}</span>
+        </ion-text>
       </div>
     </div>
 
-    <ion-button @click="$emit('serverChosen', ServerType.Custom)"> CUSTOM SERVER </ion-button>
-    <ion-button
-      @click="onChoiceMade"
-      :disabled="serverChoice === undefined"
-    >
-      CONTINUE
-    </ion-button>
+    <ion-footer class="server-modal-footer">
+      <ion-button
+        @click="$emit('serverChosen', ServerType.Custom)"
+        fill="clear"
+      >
+        {{ $msTranslate('CreateOrganization.server.ownServer') }}
+      </ion-button>
+      <ion-button
+        @click="onChoiceMade"
+        :disabled="serverChoice === undefined"
+      >
+        {{ $msTranslate('CreateOrganization.button.continue') }}
+      </ion-button>
+    </ion-footer>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonPage, IonButton } from '@ionic/vue';
+import { IonPage, IonButton, IonText, IonFooter } from '@ionic/vue';
 import { ref } from 'vue';
 import { ServerType } from '@/services/parsecServers';
 import CreateOrganizationModalHeader from '@/components/organizations/CreateOrganizationModalHeader.vue';
@@ -55,12 +93,79 @@ async function onChoiceMade(): Promise<void> {
 </script>
 
 <style scoped lang="scss">
-.server-type-container {
-  width: 12em;
-  height: 12em;
-  border: 1px solid red;
+.server-modal {
+  padding: 2.5rem;
+  display: flex;
+  height: auto;
+  width: 100%;
+
+  &-footer {
+    display: flex;
+    justify-content: space-between;
+    margin-top: 2.5rem;
+  }
 }
-.selected {
-  background-color: pink;
+
+.server-choice {
+  display: flex;
+  gap: 4rem;
+  justify-content: center;
+
+  &-item {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    cursor: pointer;
+    flex: 1;
+
+    &:first-child {
+      align-items: end;
+    }
+
+    &__image {
+      width: 12em;
+      height: 12em;
+      background-color: var(--parsec-color-light-secondary-premiere);
+      border-radius: var(--parsec-radius-12);
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+
+    &__label {
+      text-align: center;
+      display: flex;
+      flex-direction: column;
+      color: var(--parsec-color-light-secondary-text);
+      max-width: 12rem;
+      line-height: 150%; /* 150% */
+      gap: 0.25rem;
+
+      span {
+        color: var(--parsec-color-light-secondary-hard-grey);
+      }
+    }
+
+    &:hover {
+      .server-choice-item__image {
+        outline: 3px solid var(--parsec-color-light-primary-100);
+        color: var(--parsec-color-light-secondary-grey);
+      }
+    }
+
+    &.selected {
+      .server-choice-item__image {
+        outline: 3px solid var(--parsec-color-light-primary-300);
+        background-color: var(--parsec-color-light-primary-30);
+      }
+      .server-choice-item__label {
+        color: var(--parsec-color-light-primary-600);
+
+        span {
+          color: var(--parsec-color-light-danger-700);
+        }
+      }
+    }
+  }
 }
 </style>
