f7c1d0f7f56cd2a793028b84b7217b1d0cac1009
Vincent Michel <vxgmichel@gmail.com>
2024-07-10 15:57:01 +0200

    Rename Chunk to ChunkView in the testbed crc hash

--- libparsec/crates/testbed/src/template/crc_hash.rs
+++ libparsec/crates/testbed/src/template/crc_hash.rs
@@ -468,8 +468,7 @@ impl CrcHash for BlockAccess {
 }
 impl CrcHash for ChunkView {
     fn crc_hash(&self, hasher: &mut crc32fast::Hasher) {
-        // TODO: change to `ChunkView``
-        hasher.update(b"Chunk");
+        hasher.update(b"ChunkView");
 
         let ChunkView {
             id,
