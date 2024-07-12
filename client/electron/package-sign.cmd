20730d98d88ea28aabe49fd4cfc84d74d6fd73b2
Vincent Michel <vxgmichel@gmail.com>
2024-07-12 16:43:17 +0200

    Fix spelling

--- client/electron/package-sign.cmd
+++ client/electron/package-sign.cmd
@@ -1,5 +1,5 @@
 :: This script is used to create a signed release of Parsec
-:: using a single command from the pre-built windows aretfact.
+:: using a single command from the pre-built windows artefact.
 
 :: This is meant to be run in the `client/electron` directory.
 powershell.exe -executionpolicy bypass -file scripts\package-sign.ps1
