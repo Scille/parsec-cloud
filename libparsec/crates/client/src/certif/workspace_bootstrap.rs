202dfbbcb87230a50a6be87fb7f4550e223b6bba
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2024-07-18 08:33:13 +0200

    Fix TooMuchDriftWithServerClock event not being sent on event bus in `CertificatesOps::bootstrap_workspace`

--- libparsec/crates/client/src/certif/workspace_bootstrap.rs
+++ libparsec/crates/client/src/certif/workspace_bootstrap.rs
@@ -3,10 +3,13 @@
 use libparsec_client_connection::ConnectionError;
 use libparsec_types::prelude::*;
 
+use crate::EventTooMuchDriftWithServerClock;
+
 use super::{
-    store::CertifStoreError, CertifEnsureRealmCreatedError, CertifPollServerError,
-    CertifRenameRealmError, CertifRotateRealmKeyError, CertificateBasedActionOutcome,
-    CertificateOps, InvalidCertificateError, InvalidKeysBundleError,
+    store::{CertifStoreError, RealmBootstrapState},
+    CertifEnsureRealmCreatedError, CertifPollServerError, CertifRenameRealmError,
+    CertifRotateRealmKeyError, CertificateBasedActionOutcome, CertificateOps,
+    InvalidCertificateError, InvalidKeysBundleError,
 };
 
 #[derive(Debug, thiserror::Error)]
@@ -66,67 +69,133 @@ pub(super) async fn bootstrap_workspace(
     // Given those operations are not done in an atomic way, they are all
     // idempotent and we retry them until they are all done.
 
-    // Cheap check: only OWNER can do the bootstrap
-    let current_role = ops.get_current_self_realm_role(realm_id).await?;
-    match current_role {
-        // `None` can mean:
-        // - We are the original OWNER, and the realm hasn't been created on the server yet
-        //   (hence there is not realm role certificate so far).
-        // - We never had access to the realm (unlikely: why do we know about it then ?).
-        // - The local certificates database got cleared (or we are upgrading from Parsec <v3
-        //   where certificates were only kept in RAM).
-        // So we have to give it the benefit of the doubt here and rely on server access
-        // control instead.
-        Some(Some(RealmRole::Owner)) | None => (),
-        _ => return Err(CertifBootstrapWorkspaceError::AuthorNotAllowed),
+    // First retrieve if we can do the bootstrap and what is still to be done (if any !)
+
+    let mut bootstrap_state = ops
+        .store
+        .for_read(|store| async move {
+            // Only OWNER can do the bootstrap
+
+            let current_role = store.get_self_user_realm_role(realm_id).await?;
+            match current_role {
+                // `None` can mean:
+                // - We are the original OWNER, and the realm hasn't been created on the server yet
+                //   (hence there is not realm role certificate so far).
+                // - We never had access to the realm (unlikely: why do we know about it then ?).
+                // - The local certificates database got cleared (e.g. due to switch to/from OUTSIDER profile).
+                // So we have to give it the benefit of the doubt here and rely on server access
+                // control instead.
+                Some(Some(RealmRole::Owner)) | None => (),
+                _ => return Err(CertifBootstrapWorkspaceError::AuthorNotAllowed),
+            }
+
+            // Actual retrieve of the state
+
+            let bootstrap_state = store.get_realm_bootstrap_state(realm_id).await?;
+
+            Ok(bootstrap_state)
+        })
+        .await??;
+
+    if matches!(bootstrap_state, RealmBootstrapState::WorkspaceBootstrapped) {
+        return Ok(CertificateBasedActionOutcome::LocalIdempotent);
     }
 
     // Step 1
 
-    super::realm_create::ensure_realm_created(ops, realm_id)
-        .await
-        .map_err(|e| match e {
-            CertifEnsureRealmCreatedError::Offline => CertifBootstrapWorkspaceError::Offline,
-            CertifEnsureRealmCreatedError::Stopped => CertifBootstrapWorkspaceError::Stopped,
-            CertifEnsureRealmCreatedError::TimestampOutOfBallpark {
-                server_timestamp,
-                client_timestamp,
-                ballpark_client_early_offset,
-                ballpark_client_late_offset,
-            } => CertifBootstrapWorkspaceError::TimestampOutOfBallpark {
-                server_timestamp,
-                client_timestamp,
-                ballpark_client_early_offset,
-                ballpark_client_late_offset,
+    if matches!(bootstrap_state, RealmBootstrapState::LocalOnly) {
+        let outcome = super::realm_create::ensure_realm_created(ops, realm_id).await;
+        match outcome {
+            Ok(_) => (),
+            Err(err) => match err {
+                CertifEnsureRealmCreatedError::Offline => {
+                    return Err(CertifBootstrapWorkspaceError::Offline)
+                }
+                CertifEnsureRealmCreatedError::Stopped => {
+                    return Err(CertifBootstrapWorkspaceError::Stopped)
+                }
+                CertifEnsureRealmCreatedError::TimestampOutOfBallpark {
+                    server_timestamp,
+                    client_timestamp,
+                    ballpark_client_early_offset,
+                    ballpark_client_late_offset,
+                } => {
+                    let event = EventTooMuchDriftWithServerClock {
+                        server_timestamp,
+                        ballpark_client_early_offset,
+                        ballpark_client_late_offset,
+                        client_timestamp,
+                    };
+                    ops.event_bus.send(&event);
+
+                    return Err(CertifBootstrapWorkspaceError::TimestampOutOfBallpark {
+                        server_timestamp,
+                        client_timestamp,
+                        ballpark_client_early_offset,
+                        ballpark_client_late_offset,
+                    });
+                }
+                CertifEnsureRealmCreatedError::Internal(err) => {
+                    return Err(err.context("Cannot do server-side realm creation").into())
+                }
             },
-            CertifEnsureRealmCreatedError::Internal(err) => {
-                err.context("Cannot do server-side realm creation").into()
-            }
-        })?;
+        }
+
+        bootstrap_state = RealmBootstrapState::CreatedInServer;
+    }
 
     // Step 2
 
-    super::realm_key_rotation::ensure_realm_initial_key_rotation(ops, realm_id).await.map_err(|e| match e {
-        CertifRotateRealmKeyError::Offline => CertifBootstrapWorkspaceError::Offline,
-        CertifRotateRealmKeyError::Stopped => CertifBootstrapWorkspaceError::Stopped,
-        CertifRotateRealmKeyError::AuthorNotAllowed => CertifBootstrapWorkspaceError::AuthorNotAllowed,
-        CertifRotateRealmKeyError::TimestampOutOfBallpark {
-            server_timestamp, client_timestamp, ballpark_client_early_offset, ballpark_client_late_offset
-        } => CertifBootstrapWorkspaceError::TimestampOutOfBallpark {
-            server_timestamp,
-            client_timestamp,
-            ballpark_client_early_offset,
-            ballpark_client_late_offset,
-        },
-        CertifRotateRealmKeyError::InvalidCertificate(err) => CertifBootstrapWorkspaceError::InvalidCertificate(err),
-        CertifRotateRealmKeyError::Internal(err) => err.context("Cannot do initial server-side key rotation").into(),
-        // Unexpected given we have just made sure the realm was created
-        CertifRotateRealmKeyError::UnknownRealm => anyhow::anyhow!(
-            "Cannot do initial server-side key rotation: server doesn't know the realm while we have just made sure it exists !"
-        ).into(),
-    })?;
+    if matches!(bootstrap_state, RealmBootstrapState::CreatedInServer) {
+        // Do the initial key rotation, i.e. key index == 1
+        let outcome =
+            super::realm_key_rotation::rotate_realm_key_idempotent(ops, realm_id, 1).await;
+        match outcome {
+            Ok(_) => (),
+            Err(err) => match err {
+                CertifRotateRealmKeyError::Offline => return Err(CertifBootstrapWorkspaceError::Offline),
+                CertifRotateRealmKeyError::Stopped => return Err(CertifBootstrapWorkspaceError::Stopped),
+                CertifRotateRealmKeyError::AuthorNotAllowed => return Err(CertifBootstrapWorkspaceError::AuthorNotAllowed),
+                CertifRotateRealmKeyError::TimestampOutOfBallpark {
+                    server_timestamp, client_timestamp, ballpark_client_early_offset, ballpark_client_late_offset
+                } => {
+                    let event = EventTooMuchDriftWithServerClock {
+                        server_timestamp,
+                        ballpark_client_early_offset,
+                        ballpark_client_late_offset,
+                        client_timestamp,
+                    };
+                    ops.event_bus.send(&event);
+
+                    return Err(CertifBootstrapWorkspaceError::TimestampOutOfBallpark {
+                        server_timestamp,
+                        client_timestamp,
+                        ballpark_client_early_offset,
+                        ballpark_client_late_offset,
+                    });
+                }
+                CertifRotateRealmKeyError::InvalidCertificate(err) => return Err(CertifBootstrapWorkspaceError::InvalidCertificate(err)),
+                CertifRotateRealmKeyError::Internal(err) => return Err(err.context("Cannot do initial server-side key rotation").into()),
+                // Unexpected given we have just made sure the realm was created
+                CertifRotateRealmKeyError::UnknownRealm => return Err(anyhow::anyhow!(
+                    "Cannot do initial server-side key rotation: server doesn't know the realm while we have just made sure it exists !"
+                ).into()),
+                // Unexpected given we have just checked no previous keys bundle exists.
+                // The fact this new keys bundle is invalid is none of our concern: we
+                // only consider it as proof a key rotation certificate has been concurrently
+                // uploaded and hence this step 2 has been done ¯\_(ツ)_/¯
+                CertifRotateRealmKeyError::CurrentKeysBundleCorrupted(_) => (),
+            }
+        }
+
+        bootstrap_state = RealmBootstrapState::InitialKeyRotationDone;
+    }
 
     // Step 3
+
+    // Sanity check: given this is the last step, if the realm doesn't need to do it...
+    // then it means it was bootstrapped in the first place !
+    assert_eq!(bootstrap_state, RealmBootstrapState::InitialKeyRotationDone);
 
     loop {
         let outcome =
@@ -163,12 +232,22 @@ pub(super) async fn bootstrap_workspace(
                 CertifRenameRealmError::AuthorNotAllowed => Err(CertifBootstrapWorkspaceError::AuthorNotAllowed),
                 CertifRenameRealmError::TimestampOutOfBallpark {
                     server_timestamp, client_timestamp, ballpark_client_early_offset, ballpark_client_late_offset
-                } => Err(CertifBootstrapWorkspaceError::TimestampOutOfBallpark {
-                    server_timestamp,
-                    client_timestamp,
-                    ballpark_client_early_offset,
-                    ballpark_client_late_offset,
-                }),
+                } => {
+                    let event = EventTooMuchDriftWithServerClock {
+                        server_timestamp,
+                        ballpark_client_early_offset,
+                        ballpark_client_late_offset,
+                        client_timestamp,
+                    };
+                    ops.event_bus.send(&event);
+
+                    Err(CertifBootstrapWorkspaceError::TimestampOutOfBallpark {
+                        server_timestamp,
+                        client_timestamp,
+                        ballpark_client_early_offset,
+                        ballpark_client_late_offset,
+                    })
+                }
                 // It is unlikely the keys bundle is invalid given we've most likely just
                 // uploaded it. Anyway, we don't try to play smart here and just propagate
                 // the error: a dedicated monitor should detect the issue, heal the keys
