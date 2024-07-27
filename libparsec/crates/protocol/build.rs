596357b24c08079e9eac3a013597e6dea3114919
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2024-07-27 09:12:27 +0200

    Document build.rs's rerun-if-changed on json5 schemas in libparsec_types & libparsec_protocol

--- libparsec/crates/protocol/build.rs
+++ libparsec/crates/protocol/build.rs
@@ -3,6 +3,9 @@
 use glob::glob;
 
 fn main() {
+    // Register .json5 schema files to trigger recompilation on change
+    // Note there is catch though: adding a new .json5 schema file will not
+    // trigger a recompilation !
     for entry in glob("schema/**/*.json5").expect("Failed to parse glob pattern") {
         let path = entry.expect("Glob iteration error");
         println!("cargo:rerun-if-changed={}", path.display())
