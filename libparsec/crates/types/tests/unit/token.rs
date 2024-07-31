6af9364286d97a03998505935d7f7d399b911477
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2024-07-31 11:46:21 +0200

    Add minor tests in libparsec_types for tokens

--- libparsec/crates/types/tests/unit/token.rs
+++ libparsec/crates/types/tests/unit/token.rs
@@ -1,5 +1,7 @@
 // Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 2016-present Scille SAS
 
+use libparsec_tests_lite::prelude::*;
+
 use super::*;
 
 macro_rules! base_token_tests {
@@ -8,21 +10,21 @@ macro_rules! base_token_tests {
             use super::*;
 
             #[test]
-            fn test_default_random() {
+            fn default_random() {
                 let token = $name::default();
-                assert_eq!(token.as_bytes().len(), TOKEN_SIZE);
+                p_assert_eq!(token.as_bytes().len(), TOKEN_SIZE);
             }
 
             #[test]
-            fn test_eq() {
+            fn eq() {
                 let token = $name::from_hex("000102030405060708090a0b0c0d0e0f").unwrap();
                 let token2 = $name::from_hex("000102030405060708090a0b0c0d0e0f").unwrap();
 
-                assert_eq!(token, token2);
+                p_assert_eq!(token, token2);
             }
 
             #[test]
-            fn test_ne() {
+            fn ne() {
                 let token = $name::from_hex("000102030405060708090a0b0c0d0e0f").unwrap();
                 let token2 = $name::from_hex("000102030405060708090a0b0c0d0e0e").unwrap();
 
@@ -30,14 +32,65 @@ macro_rules! base_token_tests {
             }
 
             #[test]
-            fn test_hex() {
+            fn hex() {
                 let token = $name::default();
 
                 let hex = token.hex();
 
                 let from_hex = $name::from_hex(&hex).unwrap();
 
-                assert_eq!(token, from_hex);
+                p_assert_eq!(token, from_hex);
+
+                p_assert_matches!($name::from_hex("#~!"), Err(TokenDecodeError::InvalidHex),);
+                p_assert_matches!(
+                    $name::from_hex("00010203"),
+                    Err(TokenDecodeError::InvalidSize { .. }),
+                );
+            }
+
+            #[test]
+            fn debug() {
+                let token = $name::from_hex("000102030405060708090a0b0c0d0e0f").unwrap();
+
+                p_assert_eq!(
+                    format!("{:?}", token),
+                    concat!(stringify!($name), "(\"000102030405060708090a0b0c0d0e0f\")")
+                );
+            }
+
+            #[test]
+            fn display() {
+                let token = $name::from_hex("000102030405060708090a0b0c0d0e0f").unwrap();
+
+                p_assert_eq!(format!("{}", token), "000102030405060708090a0b0c0d0e0f");
+            }
+
+            #[test]
+            fn try_from_str() {
+                let token: $name = $name::try_from("000102030405060708090a0b0c0d0e0f").unwrap();
+                let expected = $name::from_hex("000102030405060708090a0b0c0d0e0f").unwrap();
+
+                p_assert_eq!(token, expected);
+
+                p_assert_matches!($name::try_from("#~!"), Err(TokenDecodeError::InvalidHex),);
+                p_assert_matches!(
+                    $name::try_from("00010203"),
+                    Err(TokenDecodeError::InvalidSize { .. }),
+                );
+            }
+
+            #[test]
+            fn parse() {
+                let token: $name = "000102030405060708090a0b0c0d0e0f".parse().unwrap();
+                let expected = $name::from_hex("000102030405060708090a0b0c0d0e0f").unwrap();
+
+                p_assert_eq!(token, expected);
+
+                p_assert_matches!("#~!".parse::<$name>(), Err(TokenDecodeError::InvalidHex),);
+                p_assert_matches!(
+                    "00010203".parse::<$name>(),
+                    Err(TokenDecodeError::InvalidSize { .. }),
+                );
             }
         }
     };
