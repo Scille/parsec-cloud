df8014e2997bd5f6cdacc13c7156a6d0346b766a
fabienSvstr <fabien.sevestre@my-digital-school.org>
2024-08-07 08:07:51 +0200

    [MS] Updated create account section

--- client/tests/pw/e2e/about_modal.spec.ts
+++ client/tests/pw/e2e/about_modal.spec.ts
@@ -4,7 +4,7 @@ import { expect } from '@tests/pw/helpers/assertions';
 import { msTest } from '@tests/pw/helpers/fixtures';
 
 msTest('Opens the about dialog', async ({ home }) => {
-  await home.locator('.topbar-left__version').click();
+  await home.locator('#trigger-version-button').click();
 
   await expect(home.locator('.ms-modal')).toBeVisible();
   await expect(home.locator('.ms-modal').locator('.ms-modal-header__title')).toContainText('About');
