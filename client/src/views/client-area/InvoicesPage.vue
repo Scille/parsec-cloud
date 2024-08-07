ba94320281d692a5d9dd53f5678ed73255392e2f
fabienSvstr <fabien.sevestre@my-digital-school.org>
2024-08-07 11:40:58 +0200

    [MS] Added skeleton text

--- client/src/views/client-area/InvoicesPage.vue
+++ client/src/views/client-area/InvoicesPage.vue
@@ -45,27 +45,97 @@
     </div>
 
     <!-- invoices -->
-    <div
-      class="invoices-container"
-      v-if="invoices.length"
-    >
-      <invoices-container
-        v-for="year in years"
-        v-show="selectedYear === undefined || selectedYear === year"
-        :key="year"
-        :invoices="getInvoicesByYear(year)"
-        :title="{ key: 'common.date.asIs', data: { date: year.toString() }}"
-        :month-filter="selectedMonth"
-      />
-    </div>
+    <template v-if="invoices !== undefined">
+      <div
+        class="invoices-container"
+        v-if="invoices.length > 0"
+      >
+        <invoices-container
+          v-for="year in years"
+          v-show="selectedYear === undefined || selectedYear === year"
+          :key="year"
+          :invoices="getInvoicesByYear(year)"
+          :title="{ key: 'common.date.asIs', data: { date: year.toString() }}"
+          :month-filter="selectedMonth"
+        />
+      </div>
+      <!-- skeleton invoices -->
+      <div
+        class="skeleton"
+        v-else
+      >
+        <!-- skeleton year -->
+        <ion-skeleton-text
+          :animated="true"
+          class="skeleton-year"
+        />
+
+        <!-- skeleton row header -->
+        <div
+          class="skeleton-header-list"
+        >
+          <ion-skeleton-text
+            :animated="true"
+            class="invoices-date"
+          />
+          <ion-skeleton-text
+            :animated="true"
+            class="invoices-number"
+          />
+          <ion-skeleton-text
+            :animated="true"
+            class="invoices-organization"
+          />
+          <ion-skeleton-text
+            :animated="true"
+            class="invoices-amount"
+          />
+          <ion-skeleton-text
+            :animated="true"
+            class="invoices-status"
+          />
+        </div>
+
+        <!-- skeleton row invoices -->
+        <div class="skeleton-content-list">
+          <div
+            class="skeleton-content-list-item"
+            v-for="index in 5"
+            :key="index"
+          >
+            <ion-skeleton-text
+              :animated="true"
+              class="invoices-date"
+            />
+            <ion-skeleton-text
+              :animated="true"
+              class="invoices-number"
+            />
+            <ion-skeleton-text
+              :animated="true"
+              class="invoices-organization"
+            />
+            <ion-skeleton-text
+              :animated="true"
+              class="invoices-amount"
+            />
+            <ion-skeleton-text
+              :animated="true"
+              class="invoices-status"
+            />
+          </div>
+        </div>
+      </div>
+    </template>
 
     <!-- no invoices -->
-    <ion-text
-      class="body-lg"
-      v-else
-    >
-      {{ $msTranslate('clientArea.invoices.noInvoice') }}
-    </ion-text>
+    <template v-else>
+      <ion-text
+        class="body-lg"
+      >
+        {{ $msTranslate('clientArea.invoices.noInvoice') }}
+      </ion-text>
+    </template>
   </div>
 </template>
 
@@ -194,6 +264,7 @@ function getInvoicesByYear(year: number) : Array<BmsInvoice> {
       color: var(--parsec-color-light-secondary-hard-grey);
       padding: 0.375rem 0.625rem;
       border-radius: var(--parsec-radius-6);
+      border: 1px solid var(--parsec-color-light-secondary-disabled);
       cursor: pointer;
 
       &__calendar {
@@ -206,10 +277,10 @@ function getInvoicesByYear(year: number) : Array<BmsInvoice> {
 
       &:hover {
         color: var(--parsec-color-light-secondary-text);
-        background: var(--parsec-color-light-primary-100);
+        background: var(--parsec-color-light-secondary-background);
 
         .invoices-header-filter-button__calendar {
-          color: var(--parsec-color-light-primary-700);
+          color: var(--parsec-color-light-secondary-soft-text);
         }
 
         .invoices-header-filter-button__chevron {
@@ -225,5 +296,78 @@ function getInvoicesByYear(year: number) : Array<BmsInvoice> {
   flex-direction: column;
   gap: 1rem;
   position: relative;
+}
+
+.skeleton {
+  display: flex;
+  flex-direction: column;
+  padding: 0.5rem;
+  margin-top: 0.5rem;
+  border-radius: var(--parsec-radius-12);
+  background: var(--parsec-color-light-secondary-premiere);
+  --max-width-date: 12rem;
+  --max-width-number: 12rem;
+  --max-width-organization: 20rem;
+  --max-width-amount: 10rem;
+  transition: padding 0.2s;
+
+  [class^='invoices-'] {
+    height: 1rem;
+    width: 100%;
+    border-radius: var(--parsec-radius-8);
+  }
+
+  .invoices-date {
+    max-width: calc(var(--max-width-date) - 1rem);
+  }
+
+  .invoices-number {
+    max-width: calc(var(--max-width-date) - 1rem);
+  }
+
+  .invoices-organization {
+    max-width: calc(var(--max-width-organization) - 1rem);
+  }
+
+  .invoices-amount {
+    max-width: calc(var(--max-width-amount) - 1rem);
+  }
+
+  ion-skeleton-text {
+    border-radius: var(--parsec-radius-8);
+  }
+
+  &-year {
+    margin: 0.5rem 1rem;
+    height: 1.5rem;
+    width: 5rem;
+  }
+
+  &-header-list {
+    display: flex;
+    gap: 0.5rem;
+    padding: 0.5rem 1rem;
+    background: var(--parsec-color-light-secondary-premiere);
+    border-radius: var(--parsec-radius-8);
+    position: relative;
+    align-items: center;
+  }
+
+  &-content-list {
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+
+    &-item {
+      background: var(--parsec-color-light-secondary-white);
+      display: flex;
+      justify-content: space-between;
+    gap: 0.5rem;
+      border-radius: var(--parsec-radius-8);
+      position: relative;
+      align-items: center;
+      padding: 0.5rem 1rem;
+    }
+  }
 }
 </style>
