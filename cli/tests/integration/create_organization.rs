c6c9512ef144ed67ebc62f59fd8bcd6a3e3f32e9
Firelight Flagboy <firelight.flagboy@gmail.com>
2024-08-09 10:05:09 +0200

    Add macro to reduce the boilerplate to run CLI commands in tests

--- cli/tests/integration/create_organization.rs
+++ cli/tests/integration/create_organization.rs
@@ -1,5 +1,3 @@
-use assert_cmd::Command;
-
 use libparsec::{tmp_path, TmpPath};
 
 use super::{bootstrap_cli_test, unique_org_id};
@@ -10,17 +8,14 @@ use crate::testenv_utils::{DEFAULT_ADMINISTRATION_TOKEN, TESTBED_SERVER_URL};
 async fn create_organization(tmp_path: TmpPath) {
     bootstrap_cli_test(&tmp_path).await.unwrap();
 
-    Command::cargo_bin("parsec_cli")
-        .unwrap()
-        .args([
-            "create-organization",
-            "--organization-id",
-            unique_org_id().as_ref(),
-            "--addr",
-            &std::env::var(TESTBED_SERVER_URL).unwrap(),
-            "--token",
-            DEFAULT_ADMINISTRATION_TOKEN,
-        ])
-        .assert()
-        .stdout(predicates::str::contains("Organization bootstrap url:"));
+    crate::assert_cmd_success!(
+        "create-organization",
+        "--organization-id",
+        unique_org_id().as_ref(),
+        "--addr",
+        &std::env::var(TESTBED_SERVER_URL).unwrap(),
+        "--token",
+        DEFAULT_ADMINISTRATION_TOKEN
+    )
+    .stdout(predicates::str::contains("Organization bootstrap url:"));
 }
