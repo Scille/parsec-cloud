1602f4253e85533be02721c95a4aacea92cd1b02
Firelight Flagboy <firelight.flagboy@gmail.com>
2024-08-09 10:50:57 +0200

    Resolve TODO in CLI tests

--- cli/tests/integration/stats_server.rs
+++ cli/tests/integration/stats_server.rs
@@ -5,7 +5,6 @@ use crate::testenv_utils::DEFAULT_ADMINISTRATION_TOKEN;
 
 #[rstest::rstest]
 #[tokio::test]
-// TODO: Split me into different tests
 async fn stats_server(tmp_path: TmpPath) {
     let (url, _, _) = bootstrap_cli_test(&tmp_path).await.unwrap();
 
@@ -16,6 +15,12 @@ async fn stats_server(tmp_path: TmpPath) {
         "--token",
         DEFAULT_ADMINISTRATION_TOKEN
     );
+}
+
+#[rstest::rstest]
+#[tokio::test]
+async fn csv_format(tmp_path: TmpPath) {
+    let (url, _, _) = bootstrap_cli_test(&tmp_path).await.unwrap();
 
     crate::assert_cmd_success!(
         "stats-server",
@@ -27,6 +32,12 @@ async fn stats_server(tmp_path: TmpPath) {
         "csv"
     )
         .stdout(predicates::str::contains("organization_id,data_size,metadata_size,realms,active_users,admin_users_active,admin_users_revoked,standard_users_active,standard_users_revoked,outsider_users_active,outsider_users_revoked\r\n"));
+}
+
+#[rstest::rstest]
+#[tokio::test]
+async fn with_end_date(tmp_path: TmpPath) {
+    let (url, _, _) = bootstrap_cli_test(&tmp_path).await.unwrap();
 
     crate::assert_cmd_success!(
         "stats-server",
