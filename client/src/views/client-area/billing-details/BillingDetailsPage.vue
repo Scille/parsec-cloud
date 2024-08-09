1a6269f1c69931300cca758260f7049975ee92a3
fabienSvstr <fabien.sevestre@my-digital-school.org>
2024-08-09 07:02:17 +0200

    [MS] Updated style

--- client/src/views/client-area/billing-details/BillingDetailsPage.vue
+++ client/src/views/client-area/billing-details/BillingDetailsPage.vue
@@ -1,70 +1,81 @@
 <!-- Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 2016-present Scille SAS -->
 
 <template>
-  <div class="billing-header">
-    <ion-title class="billing-header__title title-h2">{{ $msTranslate('clientArea.billingDetailsPage.title') }}</ion-title>
-  </div>
-
-  <div class="form">
-    <div class="form-item-content">
-      <ms-input
-        class="form-item"
-        v-model="line1"
-        :maxlength="256"
-        label="clientArea.billingDetailsPage.placeholders.line1"
-        @on-enter-keyup="submit"
-      />
+  <div class="client-billing-page">
+    <div class="billing-header">
+      <ion-title class="billing-header__title title-h2">{{ $msTranslate('clientArea.billingDetailsPage.title') }}</ion-title>
     </div>
-    <div class="form-item-content">
-      <ms-input
-        class="form-item"
-        v-model="line2"
-        :maxlength="256"
-        label="clientArea.billingDetailsPage.placeholders.line2"
-        @on-enter-keyup="submit"
-      />
-    </div>
-    <div class="form-row">
-      <div class="form-item-content">
+    <div class="form-container">
+      <div class="form">
+        <div
+          class="form-item-content"
+          id="address"
+        >
+          <ms-input
+            class="form-item"
+            v-model="line1"
+            :maxlength="256"
+            label="clientArea.billingDetailsPage.placeholders.line1"
+            @on-enter-keyup="submit"
+          />
+          <ms-input
+            class="form-item"
+            v-model="line2"
+            :maxlength="256"
+            :placeholder="'clientArea.billingDetailsPage.placeholders.line2'"
+            @on-enter-keyup="submit"
+          />
+        </div>
+        <div class="form-row">
+          <div
+            class="form-item-content"
+            id="postalCode"
+          >
+            <ms-input
+              type="tel"
+              v-model="postalCode"
+              :maxlength="32"
+              label="clientArea.billingDetailsPage.placeholders.postalCode"
+              @on-enter-keyup="submit"
+            />
+          </div>
+          <div
+            class="form-item-content"
+            id="city"
+          >
+            <ms-input
+              v-model="city"
+              :maxlength="128"
+              label="clientArea.billingDetailsPage.placeholders.city"
+              @on-enter-keyup="submit"
+            />
+          </div>
+        </div>
         <ms-input
-          type="tel"
-          v-model="postalCode"
-          :maxlength="32"
-          label="clientArea.billingDetailsPage.placeholders.postalCode"
-          @on-enter-keyup="submit"
-        />
-      </div>
-      <div class="form-item-content">
-        <ms-input
-          v-model="city"
+          class="form-item"
+          v-model="country"
           :maxlength="128"
-          label="clientArea.billingDetailsPage.placeholders.city"
+          label="clientArea.billingDetailsPage.placeholders.country"
           @on-enter-keyup="submit"
         />
       </div>
-    </div>
-    <ms-input
-      class="form-item"
-      v-model="country"
-      :maxlength="128"
-      label="clientArea.billingDetailsPage.placeholders.country"
-      @on-enter-keyup="submit"
-    />
-  </div>
 
-  <div class="submit">
-    <ion-button
-      @click="submit"
-      class="submit-button"
-      :disabled="!isFormValid"
-    >
-      {{ $msTranslate('clientArea.billingDetailsPage.submit') }}
-    </ion-button>
+      <div class="form-submit">
+        <ion-button
+          @click="submit"
+          class="submit-button"
+          :disabled="!isFormValid"
+        >
+          {{ $msTranslate('clientArea.billingDetailsPage.submit') }}
+        </ion-button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { IonButton } from '@ionic/vue';
+import { IonButton, IonTitle, IonIcon } from '@ionic/vue';
+import { pencil } from 'ionicons/icons';
 import { BmsAccessInstance, BmsOrganization, DataType } from '@/services/bms';
 import { MsInput } from 'megashark-lib';
 import { onMounted, ref, computed, inject } from 'vue';
@@ -129,13 +140,53 @@ async function submit(): Promise<void> {
 </script>
 
 <style scoped lang="scss">
-.billing-header {
+.client-billing-page {
   display: flex;
   flex-direction: column;
-  gap: 1rem;
+  gap: 1.5rem;
+  max-width: 32rem;
+}
+
+.billing-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 1.5rem;
 
   &__title {
     color: var(--parsec-color-light-primary-700);
+  }
+}
+
+.form-container {
+  display: flex;
+  flex-direction: column;
+  gap: 1.5rem;
+  width: 100%;
+}
+
+.form {
+  display: flex;
+  flex-direction: column;
+  gap: 1.5rem;
+
+  &-row {
+    display: flex;
+    gap: 1em;
+  }
+
+  #address {
+    display: flex;
+    flex-direction: column;
+    gap: 1em;
+  }
+
+  #postalCode {
+    max-width: 10rem;
+  }
+
+  #city {
+    flex: 1;
   }
 }
 </style>
