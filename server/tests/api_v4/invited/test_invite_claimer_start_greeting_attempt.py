92dc1d3f0bfee88e0e27d3cb737a27448349d5f7
Vincent Michel <vxgmichel@gmail.com>
2024-08-17 18:29:45 +0200

    Add memory implementation of claimer_start_greeting_attempt

--- server/tests/api_v4/invited/test_invite_claimer_start_greeting_attempt.py
+++ server/tests/api_v4/invited/test_invite_claimer_start_greeting_attempt.py
@@ -1,14 +1,19 @@
 # Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 2016-present Scille SAS
 
-from unittest.mock import ANY
 
-import pytest
-
-from parsec._parsec import UserID, invited_cmds
+from parsec._parsec import (
+    DateTime,
+    GreetingAttemptID,
+    RevokedUserCertificate,
+    UserID,
+    UserProfile,
+    UserUpdateCertificate,
+    authenticated_cmds,
+    invited_cmds,
+)
 from tests.common import Backend, CoolorgRpcClients
 
 
-@pytest.mark.skip("Not implemented yet")
 async def test_invited_invite_claimer_start_greeting_attempt_ok(
     coolorg: CoolorgRpcClients, backend: Backend
 ) -> None:
@@ -18,10 +23,10 @@ async def test_invited_invite_claimer_start_greeting_attempt_ok(
         token=invitation_token,
         greeter=coolorg.alice.user_id,
     )
-    assert rep == invited_cmds.v4.invite_claimer_start_greeting_attempt.RepOk(greeting_attempt=ANY)
+    assert isinstance(rep, invited_cmds.v4.invite_claimer_start_greeting_attempt.RepOk)
+    assert isinstance(rep.greeting_attempt, GreetingAttemptID)
 
 
-@pytest.mark.skip("Not implemented yet")
 async def test_invited_invite_claimer_start_greeting_attempt_greeter_not_found(
     coolorg: CoolorgRpcClients,
 ) -> None:
@@ -35,7 +40,6 @@ async def test_invited_invite_claimer_start_greeting_attempt_greeter_not_found(
     assert rep == invited_cmds.v4.invite_claimer_start_greeting_attempt.RepGreeterNotFound()
 
 
-@pytest.mark.skip("Not implemented yet")
 async def test_invited_invite_claimer_start_greeting_attempt_greeter_not_allowed(
     coolorg: CoolorgRpcClients,
 ) -> None:
@@ -47,13 +51,30 @@ async def test_invited_invite_claimer_start_greeting_attempt_greeter_not_allowed
     assert rep == invited_cmds.v4.invite_claimer_start_greeting_attempt.RepGreeterNotAllowed()
 
 
-@pytest.mark.skip("Not implemented yet")
 async def test_invited_invite_claimer_start_greeting_attempt_greeter_revoked(
     coolorg: CoolorgRpcClients,
 ) -> None:
-    # TODO
-    # await coolorg.bob.user_revoke(alice_revoked_user_certificate)
+    # Make Bob an admin
+    certif = UserUpdateCertificate(
+        author=coolorg.alice.device_id,
+        new_profile=UserProfile.ADMIN,
+        user_id=coolorg.bob.user_id,
+        timestamp=DateTime.now(),
+    ).dump_and_sign(coolorg.alice.signing_key)
+    rep = await coolorg.alice.user_update(certif)
+    assert rep == authenticated_cmds.v4.user_update.RepOk()
 
+    # Revoke Alice
+    now = DateTime.now()
+    certif = RevokedUserCertificate(
+        author=coolorg.bob.device_id,
+        timestamp=now,
+        user_id=coolorg.alice.user_id,
+    ).dump_and_sign(coolorg.bob.signing_key)
+    rep = await coolorg.bob.user_revoke(certif)
+    assert rep == authenticated_cmds.v4.user_revoke.RepOk()
+
+    # Try to invite
     rep = await coolorg.invited_alice_dev3.invite_claimer_start_greeting_attempt(
         token=coolorg.invited_alice_dev3.token,
         greeter=coolorg.alice.user_id,
