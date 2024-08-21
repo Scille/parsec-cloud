4d45f1accedf94bcb02498e49f85b26712599274
Maxime GRANDCOLAS <maxime.grandcolas@scille.fr>
2024-08-21 17:23:02 +0200

    [MS] Adds .env for production and rc

--- client/src/services/environment.ts
+++ client/src/services/environment.ts
@@ -4,17 +4,20 @@ import { EnvironmentType } from 'megashark-lib';
 
 const STRIPE_ENV_VARIABLE = 'VITE_STRIPE_API_KEY';
 // cspell:disable-next-line
-const DEFAULT_STRIPE_API_KEY = 'pk_live_5hF9sn4DLUTYWx9uWPDJv51s00Q8ktUVfI';
+const DEFAULT_STRIPE_API_KEY = 'pk_test_P4dfuyoLBQtDHKjTiNDH3JH700TT3mCLbE';
 
 function getStripeApiKey(): { key: string; mode: EnvironmentType } {
   if (import.meta.env[STRIPE_ENV_VARIABLE]) {
-    return { key: import.meta.env[STRIPE_ENV_VARIABLE], mode: EnvironmentType.Development };
+    return {
+      key: import.meta.env[STRIPE_ENV_VARIABLE],
+      mode: (import.meta.env[STRIPE_ENV_VARIABLE] as string).startsWith('pk_live_') ? EnvironmentType.Production : EnvironmentType.Development,
+    };
   }
-  return { key: DEFAULT_STRIPE_API_KEY, mode: EnvironmentType.Production };
+  return { key: DEFAULT_STRIPE_API_KEY, mode: EnvironmentType.Development };
 }
 
 const BMS_ENV_VARIABLE = 'VITE_BMS_API_URL';
-const DEFAULT_BMS_URL = 'https://bms.parsec.cloud';
+const DEFAULT_BMS_URL = 'https://bms-dev.parsec.cloud';
 
 function getBmsUrl(): string {
   if (import.meta.env[BMS_ENV_VARIABLE]) {
@@ -24,7 +27,7 @@ function getBmsUrl(): string {
 }
 
 const SIGN_ENV_VARIABLE = 'VITE_SIGN_URL';
-const DEFAULT_SIGN_URL = 'https://sign.parsec.cloud';
+const DEFAULT_SIGN_URL = 'https://sign-dev.parsec.cloud';
 
 function getSignUrl(): string {
   if (import.meta.env[SIGN_ENV_VARIABLE]) {
