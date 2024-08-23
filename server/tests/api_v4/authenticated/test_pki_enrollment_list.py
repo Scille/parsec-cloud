08c5999cfe1777f9d2957250bd7bba12c152a13c
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2024-08-23 02:53:25 +0200

    Add missing tests for pki commands in server

--- server/tests/api_v4/authenticated/test_pki_enrollment_list.py
+++ server/tests/api_v4/authenticated/test_pki_enrollment_list.py
@@ -2,12 +2,67 @@
 
 import pytest
 
+from parsec._parsec import (
+    DateTime,
+    DeviceLabel,
+    EnrollmentID,
+    PkiEnrollmentSubmitPayload,
+    PrivateKey,
+    SigningKey,
+    authenticated_cmds,
+)
+from tests.common import Backend, CoolorgRpcClients
 
-@pytest.mark.xfail(reason="TODO: PKI is not fully implemented yet")
-async def test_authenticated_pki_enrollment_list_ok() -> None:
-    raise NotImplementedError
+
+@pytest.mark.parametrize("kind", ("empty", "items"))
+async def test_authenticated_pki_enrollment_list_ok(
+    coolorg: CoolorgRpcClients,
+    backend: Backend,
+    kind: str,
+) -> None:
+    match kind:
+        case "empty":
+            expected_enrollments = []
+
+        case "items":
+            enrollment_id = EnrollmentID.new()
+            submitted_on = DateTime.now()
+            submit_payload = PkiEnrollmentSubmitPayload(
+                verify_key=SigningKey.generate().verify_key,
+                public_key=PrivateKey.generate().public_key,
+                requested_device_label=DeviceLabel("Dev1"),
+            ).dump()
+            outcome = await backend.pki.submit(
+                now=submitted_on,
+                organization_id=coolorg.organization_id,
+                enrollment_id=enrollment_id,
+                force=False,
+                submitter_der_x509_certificate=b"<philip der x509 certificate>",
+                submitter_der_x509_certificate_email="philip@example.invalid",
+                submit_payload_signature=b"<philip submit payload signature>",
+                submit_payload=submit_payload,
+            )
+            assert outcome is None
+
+            expected_enrollments = [
+                authenticated_cmds.v4.pki_enrollment_list.PkiEnrollmentListItem(
+                    enrollment_id=enrollment_id,
+                    submit_payload=submit_payload,
+                    submit_payload_signature=b"<philip submit payload signature>",
+                    submitted_on=submitted_on,
+                    submitter_der_x509_certificate=b"<philip der x509 certificate>",
+                )
+            ]
+
+        case unknown:
+            assert False, unknown
+
+    rep = await coolorg.alice.pki_enrollment_list()
+    assert rep == authenticated_cmds.v4.pki_enrollment_list.RepOk(enrollments=expected_enrollments)
 
 
-@pytest.mark.xfail(reason="TODO: PKI is not fully implemented yet")
-async def test_authenticated_pki_enrollment_list_author_not_allowed() -> None:
-    raise NotImplementedError
+async def test_authenticated_pki_enrollment_list_author_not_allowed(
+    coolorg: CoolorgRpcClients,
+) -> None:
+    rep = await coolorg.bob.pki_enrollment_list()
+    assert rep == authenticated_cmds.v4.pki_enrollment_list.RepAuthorNotAllowed()
--- server/tests/api_v4/authenticated/test_pki_enrollment_reject.py
+++ server/tests/api_v4/authenticated/test_pki_enrollment_reject.py
@@ -2,22 +2,80 @@
 
 import pytest
 
-
-@pytest.mark.xfail(reason="TODO: PKI is not fully implemented yet")
-async def test_authenticated_pki_enrollment_reject_author_not_allowed() -> None:
-    raise NotImplementedError
-
-
-@pytest.mark.xfail(reason="TODO: PKI is not fully implemented yet")
-async def test_authenticated_pki_enrollment_reject_enrollment_no_longer_available() -> None:
-    raise NotImplementedError
-
-
-@pytest.mark.xfail(reason="TODO: PKI is not fully implemented yet")
-async def test_authenticated_pki_enrollment_reject_enrollment_not_found() -> None:
-    raise NotImplementedError
+from parsec._parsec import (
+    DateTime,
+    DeviceLabel,
+    EnrollmentID,
+    PkiEnrollmentSubmitPayload,
+    PrivateKey,
+    SigningKey,
+    authenticated_cmds,
+)
+from tests.common import Backend, CoolorgRpcClients
 
 
-@pytest.mark.xfail(reason="TODO: PKI is not fully implemented yet")
-async def test_authenticated_pki_enrollment_reject_ok() -> None:
-    raise NotImplementedError
+@pytest.fixture
+async def enrollment_id(
+    coolorg: CoolorgRpcClients,
+    backend: Backend,
+) -> EnrollmentID:
+    enrollment_id = EnrollmentID.new()
+    submitted_on = DateTime.now()
+    submit_payload = PkiEnrollmentSubmitPayload(
+        verify_key=SigningKey.generate().verify_key,
+        public_key=PrivateKey.generate().public_key,
+        requested_device_label=DeviceLabel("Dev1"),
+    ).dump()
+    outcome = await backend.pki.submit(
+        now=submitted_on,
+        organization_id=coolorg.organization_id,
+        enrollment_id=enrollment_id,
+        force=False,
+        submitter_der_x509_certificate=b"<philip der x509 certificate>",
+        submitter_der_x509_certificate_email="philip@example.invalid",
+        submit_payload_signature=b"<philip submit payload signature>",
+        submit_payload=submit_payload,
+    )
+    assert outcome is None
+
+    return enrollment_id
+
+
+async def test_authenticated_pki_enrollment_reject_ok(
+    coolorg: CoolorgRpcClients,
+    enrollment_id: EnrollmentID,
+) -> None:
+    rep = await coolorg.alice.pki_enrollment_reject(enrollment_id=enrollment_id)
+    assert rep == authenticated_cmds.v4.pki_enrollment_reject.RepOk()
+
+
+async def test_authenticated_pki_enrollment_reject_author_not_allowed(
+    coolorg: CoolorgRpcClients,
+    enrollment_id: EnrollmentID,
+) -> None:
+    rep = await coolorg.bob.pki_enrollment_reject(enrollment_id=enrollment_id)
+    assert rep == authenticated_cmds.v4.pki_enrollment_reject.RepAuthorNotAllowed()
+
+
+async def test_authenticated_pki_enrollment_reject_enrollment_no_longer_available(
+    coolorg: CoolorgRpcClients,
+    backend: Backend,
+    enrollment_id: EnrollmentID,
+) -> None:
+    outcome = await backend.pki.reject(
+        now=DateTime.now(),
+        organization_id=coolorg.organization_id,
+        author=coolorg.alice.device_id,
+        enrollment_id=enrollment_id,
+    )
+    assert outcome is None
+
+    rep = await coolorg.alice.pki_enrollment_reject(enrollment_id=enrollment_id)
+    assert rep == authenticated_cmds.v4.pki_enrollment_reject.RepEnrollmentNoLongerAvailable()
+
+
+async def test_authenticated_pki_enrollment_reject_enrollment_not_found(
+    coolorg: CoolorgRpcClients,
+) -> None:
+    rep = await coolorg.alice.pki_enrollment_reject(enrollment_id=EnrollmentID.new())
+    assert rep == authenticated_cmds.v4.pki_enrollment_reject.RepEnrollmentNotFound()
