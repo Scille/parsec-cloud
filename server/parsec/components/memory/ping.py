fcf0c774f47e22a49a81941b01f964e181364646
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2024-08-26 09:55:50 +0200

    Remove useless `MemoryPingComponent.register_components` in server

--- server/parsec/components/memory/ping.py
+++ server/parsec/components/memory/ping.py
@@ -1,7 +1,7 @@
 # Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 2016-present Scille SAS
 from __future__ import annotations
 
-from typing import Any, override
+from typing import override
 
 from parsec._parsec import OrganizationID
 from parsec.components.events import EventBus
@@ -12,9 +12,6 @@ from parsec.events import EventPinged
 class MemoryPingComponent(BasePingComponent):
     def __init__(self, event_bus: EventBus) -> None:
         self._event_bus = event_bus
-
-    def register_components(self, **other_components: Any) -> None:
-        pass
 
     @override
     async def ping(self, organization_id: OrganizationID, ping: str) -> None:
