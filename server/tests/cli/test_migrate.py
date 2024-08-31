49f143b4a46773ec0405b1341a63c4c1b28b38d5
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2024-08-31 06:36:19 +0200

    Fix postgresql migration system & tests

--- server/tests/cli/test_migrate.py
+++ server/tests/cli/test_migrate.py
@@ -1,6 +1,7 @@
 # Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 2016-present Scille SAS
 from __future__ import annotations
 
+import asyncio
 import sys
 from unittest.mock import patch
 
@@ -9,11 +10,29 @@ from click.testing import CliRunner
 
 from parsec.cli import cli
 from parsec.components.postgresql import MigrationItem
+from tests.common import execute_pg_queries
 
 
 @pytest.mark.postgresql
 @pytest.mark.skipif(sys.platform == "win32", reason="Hard to test on Windows...")
-def test_migrate(postgresql_url) -> None:
+def test_migrate(postgresql_url: str, monkeypatch: pytest.MonkeyPatch) -> None:
+    # Must use a fake migration table, otherwise we will break the database for
+    # the other tests (or ourself is the database is kept between pytest runs)
+    asyncio.run(
+        execute_pg_queries(
+            postgresql_url,
+            """
+CREATE TABLE IF NOT EXISTS migration_dummy (
+    _id INTEGER PRIMARY KEY,
+    name VARCHAR(256) NOT NULL UNIQUE,
+    applied TIMESTAMPTZ NOT NULL
+);
+TRUNCATE TABLE migration_dummy RESTART IDENTITY CASCADE;
+""",
+        )
+    )
+    monkeypatch.setattr("parsec.components.postgresql.handler.MIGRATION_TABLE", "migration_dummy")
+
     sql = "SELECT current_database();"  # Dummy migration content
     dry_run_args = f"migrate --db {postgresql_url} --dry-run"
     apply_args = f"migrate --db {postgresql_url}"
@@ -21,10 +40,10 @@ def test_migrate(postgresql_url) -> None:
     with patch("parsec.cli.migration.retrieve_migrations") as retrieve_migrations:
         retrieve_migrations.return_value = [
             MigrationItem(
-                idx=100001, name="migration1", file_name="100001_migration1.sql", sql=sql
+                index=100001, name="migration1", file_name="100001_migration1.sql", sql=sql
             ),
             MigrationItem(
-                idx=100002, name="migration2", file_name="100002_migration2.sql", sql=sql
+                index=100002, name="migration2", file_name="100002_migration2.sql", sql=sql
             ),
         ]
         runner = CliRunner()
@@ -40,7 +59,9 @@ def test_migrate(postgresql_url) -> None:
         assert "100002_migration2.sql ✔" in result.output
 
         retrieve_migrations.return_value.append(
-            MigrationItem(idx=100003, name="migration3", file_name="100003_migration3.sql", sql=sql)
+            MigrationItem(
+                index=100003, name="migration3", file_name="100003_migration3.sql", sql=sql
+            )
         )
 
         result = runner.invoke(cli, dry_run_args)
