3bc41548d76f2592504a07fd4aeb27b8b32dca25
Firelight Flagboy <firelight.flagboy@gmail.com>
2024-09-03 15:33:04 +0200

    Perform inbound sync when listing files in CLI
    Add a flag `--offline` to the `ls` command in the CLI to disable fetching the latest change to the server

--- cli/src/commands/ls.rs
+++ cli/src/commands/ls.rs
@@ -8,6 +8,9 @@ crate::clap_parser_with_shared_opts_builder!(
         workspace_id: VlobID,
         /// Path to list
         path: Option<FsPath>,
+        /// Use offline mode, do not sync with the server for latest changes
+        #[arg(long)]
+        offline: bool,
     }
 );
 
@@ -18,6 +21,7 @@ pub async fn ls(args: Ls) -> anyhow::Result<()> {
         password_stdin,
         device,
         config_dir,
+        offline,
     } = args;
     let path = path.unwrap_or(FsPath::default());
 
@@ -29,6 +33,21 @@ pub async fn ls(args: Ls) -> anyhow::Result<()> {
 
     let client = crate::utils::load_client(&config_dir, device, password_stdin).await?;
     let workspace = client.start_workspace(workspace_id).await?;
+    if !offline {
+        workspace.refresh_realm_checkpoint().await?;
+    }
+
+    loop {
+        let entries_to_sync = workspace.get_need_inbound_sync(32).await?;
+        log::debug!("Entries to inbound sync: {:?}", entries_to_sync);
+        if entries_to_sync.is_empty() {
+            break;
+        }
+        for entry in entries_to_sync {
+            workspace.inbound_sync(entry).await?;
+        }
+    }
+
     let entries = workspace.stat_folder_children(&path).await?;
 
     for (entry, _stat) in entries {
