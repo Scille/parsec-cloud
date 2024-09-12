b4af3b4dd65a7efddd634a3c9a997b8b8a8681d7
Vincent Michel <vxgmichel@gmail.com>
2024-09-12 13:46:22 +0200

    Add read_folder_with_confined_entries test

--- libparsec/crates/client/tests/unit/workspace/read_folder.rs
+++ libparsec/crates/client/tests/unit/workspace/read_folder.rs
@@ -7,7 +7,7 @@ use libparsec_tests_fixtures::prelude::*;
 use libparsec_types::prelude::*;
 
 use super::utils::workspace_ops_factory;
-use crate::workspace::{transactions::FolderReaderStatNextOutcome, EntryStat};
+use crate::workspace::{transactions::FolderReaderStatNextOutcome, EntryStat, MoveEntryMode};
 
 fn expect_entry(stat: FolderReaderStatNextOutcome<'_>) -> (&EntryName, EntryStat) {
     match stat {
@@ -373,3 +373,132 @@ async fn ignore_invalid_children(env: &TestbedEnv) {
 }
 
 // TODO: having multiple children that point to the same entry ID is currently accepted, should we do something about it ?
+
+#[parsec_test(testbed = "minimal_client_ready")]
+async fn read_folder_with_confined_entries(
+    #[values(true, false)] folder_is_confined: bool,
+    env: &TestbedEnv,
+) {
+    let wksp1_id: VlobID = *env.template.get_stuff("wksp1_id");
+    let wksp1_foo_id: VlobID = *env.template.get_stuff("wksp1_foo_id");
+    let wksp1_foo_spam_id: VlobID = *env.template.get_stuff("wksp1_foo_spam_id");
+    let wksp1_foo_egg_txt_id: VlobID = *env.template.get_stuff("wksp1_foo_egg_txt_id");
+
+    let alice = env.local_device("alice@dev1");
+    let ops = workspace_ops_factory(&env.discriminant_dir, &alice, wksp1_id.to_owned()).await;
+
+    // Rename foo to foo.tmp
+    let target_path;
+    let expected_egg_txt_confinement_point;
+    let expected_spam_confinement_point;
+    if folder_is_confined {
+        ops.rename_entry_by_id(
+            wksp1_id,
+            "foo".parse().unwrap(),
+            "foo.tmp".parse().unwrap(),
+            MoveEntryMode::NoReplace,
+        )
+        .await
+        .unwrap();
+        target_path = "/foo.tmp".parse().unwrap();
+        expected_egg_txt_confinement_point = Some(wksp1_id);
+        expected_spam_confinement_point = Some(wksp1_id);
+    } else {
+        target_path = "/foo".parse().unwrap();
+        expected_egg_txt_confinement_point = Some(wksp1_foo_id);
+        expected_spam_confinement_point = None;
+    }
+
+    // Rename egg.txt to egg.txt.tmp
+    ops.rename_entry_by_id(
+        wksp1_foo_id,
+        "egg.txt".parse().unwrap(),
+        "egg.txt.tmp".parse().unwrap(),
+        MoveEntryMode::NoReplace,
+    )
+    .await
+    .unwrap();
+
+    let target_id = wksp1_foo_id;
+    let expected_ordered_children_stats = [
+        (
+            "egg.txt.tmp".parse().unwrap(),
+            EntryStat::File {
+                confinement_point: expected_egg_txt_confinement_point,
+                id: wksp1_foo_egg_txt_id,
+                parent: wksp1_foo_id,
+                created: "2000-01-09T00:00:00Z".parse().unwrap(),
+                updated: "2000-01-09T00:00:00Z".parse().unwrap(),
+                base_version: 1,
+                is_placeholder: false,
+                need_sync: false,
+                size: 0,
+            },
+        ),
+        (
+            "spam".parse().unwrap(),
+            EntryStat::Folder {
+                confinement_point: expected_spam_confinement_point,
+                id: wksp1_foo_spam_id,
+                parent: wksp1_foo_id,
+                created: "2000-01-08T00:00:00Z".parse().unwrap(),
+                updated: "2000-01-08T00:00:00Z".parse().unwrap(),
+                base_version: 1,
+                is_placeholder: false,
+                need_sync: false,
+            },
+        ),
+    ];
+
+    // 1) Test `open_folder_reader`
+
+    let reader = ops.open_folder_reader(&target_path).await.unwrap();
+
+    let a0 = expect_entry(reader.stat_child(&ops, 0).await.unwrap());
+    let a1 = expect_entry(reader.stat_child(&ops, 1).await.unwrap());
+    expect_no_more_entries(reader.stat_child(&ops, 2).await.unwrap());
+
+    let a0_retry = expect_entry(reader.stat_child(&ops, 0).await.unwrap());
+    p_assert_eq!(a0, a0_retry);
+
+    p_assert_eq!(
+        {
+            let mut items: Vec<_> = [a0, a1]
+                .into_iter()
+                .map(|(name_ref, stat)| (name_ref.to_owned(), stat))
+                .collect();
+            items.sort_by(|a, b| a.0.cmp(&b.0));
+            items
+        },
+        expected_ordered_children_stats,
+    );
+
+    // 2) Test `open_folder_reader_by_id`
+
+    let reader = ops.open_folder_reader_by_id(target_id).await.unwrap();
+
+    let a0 = expect_entry(reader.stat_child(&ops, 0).await.unwrap());
+    let a1 = expect_entry(reader.stat_child(&ops, 1).await.unwrap());
+    expect_no_more_entries(reader.stat_child(&ops, 2).await.unwrap());
+
+    let a0_retry = expect_entry(reader.stat_child(&ops, 0).await.unwrap());
+    p_assert_eq!(a0, a0_retry);
+
+    p_assert_eq!(
+        {
+            let mut items: Vec<_> = [a0, a1]
+                .into_iter()
+                .map(|(name_ref, stat)| (name_ref.to_owned(), stat))
+                .collect();
+            items.sort_by(|a, b| a.0.cmp(&b.0));
+            items
+        },
+        expected_ordered_children_stats,
+    );
+
+    // 3) Test `stat_folder_children`
+
+    let mut children_stats = ops.stat_folder_children(&target_path).await.unwrap();
+    children_stats.sort_by(|a, b| a.0.cmp(&b.0));
+    p_assert_eq!(children_stats, expected_ordered_children_stats,);
+}
