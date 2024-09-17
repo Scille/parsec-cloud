0e72097f034cadc5d2f3ad3783f3923db524cd13
fabienSvstr <fabien.sevestre@my-digital-school.org>
2024-09-17 11:20:15 +0200

    [MS] Hide `lastUpdated` in workspace

--- client/tests/component/specs/testFileCard.spec.ts
+++ client/tests/component/specs/testFileCard.spec.ts
@@ -48,7 +48,7 @@ describe('File Card Item', () => {
     });
 
     expect(wrapper.get('.card-content__title').text()).to.equal('A File.txt');
-    expect(wrapper.get('.card-content-last-update').text()).to.equal('now');
+    // expect(wrapper.get('.card-content-last-update').text()).to.equal('now');
     wrapper.trigger('dblclick');
     expect(wrapper.emitted('click')?.length).to.equal(1);
     expect(wrapper.emitted('click')?.at(0)?.at(1)).to.deep.equal(FILE);
@@ -86,7 +86,7 @@ describe('File Card Item', () => {
     });
 
     expect(wrapper.get('.card-content__title').text()).to.equal('A Folder');
-    expect(wrapper.get('.card-content-last-update').text()).to.equal('now');
+    // expect(wrapper.get('.card-content-last-update').text()).to.equal('now');
     // expect(wrapper.get('.label-size')).not.to.be.visible;
     wrapper.trigger('dblclick');
     expect(wrapper.emitted('click')?.length).to.equal(1);
