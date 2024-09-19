012958068d80e80ccdf9ec3f102a4b7705e2b524
Aurelia <aurelia.dolo@gmail.com>
2024-09-19 18:02:05 +0200

    Add test to show that we must add a const value to timestamp after receiving a require greater timestamp error.

--- libparsec/crates/client/tests/unit/client/revoke_user.rs
+++ libparsec/crates/client/tests/unit/client/revoke_user.rs
@@ -63,6 +63,74 @@ async fn ok(env: &TestbedEnv) {
 }
 
 #[parsec_test(testbed = "coolorg")]
+async fn loop_require_greater_timestamp(env: &TestbedEnv) {
+    let alice = env.local_device("alice@dev1");
+    let client = client_factory(&env.discriminant_dir, alice.clone()).await;
+
+    // Mock requests to server
+    let new_common_certificates: Arc<Mutex<Vec<Bytes>>> = Arc::default();
+
+    test_register_sequence_of_send_hooks!(
+        &env.discriminant_dir,
+        // 1) try to Revoke
+        {
+            let new_common_certificates = new_common_certificates.clone();
+            let alice = alice.clone();
+            move |_req: authenticated_cmds::latest::user_revoke::Req| {
+                let now = DateTime::now();
+                let device_id = DeviceID::default();
+                let crafted_common_cert = DeviceCertificate {
+                    timestamp: now.add_us(100), // simulate client lagging behind server
+                    user_id: alice.user_id,
+                    device_id,
+                    author: Some(alice.device_id).into(),
+                    device_label: MaybeRedacted::Redacted(DeviceLabel::new_redacted(device_id)),
+                    verify_key: alice.verify_key(),
+                    algorithm: SigningKeyAlgorithm::Ed25519,
+                };
+
+                let cert = crafted_common_cert.dump_and_sign(&alice.signing_key).into();
+
+                new_common_certificates.lock().unwrap().push(cert);
+                authenticated_cmds::latest::user_revoke::Rep::RequireGreaterTimestamp {
+                    strictly_greater_than: now,
+                }
+            }
+        },
+        // 1) re Revoke
+        {
+            let new_common_certificates = new_common_certificates.clone();
+            move |req: authenticated_cmds::latest::user_revoke::Req| {
+                new_common_certificates
+                    .lock()
+                    .unwrap()
+                    .push(req.revoked_user_certificate);
+                authenticated_cmds::latest::user_revoke::Rep::Ok
+            }
+        },
+        // 2) Fetch new certificates
+        {
+            let new_common_certificates = new_common_certificates.clone();
+            move |_req: authenticated_cmds::latest::certificate_get::Req| {
+                authenticated_cmds::latest::certificate_get::Rep::Ok {
+                    common_certificates: new_common_certificates.lock().unwrap().clone(),
+                    realm_certificates: HashMap::new(),
+                    sequester_certificates: vec![],
+                    shamir_recovery_certificates: vec![],
+                }
+            }
+        },
+    );
+
+    let bob_user_id: UserID = "bob".parse().unwrap();
+    client.revoke_user(bob_user_id).await.unwrap();
+
+    let users = client.list_users(false, None, None).await.unwrap();
+    let bob = users.iter().find(|user| user.id == bob_user_id).unwrap();
+    p_assert_eq!(bob.revoked_by, Some("alice@dev1".parse().unwrap()));
+}
+
+#[parsec_test(testbed = "coolorg")]
 async fn not_allowed(env: &TestbedEnv) {
     let bob = env.local_device("bob@dev1");
     let client = client_factory(&env.discriminant_dir, bob).await;
