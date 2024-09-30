556e6180f4f772ef6f3c227932bcc08811d409ac
Vincent Michel <vxgmichel@gmail.com>
2024-09-30 21:49:42 +0200

    Bump version 3.0.2-a.0+dev -> 3.0.2-rc.1

--- client/electron/assets/electron-publisher-custom.js
+++ client/electron/assets/electron-publisher-custom.js
@@ -2,7 +2,7 @@
 
 const { GitHubPublisher } = require('electron-publish/out/gitHubPublisher');
 
-const VERSION = '3.0.2-a.0+dev';
+const VERSION = '3.0.2-rc.1';
 
 class CustomGitHubPublisher extends GitHubPublisher {
   /**
