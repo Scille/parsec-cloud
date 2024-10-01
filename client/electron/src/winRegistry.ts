f3b04b3feb01071dfa44304626ff39fb1339e5d5
Maxime GRANDCOLAS <maxime.grandcolas@scille.fr>
2024-10-01 10:34:42 +0200

    [MS] Added some try/catch on electron side

--- client/electron/src/winRegistry.ts
+++ client/electron/src/winRegistry.ts
@@ -10,117 +10,125 @@ export class WinRegistry {
   constructor(appGuid: string, appPath: string) {
     this.APP_GUID = appGuid;
     if (process.platform === 'win32') {
-      const reg = require('regedit');
-      if (!electronIsDev) {
-        const vbsDirectory = join(appPath, '../vbs');
-        reg.setExternalVBSLocation(vbsDirectory);
+      try {
+        const reg = require('regedit');
+        if (!electronIsDev) {
+          const vbsDirectory = join(appPath, '../vbs');
+          reg.setExternalVBSLocation(vbsDirectory);
+        }
+        this.regedit = reg.promisified;
+      } catch (error: any) {
+        console.error('Failed to init regedit', error);
       }
-      this.regedit = reg.promisified;
     }
   }
 
   async addMountpointToQuickAccess(mountpointPath: string, iconPath: string): Promise<void> {
-    if (process.platform !== 'win32' || !this.regedit) {
-      return;
-    }
-    await this.removeMountpointFromQuickAccess();
+    try {
+      if (process.platform !== 'win32' || !this.regedit) {
+        return;
+      }
+      await this.removeMountpointFromQuickAccess();
 
-    const baseKey1 = `HKCU\\Software\\Classes\\CLSID\\{${this.APP_GUID}}`;
-    const baseKey2 = `HKCU\\Software\\Classes\\Wow6432Node\\CLSID\\{${this.APP_GUID}}`;
-    const systemRoot = process.env.SYSTEMROOT || 'C:\\Windows';
+      const baseKey1 = `HKCU\\Software\\Classes\\CLSID\\{${this.APP_GUID}}`;
+      const baseKey2 = `HKCU\\Software\\Classes\\Wow6432Node\\CLSID\\{${this.APP_GUID}}`;
+      const systemRoot = process.env.SYSTEMROOT || 'C:\\Windows';
 
-    for (const key of [baseKey1, baseKey2]) {
-      await this.regedit.createKey([key]);
+      for (const key of [baseKey1, baseKey2]) {
+        await this.regedit.createKey([key]);
+        await this.regedit.putValue({
+          [key]: {
+            AppName: {
+              value: 'Parsec',
+              type: 'REG_DEFAULT',
+            },
+            SortOrderIndex: {
+              value: 0x42,
+              type: 'REG_DWORD',
+            },
+            'System.IsPinnedToNamespaceTree': {
+              value: 0x01,
+              type: 'REG_DWORD',
+            },
+          },
+        });
+        await this.regedit.createKey([`${key}\\DefaultIcon`]);
+        await this.regedit.putValue({
+          [`${key}\\DefaultIcon`]: {
+            IconPath: {
+              value: iconPath,
+              type: 'REG_DEFAULT',
+            },
+          },
+        });
+        await this.regedit.createKey([`${key}\\InProcServer32`]);
+        await this.regedit.putValue({
+          [`${key}\\InProcServer32`]: {
+            IconPath: {
+              value: `${systemRoot}\\system32\\shell32.dll`,
+              type: 'REG_DEFAULT',
+            },
+          },
+        });
+        await this.regedit.createKey([`${key}\\Instance`]);
+        await this.regedit.putValue({
+          [`${key}\\Instance`]: {
+            CLSID: {
+              value: '{0E5AAE11-A475-4c5b-AB00-C66DE400274E}',
+              type: 'REG_SZ',
+            },
+          },
+        });
+        await this.regedit.createKey([`${key}\\Instance\\InitPropertyBag`]);
+        await this.regedit.putValue({
+          [`${key}\\Instance\\InitPropertyBag`]: {
+            Attributes: {
+              value: 0x11,
+              type: 'REG_DWORD',
+            },
+            TargetFolderPath: {
+              value: mountpointPath,
+              type: 'REG_SZ',
+            },
+          },
+        });
+
+        await this.regedit.createKey([`${key}\\ShellFolder`]);
+        await this.regedit.putValue({
+          [`${key}\\ShellFolder`]: {
+            Attributes: {
+              value: 0xf080004d,
+              type: 'REG_DWORD',
+            },
+            FolderValueFlags: {
+              value: 0x28,
+              type: 'REG_DWORD',
+            },
+          },
+        });
+      }
+
+      await this.regedit.createKey(`HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Desktop\\NameSpace\\{${this.APP_GUID}}`);
       await this.regedit.putValue({
-        [key]: {
+        [`HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Desktop\\NameSpace\\{${this.APP_GUID}}`]: {
           AppName: {
             value: 'Parsec',
             type: 'REG_DEFAULT',
           },
-          SortOrderIndex: {
-            value: 0x42,
-            type: 'REG_DWORD',
-          },
-          'System.IsPinnedToNamespaceTree': {
-            value: 0x01,
-            type: 'REG_DWORD',
-          },
-        },
-      });
-      await this.regedit.createKey([`${key}\\DefaultIcon`]);
-      await this.regedit.putValue({
-        [`${key}\\DefaultIcon`]: {
-          IconPath: {
-            value: iconPath,
-            type: 'REG_DEFAULT',
-          },
-        },
-      });
-      await this.regedit.createKey([`${key}\\InProcServer32`]);
-      await this.regedit.putValue({
-        [`${key}\\InProcServer32`]: {
-          IconPath: {
-            value: `${systemRoot}\\system32\\shell32.dll`,
-            type: 'REG_DEFAULT',
-          },
-        },
-      });
-      await this.regedit.createKey([`${key}\\Instance`]);
-      await this.regedit.putValue({
-        [`${key}\\Instance`]: {
-          CLSID: {
-            value: '{0E5AAE11-A475-4c5b-AB00-C66DE400274E}',
-            type: 'REG_SZ',
-          },
-        },
-      });
-      await this.regedit.createKey([`${key}\\Instance\\InitPropertyBag`]);
-      await this.regedit.putValue({
-        [`${key}\\Instance\\InitPropertyBag`]: {
-          Attributes: {
-            value: 0x11,
-            type: 'REG_DWORD',
-          },
-          TargetFolderPath: {
-            value: mountpointPath,
-            type: 'REG_SZ',
-          },
         },
       });
 
-      await this.regedit.createKey([`${key}\\ShellFolder`]);
       await this.regedit.putValue({
-        [`${key}\\ShellFolder`]: {
-          Attributes: {
-            value: 0xf080004d,
-            type: 'REG_DWORD',
-          },
-          FolderValueFlags: {
-            value: 0x28,
+        'HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\HideDesktopIcons\\NewStartPanel': {
+          [`{${this.APP_GUID}}`]: {
+            value: 0x1,
             type: 'REG_DWORD',
           },
         },
       });
+    } catch (error: any) {
+      console.error('Failed to add mountpoint link to sidebar', error);
     }
-
-    await this.regedit.createKey(`HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Desktop\\NameSpace\\{${this.APP_GUID}}`);
-    await this.regedit.putValue({
-      [`HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Desktop\\NameSpace\\{${this.APP_GUID}}`]: {
-        AppName: {
-          value: 'Parsec',
-          type: 'REG_DEFAULT',
-        },
-      },
-    });
-
-    await this.regedit.putValue({
-      'HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\HideDesktopIcons\\NewStartPanel': {
-        [`{${this.APP_GUID}}`]: {
-          value: 0x1,
-          type: 'REG_DWORD',
-        },
-      },
-    });
   }
 
   async removeMountpointFromQuickAccess(): Promise<void> {
