b717b5dff974ab18713204cc365a49c4f4b4eb64
Maxime GRANDCOLAS <maxime.grandcolas@scille.fr>
2024-10-02 08:12:23 +0200

    [MS] Fixed the reloading of the client area

---
+++ client/src/views/client-area/ClientAreaLayout.vue
@@ -0,0 +1,16 @@
+<!-- Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 2016-present Scille SAS -->
+
+<template>
+  <ion-page>
+    <client-area-page
+      id="page"
+      :key="BmsAccessInstance.get().reloadKey"
+    />
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import { IonPage } from '@ionic/vue';
+import { BmsAccessInstance } from '@/services/bms';
+import ClientAreaPage from '@/views/client-area/ClientAreaPage.vue';
+</script>
