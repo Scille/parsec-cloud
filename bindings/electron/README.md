f4efd5dfef9ff1e2b6a843822deb59358861cd7c
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2024-10-03 16:26:24 +0200

    Improve README documentation for electron & web bindings

--- bindings/electron/README.md
+++ bindings/electron/README.md
@@ -1,9 +1,42 @@
 # libparsec-electron-bindings
 
+## Build
+
+tl;dr: `../../make.py er`
+
+First run (to install dependencies)
+
+```shell
+../../make.py ei    # ei is an alias for electron-dev-install
+```
+
+Subsequent runs
+
+```shell
+../../make.py er    # er is an alias for electron-dev-rebuild
+```
+
+Basically `make.py` wraps the following commands:
+
 ```shell
 npm install
 # (re)generate index.node
 npm run build:dev
 # Or for release
 npm run build:release
+```
+
+## Testing (the simple way)
+
+Start node shell
+
+```shell
+node
+```
+
+Then import libparsec package and you're good !
+
+```javascript
+libparsec = require("./bindings/electron/dist/libparsec");
+await libparsec.listAvailableDevices("/foo")
 ```
---
+++ bindings/web/scripts/liveplay.py
@@ -0,0 +1,44 @@
+#! /usr/bin/env python
+# Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 2016-present Scille SAS
+
+
+import sys
+import subprocess
+from pathlib import Path
+
+
+BASEDIR = Path(__file__).parent
+PKG_DIR = BASEDIR / "../pkg"
+
+
+INDEX_HTML_SRC = """<!DOCTYPE html>
+<html>
+  <head>
+    <meta content="text/html;charset=utf-8" http-equiv="Content-Type"/>
+  </head>
+  <body>
+    <script src="libparsec_bindings_web.js" type="module"></script>
+    <script type="module">
+        console.log("loading libparsec...");
+        import initModule, * as libparsec from './libparsec_bindings_web.js';
+        await initModule();
+        libparsec.initLogger();
+        window.libparsec = libparsec;
+        console.log("libparsec is loaded !");
+    </script>
+    <p>Open you browser console and start with e.g.
+    <code>libparsec.listAvailableDevices(&quot;/foo&quot;)</code></p>
+  </body>
+</html>
+"""
+
+
+if __name__ == "__main__":
+    if not PKG_DIR.exists():
+        raise SystemExit(
+            f"{PKG_DIR} doesn't exist, you should build the bindings with `make.py` first !"
+        )
+    print(f"Creating {PKG_DIR}/index.html...")
+    (PKG_DIR / "index.html").write_text(INDEX_HTML_SRC)
+
+    subprocess.check_call([sys.executable, "-m", "http.server", "8000"], cwd=PKG_DIR)
