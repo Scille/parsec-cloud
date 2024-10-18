e4929b5cf29662d7582a75a6dec70ef50e8aa8dc
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2024-10-18 20:47:16 +0200

    Implement workspace history in libparsec_client

---
+++ libparsec/crates/client/src/workspace/history/fd_read.rs
@@ -0,0 +1,101 @@
+// Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 2016-present Scille SAS
+
+use libparsec_client_connection::ConnectionError;
+use libparsec_types::prelude::*;
+
+use super::{WorkspaceHistoryGetBlockError, WorkspaceHistoryOps};
+use crate::{InvalidBlockAccessError, InvalidCertificateError, InvalidKeysBundleError};
+
+#[derive(Debug, thiserror::Error)]
+pub enum WorkspaceHistoryFdReadError {
+    #[error("Cannot reach the server")]
+    Offline,
+    #[error("Component has stopped")]
+    Stopped,
+    #[error("File descriptor not found")]
+    BadFileDescriptor,
+    #[error("Not allowed to access this realm")]
+    NoRealmAccess,
+    #[error("Block doesn't exist on the server")]
+    BlockNotFound,
+    #[error(transparent)]
+    InvalidBlockAccess(#[from] Box<InvalidBlockAccessError>),
+    #[error(transparent)]
+    InvalidKeysBundle(#[from] Box<InvalidKeysBundleError>),
+    #[error(transparent)]
+    InvalidCertificate(#[from] Box<InvalidCertificateError>),
+    #[error(transparent)]
+    Internal(#[from] anyhow::Error),
+}
+
+impl From<ConnectionError> for WorkspaceHistoryFdReadError {
+    fn from(value: ConnectionError) -> Self {
+        match value {
+            ConnectionError::NoResponse(_) => Self::Offline,
+            err => Self::Internal(err.into()),
+        }
+    }
+}
+
+pub async fn fd_read(
+    ops: &WorkspaceHistoryOps,
+    fd: FileDescriptor,
+    offset: u64,
+    size: u64,
+    buf: &mut impl std::io::Write,
+) -> Result<u64, WorkspaceHistoryFdReadError> {
+    let manifest = {
+        let cache = ops.cache.lock().expect("Mutex is poisoned");
+        let manifest = cache
+            .opened_files
+            .get(&fd)
+            .ok_or(WorkspaceHistoryFdReadError::BadFileDescriptor)?;
+        manifest.clone()
+    };
+
+    let (written_size, chunk_views) =
+        crate::workspace::transactions::prepare_read(&manifest, size, offset);
+    let mut buf_size = 0;
+    for chunk_view in chunk_views {
+        let access = chunk_view
+            .access
+            .as_ref()
+            .expect("history only works on blocks");
+        let chunk_data = ops
+            .get_block(access, &manifest.base)
+            .await
+            .map_err(|err| match err {
+                WorkspaceHistoryGetBlockError::Offline => WorkspaceHistoryFdReadError::Offline,
+                WorkspaceHistoryGetBlockError::Stopped
+                | WorkspaceHistoryGetBlockError::StoreUnavailable => {
+                    WorkspaceHistoryFdReadError::Stopped
+                }
+                WorkspaceHistoryGetBlockError::NoRealmAccess => {
+                    WorkspaceHistoryFdReadError::NoRealmAccess
+                }
+                WorkspaceHistoryGetBlockError::BlockNotFound => {
+                    WorkspaceHistoryFdReadError::BlockNotFound
+                }
+                WorkspaceHistoryGetBlockError::InvalidBlockAccess(err) => {
+                    WorkspaceHistoryFdReadError::InvalidBlockAccess(err)
+                }
+                WorkspaceHistoryGetBlockError::InvalidCertificate(err) => {
+                    WorkspaceHistoryFdReadError::InvalidCertificate(err)
+                }
+                WorkspaceHistoryGetBlockError::InvalidKeysBundle(err) => {
+                    WorkspaceHistoryFdReadError::InvalidKeysBundle(err)
+                }
+                WorkspaceHistoryGetBlockError::Internal(err) => {
+                    err.context("cannot read chunk").into()
+                }
+            })?;
+        chunk_view
+            .copy_between_start_and_stop(&chunk_data, offset, buf, &mut buf_size)
+            .expect("prepare_read/buf/size are consistent");
+    }
+    if buf_size < written_size as usize {
+        buf.write_all(&vec![0; written_size as usize - buf_size])
+            .expect("write_all should not fail");
+    }
+    Ok(written_size)
+}
