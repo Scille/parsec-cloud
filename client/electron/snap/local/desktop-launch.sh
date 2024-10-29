03adbdcf6aad6a35b534a84af54075d4e1ae1178
Vincent Michel <vxgmichel@gmail.com>
2024-10-29 11:32:41 +0100

    Save the environment and restore it instead of creating a fresh one in xdg-open

--- client/electron/snap/local/desktop-launch.sh
+++ client/electron/snap/local/desktop-launch.sh
@@ -1,5 +1,13 @@
 #!/bin/bash -e
+
+# Capture the current environment for the future xdg-open calls.
+XDG_OPEN_ENVIRONMENT=$(export; echo 'xdg-open "$@"')
+export XDG_OPEN_ENVIRONMENT
+
 # Snap bin directory needs more priority than `/usr/bin` and `/bin` in order for
 # the `xdg-open` script to work properly.
 export PATH="$SNAP/bin:$PATH"
+
+# Run the app through `desktop-launch-wrapper` to populate the environment with
+# variables that are specific to the snap environment.
 exec "$SNAP/bin/desktop-launch-wrapper" "$SNAP/app/parsec" "$@" --no-sandbox
