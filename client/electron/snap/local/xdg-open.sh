21c14570dac8418bae0c6074d8857c13f4b66f47
Vincent Michel <vxgmichel@gmail.com>
2024-10-29 11:32:41 +0100

    Save the environment and restore it instead of creating a fresh one in xdg-open

--- client/electron/snap/local/xdg-open.sh
+++ client/electron/snap/local/xdg-open.sh
@@ -1,13 +1,19 @@
 #!/bin/bash -e
-# Explanation:
+
 # When running the app through `desktop-launch-wrapper`, the environment is populated
-# with variables that are specific to the snap enviroment. But when the electron app
+# with variables that are specific to the snap environment. But when the electron app
 # uses `xdg-open` to open an external resource, we want to use the host environment
 # (otherwise, the host applications might end up crashing when trying to load libraries
-# from the snap environment). This is why we expose an `xdg-open` script with a higher
-# priority in the PATH that will call the host `xdg-open` binary (see `command.sh`
-# above).
+# from the snap environment). This is why we expose this `xdg-open` script with a higher
+# priority in the PATH (see `desktop-launch.sh`).
 # See: https://github.com/Scille/parsec-cloud/issues/8737
-XDG_OPEN=$(which -a xdg-open | grep -v "$SNAP" | head -n 1)
-FILTERED_PATH=$(echo -n "$PATH" | sed -e "s#$SNAP/bin:##g")
-exec env - LANG="$LANG" DISPLAY="$DISPLAY" PATH="$FILTERED_PATH" "$XDG_OPEN" "$@"
+
+if [ -z "$XDG_OPEN_ENVIRONMENT" ];
+then
+    # A fallback in case the environment variable is not set.
+    # This should not happen, but better safe than sorry.
+    XDG_OPEN=$(which -a xdg-open | grep -v "/snap/" | head -n 1)
+    exec "$XDG_OPEN" "$@"
+else
+    exec env - bash -c "$XDG_OPEN_ENVIRONMENT" -- "$@"
+fi
