c577778ecaa7ca43a032530973532b0565cf5920
Vincent Michel <vxgmichel@gmail.com>
2024-11-06 18:56:20 +0100

    Add shamir recovery invitation to invite_list tests

--- server/tests/api_v4/authenticated/test_invite_list.py
+++ server/tests/api_v4/authenticated/test_invite_list.py
@@ -1,7 +1,85 @@
 # Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 2016-present Scille SAS
 
-from parsec._parsec import DateTime, InvitationStatus, authenticated_cmds
+import pytest
+
+from parsec._parsec import (
+    DateTime,
+    InvitationStatus,
+    InvitationToken,
+    ShamirRecoveryBriefCertificate,
+    ShamirRecoveryShareCertificate,
+    authenticated_cmds,
+)
 from tests.common import Backend, CoolorgRpcClients, HttpCommonErrorsTester, MinimalorgRpcClients
+
+
+@pytest.fixture
+async def alice_shamir(backend: Backend, coolorg: CoolorgRpcClients, with_postgresql: bool) -> None:
+    if with_postgresql:
+        pytest.xfail("TODO: postgre not implemented yet")
+    dt = DateTime.now()
+    bob_share = ShamirRecoveryShareCertificate(
+        author=coolorg.alice.device_id,
+        user_id=coolorg.alice.user_id,
+        timestamp=dt,
+        recipient=coolorg.bob.user_id,
+        ciphered_share=b"abc",
+    )
+    mallory_share = ShamirRecoveryShareCertificate(
+        author=coolorg.alice.device_id,
+        user_id=coolorg.alice.user_id,
+        timestamp=dt,
+        recipient=coolorg.mallory.user_id,
+        ciphered_share=b"abc",
+    )
+    brief = ShamirRecoveryBriefCertificate(
+        author=coolorg.alice.device_id,
+        user_id=coolorg.alice.user_id,
+        timestamp=dt,
+        threshold=2,
+        per_recipient_shares={coolorg.bob.user_id: 1, coolorg.mallory.user_id: 1},
+    )
+
+    setup = authenticated_cmds.v4.shamir_recovery_setup.ShamirRecoverySetup(
+        b"abc",
+        InvitationToken.new(),
+        brief.dump_and_sign(coolorg.alice.signing_key),
+        [
+            bob_share.dump_and_sign(coolorg.alice.signing_key),
+            mallory_share.dump_and_sign(coolorg.alice.signing_key),
+        ],
+    )
+    rep = await coolorg.alice.shamir_recovery_setup(setup)
+    assert rep == authenticated_cmds.v4.shamir_recovery_setup.RepOk()
+
+
+async def test_authenticated_invite_list_ok_with_shamir_recovery(
+    coolorg: CoolorgRpcClients, backend: Backend, alice_shamir: None
+) -> None:
+    expected_invitations = []
+
+    # Shamir recovery invitation
+    t6 = DateTime(2020, 1, 6)
+    outcome = await backend.invite.new_for_shamir_recovery(
+        now=t6,
+        organization_id=coolorg.organization_id,
+        author=coolorg.bob.device_id,
+        claimer_user_id=coolorg.alice.user_id,
+        send_email=False,
+    )
+    assert isinstance(outcome, tuple)
+    expected_invitations.append(
+        authenticated_cmds.v4.invite_list.InviteListItemShamirRecovery(
+            created_on=t6,
+            status=InvitationStatus.IDLE,
+            claimer_user_id=coolorg.alice.user_id,
+            token=outcome[0],
+        )
+    )
+
+    rep = await coolorg.bob.invite_list()
+    assert isinstance(rep, authenticated_cmds.v4.invite_list.RepOk)
+    assert rep.invitations == expected_invitations
 
 
 async def test_authenticated_invite_list_ok(
