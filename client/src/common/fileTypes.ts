cf90c77373540a6ba529750419767323c8a774c5
Maxime GRANDCOLAS <maxime.grandcolas@scille.fr>
2024-11-06 11:22:14 +0100

    [MS] Added mock file content

--- client/src/common/fileTypes.ts
+++ client/src/common/fileTypes.ts
@@ -2,33 +2,40 @@
 
 import { fileTypeFromBuffer } from 'file-type';
 
-enum FileType {
+enum FileContentType {
   Image = 'image',
   Video = 'video',
   Audio = 'audio',
+  Spreadsheet = 'spreadsheet',
+  Document = 'document',
   Unknown = 'unknown',
 }
 
 interface DetectedFileType {
-  type: FileType;
+  type: FileContentType;
   extension: string;
   mimeType: string;
 }
 
 const IMAGES = ['image/png', 'image/webp', 'image/jpeg', 'image/svg+xml', 'image/bmp', 'image/gif'];
+const SPREADSHEETS = ['application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'];
 
-async function detectFileType(buffer: Uint8Array): Promise<DetectedFileType> {
+async function detectFileContentType(buffer: Uint8Array): Promise<DetectedFileType> {
   const result = await fileTypeFromBuffer(buffer);
 
   if (!result) {
-    return { type: FileType.Unknown, extension: '', mimeType: 'application/octet-stream' };
+    return { type: FileContentType.Unknown, extension: '', mimeType: 'application/octet-stream' };
   }
 
   if (IMAGES.includes(result.mime)) {
-    return { type: FileType.Image, extension: result.ext, mimeType: result.mime };
+    return { type: FileContentType.Image, extension: result.ext, mimeType: result.mime };
   }
+  if (SPREADSHEETS.includes(result.mime)) {
+    return { type: FileContentType.Spreadsheet, extension: result.ext, mimeType: result.mime };
+  }
+  console.log(`Unhandled mimetype ${result.mime}`);
 
-  return { type: FileType.Unknown, extension: result.ext, mimeType: result.mime };
+  return { type: FileContentType.Unknown, extension: result.ext, mimeType: result.mime };
 }
 
-export { DetectedFileType, FileType, detectFileType };
+export { DetectedFileType, FileContentType, detectFileContentType };
