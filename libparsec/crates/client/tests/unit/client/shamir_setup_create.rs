7b89e1b8524cb6b48055c2de457ac185428b786d
Vincent Michel <vxgmichel@gmail.com>
2024-11-08 11:40:05 +0100

    Improve shamir_setup_create::ok test

--- libparsec/crates/client/tests/unit/client/shamir_setup_create.rs
+++ libparsec/crates/client/tests/unit/client/shamir_setup_create.rs
@@ -7,7 +7,7 @@ use libparsec_types::prelude::*;
 
 use super::utils::client_factory;
 
-#[parsec_test(testbed = "coolorg")]
+#[parsec_test(testbed = "coolorg", with_server)]
 async fn ok(env: &TestbedEnv) {
     let alice = env.local_device("alice@dev1");
     let client = client_factory(&env.discriminant_dir, alice.clone()).await;
@@ -18,4 +18,18 @@ async fn ok(env: &TestbedEnv) {
         .shamir_setup_create(share_recipients, 1)
         .await
         .unwrap();
+
+    // The `shamir_setup_create` already polls the server for new certificates,
+    // But we do it once more to check its idempotent behavior
+    client.poll_server_for_new_certificates().await.unwrap();
+
+    // Also check that Bob can successfully retrieve the new certificates
+    let bob = env.local_device("bob@dev1");
+    let client = client_factory(&env.discriminant_dir, bob.clone()).await;
+    client.poll_server_for_new_certificates().await.unwrap();
+
+    // Also check that Mallory is unaffected
+    let mallory = env.local_device("mallory@dev1");
+    let client = client_factory(&env.discriminant_dir, mallory.clone()).await;
+    client.poll_server_for_new_certificates().await.unwrap();
 }
