609f628cc302694687e9a17a17ad763bb3aa66ed
Vincent Michel <vxgmichel@gmail.com>
2024-11-21 18:32:32 +0100

    Add test_invited_invite_info_ok_with_shamir test

--- server/tests/api_v4/invited/test_invite_info.py
+++ server/tests/api_v4/invited/test_invite_info.py
@@ -2,8 +2,9 @@
 
 import pytest
 
-from parsec._parsec import invited_cmds
-from tests.common import CoolorgRpcClients, HttpCommonErrorsTester
+from parsec._parsec import authenticated_cmds, invited_cmds
+from tests.common import CoolorgRpcClients, HttpCommonErrorsTester, ShamirOrgRpcClients
+from tests.common.client import InvitedRpcClient
 
 
 @pytest.mark.parametrize("user_or_device", ("user", "device"))
@@ -30,6 +31,40 @@ async def test_invited_invite_info_ok(user_or_device: str, coolorg: CoolorgRpcCl
 
         case unknown:
             assert False, unknown
+
+
+async def test_invited_invite_info_ok_with_shamir(shamirorg: ShamirOrgRpcClients) -> None:
+    # TODO: add `TestbedEventNewShamirRecoveryInvitation` event
+    rep = await shamirorg.bob.invite_new_shamir_recovery(
+        send_email=False,
+        claimer_user_id=shamirorg.alice.user_id,
+    )
+    assert isinstance(rep, authenticated_cmds.v4.invite_new_shamir_recovery.RepOk)
+
+    shamir_invited_bob = InvitedRpcClient(shamirorg.raw_client, shamirorg.organization_id, rep)  # type: ignore
+    rep = await shamir_invited_bob.invite_info()
+    assert rep == invited_cmds.v4.invite_info.RepOk(
+        invited_cmds.v4.invite_info.UserOrDeviceShamirRecovery(
+            threshold=2,
+            recipients=[
+                invited_cmds.latest.invite_info.ShamirRecoveryRecipient(
+                    user_id=shamirorg.bob.user_id,
+                    human_handle=shamirorg.bob.human_handle,
+                    shares=2,
+                ),
+                invited_cmds.latest.invite_info.ShamirRecoveryRecipient(
+                    user_id=shamirorg.mallory.user_id,
+                    human_handle=shamirorg.mallory.human_handle,
+                    shares=1,
+                ),
+                invited_cmds.latest.invite_info.ShamirRecoveryRecipient(
+                    user_id=shamirorg.mike.user_id,
+                    human_handle=shamirorg.mike.human_handle,
+                    shares=1,
+                ),
+            ],
+        )
+    )
 
 
 async def test_invited_invite_info_http_common_errors(
