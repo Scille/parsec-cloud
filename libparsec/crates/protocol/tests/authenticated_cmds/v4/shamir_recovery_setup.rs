bb8eaeb15e485fec58c4bf7208dcfec0dd7a754a
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2024-11-25 10:54:17 +0100

    Update `shamir_recovery_setup` protocol schema to support certificate-based deletion

--- libparsec/crates/protocol/tests/authenticated_cmds/v4/shamir_recovery_setup.rs
+++ libparsec/crates/protocol/tests/authenticated_cmds/v4/shamir_recovery_setup.rs
@@ -1,7 +1,6 @@
 // Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 2016-present Scille SAS
 #![allow(clippy::unwrap_used)]
 
-use libparsec_protocol::authenticated_cmds::v4::shamir_recovery_setup::ShamirRecoverySetup;
 use libparsec_tests_lite::prelude::*;
 use libparsec_types::InvitationToken;
 
@@ -38,202 +37,228 @@ fn rep_helper(raw: &[u8], expected: authenticated_cmds::shamir_recovery_setup::R
     p_assert_eq!(data2, expected);
 }
 
-fn req_empty_setup() {
-    // Generated from Parsec 3.0.0-b.12+dev
+pub fn req() {
+    // Generated from Parsec 3.2.1-a.0+dev
     // Content:
     //   cmd: 'shamir_recovery_setup'
-    //   setup: None
-    let raw: &[u8] =
-        hex!("82a3636d64b57368616d69725f7265636f766572795f7365747570a57365747570c0").as_ref();
-
-    let expected = authenticated_cmds::shamir_recovery_setup::Req { setup: None };
-
-    req_helper(raw, expected);
-}
-
-fn req_with_setup() {
-    // Generated from Parsec 3.0.0-b.12+dev
-    // Content:
-    //   cmd: 'shamir_recovery_setup'
-    //   setup: {
-    //     brief: 0x6272696566,
-    //     ciphered_data: 0x63697068657265645f64617461,
-    //     reveal_token: 0x0563ff98846c4dbf9e0a1cc2f6fbb149,
-    //     shares: [ 0x736861726573 ],
-    //   }
+    //   ciphered_data: 0x63697068657265645f64617461
+    //   reveal_token: 0x0563ff98846c4dbf9e0a1cc2f6fbb149
+    //   shamir_recovery_brief_certificate: 0x6272696566
+    //   shamir_recovery_share_certificates: [ 0x736861726573, ]
     let raw: &[u8] = hex!(
-    "82a3636d64b57368616d69725f7265636f766572795f7365747570a5736574757084a5"
-    "6272696566c4056272696566ad63697068657265645f64617461c40d63697068657265"
-    "645f64617461ac72657665616c5f746f6b656ec4100563ff98846c4dbf9e0a1cc2f6fb"
-    "b149a673686172657391c406736861726573"
+        "85a3636d64b57368616d69725f7265636f766572795f7365747570ad63697068657265"
+        "645f64617461c40d63697068657265645f64617461ac72657665616c5f746f6b656ec4"
+        "100563ff98846c4dbf9e0a1cc2f6fbb149d9217368616d69725f7265636f766572795f"
+        "62726965665f6365727469666963617465c4056272696566d9227368616d69725f7265"
+        "636f766572795f73686172655f63657274696669636174657391c406736861726573"
     )
     .as_ref();
 
     let expected = authenticated_cmds::shamir_recovery_setup::Req {
-        setup: Some(ShamirRecoverySetup {
-            brief: "brief".into(),
-            ciphered_data: "ciphered_data".into(),
-            reveal_token: InvitationToken::from_hex("0563ff98846c4dbf9e0a1cc2f6fbb149").unwrap(),
-            shares: vec!["shares".into()],
-        }),
+        shamir_recovery_brief_certificate: "brief".into(),
+        shamir_recovery_share_certificates: vec!["shares".into()],
+        ciphered_data: "ciphered_data".into(),
+        reveal_token: InvitationToken::from_hex("0563ff98846c4dbf9e0a1cc2f6fbb149").unwrap(),
     };
 
     req_helper(raw, expected);
 }
 
-pub fn req() {
-    req_empty_setup();
-    req_with_setup();
+pub fn rep_ok() {
+    let raw: &[u8] = hex!("81a6737461747573a26f6b").as_ref();
+
+    let expected = authenticated_cmds::shamir_recovery_setup::Rep::Ok;
+
+    rep_helper(raw, expected);
 }
 
-pub fn rep_shamir_setup_already_exists() {
-    // Generated from Parsec 3.0.0-b.12+dev
+pub fn rep_invalid_certificate_brief_corrupted() {
+    // Generated from Parsec 3.2.1-a.0+dev
     // Content:
-    //   status: 'shamir_setup_already_exists'
-    //   last_shamir_certificate_timestamp: ext(1, 946684800000000) i.e. 2000-01-01T01:00:00Z
+    //   status: 'invalid_certificate_brief_corrupted'
     let raw: &[u8] = hex!(
-    "82a6737461747573bb7368616d69725f73657475705f616c72656164795f6578697374"
-    "73d9216c6173745f7368616d69725f63657274696669636174655f74696d657374616d"
-    "70d70100035d013b37e000"
+        "81a6737461747573d923696e76616c69645f63657274696669636174655f6272696566"
+        "5f636f72727570746564"
     )
     .as_ref();
 
-    let expected = authenticated_cmds::shamir_recovery_setup::Rep::ShamirSetupAlreadyExists {
+    let expected = authenticated_cmds::shamir_recovery_setup::Rep::InvalidCertificateBriefCorrupted;
+
+    rep_helper(raw, expected);
+}
+
+pub fn rep_invalid_certificate_share_corrupted() {
+    // Generated from Parsec 3.2.1-a.0+dev
+    // Content:
+    //   status: 'invalid_certificate_share_corrupted'
+    let raw: &[u8] = hex!(
+        "81a6737461747573d923696e76616c69645f63657274696669636174655f7368617265"
+        "5f636f72727570746564"
+    )
+    .as_ref();
+
+    let expected = authenticated_cmds::shamir_recovery_setup::Rep::InvalidCertificateShareCorrupted;
+
+    rep_helper(raw, expected);
+}
+
+pub fn rep_invalid_certificate_share_recipient_not_in_brief() {
+    // Generated from Parsec 3.2.1-a.0+dev
+    // Content:
+    //   status: 'invalid_certificate_share_recipient_not_in_brief'
+    let raw: &[u8] = hex!(
+        "81a6737461747573d930696e76616c69645f63657274696669636174655f7368617265"
+        "5f726563697069656e745f6e6f745f696e5f6272696566"
+    )
+    .as_ref();
+
+    let expected =
+        authenticated_cmds::shamir_recovery_setup::Rep::InvalidCertificateShareRecipientNotInBrief;
+
+    rep_helper(raw, expected);
+}
+
+pub fn rep_invalid_certificate_duplicate_share_for_recipient() {
+    // Generated from Parsec 3.2.1-a.0+dev
+    // Content:
+    //   status: 'invalid_certificate_duplicate_share_for_recipient'
+    let raw: &[u8] = hex!(
+        "81a6737461747573d931696e76616c69645f63657274696669636174655f6475706c69"
+        "636174655f73686172655f666f725f726563697069656e74"
+    )
+    .as_ref();
+
+    let expected = authenticated_cmds::shamir_recovery_setup::Rep::InvalidCertificateDuplicateShareForRecipient;
+
+    rep_helper(raw, expected);
+}
+
+pub fn rep_invalid_certificate_author_included_as_recipient() {
+    // Generated from Parsec 3.2.1-a.0+dev
+    // Content:
+    //   status: 'invalid_certificate_author_included_as_recipient'
+    let raw: &[u8] = hex!(
+        "81a6737461747573d930696e76616c69645f63657274696669636174655f617574686f"
+        "725f696e636c756465645f61735f726563697069656e74"
+    )
+    .as_ref();
+
+    let expected =
+        authenticated_cmds::shamir_recovery_setup::Rep::InvalidCertificateAuthorIncludedAsRecipient;
+
+    rep_helper(raw, expected);
+}
+
+pub fn rep_invalid_certificate_missing_share_for_recipient() {
+    // Generated from Parsec 3.2.1-a.0+dev
+    // Content:
+    //   status: 'invalid_certificate_missing_share_for_recipient'
+    let raw: &[u8] = hex!(
+        "81a6737461747573d92f696e76616c69645f63657274696669636174655f6d69737369"
+        "6e675f73686172655f666f725f726563697069656e74"
+    )
+    .as_ref();
+
+    let expected =
+        authenticated_cmds::shamir_recovery_setup::Rep::InvalidCertificateMissingShareForRecipient;
+
+    rep_helper(raw, expected);
+}
+
+pub fn rep_invalid_certificate_share_inconsistent_timestamp() {
+    // Generated from Parsec 3.2.1-a.0+dev
+    // Content:
+    //   status: 'invalid_certificate_share_inconsistent_timestamp'
+    let raw: &[u8] = hex!(
+        "81a6737461747573d930696e76616c69645f63657274696669636174655f7368617265"
+        "5f696e636f6e73697374656e745f74696d657374616d70"
+    )
+    .as_ref();
+
+    let expected = authenticated_cmds::shamir_recovery_setup::Rep::InvalidCertificateShareInconsistentTimestamp;
+
+    rep_helper(raw, expected);
+}
+
+pub fn rep_invalid_certificate_user_id_must_be_self() {
+    // Generated from Parsec 3.2.1-a.0+dev
+    // Content:
+    //   status: 'invalid_certificate_user_id_must_be_self'
+    let raw: &[u8] = hex!(
+        "81a6737461747573d928696e76616c69645f63657274696669636174655f757365725f"
+        "69645f6d7573745f62655f73656c66"
+    )
+    .as_ref();
+
+    let expected =
+        authenticated_cmds::shamir_recovery_setup::Rep::InvalidCertificateUserIdMustBeSelf;
+
+    rep_helper(raw, expected);
+}
+
+pub fn rep_recipient_not_found() {
+    // Generated from Parsec 3.2.1-a.0+dev
+    // Content:
+    //   status: 'recipient_not_found'
+    let raw: &[u8] = hex!("81a6737461747573b3726563697069656e745f6e6f745f666f756e64").as_ref();
+
+    let expected = authenticated_cmds::shamir_recovery_setup::Rep::RecipientNotFound;
+
+    rep_helper(raw, expected);
+}
+
+pub fn rep_revoked_recipient() {
+    // Generated from Parsec 3.2.1-a.0+dev
+    // Content:
+    //   status: 'revoked_recipient'
+    //   last_common_certificate_timestamp: ext(1, 946684800000000) i.e. 2000-01-01T01:00:00Z
+    let raw: &[u8] = hex!(
+        "82a6737461747573b17265766f6b65645f726563697069656e74d9216c6173745f636f"
+        "6d6d6f6e5f63657274696669636174655f74696d657374616d70d70100035d013b37e0"
+        "00"
+    )
+    .as_ref();
+
+    let expected = authenticated_cmds::shamir_recovery_setup::Rep::RevokedRecipient {
+        last_common_certificate_timestamp: "2000-01-01T00:00:00Z".parse().unwrap(),
+    };
+
+    rep_helper(raw, expected);
+}
+
+pub fn rep_shamir_recovery_already_exists() {
+    // Generated from Parsec 3.2.1-a.0+dev
+    // Content:
+    //   status: 'shamir_recovery_already_exists'
+    //   last_shamir_certificate_timestamp: ext(1, 946684800000000) i.e. 2000-01-01T01:00:00Z
+    let raw: &[u8] = hex!(
+        "82a6737461747573be7368616d69725f7265636f766572795f616c72656164795f6578"
+        "69737473d9216c6173745f7368616d69725f63657274696669636174655f74696d6573"
+        "74616d70d70100035d013b37e000"
+    )
+    .as_ref();
+
+    let expected = authenticated_cmds::shamir_recovery_setup::Rep::ShamirRecoveryAlreadyExists {
         last_shamir_certificate_timestamp: "2000-01-01T00:00:00Z".parse().unwrap(),
     };
 
     rep_helper(raw, expected);
 }
 
-pub fn rep_brief_invalid_data() {
-    // Generated from Parsec 3.0.0-b.12+dev
-    // Content:
-    //   status: 'brief_invalid_data'
-    let raw: &[u8] = hex!("81a6737461747573b262726965665f696e76616c69645f64617461").as_ref();
-
-    let expected = authenticated_cmds::shamir_recovery_setup::Rep::BriefInvalidData;
-
-    rep_helper(raw, expected);
-}
-
-pub fn rep_share_invalid_data() {
-    // Generated from Parsec 3.0.0-b.12+dev
-    // Content:
-    //   status: 'share_invalid_data'
-    let raw: &[u8] = hex!("81a6737461747573b273686172655f696e76616c69645f64617461").as_ref();
-
-    let expected = authenticated_cmds::shamir_recovery_setup::Rep::ShareInvalidData;
-
-    rep_helper(raw, expected);
-}
-
-pub fn rep_invalid_recipient() {
-    // Generated from Parsec 3.0.0-b.12+dev
-    // Content:
-    //   status: 'invalid_recipient'
-    //   user_id: ext(2, 0xa11cec00100000000000000000000000)
-    let raw: &[u8] = hex!(
-    "82a6737461747573b1696e76616c69645f726563697069656e74a7757365725f6964d8"
-    "02a11cec00100000000000000000000000"
-    )
-    .as_ref();
-
-    let expected = authenticated_cmds::shamir_recovery_setup::Rep::InvalidRecipient {
-        user_id: "alice".parse().unwrap(),
-    };
-
-    rep_helper(raw, expected);
-}
-
-pub fn rep_share_recipient_not_in_brief() {
-    // Generated from Parsec 3.0.0-b.12+dev
-    // Content:
-    //   status: 'share_recipient_not_in_brief'
-    let raw: &[u8] = hex!(
-    "81a6737461747573bc73686172655f726563697069656e745f6e6f745f696e5f627269"
-    "6566"
-    )
-    .as_ref();
-
-    let expected = authenticated_cmds::shamir_recovery_setup::Rep::ShareRecipientNotInBrief;
-
-    rep_helper(raw, expected);
-}
-
-pub fn rep_duplicate_share_for_recipient() {
-    // Generated from Parsec 3.0.0-b.12+dev
-    // Content:
-    //   status: 'duplicate_share_for_recipient'
-    let raw: &[u8] = hex!(
-    "81a6737461747573bd6475706c69636174655f73686172655f666f725f726563697069"
-    "656e74"
-    )
-    .as_ref();
-
-    let expected = authenticated_cmds::shamir_recovery_setup::Rep::DuplicateShareForRecipient;
-
-    rep_helper(raw, expected);
-}
-
-pub fn rep_author_included_as_recipient() {
-    // Generated from Parsec 3.0.0-b.12+dev
-    // Content:
-    //   status: 'author_included_as_recipient'
-    let raw: &[u8] = hex!(
-    "81a6737461747573bc617574686f725f696e636c756465645f61735f72656369706965"
-    "6e74"
-    )
-    .as_ref();
-
-    let expected = authenticated_cmds::shamir_recovery_setup::Rep::AuthorIncludedAsRecipient;
-
-    rep_helper(raw, expected);
-}
-
-pub fn rep_missing_share_for_recipient() {
-    // Generated from Parsec 3.0.0-b.12+dev
-    // Content:
-    //   status: 'missing_share_for_recipient'
-    let raw: &[u8] = hex!(
-    "81a6737461747573bb6d697373696e675f73686172655f666f725f726563697069656e"
-    "74"
-    )
-    .as_ref();
-
-    let expected = authenticated_cmds::shamir_recovery_setup::Rep::MissingShareForRecipient;
-
-    rep_helper(raw, expected);
-}
-
-pub fn rep_share_inconsistent_timestamp() {
-    // Generated from Parsec 3.0.0-b.12+dev
-    // Content:
-    //   status: 'share_inconsistent_timestamp'
-    let raw: &[u8] = hex!(
-    "81a6737461747573bc73686172655f696e636f6e73697374656e745f74696d65737461"
-    "6d70"
-    )
-    .as_ref();
-
-    let expected = authenticated_cmds::shamir_recovery_setup::Rep::ShareInconsistentTimestamp;
-
-    rep_helper(raw, expected);
-}
-
 pub fn rep_timestamp_out_of_ballpark() {
-    // Generated from Parsec 3.0.0-b.12+dev
+    // Generated from Parsec 3.2.1-a.0+dev
     // Content:
     //   status: 'timestamp_out_of_ballpark'
     //   ballpark_client_early_offset: 300.0
     //   ballpark_client_late_offset: 320.0
-    //   client_timestamp: ext(1, 946774800000000) i.e. 2000-01-02T02:00:00Z
+    //   client_timestamp: ext(1, 946778400000000) i.e. 2000-01-02T03:00:00Z
     //   server_timestamp: ext(1, 946774800000000) i.e. 2000-01-02T02:00:00Z
     let raw: &[u8] = hex!(
-    "85a6737461747573b974696d657374616d705f6f75745f6f665f62616c6c7061726bbc"
-    "62616c6c7061726b5f636c69656e745f6561726c795f6f6666736574cb4072c0000000"
-    "0000bb62616c6c7061726b5f636c69656e745f6c6174655f6f6666736574cb40740000"
-    "00000000b0636c69656e745f74696d657374616d70d70100035d162fa2e400b0736572"
-    "7665725f74696d657374616d70d70100035d162fa2e400"
+        "85a6737461747573b974696d657374616d705f6f75745f6f665f62616c6c7061726bbc"
+        "62616c6c7061726b5f636c69656e745f6561726c795f6f6666736574cb4072c0000000"
+        "0000bb62616c6c7061726b5f636c69656e745f6c6174655f6f6666736574cb40740000"
+        "00000000b0636c69656e745f74696d657374616d70d70100035d1706368800b0736572"
+        "7665725f74696d657374616d70d70100035d162fa2e400"
     )
     .as_ref();
 
@@ -241,37 +266,26 @@ pub fn rep_timestamp_out_of_ballpark() {
         ballpark_client_early_offset: 300.,
         ballpark_client_late_offset: 320.,
         server_timestamp: "2000-1-2T01:00:00Z".parse().unwrap(),
-        client_timestamp: "2000-1-2T01:00:00Z".parse().unwrap(),
+        client_timestamp: "2000-1-2T02:00:00Z".parse().unwrap(),
     };
 
     rep_helper(raw, expected);
 }
 
 pub fn rep_require_greater_timestamp() {
-    // Generated from Parsec 3.0.0-b.12+dev
+    // Generated from Parsec 3.2.1-a.0+dev
     // Content:
     //   status: 'require_greater_timestamp'
     //   strictly_greater_than: ext(1, 946684800000000) i.e. 2000-01-01T01:00:00Z
     let raw: &[u8] = hex!(
-    "82a6737461747573b9726571756972655f677265617465725f74696d657374616d70b5"
-    "7374726963746c795f677265617465725f7468616ed70100035d013b37e000"
+        "82a6737461747573b9726571756972655f677265617465725f74696d657374616d70b5"
+        "7374726963746c795f677265617465725f7468616ed70100035d013b37e000"
     )
     .as_ref();
 
     let expected = authenticated_cmds::shamir_recovery_setup::Rep::RequireGreaterTimestamp {
         strictly_greater_than: "2000-01-01T00:00:00Z".parse().unwrap(),
     };
-
-    rep_helper(raw, expected);
-}
-
-pub fn rep_ok() {
-    // Generated from Parsec 3.0.0-b.12+dev
-    // Content:
-    //   status: 'ok'
-    let raw: &[u8] = hex!("81a6737461747573a26f6b").as_ref();
-
-    let expected = authenticated_cmds::shamir_recovery_setup::Rep::Ok;
 
     rep_helper(raw, expected);
 }
