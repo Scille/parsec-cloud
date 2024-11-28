d7cdbf23997cbf2c1d78733198035375dfddcf66
Vincent Michel <vxgmichel@gmail.com>
2024-11-28 17:15:09 +0100

    Add invite_shamir_recovery_reveal command

---
+++ server/tests/api_v4/invited/test_invite_shamir_recovery_reveal.py
@@ -0,0 +1,31 @@
+# Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 2016-present Scille SAS
+
+
+from parsec._parsec import InvitationToken, invited_cmds
+from tests.common import CoolorgRpcClients, HttpCommonErrorsTester, ShamirOrgRpcClients
+
+
+async def test_invited_invite_shamir_recovery_reveal_ok(shamirorg: ShamirOrgRpcClients) -> None:
+    token = shamirorg.alice_shamir_reveal_token
+    ciphered_data = shamirorg.alice_shamir_ciphered_data
+    rep = await shamirorg.shamir_invited_alice.invite_shamir_recovery_reveal(reveal_token=token)
+    assert rep == invited_cmds.v4.invite_shamir_recovery_reveal.RepOk(ciphered_data=ciphered_data)
+
+
+async def test_invited_invite_shamir_recovery_reveal_not_found(
+    shamirorg: ShamirOrgRpcClients,
+) -> None:
+    token = InvitationToken.new()
+    rep = await shamirorg.shamir_invited_alice.invite_shamir_recovery_reveal(token)
+    assert rep == invited_cmds.v4.invite_shamir_recovery_reveal.RepNotFound()
+
+
+async def test_invited_invite_shamir_recovery_reveal_http_common_errors(
+    coolorg: CoolorgRpcClients, invited_http_common_errors_tester: HttpCommonErrorsTester
+) -> None:
+    token = InvitationToken.new()
+
+    async def do():
+        await coolorg.invited_alice_dev3.invite_shamir_recovery_reveal(reveal_token=token)
+
+    await invited_http_common_errors_tester(do)
