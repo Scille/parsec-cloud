d4c29174ed495ca979c42c7b09c4c5423bebb449
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2024-11-28 15:11:40 +0100

    Implement web for platform_async passing

--- libparsec/crates/platform_async/tests/unit/oneshot.rs
+++ libparsec/crates/platform_async/tests/unit/oneshot.rs
@@ -1,66 +1,81 @@
 // Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 2016-present Scille SAS
-use libparsec_tests_lite::parsec_test;
 
-use crate::oneshot;
+use libparsec_tests_lite::prelude::*;
+use std::time::Duration;
+
+use crate::{oneshot, select2_biased, sleep};
 
 #[parsec_test]
-#[cfg_attr(target_arch = "wasm32", should_panic(expected = "not yet implemented"))]
 pub async fn oneshot() {
     let (sx, rx) = oneshot::channel::<u8>();
 
     #[allow(clippy::let_unit_value)]
     let ret = sx.send(42).unwrap();
-    assert!(matches!(ret, ()));
+    p_assert_eq!(ret, ());
 
     let ret = rx.await.unwrap();
-    assert!(matches!(ret, 42));
+    p_assert_eq!(ret, 42);
 }
 
 #[parsec_test]
-#[cfg_attr(target_arch = "wasm32", should_panic(expected = "not yet implemented"))]
 pub async fn oneshot_try_recv() {
     let (sx, mut rx) = oneshot::channel::<u8>();
 
     let err = rx.try_recv().unwrap_err();
-    assert!(matches!(err, oneshot::TryRecvError::Empty));
+    p_assert_matches!(err, oneshot::error::TryRecvError::Empty);
 
     #[allow(clippy::let_unit_value)]
     let ret = sx.send(42).unwrap();
-    assert!(matches!(ret, ()));
+    p_assert_eq!(ret, ());
 
     let ret = rx.try_recv().unwrap();
-    assert!(matches!(ret, 42));
+    p_assert_eq!(ret, 42);
 
     let err = rx.try_recv().unwrap_err();
-    assert!(matches!(err, oneshot::TryRecvError::Closed));
+    p_assert_matches!(err, oneshot::error::TryRecvError::Closed);
 }
 
 #[parsec_test]
-#[cfg_attr(target_arch = "wasm32", should_panic(expected = "not yet implemented"))]
 pub async fn oneshot_receiver_dropped_before_send() {
     let (sx, rx) = oneshot::channel::<u8>();
     drop(rx);
 
     let err = sx.send(42).unwrap_err();
-    assert!(matches!(err, 42));
+    p_assert_eq!(err, 42);
 }
 
 #[parsec_test]
-#[cfg_attr(target_arch = "wasm32", should_panic(expected = "not yet implemented"))]
 pub async fn oneshot_sender_dropped_before_send() {
     let (sx, rx) = oneshot::channel::<u8>();
     drop(sx);
 
     let err = rx.await.unwrap_err();
-    assert!(matches!(err, oneshot::RecvError { .. }));
+    p_assert_matches!(err, oneshot::error::RecvError { .. });
 }
 
 #[parsec_test]
-#[cfg_attr(target_arch = "wasm32", should_panic(expected = "not yet implemented"))]
 pub async fn oneshot_sender_dropped_before_send_try_recv() {
     let (sx, mut rx) = oneshot::channel::<u8>();
     drop(sx);
 
     let err = rx.try_recv().unwrap_err();
-    assert!(matches!(err, oneshot::TryRecvError::Closed));
+    p_assert_matches!(err, oneshot::error::TryRecvError::Closed);
+}
+
+#[parsec_test]
+pub async fn oneshot_recv_actually_non_blocking() {
+    let (sx, rx) = oneshot::channel::<u8>();
+
+    let ret = select2_biased!(
+        // Since select2 is biased, rx is awaited first...
+        r = rx => r.unwrap() + 1,
+        _ = async {
+            sleep(Duration::from_millis(1)).await;
+            sx.send(41).unwrap();
+            // ...and this sleep never returns since "rx got message" is checked first
+            sleep(Duration::from_millis(0)).await;
+        } => unreachable!(),
+    );
+
+    p_assert_eq!(ret, 42);
 }
---
+++ libparsec/crates/platform_async/tests/unit/select.rs
@@ -0,0 +1,123 @@
+// Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 2016-present Scille SAS
+
+use std::sync::Arc;
+
+use crate::{future::pending, lock::Mutex, select2_biased, sleep, Duration};
+use libparsec_tests_lite::prelude::*;
+
+#[parsec_test]
+pub async fn select2_biased() {
+    let ret = select2_biased!(
+        _ = sleep(Duration::from_secs(0)) => 1,
+        _ = pending::<()>() => unreachable!(),
+    );
+    p_assert_eq!(ret, 1);
+
+    let ret = select2_biased!(
+        _ = pending::<()>() => unreachable!(),
+        _ = sleep(Duration::from_secs(0)) => 2,
+    );
+    p_assert_eq!(ret, 2);
+}
+
+#[parsec_test]
+pub async fn select2_biased_with_bind() {
+    let ret = select2_biased!(
+        a = async { sleep(Duration::from_secs(0)).await; 1 } => {
+            a + 1
+        },
+        _ = pending::<()>() => unreachable!(),
+    );
+    p_assert_eq!(ret, 2);
+}
+
+#[parsec_test]
+pub async fn select2_biased_actually_non_blocking() {
+    let events = Arc::new(Mutex::new(vec![]));
+    macro_rules! push_event {
+        ($event:expr) => {{
+            let mut guard = events.lock().await;
+            guard.push($event);
+        }};
+    }
+
+    let ret = select2_biased!(
+        _ = async {
+                push_event!("1 starts");
+                sleep(Duration::from_millis(1)).await;
+                push_event!("1 continue");
+                sleep(Duration::from_millis(1)).await;
+                push_event!("1 done");
+            } => 1,
+        _ = async {
+            push_event!("2 starts");
+            sleep(Duration::from_millis(1)).await;
+            push_event!("2 continue");
+            sleep(Duration::from_millis(2)).await;
+            push_event!("2 done");
+        } => unreachable!(),
+    );
+    p_assert_eq!(ret, 1);
+    p_assert_eq!(
+        *events.lock().await,
+        vec!["1 starts", "2 starts", "1 continue", "2 continue", "1 done",]
+    );
+}
+
+#[parsec_test]
+pub async fn select3_biased() {
+    let ret = select3_biased!(
+        _ = sleep(Duration::from_secs(0)) => 1,
+        _ = pending::<()>() => unreachable!(),
+        _ = pending::<()>() => unreachable!(),
+    );
+    p_assert_eq!(ret, 1);
+}
+
+#[parsec_test]
+pub async fn select3_biased_actually_non_blocking() {
+    let events = Arc::new(Mutex::new(vec![]));
+    macro_rules! push_event {
+        ($event:expr) => {{
+            let mut guard = events.lock().await;
+            guard.push($event);
+        }};
+    }
+
+    let ret = select3_biased!(
+        _ = async {
+                push_event!("1 starts");
+                sleep(Duration::from_millis(1)).await;
+                push_event!("1 continue");
+                sleep(Duration::from_millis(1)).await;
+                push_event!("1 done");
+            } => 1,
+        _ = async {
+            push_event!("2 starts");
+            sleep(Duration::from_millis(1)).await;
+            push_event!("2 continue");
+            sleep(Duration::from_millis(2)).await;
+            push_event!("2 done");
+        } => unreachable!(),
+        _ = async {
+            push_event!("3 starts");
+            sleep(Duration::from_millis(1)).await;
+            push_event!("3 continue");
+            sleep(Duration::from_millis(2)).await;
+            push_event!("3 done");
+        } => unreachable!(),
+    );
+    p_assert_eq!(ret, 1);
+    p_assert_eq!(
+        *events.lock().await,
+        vec![
+            "1 starts",
+            "2 starts",
+            "3 starts",
+            "1 continue",
+            "2 continue",
+            "3 continue",
+            "1 done",
+        ]
+    );
+}
