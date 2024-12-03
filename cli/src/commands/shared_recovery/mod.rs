8d8f63846bb3552c2c03e95397615c3a4bdd7a8b
Aurelia <aurelia.dolo@gmail.com>
2024-12-03 15:35:44 +0100

    Add info shared recovery device cli command.

--- cli/src/commands/shared_recovery/mod.rs
+++ cli/src/commands/shared_recovery/mod.rs
@@ -1,5 +1,6 @@
 pub mod create;
 pub mod delete;
+pub mod info;
 pub mod list;
 
 #[derive(clap::Subcommand)]
@@ -8,8 +9,10 @@ pub enum Group {
     Create(create::Args),
     /// list shared recovery the user partakes in
     List(list::Args),
-    /// Delete shared recovery setup
+    /// delete shared recovery setup
     Delete(delete::Args),
+    /// info about user's shared recovery setup
+    Info(info::Args),
 }
 
 pub async fn dispatch_command(command: Group) -> anyhow::Result<()> {
@@ -17,5 +20,6 @@ pub async fn dispatch_command(command: Group) -> anyhow::Result<()> {
         Group::Create(args) => create::main(args).await,
         Group::List(args) => list::main(args).await,
         Group::Delete(args) => delete::main(args).await,
+        Group::Info(args) => info::main(args).await,
     }
 }
