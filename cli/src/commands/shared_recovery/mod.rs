e0d41f94b78e077823e52472702c78755f1fa596
Aurelia <aurelia.dolo@gmail.com>
2024-12-03 14:31:13 +0100

    Add cli remove shared recovery setup.

--- cli/src/commands/shared_recovery/mod.rs
+++ cli/src/commands/shared_recovery/mod.rs
@@ -1,4 +1,5 @@
 pub mod create;
+pub mod delete;
 pub mod list;
 
 #[derive(clap::Subcommand)]
@@ -7,11 +8,14 @@ pub enum Group {
     Create(create::Args),
     /// list shared recovery the user partakes in
     List(list::Args),
+    /// Delete shared recovery setup
+    Delete(delete::Args),
 }
 
 pub async fn dispatch_command(command: Group) -> anyhow::Result<()> {
     match command {
         Group::Create(args) => create::main(args).await,
         Group::List(args) => list::main(args).await,
+        Group::Delete(args) => delete::main(args).await,
     }
 }
