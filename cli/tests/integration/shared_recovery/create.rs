196cb0a4c0a0ca56c6f22d448889553d65f42917
Aurelia <aurelia.dolo@gmail.com>
2024-12-06 16:54:46 +0100

    CLI shared recovery create - add test for interactive threshold

--- cli/tests/integration/shared_recovery/create.rs
+++ cli/tests/integration/shared_recovery/create.rs
@@ -1,8 +1,8 @@
 use libparsec::{tmp_path, TmpPath};
 
-use crate::testenv_utils::{TestOrganization, DEFAULT_DEVICE_PASSWORD};
-
 use crate::integration_tests::bootstrap_cli_test;
+use crate::testenv_utils::{TestOrganization, DEFAULT_DEVICE_PASSWORD};
+use rexpect::session::spawn;
 
 #[rstest::rstest]
 #[tokio::test]
@@ -83,4 +83,33 @@ async fn create_shared_recovery_inexistent_email(tmp_path: TmpPath) {
         "1"
     )
     .stderr(predicates::str::contains("A user is missing"));
+}
+
+#[rstest::rstest]
+#[tokio::test]
+async fn create_shared_recovery_default(tmp_path: TmpPath) {
+    let (_, TestOrganization { bob, .. }, _) = bootstrap_cli_test(&tmp_path).await.unwrap();
+    let mut cmd = assert_cmd::Command::cargo_bin("parsec-cli").unwrap();
+    cmd.args([
+        "shared-recovery",
+        "create",
+        "--device",
+        &bob.device_id.hex(),
+    ]);
+
+    let program = cmd.get_program().to_str().unwrap().to_string();
+    let program = cmd
+        .get_args()
+        .fold(program, |acc, s| format!("{acc} {s:?}"));
+
+    let mut p = spawn(&dbg!(program), Some(1000)).unwrap();
+
+    p.exp_string("Enter password for the device:").unwrap();
+    p.send_line(DEFAULT_DEVICE_PASSWORD).unwrap();
+
+    p.exp_regex(".*The threshold is the minimum number of recipients that one must gather to recover the account:.*").unwrap();
+    p.send_line("1").unwrap();
+    p.exp_regex(".*Shared recovery setup has been created.*")
+        .unwrap();
+    p.exp_eof().unwrap();
 }
