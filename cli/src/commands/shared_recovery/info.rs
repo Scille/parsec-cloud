e4fecc0353f9aa1099769c2c0f9ae8871c6a4bea
Aurelia <aurelia.dolo@gmail.com>
2024-12-11 10:05:28 +0100

    CLI factorize client start up for shared recovery.

--- cli/src/commands/shared_recovery/info.rs
+++ cli/src/commands/shared_recovery/info.rs
@@ -1,6 +1,6 @@
 use itertools::Itertools;
 
-use crate::utils::*;
+use crate::{build_main_with_client, utils::*};
 
 crate::clap_parser_with_shared_opts_builder!(
     #[with = config_dir, device, password_stdin]
@@ -8,15 +8,9 @@ crate::clap_parser_with_shared_opts_builder!(
     }
 );
 
-pub async fn main(args: Args) -> anyhow::Result<()> {
-    let Args {
-        password_stdin,
-        device,
-        config_dir,
-    } = args;
+build_main_with_client!(main, list_shared_recovery);
 
-    let client = load_client(&config_dir, device, password_stdin).await?;
-
+pub async fn list_shared_recovery(_args: Args, client: &StartedClient) -> anyhow::Result<()> {
     {
         let mut spinner = start_spinner("Poll server for new certificates".into());
         client.poll_server_for_new_certificates().await?;
--- cli/src/commands/shared_recovery/list.rs
+++ cli/src/commands/shared_recovery/list.rs
@@ -1,6 +1,6 @@
 use itertools::Itertools;
 
-use crate::utils::*;
+use crate::{build_main_with_client, utils::*};
 
 crate::clap_parser_with_shared_opts_builder!(
     #[with = config_dir, device, password_stdin]
@@ -8,15 +8,9 @@ crate::clap_parser_with_shared_opts_builder!(
     }
 );
 
-pub async fn main(args: Args) -> anyhow::Result<()> {
-    let Args {
-        password_stdin,
-        device,
-        config_dir,
-    } = args;
+build_main_with_client!(main, shared_recovery_info);
 
-    let client = load_client(&config_dir, device, password_stdin).await?;
-
+pub async fn shared_recovery_info(_args: Args, client: &StartedClient) -> anyhow::Result<()> {
     {
         let mut spinner = start_spinner("Poll server for new certificates".into());
         client.poll_server_for_new_certificates().await?;
