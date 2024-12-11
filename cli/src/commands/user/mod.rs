94707c671b4f20a5c6f6c9a91e3f793337afb6b3
Aurelia <aurelia.dolo@gmail.com>
2024-12-11 11:22:11 +0100

    CLI add user revoke command.

--- cli/src/commands/user/mod.rs
+++ cli/src/commands/user/mod.rs
@@ -1,12 +1,15 @@
 pub mod list;
+pub mod revoke;
 
 #[derive(clap::Subcommand)]
 pub enum Group {
     List(list::Args),
+    Revoke(revoke::Args),
 }
 
 pub async fn dispatch_command(command: Group) -> anyhow::Result<()> {
     match command {
         Group::List(args) => list::main(args).await,
+        Group::Revoke(args) => revoke::main(args).await,
     }
 }
