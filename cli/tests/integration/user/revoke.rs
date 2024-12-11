ba5ca24065177e50bd290c7e16db0459199813c7
Aurelia <aurelia.dolo@gmail.com>
2024-12-11 11:29:09 +0100

    CLI add tests for user revoke.

---
+++ cli/tests/integration/user/revoke.rs
@@ -0,0 +1,43 @@
+use libparsec::{tmp_path, TmpPath};
+
+use crate::{
+    integration_tests::bootstrap_cli_test,
+    testenv_utils::{TestOrganization, DEFAULT_DEVICE_PASSWORD},
+};
+
+#[rstest::rstest]
+#[tokio::test]
+async fn revoke_user_ok(tmp_path: TmpPath) {
+    let (_, TestOrganization { alice, toto, .. }, _) = bootstrap_cli_test(&tmp_path).await.unwrap();
+
+    crate::assert_cmd_success!(
+        with_password = DEFAULT_DEVICE_PASSWORD,
+        "user",
+        "revoke",
+        "--device",
+        &alice.device_id.hex(),
+        "--email",
+        toto.human_handle.email()
+    )
+    .stdout(predicates::str::contains(format!(
+        "User {} has been revoked",
+        toto.human_handle.email()
+    )));
+}
+
+#[rstest::rstest]
+#[tokio::test]
+async fn revoke_user_not_found(tmp_path: TmpPath) {
+    let (_, TestOrganization { alice, .. }, _) = bootstrap_cli_test(&tmp_path).await.unwrap();
+
+    crate::assert_cmd_failure!(
+        with_password = DEFAULT_DEVICE_PASSWORD,
+        "user",
+        "revoke",
+        "--device",
+        &alice.device_id.hex(),
+        "--email",
+        "not-existing@example.com"
+    )
+    .stderr(predicates::str::contains("Error: User not found"));
+}
