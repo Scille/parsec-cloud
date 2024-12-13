1f879ffe6b0776ac2811bd7b11e7a58fc72ab895
Firelight Flagboy <firelight.flagboy@gmail.com>
2024-12-13 10:31:34 +0100

    The CLI command `invite cancel` now takes the token as an argument instead of an option

--- cli/src/commands/invite/cancel.rs
+++ cli/src/commands/invite/cancel.rs
@@ -9,7 +9,7 @@ crate::clap_parser_with_shared_opts_builder!(
     #[with = config_dir, device, password_stdin]
     pub struct Args {
         /// Invitation token
-        #[arg(short, long, value_parser = InvitationToken::from_hex)]
+        #[arg(value_parser = InvitationToken::from_hex)]
         token: InvitationToken,
     }
 );
@@ -27,7 +27,7 @@ pub async fn invite_cancel(args: Args, client: &StartedClient) -> anyhow::Result
         .await
         .context("Server refused to cancel invitation")?;
 
-    handle.stop_with_message("Invitation deleted".into());
+    handle.stop_with_message("Invitation canceled".into());
 
     Ok(())
 }
