adc69d217b35921ba5e06385980fb57cfaefe3db
Firelight Flagboy <firelight.flagboy@gmail.com>
2024-12-13 11:12:15 +0100

    Proposal: Use prettytable in CLI
    Goes from:
    
    ```
    02ff2c07042dd5bde23fe108b048f76f        cancelled       user (email=[redacted]
    801167da3548c3f4613235528f6399f3        finished        device
    7b9bbc4c33fa062a8e297b73b8a472ed        finished        device
    b258fca310f2305c10abb3c42dca4906        finished        user (email=[redacted]
    f1e0c71d31c1dec07699b0e06b2fa8c2        cancelled       user (email=[redacted]
    ```
    
    To
    
    ```
    +----------------------------------+-----------+--------+------------+
    | Invitation token                 | Status    | Type   | Email      |
    +==================================+===========+========+============+
    | 02ff2c07042dd5bde23fe108b048f76f | cancelled | user   | [redacted] |
    +----------------------------------+-----------+--------+------------+
    | 801167da3548c3f4613235528f6399f3 | finished  | device |            |
    +----------------------------------+-----------+--------+------------+
    | 7b9bbc4c33fa062a8e297b73b8a472ed | finished  | device |            |
    +----------------------------------+-----------+--------+------------+
    | b258fca310f2305c10abb3c42dca4906 | finished  | user   | [redacted] |
    +----------------------------------+-----------+--------+------------+
    | f1e0c71d31c1dec07699b0e06b2fa8c2 | cancelled | user   | [redacted] |
    +----------------------------------+-----------+--------+------------+
    ```
    
    Or in csv format:
    
    ```
    Invitation token,Status,Type,Email
    02ff2c07042dd5bde23fe108b048f76f,cancelled,user,[redacted]
    801167da3548c3f4613235528f6399f3,finished,device,
    7b9bbc4c33fa062a8e297b73b8a472ed,finished,device,
    b258fca310f2305c10abb3c42dca4906,finished,user,[redacted]
    f1e0c71d31c1dec07699b0e06b2fa8c2,cancelled,user,[redacted]
    ```

--- cli/src/commands/invite/list.rs
+++ cli/src/commands/invite/list.rs
@@ -1,17 +1,23 @@
 // Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 2016-present Scille SAS
 
 use libparsec::{authenticated_cmds::latest::invite_list::InviteListItem, InvitationStatus};
+use prettytable::{cell, row, Row, Table};
 
 use crate::utils::*;
 
 crate::clap_parser_with_shared_opts_builder!(
     #[with = config_dir, device, password_stdin]
-    pub struct Args {}
+    pub struct Args {
+        /// Display the output in CSV format
+        #[arg(long)]
+        csv: bool,
+    }
 );
 
 crate::build_main_with_client!(main, list_invite);
 
-pub async fn list_invite(_args: Args, client: &StartedClient) -> anyhow::Result<()> {
+pub async fn list_invite(args: Args, client: &StartedClient) -> anyhow::Result<()> {
+    let Args { csv, .. } = args;
     log::trace!("Listing invitations");
 
     let mut handle = start_spinner("Listing invitations".into());
@@ -20,19 +26,23 @@ pub async fn list_invite(_args: Args, client: &StartedClient) -> anyhow::Result<
 
     let users = client.list_users(false, None, None).await?;
 
+    let mut table = Table::new();
+    table.set_titles(row!["Invitation token", "Status", "Type", "Email"]);
+
     if invitations.is_empty() {
         handle.stop_with_message("No invitation.".into());
     } else {
         handle.stop_with_message(format!("{} invitations found.", invitations.len()));
+
         for invitation in invitations {
-            let (token, status, display_type) = match invitation {
+            let (token, status, display_type, email) = match invitation {
                 InviteListItem::User {
                     claimer_email,
                     status,
                     token,
                     ..
-                } => (token, status, format!("user (email={claimer_email}")),
-                InviteListItem::Device { status, token, .. } => (token, status, "device".into()),
+                } => (token, status, "user", Some(claimer_email)),
+                InviteListItem::Device { status, token, .. } => (token, status, "device", None),
                 InviteListItem::ShamirRecovery {
                     status,
                     token,
@@ -44,25 +54,34 @@ pub async fn list_invite(_args: Args, client: &StartedClient) -> anyhow::Result<
                         .find(|user| user.id == claimer_user_id)
                         .map(|user| format!("{}", user.human_handle))
                         .unwrap_or("N/A".to_string());
-                    (
-                        token,
-                        status,
-                        format!("shamir recovery ({claimer_human_handle})"),
-                    )
+                    (token, status, "shamir recovery", Some(claimer_human_handle))
                 }
             };
 
             let token = token.hex();
 
             let display_status = match status {
-                InvitationStatus::Idle => format!("{YELLOW}idle{RESET}"),
-                InvitationStatus::Ready => format!("{GREEN}ready{RESET}"),
-                InvitationStatus::Cancelled => format!("{RED}cancelled{RESET}"),
-                InvitationStatus::Finished => format!("{GREEN}finished{RESET}"),
+                InvitationStatus::Idle => cell!(Fy->"idle"),
+                InvitationStatus::Ready => cell!(Fg->"ready"),
+                InvitationStatus::Cancelled => cell!(Fr->"cancelled"),
+                InvitationStatus::Finished => cell!(Fg->"finished"),
             };
 
-            println!("{token}\t{display_status}\t{display_type}");
+            table.add_row(Row::new(vec![
+                cell!(token),
+                display_status,
+                cell!(display_type),
+                cell!(email.as_deref().unwrap_or("")),
+            ]));
         }
+    }
+
+    if csv {
+        drop(table.to_csv_writer(prettytable::csv::Writer::from_writer(
+            std::io::stdout().lock(),
+        ))?);
+    } else if !table.is_empty() {
+        table.printstd();
     }
 
     Ok(())
