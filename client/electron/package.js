0a3ccf33b0c40ea37b1da283c157cf34632e7f52
Vincent Michel <vxgmichel@gmail.com>
2024-12-17 15:50:14 +0100

    Bump version 3.2.3-rc.0 -> 3.2.3-rc.1+dev

--- client/electron/package.js
+++ client/electron/package.js
@@ -126,7 +126,7 @@ const options = {
   appId: 'ParsecCloud.Parsec.Parsec.3',
   productName: 'Parsec',
   artifactName: UNSIGNED_ARTIFACT_NAME,
-  buildVersion: '3.2.3-rc.0',
+  buildVersion: '3.2.3-rc.1+dev',
   protocols: {
     name: 'Parsec-v3',
     schemes: [PARSEC_SCHEME],
