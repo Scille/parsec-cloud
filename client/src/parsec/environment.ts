8c1cf273a055d9dbb3dd815f4eb70e478eeb7242
Vincent Michel <vxgmichel@gmail.com>
2025-01-17 16:05:44 +0100

    Disable all the call to `needsMocks` at once

--- client/src/parsec/environment.ts
+++ client/src/parsec/environment.ts
@@ -37,6 +37,9 @@ export function usesTestbed(): boolean {
 // Currently, this can be used on web, since the bindings are not fully
 // implemented, but it could also prove useful when in a testing environment.
 export function needsMocks(): boolean {
+  // Shortcut to disable all of the needsMocks at once.
+  // TODO: Remove `needsMocks` altogether.
+  return false;
   return !isDesktop();
 }
 
