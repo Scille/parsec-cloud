b8b4cf213b847776bc84c318cb1ef74295adf413
Aurelia <aurelia.dolo@gmail.com>
2025-01-17 16:40:12 +0100

    [CLI] Ask for confirmation before creating shared recovery.

--- cli/tests/integration/mod.rs
+++ cli/tests/integration/mod.rs
@@ -190,7 +190,8 @@ fn shared_recovery_create(
             "1",
             "1",
             "--threshold",
-            "1"
+            "1",
+            "--no-confirmation"
         )
         .stdout(predicates::str::contains(
             "Shared recovery setup has been created",
@@ -207,7 +208,8 @@ fn shared_recovery_create(
             "--weights",
             "1",
             "--threshold",
-            "1"
+            "1",
+            "--no-confirmation"
         )
         .stdout(predicates::str::contains(
             "Shared recovery setup has been created",
