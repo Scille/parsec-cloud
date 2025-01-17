2d83dfafb813e3e3c364cf6beff72246e05c4351
Vincent Michel <vxgmichel@gmail.com>
2025-01-17 16:02:56 +0100

    Hotfix to sleep forever in web implementation of sleep

--- libparsec/crates/platform_async/src/web/mod.rs
+++ libparsec/crates/platform_async/src/web/mod.rs
@@ -13,6 +13,18 @@ pub async fn sleep(duration: std::time::Duration) {
     // Cannot use `tokio::time::sleep` since it depends on a timer API that is
     // not provided by `wasm32-unknown-unknown`.
     // Instead we use `gloo_timers` that is a convenient wrapper around js_sys.
+
+    // Hotfix: sleep forever if Duration is MAX
+    // In practice, gloo_timers::future::sleep panics if the duration cannot be casted into a u32 in milliseconds.
+    // TODO: divide the duration in u32::MAX chunks and sleep for each chunk
+    if duration == std::time::Duration::MAX {
+        loop {
+            let not_send_future =
+                gloo_timers::future::sleep(std::time::Duration::from_secs_f64(1.0));
+            pretend_future_is_send_on_web(not_send_future).await
+        }
+        return;
+    }
     let not_send_future = gloo_timers::future::sleep(duration);
     pretend_future_is_send_on_web(not_send_future).await
 }
