e4c793aba6bdf87112a120f66815028aa99d46c4
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2025-01-17 17:09:24 +0100

    Proper fix to correct libparsec_platform_async's sleep forever on web

--- libparsec/crates/platform_async/src/web/mod.rs
+++ libparsec/crates/platform_async/src/web/mod.rs
@@ -14,19 +14,33 @@ pub async fn sleep(duration: std::time::Duration) {
     // not provided by `wasm32-unknown-unknown`.
     // Instead we use `gloo_timers` that is a convenient wrapper around js_sys.
 
-    // Hotfix: sleep forever if Duration is MAX
-    // In practice, gloo_timers::future::sleep panics if the duration cannot be casted into a u32 in milliseconds.
-    // TODO: divide the duration in u32::MAX chunks and sleep for each chunk
-    if duration == std::time::Duration::MAX {
-        loop {
-            let not_send_future =
-                gloo_timers::future::sleep(std::time::Duration::from_secs_f64(1.0));
-            pretend_future_is_send_on_web(not_send_future).await
-        }
-        return;
-    }
-    let not_send_future = gloo_timers::future::sleep(duration);
-    pretend_future_is_send_on_web(not_send_future).await
+    pretend_future_is_send_on_web(async {
+        // There is two issues here:
+        // - `gloo_timers` requires a u32 as duration in milliseconds which is smaller (~49 days)
+        //   than what `std::time::Duration` can represent (~584,942,417,355 years).
+        // - The Javascript timeout API internally uses a i32 to store the duration in
+        //   milliseconds (~24 days), leading to an instantaneous wake up if `gloo_timers`'s
+        //   u32 represents a value too large.
+        //
+        // Realistically, all sleep are below 24 days so those issues are not a problem...
+        // unless when sleeping forever, since we use `std::time::Duration::MAX` in this case!
+        //
+        // So the solution is to consider everything above 24 days as an infinite sleep.
+        //
+        // see:
+        // - https://developer.mozilla.org/en-US/docs/Web/API/Window/setTimeout#maximum_delay_value0
+        // - https://github.com/rustwasm/gloo/issues/121
+        match i32::try_from(duration.as_millis()) {
+            Ok(millis) => {
+                gloo_timers::future::TimeoutFuture::new(millis as u32).await;
+            }
+            // Sleep is bigger than 24 days, consider it is a sleep forever then !
+            Err(_) => loop {
+                gloo_timers::future::TimeoutFuture::new(i32::MAX as u32).await;
+            },
+        };
+    })
+    .await
 }
 
 #[inline(always)]
