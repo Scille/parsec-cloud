a9abbbc8c95e30557a8e3842871804a36fa780cc
Vincent Michel <vxgmichel@gmail.com>
2025-02-10 23:09:12 +0100

    Bump version 3.3.0-rc.6 -> 3.3.0-rc.7+dev

--- cli/tests/integration/version.rs
+++ cli/tests/integration/version.rs
@@ -2,6 +2,6 @@
 fn version() {
     crate::assert_cmd_success!("--version").stdout(
         // Using `concat!` simplify updating the version using `version-updater`
-        concat!("parsec-cli 3.3.0-rc.6", "\n"),
+        concat!("parsec-cli 3.3.0-rc.7+dev", "\n"),
     );
 }
--- client/electron/assets/electron-publisher-custom.js
+++ client/electron/assets/electron-publisher-custom.js
@@ -2,7 +2,7 @@
 
 const { GitHubPublisher } = require('electron-publish/out/gitHubPublisher');
 
-const VERSION = '3.3.0-rc.6';
+const VERSION = '3.3.0-rc.7+dev';
 
 class CustomGitHubPublisher extends GitHubPublisher {
   /**
