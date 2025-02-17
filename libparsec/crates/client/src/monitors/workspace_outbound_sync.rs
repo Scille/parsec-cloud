f07b2ba8233a0704ad4f8105332f92f3cdcd8629
Vincent Michel <vxgmichel@gmail.com>
2025-02-17 19:29:01 +0100

    Track confined entries in both outbound and inbound sync monitors

--- libparsec/crates/client/src/monitors/workspace_outbound_sync.rs
+++ libparsec/crates/client/src/monitors/workspace_outbound_sync.rs
@@ -1,6 +1,11 @@
 // Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 2016-present Scille SAS
 
-use std::{collections::HashMap, future::Future, pin::pin, sync::Arc};
+use std::{
+    collections::{HashMap, HashSet},
+    future::Future,
+    pin::pin,
+    sync::Arc,
+};
 
 use libparsec_platform_async::{
     channel, event, pretend_future_is_send_on_web, select2_biased, select3_biased, spawn,
@@ -14,6 +19,7 @@ use crate::{
         InboundSyncOutcome, OutboundSyncOutcome, WorkspaceGetNeedOutboundSyncEntriesError,
         WorkspaceOps, WorkspaceSyncError,
     },
+    EventWorkspaceOpsInboundSyncDone,
 };
 
 const WORKSPACE_OUTBOUND_SYNC_MONITOR_NAME: &str = "workspace_outbound_sync";
@@ -48,13 +54,61 @@ pub(crate) async fn start_workspace_outbound_sync_monitor(
     .await
 }
 
+#[derive(Default, Debug)]
+struct ConfinedEntriesTracker {
+    confinement_point_to_confined_entries: HashMap<VlobID, HashSet<VlobID>>,
+    confined_entry_to_confinement_point: HashMap<VlobID, VlobID>,
+}
+
+impl ConfinedEntriesTracker {
+    fn register_confined_entry(&mut self, confinement_point: VlobID, confined_entry: VlobID) {
+        self.confinement_point_to_confined_entries
+            .entry(confinement_point)
+            .or_default()
+            .insert(confined_entry);
+        self.confined_entry_to_confinement_point
+            .insert(confined_entry, confinement_point);
+    }
+
+    fn unregister_confined_entry(&mut self, confined_entry: VlobID) {
+        if let Some(confinement_point) = self
+            .confined_entry_to_confinement_point
+            .remove(&confined_entry)
+        {
+            if let Some(confined_entries) = self
+                .confinement_point_to_confined_entries
+                .get_mut(&confinement_point)
+            {
+                confined_entries.remove(&confined_entry);
+                if confined_entries.is_empty() {
+                    self.confinement_point_to_confined_entries
+                        .remove(&confinement_point);
+                }
+            }
+        }
+    }
+
+    fn get_confined_entries(&self, confinement_point: VlobID) -> Vec<VlobID> {
+        self.confinement_point_to_confined_entries
+            .get(&confinement_point)
+            .map(|confined_entries| confined_entries.iter().copied().collect())
+            .unwrap_or_default()
+    }
+}
+
+#[derive(Debug)]
+enum IncomingEvent {
+    OutboundSyncNeeded { entry_id: VlobID },
+    InboundSyncDone { entry_id: VlobID },
+}
+
 fn task_future_factory(
     workspace_ops: Arc<WorkspaceOps>,
     event_bus: EventBus,
     device: Arc<LocalDevice>,
 ) -> (impl Future<Output = ()>, Box<dyn FnOnce() + Send + 'static>) {
     let realm_id = workspace_ops.realm_id();
-    let (tx, rx) = channel::unbounded::<VlobID>();
+    let (tx, rx) = channel::unbounded::<IncomingEvent>();
 
     let request_stop = event::Event::new();
     let stop_requested = request_stop.listen();
@@ -64,18 +118,37 @@ fn task_future_factory(
         request_stop.notify(usize::MAX);
     });
 
-    let events_connection_lifetime = ({
+    let outbound_sync_needed_events_connection_lifetime = ({
         let tx = tx.clone();
         let realm_id = workspace_ops.realm_id();
         event_bus.connect(move |e: &EventWorkspaceOpsOutboundSyncNeeded| {
             if e.realm_id == realm_id {
-                let _ = tx.send(e.entry_id);
+                let _ = tx.send(IncomingEvent::OutboundSyncNeeded {
+                    entry_id: e.entry_id,
+                });
+            }
+        })
+    },);
+
+    let inbound_sync_needed_events_connection_lifetime = ({
+        let tx = tx.clone();
+        let realm_id = workspace_ops.realm_id();
+        event_bus.connect(move |e: &EventWorkspaceOpsInboundSyncDone| {
+            if e.realm_id == realm_id {
+                let _ = tx.send(IncomingEvent::InboundSyncDone {
+                    entry_id: e.entry_id,
+                });
             }
         })
     },);
 
     let task_future = async move {
-        let _events_connection_lifetime = events_connection_lifetime;
+        let _outbound_sync_needed_events_connection_lifetime =
+            outbound_sync_needed_events_connection_lifetime;
+        let _inbound_sync_needed_events_connection_lifetime =
+            inbound_sync_needed_events_connection_lifetime;
+        let confined_entries_tracker =
+            Arc::new(std::sync::Mutex::new(ConfinedEntriesTracker::default()));
 
         // Start a sub-task that will do the actual synchronization work
 
@@ -89,6 +162,7 @@ fn task_future_factory(
             let workspace_ops = workspace_ops.clone();
             let tx = tx.clone();
             let device = device.clone();
+            let confined_entries_tracker = confined_entries_tracker.clone();
             async move {
                 let mut syncer_stop_requested = pin!(syncer_stop_requested);
                 macro_rules! handle_workspace_sync_error {
@@ -130,7 +204,7 @@ fn task_future_factory(
                                 // Re-enqueue to retry later
                                 let entry_id = $entry_id;
                                 log::info!("Workspace {realm_id}: {entry_id} sync failed due to server block store unavailable, aborting sync and waiting {}s", SERVER_STORE_UNAVAILABLE_WAIT.num_seconds());
-                                let _ = tx.send(entry_id);
+                                let _ = tx.send(IncomingEvent::OutboundSyncNeeded { entry_id });
                                 device
                                     .time_provider
                                     .sleep(SERVER_STORE_UNAVAILABLE_WAIT)
@@ -164,16 +238,29 @@ fn task_future_factory(
                         match outcome {
                             Ok(
                                 OutboundSyncOutcome::Done | OutboundSyncOutcome::EntryIsUnreachable,
-                            ) => break,
-                            // TODO: register the confinement point
-                            Ok(OutboundSyncOutcome::EntryIsConfined(_)) => break,
+                            ) => {
+                                // Unregister the confinement point
+                                confined_entries_tracker
+                                    .lock()
+                                    .expect("Mutex is poisoned")
+                                    .unregister_confined_entry(entry_id);
+                                break;
+                            }
+                            Ok(OutboundSyncOutcome::EntryIsConfined(confinement_point)) => {
+                                // Register the confinement point
+                                confined_entries_tracker
+                                    .lock()
+                                    .expect("Mutex is poisoned")
+                                    .register_confined_entry(confinement_point, entry_id);
+                                break;
+                            }
                             Ok(OutboundSyncOutcome::InboundSyncNeeded) => (),
                             Ok(OutboundSyncOutcome::EntryIsBusy) => {
                                 // Re-enqueue to retry later
                                 // Note the send may fail if the syncer sub task has crashed,
                                 // in which case there is nothing we can do :(
                                 log::info!("Workspace {realm_id}: {entry_id} is busy so aborting sync to retry later");
-                                let _ = tx.send(entry_id);
+                                let _ = tx.send(IncomingEvent::OutboundSyncNeeded { entry_id });
                                 break;
                             }
                             Err(err) => handle_workspace_sync_error!(err, entry_id),
@@ -187,7 +274,7 @@ fn task_future_factory(
                             Ok(InboundSyncOutcome::EntryIsBusy) => {
                                 // Re-enqueue to retry later
                                 log::info!("Workspace {realm_id}: {entry_id} is busy so aborting sync to retry later");
-                                let _ = tx.send(entry_id);
+                                let _ = tx.send(IncomingEvent::OutboundSyncNeeded { entry_id });
                                 break;
                             }
                             Ok(
@@ -249,6 +336,7 @@ fn task_future_factory(
         enum Action {
             Stop,
             NewLocalChange { entry_id: VlobID },
+            NewRemoteChange { entry_id: VlobID },
             DueTimeReached,
         }
 
@@ -279,7 +367,8 @@ fn task_future_factory(
                 _ = &mut stop_requested => Action::Stop,
                 outcome = rx.recv_async() => {
                     match outcome {
-                        Ok(entry_id) => Action::NewLocalChange { entry_id },
+                        Ok(IncomingEvent::InboundSyncDone { entry_id }) => Action::NewRemoteChange { entry_id },
+                        Ok(IncomingEvent::OutboundSyncNeeded { entry_id }) => Action::NewLocalChange { entry_id },
                         Err(_) => Action::Stop,
                     }
                 },
@@ -338,32 +427,46 @@ fn task_future_factory(
                     due_time = to_sync.values().map(|time| time.due_time).min();
                 }
 
-                Action::NewLocalChange { entry_id } => {
+                Action::NewLocalChange { entry_id } | Action::NewRemoteChange { entry_id } => {
+                    // For new local change, schedule the entry for sync.
+                    let mut entries_to_schedule = vec![];
+                    if let Action::NewLocalChange { .. } = action {
+                        entries_to_schedule.push(entry_id);
+                    }
+                    // In both cases, we also need to schedule the confined entries
+                    entries_to_schedule.extend(
+                        confined_entries_tracker
+                            .lock()
+                            .expect("Mutex is poisoned")
+                            .get_confined_entries(entry_id),
+                    );
                     let now = device.now();
-                    let potential_due_time = match to_sync.entry(entry_id) {
-                        std::collections::hash_map::Entry::Vacant(entry) => {
-                            let due_time = now + MIN_SYNC_WAIT;
-                            entry.insert(DueTime {
-                                since: now,
-                                due_time,
-                            });
-                            due_time
-                        }
-                        std::collections::hash_map::Entry::Occupied(mut entry) => {
-                            let e = entry.get_mut();
-                            let new_due_time = now + MIN_SYNC_WAIT;
-                            if new_due_time - e.since < MAX_SYNC_WAIT {
-                                e.due_time = new_due_time;
+                    for entry_id in entries_to_schedule {
+                        let potential_due_time = match to_sync.entry(entry_id) {
+                            std::collections::hash_map::Entry::Vacant(entry) => {
+                                let due_time = now + MIN_SYNC_WAIT;
+                                entry.insert(DueTime {
+                                    since: now,
+                                    due_time,
+                                });
+                                due_time
                             }
-                            e.due_time
+                            std::collections::hash_map::Entry::Occupied(mut entry) => {
+                                let e = entry.get_mut();
+                                let new_due_time = now + MIN_SYNC_WAIT;
+                                if new_due_time - e.since < MAX_SYNC_WAIT {
+                                    e.due_time = new_due_time;
+                                }
+                                e.due_time
+                            }
+                        };
+                        match due_time {
+                            None => due_time = Some(potential_due_time),
+                            Some(current_due_time) if current_due_time > potential_due_time => {
+                                due_time = Some(potential_due_time)
+                            }
+                            _ => (),
                         }
-                    };
-                    match due_time {
-                        None => due_time = Some(potential_due_time),
-                        Some(current_due_time) if current_due_time > potential_due_time => {
-                            due_time = Some(potential_due_time)
-                        }
-                        _ => (),
                     }
                 }
             }
