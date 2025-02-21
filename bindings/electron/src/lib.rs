f2bfb6b4ce978737961c3f343459b7536cb65651
Firelight Flagboy <firelight.flagboy@gmail.com>
2025-02-21 10:36:21 +0100

    feat(binding-electron): Remove sentry init

--- bindings/electron/src/lib.rs
+++ bindings/electron/src/lib.rs
@@ -1,8 +1,6 @@
 // Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 2016-present Scille SAS
 
 use neon::prelude::*;
-use once_cell::sync::OnceCell;
-use sentry::{ClientInitGuard, ClientOptions};
 use std::sync::Mutex;
 
 mod meths;
@@ -12,21 +10,8 @@ lazy_static::lazy_static! {
         Mutex::new(tokio::runtime::Runtime::new().expect("Cannot start tokio runtime for libparsec"));
 }
 
-// TODO: Read from env var
-static SENTRY_DSN_LIBPARSEC: &str =
-    "https://215e9cba2353323a1c5156cf8f845129@o155936.ingest.us.sentry.io/4507644297019392";
-static SENTRY_CLIENT_GUARD: OnceCell<ClientInitGuard> = OnceCell::new();
-
 fn init_sentry() {
-    SENTRY_CLIENT_GUARD.get_or_init(|| {
-        sentry::init((
-            SENTRY_DSN_LIBPARSEC,
-            ClientOptions {
-                release: sentry::release_name!(),
-                ..Default::default()
-            },
-        ))
-    });
+    // Nothing here, just a no-op
 }
 
 #[neon::main]
