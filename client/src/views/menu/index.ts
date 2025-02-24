58b68fe2e9325b94ce9a70db857bca4737094be8
fabienSvstr <fabien.sevestre@my-digital-school.org>
2025-02-24 12:45:18 +0100

    [MS] Added Responsive Menu

--- client/src/views/menu/index.ts
+++ client/src/views/menu/index.ts
@@ -1,5 +1,5 @@
 // Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 2016-present Scille SAS
 
-import { SidebarSavedData } from '@/views/sidebar-menu/utils';
+import { SidebarSavedData } from '@/views/menu/utils';
 
 export { SidebarSavedData };
