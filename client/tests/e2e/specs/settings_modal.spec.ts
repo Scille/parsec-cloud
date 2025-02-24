2988e30ccaf873e5493af87dd1ba763fc81d90db
fabienSvstr <fabien.sevestre@my-digital-school.org>
2025-02-24 10:13:35 +0100

    [MS] Updated tests

--- client/tests/e2e/specs/settings_modal.spec.ts
+++ client/tests/e2e/specs/settings_modal.spec.ts
@@ -7,7 +7,7 @@ async function checkModal(modal: Locator): Promise<void> {
   await expect(modal.locator('.ms-modal-header__title')).toHaveText('Settings');
   const content = modal.locator('.settings-list-container');
   const options = content.locator('.settings-option');
-  await expect(options).toHaveCount(5);
+  await expect(options).toHaveCount(4);
   const lang = options.nth(0);
   await expect(lang.locator('.settings-option__content').locator('.title')).toHaveText('Language');
   await expect(lang.locator('.settings-option__content').locator('.description')).toHaveText('Choose application language');
