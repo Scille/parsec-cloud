568730a4e102cf95819eb3fe013a2415e60d194c
Maxime GRANDCOLAS <maxime.grandcolas@scille.fr>
2025-03-26 10:10:19 +0100

    [MS] Fixed lint

--- client/tests/component/specs/testFileListItem.spec.ts
+++ client/tests/component/specs/testFileListItem.spec.ts
@@ -33,7 +33,7 @@ describe('File List Item', () => {
       baseVersion: 1,
       isPlaceholder: false,
       needSync: false,
-      size: BigInt(43_297_832_478),
+      size: 43_297_832_478,
       name: 'A File.txt',
       path: '/',
       isFile: (): boolean => true,
