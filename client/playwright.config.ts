ae79b9ea2c036f8acb358797bf0f92fb817e0cb6
NicoTuxx <nicolasstempert@gmail.com>
2025-03-28 17:49:47 +0100

    [MS] Organization switch popover web tests

--- client/playwright.config.ts
+++ client/playwright.config.ts
@@ -40,7 +40,8 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
-  testIgnore: ['**/organization_switch_popover.spec.ts'],
+  /* Leaving the empty array to make it easier to ignore tests */
+  testIgnore: [],
   /* Configure projects for major browsers */
   projects: [
     {
