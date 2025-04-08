8a822f5ff58f84f97f0abfa4da6ee4a9f38bf4c5
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2025-04-08 15:00:59 +0200

    Fix typo in `client/src/plugins/libparsec/index.ts`

--- client/src/plugins/libparsec/index.ts
+++ client/src/plugins/libparsec/index.ts
@@ -10,12 +10,12 @@ const _libparsec = registerPlugin<LibParsecPlugin>('LibParsec', {
 
   // In Web we have the whole libparsec compiled in Wasm and provided as a module
   // that we can import just like regular js.
-  // This is cool but has an unexpected side effect: Webpack is going to package
-  // this module event if we are not building the application for web.
+  // This is cool but has an unexpected side effect: this module is going to be
+  // packaged even if we are not building the application for web.
   // Worst: if we are building from a clean repo, the Wasm module doesn't exist
   // at all and the packaging fails :'(
   //
-  // To avoid this we have to make sure the code that contain the import of the
+  // To avoid this we have to make sure the code that contains the import of the
   // Wasm module is only present when building the application for web.
   // We do this by extracting the `LoadWebLibParsecPlugin` into a `trampoline.ts`, this
   // file having two implementation (the real one when building for the web and a dummy
