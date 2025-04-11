5fdc368aafe83f0ca2a59a66fc5d1819383ad91b
Maxime GRANDCOLAS <maxime.grandcolas@scille.fr>
2025-04-11 10:50:51 +0200

    [MS] Retrieve started clients list from libparsec

--- client/src/main.ts
+++ client/src/main.ts
@@ -33,7 +33,6 @@ import {
   ClientEventTag,
   ConnectionHandle,
   Platform,
-  getConnectionInfo,
   getOrganizationHandle,
   isElectron,
   listAvailableDevices,
@@ -58,25 +57,15 @@ enum AppState {
 type LogLevel = 'debug' | 'info' | 'warn' | 'error';
 
 async function parsecEventCallback(handle: ConnectionHandle, event: ClientEvent): Promise<void> {
-  const connInfo = getConnectionInfo(handle);
   const distributor = injectionProvider.getInjections(handle).eventDistributor;
   switch (event.tag) {
     case ClientEventTag.Online:
-      if (connInfo) {
-        connInfo.isOnline = true;
-      }
       distributor.dispatchEvent(Events.Online);
       break;
     case ClientEventTag.Offline:
-      if (connInfo) {
-        connInfo.isOnline = false;
-      }
       distributor.dispatchEvent(Events.Offline);
       break;
     case ClientEventTag.MustAcceptTos:
-      if (connInfo) {
-        connInfo.shouldAcceptTos = true;
-      }
       distributor.dispatchEvent(Events.TOSAcceptRequired, undefined, { delay: 2000 });
       break;
     case ClientEventTag.InvitationChanged:
@@ -115,9 +104,6 @@ async function parsecEventCallback(handle: ConnectionHandle, event: ClientEvent)
       distributor.dispatchEvent(Events.ClientRevoked);
       break;
     case ClientEventTag.ExpiredOrganization:
-      if (connInfo) {
-        connInfo.isExpired = true;
-      }
       distributor.dispatchEvent(Events.ExpiredOrganization);
       break;
     case ClientEventTag.WorkspaceLocallyCreated:
