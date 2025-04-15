fd798abdc4051c3334a87a858b7dd7f9ea8cc256
Firelight Flagboy <firelight.flagboy@gmail.com>
2025-04-15 09:03:59 +0200

    Bump version 3.4.0-a.9+dev -> 3.4.0-a.9

--- docs/conf.py
+++ docs/conf.py
@@ -61,7 +61,7 @@ copyright = "2016-present, Scille SAS"
 # the built documents.
 #
 # The short X.Y version.
-version = "3.4.0-a.9+dev"
+version = "3.4.0-a.9"
 # The full version, including alpha/beta/rc tags.
 release = version
 
