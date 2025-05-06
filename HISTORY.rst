97b5ded4b542369391c87ebc72d96a334fc6e1c2
Firelight Flagboy <firelight.flagboy@gmail.com>
2025-05-06 08:22:36 +0200

    Bump version 3.4.0-a.12+dev -> 3.4.0-a.12

--- HISTORY.rst
+++ HISTORY.rst
@@ -4,6 +4,12 @@ History
 .. towncrier release notes start
 
 
+Parsec v3.4.0-a.12 (2025-05-06)
+-------------------------------
+
+No significant changes.
+
+
 Parsec v3.4.0-a.11 (2025-04-30)
 -------------------------------
 
--- client/electron/assets/electron-publisher-custom.js
+++ client/electron/assets/electron-publisher-custom.js
@@ -2,7 +2,7 @@
 
 const { GitHubPublisher } = require('electron-publish/out/gitHubPublisher');
 
-const VERSION = '3.4.0-a.12+dev';
+const VERSION = '3.4.0-a.12';
 
 class CustomGitHubPublisher extends GitHubPublisher {
   /**
