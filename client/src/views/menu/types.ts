45219abfbfb1ee1a87ded287ddfe4f6a7f323446
Maxime GRANDCOLAS <maxime.grandcolas@scille.fr>
2025-05-07 10:12:25 +0200

    [MS] Generic tab bar menu modal

--- client/src/views/menu/types.ts
+++ client/src/views/menu/types.ts
@@ -1,13 +1,11 @@
 // Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 2016-present Scille SAS
 
-enum AddElementAction {
-  CreateWorkspace,
-  CreateFolder,
-  ImportFolder,
-  ImportFile,
-  ScanDocument,
-  ImportPhoto,
-  AddUser,
+import { Translatable } from 'megashark-lib';
+
+interface MenuAction {
+  icon?: string;
+  label: Translatable;
+  action: any;
 }
 
-export { AddElementAction };
+export { MenuAction };
