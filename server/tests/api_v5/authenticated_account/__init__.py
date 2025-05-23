933f34e1ea1dea3608bbe1e933f34c8a4367b3a0
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2025-05-23 15:09:42 +0200

    Add stub server tests for (to-be-implemented) `vault_item_list/vault_item_recovery_list/vault_item_upload/vault_key_rotation` sever API commands

--- server/tests/api_v5/authenticated_account/__init__.py
+++ server/tests/api_v5/authenticated_account/__init__.py
@@ -1,3 +1,7 @@
 # Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 2016-present Scille SAS
 
 from .test_ping import *  # noqa
+from .test_vault_item_list import *  # noqa
+from .test_vault_item_recovery_list import *  # noqa
+from .test_vault_item_upload import *  # noqa
+from .test_vault_key_rotation import *  # noqa
---
+++ server/tests/api_v5/authenticated_account/test_vault_item_upload.py
@@ -0,0 +1,18 @@
+# Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 2016-present Scille SAS
+
+import pytest
+
+
+@pytest.mark.xfail(reason="TODO: not implemented yet !")
+def test_authenticated_account_vault_item_upload_ok():
+    raise NotImplementedError
+
+
+@pytest.mark.xfail(reason="TODO: not implemented yet !")
+def test_authenticated_account_vault_item_upload_fingerprint_already_exists():
+    raise NotImplementedError
+
+
+@pytest.mark.xfail(reason="TODO: not implemented yet !")
+def test_authenticated_account_vault_item_upload_http_common_errors():
+    raise NotImplementedError
