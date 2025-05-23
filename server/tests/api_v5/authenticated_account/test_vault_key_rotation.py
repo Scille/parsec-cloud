5bda9d76b5bf2c660818367664361fe9ca478428
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2025-05-23 15:09:42 +0200

    Add stub server tests for (to-be-implemented) `vault_item_list/vault_item_recovery_list/vault_item_upload/vault_key_rotation` sever API commands

---
+++ server/tests/api_v5/authenticated_account/test_vault_key_rotation.py
@@ -0,0 +1,13 @@
+# Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 2016-present Scille SAS
+
+import pytest
+
+
+@pytest.mark.xfail(reason="TODO: not implemented yet !")
+def test_authenticated_account_vault_key_rotation_ok():
+    raise NotImplementedError
+
+
+@pytest.mark.xfail(reason="TODO: not implemented yet !")
+def test_authenticated_account_vault_key_rotation_http_common_errors():
+    raise NotImplementedError
