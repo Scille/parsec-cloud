a8d0d357eb4df382797ecdf2705fb674ea51b2b6
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2025-05-25 08:34:58 +0200

    Remove unused `backend` fixture from `test_authenticated_account_ping_ok`

--- server/tests/api_v5/authenticated_account/test_ping.py
+++ server/tests/api_v5/authenticated_account/test_ping.py
@@ -1,13 +1,12 @@
 # Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 2016-present Scille SAS
 
 from parsec._parsec import authenticated_account_cmds
-from tests.common import AuthenticatedAccountRpcClient, Backend, HttpCommonErrorsTester
+from tests.common import AuthenticatedAccountRpcClient, HttpCommonErrorsTester
 
 
 async def test_authenticated_account_ping_ok(
     xfail_if_postgresql: None,
     alice_account: AuthenticatedAccountRpcClient,
-    backend: Backend,
 ) -> None:
     rep = await alice_account.ping(ping="hello")
     assert rep == authenticated_account_cmds.latest.ping.RepOk(pong="hello")
