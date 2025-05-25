2ef1a4cedf92b5cb4978bee51577b4681574a098
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2025-05-25 08:09:42 +0200

    Correct `alice_account` test server fixture to directly pick into the memory datamodel

--- server/tests/common/account.py
+++ server/tests/common/account.py
@@ -11,10 +11,16 @@ import base64
 
 import pytest
 from httpx import AsyncClient
-from pydantic import EmailStr
 
 from parsec._parsec import DateTime, SecretKey
-from parsec.components.auth import AccountPasswordAuthenticationToken
+from parsec.backend import Backend
+from parsec.components.account import PasswordAlgorithmArgon2ID
+from parsec.components.memory.account import MemoryAccountComponent
+from parsec.components.memory.datamodel import (
+    MemoryAccount,
+    MemoryAccountVault,
+    MemoryAuthenticationMethod,
+)
 from tests.common.client import AnonymousAccountRpcClient, AuthenticatedAccountRpcClient
 
 # Note `alice@invalid.com` is Alice's email in `CoolOrg`, `MinimalOrg` etc.
@@ -22,45 +28,35 @@ ALICE_ACCOUNT_EMAIL = "alice@invalid.com"
 ALICE_ACCOUNT_MAC_KEY = SecretKey(base64.b64decode("0UdFSrwcYKyfhAkTdorrLM46+cwHh49XelCMdoxI4qA="))
 
 
-# TODO: remove me once `MemoryAuthComponent.authenticated_account_auth` is implemented !
-@pytest.fixture
-def patch_authenticated_account_auth(
-    monkeypatch: pytest.MonkeyPatch,
-) -> dict[EmailStr, SecretKey]:
-    from parsec.components.auth import (
-        AuthAuthenticatedAccountAuthBadOutcome,
-        AuthenticatedAccountAuthInfo,
-    )
-    from parsec.components.memory.auth import MemoryAuthComponent
-
-    patched_accounts_auth = {}
-
-    async def authenticated_account_auth(
-        self,
-        now: DateTime,
-        token: AccountPasswordAuthenticationToken,
-    ) -> AuthenticatedAccountAuthInfo | AuthAuthenticatedAccountAuthBadOutcome:
-        mac_key = patched_accounts_auth.get(token.email)
-        if not mac_key:
-            return AuthAuthenticatedAccountAuthBadOutcome.ACCOUNT_NOT_FOUND
-        return AuthenticatedAccountAuthInfo(mac_key=mac_key)
-
-    monkeypatch.setattr(
-        MemoryAuthComponent,
-        "authenticated_account_auth",
-        authenticated_account_auth,
-        raising=False,
-    )
-
-    return patched_accounts_auth
-
-
 @pytest.fixture
 async def alice_account(
-    patch_authenticated_account_auth: dict[EmailStr, SecretKey],
+    backend: Backend,
     client: AsyncClient,
 ) -> AuthenticatedAccountRpcClient:
-    patch_authenticated_account_auth[ALICE_ACCOUNT_EMAIL] = ALICE_ACCOUNT_MAC_KEY
+    # TODO: Replace this by a proper call to `backend.account.create_account()` once available
+    assert isinstance(backend.account, MemoryAccountComponent)
+    backend.account._data.accounts[ALICE_ACCOUNT_EMAIL] = MemoryAccount(
+        account_email=ALICE_ACCOUNT_EMAIL,
+        human_label="Alicey McAliceFace",
+        current_vault=MemoryAccountVault(
+            items={},
+            authentication_methods=[
+                MemoryAuthenticationMethod(
+                    created_on=DateTime.now(),
+                    created_by_ip="127.0.0.1",
+                    created_by_user_agent="Parsec-Client/3.4.0 Linux",
+                    mac_key=ALICE_ACCOUNT_MAC_KEY,
+                    vault_key_access=b"<vault_key_access>",
+                    password_secret_algorithm=PasswordAlgorithmArgon2ID(
+                        salt=b"<dummy salt>",
+                        opslimit=65536,
+                        memlimit_kb=3,
+                        parallelism=1,
+                    ),
+                )
+            ],
+        ),
+    )
     return AuthenticatedAccountRpcClient(client, ALICE_ACCOUNT_EMAIL, ALICE_ACCOUNT_MAC_KEY)
 
 
