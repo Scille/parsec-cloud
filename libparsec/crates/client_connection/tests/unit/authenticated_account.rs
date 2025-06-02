e522e88c801bf39f2eeb66b2c961d190b17b2cf3
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2025-06-02 14:14:45 +0200

    Remove `ParsecAuthenticatedAccountAddr`&`ParsecAnonymousAccountAddr` since `ParsecAddr` can be used instead

--- libparsec/crates/client_connection/tests/unit/authenticated_account.rs
+++ libparsec/crates/client_connection/tests/unit/authenticated_account.rs
@@ -31,7 +31,7 @@ async fn ok_with_server(env: &TestbedEnv) {
 async fn ok(env: &TestbedEnv, mocked: bool) {
     // Good request
 
-    let addr = ParsecAuthenticatedAccountAddr::new(env.server_addr.clone());
+    let addr = env.server_addr.clone();
     let auth_method = AccountAuthMethod {
         time_provider: TimeProvider::default(),
         id: AccountAuthMethodID::from_hex("9aae259f748045cc9fe7146eab0b132e").unwrap(),
@@ -108,7 +108,7 @@ async fn invalid_token_with_server(env: &TestbedEnv) {
 async fn invalid_token(env: &TestbedEnv, mocked: bool) {
     // Bad request: invalid account token
 
-    let addr = ParsecAuthenticatedAccountAddr::new(env.server_addr.clone());
+    let addr = env.server_addr.clone();
     let auth_method = AccountAuthMethod {
         id: AccountAuthMethodID::from_hex("9aae259f748045cc9fe7146eab0b132e").unwrap(),
         time_provider: TimeProvider::default(),
@@ -146,9 +146,7 @@ macro_rules! register_rpc_http_hook {
     ($test_name: ident, $response_status_code: expr, $assert_err_cb: expr $(, $header_key:literal : $header_value:expr)* $(,)?) => {
         #[parsec_test(testbed = "minimal")]
         async fn $test_name(env: &TestbedEnv) {
-            let addr = ParsecAuthenticatedAccountAddr::new(
-                env.server_addr.clone(),
-            );
+            let addr = env.server_addr.clone();
             let auth_method = AccountAuthMethod {
                 id: AccountAuthMethodID::from_hex("9aae259f748045cc9fe7146eab0b132e").unwrap(),
                 time_provider: TimeProvider::default(),
