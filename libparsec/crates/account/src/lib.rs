f18520cbb5bb92918427aa45bb1f7169992b69f0
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2025-06-02 12:30:23 +0200

    Implement `libparsec_account::Account` with minimal init

--- libparsec/crates/account/src/lib.rs
+++ libparsec/crates/account/src/lib.rs
@@ -1,1 +1,110 @@
 // Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 2016-present Scille SAS
+
+use std::path::Path;
+
+use libparsec_client_connection::{
+    AccountAuthMethod, AnonymousAccountCmds, AuthenticatedAccountCmds, ConnectionError, ProxyConfig,
+};
+use libparsec_types::prelude::*;
+
+// The auth method master secret is the root secret from which are derived
+// all other data used for authentication and end-2-end encryption:
+// - ID & MAC key: used for server authentication.
+// - Secret key: used for end-2-end encryption.
+pub const AUTH_METHOD_ID_DERIVATION_UUID: uuid::Uuid =
+    uuid::uuid!("00000000-0000-0000-0000-000000000000");
+pub const AUTH_METHOD_MAC_KEY_DERIVATION_UUID: uuid::Uuid =
+    uuid::uuid!("11111111-1111-1111-1111-111111111111");
+pub const AUTH_METHOD_SECRET_KEY_DERIVATION_UUID: uuid::Uuid =
+    uuid::uuid!("22222222-2222-2222-2222-222222222222");
+
+#[derive(Debug)]
+pub struct Account {
+    #[expect(unused)]
+    cmds: AuthenticatedAccountCmds,
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum AccountFromPasswordError {
+    #[error("Server provided an invalid password algorithm config: {0}")]
+    BadPasswordAlgorithm(CryptoError),
+    #[error("Cannot communicate with the server: {0}")]
+    Offline(#[from] ConnectionError),
+    #[error(transparent)]
+    Internal(#[from] anyhow::Error),
+}
+
+impl Account {
+    pub fn new(
+        config_dir: &Path,
+        proxy: ProxyConfig,
+        addr: ParsecAddr,
+        auth_method_master_secret: KeyDerivation,
+    ) -> anyhow::Result<Self> {
+        // Derive from the master secret the data used to authenticate with the server.
+        let auth_method_id = AccountAuthMethodID::from(
+            auth_method_master_secret.derive_uuid_from_uuid(AUTH_METHOD_ID_DERIVATION_UUID),
+        );
+        let auth_method_mac_key = auth_method_master_secret
+            .derive_secret_key_from_uuid(AUTH_METHOD_MAC_KEY_DERIVATION_UUID);
+
+        let auth_method = AccountAuthMethod {
+            time_provider: TimeProvider::default(),
+            id: auth_method_id,
+            mac_key: auth_method_mac_key,
+        };
+        let cmds = AuthenticatedAccountCmds::new(config_dir, addr, proxy, auth_method)
+            .context("Cannot configure server connection")?;
+
+        Ok(Self { cmds })
+    }
+
+    pub async fn from_password(
+        config_dir: &Path,
+        proxy: ProxyConfig,
+        addr: ParsecAddr,
+        email: EmailAddress,
+        password: Password,
+    ) -> Result<Self, AccountFromPasswordError> {
+        // The password algorithm configuration is obtained from the server
+        // to know how to turn the password into `auth_method_master_secret`.
+        let password_algorithm = {
+            let cmds = AnonymousAccountCmds::new(config_dir, addr.clone(), proxy.clone())
+                .context("Cannot configure server connection")?;
+
+            use libparsec_protocol::anonymous_account_cmds::latest::auth_method_password_get_algorithm::{Rep, Req};
+
+            let req = Req { email };
+            let rep = cmds.send(req).await?;
+
+            match rep {
+                Rep::Ok { password_algorithm } => password_algorithm,
+                bad_rep @ Rep::UnknownStatus { .. } => {
+                    return Err(anyhow::anyhow!("Unexpected server response: {:?}", bad_rep).into());
+                }
+            }
+        };
+
+        let auth_method_master_secret = match password_algorithm {
+            PasswordAlgorithm::Argon2id {
+                salt,
+                opslimit,
+                memlimit_kb,
+                parallelism,
+            } => KeyDerivation::from_argon2id_password(
+                &password,
+                &salt,
+                opslimit,
+                memlimit_kb,
+                parallelism,
+            )
+            .map_err(AccountFromPasswordError::BadPasswordAlgorithm)?,
+        };
+
+        Self::new(config_dir, proxy, addr, auth_method_master_secret).map_err(|err| err.into())
+    }
+}
+
+#[cfg(test)]
+#[path = "../tests/unit/account.rs"]
+mod tests;
---
+++ libparsec/crates/account/tests/unit/account.rs
@@ -0,0 +1,114 @@
+// Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 2016-present Scille SAS
+
+// `allow-unwrap-in-test` don't behave as expected, see:
+// https://github.com/rust-lang/rust-clippy/issues/11119
+#![allow(clippy::unwrap_used)]
+
+use libparsec_client_connection::{
+    protocol::anonymous_account_cmds, test_register_sequence_of_send_hooks, ProxyConfig,
+};
+use libparsec_tests_fixtures::prelude::*;
+use libparsec_types::prelude::*;
+
+use crate::{Account, AccountFromPasswordError};
+
+#[parsec_test(testbed = "empty")]
+async fn from_master_secret(env: &TestbedEnv) {
+    let addr = env.server_addr.clone();
+    Account::new(
+        &env.discriminant_dir,
+        ProxyConfig::default(),
+        addr,
+        KeyDerivation::from(hex!(
+            "2ff13803789977db4f8ccabfb6b26f3e70eb4453d396dcb2315f7690cbc2e3f1"
+        )),
+    )
+    .unwrap();
+}
+
+#[parsec_test(testbed = "empty")]
+async fn from_password(env: &TestbedEnv) {
+    let email: EmailAddress = "zack@example.com".parse().unwrap();
+    let password: Password = "P@ssw0rd.".to_string().into();
+
+    test_register_sequence_of_send_hooks!(&env.discriminant_dir, {
+        let email = email.clone();
+        move |req: anonymous_account_cmds::latest::auth_method_password_get_algorithm::Req| {
+            p_assert_eq!(req.email, email);
+            anonymous_account_cmds::latest::auth_method_password_get_algorithm::Rep::Ok {
+                password_algorithm: PasswordAlgorithm::Argon2id {
+                    salt: Bytes::from_static(b"s4lT-s4lt"),
+                    opslimit: 1,
+                    memlimit_kb: 1024,
+                    parallelism: 1,
+                },
+            }
+        }
+    });
+
+    let addr = env.server_addr.clone();
+    Account::from_password(
+        &env.discriminant_dir,
+        ProxyConfig::default(),
+        addr,
+        email,
+        password,
+    )
+    .await
+    .unwrap();
+}
+
+#[parsec_test(testbed = "empty")]
+async fn from_password_server_returns_bad_config(env: &TestbedEnv) {
+    let email: EmailAddress = "zack@example.com".parse().unwrap();
+    let password: Password = "P@ssw0rd.".to_string().into();
+
+    test_register_sequence_of_send_hooks!(&env.discriminant_dir, {
+        let email = email.clone();
+        move |req: anonymous_account_cmds::latest::auth_method_password_get_algorithm::Req| {
+            p_assert_eq!(req.email, email);
+            anonymous_account_cmds::latest::auth_method_password_get_algorithm::Rep::Ok {
+                password_algorithm: PasswordAlgorithm::Argon2id {
+                    salt: Bytes::from_static(b"s4lT-s4lt"),
+                    opslimit: 1,
+                    memlimit_kb: 1, // Too small !
+                    parallelism: 1,
+                },
+            }
+        }
+    });
+
+    let addr = env.server_addr.clone();
+    p_assert_matches!(
+        Account::from_password(
+            &env.discriminant_dir,
+            ProxyConfig::default(),
+            addr,
+            email,
+            password,
+        )
+        .await
+        .unwrap_err(),
+        AccountFromPasswordError::BadPasswordAlgorithm(_)
+    );
+}
+
+#[parsec_test(testbed = "empty")]
+async fn from_password_server_offline(env: &TestbedEnv) {
+    let email: EmailAddress = "zack@example.com".parse().unwrap();
+    let password: Password = "P@ssw0rd.".to_string().into();
+
+    let addr = env.server_addr.clone();
+    p_assert_matches!(
+        Account::from_password(
+            &env.discriminant_dir,
+            ProxyConfig::default(),
+            addr,
+            email,
+            password,
+        )
+        .await
+        .unwrap_err(),
+        AccountFromPasswordError::Offline(_)
+    );
+}
