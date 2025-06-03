fcc7d0c7f797209dc7f9d2a2dc5898a8e10ac836
Firelight Flagboy <firelight.flagboy@gmail.com>
2025-06-03 16:43:24 +0200

    test(account): Refactor fixture to use component method
    Create the alice & bob account use the `MemoryAccountComponent` methods

--- server/tests/common/account.py
+++ server/tests/common/account.py
@@ -12,15 +12,16 @@ import base64
 import pytest
 from httpx import AsyncClient
 
-from parsec._parsec import AccountAuthMethodID, DateTime, EmailAddress, SecretKey
-from parsec.backend import Backend
-from parsec.components.account import PasswordAlgorithmArgon2ID
-from parsec.components.memory.account import MemoryAccountComponent
-from parsec.components.memory.datamodel import (
-    MemoryAccount,
-    MemoryAccountVault,
-    MemoryAuthenticationMethod,
+from parsec._parsec import (
+    AccountAuthMethodID,
+    DateTime,
+    EmailAddress,
+    EmailValidationToken,
+    SecretKey,
 )
+from parsec.backend import Backend
+from parsec.components.account import BaseAccountComponent, PasswordAlgorithmArgon2ID
+from parsec.components.memory.account import MemoryAccountComponent
 from tests.common.client import AnonymousAccountRpcClient, AuthenticatedAccountRpcClient
 
 # Note `alice@invalid.com` is Alice's email in `CoolOrg`, `MinimalOrg` etc.
@@ -40,35 +41,55 @@ BOB_ACCOUNT_AUTH_METHOD_ID = AccountAuthMethodID.from_hex("1ce9bfb763ba4674a11fe
 BOB_ACCOUNT_CREATED_ON = DateTime(2000, 1, 2)
 
 
+async def create_account_with_password(
+    account_component: BaseAccountComponent,
+    account_email: EmailAddress,
+    auth_method_id: AccountAuthMethodID,
+    created_by_ip: str,
+    created_by_user_agent: str,
+    created_on: DateTime,
+    human_label: str,
+    mac_key: SecretKey,
+    password_secret_algorithm: PasswordAlgorithmArgon2ID,
+    vault_key_access: bytes,
+):
+    email_token = await account_component.create_email_validation_token(account_email, created_on)
+    assert isinstance(email_token, EmailValidationToken), email_token
+    res = await account_component.create_account_with_password(
+        token=email_token,
+        now=created_on,
+        mac_key=mac_key,
+        vault_key_access=vault_key_access,
+        human_label=human_label,
+        created_by_ip=created_by_ip,
+        created_by_user_agent=created_by_user_agent,
+        password_secret_algorithm=password_secret_algorithm,
+        auth_method_id=auth_method_id,
+    )
+    assert res is None
+
+
 @pytest.fixture
 async def alice_account(
     backend: Backend,
     client: AsyncClient,
 ) -> AuthenticatedAccountRpcClient:
-    # TODO: Replace this by a proper call to `backend.account.create_account()` once available
     assert isinstance(backend.account, MemoryAccountComponent)
-    assert ALICE_ACCOUNT_EMAIL not in backend.account._data.accounts
-    backend.account._data.accounts[ALICE_ACCOUNT_EMAIL] = MemoryAccount(
+    await create_account_with_password(
+        backend.account,
         account_email=ALICE_ACCOUNT_EMAIL,
+        auth_method_id=ALICE_ACCOUNT_AUTH_METHOD_ID,
+        created_by_ip="127.0.0.1",
+        created_by_user_agent="Parsec-Client/3.4.0 Linux",
+        created_on=ALICE_ACCOUNT_CREATED_ON,
         human_label="Alicey McAliceFace",
-        current_vault=MemoryAccountVault(
-            items={},
-            authentication_methods={
-                ALICE_ACCOUNT_AUTH_METHOD_ID: MemoryAuthenticationMethod(
-                    id=ALICE_ACCOUNT_AUTH_METHOD_ID,
-                    created_on=ALICE_ACCOUNT_CREATED_ON,
-                    created_by_ip="127.0.0.1",
-                    created_by_user_agent="Parsec-Client/3.4.0 Linux",
-                    mac_key=ALICE_ACCOUNT_AUTH_METHOD_MAC_KEY,
-                    vault_key_access=b"<alice_vault_key_access>",
-                    password_secret_algorithm=PasswordAlgorithmArgon2ID(
-                        salt=b"<alice_dummy_salt>",
-                        opslimit=65536,
-                        memlimit_kb=3,
-                        parallelism=1,
-                    ),
-                )
-            },
+        mac_key=ALICE_ACCOUNT_AUTH_METHOD_MAC_KEY,
+        vault_key_access=b"<alice_vault_key_access>",
+        password_secret_algorithm=PasswordAlgorithmArgon2ID(
+            salt=b"<alice_dummy_salt>",
+            opslimit=65536,
+            memlimit_kb=3,
+            parallelism=1,
         ),
     )
     return AuthenticatedAccountRpcClient(
@@ -81,30 +102,22 @@ async def bob_account(
     backend: Backend,
     client: AsyncClient,
 ) -> AuthenticatedAccountRpcClient:
-    # TODO: Replace this by a proper call to `backend.account.create_account()` once available
     assert isinstance(backend.account, MemoryAccountComponent)
-    assert BOB_ACCOUNT_EMAIL not in backend.account._data.accounts
-    backend.account._data.accounts[BOB_ACCOUNT_EMAIL] = MemoryAccount(
+    await create_account_with_password(
+        backend.account,
         account_email=BOB_ACCOUNT_EMAIL,
+        auth_method_id=BOB_ACCOUNT_AUTH_METHOD_ID,
+        created_by_ip="127.0.0.1",
+        created_by_user_agent="Parsec-Client/3.4.0 Linux",
+        created_on=BOB_ACCOUNT_CREATED_ON,
         human_label="Boby McBobFace",
-        current_vault=MemoryAccountVault(
-            items={},
-            authentication_methods={
-                BOB_ACCOUNT_AUTH_METHOD_ID: MemoryAuthenticationMethod(
-                    id=BOB_ACCOUNT_AUTH_METHOD_ID,
-                    created_on=BOB_ACCOUNT_CREATED_ON,
-                    created_by_ip="127.0.0.1",
-                    created_by_user_agent="Parsec-Client/3.4.0 Linux",
-                    mac_key=BOB_ACCOUNT_AUTH_METHOD_MAC_KEY,
-                    vault_key_access=b"<bob_vault_key_access>",
-                    password_secret_algorithm=PasswordAlgorithmArgon2ID(
-                        salt=b"<bob_dummy_salt>",
-                        opslimit=65536,
-                        memlimit_kb=3,
-                        parallelism=1,
-                    ),
-                )
-            },
+        mac_key=BOB_ACCOUNT_AUTH_METHOD_MAC_KEY,
+        vault_key_access=b"<bob_vault_key_access>",
+        password_secret_algorithm=PasswordAlgorithmArgon2ID(
+            salt=b"<bob_dummy_salt>",
+            opslimit=65536,
+            memlimit_kb=3,
+            parallelism=1,
         ),
     )
     return AuthenticatedAccountRpcClient(
