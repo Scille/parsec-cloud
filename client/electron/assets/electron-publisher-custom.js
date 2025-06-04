3626ad8c8259e373edc48cd1fa209d3d03757c11
Firelight Flagboy <firelight.flagboy@gmail.com>
2025-06-04 15:13:58 +0200

    Bump version 3.4.0-rc.6 -> 3.4.0-rc.7+dev

--- client/electron/assets/electron-publisher-custom.js
+++ client/electron/assets/electron-publisher-custom.js
@@ -2,7 +2,7 @@
 
 const { GitHubPublisher } = require('electron-publish/out/gitHubPublisher');
 
-const VERSION = '3.4.0-rc.6';
+const VERSION = '3.4.0-rc.7+dev';
 
 class CustomGitHubPublisher extends GitHubPublisher {
   /**
--- docs/conf.py
+++ docs/conf.py
@@ -61,7 +61,7 @@ copyright = "2016-present, Scille SAS"
 # the built documents.
 #
 # The short X.Y version.
-version = "3.4.0-rc.6"
+version = "3.4.0-rc.7+dev"
 # The full version, including alpha/beta/rc tags.
 release = version
 
