b08010b159c84e198c0a9b871937e617c57fe200
Firelight Flagboy <firelight.flagboy@gmail.com>
2025-06-05 10:27:31 +0200

    feat(libparsec): Add `EmailDeletionToken`

--- libparsec/crates/types/src/token.rs
+++ libparsec/crates/types/src/token.rs
@@ -133,6 +133,7 @@ macro_rules! new_token_type {
 new_token_type!(BootstrapToken);
 new_token_type!(InvitationToken);
 new_token_type!(EmailValidationToken);
+new_token_type!(AccountDeletionToken);
 
 #[cfg(test)]
 #[path = "../tests/unit/token.rs"]
--- server/src/token.rs
+++ server/src/token.rs
@@ -80,3 +80,15 @@ crate::binding_utils::gen_py_wrapper_class_for_id!(
     __hash__,
 );
 gen_token!(EmailValidationToken);
+
+crate::binding_utils::gen_py_wrapper_class_for_id!(
+    AccountDeletionToken,
+    libparsec_types::AccountDeletionToken,
+    __repr__,
+    __copy__,
+    __deepcopy__,
+    __str__,
+    __richcmp__ eq,
+    __hash__,
+);
+gen_token!(AccountDeletionToken);
