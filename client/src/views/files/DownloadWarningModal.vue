9adbdc44164c3eb5e1c564d4dcf9c32c3b2dcd0c
fabienSvstr <fabien.sevestre@my-digital-school.org>
2025-06-20 15:26:44 +0200

    [MS] Updated style + including warning download image

--- client/src/views/files/DownloadWarningModal.vue
+++ client/src/views/files/DownloadWarningModal.vue
@@ -6,21 +6,28 @@
     :close-button="{ visible: true }"
     :cancel-button="{
       disabled: false,
-      label: 'CANCEL',
+      label: 'FoldersPage.DownloadFile.cancel',
     }"
     :confirm-button="{
-      label: 'DOWNLOAD',
+      label: 'FoldersPage.DownloadFile.download',
       disabled: false,
       theme: MsReportTheme.Success,
       onClick: onConfirmClicked,
     }"
   >
-    <div class="download-warning-content">
-      {{ $msTranslate('FoldersPage.DownloadFile.downloadWarningText') }}
+    <div class="download-warning">
+      <ms-report-text :theme="MsReportTheme.Warning">
+        {{ $msTranslate('FoldersPage.DownloadFile.downloadWarningText') }}
+      </ms-report-text>
+      <ms-image
+        :image="DownloadWarningImage"
+        class="download-warning-image"
+      />
       <i18n-t
         keypath="FoldersPage.DownloadFile.downloadWarningDocMessage"
         tag="label"
         for="FoldersPage.DownloadFile.downloadWarningDoc"
+        class="download-warning-documentation body"
       >
         <a
           class="doc-link"
@@ -29,24 +36,24 @@
           {{ $msTranslate('FoldersPage.DownloadFile.downloadWarningDoc') }}
         </a>
       </i18n-t>
-
-      <ms-checkbox
-        v-model="noReminder"
-        label-placement="end"
-        justify="start"
-        class="tos-checkbox body"
-      >
-        {{ $msTranslate('Do not remind me') }}
-      </ms-checkbox>
     </div>
+    <ms-checkbox
+      v-model="noReminder"
+      label-placement="end"
+      justify="start"
+      class="download-warning-checkbox body"
+    >
+      {{ $msTranslate('FoldersPage.DownloadFile.notRemindMe') }}
+    </ms-checkbox>
   </ms-modal>
 </template>
 
 <script setup lang="ts">
-import { IonButton, IonIcon, modalController } from '@ionic/vue';
+import { modalController } from '@ionic/vue';
 import { onMounted, ref } from 'vue';
-import { MsReportTheme, MsModal, MsCheckbox, MsModalResult } from 'megashark-lib';
+import { MsReportTheme, MsModal, MsCheckbox, MsModalResult, MsReportText, MsImage } from 'megashark-lib';
 import { Env } from '@/services/environment';
+import DownloadWarningImage from '@/assets/images/warning-download.svg';
 
 const noReminder = ref(false);
 
@@ -62,14 +69,47 @@ async function onConfirmClicked(): Promise<boolean> {
 </script>
 
 <style scoped lang="scss">
-.download-warning-content {
-  margin-top: 1rem;
+.download-warning {
+  margin-top: 0.5rem;
   display: flex;
   flex-direction: column;
-  gap: 1.5rem;
-}
+  gap: 1rem;
+  position: relative;
 
+  &-image {
+    width: 100%;
+    max-width: 100%;
+    height: auto;
+    margin: 0 auto;
+  }
+
+  &-documentation {
+    color: var(--parsec-color-light-secondary-hard-grey);
+  }
+
+  &-checkbox {
+    position: absolute;
+    bottom: 2.5rem;
+    color: var(--parsec-color-light-secondary-soft-text);
+
+    @include ms.responsive-breakpoint('sm') {
+      bottom: 3.5rem;
+    }
+
+    @include ms.responsive-breakpoint('xs') {
+      position: relative;
+      bottom: 0;
+      margin-top: 1rem;
+    }
+  }
+}
 .doc-link {
   cursor: pointer;
+  text-decoration: underline;
+  font-weight: 500;
+
+  &:hover {
+    color: var(--parsec-color-light-primary-600);
+  }
 }
 </style>
