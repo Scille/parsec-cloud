10301a52649637885b47cba439297bbdaf0f88c5
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2025-06-23 15:41:53 +0200

    Rework `libparsec_account::Account` login methods

--- libparsec/crates/account/tests/unit/account.rs
+++ libparsec/crates/account/tests/unit/account.rs
@@ -5,17 +5,27 @@
 #![allow(clippy::unwrap_used)]
 
 use libparsec_client_connection::{
-    protocol::anonymous_account_cmds, test_register_sequence_of_send_hooks, ProxyConfig,
+    protocol::{anonymous_account_cmds, authenticated_account_cmds},
+    test_register_sequence_of_send_hooks, ProxyConfig,
 };
 use libparsec_tests_fixtures::prelude::*;
 use libparsec_types::prelude::*;
 
-use crate::{Account, AccountFromPasswordError};
+use crate::{Account, AccountLoginWithMasterSecretError, AccountLoginWithPasswordError};
 
 #[parsec_test(testbed = "empty")]
-async fn from_master_secret(env: &TestbedEnv) {
+async fn login_with_master_secret(env: &TestbedEnv) {
     let addr = env.server_addr.clone();
-    Account::new(
+    let human_handle: HumanHandle = "Zack <zack@example.com>".parse().unwrap();
+
+    test_register_sequence_of_send_hooks!(&env.discriminant_dir, {
+        let human_handle = human_handle.clone();
+        move |_req: authenticated_account_cmds::latest::account_info::Req| {
+            authenticated_account_cmds::latest::account_info::Rep::Ok { human_handle }
+        }
+    });
+
+    let account = Account::login_with_master_secret(
         &env.discriminant_dir,
         ProxyConfig::default(),
         addr,
@@ -23,30 +33,43 @@ async fn from_master_secret(env: &TestbedEnv) {
             "2ff13803789977db4f8ccabfb6b26f3e70eb4453d396dcb2315f7690cbc2e3f1"
         )),
     )
+    .await
     .unwrap();
+
+    p_assert_eq!(*account.human_handle(), human_handle,);
 }
 
 #[parsec_test(testbed = "empty")]
-async fn from_password(env: &TestbedEnv) {
+async fn login_with_password(env: &TestbedEnv) {
     let email: EmailAddress = "zack@example.com".parse().unwrap();
     let password: Password = "P@ssw0rd.".to_string().into();
+    let human_handle: HumanHandle = "Zack <zack@example.com>".parse().unwrap();
 
-    test_register_sequence_of_send_hooks!(&env.discriminant_dir, {
-        let email = email.clone();
-        move |req: anonymous_account_cmds::latest::auth_method_password_get_algorithm::Req| {
-            p_assert_eq!(req.email, email);
-            anonymous_account_cmds::latest::auth_method_password_get_algorithm::Rep::Ok {
-                password_algorithm: UntrustedPasswordAlgorithm::Argon2id {
-                    opslimit: 3,
-                    memlimit_kb: 128 * 1024,
-                    parallelism: 1,
-                },
+    test_register_sequence_of_send_hooks!(
+        &env.discriminant_dir,
+        {
+            let email = email.clone();
+            move |req: anonymous_account_cmds::latest::auth_method_password_get_algorithm::Req| {
+                p_assert_eq!(req.email, email);
+                anonymous_account_cmds::latest::auth_method_password_get_algorithm::Rep::Ok {
+                    password_algorithm: UntrustedPasswordAlgorithm::Argon2id {
+                        opslimit: 3,
+                        memlimit_kb: 128 * 1024,
+                        parallelism: 1,
+                    },
+                }
+            }
+        },
+        {
+            let human_handle = human_handle.clone();
+            move |_req: authenticated_account_cmds::latest::account_info::Req| {
+                authenticated_account_cmds::latest::account_info::Rep::Ok { human_handle }
             }
         }
-    });
+    );
 
     let addr = env.server_addr.clone();
-    Account::from_password(
+    let account = Account::login_with_password(
         &env.discriminant_dir,
         ProxyConfig::default(),
         addr,
@@ -55,10 +78,12 @@ async fn from_password(env: &TestbedEnv) {
     )
     .await
     .unwrap();
+
+    p_assert_eq!(*account.human_handle(), human_handle,);
 }
 
 #[parsec_test(testbed = "empty")]
-async fn from_password_server_returns_bad_config(env: &TestbedEnv) {
+async fn login_with_password_server_returns_bad_config(env: &TestbedEnv) {
     let email: EmailAddress = "zack@example.com".parse().unwrap();
     let password: Password = "P@ssw0rd.".to_string().into();
 
@@ -78,7 +103,7 @@ async fn from_password_server_returns_bad_config(env: &TestbedEnv) {
 
     let addr = env.server_addr.clone();
     p_assert_matches!(
-        Account::from_password(
+        Account::login_with_password(
             &env.discriminant_dir,
             ProxyConfig::default(),
             addr,
@@ -87,18 +112,18 @@ async fn from_password_server_returns_bad_config(env: &TestbedEnv) {
         )
         .await
         .unwrap_err(),
-        AccountFromPasswordError::BadPasswordAlgorithm(_)
+        AccountLoginWithPasswordError::BadPasswordAlgorithm(_)
     );
 }
 
 #[parsec_test(testbed = "empty")]
-async fn from_password_server_offline(env: &TestbedEnv) {
+async fn login_with_password_server_offline(env: &TestbedEnv) {
     let email: EmailAddress = "zack@example.com".parse().unwrap();
     let password: Password = "P@ssw0rd.".to_string().into();
 
     let addr = env.server_addr.clone();
     p_assert_matches!(
-        Account::from_password(
+        Account::login_with_password(
             &env.discriminant_dir,
             ProxyConfig::default(),
             addr,
@@ -107,6 +132,24 @@ async fn from_password_server_offline(env: &TestbedEnv) {
         )
         .await
         .unwrap_err(),
-        AccountFromPasswordError::Offline(_)
+        AccountLoginWithPasswordError::Offline(_)
+    );
+}
+
+#[parsec_test(testbed = "empty")]
+async fn login_with_master_secret_server_offline(env: &TestbedEnv) {
+    let addr = env.server_addr.clone();
+    p_assert_matches!(
+        Account::login_with_master_secret(
+            &env.discriminant_dir,
+            ProxyConfig::default(),
+            addr,
+            KeyDerivation::from(hex!(
+                "2ff13803789977db4f8ccabfb6b26f3e70eb4453d396dcb2315f7690cbc2e3f1"
+            )),
+        )
+        .await
+        .unwrap_err(),
+        AccountLoginWithMasterSecretError::Offline(_)
     );
 }
