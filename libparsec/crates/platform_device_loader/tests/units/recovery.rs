984f444c3295fb430e8c7281fd97f09ebce98e39
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2025-06-26 17:09:38 +0200

    Rework `libparsec_platform_device_loader` so that it is not responsible for generating the new device when importing a recovery device

--- libparsec/crates/platform_device_loader/tests/units/recovery.rs
+++ libparsec/crates/platform_device_loader/tests/units/recovery.rs
@@ -2,13 +2,13 @@
 
 #![allow(clippy::unwrap_used)]
 
-use crate::{export_recovery_device, import_recovery_device, PlatformImportRecoveryDeviceError};
+use crate::{dump_recovery_device, load_recovery_device, LoadRecoveryDeviceError};
 use libparsec_tests_lite::p_assert_matches;
 use libparsec_tests_lite::parsec_test;
 use libparsec_types::HumanHandle;
+use libparsec_types::LocalDevice;
 use libparsec_types::ParsecOrganizationAddr;
 use libparsec_types::UserProfile;
-use libparsec_types::{DeviceLabel, LocalDevice};
 
 #[parsec_test]
 async fn test_ok_recovery() {
@@ -30,39 +30,33 @@ async fn test_ok_recovery() {
         None,
         None,
     );
-    let recovery_device_label = DeviceLabel::default();
-    let (secret_passphrase, data, exported_recovery_device) =
-        export_recovery_device(&local_device, recovery_device_label.clone()).await;
-    let imported_recovery_device = import_recovery_device(&data, secret_passphrase.clone())
-        .await
-        .unwrap();
+    let (secret_passphrase, data) = dump_recovery_device(&local_device);
+    let imported_recovery_device = load_recovery_device(&data, secret_passphrase.clone()).unwrap();
 
-    assert_eq!(exported_recovery_device, imported_recovery_device);
-    assert_eq!(imported_recovery_device.device_label, recovery_device_label);
+    assert_eq!(*imported_recovery_device, local_device);
 
     // It's convenient to also test the import bad cases here
 
     // Invalid data
     p_assert_matches!(
-        import_recovery_device(&b"dummy"[..], secret_passphrase.clone()).await,
-        Err(PlatformImportRecoveryDeviceError::InvalidData)
+        load_recovery_device(&b"dummy"[..], secret_passphrase.clone()),
+        Err(LoadRecoveryDeviceError::InvalidData)
     );
 
     // Invalid passphrase
 
     p_assert_matches!(
-        import_recovery_device(&data, "dummy".into()).await,
-        Err(PlatformImportRecoveryDeviceError::InvalidPassphrase)
+        load_recovery_device(&data, "dummy".into()),
+        Err(LoadRecoveryDeviceError::InvalidPassphrase)
     );
 
     // Decryption failed
 
     p_assert_matches!(
-        import_recovery_device(
+        load_recovery_device(
             &data,
             libparsec_types::SecretKey::generate_recovery_passphrase().0
-        )
-        .await,
-        Err(PlatformImportRecoveryDeviceError::DecryptionFailed)
+        ),
+        Err(LoadRecoveryDeviceError::DecryptionFailed)
     );
 }
