33c1719199a542fc7bdd2aeb297a574dff44a243
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2025-06-27 16:01:37 +0200

    Improve `libparsec_account` account creation methods & related tests

--- libparsec/crates/account/src/lib.rs
+++ libparsec/crates/account/src/lib.rs
@@ -1,21 +1,17 @@
 // Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 2016-present Scille SAS
 
 use std::{collections::HashSet, path::PathBuf, sync::Arc};
-use std::{path::Path, str::FromStr};
 
-use libparsec_client_connection::{
-    AccountAuthMethod, AnonymousAccountCmds, AuthenticatedAccountCmds, ConnectionError, ProxyConfig,
-};
-use libparsec_protocol::anonymous_account_cmds::v5::{
-    account_create_proceed, account_create_send_validation_email,
-};
+use libparsec_client_connection::{AnonymousAccountCmds, AuthenticatedAccountCmds, ProxyConfig};
 use libparsec_types::prelude::*;
 
+mod account_create;
 mod fetch_list_registration_devices;
 mod list_invitations;
 mod login;
 mod register_new_device;
 
+pub use account_create::*;
 pub use fetch_list_registration_devices::*;
 pub use list_invitations::*;
 pub use login::*;
@@ -42,23 +38,58 @@ pub struct Account {
     registration_devices_cache: Vec<Arc<LocalDevice>>,
 }
 
-#[derive(Debug, thiserror::Error)]
-pub enum AccountSendEmailValidationTokenError {
-    #[error("Email recipient refused")]
-    EmailRecipientRefused,
-    #[error("Email server unavailable")]
-    EmailServerUnavailable,
-    #[error("Unable to parse email")]
-    EmailParseError(#[from] EmailAddressParseError),
-    #[error("Component has stopped")]
-    Stopped,
-    #[error("Cannot communicate with the server: {0}")]
-    Offline(#[from] ConnectionError),
-    #[error(transparent)]
-    Internal(#[from] anyhow::Error),
-}
-
 impl Account {
+    /// To create a new account, the user's email address must first be validated.
+    /// This method ask the server to send an email containing a validation code.
+    pub async fn create_1_send_validation_email(
+        cmds: &AnonymousAccountCmds,
+        email: EmailAddress,
+    ) -> Result<(), AccountCreateSendValidationEmailError> {
+        account_create_send_validation_email(cmds, email).await
+    }
+
+    /// Since the validation code is supposed to be copied by the user from his
+    /// email client to the Parsec client by hand, this method can be used (but
+    /// is not mandatory, `create_3_proceed` can also be called right away!) to
+    /// ensure the validation code + email couple is valid and can lead.
+    ///
+    /// Also note that calling too many time this method with an invalid
+    /// validation code leads to a full invalidation of the attempt and requires
+    /// a new validation email to be sent.
+    pub async fn create_2_check_validation_code(
+        cmds: &AnonymousAccountCmds,
+        validation_code: ValidationCode,
+        email: EmailAddress,
+    ) -> Result<(), AccountCreateError> {
+        account_create(
+            cmds,
+            AccountCreateStep::CheckCode {
+                validation_code,
+                email,
+            },
+        )
+        .await
+    }
+
+    /// Similar to `create_2_check_validation_code`, but this time we actually want
+    /// to create the account!
+    pub async fn create_3_proceed(
+        cmds: &AnonymousAccountCmds,
+        validation_code: ValidationCode,
+        human_handle: HumanHandle,
+        password: Password,
+    ) -> Result<(), AccountCreateError> {
+        account_create(
+            cmds,
+            AccountCreateStep::Proceed {
+                human_handle,
+                password,
+                validation_code,
+            },
+        )
+        .await
+    }
+
     /// Convenient helper to avoid the first request (fetching the human handle)
     /// whenever we need an `Account` instance in the tests.
     #[cfg(test)]
@@ -143,143 +174,5 @@ impl Account {
             save_strategy,
         )
         .await
-    }
-}
-
-pub async fn account_create_send_validation_email(
-    email: &str,
-    config_dir: &Path,
-    addr: ParsecAddr,
-) -> Result<(), AccountSendEmailValidationTokenError> {
-    let cmds = AnonymousAccountCmds::new(config_dir, addr, ProxyConfig::default())?;
-    let email = EmailAddress::from_str(email)?;
-
-    match cmds
-        .send(account_create_send_validation_email::Req { email })
-        .await?
-    {
-        account_create_send_validation_email::Rep::Ok => Ok(()),
-
-        account_create_send_validation_email::Rep::EmailRecipientRefused => {
-            Err(AccountSendEmailValidationTokenError::EmailRecipientRefused)
-        }
-        account_create_send_validation_email::Rep::EmailServerUnavailable => {
-            Err(AccountSendEmailValidationTokenError::EmailServerUnavailable)
-        }
-        bad_rep @ account_create_send_validation_email::Rep::UnknownStatus { .. } => {
-            Err(anyhow::anyhow!("Unexpected server response: {:?}", bad_rep).into())
-        }
-    }
-}
-
-#[derive(Debug, thiserror::Error)]
-pub enum AccountCreateProceedError {
-    #[error("Component has stopped")]
-    Stopped,
-    #[error("Cannot communicate with the server: {0}")]
-    Offline(#[from] ConnectionError),
-    #[error(transparent)]
-    Internal(#[from] anyhow::Error),
-    #[error("Invalid validation code")]
-    InvalidValidationCode,
-    #[error("Auth method id already exists")]
-    AuthMethodIdAlreadyExists,
-    #[error(transparent)]
-    CryptoError(#[from] CryptoError),
-}
-
-pub enum AccountCreateStep {
-    CheckCode {
-        validation_code: ValidationCode,
-        email: EmailAddress,
-    },
-    Create {
-        human_handle: HumanHandle,
-        password: Password,
-        validation_code: ValidationCode,
-    },
-}
-
-pub async fn account_create_proceed(
-    step: AccountCreateStep,
-    config_dir: &Path,
-    addr: ParsecAddr,
-) -> Result<(), AccountCreateProceedError> {
-    let cmds = AnonymousAccountCmds::new(config_dir, addr, ProxyConfig::default())?;
-
-    let req_step = match step {
-        AccountCreateStep::CheckCode {
-            validation_code,
-            email,
-        } => account_create_proceed::AccountCreateStep::Number0CheckCode {
-            validation_code,
-            email,
-        },
-        AccountCreateStep::Create {
-            human_handle,
-            password,
-            validation_code,
-        } => {
-            let memlimit_kb = 1024;
-            let parallelism = 1;
-            let opslimit = 1;
-
-            let auth_method_password_algorithm = UntrustedPasswordAlgorithm::Argon2id {
-                opslimit,
-                memlimit_kb,
-                parallelism,
-            };
-
-            let trusted_password_algorithm = auth_method_password_algorithm
-                .clone()
-                .validate(human_handle.email().as_ref())?;
-
-            let auth_method_master_secret =
-                trusted_password_algorithm.compute_key_derivation(&password)?;
-
-            let auth_method_secret_key = auth_method_master_secret
-                .derive_secret_key_from_uuid(AUTH_METHOD_SECRET_KEY_DERIVATION_UUID);
-
-            let auth_method = AccountAuthMethod {
-                time_provider: TimeProvider::default(),
-                id: AccountAuthMethodID::from(
-                    auth_method_master_secret.derive_uuid_from_uuid(AUTH_METHOD_ID_DERIVATION_UUID),
-                ),
-                mac_key: auth_method_master_secret
-                    .derive_secret_key_from_uuid(AUTH_METHOD_MAC_KEY_DERIVATION_UUID),
-            };
-            let vault_key = SecretKey::generate();
-
-            let vault_key_access = AccountVaultKeyAccess { vault_key };
-
-            let vault_key_access_bytes = vault_key_access.dump_and_encrypt(&auth_method_secret_key);
-
-            account_create_proceed::AccountCreateStep::Number1Create {
-                validation_code,
-                auth_method_hmac_key: auth_method.mac_key,
-                auth_method_id: auth_method.id,
-                auth_method_password_algorithm: Some(auth_method_password_algorithm),
-                human_handle,
-                vault_key_access: vault_key_access_bytes.into(),
-            }
-        }
-    };
-
-    match cmds
-        .send(account_create_proceed::Req {
-            account_create_step: req_step,
-        })
-        .await?
-    {
-        account_create_proceed::Rep::Ok => Ok(()),
-        account_create_proceed::Rep::AuthMethodIdAlreadyExists => {
-            Err(AccountCreateProceedError::AuthMethodIdAlreadyExists)
-        }
-        account_create_proceed::Rep::InvalidValidationCode => {
-            Err(AccountCreateProceedError::InvalidValidationCode)
-        }
-        bad_rep @ account_create_proceed::Rep::UnknownStatus { .. } => {
-            Err(anyhow::anyhow!("Unexpected server response: {:?}", bad_rep).into())
-        }
     }
 }
---
+++ libparsec/crates/account/tests/unit/account_create/create.rs
@@ -0,0 +1,224 @@
+// Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 2016-present Scille SAS
+
+// `allow-unwrap-in-test` don't behave as expected, see:
+// https://github.com/rust-lang/rust-clippy/issues/11119
+#![allow(clippy::unwrap_used)]
+
+use std::{
+    collections::HashMap,
+    sync::{Arc, Mutex},
+};
+
+use libparsec_client_connection::{
+    protocol::{anonymous_account_cmds, authenticated_account_cmds},
+    test_register_sequence_of_send_hooks, AnonymousAccountCmds, ProxyConfig,
+};
+use libparsec_tests_fixtures::prelude::*;
+use libparsec_types::prelude::*;
+
+use crate::{Account, AccountCreateError};
+
+#[parsec_test(testbed = "empty")]
+async fn ok(env: &TestbedEnv) {
+    let human_handle: HumanHandle = "Zack <zack@example.com>".parse().unwrap();
+    let password: Password = "P@ssw0rd.".to_string().into();
+    let validation_code: ValidationCode = "AD3FXJ".parse().unwrap();
+    let cmds = AnonymousAccountCmds::new(
+        &env.discriminant_dir,
+        env.server_addr.clone(),
+        ProxyConfig::default(),
+    )
+    .unwrap();
+
+    let retreived_stuff = Arc::new(Mutex::new(None));
+
+    test_register_sequence_of_send_hooks!(&env.discriminant_dir, {
+        let expected_human_handle = human_handle.clone();
+        let expected_validation_code = validation_code.clone();
+        let retreived_stuff = retreived_stuff.clone();
+        move |req: anonymous_account_cmds::latest::account_create_proceed::Req| {
+            match req.account_create_step {
+                anonymous_account_cmds::latest::account_create_proceed::AccountCreateStep::Number1Create {
+                    human_handle,
+                    validation_code,
+                    auth_method_password_algorithm,
+                    auth_method_id,
+                    auth_method_hmac_key,
+                    vault_key_access,
+                } => {
+                    p_assert_eq!(human_handle, expected_human_handle);
+                    p_assert_eq!(validation_code, expected_validation_code);
+
+                    retreived_stuff.lock().unwrap().replace((
+                        auth_method_password_algorithm,
+                        auth_method_id,
+                        auth_method_hmac_key,
+                        vault_key_access,
+                    ));
+                }
+                unknown => unreachable!("{:?}", unknown)
+            }
+            anonymous_account_cmds::latest::account_create_proceed::Rep::Ok
+        }
+    });
+
+    p_assert_matches!(
+        Account::create_3_proceed(
+            &cmds,
+            validation_code,
+            human_handle.clone(),
+            password.clone()
+        )
+        .await,
+        Ok(())
+    );
+
+    // Now try the connection
+
+    let (auth_method_password_algorithm, _auth_method_id, _auth_method_hmac_key, vault_key_access) =
+        retreived_stuff.lock().unwrap().take().unwrap();
+
+    test_register_sequence_of_send_hooks!(
+        &env.discriminant_dir,
+        {
+            move |_req: anonymous_account_cmds::latest::auth_method_password_get_algorithm::Req| {
+                anonymous_account_cmds::latest::auth_method_password_get_algorithm::Rep::Ok {
+                    password_algorithm: auth_method_password_algorithm.unwrap(),
+                }
+            }
+        },
+        {
+            let human_handle = human_handle.clone();
+            move |_req: authenticated_account_cmds::latest::account_info::Req| {
+                authenticated_account_cmds::latest::account_info::Rep::Ok { human_handle }
+            }
+        }
+    );
+
+    let account = Account::login_with_password(
+        env.discriminant_dir.clone(),
+        ProxyConfig::default(),
+        env.server_addr.clone(),
+        human_handle.email().to_owned(),
+        password,
+    )
+    .await
+    .unwrap();
+
+    test_register_sequence_of_send_hooks!(
+        &env.discriminant_dir,
+        move |_req: authenticated_account_cmds::latest::vault_item_list::Req| {
+            authenticated_account_cmds::latest::vault_item_list::Rep::Ok {
+                items: HashMap::new(),
+                key_access: vault_key_access,
+            }
+        }
+    );
+
+    account.fetch_registration_devices().await.unwrap();
+}
+
+#[parsec_test(testbed = "empty")]
+async fn offline(env: &TestbedEnv) {
+    let human_handle: HumanHandle = "Zack <zack@example.com>".parse().unwrap();
+    let password = "P@ssw0rd.".to_string().into();
+    let validation_code: ValidationCode = "AD3FXJ".parse().unwrap();
+    let cmds = AnonymousAccountCmds::new(
+        &env.discriminant_dir,
+        env.server_addr.clone(),
+        ProxyConfig::default(),
+    )
+    .unwrap();
+
+    p_assert_matches!(
+        Account::create_3_proceed(&cmds, validation_code, human_handle, password)
+            .await
+            .unwrap_err(),
+        AccountCreateError::Offline(_)
+    );
+}
+
+#[parsec_test(testbed = "empty")]
+async fn unknown_status(env: &TestbedEnv) {
+    let human_handle: HumanHandle = "Zack <zack@example.com>".parse().unwrap();
+    let password = "P@ssw0rd.".to_string().into();
+    let validation_code: ValidationCode = "AD3FXJ".parse().unwrap();
+    let cmds = AnonymousAccountCmds::new(
+        &env.discriminant_dir,
+        env.server_addr.clone(),
+        ProxyConfig::default(),
+    )
+    .unwrap();
+
+    test_register_sequence_of_send_hooks!(
+        &env.discriminant_dir,
+        move |_req: anonymous_account_cmds::latest::account_create_proceed::Req| {
+            anonymous_account_cmds::latest::account_create_proceed::Rep::UnknownStatus {
+                unknown_status: "unknown".to_string(),
+                reason: None,
+            }
+        }
+    );
+
+    p_assert_matches!(
+        Account::create_3_proceed(&cmds, validation_code, human_handle, password)
+        .await
+        .unwrap_err(),
+        AccountCreateError::Internal(err)
+        if format!("{}", err) == "Unexpected server response: UnknownStatus { unknown_status: \"unknown\", reason: None }"
+    );
+}
+
+#[parsec_test(testbed = "empty")]
+async fn invalid_validation_code(env: &TestbedEnv) {
+    let human_handle: HumanHandle = "Zack <zack@example.com>".parse().unwrap();
+    let password = "P@ssw0rd.".to_string().into();
+    let validation_code: ValidationCode = "AD3FXJ".parse().unwrap();
+    let cmds = AnonymousAccountCmds::new(
+        &env.discriminant_dir,
+        env.server_addr.clone(),
+        ProxyConfig::default(),
+    )
+    .unwrap();
+
+    test_register_sequence_of_send_hooks!(
+        &env.discriminant_dir,
+        move |_req: anonymous_account_cmds::latest::account_create_proceed::Req| {
+            anonymous_account_cmds::latest::account_create_proceed::Rep::InvalidValidationCode
+        }
+    );
+
+    p_assert_matches!(
+        Account::create_3_proceed(&cmds, validation_code, human_handle, password)
+            .await
+            .unwrap_err(),
+        AccountCreateError::InvalidValidationCode
+    );
+}
+
+#[parsec_test(testbed = "empty")]
+async fn auth_method_id_already_exists(env: &TestbedEnv) {
+    let human_handle: HumanHandle = "Zack <zack@example.com>".parse().unwrap();
+    let password = "P@ssw0rd.".to_string().into();
+    let validation_code: ValidationCode = "AD3FXJ".parse().unwrap();
+    let cmds = AnonymousAccountCmds::new(
+        &env.discriminant_dir,
+        env.server_addr.clone(),
+        ProxyConfig::default(),
+    )
+    .unwrap();
+
+    test_register_sequence_of_send_hooks!(
+        &env.discriminant_dir,
+        move |_req: anonymous_account_cmds::latest::account_create_proceed::Req| {
+            anonymous_account_cmds::latest::account_create_proceed::Rep::AuthMethodIdAlreadyExists
+        }
+    );
+
+    p_assert_matches!(
+        Account::create_3_proceed(&cmds, validation_code, human_handle, password)
+            .await
+            .unwrap_err(),
+        AccountCreateError::AuthMethodIdAlreadyExists
+    );
+}
