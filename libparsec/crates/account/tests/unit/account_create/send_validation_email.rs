2340838d66b21d9cf4730cf907c059eb5b080266
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2025-06-30 19:46:09 +0200

    Add tests to `libparsec_account` actually interacting with the testbed server

--- libparsec/crates/account/tests/unit/account_create/send_validation_email.rs
+++ libparsec/crates/account/tests/unit/account_create/send_validation_email.rs
@@ -13,8 +13,73 @@ use libparsec_types::prelude::*;
 
 use crate::{Account, AccountCreateSendValidationEmailError};
 
+#[parsec_test(testbed = "empty", with_server)]
+async fn ok_with_server_and_create(env: &TestbedEnv) {
+    // Randomize email since the testbed server will conserve it between runs
+    let email: EmailAddress = format!("{}@example.com", uuid::Uuid::new_v4())
+        .parse()
+        .unwrap();
+    let cmds = AnonymousAccountCmds::new(
+        &env.discriminant_dir,
+        env.server_addr.clone(),
+        ProxyConfig::default(),
+    )
+    .unwrap();
+
+    let mails = libparsec_tests_fixtures::test_check_mailbox(&env.server_addr, &email)
+        .await
+        .unwrap();
+    p_assert_eq!(mails.len(), 0);
+
+    p_assert_matches!(
+        Account::create_1_send_validation_email(&cmds, email.clone()).await,
+        Ok(())
+    );
+
+    let mails = libparsec_tests_fixtures::test_check_mailbox(&env.server_addr, &email)
+        .await
+        .unwrap();
+    p_assert_eq!(mails.len(), 1);
+
+    // TODO: Get validation code from the email and use to to actually create the account
+    //       once migration out of invitation addr is done.
+
+    // let invitation_token = {
+    //     let mail_body: &str = mails[0].2.as_ref();
+    //     let needle = "<a href=\"";
+    //     let addr_start = mail_body.find(needle).unwrap() + needle.len();
+    //     let addr_offset = mail_body[addr_start..].find("\"").unwrap();
+    //     // Note we cannot use the address to configure `AnonymousAccountCmds` since its
+    //     // contains an invalid domain (i.e. `saas.parsec.invalid/`)
+    //     let invitation_addr = ParsecAccountEmailValidationAddr::from_http_redirection(&mail_body[addr_start..addr_start + addr_offset]).unwrap();
+    //     invitation_addr.token()
+    // };
+
+    // let human_handle: HumanHandle = "Zack <zack@example.com>".parse().unwrap();
+    // let password: Password = "P@ssw0rd.".to_string().into();
+    // let validation_code: ValidationCode = "AD3FXJ".parse().unwrap();
+
+    // let cmds = AnonymousAccountCmds::new(
+    //     &env.discriminant_dir,
+    //     env.server_addr.clone(),
+    //     ProxyConfig::default(),
+    // )
+    // .unwrap();
+
+    // p_assert_matches!(
+    //     Account::create_3_proceed(
+    //         &cmds,
+    //         validation_code,
+    //         human_handle.clone(),
+    //         password.clone()
+    //     )
+    //     .await,
+    //     Ok(())
+    // );
+}
+
 #[parsec_test(testbed = "empty")]
-async fn ok(env: &TestbedEnv) {
+async fn ok_mocked(env: &TestbedEnv) {
     let email: EmailAddress = "zack@example.com".parse().unwrap();
     let cmds = AnonymousAccountCmds::new(
         &env.discriminant_dir,
@@ -32,7 +97,7 @@ async fn ok(env: &TestbedEnv) {
     });
 
     p_assert_matches!(
-        Account::create_1_send_validation_email(&cmds, email).await,
+        Account::create_1_send_validation_email(&cmds, email.clone()).await,
         Ok(())
     );
 }
