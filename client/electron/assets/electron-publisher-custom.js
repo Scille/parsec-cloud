21db46b2d2a96fca6eeb2b12305d79d1d8d74e48
Firelight Flagboy <firelight.flagboy@gmail.com>
2025-07-09 16:32:27 +0200

    Bump version 3.4.1-a.0+dev -> 3.5.0-a.1

--- client/electron/assets/electron-publisher-custom.js
+++ client/electron/assets/electron-publisher-custom.js
@@ -2,7 +2,7 @@
 
 const { GitHubPublisher } = require('electron-publish/out/gitHubPublisher');
 
-const VERSION = '3.4.1-a.0+dev';
+const VERSION = '3.5.0-a.1';
 
 class CustomGitHubPublisher extends GitHubPublisher {
   /**
