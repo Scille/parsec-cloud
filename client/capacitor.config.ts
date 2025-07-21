6435a02c237a0ea4b7be8d3ccd19cd2b5ca68a63
NicoTuxx <nicotuxxdev@gmail.com>
2025-07-21 19:23:53 +0200

    [MS] Improve Electron CSP config
    - add custom URL scheme for Electron app
    - enhance CSP setup with authorized domains

--- client/capacitor.config.ts
+++ client/capacitor.config.ts
@@ -20,6 +20,7 @@ const config: CapacitorElectronConfig = {
     splashScreenEnabled: true,
     deepLinkingEnabled: true,
     deepLinkingCustomProtocol: 'parsec3',
+    customUrlScheme: 'parsec-desktop',
   },
 };
 
