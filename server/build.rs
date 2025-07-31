bb730621b533497ba2e32da9542dc5b1135f5b4f
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2025-07-31 10:04:33 +0200

    Fix `build.rs` in `libparsec_protocol` & Python bindings not detecting a rebuild is needed when a .json5 schema is added
    And also remove useless `build.rs` from `libparsec_types` (since all .json5 schema are never referenced by folder there)

--- server/build.rs
+++ server/build.rs
@@ -1,15 +1,8 @@
 // Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 2016-present Scille SAS
 
-use glob::glob;
-
 fn main() {
-    // Register protocol .json5 schema files to trigger recompilation on change
-    // Note there is catch though: adding a new .json5 schema file will not
-    // trigger a recompilation !
-    for entry in glob("../libparsec/crates/protocol/schema/**/*.json5")
-        .expect("Failed to parse glob pattern")
-    {
-        let path = entry.expect("Glob iteration error");
-        println!("cargo:rerun-if-changed={}", path.display())
-    }
+    // Register .json5 schema files to trigger recompilation on change.
+    // Note we specify the root schema folder (instead of using glob to list the
+    // actual .json5 files) so that cargo is able to detect newly added files.
+    println!("cargo:rerun-if-changed=../libparsec/crates/protocol/schema/")
 }
