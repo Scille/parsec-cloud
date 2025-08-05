e7bdd4f06080d31c111b51ed505c1bc6cb1f5e6f
Firelight Flagboy <firelight.flagboy@gmail.com>
2025-08-05 08:56:10 +0200

    chore(make.py): Update script to use `uv`

--- make.py
+++ make.py
@@ -230,19 +230,19 @@ COMMANDS: dict[tuple[str, ...], Op | tuple[Op, ...]] = {
         # (note only the first `maturin develop` is impacted, as all maturin
         # develop uses the same default virtualenv)
         Cmd(
-            cmd="poetry install --with=testbed-server",
+            cmd="uv sync --dev --group testbed-server",
             extra_env={"UV_LIBPARSEC_BUILD_STRATEGY": "no_build"},
         ),
         Cmd(f"poetry run maturin develop --locked {PYTHON_DEV_CARGO_FLAGS}"),
     ),
     ("python-dev-rebuild", "r"): (
         Cwd(SERVER_DIR),
-        Cmd(f"poetry run maturin develop --locked {PYTHON_DEV_CARGO_FLAGS}"),
+        Cmd(f"uv run maturin develop --locked {PYTHON_DEV_CARGO_FLAGS}"),
     ),
     ("python-ci-install",): (
         Cwd(SERVER_DIR),
         Cmd(
-            cmd="poetry install",
+            cmd="uv sync",
             extra_env={"UV_LIBPARSEC_BUILD_PROFILE": "ci"},
         ),
     ),
