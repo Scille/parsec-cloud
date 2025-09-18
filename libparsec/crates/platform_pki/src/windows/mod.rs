8e8363103c4058ddcb2f82cff78f55b57529d30b
Firelight Flagboy <firelight.flagboy@gmail.com>
2025-09-18 14:19:31 +0200

    feat(pki): Allow to decrypt message using cert store
    (Windows only)
    
    - Add example to decrypt message
    
    Closes #11079

--- libparsec/crates/platform_pki/src/windows/mod.rs
+++ libparsec/crates/platform_pki/src/windows/mod.rs
@@ -4,8 +4,8 @@ use std::ffi::c_void;
 
 use crate::{
     CertificateDer, CertificateHash, CertificateReference, CertificateReferenceIdOrHash,
-    EncryptMessageError, EncryptedMessage, GetDerEncodedCertificateError, SignMessageError,
-    SignedMessage,
+    DecryptMessageError, DecryptedMessage, EncryptMessageError, EncryptedMessage,
+    GetDerEncodedCertificateError, SignMessageError, SignedMessage,
 };
 use bytes::Bytes;
 use schannel::{
@@ -16,10 +16,10 @@ use schannel::{
 };
 use sha2::Digest as _;
 use windows_sys::Win32::Security::Cryptography::{
-    CertGetCertificateContextProperty, NCryptEncrypt, NCryptSignHash, BCRYPT_OAEP_PADDING_INFO,
-    BCRYPT_PSS_PADDING_INFO, CERT_CONTEXT, CERT_KEY_PROV_INFO_PROP_ID, HCERTSTORE,
-    NCRYPT_KEY_HANDLE, NCRYPT_PAD_OAEP_FLAG, NCRYPT_PAD_PSS_FLAG, NCRYPT_SHA256_ALGORITHM,
-    UI::CryptUIDlgSelectCertificateFromStore,
+    CertGetCertificateContextProperty, NCryptDecrypt, NCryptEncrypt, NCryptSignHash,
+    BCRYPT_OAEP_PADDING_INFO, BCRYPT_PSS_PADDING_INFO, CERT_CONTEXT, CERT_KEY_PROV_INFO_PROP_ID,
+    HCERTSTORE, NCRYPT_KEY_HANDLE, NCRYPT_PAD_OAEP_FLAG, NCRYPT_PAD_PSS_FLAG,
+    NCRYPT_SHA256_ALGORITHM, UI::CryptUIDlgSelectCertificateFromStore,
 };
 
 fn open_store() -> std::io::Result<CertStore> {
@@ -381,6 +381,96 @@ fn ncrypt_encrypt_message(message: &[u8], handle: &NcryptKey) -> std::io::Result
             raw_handle,
             message.as_ptr(),
             message.len() as u32,
+            padding_info_ptr,
+            buff.as_mut_ptr(),
+            buff.len() as u32,
+            &mut len,
+            flags,
+        );
+        if res != 0 {
+            return Err(std::io::Error::last_os_error());
+        }
+        Ok(buff)
+    }
+}
+
+pub fn decrypt_message(
+    encrypted_message: &[u8],
+    certificate_ref: &CertificateReference,
+) -> Result<DecryptedMessage, DecryptMessageError> {
+    let store = open_store().map_err(DecryptMessageError::CannotOpenStore)?;
+    let cert_context =
+        find_certificate(&store, certificate_ref).ok_or(DecryptMessageError::NotFound)?;
+    let reference = get_id_and_hash_from_cert_context(&cert_context)
+        .map_err(DecryptMessageError::CannotGetCertificateInfo)?;
+    let keypair = get_keypair(&cert_context)?;
+    let data = match keypair {
+        // We do not support a CryptoAPI provider as its API is marked for depreciation by windows.
+        PrivateKey::CryptProv(..) => {
+            todo!("Use CryptGetUserKey to get the keypair")
+        }
+        // Handle to a CryptoGraphy Next Generation (CNG) API
+        PrivateKey::NcryptKey(handle) => {
+            ncrypt_decrypt_message(encrypted_message, &handle).map(Into::into)
+        }
+    }
+    .map_err(DecryptMessageError::CannotDecrypt)?;
+
+    Ok(DecryptedMessage {
+        data,
+        cert_ref: reference,
+    })
+}
+
+fn ncrypt_decrypt_message(
+    encrypted_message: &[u8],
+    handle: &NcryptKey,
+) -> std::io::Result<Vec<u8>> {
+    // SAFETY: NcryptKey is obtain from an NCRYPT_KEY_HANDLE, here we retrieve the underlying
+    // handle.
+    let raw_handle = unsafe { RawPointer::as_ptr(handle) } as NCRYPT_KEY_HANDLE;
+
+    // SAFETY: We follow the windows documentation by correctly passing the correct flags according
+    // to padding_info type, and the other pointer are either coming from allocated buffer or null
+    // pointer with their correct associated sizes.
+    //
+    // Documentation of the below function:
+    // https://learn.microsoft.com/en-us/windows/win32/api/ncrypt/nf-ncrypt-ncryptdecrypt
+    // Rust doc:
+    // https://docs.rs/windows-sys/latest/windows_sys/Win32/Security/Cryptography/fn.NCryptDecrypt.html
+    unsafe {
+        // We have padded the ciphered data using OAEP.
+        let flags = NCRYPT_PAD_OAEP_FLAG;
+        // https://learn.microsoft.com/en-us/windows/win32/api/bcrypt/ns-bcrypt-bcrypt_oaep_padding_info
+        let padding_info = BCRYPT_OAEP_PADDING_INFO {
+            pszAlgId: NCRYPT_SHA256_ALGORITHM,
+            pbLabel: std::ptr::null_mut(),
+            cbLabel: 0,
+        };
+        let padding_info_ptr = (&raw const padding_info) as *const std::os::raw::c_void;
+        let mut len = 0_u32;
+
+        // 1. Get size of the resulting ciphered data.
+        let res = NCryptDecrypt(
+            raw_handle,
+            encrypted_message.as_ptr(),
+            encrypted_message.len() as u32,
+            padding_info_ptr,
+            std::ptr::null_mut(),
+            0,
+            &mut len,
+            flags,
+        );
+        if res != 0 {
+            return Err(std::io::Error::last_os_error());
+        }
+
+        // 2. Actually perform the decryption.
+        let mut buff = vec![0_u8; len as usize];
+        let res = NCryptDecrypt(
+            raw_handle,
+            encrypted_message.as_ptr(),
+            encrypted_message.len() as u32,
             padding_info_ptr,
             buff.as_mut_ptr(),
             buff.len() as u32,
