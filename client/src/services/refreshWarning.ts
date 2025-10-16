68457bad3f805c1adb3461d5c825dbf10c0b2263
Maxime GRANDCOLAS <maxime.grandcolas@scille.fr>
2025-10-16 07:42:26 +0200

    [MS] Remove refresh warning when logging out

--- client/src/services/refreshWarning.ts
+++ client/src/services/refreshWarning.ts
@@ -1,30 +1,39 @@
 // Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 2016-present Scille SAS
 
 import { isWeb } from '@/parsec';
-import { ref } from 'vue';
 
-const isActive = ref(false);
+function beforeUnload(event: BeforeUnloadEvent): void {
+  event.preventDefault();
+  event.returnValue = true;
+}
+
+let controller: undefined | AbortController = undefined;
 
 export default function useRefreshWarning(): any {
   function warnOnRefresh(): void {
     if (window.isDev() || !isWeb()) {
       return;
     }
-    if (!isActive.value) {
-      window.addEventListener('beforeunload', (event: BeforeUnloadEvent) => {
-        event.preventDefault();
-        event.returnValue = true;
-      });
-      isActive.value = true;
+    if (!controller) {
+      controller = new AbortController();
+      window.addEventListener('beforeunload', beforeUnload, { signal: controller.signal });
     }
   }
 
+  function removeWarning(): void {
+    if (controller) {
+      controller.abort();
+    }
+    controller = undefined;
+  }
+
   function isWarningActive(): boolean {
-    return isActive.value;
+    return controller !== undefined;
   }
 
   return {
     warnOnRefresh,
     isWarningActive,
+    removeWarning,
   };
 }
