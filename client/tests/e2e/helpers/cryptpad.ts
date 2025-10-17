085c40fe53601d7343cc47f95df911ac6ee0e5de
Maxime GRANDCOLAS <maxime.grandcolas@scille.fr>
2025-10-17 06:43:55 +0200

    Fixed cryptpad e2e tests

--- client/tests/e2e/helpers/cryptpad.ts
+++ client/tests/e2e/helpers/cryptpad.ts
@@ -1,7 +1,8 @@
 // Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 2016-present Scille SAS
 
-import { expect } from '@playwright/test';
+import { FrameLocator } from '@playwright/test';
 import { MsPage } from '@tests/e2e/helpers/types';
+import { expect } from '@tests/e2e/helpers/assertions';
 
 // cspell:disable-next-line
 export const CRYPTPAD_SERVER = process.env.CRYPTPAD_SERVER || 'https://cryptpad-dev.parsec.cloud';
@@ -26,4 +27,16 @@ export async function mockCryptpadServer(page: MsPage, opts?: MockCryptpadOption
 export async function waitUntilSaved(page: MsPage, timeout = 10000): Promise<void> {
   await expect(page.locator('#unsaved-changes')).toBeHidden();
   await expect(page.locator('#saved-changes')).toBeVisible({ timeout: timeout });
+}
+
+export async function waitUntilLoaded(page: MsPage): Promise<FrameLocator> {
+  await expect(page.locator('#cryptpad-editor')).toBeVisible();
+  const mainFrame = page.locator('#cryptpad-editor').contentFrame();
+  await expect(mainFrame.locator('.placeholder-message-container')).toBeVisible({ timeout: 30000 });
+  await expect(mainFrame.locator('.placeholder-message-container')).toHaveText('Loading...');
+  await expect(mainFrame.locator('#sbox-iframe')).toBeVisible({ timeout: 30000 });
+  const sboxFrame = mainFrame.locator('#sbox-iframe').contentFrame();
+  await expect(sboxFrame.locator('.cp-loading-progress-list')).toBeVisible();
+
+  return sboxFrame;
 }
