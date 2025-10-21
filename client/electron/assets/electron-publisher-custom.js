af3a6e5cdc8178b954ab2d3bdb0a5529b0beaa62
NicoTuxx <nicotuxxdev@gmail.com>
2025-10-21 10:42:19 +0200

    Bump version 3.5.1 -> 3.5.2-a.0+dev

--- client/electron/assets/electron-publisher-custom.js
+++ client/electron/assets/electron-publisher-custom.js
@@ -2,7 +2,7 @@
 
 const { GitHubPublisher } = require('electron-publish/out/gitHubPublisher');
 
-const VERSION = '3.5.1';
+const VERSION = '3.5.2-a.0+dev';
 
 class CustomGitHubPublisher extends GitHubPublisher {
   /**
