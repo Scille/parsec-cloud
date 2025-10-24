6ef45a96f358eeb2f88818dcb5ac38b38fee19d0
Alban Costaz <alban.costaz@gmail.com>
2025-10-24 10:26:06 +0200

    [MS] Updates for eslint

---
+++ client/eslint.config.js
@@ -0,0 +1,21 @@
+// Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 2016-present Scille SAS
+
+export default [
+  {
+    ignores: [
+      'pkg/',
+      'dist',
+      'node_modules',
+      'electron/build',
+      'electron/app',
+      'electron/src/',
+      'electron/dist',
+      'electron/node_modules',
+      'src/views/testing/TestPage.vue',
+      'bindings',
+      'playwright.config.ts',
+      'merge-playwright.ts',
+      'src/parsec/mock_files',
+    ],
+  },
+];
