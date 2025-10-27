2992c455b6f3179c43de55d1076253c6a176f389
Alban Costaz <alban.costaz@gmail.com>
2025-10-27 14:41:40 +0100

    Bump version 3.5.3-a.0+dev -> 3.6.0-rc.0

--- client/electron/assets/electron-publisher-custom.js
+++ client/electron/assets/electron-publisher-custom.js
@@ -2,7 +2,7 @@
 
 const { GitHubPublisher } = require('electron-publish/out/gitHubPublisher');
 
-const VERSION = '3.5.3-a.0+dev';
+const VERSION = '3.6.0-rc.0';
 
 class CustomGitHubPublisher extends GitHubPublisher {
   /**
