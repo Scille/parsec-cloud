31b010e6a93e3ab7e66f6501e5f5a5911fe78c70
Firelight Flagboy <firelight.flagboy@gmail.com>
2025-11-03 16:05:28 +0100

    feat(client): Simplify pki submit params
    Now the caller need to pass the underlying elements needed to create the payload over pre-computing it.
    
    Closes #11440

--- libparsec/crates/client/src/client/pki_enrollment_submit.rs
+++ libparsec/crates/client/src/client/pki_enrollment_submit.rs
@@ -5,8 +5,10 @@ use libparsec_protocol::anonymous_cmds::latest::pki_enrollment_submit::{Rep, Req
 use libparsec_types::prelude::*;
 use std::sync::Arc;
 
+use crate::ClientConfig;
+
 #[derive(Debug, thiserror::Error)]
-pub enum ClientPkiEnrollmentSubmitError {
+pub enum PkiEnrollmentSubmitError {
     #[error("Cannot communicate with the server: {0}")]
     Offline(#[from] ConnectionError),
     #[error(transparent)]
@@ -21,37 +23,91 @@ pub enum ClientPkiEnrollmentSubmitError {
     IdAlreadyUsed,
     #[error("Unable to read payload")]
     InvalidPayload,
+    #[error("PKI operation error: {0}")]
+    PkiOperationError(anyhow::Error),
 }
 
 pub async fn pki_enrollment_submit(
-    cmds: &Arc<AnonymousCmds>,
-    der_x509_certificate: Bytes,
-    enrollment_id: EnrollmentID,
+    config: Arc<ClientConfig>,
+    addr: ParsecPkiEnrollmentAddr,
+    x509_cert_ref: X509CertificateReference,
+    human_handle: HumanHandle,
+    device_label: DeviceLabel,
     force: bool,
-    payload: Bytes,
-    payload_signature: Bytes,
-) -> Result<DateTime, ClientPkiEnrollmentSubmitError> {
-    let _ = match cmds
+) -> Result<DateTime, PkiEnrollmentSubmitError> {
+    let cmds = AnonymousCmds::new(
+        &config.config_dir,
+        ParsecAnonymousAddr::ParsecPkiEnrollmentAddr(addr.clone()),
+        config.proxy.clone(),
+    )?;
+
+    let enrollment_id = EnrollmentID::default();
+    let signing_key = SigningKey::generate();
+    let private_key = PrivateKey::generate();
+
+    let der_x509_certificate = libparsec_platform_pki::get_der_encoded_certificate(&x509_cert_ref)
+        .map_err(anyhow::Error::from)
+        .context("Failed to get certificate DER content")
+        .map_err(PkiEnrollmentSubmitError::PkiOperationError)?;
+    let payload = PkiEnrollmentSubmitPayload {
+        verify_key: signing_key.verify_key(),
+        public_key: private_key.public_key(),
+        device_label,
+        human_handle,
+    };
+    let raw_payload = payload.dump();
+    let payload_signature = libparsec_platform_pki::sign_message(&raw_payload, &x509_cert_ref)
+        .map_err(anyhow::Error::from)
+        .context("Failed to sign payload with PKI")
+        .map_err(PkiEnrollmentSubmitError::PkiOperationError)?;
+
+    let submitted_on = match cmds
         .send(Req {
-            der_x509_certificate,
+            der_x509_certificate: der_x509_certificate.der_content,
             enrollment_id,
             force,
-            payload,
-            payload_signature,
+            payload: raw_payload.into(),
+            payload_signature: payload_signature.signature,
         })
         .await?
     {
         Rep::Ok { submitted_on } => Ok(submitted_on),
-        Rep::AlreadyEnrolled => Err(ClientPkiEnrollmentSubmitError::AlreadyEnrolled),
-        Rep::AlreadySubmitted { submitted_on } => Err(
-            ClientPkiEnrollmentSubmitError::AlreadySubmitted(submitted_on),
-        ),
-        Rep::EmailAlreadyUsed => Err(ClientPkiEnrollmentSubmitError::EmailAlreadyUsed),
-        Rep::IdAlreadyUsed => Err(ClientPkiEnrollmentSubmitError::IdAlreadyUsed),
-        Rep::InvalidPayload => Err(ClientPkiEnrollmentSubmitError::InvalidPayload),
+        Rep::AlreadyEnrolled => Err(PkiEnrollmentSubmitError::AlreadyEnrolled),
+        Rep::AlreadySubmitted { submitted_on } => {
+            Err(PkiEnrollmentSubmitError::AlreadySubmitted(submitted_on))
+        }
+        Rep::EmailAlreadyUsed => Err(PkiEnrollmentSubmitError::EmailAlreadyUsed),
+        Rep::IdAlreadyUsed => Err(PkiEnrollmentSubmitError::IdAlreadyUsed),
+        Rep::InvalidPayload => Err(PkiEnrollmentSubmitError::InvalidPayload),
         bad_rep @ Rep::UnknownStatus { .. } => {
             Err(anyhow::anyhow!("Unexpected server response: {:?}", bad_rep).into())
         }
+    }?;
+
+    let private_parts = PrivateParts {
+        private_key,
+        signing_key,
     };
-    todo!("#11440");
+    let local_pending = libparsec_platform_pki::create_local_pending(
+        &x509_cert_ref,
+        addr,
+        enrollment_id,
+        submitted_on,
+        payload,
+        private_parts,
+    )
+    .map_err(anyhow::Error::from)
+    .context("Failed to encrypt local parts with PKI")
+    .map_err(PkiEnrollmentSubmitError::PkiOperationError)?;
+
+    let local_file = libparsec_platform_device_loader::get_default_local_pending_file(
+        &config.config_dir,
+        enrollment_id,
+    );
+    libparsec_platform_device_loader::save_pki_local_pending(local_pending, local_file)
+        .await
+        .map_err(Into::into)
+        .map_err(PkiEnrollmentSubmitError::Internal)?;
+
+    Ok(submitted_on)
 }
