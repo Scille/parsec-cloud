4680c9011505d691bd16a3a35566b05cf51d8155
Alban Costaz <alban.costaz@gmail.com>
2025-11-05 15:24:11 +0100

    Bump version 3.6.0-rc.4 -> 3.6.0-rc.5+dev

--- docs/conf.py
+++ docs/conf.py
@@ -61,7 +61,7 @@ copyright = "2016-present, Scille SAS"
 # the built documents.
 #
 # The short X.Y version.
-version = "3.6.0-rc.4"
+version = "3.6.0-rc.5+dev"
 # The full version, including alpha/beta/rc tags.
 release = version
 
