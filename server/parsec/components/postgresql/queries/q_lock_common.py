978a2f9e33e4ae87382b8a166dcc3d21600dfde0
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2025-11-06 16:39:55 +0100

    Add `server/parsec/components/postgresql/queries/q_lock_common.py`

---
+++ server/parsec/components/postgresql/queries/q_lock_common.py
@@ -0,0 +1,78 @@
+# Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 2016-present Scille SAS
+from __future__ import annotations
+
+from dataclasses import dataclass
+from enum import auto
+
+from parsec._parsec import (
+    DateTime,
+)
+from parsec.components.postgresql import AsyncpgConnection
+from parsec.components.postgresql.utils import (
+    Q,
+)
+from parsec.types import BadOutcomeEnum
+
+
+class LockCommonWriteRealmBadOutcome(BadOutcomeEnum):
+    REALM_NOT_FOUND = auto()
+    USER_NOT_IN_COMMON = auto()
+
+
+@dataclass(slots=True)
+class LockCommonData:
+    last_common_certificate_timestamp: DateTime
+
+
+_Q_LOCK_COMMON_TEMPLATE = """
+SELECT last_timestamp
+FROM common_topic
+WHERE organization = $organization_internal_id
+LIMIT 1
+-- Read or write lock?
+{row_lock}
+"""
+
+_q_lock_common_write = Q(_Q_LOCK_COMMON_TEMPLATE.format(row_lock="FOR UPDATE"))
+_q_lock_common_read = Q(_Q_LOCK_COMMON_TEMPLATE.format(row_lock="FOR SHARE"))
+
+
+async def lock_common_write(
+    conn: AsyncpgConnection,
+    organization_internal_id: int,
+) -> LockCommonData | LockCommonWriteRealmBadOutcome:
+    return await _do_lock_common(
+        conn,
+        _q_lock_common_write,
+        organization_internal_id=organization_internal_id,
+    )
+
+
+async def lock_common_read(
+    conn: AsyncpgConnection,
+    organization_internal_id: int,
+) -> LockCommonData | LockCommonWriteRealmBadOutcome:
+    return await _do_lock_common(
+        conn,
+        _q_lock_common_read,
+        organization_internal_id=organization_internal_id,
+    )
+
+
+async def _do_lock_common(
+    conn: AsyncpgConnection,
+    lock_query: Q,
+    organization_internal_id: int,
+) -> LockCommonData | LockCommonWriteRealmBadOutcome:
+    row = await conn.fetchrow(*lock_query(organization_internal_id=organization_internal_id))
+    assert row is not None
+
+    match row["last_timestamp"]:
+        case DateTime() as last_timestamp:
+            pass
+        case _:
+            assert False, row
+
+    return LockCommonData(
+        last_common_certificate_timestamp=last_timestamp,
+    )
