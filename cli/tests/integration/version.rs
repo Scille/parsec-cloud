8bb57997d4a3d2cc7f0a984abcc0fffd38380caf
Alban Costaz <alban.costaz@gmail.com>
2025-11-13 10:33:30 +0100

    Bump version 3.6.1-rc.0 -> 3.6.1-rc.1+dev

--- cli/tests/integration/version.rs
+++ cli/tests/integration/version.rs
@@ -2,6 +2,6 @@
 fn version() {
     crate::assert_cmd_success!("--version").stdout(
         // Using `concat!` simplify updating the version using `version-updater`
-        concat!("parsec-cli 3.6.1-rc.0", "\n"),
+        concat!("parsec-cli 3.6.1-rc.1+dev", "\n"),
     );
 }
