dae77ae691d1a6b11ae5b74088ca951b5b439049
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2025-11-17 18:14:58 +0100

    Remove useless part in `impl_transparent_data_format_conversion` macro

--- libparsec/crates/types/src/data_macros.rs
+++ libparsec/crates/types/src/data_macros.rs
@@ -2,7 +2,7 @@
 
 #[macro_export]
 macro_rules! impl_transparent_data_format_conversion {
-    ($obj_type:ty, $data_type:ty, $($field:ident),* $(hooks: $($hook:expr),*)? $(,)?) => {
+    ($obj_type:ty, $data_type:ty, $($field:ident),* $(,)?) => {
 
         impl From<$data_type> for $obj_type {
             #[allow(unused_variables)]
