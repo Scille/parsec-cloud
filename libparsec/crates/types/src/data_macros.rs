ec6252bba011afda1e63afc06efee8a71b7dcb01
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2025-11-17 18:15:49 +0100

    Introduce `impl_data_format_conversion;` macro in `libparsec_types`

--- libparsec/crates/types/src/data_macros.rs
+++ libparsec/crates/types/src/data_macros.rs
@@ -27,3 +27,53 @@ macro_rules! impl_transparent_data_format_conversion {
 }
 
 pub use impl_transparent_data_format_conversion;
+
+#[macro_export]
+macro_rules! impl_data_format_conversion {
+    (@internal $target:expr, can_fail $hook:expr) => {
+        $hook($target)?
+    };
+    (@internal $target:expr, $hook:expr) => {
+        $hook($target)
+    };
+    (@internal $target:expr $(,)?) => {
+        $target.into()
+    };
+
+    (
+        $obj_type:ty,
+        $data_type:ty,
+        $($field:ident $( with_hooks { serialize: $serialize_hook_fn:expr; deserialize: $deserialize_hook_fn:expr; } )? ),*
+        $(,)?
+    ) => {
+
+        impl TryFrom<$data_type> for $obj_type {
+            type Error = &'static str;
+
+            fn try_from(data: $data_type) -> Result<Self, Self::Error> {
+                $(
+                    let $field = impl_data_format_conversion!(@internal data.$field, $( can_fail $deserialize_hook_fn )?);
+                )*
+                Ok(Self {
+                    $($field),*
+                })
+            }
+        }
+
+        // Note this is not a `TryFrom` since our internal object cannot represent invalid data.
+        impl From<$obj_type> for $data_type {
+            fn from(obj: $obj_type) -> Self {
+                $(
+                    let $field = impl_data_format_conversion!(@internal obj.$field, $( $serialize_hook_fn )?);
+                )*
+                Self {
+                    ty: Default::default(),
+                    $($field),*
+                }
+            }
+        }
+
+    };
+}
+
+pub use impl_data_format_conversion;
