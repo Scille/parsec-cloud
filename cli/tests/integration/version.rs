69167cb7511880ccbeec9eaafa20ddf6e7651661
Firelight Flagboy <firelight.flagboy@gmail.com>
2025-11-26 17:44:25 +0100

    Bump version 3.6.2-a.0+dev -> 3.7.0-rc.1

--- cli/tests/integration/version.rs
+++ cli/tests/integration/version.rs
@@ -2,6 +2,6 @@
 fn version() {
     crate::assert_cmd_success!("--version").stdout(
         // Using `concat!` simplify updating the version using `version-updater`
-        concat!("parsec-cli 3.6.2-a.0+dev", "\n"),
+        concat!("parsec-cli 3.7.0-rc.1", "\n"),
     );
 }
