a81241aa2314b6977202c2388c0d28eecc72200c
NicoTuxx <nicotuxxdev@gmail.com>
2025-12-03 13:58:21 +0100

    feat(editor): add timeout modal for file loading issues and enhance error handling

--- client/src/views/files/handler/editor/types.ts
+++ client/src/views/files/handler/editor/types.ts
@@ -8,3 +8,31 @@ export enum SaveState {
   Error = 'save-error',
   Offline = 'offline',
 }
+
+export enum EditorIssueStatus {
+  UnsupportedFileType = 'unsupported_file_type',
+  NetworkOffline = 'network_offline',
+  EditionNotAvailable = 'edition_not_available',
+  LoadingTimeout = 'loading_timeout',
+}
+
+export enum EditorErrorTitle {
+  GenericError = 'fileViewers.errors.titles.genericError',
+  UnsupportedFileType = 'fileViewers.errors.titles.unsupportedFileType',
+  EditionNotAvailable = 'fileViewers.errors.titles.editionNotAvailable',
+  NetworkOffline = 'fileEditors.errors.titles.networkOffline',
+  TooLongToOpen = 'fileEditors.errors.titles.tooLongToOpen',
+}
+
+export enum EditorErrorMessage {
+  EditableOnlyOnSystem = 'fileViewers.errors.informationEditDownload',
+  NetworkOffline = 'fileEditors.errors.networkOfflineMessage',
+  TooLongToOpenOnWeb = 'fileEditors.errors.tooLongToOpenOnWeb',
+  TooLongToOpenOnDesktop = 'fileEditors.errors.tooLongToOpenOnDesktop',
+}
+
+export enum EditorButtonAction {
+  BackToFiles = 'fileEditors.actions.backToFiles',
+  Close = 'fileEditors.actions.close',
+  Wait = 'fileEditors.actions.wait',
+}
