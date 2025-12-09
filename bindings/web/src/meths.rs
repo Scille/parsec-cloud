17d01f4e2cbfec0299bcc94623740c23f580df43
Firelight Flagboy <firelight.flagboy@gmail.com>
2025-12-09 10:18:30 +0100

    refactor(pki): Add error to pki list

--- bindings/web/src/meths.rs
+++ bindings/web/src/meths.rs
@@ -14682,6 +14682,13 @@ fn variant_pki_enrollment_list_error_rs_to_js(
                 &"PkiEnrollmentListErrorInternal".into(),
             )?;
         }
+        libparsec::PkiEnrollmentListError::InvalidSubmitterX509Certificates { .. } => {
+            Reflect::set(
+                &js_obj,
+                &"tag".into(),
+                &"PkiEnrollmentListErrorInvalidSubmitterX509Certificates".into(),
+            )?;
+        }
         libparsec::PkiEnrollmentListError::Offline { .. } => {
             Reflect::set(
                 &js_obj,
