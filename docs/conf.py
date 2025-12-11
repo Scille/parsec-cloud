d9d39484213909964cae95395c74704233983798
Alban Costaz <alban.costaz@gmail.com>
2025-12-11 11:33:29 +0100

    Bump version 3.7.1-a.0+dev -> 3.7.2-a.0+dev

--- docs/conf.py
+++ docs/conf.py
@@ -61,7 +61,7 @@ copyright = "2016-present, Scille SAS"
 # the built documents.
 #
 # The short X.Y version.
-version = "3.7.1"
+version = "3.7.2-a.0+dev"
 # The full version, including alpha/beta/rc tags.
 release = version
 
