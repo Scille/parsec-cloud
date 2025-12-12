045a0848c530eda08513a2222a72db38a55f937e
Firelight Flagboy <firelight.flagboy@gmail.com>
2025-12-12 10:10:41 +0100

    refactor(storage): Add "proper" error for `remove_device_data`

--- libparsec/crates/platform_storage/src/native/cleanup.rs
+++ libparsec/crates/platform_storage/src/native/cleanup.rs
@@ -4,13 +4,17 @@ use std::path::Path;
 
 use libparsec_types::{anyhow, DeviceID};
 
-pub async fn remove_device_data(data_base_dir: &Path, device_id: DeviceID) -> anyhow::Result<()> {
+pub async fn remove_device_data(
+    data_base_dir: &Path,
+    device_id: DeviceID,
+) -> Result<(), crate::RemoveDeviceDataError> {
     let path = data_base_dir.join(device_id.hex());
     log::debug!("Removing device data at {}", path.display());
 
     tokio::fs::remove_dir_all(&path)
         .await
         .map_err(anyhow::Error::from)
+        .map_err(Into::into)
 }
 
 #[cfg(test)]
