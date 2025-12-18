68595aa756202d907d77541722a0f8d583047b6a
Aurelia <aurelia.dolo@gmail.com>
2025-12-18 15:49:31 +0100

    Create platform_filesystem crate.

---
+++ libparsec/crates/platform_filesystem/src/native/mod.rs
@@ -0,0 +1,7 @@
+// Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 2016-present Scille SAS
+
+mod list;
+mod load;
+mod remove;
+mod rename;
+mod save;
---
+++ libparsec/crates/platform_filesystem/src/web/mod.rs
@@ -0,0 +1,8 @@
+// Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 2016-present Scille SAS
+
+mod common;
+mod list;
+mod load;
+mod remove;
+mod rename;
+mod save;
