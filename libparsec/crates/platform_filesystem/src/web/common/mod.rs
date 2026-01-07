c35c5c46f5d5da64daa75697e2f7bbc0d6fe7403
Aurelia <aurelia.dolo@gmail.com>
2026-01-07 15:39:33 +0100

    Move web wrapper to the new platform_filesystem crate without error_set.

--- libparsec/crates/platform_filesystem/src/web/common/mod.rs
+++ libparsec/crates/platform_filesystem/src/web/common/mod.rs
@@ -1,1 +1,5 @@
 // Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 2016-present Scille SAS
+pub(crate) mod error;
+
+#[expect(unused)] // TODO remove when finishing ##11954
+pub(crate) mod wrapper;
