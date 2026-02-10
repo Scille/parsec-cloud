8263609988fb0c47941cf3b113ff678c9ed04cac
Aurelia <aurelia.dolo@gmail.com>
2026-02-10 15:21:33 +0100

    Remove pki_enrollment protocol.
    It is been replaced by async enrollment.

--- bindings/generator/api/pki.py
+++ bindings/generator/api/pki.py
@@ -1,14 +1,8 @@
 # Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 2016-present Scille SAS
 
 from .common import (
-    Bytes,
-    DateTime,
-    EmailAddress,
     ErrorVariant,
     Result,
-    Structure,
-    Variant,
-    VariantItemTuple,
     X509CertificateReference,
 )
 
@@ -26,55 +20,4 @@ async def show_certificate_selection_dialog_windows_only() -> Result[
 
 
 async def is_pki_available() -> bool:
-    raise NotImplementedError
-
-
-class DistinguishedNameValue(Variant):
-    CommonName = VariantItemTuple(str)
-    EmailAddress = VariantItemTuple(str)
-
-
-class CertificateDetails(Structure):
-    name: str | None
-    subject: list[DistinguishedNameValue]
-    issuer: list[DistinguishedNameValue]
-    not_before: DateTime
-    not_after: DateTime
-    serial: Bytes
-    emails: list[EmailAddress]
-    can_sign: bool
-    can_encrypt: bool
-
-
-class InvalidCertificateReason(Variant):
-    class UnableToParseTime:
-        pass
-
-    class UnableToParseCert:
-        pass
-
-    class InvalidEmail:
-        pass
-
-
-class CertificateWithDetails(Variant):
-    class Valid:
-        handle: X509CertificateReference
-        friendly_name: str | None
-        details: CertificateDetails
-
-    class Invalid:
-        handle: X509CertificateReference
-        friendly_name: str | None
-        invalid_reason: InvalidCertificateReason
-
-
-class ListUserCertificatesError(ErrorVariant):
-    class CannotOpenStore:
-        pass
-
-
-async def list_user_certificates_with_details() -> Result[
-    list[CertificateWithDetails], ListUserCertificatesError
-]:
     raise NotImplementedError
