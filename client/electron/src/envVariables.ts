0a2bb14e0f8a7225aa2d99fcb3270302fd48d807
Alban Costaz <alban.costaz@gmail.com>
2026-02-11 16:55:45 +0100

    Added CSPN-specific env variables

--- client/electron/src/envVariables.ts
+++ client/electron/src/envVariables.ts
@@ -1,7 +1,7 @@
 // Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 2016-present Scille SAS
 
 export const Env = {
-  DISABLE_SENTRY: false,
-  DISABLE_UPDATES: false,
-  ENABLE_CUSTOM_BRANDING: process.env.PARSEC_APP_ENABLE_CUSTOM_BRANDING === 'true',
+  DISABLE_SENTRY: process.env.PARSEC_APP_IS_CSPN === 'true',
+  DISABLE_UPDATES: process.env.PARSEC_APP_IS_CSPN === 'true',
+  ENABLE_CUSTOM_BRANDING: process.env.PARSEC_APP_IS_CSPN !== 'true' && process.env.PARSEC_APP_ENABLE_CUSTOM_BRANDING === 'true',
 };
