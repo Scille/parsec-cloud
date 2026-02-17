16a80254f57787f91bf772fbe3c78bef0d24dc57
Maxime GRANDCOLAS <maxime.grandcolas@scille.fr>
2026-02-17 06:30:59 +0100

    [MS] Removed unused dependencies and code

--- client/src/services/viewers.ts
+++ client/src/services/viewers.ts
@@ -1,11 +1,5 @@
 // Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 2016-present Scille SAS
 
-import * as monaco from 'monaco-editor';
-import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker';
-import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker';
-import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker';
-import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker';
-import jsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker';
 import * as pdfjs from 'pdfjs-dist';
 import pdfjsWorker from 'pdfjs-dist/build/pdf.worker?worker&url';
 
@@ -13,50 +7,6 @@ async function _initPdf(): Promise<void> {
   pdfjs.GlobalWorkerOptions.workerSrc = pdfjsWorker;
 }
 
-async function _initText(): Promise<void> {
-  /* MsTheme: Monaco editor custom theme */
-  monaco.editor.defineTheme('msEditorTheme', {
-    base: 'vs',
-    inherit: true,
-    rules: [],
-    colors: {
-      'editor.foreground': '#000000',
-      'editor.background': '#ffffff',
-      'editorCursor.foreground': '#0058cc',
-      'editor.lineHighlightBackground': '#f3f3f7',
-      'editorLineNumber.foreground': '#0058cc',
-      'editor.selectionBackground': '#99c5ff',
-      'editor.inactiveSelectionBackground': '#88000015',
-      'scrollbar.shadow': '#f3f3f7',
-      'scrollbarSlider.background': '#cce2ff',
-      'scrollbarSlider.hoverBackground': '#99c5ff',
-      'scrollbarSlider.activeBackground': '#66a8ff',
-    },
-  });
-  self.MonacoEnvironment = {
-    getWorker: function (_workerId, label): Worker {
-      switch (label) {
-        case 'json':
-          return new jsonWorker();
-        case 'css':
-        case 'scss':
-        case 'less':
-          return new cssWorker();
-        case 'html':
-        case 'handlebars':
-        case 'razor':
-          return new htmlWorker();
-        case 'typescript':
-        case 'javascript':
-          return new jsWorker();
-        default:
-          return new editorWorker();
-      }
-    },
-  };
-}
-
 export async function initViewers(): Promise<void> {
   await _initPdf();
-  await _initText();
 }
--- client/src/views/files/handler/viewer/index.ts
+++ client/src/views/files/handler/viewer/index.ts
@@ -1,13 +1,10 @@
 // Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 2016-present Scille SAS
 
 import AudioViewer from '@/views/files/handler/viewer/AudioViewer.vue';
-import DocumentViewer from '@/views/files/handler/viewer/DocumentViewer.vue';
 import FileViewer from '@/views/files/handler/viewer/FileViewer.vue';
 import FileViewerWrapper from '@/views/files/handler/viewer/FileViewerWrapper.vue';
 import ImageViewer from '@/views/files/handler/viewer/ImageViewer.vue';
 import PdfViewer from '@/views/files/handler/viewer/PdfViewer.vue';
-import SpreadsheetViewer from '@/views/files/handler/viewer/SpreadsheetViewer.vue';
-import TextViewer from '@/views/files/handler/viewer/TextViewer.vue';
 import VideoViewer from '@/views/files/handler/viewer/VideoViewer.vue';
 
-export { AudioViewer, DocumentViewer, FileViewer, FileViewerWrapper, ImageViewer, PdfViewer, SpreadsheetViewer, TextViewer, VideoViewer };
+export { AudioViewer, FileViewer, FileViewerWrapper, ImageViewer, PdfViewer, VideoViewer };
