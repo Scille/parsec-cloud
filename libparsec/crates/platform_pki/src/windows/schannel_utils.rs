651ebce386b4e4f36eb59c71eedcd68acac99977
Firelight Flagboy <firelight.flagboy@gmail.com>
2026-03-04 15:09:10 +0100

    refactor(pki): Direct acquire private key

--- libparsec/crates/platform_pki/src/windows/schannel_utils.rs
+++ libparsec/crates/platform_pki/src/windows/schannel_utils.rs
@@ -1,8 +1,6 @@
 // Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 2016-present Scille SAS
 
-use schannel::{
-    cert_context::CertContext, cert_store::CertStore, ncrypt_key::NcryptKey, RawPointer,
-};
+use schannel::{cert_context::CertContext, cert_store::CertStore, RawPointer};
 use windows_sys::Win32::Security::Cryptography;
 
 pub(super) fn get_raw_store(store: &CertStore) -> Cryptography::HCERTSTORE {
@@ -25,8 +23,72 @@ pub(super) fn cert_context_to_raw(cert_context: &CertContext) -> *const Cryptogr
     (unsafe { RawPointer::as_ptr(cert_context) }) as *const Cryptography::CERT_CONTEXT
 }
 
-pub(super) fn ncrypt_key_to_ptr(ncrypt_key: &NcryptKey) -> Cryptography::NCRYPT_KEY_HANDLE {
-    // SAFETY: NcryptKey is obtain from an NCRYPT_KEY_HANDLE, here we retrieve the underlying
-    // handle.
-    (unsafe { RawPointer::as_ptr(ncrypt_key) }) as Cryptography::NCRYPT_KEY_HANDLE
+pub(super) struct PrivateKey {
+    handle: Cryptography::NCRYPT_KEY_HANDLE,
+    must_free: bool,
+}
+
+impl Drop for PrivateKey {
+    fn drop(&mut self) {
+        if self.must_free {
+            // SAFETY: The handle to free is obtain by a previous call to
+            // `CryptAcquireCertificatePrivateKey` where proper check where made.
+            // Furthermore, we only free the handle if asked to by
+            // `CryptAcquireCertificatePrivateKey` (via `must_free` param).
+            unsafe {
+                Cryptography::NCryptFreeObject(self.handle);
+            }
+        }
+    }
+}
+
+impl PrivateKey {
+    pub fn handle(&self) -> Cryptography::NCRYPT_KEY_HANDLE {
+        self.handle
+    }
+}
+
+/// Return a private key handle from a certificate context
+pub(super) fn acquire_private_key(context: &CertContext) -> std::io::Result<PrivateKey> {
+    let raw_context = cert_context_to_raw(context);
+    let mut handle = Cryptography::NCRYPT_KEY_HANDLE::default();
+    let flags = Cryptography::CRYPT_ACQUIRE_COMPARE_KEY_FLAG // Ensure that the private key correspond to the
+    // certificate
+    | Cryptography::CRYPT_ACQUIRE_ONLY_NCRYPT_KEY_FLAG // Only return a Ncrypt key handle (since we
+    // do not cater to Windows SRV 2003 nor Windows XP)
+    ;
+    let mut spec = 0;
+    let mut must_free = windows_sys::core::BOOL::default();
+    // SAFETY: We pass the expected parameters following the documentation.
+    // The certificate context handle is a valid handle obtained by converting `CertContext`.
+    // Doc: https://learn.microsoft.com/en-us/windows/win32/api/wincrypt/nf-wincrypt-cryptacquirecertificateprivatekey
+    let res = unsafe {
+        Cryptography::CryptAcquireCertificatePrivateKey(
+            raw_context,
+            flags,
+            std::ptr::null_mut(),
+            &mut handle,
+            &mut spec,
+            &mut must_free,
+        )
+    };
+
+    if res == 0 {
+        // `res` correspond to `BOOL`, and is set to `FALSE` on failure and the error reason is
+        // obtain by a call to `GetLastError`.
+        return Err(std::io::Error::last_os_error());
+    }
+
+    // Check if the returned key is of expected spec
+    if spec != Cryptography::CERT_NCRYPT_KEY_SPEC {
+        return Err(std::io::Error::new(
+            std::io::ErrorKind::InvalidData,
+            format!("invalid returned key spec ({spec:#x})"),
+        ));
+    }
+
+    Ok(PrivateKey {
+        handle,
+        must_free: must_free == 1,
+    })
 }
