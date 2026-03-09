3d1eb6aa9200d61a90759982c550781eda9f3c86
Alban Costaz <alban.costaz@gmail.com>
2026-03-09 14:08:59 +0100

    Bump version 3.8.0 -> 3.8.1-a.0+dev

--- cli/tests/integration/version.rs
+++ cli/tests/integration/version.rs
@@ -2,6 +2,6 @@
 fn version() {
     crate::assert_cmd_success!("--version").stdout(
         // Using `concat!` simplify updating the version using `version-updater`
-        concat!("parsec-cli 3.8.0", "\n"),
+        concat!("parsec-cli 3.8.1-a.0+dev", "\n"),
     );
 }
--- client/electron/assets/electron-publisher-custom.js
+++ client/electron/assets/electron-publisher-custom.js
@@ -2,7 +2,7 @@
 
 const { GitHubPublisher } = require('electron-publish/out/gitHubPublisher');
 
-const VERSION = '3.8.0';
+const VERSION = '3.8.1-a.0+dev';
 
 class CustomGitHubPublisher extends GitHubPublisher {
   /**
