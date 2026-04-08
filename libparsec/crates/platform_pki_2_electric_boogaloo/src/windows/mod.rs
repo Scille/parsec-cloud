a36c99c3433cdff413e152239d79919690717262
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2026-04-08 20:22:26 +0200

    `platform_pki_2_electric_boogaloo` Windows implementation: add trivial `mod.rs`

---
+++ libparsec/crates/platform_pki_2_electric_boogaloo/src/windows/mod.rs
@@ -0,0 +1,11 @@
+// Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 2016-present Scille SAS
+
+mod find_in_store;
+mod pki_certificate;
+mod pki_private_key;
+mod pki_system;
+mod schannel_utils;
+
+pub use pki_certificate::*;
+pub use pki_private_key::*;
+pub use pki_system::*;
