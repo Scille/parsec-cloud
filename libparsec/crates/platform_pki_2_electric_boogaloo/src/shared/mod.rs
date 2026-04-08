e8fa8b1c7cd9151443497097c58a1316d057fffc
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2026-04-08 17:49:53 +0200

    Define new API in for `libparsec_platform_pki` in temporary crate `platform_pki_2_electric_boogaloo`

---
+++ libparsec/crates/platform_pki_2_electric_boogaloo/src/shared/mod.rs
@@ -0,0 +1,9 @@
+// Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 2016-present Scille SAS
+
+mod encrypt;
+mod info;
+mod verify;
+
+pub use encrypt::*;
+pub use info::*;
+pub use verify::*;
