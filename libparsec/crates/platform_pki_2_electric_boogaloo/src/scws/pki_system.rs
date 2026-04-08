603bf879a63b95dce655b064d6f077147e14ab87
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2026-04-08 18:27:27 +0200

    Add SCWS (for web) mocked implementation for `platform_pki_2_electric_boogaloo`

---
+++ libparsec/crates/platform_pki_2_electric_boogaloo/src/scws/pki_system.rs
@@ -0,0 +1,29 @@
+// Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 2016-present Scille SAS
+
+use crate::{
+    AvailablePkiCertificate, PkiCertificate, PkiScwsConfig, PkiSystemInitError,
+    PkiSystemListUserCertificateError, PkiSystemOpenCertificateError,
+};
+use libparsec_types::prelude::*;
+
+#[derive(Debug)]
+pub struct PlatformPkiSystem {}
+
+impl PlatformPkiSystem {
+    pub async fn init(_scws_config: Option<PkiScwsConfig>) -> Result<Self, PkiSystemInitError> {
+        Err(PkiSystemInitError::NotAvailable)
+    }
+
+    pub async fn open_certificate(
+        &self,
+        _cert_ref: &X509CertificateReference,
+    ) -> Result<PkiCertificate, PkiSystemOpenCertificateError> {
+        unimplemented!("platform not supported")
+    }
+
+    pub async fn list_user_certificates(
+        &self,
+    ) -> Result<Vec<AvailablePkiCertificate>, PkiSystemListUserCertificateError> {
+        unimplemented!("platform not supported");
+    }
+}
