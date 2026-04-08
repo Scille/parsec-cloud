724b624fb20d39c58718230c10b43544fefd965f
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2026-04-08 18:05:58 +0200

    Add UNIX mocked implementation for `platform_pki_2_electric_boogaloo`

---
+++ libparsec/crates/platform_pki_2_electric_boogaloo/src/unix/mod.rs
@@ -0,0 +1,9 @@
+// Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 2016-present Scille SAS
+
+mod pki_certificate;
+mod pki_private_key;
+mod pki_system;
+
+pub use pki_certificate::*;
+pub use pki_private_key::*;
+pub use pki_system::*;
---
+++ libparsec/crates/platform_pki_2_electric_boogaloo/src/unix/pki_certificate.rs
@@ -0,0 +1,37 @@
+// Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 2016-present Scille SAS
+
+use libparsec_types::prelude::*;
+use rustls_pki_types::CertificateDer;
+
+use crate::{
+    PkiCertificateGetDerError, PkiCertificateGetValidationPathError,
+    PkiCertificateRequestPrivateKeyError, PkiCertificateToReferenceError, PkiPrivateKey,
+    X509ValidationPathOwned,
+};
+
+#[derive(Debug)]
+pub struct PlatformPkiCertificate {}
+
+impl PlatformPkiCertificate {
+    pub async fn get_der(&self) -> Result<CertificateDer<'static>, PkiCertificateGetDerError> {
+        unimplemented!("platform not supported");
+    }
+
+    pub async fn request_private_key(
+        &self,
+    ) -> Result<PkiPrivateKey, PkiCertificateRequestPrivateKeyError> {
+        unimplemented!("platform not supported");
+    }
+
+    pub async fn to_reference(
+        &self,
+    ) -> Result<X509CertificateReference, PkiCertificateToReferenceError> {
+        unimplemented!("platform not supported");
+    }
+
+    pub async fn get_validation_path(
+        &self,
+    ) -> Result<X509ValidationPathOwned, PkiCertificateGetValidationPathError> {
+        unimplemented!("platform not supported")
+    }
+}
