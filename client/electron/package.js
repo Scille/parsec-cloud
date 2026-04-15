6f3af0e11a1d8a579e60f849484262132caf274e
Firelight Flagboy <firelight.flagboy@gmail.com>
2026-04-15 13:07:23 +0200

    Bump version 3.7.10-rc.1 -> 3.7.10-rc.2+dev

--- client/electron/package.js
+++ client/electron/package.js
@@ -145,7 +145,7 @@ const options = {
   appId: 'ParsecCloud.Parsec.Parsec.3',
   productName: 'Parsec',
   artifactName: ARTIFACT_NAME,
-  buildVersion: '3.7.10-rc.1',
+  buildVersion: '3.7.10-rc.2+dev',
   protocols: {
     name: 'Parsec-v3',
     schemes: [PARSEC_SCHEME],
