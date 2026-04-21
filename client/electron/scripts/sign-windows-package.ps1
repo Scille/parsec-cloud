0ce64e6fa7f6515a890d7a2139b9f2a7d7d5b5b9
Firelight Flagboy <firelight.flagboy@gmail.com>
2026-04-21 13:25:41 +0200

    fix(sign-windows): Add `-Hardened` param to build & sign hardened build
    - Update cmd script to pass extra argument to powershell script
    - Python script call cmd script with param `-Hardened` when needed

--- client/electron/scripts/sign-windows-package.ps1
+++ client/electron/scripts/sign-windows-package.ps1
@@ -1,18 +1,40 @@
-# This script is used to create a signed release of Parsec
-# using a single command from the pre-built windows artefact.
+<#
+.SYNOPSIS
+  Create a signed release of Parsec
 
-# This is meant to be run in the `client/electron` directory.
+.DESCRIPTION
+  This script is used to create a signed release of Parsec
+  using a single command from the pre-built windows artefact.
+
+  This is meant to be run in the `client/electron` directory.
+
+.PARAMETER Hardened
+    Perform the signature for an hardened build
+#>
+[CmdletBinding()]
+param(
+    [switch] $Hardened
+)
+# Check node version
+$expected_node_version = "v22.14.0"
+$node_version = node --version
+$need_to_install_node = ($LastExitCode -ne 0) -or (-Not ($node_version -eq $expected_node_version))
 
 # Stop at first error
 # Note this only works for shell commands, executables return code must still
 # be checked manually :/
+# NB: We set it after node checks because else it will stop at the first failed check preventing installation from fnm
 $ErrorActionPreference = "Stop"
 
-# Check node version
-$expected_node_version = "v22.14.0"
-$node_version = node --version
-if (-Not ($node_version -eq $expected_node_version)) {
+if ($need_to_install_node) {
+    fnm env --use-on-cd --shell powershell | Out-String | Invoke-Expression
     fnm use $expected_node_version
+} else {
+    echo "Node is installed with the correct version"
+}
+
+if ($Hardened) {
+    echo "Doing an hardened build!"
 }
 
 # Cleanup dist directory
@@ -20,11 +42,17 @@ if (Test-Path dist) {
     Remove-Item dist -r -force
 }
 
+Set-PSDebug -Trace 1
 # Install node-modules
 npm clean-install
 
 # Build and sign the release
-npm run electron:sign
+if ($Hardened) {
+    node package.js --mode prod --sign --hardened
+} else {
+    npm run electron:sign
+}
+Set-PSDebug -Trace 0
 if ($LastExitCode -ne 0) {
     exit $LastExitCode
 }
@@ -36,13 +64,18 @@ if (Test-Path -Path upload) {
 New-Item "upload" -Type Directory | Out-Null
 
 # Rename `latest.yml` file to include architecture
-Copy-Item dist\latest.yml dist\latest-win-x86_64.yml
+if ($Hardened) {
+    Copy-Item dist\latest.yml dist\latest-hardened-x86_64.yml
+} else {
+    Copy-Item dist\latest.yml dist\latest-win-x86_64.yml
+}
+
 
 # Loop over 3 files:
 # - Parsec_<version>_win_x86_64.exe
 # - Parsec_<version>_win_x86_64.exe.blockmap
 # - latest-win-x86_64.yml
-Get-ChildItem "dist\*" -Include "Parsec*.exe*","latest-win*.yml" |
+Get-ChildItem "dist\*" -Include "Parsec*.exe*","latest-*.yml" |
 Foreach-Object {
     $Source = $_.FullName
     $Destination = Join-Path ".\upload" $_.Name
