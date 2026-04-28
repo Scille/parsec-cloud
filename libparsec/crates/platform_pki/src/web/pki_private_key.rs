3fd012c43cb9774df6d0ebb6d1e2e4e30307797f
Firelight Flagboy <firelight.flagboy@gmail.com>
2026-04-28 16:39:08 +0200

    feat(pki): Impl `PlatformPkiPrivateKey::sign` for web

--- libparsec/crates/platform_pki/src/web/pki_private_key.rs
+++ libparsec/crates/platform_pki/src/web/pki_private_key.rs
@@ -1,17 +1,40 @@
 // Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 2016-present Scille SAS
 
+use std::fmt::Debug;
+
 use crate::{PkiPrivateKeyDecryptError, PkiPrivateKeySignError};
-use libparsec_types::prelude::*;
+use libparsec_types::prelude::{anyhow, Bytes, PKIEncryptionAlgorithm, PkiSignatureAlgorithm};
 
 #[derive(Debug)]
-pub struct PlatformPkiPrivateKey {}
+pub struct PlatformPkiPrivateKey(pub(super) scwsapi::PrivateKey);
 
 impl PlatformPkiPrivateKey {
     pub async fn sign(
         &self,
-        _message: &[u8],
+        message: &[u8],
     ) -> Result<(PkiSignatureAlgorithm, Bytes), PkiPrivateKeySignError> {
-        unimplemented!("platform not supported")
+        self.0
+            .sign(message)
+            .await
+            .map_err(|e| match e {
+                scwsapi::SignError::CannotGenerateSignatureConfig(_) => {
+                    PkiPrivateKeySignError::Internal(e.into())
+                }
+                scwsapi::SignError::SignError(js_value) => PkiPrivateKeySignError::Sign(
+                    anyhow::anyhow!("Cannot sign message ({js_value:?})"),
+                ),
+            })
+            .and_then(|(inner_algo, signature)| {
+                let algo = match inner_algo {
+                    scwsapi::SignatureAlgorithm::RsassaPssSha256 => {
+                        PkiSignatureAlgorithm::RsassaPssSha256
+                    }
+                    scwsapi::SignatureAlgorithm::Pkcs1Sha256 => {
+                        return Err(PkiPrivateKeySignError::UnsupportedAlgorithm)
+                    }
+                };
+                Ok((algo, signature.into()))
+            })
     }
 
     pub async fn decrypt(
