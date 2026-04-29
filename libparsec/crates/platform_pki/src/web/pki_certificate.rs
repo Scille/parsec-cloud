ecd72be63ec986f632161cdbb70ec6c33b3cda8b
Aurelia <aurelia.dolo@gmail.com>
2026-04-29 16:36:33 +0200

    [SCWS] Implement get_validation_path for PlatformPkiCertificate

--- libparsec/crates/platform_pki/src/web/pki_certificate.rs
+++ libparsec/crates/platform_pki/src/web/pki_certificate.rs
@@ -1,5 +1,7 @@
 // Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 2016-present Scille SAS
 
+use std::collections::VecDeque;
+
 use libparsec_types::prelude::*;
 use rustls_pki_types::CertificateDer;
 
@@ -42,7 +44,53 @@ impl PlatformPkiCertificate {
     pub async fn get_validation_path(
         &self,
     ) -> Result<X509ValidationPathOwned, PkiCertificateGetValidationPathError> {
-        unimplemented!("platform not supported")
+        let trust = self.0.get_trust().await.map_err(|_| {
+            PkiCertificateGetValidationPathError::Internal(anyhow::anyhow!("unable to get trust"))
+        })?;
+        if trust.status().is_err() {
+            return Err(PkiCertificateGetValidationPathError::Untrusted);
+        }
+        let mut certs: VecDeque<_> = trust.cert_path().into_iter().collect();
+
+        // Current cert
+        let leaf = certs
+            .pop_front()
+            .ok_or(PkiCertificateGetValidationPathError::Internal(
+                anyhow::anyhow!("no leaf cert"),
+            ))?;
+        let leaf = leaf
+            .get_der()
+            .await
+            .map_err(|e| PkiCertificateGetValidationPathError::Internal(e.into()))?;
+        // root cert
+        let root = certs
+            .pop_back()
+            .ok_or(PkiCertificateGetValidationPathError::Internal(
+                anyhow::anyhow!("no root cert"),
+            ))?;
+        let root = root
+            .get_der()
+            .await
+            .map_err(|e| PkiCertificateGetValidationPathError::Internal(e.into()))?;
+        let root = webpki::anchor_from_trusted_cert(&root.into())
+            .map(|anchor| anchor.to_owned())
+            .map_err(|e| PkiCertificateGetValidationPathError::Internal(e.into()))?;
+
+        // Intermediates certs
+        let mut intermediates = Vec::with_capacity(certs.len());
+        for c in certs {
+            intermediates.push(
+                c.get_der()
+                    .await
+                    .map_err(|e| PkiCertificateGetValidationPathError::Internal(e.into()))?,
+            )
+        }
+
+        Ok(X509ValidationPathOwned {
+            leaf,
+            root,
+            intermediates,
+        })
     }
 }
 
