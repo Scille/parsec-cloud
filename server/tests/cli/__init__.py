ba5bde26f93ad05fbdbdc80817c94b4b9712da8b
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2026-05-07 10:39:07 +0200

    Add a test for the server CLI to ensure the environ variable names length < 64 characters

--- server/tests/cli/__init__.py
+++ server/tests/cli/__init__.py
@@ -1,1 +1,22 @@
 # Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 2016-present Scille SAS
+
+import click
+
+from parsec.cli import cli
+
+
+# Some PAAS services (e.g. Scalingo) has a 64 character limit on the environ variable name
+def test_environ_variable_name_size():
+    def _recursive(item: click.Command):
+        if isinstance(item, click.Group):
+            for cmd in item.commands.values():
+                _recursive(cmd)
+        else:
+            for param in item.params:
+                if param.envvar is None:
+                    continue
+                assert len(param.envvar) < 64, (
+                    f"Command `{item.name}`: environ variable name too long `{param.envvar}`"
+                )
+
+    _recursive(cli)
