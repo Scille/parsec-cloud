be17c044786e4818795fc0833a986cf28033b5e9
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2026-07-07 15:33:38 +0200

    PostgreSQL implementation of `cryptpad_register_session` on server

--- server/parsec/components/postgresql/migrations/datamodel.sql
+++ server/parsec/components/postgresql/migrations/datamodel.sql
@@ -766,9 +766,27 @@ CREATE TABLE totp_opaque_key (
 
 
 -------------------------------------------------------
---  Migration
+--  Cryptpad
 -------------------------------------------------------
 
+CREATE TABLE cryptpad_session (
+    _id SERIAL PRIMARY KEY,
+    organization INTEGER REFERENCES organization (_id) NOT NULL,
+    document_id UUID NOT NULL,
+    author INTEGER REFERENCES device (_id) NOT NULL,
+    created_on TIMESTAMPTZ NOT NULL,
+    key_index INTEGER NOT NULL,
+    encrypted_view_key BYTEA NOT NULL,
+    -- NULL if the session has been created by a user without write
+    -- access on the realm (hence making the session read-only).
+    encrypted_edit_key BYTEA,
+    UNIQUE (organization, document_id)
+);
+
+
+-------------------------------------------------------
+--  Migration
+-------------------------------------------------------
 
 CREATE TABLE migration (
     _id INTEGER PRIMARY KEY,
