6f0811803eae786c18b0b7c81aa118dc27b4d29e
Maxime GRANDCOLAS <maxime.grandcolas@scille.fr>
2026-07-15 06:47:42 +0200

    [MS] Properly disable stripe in tests

--- client/tests/e2e/helpers/mock.ts
+++ client/tests/e2e/helpers/mock.ts
@@ -1,9 +1,20 @@
 // Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 2016-present Scille SAS
 
+import { BrowserContext } from '@playwright/test';
 import { MsPage, ProtectionMethod } from '@tests/e2e/helpers/types';
 
 export async function mockAllowedProtectionMethods(page: MsPage, allowedMethods: Array<ProtectionMethod>): Promise<void> {
   await page.evaluate((methods) => {
     (window as any).TESTING_MOCK_ALLOWED_PROTECTION_METHODS = methods;
   }, allowedMethods);
+}
+
+export async function mockStripe(context: BrowserContext): Promise<void> {
+  await context.route('https://js.stripe.com/**', async (route) => {
+    await route.fulfill({
+      status: 200,
+      contentType: 'text/plain',
+      body: '',
+    });
+  });
 }
