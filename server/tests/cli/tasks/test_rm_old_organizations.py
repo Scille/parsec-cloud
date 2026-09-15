3f6dbf584a1a152a032e8dcefa87baefca73a0f9
Firelight Flagboy <firelight.flagboy@gmail.com>
2026-09-15 11:49:52 +0200

    fixup! feat(server): Add tasks to remove old organization

--- server/tests/cli/tasks/test_rm_old_organizations.py
+++ server/tests/cli/tasks/test_rm_old_organizations.py
@@ -9,25 +9,19 @@ from click.testing import CliRunner
 from parsec.cli.options import Duration
 from parsec.cli.tasks import delete_old_organization
 from parsec.cli.testbed import TestbedBackend
-from parsec.config import BaseDatabaseConfig, PostgreSQLDatabaseConfig
+from parsec.components.organization import Organization
+from tests.common.client import CoolorgRpcClients
 
 
-def test_delete_old_organization_cmd(
-    db_config: BaseDatabaseConfig, db_args: list[str], testbed: TestbedBackend
-):
+def test_delete_old_organization_cmd(db_args: list[str], testbed: TestbedBackend):
     runner = CliRunner()
-    args = [*db_args, "--remove-older-than=1d", "--blockstore=MOCKED"]
-    use_pg = isinstance(db_config, PostgreSQLDatabaseConfig)
+    args = [*db_args, "--remove-older-than=9999d", "--blockstore=MOCKED"]
     result = runner.invoke(delete_old_organization.cmd, args)
     # assert result.stderr_bytes == b""
     assert result.exception is None, result.exc_info[1]
     assert result.exit_code == 0
     data = json.loads(result.stdout)
-    if not use_pg:
-        assert data == {}
-    else:
-        assert isinstance(data, dict)
-        assert list(data.keys()) != []
+    assert data == []
 
 
 @pytest.mark.parametrize(
@@ -45,3 +39,22 @@ def test_parse_duration(value: str, expected: timedelta):
     parser = Duration()
     got = parser.convert(value, None, None)
     assert got == expected
+
+
+async def test_rm_old_orga(coolorg: CoolorgRpcClients, testbed: TestbedBackend):
+    coolorg_org = await testbed.backend.organization.get(coolorg.organization_id)
+    assert isinstance(coolorg_org, Organization)
+
+    deletion_date = coolorg_org.created_on.add(microseconds=1)
+
+    deleted_orgs = await delete_old_organization.delete_old_organizations(
+        deletion_date, testbed.backend.organization
+    )
+
+    assert coolorg.organization_id not in deleted_orgs
+
+    deletion_date = coolorg_org.created_on
+    deleted_orgs = await delete_old_organization.delete_old_organizations(
+        deletion_date, testbed.backend.organization
+    )
+    assert coolorg.organization_id in deleted_orgs
