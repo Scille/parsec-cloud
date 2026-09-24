94cf81cd8e291f8539ddcd19cbbe7488e02c7c70
Firelight Flagboy <firelight.flagboy@gmail.com>
2026-09-24 12:14:26 +0200

    refactor(postgresql): Add delete constraints to vlob
    Co-authored-by: Marcos Medrano <786907+mmmarcos@users.noreply.github.com>

---
+++ server/tests/postgres_wipe/test_vlob_wipe.py
@@ -0,0 +1,54 @@
+# Parsec Cloud (https://parsec.cloud) Copyright (c) BUSL-1.1 2016-present Scille SAS
+
+import itertools
+
+import pytest
+from asyncpg import Record
+from asyncpg.pool import PoolConnectionProxy
+
+from parsec.backend import Backend
+from parsec.components.postgresql.vlob import PGVlobComponent
+from tests.common.client import CoolorgRpcClients
+from tests.postgres_wipe.test_account_wipe import fetch_many_row_ids_from_references
+from tests.postgres_wipe.test_sequester_wipe import fetch_org_id_from_org_name
+
+type Conn = PoolConnectionProxy[Record]
+
+
+@pytest.mark.postgresql
+async def test_vlob_cascade_wipe(coolorg: CoolorgRpcClients, backend: Backend):
+    vlob = backend.vlob
+    assert isinstance(vlob, PGVlobComponent)
+
+    # 1. Acquire related info about the realm vlobs
+    async with vlob.pool.acquire() as conn:
+        pg_org_id = await fetch_org_id_from_org_name(conn, coolorg.organization_id)
+        realm_ids = await fetch_many_row_ids_from_references(
+            conn, "realm", "organization", (pg_org_id,)
+        )
+        assert realm_ids
+
+        vlob_atom_ids = await fetch_many_row_ids_from_references(
+            conn, "vlob_atom", "realm", realm_ids
+        )
+        assert vlob_atom_ids
+
+        realm_vlob_update_ids = await fetch_many_row_ids_from_references(
+            conn, "realm_vlob_update", "realm", realm_ids
+        )
+        assert realm_vlob_update_ids
+
+    # 2. Wipe the realm vlobs
+    async with vlob.pool.acquire() as conn:
+        await conn.fetchmany(
+            "DELETE FROM vlob_atom WHERE _id = $1", ((id,) for id in vlob_atom_ids)
+        )
+
+    # 3. Verify that the wipe has cascaded
+    async with vlob.pool.acquire() as conn:
+        for table, id in itertools.chain(
+            (("vlob_atom", id) for id in vlob_atom_ids),
+            (("realm_vlob_update", id) for id in realm_vlob_update_ids),
+        ):
+            row_id = await conn.fetchval(f"SELECT _id FROM {table} WHERE _id = $1", id)
+            assert row_id is None, f"Table {table} still has a row linked to the deleted vlob"
